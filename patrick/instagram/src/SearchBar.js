5826ffbb67a7596e97fa30a9dd00cdf91f802662
mixelpixel <mixelpix@mac.com>
2017-07-25 16:32:31 -0400

    logo

--- patrick/instagram/src/SearchBar.js
+++ patrick/instagram/src/SearchBar.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './SearchBar.css';
-
+import logo from './logo.svg';
 
 
 
 const SearchBar = (props) => {
   return (
     <div className="masthead">
+      <img src={logo} className="App-logo" alt="logo" />
       &loz; | Instagram&nbsp;
       <input
         type="text"
         placeholder="...I can help!"
       />
       &nbsp;&lambda; | &hearts; | lol
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   )
 }
