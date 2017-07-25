c546b49a2671c39fa4550eeaf35bf848f2ea77e0
mixelpixel <mixelpix@mac.com>
2017-07-25 14:18:43 -0400

    remove comments

--- patrick/instagram/src/App.js
+++ patrick/instagram/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import SearchBar from './SearchBar';
 import PostContainer from './PostContainer';
@@ -14,22 +13,8 @@ class App extends Component {
         {/* {postData.map((data, i) => {
           return <PostContainer postData={data} />
         })} */}
-        {/* <PostContainer postData={postData} /> // Map is the preferred way! Put mapping into component tho?*/}
       </div>
     )
-
-    // // Default create-react-app return
-    // return (
-    //   <div className="App">
-    //     <div className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <h2>Welcome to React</h2>
-    //     </div>
-    //     <p className="App-intro">
-    //       To get started, edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //   </div>
-    // );
   }
 }
 
