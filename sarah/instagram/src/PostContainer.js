b28226618060bd2619d8435eda2b6e126242988b
sfmajors373 <sfmajors373@gmail.com>
2017-07-25 18:40:16 -0400

    PostContainer styling

--- sarah/instagram/src/PostContainer.js
+++ sarah/instagram/src/PostContainer.js
@@ -2,15 +2,15 @@
 // should render two image tags
 import React, { Component } from 'react';
 import CommentSection from './CommentSection';
-import './PostContainer.scss';
+import './PostContainer.css';
 
 class PostContainer extends Component {
   render() {
     return (
-      <div>
-	<div>
+      <div className="center">
+	<div className="header">
 	 <img className="thumbnail" src={this.props.thumbnail} alt='Thumbnail'/>
-	 <h4> {this.props.username}</h4>
+	 <h4 className="name"> {this.props.username}</h4>
 	</div>
 	<img className="Post" src={this.props.img} alt='PostedPicture'/>
 	<CommentSection comments={this.props.comments}/>
