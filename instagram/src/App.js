57a477bb0fa9f5433a7f71159ca89f24000628ab
mixelpixel <mixelpix@mac.com>
2017-07-24 21:38:20 -0400

    import statements for components

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CommentSection from './components/CommentSection';
+import PostContainer from './components/PostContainer';
+import SearchBar from './components/SearchBar';
 
 class App extends Component {
   render() {
