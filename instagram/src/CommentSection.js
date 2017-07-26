2b9c0c3a59e695553649e010e5fb20af0c85733d
Chooster <jcooleycoder@gmail.com>
2017-07-25 21:41:32 -0700

    Finished

--- instagram/src/CommentSection.js
+++ instagram/src/CommentSection.js
@@ -11,6 +11,7 @@ class CommentSection extends Component {
     };
     this.onSave = this.onSave.bind(this);
     this.onHeart = this.onHeart.bind(this);
+    this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this);
   }
 
   onSave(comment) {
@@ -27,6 +28,12 @@ class CommentSection extends Component {
     });
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      comments: nextProps.postData.comments,
+      hearts: nextProps.postData.likes
+    });
+  }
 
   render() {
     let imgSource = ((this.state.hearts > this.props.postData.likes) ? "http://i.imgur.com/AqiwoAc.png" : "https://d30y9cdsu7xlg0.cloudfront.net/png/65570-200.png");
@@ -39,10 +46,8 @@ class CommentSection extends Component {
         <div className="likes"><p>{this.state.hearts} likes</p></div>
         <div className="comments-div">
           <ul className="comments">
-            {this.state.comments.map((prop) => {
-              return <div>
-                  <li><strong>{prop.username}</strong>{prop.text}</li>
-              </div>
+            {this.state.comments.map((prop, i) => {
+              return <li key={i}><strong>{prop.username}</strong>{prop.text}</li>
             })}
           </ul>
         </div>
