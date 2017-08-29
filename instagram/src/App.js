9a1e81c2e1330617ae3bbab60527158a983b63db
phytertek <ryan.phytertek@gmail.com>
2017-08-28 20:07:45 -0400

    scaffolding complete

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
+import { postData } from './data/application-data'
+
+import { SearchBar, PostContainer } from './Components'
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+  
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
       </div>
     );
   }
