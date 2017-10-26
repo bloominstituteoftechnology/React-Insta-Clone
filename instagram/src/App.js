b11d94181fc8ee6a1b866469caaee733976d9254
Jaosn Nuhn <jasonnuhn@Jaosns-Air.fios-router.home>
2017-10-26 10:25:52 -0400

    current commit

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -11,11 +11,9 @@ class App extends Component {
     super();
     this.state = {
       query: '',
-      comment: '',
       postData: []
     }
     this.handleQuery = this.handleQuery.bind(this);
-    this.handleComment = this.handleComment.bind(this);
   }
 
   componentDidMount() {
@@ -24,10 +22,6 @@ class App extends Component {
 
   handleQuery(event) {
     this.setState({query: event.target.value});
-  }
-
-  handleComment(event) {
-    this.setState({comment: event.target.value});
   }
 
   render() {
--- instagram/src/Comments.js
+++ instagram/src/Comments.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import './Search.css';
 
-const Comment = (props) => {
-  return (
-    <div className="background">
-      <div className="appName">JaPANgram</div>
-      <input type="text" value={props.comment} onChange={props.changeHandler} />
-      <button onClick={props.addToList} className="input__button">Add a comment</button>
-      <p></p>
-    </div>
-  )
-}
 
 export default Comment;
