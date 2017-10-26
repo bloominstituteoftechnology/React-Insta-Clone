56d895b550403701c461aecaf777eab172a741ef
Alex Cassell <alex@simulism.net>
2017-10-26 11:23:20 -0400

    Almost

--- instagram/src/InstagramList.js
+++ instagram/src/InstagramList.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './InstagramList.css';
 
+
 const InstagramList = (props) => {
     return (
         <div className="InstagramList">
             {props.postData.map((item) => {
+                // const com = {item:item.comments};
                 return(
                     <div className="InstagramList__item" key={item.id}>
-                    <h3>{item.username}</h3>
+                        <div className="InstagramList__item__topWrapper">
+                            <img className="InstagramList__item__topWrapper__thumbnail" src= {item.thumbnailUrl} alt={item.username}></img>
+                            <h3>{item.username}</h3>
+                    </div>
                         <img className="InstagramList__item__mainImages" src= {item.imageUrl} alt={item.username}></img>
                         <div className="InstagramList__item__bottomSection">
-                        <div className="InstagramList__item__bottomSection__likes">{item.likes}</div>
+                        <div className="InstagramList__item__bottomSection__likes">{item.likes} likes</div>
                         <img src="./images/icons.png"></img>
-                        {/* {let length = {item.comments.length};
-                        for(let i = 0; i < length; i++){
-                        } */}
+                        {/* <ul>
+                        {this.props.com.map(function(listValue){
+                          return <li>{listValue}</li>;
+                        })}
+                      </ul> */}
                         </div>
                     </div>
                 )
