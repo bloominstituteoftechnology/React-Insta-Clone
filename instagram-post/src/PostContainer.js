1dbdfb50d25fd68308083a6750253dc3269cd533
Rashmi Baheti <baheti.rash123@gmail.com>
2017-10-25 22:33:17 -0700

    completed Image bottom part

--- instagram-post/src/PostContainer.js
+++ instagram-post/src/PostContainer.js
@@ -6,12 +6,29 @@ const PostContainer = (props) => {
 		<div>
 			{props.appData.map((data) => {
 				return (
-					<div className="img-container">
-						<div className="img-header">
-							<img src={data.thumbnailUrl} alt={data.username} width="50" height="50"/>
-							<h3>{data.username}</h3>
+					<div><br/><br/>
+						<div className="img-container">
+							<div className="img-header">
+								<img src={data.thumbnailUrl} alt={data.username} width="50" height="50"/>
+								<h3>{data.username}</h3>
+							</div>
+							<img src={data.imageUrl} alt={data.username} width="550" height="550"/>
+							<div className="img-caption">
+								<div className="unicode-size">&#9825; &#128489;</div>
+								<div>{data.likes} likes</div>
+								<div className="comment-section">
+									{data.comments.map((comment) => {
+										return (
+											<div>
+												<div><b>{comment.username}</b> {comment.text}</div>
+											</div>
+										)
+									})}
+								</div>
+								<br/>
+								<div>{data.timestamp}</div>
+							</div>
 						</div>
-						<img src={data.imageUrl} alt={data.username} width="550" height="550"/>
 					</div>
 				)
 			})}
