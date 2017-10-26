6f29e2f76270003024bf9a32692cdb3d19a2fbf2
Jaosn Nuhn <jasonnuhn@Jaosns-Air.fios-router.home>
2017-10-26 10:59:05 -0400

    commit comment.js

--- instagram/src/Comments.js
+++ instagram/src/Comments.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import './Search.css';
 
-
+const Comment = (props) => {
+  return (
+    <div className="background">
+      <div className="appName">Comment</div>
+      <input type="text" value={props.comment} onChange={props.changeHandler} />
+      <button onClick={props.comment} className="input__button">Comment</button>
+      <p></p>
+    </div>
+  )
+}
 export default Comment;
