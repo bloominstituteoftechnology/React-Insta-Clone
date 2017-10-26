7d5e42078972997eb0836e95bce2502f5dba4140
Jesse Reichel <31119775+KryoKorpz@users.noreply.github.com>
2017-10-26 11:56:25 -0700

    final changes

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import postData from './application-data.js';
+import PostContainer from './PostContainer.js';
+import CommentSection from './CommentSection.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state= {
+      postData: [],
+      comments: []
+  }
+}
+
+componentDidMount(){
+  this.setState({postData});
+}
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <PostContainer postData={this.state.postData} />
+        <CommentSection comments={this.state.postData} />
       </div>
     );
   }
