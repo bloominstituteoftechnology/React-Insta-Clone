e443e46628da56f7156af700e07db2fc04ca5885
Rashmi Baheti <baheti.rash123@gmail.com>
2017-10-26 09:55:38 -0700

    creating search bar

--- instagram-post/src/PostContainer.js
+++ instagram-post/src/PostContainer.js
@@ -4,15 +4,23 @@ import './PostContainer.css';
 const PostContainer = (props) => {
 	return (
 		<div>
+			<div className="navbar"></div>
+			<div><br/><br/></div>
+			<form>
+				<input type="text" name="search" placeholder="Search"/>
+			</form>
+			<div className="horizontal-bar"></div>
 			{props.appData.map((data) => {
 				return (
 					<div><br/><br/>
 						<div className="img-container">
 							<div className="img-header">
-								<img src={data.thumbnailUrl} alt={data.username} width="50" height="50"/>
-								<h3>{data.username}</h3>
+							    <div className="header-icon-div">
+									<img className="header-icon-img" src={data.thumbnailUrl} alt={data.username}/>
+								</div>
+								<div className="header-username">{data.username}</div>
 							</div>
-							<img src={data.imageUrl} alt={data.username} width="550" height="550"/>
+							<img class="img-post" src={data.imageUrl} alt={data.username} width="550" height="550"/>
 							<div className="img-caption">
 								<div className="unicode-size">&#9825; &#128489;</div>
 								<div>{data.likes} likes</div>
@@ -20,7 +28,7 @@ const PostContainer = (props) => {
 									{data.comments.map((comment) => {
 										return (
 											<div>
-												<div><b>{comment.username}</b> {comment.text}</div>
+													<div><b>{comment.username}</b> {comment.text}</div>
 											</div>
 										)
 									})}
