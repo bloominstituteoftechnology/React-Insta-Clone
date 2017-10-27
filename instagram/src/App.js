35a9fce49f6ea8896afc30e4012e9025e75ea04e
Sam Kim <sams404link@gmail.com>
2017-10-27 14:12:00 -0700

    React-II updated

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import postData from './application-data.js'
-import PostList from './PostList.js'
+import postData from './application-data.js';
+import PostList from './PostList.js';
 
 class App extends Component {
   constructor() {
