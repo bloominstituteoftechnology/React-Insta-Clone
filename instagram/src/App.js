db910e15fcfc83f06c4ab210f4ec0cc29d7c2d48
sarika <sarikaresume@gmail.com>
2017-12-07 11:57:49 -0800

    react 2 instagram

---
+++ instagram/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import dummyData from './dummy-data';
+import './App.css';
+console.log(dummyData);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        add map ADD MAP INFO 
+        <p className="App-intro">
+          To get started, <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
