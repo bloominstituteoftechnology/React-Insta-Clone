2227d260e4486c3ee1bc8ca9e11eb91296bca50a
Pat Collins <padjo@mac.com>
2017-12-08 07:54:55 +0000

    everyting is working with the help of the solution lecture

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import { Navbar, Button } from 'react-bootstrap';
+import React, { Component } from 'react'
+import { Navbar, Button } from 'react-bootstrap'
 
-const SearchBar = () => (
-    <Navbar>
-        <Navbar.Header>
-            <Navbar.Brand>
-                <a href="#"></a>
-            </Navbar.Brand>
-        </Navbar.Header>
-        <Navbar.Form pullLeft>
-            <input type="text" placeholder="search instagram" />
-            <Button type="submit">Submit</Button>
-        </Navbar.Form>
-    </Navbar>
-);
+class SearchBar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            searchInput: ''
+        };
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Search input: ', this.state.searchInput);
+        this.setState({ searchInput: '' });
+    }
+
+    handleSearchInput = (e) => {
+        this.setState({ searchInput: e.target.value });
+    }
+
+    render() {
+        return (
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">Lambda School React II</a>
+                    </Navbar.Brand>
+                </Navbar.Header>
+                <Navbar.Form pullLeft>
+                    <input type="text" placeholder="Search" value={this.state.searchInput} onChange={this.handleSearchInput} />
+                    <Button type="submit" onClick={this.handleSubmit}>Submit</Button>
+                </Navbar.Form>
+            </Navbar>
+        )
+    }
+}
 
 export default SearchBar;
