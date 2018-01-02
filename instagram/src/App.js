748236ed6c48fd058f39bf8fe95f34f51592ab28
Kwaku Dapaah <kwakudapaah@Kwakus-MBP.fios-router.home>
2018-01-02 14:59:20 -0500

    changes made again

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data';
 import PostContainer from './components/PostContainer/PostContainer'
--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,44 +1,54 @@
 import React, { Component } from 'react';
 
 class CommentSection extends Component {
-  constructor() {
-  	super();
-  	this.state = {
-  		commentList: [],
-  		newComment: ''
-  	}
-  }
+    constructor() {
+        super();
+        this.state = {
+            comments: [],
+            newComment: ''
+        };
+    }
 
-  componentDidMount() {
-    this.setState( {
-    	commentList: this.props.comments
-    })
-  }
+    componentDidMount() {
+        this.setState({
+            comments: this.props.comments
+        });
+    }
 
-  handleinputComment = (e) => {
-    this.setState( {
-    	newComment: e.target.value
-    })
-  }
-  
-  addComment = () => {
-   const commentList1 = this.state.commentList;
-   commentList1.push(this.state.newComment);
-   this.setState({
-   	  commentList: commentList1,
-   	  newComment: ''
-   })
-  }
+    addComment = (e) => {
+        e.preventDefault();
+        const commentObj = {
+            username: 'Kwaku',
+            text: this.state.newComment
+        };
+        const commentArray = this.state.comments;
+        commentArray.push(commentObj);
+        this.setState({
+            comments: commentArray,
+            newComment: ''
+        });
+    }
 
-  render() {
-  	return(
-      <div>
-      <div>{this.state.commentList}</div>
-      <div><input onSubmit={this.handleinputComment} placeholder="..." value={this.state.newComment} /><span>
-      </span>
-      </div>
-      </div>
-    ); 
-  }
+    handleCommentInput = (e) => {
+        this.setState({ newComment: e.target.value });
+    }
+
+    render() {
+        return (
+            <div className="comment-section">
+                {this.state.comments? this.state.comments.map((comment, i) => ( 
+                    <div className="comment" key={i}>
+                        <div className="comment-user"><b>{comment.username}</b></div>
+                        <div className="comment-text">{comment.text}</div>
+                    </div>)
+                ) : null}
+                 <div className="comment-input">
+                    <input type="text" placeholder="..." value={this.state.newComment} onChange={this.handleCommentInput} />
+                    <button type="submit" onClick={this.addComment}>Submit</button>
+                </div>
+            </div>
+        );
+    }
 }
+
 export default CommentSection;
