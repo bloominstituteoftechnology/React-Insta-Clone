e7b7fdd022b1054db9db3ae6c82638ec83109b6c
Clara Andrew <candrewwani@gmail.com>
2018-01-11 13:54:37 -0500

    add comments working, no styling

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
 
 class CommentSection extends Component {
+    constructor() {
+        super();
+        this.state = {
+            newComment: '',
+            allComments: []
+
+        }
+    }
+
+    componentDidMount = () => {
+        this.setState({
+            allComments: this.props.comments
+        })
+    }
+
+    onChange = (event) => {
+        this.setState({
+            newComment: event.target.value
+        })
+    }
+
+    addComment = () => {
+        const myComment = this.state.newComment;
+        const myEntireComment = {
+            username: 'Clara',
+            text: myComment
+        };
+        const allComments = this.state.allComments;
+        allComments.push(myEntireComment);
+        this.setState({
+            allComments,
+            newComment: ''
+        })
+    }
+
     render() {
         return (
             <div>
                 <div>
-                    {props.comments.map((comment, index) => {
+                    {this.state.allComments.map((comment, index) => {
                         return <div key={index}>
                             {comment.username} {' '} {comment.text}
                         </div>
                     })}
                 </div>
                 <div>
-                    <input placeholder="Add a comment..." />
+                    <input placeholder="Add a comment..." value={this.state.newComment} onChange={this.onChange}/>
+                    <button onClick={this.addComment}>Submit</button>
                 </div>
             </div>
         );
