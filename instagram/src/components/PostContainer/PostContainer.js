1349ab170a6edf6aef5226256311ffd639493271
John C <johnjohnx4@gmail.com>
2018-01-11 14:07:03 -0500

    final changes

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import CommentSection from '../CommentSection/CommentSection.js';
+import './PostContainer.css'
+
+const PostContainer = ({ dummydata }) => {
+    return (
+        <article className="post">
+            <header className="post__user">
+                <img src={dummydata.thumbnailUrl} alt="user" className="post__user__thumbnail" />
+                <div className="post__user-text"><b>{dummydata.username}</b></div>
+            </header>
+            <div>
+                <img src={dummydata.imageUrl} alt="post" className="post__user__image"/>
+            </div>
+            <div>
+                <CommentSection comments={dummydata.comments} timestamp={dummydata.timestamp} likes={dummydata.likes}/>
+            </div>
+        </article>
+    );
+}
+
+
+export default PostContainer;
