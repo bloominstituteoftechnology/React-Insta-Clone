89bc1e92478075bf9689a3d760ee9e3325d0324b
Camila Daniels <camila.daniels926@gmail.com>
2018-01-11 13:06:02 -0600

    It's starting to look a little better. Still need to work on comment section. Camila Daniels camila.daniels926@gmail.com

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -33,20 +33,20 @@ const PostContainer = (props) => {
 				return (
 					<div className="PostContainer">
 						<div className="PostContainer_header">
-							<img src={container.thumbnailUrl}/>
-							<h3 className="PostContainer_username">{container.username}</h3>
+							<img className="PostContainer_thumbnailUrl" width="30" height="30" src={container.thumbnailUrl}/>
+							<p className="PostContainer_username">{container.username}</p>
 						</div>
 
 						<div className="PostContainer_image">
-							<img src={container.imageUrl}/>
+							<img className="PostContainer_imageUrl" src={container.imageUrl}/>
 						</div>
 
 						<div className="PostContainer_body">
 							<div className="PostContainer_indications">
-								<h3 className="PostContainer_likes">{container.likes} likes</h3>
+								<p className="PostContainer_likes">{container.likes} likes</p>
 							</div>
 
-							<CommentSection comments={container.comments} timestamp={container.timestamp}/>
+							<CommentSection className="CommentSection" comments={container.comments} timestamp={container.timestamp}/>
 						</div>
 					</div>
 				);
