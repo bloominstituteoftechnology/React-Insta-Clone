7ab046d4fd3f11d12f38b1c06b2b6bf6cb1f71b8
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-02-06 16:17:01 -0500

    Finished setting up file structure

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
 
 class App extends Component {
+  state = {dummyData};
+  // console.log(this.state);
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +18,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        
       </div>
     );
   }
---
+++ instagram/src/components/CommentSection/comment-section.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+
+
+export default CommentSection;
