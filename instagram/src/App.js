def583fdddedcec872d737671b2aabe2e455f62b
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-06 14:17:24 -0600

    add import to App.js
    lines 3-5: import components
    
    Also adds component files and styles

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import CommentSection from './components/CommentSection/CommentSection.js';
+import PostContainer from './components/PostContainer/PostContainer.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
 import './App.css';
 
 class App extends Component {
