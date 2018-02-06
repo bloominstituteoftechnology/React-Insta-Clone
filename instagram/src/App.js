af5cb975b9724a3f3e280941b9330156e903781b
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-06 16:02:20 -0600

    add PostContainer iteration

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import CommentSection from './components/CommentSection/CommentSection.js';
 import PostContainer from './components/PostContainer/PostContainer.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
 import './App.css';
+import dummyData from './dummy-data';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app__container">
+        <SearchBar />
+        <div>
+          {dummyData.map((post) => {
+            return (
+              <div>
+                <PostContainer
+                  key={post.username}
+                  post={post}
+                />
+              </div>
+            )
+          })}
+        </div>
       </div>
     );
   }
