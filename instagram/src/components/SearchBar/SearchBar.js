4695186b4c1537c78bd360637d21c0abdec2d982
Smithface <kylewibblesmith@gmail.com>
2018-02-06 16:59:38 -0500

    added search Bar in middle
    using flexbox to space out the elements as I want them

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import './SearchBar.css';
 
 function SearchBar() {
-  return <div>hi</div>;
+  return (<div className="container">
+    <div className="container__box container__box-1">
+      <div>hi</div>
+      <div>hello</div>
+      <div>hey</div>
+    </div>
+    
+    <div className="container__box container__box-2">
+      <form>
+        <input type="text" placeholder="Search"/>
+      </form>
+    </div>
+
+    <div className="container__box container__box-3">
+      <div>hey</div>
+      <div>hi</div>
+      <div>hello</div>
+    </div>
+
+  </div>);
 }
 
 export default SearchBar;
