5695ae10e432dfb7286a7f1c0c83c2a3b101d2ed
Unknown <peter@petergraycreative.com>
2018-02-07 09:29:23 -0600

    Add SearchBar component
    Added a simple search bar component. No onSubmit or onChange built.

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 function SearchBar() {
-    return <form onSubmit={this.handleSubmit}>
+    return <form>
     <label htmlFor="search"></label>
     <input
       type="text"
-      onChange={this.handleInputChange}
+      placeholder="Search"
     />
   </form>;
 }
+
+export default SearchBar;
