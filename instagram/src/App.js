c207babd9047bcd28bcdcb799d22a1b03c4e2ee0
Unknown <peter@petergraycreative.com>
2018-02-06 23:15:56 -0600

    Remove unnecessary tags and classes
    Simplified App.js for the Post container

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -5,12 +5,11 @@ import PostContainer from './components/PostContainer/PostContainer';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        </header>
-        <p className="App-intro">
+      <div className="container">
+      <header></header>
+      <div className="posts">
           <PostContainer />
-        </p>
+      </div>
       </div>
     );
   }
