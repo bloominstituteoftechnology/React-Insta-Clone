d800abf0381e8d3b55b250f8f772d25211917bcf
Smithface <kylewibblesmith@gmail.com>
2018-02-06 19:11:28 -0500

    begin work on PostContainer
    got the basic setup down, meaning App.js now imports and calls on PostContainer to do something

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import dummyData from './dummy-data';
 import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
   render() {
@@ -9,6 +10,9 @@ class App extends Component {
       <div>
         <SearchBar />
         <hr />
+        {dummyData.map((post, i) => {
+          return <PostContainer key={i} postInfo={post} />;
+        })}
       </div>
     );
   }
---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PostContainer(props) {
+  return null;
+}
+
+export default PostContainer;
