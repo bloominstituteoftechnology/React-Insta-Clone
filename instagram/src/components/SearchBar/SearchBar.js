4048c457238e3537e3d57ecea2c6172d435a0191
rcs784 <rcstinson@hotmail.com>
2018-02-07 01:07:49 -0500

    update search bar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -5,7 +5,10 @@ import searchIcon from './search-icon.png';
 const SearchBar = () => {
   return (
     <div>
-      <input className="input" type="text" placeholder="Search" />
+      <form>
+        <input type="text" />
+      </form>
+      <div className="search-text">Search</div>
       <img className="search-icon" src={searchIcon} alt="search icon" />
     </div>
   );
