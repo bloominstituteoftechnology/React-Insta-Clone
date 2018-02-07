af1caf8c1af098250753d416d1d212f2ab05e2bc
Charlie Sparks <csparks@antioch.edu>
2018-02-06 19:51:43 -0800

    another test for patrick

---
+++ instagram/notes.md
@@ -0,0 +1,7 @@
+Notes:
+
+I haven't found a way to center the main content on the paid while keeping it in a flex-direction of row.
+
+Need to get the usernames and comments on the same line
+
+Need to add an input for comments
--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -4,7 +4,12 @@ import "./CommentSection.css";
 function CommentSection(props) {
   return (
     <div className="commentSection">
-      <h6>{props.dummy[0].username}</h6>
+        <h6 className="commentSection__username">{props.dummy[0].username}</h6>
+        <p className="commentSection__comments">{props.dummy[0].text}</p>
+        <h6 className="commentSection__username">{props.dummy[1].username}</h6>
+        <p className="commentSection__comments">{props.dummy[1].text}</p>
+        <h6 className="commentSection__username">{props.dummy[2].username}</h6>
+        <p className="commentSection__comments">{props.dummy[2].text}</p>
     </div>
     )
 }
