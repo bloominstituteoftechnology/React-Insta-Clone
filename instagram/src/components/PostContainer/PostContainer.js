f7e7d34d3f0ac231bb03eedfe0d9a472a53788c2
cliffkang <cliffgkang@gmail.com>
2018-02-06 19:14:07 -0800

    built scaffolding for PostContainer
    -just the JSX for PostContainer
    -now, time to hook it into App.js & dummy-data

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import './Bookmark.png';
-import './comment.png';
-import './ellipsis.png';
-import './heart.png';
+import bookmark from './Bookmark.png';
+import comment from './comment.png';
+import ellipsis from './ellipsis.png';
+import heart from './heart.png';
 
 function PostContainer(props) {
-
+  return( 
+    <div>
+      <div className="head">
+        <img src="" />
+        <h3>{props.}</h3>
+      </div>
+      <img src="" />
+      <div className="interactions">
+        <img src={heart} alt="like post" />
+        <img src={comment} alt="comment on post" />
+        <img src={bookmark} alt="bookmark this post" />
+      </div>
+      <CommentSection />
+      <h4>Timestamp</h4>
+      <div className="addComment">
+        <form>
+        </form>
+        <img src={ellipsis} alt="options" />
+      </div>
+    </div>
+  )
 }
 
 export default PostContainer;
