ea8df1d9bbaa0157953ac5575d5f502db43b9426
cliffkang <cliffgkang@gmail.com>
2018-02-06 19:59:17 -0800

    stuck on mutating timestamp
    - connected up PostContainer, showing elements
    - now need to style PostContainer
    - then, going to add comments to PostContainer

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,24 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data';
 import SearchBar from './components/SearchBar/SearchBar.js';
-// import PostContainer from './components/PostContainer/PostContainer.js';
+import PostContainer from './components/PostContainer/PostContainer.js';
 
 class App extends Component {
   state = dummyData;
 
+  // createTimestamp = () => {
+  //   const modifiedData = this.state.map((post) => {
+  //     post['time'] = post.timestamp.substring(post.timestamp.search(', '), post.timestamp.lastIndexOf(':')), post.timestamp.substr(str.length - 3);
+  //     return post;
+  //   });
+  //   this.setState( {modifiedData} );
+  // }
+
   render() {
     return (
       <div className="App">
+        {/*{this.createTimestamp()}*/}
         <SearchBar />
-        {/* {
+        {
           this.state.map(post => {
             return <PostContainer
-              // key={post.id} 
-              // post={post} 
-              // deleteMovie={this.removeMovie}
+              post={post} 
             />;
           })
-        } */}
+        }
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -8,17 +8,18 @@ function PostContainer(props) {
   return( 
     <div>
       <div className="head">
-        <img src="" />
-        <h3>{props.}</h3>
+        <img src={props.post.thumbnailUrl} alt="photo of poster" />
+        <h3>{props.post.username}</h3>
       </div>
-      <img src="" />
+      <img src={props.post.imageUrl} alt="post's photo" />
       <div className="interactions">
         <img src={heart} alt="like post" />
         <img src={comment} alt="comment on post" />
         <img src={bookmark} alt="bookmark this post" />
       </div>
-      <CommentSection />
-      <h4>Timestamp</h4>
+      <h4>{props.post.likes} likes</h4>
+      {/* <CommentSection /> */}
+      <h4>{props.post.timestamp}</h4>
       <div className="addComment">
         <form>
         </form>
