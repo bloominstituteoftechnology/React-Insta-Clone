f67185594fece5193b0d12d2b621581d0130105b
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-02-06 19:17:07 -0500

    Changed searchBar.js to Class
    searchBar.js --- Changed to class, thought it may be necessary for searching, really not sure...

--- instagram/src/components/SearchBar/searchBar.js
+++ instagram/src/components/SearchBar/searchBar.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import './searchBar.css';
 
-function SearchBar(props) {
+class SearchBar extends React.Component {
+  state = {
+    value: '',
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({value: ''});
+  }
+  handleInput = (event) => {
+    this.setState({value: event.target.value});
+  }
+
+  render() {
   return (
     <div className="searchBar">
     <img className="logo" src='https://c1.staticflickr.com/6/5515/10094221256_eb08c5088d_b.jpg' alt="logo"/>
-    <form>
+    <form onSubmit={this.handleSubmit}>
       <input 
         type="text" 
-        value='' 
+        value={this.state.value} 
+        onChange={this.handleInput}
       /> 
     <button type="submit">Search</button>
   </form></div>
   )
+}
 }
 
 export default SearchBar;
