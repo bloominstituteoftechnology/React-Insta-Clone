b72894e838705304acb98a7d729015ea36b747d3
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-02-06 20:30:59 -0500

    Embedded thumbnail images

--- instagram/src/components/PostContainer/post-container.js
+++ instagram/src/components/PostContainer/post-container.js
@@ -6,11 +6,9 @@ function PostContainer(props) {
   // console.log(props.username);
   return (
     <div className='post-container'>
-      <h2>Post</h2>
-      <p>User: {props.post.username}</p>
-      <p>{props.post.thumbnailUrl}</p>
-      {/* <p>{props.post.imageUrl}</p> */}
-      <img src={props.post.imageUrl} />
+      <img src={props.post.thumbnailUrl} className='thumbnail'/>
+      <p>{props.post.username}</p>
+      <img src={props.post.imageUrl} className='image'/>
       <p>{props.post.likes} Likes</p>
       <p>posted at {props.post.timestamp}</p>
       <div className='comments'>
