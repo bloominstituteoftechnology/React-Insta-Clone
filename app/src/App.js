95069f9bb8603890ccbb9ae9b59bde7bc76c50d3
Michael Marshal <mikemarshalman@gmail.com>
2018-02-07 11:04:07 -0600

    working on post container

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar.js';
+import PostContainer from './components/PostContainer/PostContainer.js'
+import dummyData from './dummy-data';
 
 class App extends Component {
+  state = dummyData;
   render() {
     return (
       <div className="App">
         <SearchBar />
+        {
+          this.state.map(post => {
+            return <PostContainer
+                post={post}
+                />;
+              })
+            }
         <header className="App-header">
           <h1 className="App-title">Welcome to Cool Instagram</h1>
         </header>
---
+++ app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './PostContainer.css';
+
+function PostContainer(props) {
+
+}
+
+export default PostContainer;
