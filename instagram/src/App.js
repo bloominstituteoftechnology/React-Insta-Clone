830cf450307f305780c00a6d4bd8ddcfaf3fc082
Unknown <peter@petergraycreative.com>
2018-02-07 09:21:09 -0600

    Add simple search bar component to app.js
    Also added container max-width.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import PostContainer from './components/PostContainer/PostContainer';
+import SearchBar from "./components/SearchBar/SearchBar";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-      <header></header>
+      <header><SearchBar /></header>
       <div className="posts">
           <PostContainer />
       </div>
