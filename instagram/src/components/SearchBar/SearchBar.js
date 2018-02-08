4cd5042a061164c7ea629ff246eead554e1c3c36
rcs784 <rcstinson@hotmail.com>
2018-02-07 22:58:27 -0500

    solve search bar bug

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -6,10 +6,12 @@ const SearchBar = () => {
   return (
     <div>
       <form>
-        <input type="text" />
+        <input className="search-input" type="text" />
       </form>
-      <div className="search-text">Search</div>
-      <img className="search-icon" src={searchIcon} alt="search icon" />
+      <div className="search-container">
+        <div className="search-text">Search</div>
+        <img className="search-icon" src={searchIcon} alt="search icon" />
+      </div>
     </div>
   );
 }
