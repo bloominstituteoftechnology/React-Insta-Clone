11aa448631781f17f226ae9f7942f2917b736bf6
Christopher Beards <beardscm@gmail.com>
2018-02-08 01:01:32 -0500

    Updated Icon Sizing

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -25,10 +25,10 @@ class NavBar extends Component {
         <div className='searchbarImg__wrapper'>
 
         <div className='menu__buttons'>        
-        <a href=""><img src="https://cdn1.iconfinder.com/data/icons/instagram-ui-glyph/48/Sed-04-128.png" alt="" /></a>
-          <a href=""><img src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-128.png" alt="" /></a>
-          <a href=""><img src="https://cdn0.iconfinder.com/data/icons/typicons-2/24/heart-outline-128.png" alt="" /></a>
-          <a href=""><img src="https://cdn3.iconfinder.com/data/icons/ecommerce-26/96/account-128.png" alt="" /></a>
+          <a href=""><img src="https://cdn4.iconfinder.com/data/icons/pictype-free-vector-icons/16/home-32.png" alt="" /></a>
+          <a href=""><img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-search-32.png" alt="" /></a>
+          <a href=""><img src="https://cdn0.iconfinder.com/data/icons/typicons-2/24/heart-outline-32.png" alt="" /></a>
+          <a href=""><img src="https://cdn2.iconfinder.com/data/icons/pittogrammi/142/87-32.png" alt="" /></a>
           </div>
         </div>
       </div>
