159aa809b797013b531d6e3b69191d0dd6a0c6e5
Glenn-David Daniel <d2rd@outlook.com>
2018-02-09 07:54:34 -0800

    WIP PostContainer syntax corrections for state

--- instagram/src/components/PostContainer/postContainer.js
+++ instagram/src/components/PostContainer/postContainer.js
@@ -31,31 +31,60 @@ const dummyData = [
 ];
 */
 
-// CHANGE CLASS NAMES TO MEET SPEC AND ADD JSX AND JS LOGIC
+
 class postContainer extends React.Component {
+	// key = username
+	state = {
+		dummyData: [
+			{
+				username: "philzcoffee",
+				thumbnailUrl: 'https://scontent-sjc2-1.cdninstagram.com/t51.2885-19/11201517_887808411287357_1307163552_a.jpg',
+				imageUrl: 'https://scontent-sjc2-1.cdninstagram.com/t51.2885-15/e35/20066915_1526002357431295_3266739979170086912_n.jpg',
+				likes: 400,
+				timestamp: "July 17th 2017, 12:42:40 pm",
+				comments: [
+					{
+						username: "philzcoffee",
+						text: "We've got more than just delicious coffees to offer at our shops!"
+					},
+					{
+						username: "biancasaurus",
+						text: "Looks delicious!"
+					},
+					{
+						username: "martinseludo",
+						text: "Can't wait to try it!"
+					},
+				],
+			}
+		]
+	}
 	render () {
 		return (
-			<div className='userHeader'>
-				<div className='userLogo'>
-					<img src={"./assets/android-icon-96x96.png"} className="App-logo" alt="logo" /> 
+			<div>
+				<div className='userHeader'>
+						<div className='userLogo'>
+							<img src={"./assets/android-icon-96x96.png"} className="App-logo" alt="logo" /> 
+						</div>
+						<div className='userName'>
+							{this.props.username}
+						</div>
 				</div>
-				<div className='userName'>
-					{this.props.username}
+				
+				<div className='postImage'>
+						<img src={"./assets/[placeholder-someImage].png"} className="postLogo" alt="image" /> 
 				</div>
-			</div>
-			<div className='postImage'>
-					<img src={"./assets/[placeholder-someImage].png"} className="postLogo" alt="image" /> 
-			</div>
-			<div className='favicons'>
-				<img src={"./assets/[placeholder-HeartIcon]"} className="App-logo" alt="logo" /> 
-				<img src={"./assets/[placeholder-CommentIcon]"} className="App-logo" alt="logo" /> 
-			</div>
-			<div className='postLikes'>
-				{this.props.likes}
-			</div>
-			<div className='postComment'>
-					{this.props.userName} + ":" + {this.props.text} 
-					{this.props.comments}
+				<div className='favicons'>
+					<img src={"./assets/[placeholder-HeartIcon]"} className="App-logo" alt="logo" /> 
+					<img src={"./assets/[placeholder-CommentIcon]"} className="App-logo" alt="logo" /> 
+				</div>
+				<div className='postLikes'>
+					{this.props.likes}
+				</div>
+				<div className='postComment'>
+						{this.props.userName} + ":" + {this.props.text} 
+						{this.props.comments}
+				</div>
 			</div>
 		)}
 	}
