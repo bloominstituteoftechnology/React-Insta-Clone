caee45526169657baf4e5f0f84da2f4937053b94
unknown <jacobiswinning@gmail.com>
2018-03-07 16:08:39 -0700

    added navBar

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar.js';
+import data from './dummy-data.js';
 // import PostContainer from './components/PostContainer/PostContainer.js';
 
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+    dummyDataList: [],
+    }
+  }
+  componentDidMount() {
+    const {dummyData} = data;
+    this.setState ({
+      dummyDataList: dummyData,
+    })
+    console.log(this.state.dummyDataList);
+  }
+
   render() {
     return (
       <div className='body'>
