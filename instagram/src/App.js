da1687ff6ceec8461c005398f333e5457920246b
kait-schorr <kaitlynschorr@gmail.com>
2018-03-07 15:32:22 -0600

    Added imports

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./CommentSection/CommentSection.js";
+import "./PostContainer/PostContainer.js";
+import "./SearchBar/SearchBar.js";
 
 class App extends Component {
   render() {
--- instagram/src/SearchBar/SearchBar.js
+++ instagram/src/SearchBar/SearchBar.js
@@ -1,1 +1,2 @@
 import React, { Component } from "react";
+import "./SearchBar/SearchBar.css";
