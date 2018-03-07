21d5f1886446e175bb116fba401eb64793cd7109
mjd02 <mostdarryl@gmail.com>
2018-03-07 18:05:00 -0500

    setup dev environment

---
+++ instagram/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import dummyData from './dummy-data.js';
+
+class App extends Component {
+  render() {
+    console.log(dummyData);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
