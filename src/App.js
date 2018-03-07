676f5456b43ed4dee765f922effe398ee8f14054
Juber Ahmed <a.juber@hotmail.com>
2018-03-07 18:08:15 -0500

    working progess

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import { Button } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App-header">
+        <nav>
+          <SearchBar />
+        </nav>
+
+        <Button bsStyle="warning">Warning</Button>
+
+        <Button bsStyle="success">Success</Button>
+
+        <Image src="./ilogo.png" responsive />;
+
+
       </div>
     );
   }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,2 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Image } from 'react-bootstrap';
 
+class SearchBar extends Component {
+    constructor() {
+        super();
+        // this.state
+    }
+
+    render () {
+        return (
+            <div>
+                <span><img src="./ilogo.png"/></span>
+                <span>Instagram</span>
+                <span>Search Bar</span>
+                <span>explore</span>
+                <span>heart</span>
+                <span>user</span>
+            </div>
+        );
+    }
+
+}
+
+
+export default SearchBar;
