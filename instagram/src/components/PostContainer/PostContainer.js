66fb55c84b42f3e4656dbe995a2f5d0879463a6e
Justin Kaseman <justinkaseman@live.com>
2018-03-07 17:09:00 -0800

    Some styling done

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -4,10 +4,10 @@ import './PostContainer.css';
 
 export const PostContainer = (props) => {
     return (
-        <div>
-            <div><img src={props.data.thumbnailUrl} alt=""></img> {props.data.username} </div>
-            <img src={props.data.imageUrl} alt=""></img>
-            <div>"like" "comment"</div>
+        <div className="PostContainer__main">
+            <div><img className="PostContainer__logo" src={props.data.thumbnailUrl} alt=""></img> {props.data.username} </div>
+            <img className="PostContainer__image" src={props.data.imageUrl} alt=""></img>
+            <div><i class="far fa-heart fa-2x"></i> <i class="far fa-comment fa-2x"></i></div>
             <div>{props.data.likes} likes</div>
             <CommentSection comments={props.data.comments}/>
             <input></input>
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -4,12 +4,14 @@ import './SearchBar.css';
 export const SearchBar = (props) => {
     return (
         <div className="SearchBar__Main">
-            <a className="SearchBar__Button">Instaslam</a>
-            <input className="SearchBar__Input" placeholder="Search"></input>
             <div>
-                <a className="SearchBar__Button">C</a>
-                <a className="SearchBar__Button">H</a>
-                <a className="SearchBar__Button">P</a>
+                <a><i class="fas fa-camera-retro fa-2x"></i><span className="SearchBar__Title">Instaslam</span></a>
+            </div>
+            <input className="SearchBar__Input" placeholder="&#128269; Search"></input>
+            <div>
+                <a><i className="SearchBar__Button" class="far fa-compass fa-2x"></i></a>
+                <a><i className="SearchBar__Button" class="far fa-heart fa-2x"></i></a>
+                <a><i className="SearchBar__Button" class="far fa-user fa-2x"></i></a>
             </div>
         </div>
     )
