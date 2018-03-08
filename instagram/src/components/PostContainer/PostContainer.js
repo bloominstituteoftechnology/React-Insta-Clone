cea526063707080a845f8fd8658dc9bb40c54d05
Robert Rak <robertrak@hotmail.com>
2018-03-08 16:40:33 -0500

    removed dropdown

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -10,7 +10,7 @@ export const PostContainer = (props) => {
                 <img src={props.data.thumbnailUrl} alt="thumbnail"></img>
                 <h4 >{props.data.username}</h4>
             </div>
-                <img src={props.data.imageUrl} alt={"post image by" + props.data.username}></img>
+                <img className="img-fluid" src={props.data.imageUrl} alt={"post image by" + props.data.username}></img>
                 <CommentSection comments={props.data.comments} />
         </div>
     )
