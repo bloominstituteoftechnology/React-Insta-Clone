6600f7f0134f6e4ec86dd57a49b81c6b57053e40
hrehman000@citymail.cuny.edu <hrehman000@citymail.cuny.edu>
2018-03-08 16:56:59 -0500

    changes made to searchBar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, {Component} from "react";
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import { FormGroup, Label} from "reactstrap";
 import "./SearchBar.css";
-import { FormGroup, Label, Input } from "reactstrap";
 
-const SearchBar = props => {
+// const SearchBar = props => {
+//   return (
+//     <div>
+//       <i class="fab fa-instagram" />
+//     </div>
+//   );
+// };
+
+const SearchBar = (props) => {
   return (
-    <div>
-      <i class="fab fa-instagram" />
-    </div>
-  );
-};
+    <container>
+      <Row>
+        <Col>
+          <img
+            className="logo"
+            src="http://www.clker.com/cliparts/U/d/x/w/I/q/instagram-logo-md.png"
+            alt="Logo"
+          />
+        </Col>
+        <Col sm="12" md={{ size: 4}}>
+          <InputGroup>
+            <InputGroupAddon addonType="prepend">
+              <InputGroupText>
+              <img
+                className="search-icon"
+                src="http://www.clker.com/cliparts/z/1/T/u/9/2/search-icon-hi.png"
+                alt="search icon"
+              />
+              </InputGroupText>
+            </InputGroupAddon>
+            <Input placeholder="search" />
+          </InputGroup>
+        </Col>
+      </Row>
+
+    </container>
+  )
+}
 
 export default SearchBar;
