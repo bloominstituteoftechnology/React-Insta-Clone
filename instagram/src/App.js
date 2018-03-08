14eed3f0ac930c29e46078e736283f6b3b0591bd
Brett Sullivan <opticalfalicy@gmail.com>
2018-03-07 19:02:12 -0600

    Navbar complete

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {SearchBar} from './components/SearchBar/SearchBar.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      // <div className="App">
+
+      // </div>
+      <navbar className="Nav__Section">
+      <div className="Nav__Logo">
+      <img src={require (`./components/media/Insta-Logo.jpg`)} alt="Instagram-Text-Logo" />
       </div>
+       <div className="Nav__Search">
+        <SearchBar />
+       </div>
+       <div className="Nav__Icons">
+       <img src={require (`./components/media/compass.jpg`)} alt="insta-compass" />
+       </div>
+       
+      </navbar>
     );
   }
 }
