594d1dab4995606bb16ad62f618bc1578a18bd35
Matthew Tomas <matthewebentomas@gmail.com>
2018-03-07 19:14:13 -0500

    continuing work

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,25 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data';
-
-
-
-
+import PostContainer from './components/PostContainer/PostContainer.js';
 
 class App extends Component {
   state = { 
     people: [] 
   }
+
+  componentDidMount() {
+    this.setState({ people });
+  }
+
   render() {
+    console.log("Render called: ", this.state.people);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro"></p>
+        <PostContainer people={this.state.people} />
       </div>
     );
   }
---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PostContainer = (props) => {
+    console.log("Props from PostContainer: ", props);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PostContainer;
