abe69390d13cf30c36ee08a610369e86dfcfb269
kait-schorr <kaitlynschorr@gmail.com>
2018-03-08 15:41:53 -0600

    Messing with search bar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,25 +1,9 @@
 import React from "react";
 import "./SearchBar.css";
+import { FormGroup, Label, Input } from "reactstrap";
 
 const SearchBar = props => {
-  return (
-    <div>
-      <form onSubmit={props.onSubmit} onChange={props.onChange}>
-        <input id="search-input" type="text" />
-        <button class="reset-button" type="button" onClick={props.onClick}>
-          Reset Search
-        </button>
-      </form>
-      <div className="search-container">
-        <div className="search-text">Search</div>
-        <img
-          className="search-icon"
-          src="http://www.clker.com/cliparts/z/1/T/u/9/2/search-icon-hi.png"
-          alt="search icon"
-        />
-      </div>
-    </div>
-  );
+  return <div>Blah</div>;
 };
 
 export default SearchBar;
