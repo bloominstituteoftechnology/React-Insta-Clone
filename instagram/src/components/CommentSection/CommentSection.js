76f71fbb07c1e73fc78808995e8375a8966608ea
Matthew Tomas <matthewebentomas@gmail.com>
2018-03-08 18:12:48 -0500

    comments added

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
+import './CommentSection.css';
 
 class CommentSection extends Component {
   constructor() {
     super();
     this.state = {
-      
+      // comments: [];
     }
   }
+
   componentDidMount() {
     this.setState({comments: this.props.comments})
   }
-render() {
-  console.log("render commentsection",this.state.comments);
-  return (
-    <div className="CommentContainer">
-      
 
-
-
-    </div>
-  );
-}
+  render() {
+    console.log("render commentsection",this.state.comments);
+    return (
+      <div className="CommentContainer">
+        {this.props.comments.map(comment => {
+        return (
+          <div key={comment}>
+            {comment.username}
+            {comment.text}
+          </div>);
+        }
+      )}
+      </div>
+    );
+  }
 };
-
-
 
 export default CommentSection;
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,32 +1,29 @@
 import React, { Component } from 'react';
 import CommentSection from '../../components/CommentSection/CommentSection';
+import './PostContainer.css';
 
 class PostContainer extends Component {
   constructor() {
     super();
   }
-render() {
-  console.log("render postcontainer",this);
-  return (
-    <div className="PostContainer">
-      {this.props.people.map(person => {
-        return (
-        <div className="people" key={person.timestamp}>
-        <h4>{person.username}</h4>
-        <img src={person.thumbnailUrl} alt=""/>
-        <h4>{person.likes}</h4>
-        <h4>{person.timestamp}</h4>
-        <CommentSection comments={this.props.comments} />
-        </div>);
-      }
-      )}
-
-      
-    </div>
-  );
-}
+  render() {
+    console.log("render postcontainer",this);
+    return (
+      <div className="PostContainer">
+        {this.props.people.map(person => {
+          return (
+          <div className="people" key={person.timestamp}>
+          <h4>{person.username}</h4>
+          <img src={person.thumbnailUrl} alt=""/>
+          <h4>{person.likes}</h4>
+          <h4>{person.timestamp}</h4>
+          <CommentSection comments={person.comments} />
+          </div>);
+        }
+        )}
+      </div>
+    );
+  }
 };
-
-
 
 export default PostContainer;
