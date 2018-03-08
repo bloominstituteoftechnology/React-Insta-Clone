1ac1882c79381b112efb0a988c2f900c3c165bb7
Johnathan Huggett <johnathanhuggett@Johnathans-MacBook-Pro.local>
2018-03-07 19:24:54 -0500

    Johnathan Huggett and William Winberg , working on  react-II

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data.js';
+import PostContainer from './components/PostContainer/PostContainer.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+    };
+  }
+  componentDidMount() {
+    this.setState({users: dummyData})
+  }
+  
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+  return (
+      <PostContainer users={this.state.users} />
+      
     );
   }
 }
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,1 +1,30 @@
-sadfasdf
+import React from 'react';
+
+const PostContainer = props => {
+    const { users } = props;
+
+    return (
+        <div>
+            {users.map(user => {
+                return (
+                    <div key={user.username}>
+                        <div>
+                            <img src={user.thumbnailUrl} alt={user.username + ' Thumbnail'} />
+                            <div>{user.username}</div>
+                        </div>
+                        <div>
+                            <img src={user.imageUrl} alt={user.username + ' image'} />
+                        </div>
+                        <div>
+                            <div>{user.likes + ' likes'}</div>
+                        </div>
+                        <CommentSection />
+                    </div>
+                );
+            })}
+        </div>
+    )
+}
+
+
+export default PostContainer
