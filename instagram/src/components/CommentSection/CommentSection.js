2a32c95b0e0238e2a13f45a9eaef6dd5bfbb40d9
Robert Rak <robertrak@hotmail.com>
2018-03-08 18:29:14 -0500

    Functional part completed

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 
-
 class CommentSection extends Component {
   constructor(props) {
     super(props);
     this.state = {
       comments: props.comments,
-      commentSubmitted: '',
+      commentField: '',
     };
   };
 
-  // addComment() {
-  //   event.preventDefault();
-  //   const newComment = this.state.commentSubmitted;
-  //   commentSubmitted = event.target.value;
-  //   comment.push(this.state.commentSubmitted)
-  //   commentSubmitted = '';
-  // }
+  handleListInput = event => {
+    this.setState({
+      commentField: event.target.value,
+    });
+  };
+
+  addComment = event => {
+    event.preventDefault();
+    let newComment = {username: 'dummy', text:this.state.commentField};
+    let commentsList = this.state.comments;
+    commentsList.push(newComment);
+    this.setState({
+      comments: commentsList,
+      commentField: ''
+    });
+  }
 
 
   render() {
@@ -37,9 +45,9 @@ class CommentSection extends Component {
         <form onSubmit={this.addComment}>
           <input
             type="text"
-          // onChange={this.handleListInput}
-          // placeholder="Add a new item"
-          // value={this.state.newItem.text}
+            onChange={this.handleListInput}
+            placeholder="Add a comment"
+            value={this.state.commentField}
           />
         </form>
       </div>
