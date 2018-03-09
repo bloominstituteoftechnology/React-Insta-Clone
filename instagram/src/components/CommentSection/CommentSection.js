5aaef77ccda80a80ba5cad14770049fb0e7c6e66
arcadia-ego <arcadia-ego@github.com>
2018-03-08 23:50:48 -0800

    last stuff for tonight

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,10 +1,22 @@
 import React from "react";
+import "./CommentSection.css"
+import { PostContainer } from "../PostContainer/PostContainer.js";
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
 
 export const CommentSection = (props) => {
     return(
         <div>
-            {props.username}
+            {console.log("props:", props)}
+            {props.comment.map((commentInfo => {
+                return (
+                    <div>
+                        <div>
+                        <span class="commentUsername">{commentInfo.username}</span>
+                        <span class="commentText">{commentInfo.text}</span>
+                        </div>
+                    </div>
+                )
+            }))}
         </div>
     )
 }
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./PostContainer.css";
-import { CommentSection } from "./components/CommentSection/CommentSection.js";
+import { CommentSection } from "../CommentSection/CommentSection.js";
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
 
 export const PostContainer = (props) => {
     return (
-        <div>
+        <div class= "topCard row">
             {props.post.map((usrInfo => {
                 return(
-                    <div class= "col-md-4">
+                    <div class= "bodyCard col-md-4">
                         <Card>
                             <CardBody>
                                 <img class="thumbnail rounded-circle" src={usrInfo.thumbnailUrl}></img>
@@ -25,11 +25,3 @@ export const PostContainer = (props) => {
 }
 
 
-
-// props.post.map((usrInfo => {
-//     <div>
-//         <Card>
-//             <CardImg top-width="100%" src={usrInfo.thumbnailUrl} />
-//         </Card>
-//         <h1> hi </h1>
-//     </div>
