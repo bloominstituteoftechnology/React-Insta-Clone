1364fb941629a92c52c7481095746d61034200c3
Omoniyi Alimi <omoniyialimi@Omoniyis-MacBook-Pro.local>
2018-03-09 10:22:59 -0500

    my cs8

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from '/.dummy-data';
+
+
 
 class App extends Component {
   render() {
@@ -8,7 +11,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">CS8 project week 5</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
