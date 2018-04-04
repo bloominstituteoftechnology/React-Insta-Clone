4317c4835bea090add0fa0805894017570bb89c6
totsuya <supertatortots@gmail.com>
2018-04-04 19:02:02 -0400

    got search box formatted and framework for later down the line

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   render() {
     return (<div className="App">
       <SearchBar />
+      
     </div>);
   }
 }
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,20 +1,41 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText
+} from 'reactstrap';
 
+//header should contain the elements of the SearchBar & logos
+//figure out how to contain searchbar within Header
 
-
-const Header = () => {
-  return (
-    <div class= "Header">
-     {/* <img src="http://www.freelogovectors.net/wp-content/uploads/2016/12/instagram-logo1.png" width="24" height="24"/> */}
-  </div>
-  )
-}
+// const Header = () => {
+//   return (
+//     <div class= "Header">
+//       <p>hello</p>
+//      {/* <img src="http://www.freelogovectors.net/wp-content/uploads/2016/12/instagram-logo1.png" width="24" height="24"/> */}
+//   </div>
+//   )
+// }
 
 const searchBar = () => {
-  return (
-    <div> <Input type="text" name="searchBar" placeholder="Search Here!" /> </div>
-  )
+  return (<div>
+    <div className="headerBox">
+      <Input className="searchBar" type="text" name="searchBar" placeholder="Search Here!"/>
+    </div>
+  </div>)
 }
-
-export default Header;
+// const logoLeft = () => {
+//   return (
+//
+//   )
+// }
+//
+// const logosRight = () => {
+//   return (
+//
+//   )
+// }
+export default searchBar;
