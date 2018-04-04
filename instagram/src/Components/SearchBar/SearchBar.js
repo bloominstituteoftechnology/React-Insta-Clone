4f509141c8846ccfeef92a2c11c394999c598716
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-04 15:39:13 -0700

    added searchbar function

---
+++ instagram/src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from '.react';
+
+
+export const SearchBar = props => {
+    return (
+        <div className="SearchBar">
+            <a className="SearchBar__Logo" href="/" disabled><i class="fab fa-instagram fa-2x"></i> <span className="SearchBar__Title">Instagram</span></a>
+            <input className="SearchBar__Input" placeholder="&#128269; Search"></input>
+            <span className="SearchBar__Buttons">
+                <a className="SearchBar__Button" href="/"><i class="far fa-compass fa-2x"></i></a>
+                <a className="SearchBar__Button" href="/"><i class="far fa-heart fa-2x"></i></a>
+                <a className="SearchBar__Button" href="/"><i class="far fa-user fa-2x"></i></a>
+            </span>
+        </div>
+    )
+};
