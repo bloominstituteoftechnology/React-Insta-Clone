d0eee5f559a9814db8c8951b9acf43462bf614b8
axhon <axhonruiz@gmail.com>
2018-04-04 13:16:35 -1000

    made tests pass

---
+++ src/components/CommentSection/CommentSection.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react"
+
+export default class CommentSection extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      comments: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState(() => ({
+      comments: this.props.comments ? [...this.props.comments] : []
+    }))
+  }
+
+  render() {
+    return <input />
+  }
+}
---
+++ src/components/PostContainer/PostContainer.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default props => (
+  <div>
+    <img src={props.thumbnailUrl} />
+    <img src={props.imageUrl} />
+  </div>
+)
