fe16c0632df9c19ebe1727da0622ef4a82b68c8c
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-04-04 14:33:09 -0700

    Import dummyData & applied CCR

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ data: dummyData });
+  }
+  
   render() {
     return (
       <div className="App">
