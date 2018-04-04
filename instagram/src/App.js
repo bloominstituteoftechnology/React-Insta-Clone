2dc0e18f57a386ba5ba6ce3819501a7a3cdf3877
Harrison Brock <harrison@harrisonbrock.com>
2018-04-04 18:18:22 -0400

    Added basic search bar

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import SearchBar from './components/SearchBar/SearchBar'
 class App extends Component {
   render() {
     return (
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <SearchBar />
       </div>
     );
   }
