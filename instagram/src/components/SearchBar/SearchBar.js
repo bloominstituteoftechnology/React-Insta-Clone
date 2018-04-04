63bd66ff6a32110c50631caed21c4315cc921026
Harrison Brock <harrison@harrisonbrock.com>
2018-04-04 18:18:22 -0400

    Added basic search bar

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+            <h1>Search Bar</h1>
+            </div>
+        );
+    }
+}
+export default SearchBar;
