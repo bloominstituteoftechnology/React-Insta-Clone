93e2fae5ec81f40e1609566ddd0ed0748bf7ab34
Devon Bacon <devon.bacon@gmail.com>
2018-04-04 15:21:02 -0700

    Finalized file connections

---
+++ instagram/src/components/CommentSection/commentSection.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const commentSection = props => {
+  return (
+    <div></div>
+  )
+}
+
+export default commentSection;
---
+++ instagram/src/components/SearchBar/searchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBar = props => {
+  return (
+    <div></div>
+  )
+}
+
+export default SearchBar;
