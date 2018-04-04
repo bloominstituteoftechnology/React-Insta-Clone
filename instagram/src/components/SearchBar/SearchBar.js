5335cab422a254c5a1913439e03446da3544617e
totsuya <supertatortots@gmail.com>
2018-04-04 17:52:32 -0400

    added search bar

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+const SearchBar = () => {
+  return (
+    <div class= "searchBar">
+     <Input type="text" name="searchBar" placeholder="Search Here!" />
+  </div>
+  )
+}
+
+export default SearchBar;
