c5e7381e66022f34b23c26b80d6c09cc9d07df5f
Velsu <riftmage50@gmail.com>
2018-04-05 01:27:11 +0200

    Managed to display comments

--- instagram/src/components/CommentSection/Comments.js
+++ instagram/src/components/CommentSection/Comments.js
@@ -2,7 +2,19 @@ import React from "react";
 
 class Comments extends React.Component {
   render() {
-    return <div> Hello </div>;
+    console.log("Comment Section Props", this.props);
+    return (
+      <div>
+        {this.props.comments.map(comment => {
+          return (
+            <div>
+              <p>{comment.username}</p>
+              <p>{comment.text}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
