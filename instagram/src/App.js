f97b1f83c6ad9657e7877e36dc9d50f64b11a881
Devon Bacon <devon.bacon@gmail.com>
2018-04-04 14:58:12 -0700

    Added js componenet files and imported to App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/searchBar';
+import PostContainer from './components/PostContainer/postContainer';
+import CommentSection from './components/PostContainer/postContainer';
 
 class App extends Component {
   render() {
