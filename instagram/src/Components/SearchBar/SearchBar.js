84304bcb98212ebf0d95430072f53c02b645b098
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-04 16:09:02 -0700

    added font awesome for icons

--- instagram/src/Components/SearchBar/SearchBar.js
+++ instagram/src/Components/SearchBar/SearchBar.js
@@ -2,15 +2,23 @@ import React from 'react';
 import './SearchBar.css';
 
 export const SearchBar = props => {
-    return (
-        <div className="SearchBar">
-            <a className="SearchBar__Logo" href="/" disabled><i class="fab fa-instagram fa-2x"></i> <span className="SearchBar__Title">Instagram</span></a>
-            <input className="SearchBar__Input" placeholder="&#128269; Search"></input>
-            <span className="SearchBar__Buttons">
-                <a className="SearchBar__Button" href="/"><i class="far fa-compass fa-2x"></i></a>
-                <a className="SearchBar__Button" href="/"><i class="far fa-heart fa-2x"></i></a>
-                <a className="SearchBar__Button" href="/"><i class="far fa-user fa-2x"></i></a>
-            </span>
-        </div>
-    )
+    return <div className="SearchBar">
+        <a className="SearchBar__Logo" href="/" disabled>
+          <i class="fab fa-instagram fa-2x" /> <span className="SearchBar__Title">
+            Instagram
+          </span>
+        </a>
+        <input className="SearchBar__Input" placeholder="&#128269; Search" />
+        <span className="SearchBar__Buttons">
+          <a className="SearchBar__Button" href="/">
+            <i class="far fa-compass fa-2x" />
+          </a>
+          <a className="SearchBar__Button" href="/">
+            <i class="far fa-heart fa-2x" />
+          </a>
+          <a className="SearchBar__Button" href="/">
+            <i class="far fa-user fa-2x" />
+          </a>
+        </span>
+      </div>;
 };
