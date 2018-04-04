8bbd43383c80d3cc93fe21ebbebd670e7b9b3586
Justin M <jekm321@gmail.com>
2018-04-04 19:54:01 -0400

    made searchbar with its own css

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import logo from '../images/logo.png'
+import logo from '../images/logo.png';
+import explore from '../images/explore.png';
+import activity from '../images/activity.png';
+import profile from '../images/profile.png';
+import './SearchBar.css';
 
 const SearchBar = props => {
     return (
         <div className="header">
-            <img className="logo" src={logo} alt="Instagram" />
+            <div>
+                <img className="logo" src={logo} alt="Instagram" />
+            </div>            
             <input className="search" placeholder="Search" />
+            <div className="directory">
+                <div className="links">
+                    <a href="/"><img src={explore}/></a>
+                    <a href="/"><img src={activity}/></a>
+                    <a href="/"><img src={profile}/></a>
+                </div>
+            </div>
         </div>
     );
 };
