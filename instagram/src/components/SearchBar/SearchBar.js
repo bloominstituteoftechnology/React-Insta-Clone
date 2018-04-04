f5c9fd2ed79ba2e034dc2d3cac02673d265147ff
totsuya <supertatortots@gmail.com>
2018-04-04 18:00:12 -0400

    added dabman.png

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -4,6 +4,7 @@ const SearchBar = () => {
   return (
     <div class= "searchBar">
      <Input type="text" name="searchBar" placeholder="Search Here!" />
+       <img src="./img/dabman.png" alt="dabman" className="dabman-responsive"/>
   </div>
   )
 }
