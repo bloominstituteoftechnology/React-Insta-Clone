ef3462d2ae7122577bd01623043418841af9d3c4
tanking <tanking@github.com>
2018-04-04 18:44:20 -0400

    Created PostContainer.js and PostContainer class and added imports to App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SearchBar from './components/SearchBar/SearchBar'
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+import dummyData from './dummy-data.js';
+
 class App extends Component {
   render() {
     return (
