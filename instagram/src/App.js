60b5a4747836ead8d93cf99565e47dc3cb7819a1
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-04-04 15:58:28 -0700

    working on insta-clone

---
+++ instagram/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {SearchBar} from './components/SearchBar/SearchBar.js'
+
+
+
+class App extends Component {
+  render() {
+    return (
+       <SearchBar/>
+    );
+  }
+}
+
+export default App;
