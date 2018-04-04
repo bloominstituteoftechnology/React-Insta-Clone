aa70d75c902f9c8f52d1f77c2cd5432bda71bab0
Boredom <macnwandu@gmail.com>
2018-04-04 17:29:50 -0500

    anoter commit

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Navbar, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';
+
+
+const SearchBar = () => {
+    return (
+        <div>
+            <Navbar className="sticky">
+             <div className="navBrandItems">
+                <NavbarBrand>
+                    <i className="fab fa-instagram fa-2x" />{' '}
+                    <span className="navTitle">Instagram</span>
+                </NavbarBrand>
+             </div>
+             <form action="">
+                <input type="text" placeholder="Search" />
+                </form>
+                <Nav>
+                <NavItem>
+                    <NavLink href="#" className="navLinkImgs">
+                    <i className="far fa-compass fa-2x" />
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="#" className="navLinkImgs">
+                    <i className="far fa-heart fa-2x" />
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="#" className="navLinkImgs">
+                    <i className="far fa-user fa-2x" />
+                    </NavLink>
+                </NavItem>
+                </Nav>
+            </Navbar>
+        </div>
+    );
+};
+
+export default SearchBar;
