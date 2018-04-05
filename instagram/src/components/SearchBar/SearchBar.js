1d3058739abc81ceeafc7dbd54d0ecdf6258cdf0
Harrison Brock <harrison@harrisonbrock.com>
2018-04-05 01:23:26 -0400

    Auto stash before merge of "master" and "origin/master"
    Added user and heart icon

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './SearchBar.css'
 import user from './user.png'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faUser from '@fortawesome/fontawesome-free-solid/faUser'
+import faCamera from '@fortawesome/fontawesome-free-solid/faHeart'
 export default class SearchBar extends React.Component {
     constructor() {
         super();
@@ -16,9 +19,11 @@ export default class SearchBar extends React.Component {
             value={this.props.searchText}
             onChange={this.props.handleSearch} />
 
-            <div className="SearchBar__actions">
-              <img src={user} alt="User" />
-            </div>
+            {/*<div className="SearchBar__actions">*/}
+              {/*<img src={user} alt="User" />*/}
+              <FontAwesomeIcon icon={faCamera} size='2x'/>
+                <FontAwesomeIcon  icon={faUser} size='2x' />
+            {/*</div>*/}
 
           </div>
         );
