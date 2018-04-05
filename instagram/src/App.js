96b76b9093666ad96e8a38c4a9ce356738197681
Justin M <jekm321@gmail.com>
2018-04-05 17:33:20 -0400

    basic outline done for insta-clone, gonna stat styling

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,35 +1,35 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import dummyData from './dummy-data';
 import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      dummyData: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ dummyData });
   }
 
   render() {
     return (
-      <div>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">
+            Instagram
+          </h1>
+        </header>
         <SearchBar />
+        <PostContainer data={this.state.dummyData} />
       </div>
     );
   }
-
-  // render() {
-  //   return (
-  //     <div className="App">
-  //       <header className="App-header">
-  //         <img src={logo} className="App-logo" alt="logo" />
-  //         <h1 className="App-title">Welcome to React</h1>
-  //       </header>
-  //       <p className="App-intro">
-  //         To get started, edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //     </div>
-  //   );
-  // }
 }
 
 export default App;
---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import activity from '../images/activity.png';
+import CommentSection from '../CommentSection/CommentSection';
+import './PostContainer.css';
+
+const PostContainer = props => {
+    return (
+        <div>
+            Posts
+            {props.data.map((post, index) => (
+                <div key={post.username + index}>
+                    <div>
+                        <img src={post.thumbnailUrl}/>
+                        {post.username}
+                    </div>
+                    <div>
+                        <img src={post.imageUrl}/>
+                    </div>
+                    <div>
+                        <button><img src={activity}/></button>
+                    </div>
+                    <div className="likes">
+                        {post.likes}
+                    </div>                    
+                    <CommentSection comments={post.comments} />
+                    <div className="timestamp">
+                        {post.timestamp}
+                    </div>                    
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default PostContainer;
