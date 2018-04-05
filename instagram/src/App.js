9ab870c597b44ec90213839151f786c1db0b417a
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-04-05 13:31:28 -0700

    Confirmed intial setup

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import { SearchBar } from './components/SearchBar/SearchBar';
+import { PostContainer } from './components/PostContainer/PostContainer';
+import { CommentSection } from './components/CommentSection/CommentSection';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchBar />
+        <PostContainer />
+        <CommentSection />
       </div>
     );
   }
 }
 
 export default App;
+
+//  <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
