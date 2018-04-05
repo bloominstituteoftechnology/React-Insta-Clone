05a293955ed1f7b6f99861b542f1cfb8b8c16b71
unknown <bsa6.23.94@gmail.com>
2018-04-05 17:40:02 -0400

    forgot something

--- instagram/src/Components/PostContainer.js
+++ instagram/src/Components/PostContainer.js
@@ -3,7 +3,7 @@ import Comm from './CommentSection.js';
 
 const Gram = (props) => {
   console.log("Props", props);
-    return <div>{props.full.map(p => <div key={p.username} className="cont"><img className ="thumbnail" style={{height:35}} alt="dont care"src ={p.thumbnailUrl} /> {p.username} <div><img className= "mainImg" src={p.imageUrl} alt="why"/></div><div><img className="icon" style={{height: 35, width: 40, paddingRight:30}} alt="who cares" onClick="" src="https://image.flaticon.com/icons/svg/149/149217.svg" /><img style={{height: 35, width: 40}} className="icon" alt="useless info" src="https://image.flaticon.com/icons/svg/25/25663.svg" /></div><div className="likes"><b> {p.likes} likes </b></div>
+    return <div>{props.full.map(p => <div key={p.username} className="cont"><img className ="thumbnail" style={{height:35}} alt="dont care"src ={p.thumbnailUrl} /> {p.username} <div><img className= "mainImg" src={p.imageUrl} alt="why"/></div><div><img className="icon" style={{height: 35, width: 40, paddingRight:30}} alt="who cares" src="https://image.flaticon.com/icons/svg/149/149217.svg" /><img style={{height: 35, width: 40}} className="icon" alt="useless info" src="https://image.flaticon.com/icons/svg/25/25663.svg" /></div><div className="likes"><b> {p.likes} likes </b></div>
  <Comm comments={p.comments} />
 <div>{p.timestamp}</div>
 <input
