681ac232b6a7f512a9964421eeba8851cb1d1f56
Henry K <mykim1891@hotmail.com>
2018-04-05 16:20:20 -0700

    updated CSS

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import './searchbar.css';
 
 
 const SearchBar = props => {
     return (
         <div className="navbarContainer">
             <div className="instagramLogo">
-                <img src="#" alt="instagram logo" />
+                <img className="camera" src="https://www.seeklogo.net/wp-content/uploads/2016/05/instagram-icon-logo-vector-download-400x400.jpg" alt="instagram logo" />
+                <h1>|</h1>
+                <img className="writing" src="https://cdn.worldvectorlogo.com/logos/instagram-1.svg" alt="instagram logo" />
             </div>
             <div className="searchBar">
                 <input type="text" placeholder="search" />
