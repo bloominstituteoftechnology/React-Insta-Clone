06b216fdac1c046aa53338e4088a85e804a725a6
David Finley <dfinley2917@gmail.com>
2018-04-05 17:48:31 -0400

    comment form configured

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,19 +1,29 @@
 import React from "react";
 // import PostContainer from "./src/components/PostContainer/PostContainer";
 
-const CommentSection = props => {
-	// alert('made it!');
-	console.log('commentList: ', props.commentList);
-  return (
+class CommentSection extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			comments:[],
+			username:"Guest"
+		}
+	}
+	
+	render() {
+  return(
     <div>
-    	{props.commentList.map((comment, index) => (
+    	{this.props.commentList.map((comment, index) => (
     		[
     			<div>{comment.username}</div>,
     			<div>{comment.text}</div>
-    		]
-    	))}
+					    		]
+			))}
+			<div><form><input type="text" placeholder= "Add a comment..."/><button type="button" onClick={this.addNewComment}>...</button></form></div>
     </div>
-  );
+
+	);
+	}
 }
 
 export default CommentSection;
