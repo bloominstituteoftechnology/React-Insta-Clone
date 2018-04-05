2c70eabcf515d047e8d8c99a8bed378709f8ad34
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-05 13:18:49 -0700

    added comment section functionality

---
+++ instagram/src/Components/CommentSection/CommentSection.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './CommentSection.css';
+
+export const CommentSection = props => {
+    return (
+        <div>
+            {props.comments.map(((comment, index) => {
+                return (
+                    <div className="CommentSection" key={comment.username + index}>
+                        <span className="CommentSection__comment">{comment.username}></span>
+                        {comment.text}
+                    </div>
+                )
+            }))}
+        </div>
+    )
+}
