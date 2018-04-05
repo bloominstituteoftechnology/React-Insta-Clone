86ec3d56e56122445c504d497bc8f15c72ff27bf
Justin Hong <jhong2009@gmail.com>
2018-04-05 15:04:13 -0700

    added event handler to input

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -6,23 +6,41 @@ class CommentSection extends React.Component {
 		super(props);
 		this.state = {
 			comments:[],
-			username:"Guest"
+			username:"Guest",
+			newComment: ""
 		}
 	}
-	
-	render() {
-  return(
-    <div>
-    	{this.props.commentList.map((comment, index) => (
-    		[
-    			<div>{comment.username}</div>,
-    			<div>{comment.text}</div>
-					    		]
-			))}
-			<div><form><input type="text" placeholder= "Add a comment..."/><button type="button" onClick={this.addNewComment}>...</button></form></div>
-    </div>
 
-	);
+	// handle new comments being typed in
+
+
+
+	// add new comments to a post's list of comments
+
+
+	render() {
+		console.log('comments: ', this.state.comments);
+	  return(
+	    <div>
+	    	{this.props.commentList.map((comment, index) => (
+	    		[
+	    			<div>{comment.username}</div>,
+	    			<div>{comment.text}</div>
+					]
+				))}
+				<div>
+					<form>
+						<input
+						 type="text"
+						 name="newComment"
+						 value={this.state.newComment}
+						 onChange={this.handleNewComment}
+						 placeholder= "Add a comment..."/>
+						<button type="button" onClick={this.addNewComment}>...</button>
+					</form>
+				</div>
+	    </div>
+		);
 	}
 }
 
