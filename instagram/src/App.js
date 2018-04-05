19ae4d3638012f62df73d7314084dd5eb4e19bcd
Jonathan Greene <greeneselectronics@gmail.com>
2018-04-04 23:56:01 -0600

    Got as far as I could understand

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data';
+import PostContainer from './components/PostContainer/PostContainer.js';
 // will pass to post container
 
 class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        dummyData: [{}],
+      };
+    }
+
+    componentDidMount() {
+      console.log(dummyData);
+      this.setState({ dummyData: dummyData.dummyData})
+    }
   render() {
     return (
       <div className="App">
+        <PostContainer dummyData= {this.state.dummyData}/>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Instagram</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* s */}
       </div>
     );
   }
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,1 +1,11 @@
 // will pass to comment section 
+import React from "react";
+
+const PostContainer = props => {
+  console.log("Props", props);
+  // map over citiesProp return a dom elment for each city name in list
+  return <div> </div>;
+  //return <div>{props.dummyDataProp.map(c => <div>{c.city}</div>)}</div>;
+};
+
+export default PostContainer;
