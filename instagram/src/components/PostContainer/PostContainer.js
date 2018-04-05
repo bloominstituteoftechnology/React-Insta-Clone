e7a43da1b1302d5a581dfeddf981c29fbcc10db4
Nedu Robert <nedumrobert@gmail.com>
2018-04-05 23:15:08 +0100

    completed implementation of the post container

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,18 +1,47 @@
 import React from 'react';
+import {
+  Card,
+  Button,
+  CardHeader,
+  CardFooter,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardImg
+} from 'reactstrap';
 import './PostContainer.css';
 
 const PostContainer = props => {
-    return (
-        <div>
-            {props.dataProp.map((data, index) => 
-            <div key={'post ' + index}>
-                <div>
-                <img src={data.thumbnailUrl} alt={data.username + ' profile image'}/>
+    return <div className="postContainerDiv">
+        {props.dataProp.map((data, index) => (
+          <div key={'post ' + index} className="postContainer">
+            <Card>
+              <CardHeader>
+                <img
+                  src={data.thumbnailUrl}
+                  alt={data.username + ' profile image'}
+                  className="thumbnail"
+                />
                 <span>{data.username}</span>
+              </CardHeader>
+              <CardImg
+                src={data.imageUrl}
+                alt={data.username + 'post image'}
+              />
+              <CardBody>
+                <div>
+                  <i className="far fa-heart fa-2x postLike" />
+                  <i className="far fa-comment fa-2x" />
                 </div>
-            </div>)}
-        </div>
-    );
+                <div>
+                  <p className="likesNumber">{`${data.likes} likes`}</p>
+                </div>
+              </CardBody>
+              
+            </Card>
+          </div>
+        ))}
+      </div>;
 }
 
 export default PostContainer;
