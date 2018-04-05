8d79f85b4c6ff6f85185bc3677cdfd5b5c561934
totsuya <supertatortots@gmail.com>
2018-04-05 17:22:25 -0400

    added placeholder text

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -9,14 +9,15 @@ const PostContainer = (props) => {
         <div className='postContainer'>
             <Card className='postCard'>
                 <div>
-                    <img src='https://instagram.fbna1-1.fna.fbcdn.net/vp/31c45306e6ca815b8c59940aed10cedc/5B2D3C88/t51.2885-19/11201517_887808411287357_1307163552_a.jpg'/>
+                    {/* image is here for placeholder, will be replaced with adjustable image later */}
+                    <CardTitle>palceholdercoffee</CardTitle>
+                    <img className='postContainerAvatar' src='https://instagram.fbna1-1.fna.fbcdn.net/vp/31c45306e6ca815b8c59940aed10cedc/5B2D3C88/t51.2885-19/11201517_887808411287357_1307163552_a.jpg'/>
+                    
                 </div>
                 <CardImg src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" /> 
 
                 <CardBody>
-                    <CardTitle>Card title</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
-                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    {/* Comment Component should go here when ready */}
                 </CardBody>
             </Card>
         </div>
