61119a6684f2084d6970407c152f02bdc94bf896
2940cristian <2940cristian@gmail.com>
2018-04-05 15:49:26 -0700

    made comments into a class component

--- instagram/src/Components/CommentSection/CommentSection.js
+++ instagram/src/Components/CommentSection/CommentSection.js
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import dots from "./dots.svg"
 
-const CommentSection = props => {
-    console.log('other test', props.comments);
-    return(
+class CommentSection extends Component {
+    render() {
+        return(
         <div>
-           {props.comments.map(comment => (
+           {this.props.comments.map(comment => (
                <div className = "comments">
                    <div className="comments__User">{comment.username} <span className="comments__Text">{comment.text}</span></div>
                </div>
            ))}
-           <div className="comments__time">{props.time}</div>
+           <div className="comments__time">{this.props.time}</div>
            <div className = "comment__border"></div>
            <div className="comment__Input-container">
            <input className="comment__Input" type="text" placeholder="Add a comment...."/>
            <img src={dots} className="comment__dots"/>
            </div>
         </div>
-    )
+        )
+    }
 }
 
 export default CommentSection
