2ea9e75736fdec5a500d1b32d6d3f99575713cea
totsuya <supertatortots@gmail.com>
2018-04-05 17:32:42 -0400

    adjusted postcontainer header apperance

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -8,11 +8,11 @@ const PostContainer = (props) => {
     return (
         <div className='postContainer'>
             <Card className='postCard'>
-                <div>
+                <div className='postContainerHeader'>
                     {/* image is here for placeholder, will be replaced with adjustable image later */}
-                    <CardTitle>palceholdercoffee</CardTitle>
+                   
                     <img className='postContainerAvatar' src='https://instagram.fbna1-1.fna.fbcdn.net/vp/31c45306e6ca815b8c59940aed10cedc/5B2D3C88/t51.2885-19/11201517_887808411287357_1307163552_a.jpg'/>
-                    
+                    <h5 className='postContainerAvatarText'>PlaceHolderCoffee</h5>
                 </div>
                 <CardImg src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" /> 
 
