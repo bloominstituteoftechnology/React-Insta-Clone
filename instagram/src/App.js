ee81317b80693da9faf03073ef52c8cd3a05b372
totsuya <supertatortots@gmail.com>
2018-04-05 18:06:59 -0400

    added more placeholder things for later

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -13,4 +13,6 @@ class App extends Component {
   }
 }
 
+
+
 export default App;
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -4,6 +4,18 @@ import {
     CardTitle, CardSubtitle, Button
 } from 'reactstrap';
 
+// to be adjusted later 
+// class PostContainer extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+        
+//         }
+//     }
+// }
+
+
+//general placeholder, gives idea what it should look like later down the line
 const PostContainer = (props) => {
     return (
         <div className='postContainer'>
@@ -14,8 +26,9 @@ const PostContainer = (props) => {
                     <img className='postContainerAvatar' src='https://instagram.fbna1-1.fna.fbcdn.net/vp/31c45306e6ca815b8c59940aed10cedc/5B2D3C88/t51.2885-19/11201517_887808411287357_1307163552_a.jpg'/>
                     <h5 className='postContainerAvatarText'>PlaceHolderCoffee</h5>
                 </div>
+                <div className='postContainerImageContainer'>
                 <CardImg className='postContainerImage'src="https://instagram.fbna1-1.fna.fbcdn.net/vp/557a29d3526bc7f813c2a784d82de7d3/5B4E3E05/t51.2885-15/e35/26065916_2007800899433580_5879937492399947776_n.jpg" alt="Card image cap" /> 
-
+                </div>
                 <CardBody>
                     {/* Comment Component should go here when ready */}
                 </CardBody>
