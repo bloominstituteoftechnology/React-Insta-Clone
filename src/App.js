575fa58d84f1e885cda60509b84a2908da07e5ef
axhon <axhonruiz@gmail.com>
2018-04-05 15:27:56 -1000

    introduced state to app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import "./App.css"
 import PostContainer from "./components/PostContainer"
 import SearchBar from "./components/SearchBar"
 import data from "./dummy-data"
+const { Provider, Consumer } = React.createContext()
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <SearchBar />
-        {data.map(dummy => <PostContainer {...dummy} />)}
-      </div>
+      <Provider value={data}>
+        <div className="App">
+          <SearchBar />
+          <Consumer>
+            {context =>
+              context.map((dummy, i) => <PostContainer {...dummy} key={i} />)
+            }
+          </Consumer>
+        </div>
+      </Provider>
     )
   }
 }
