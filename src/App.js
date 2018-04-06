8ed46e961112bdda08b01affa8a32afeb372817d
axhon <axhonruiz@gmail.com>
2018-04-05 22:55:28 -1000

    the job is not done until documenation is done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css"
 import PostContainer from "./components/PostContainer"
 import SearchBar from "./components/SearchBar"
 import data from "./dummy-data"
+
+// https://reactjs.org/docs/context.html#reactcreatecontext
+// http://wesbos.com/react-context/ Of course Wes Bos has a video
 const { Provider, Consumer } = React.createContext()
 
 class App extends Component {
@@ -11,6 +14,9 @@ class App extends Component {
       <Provider value={data}>
         <div className="App">
           <SearchBar />
+          {/* In this context, because the provider's value is [data], 
+              you can map over it. However, value can be anything, even
+              METHODS. */}
           <Consumer>
             {context =>
               context.map((dummy, i) => <PostContainer {...dummy} key={i} />)
