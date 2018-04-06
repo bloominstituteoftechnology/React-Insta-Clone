eeec0ec19d22f80e5446bb742a0b84b04a0b7f1a
Axhon <axhonruiz@gmail.com>
2018-04-05 16:06:57 -1000

    shows comments

--- src/components/CommentSection/CommentSection.jsx
+++ src/components/CommentSection/CommentSection.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from "react"
 import "./CommentSection.css"
 
+const Comment = props => (
+  <div>
+    {props.username}
+    {props.text}
+  </div>
+)
+
 export default class CommentSection extends Component {
   constructor(props) {
     super(props)
@@ -18,6 +25,9 @@ export default class CommentSection extends Component {
   render() {
     return (
       <div className="Post--comments Comments">
+        {this.state.comments.map((comment, i) => (
+          <Comment key={i} {...comment} />
+        ))}
         <input placeholder="Add a comment..." className="Comments--input" />
       </div>
     )
