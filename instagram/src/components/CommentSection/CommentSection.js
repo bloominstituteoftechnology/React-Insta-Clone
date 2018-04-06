fee72478fbf5a0efa6e0c1f0a6aa9e82663e3765
Nedu Robert <nedumrobert@gmail.com>
2018-04-06 02:08:53 +0100

    implemented comment section UI

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,22 +1,22 @@
 import React from 'react';
+import './CommentSection.css';
 
 const CommentSection = props => {
-    return (
-        <div>
-            <div className="commentsContainer">
-                {props.comments.map((c, index) => (
-                    <div key={`${c.username}'s comment ${index}`}>
-                        <p>
-                            <span>{c.username}</span> {c.text}
-                        </p>
-                    </div>
-                ))}
+    return <div>
+        <div className="commentsContainer">
+          {props.comments.map((c, index) => (
+            <div key={`${c.username}'s comment ${index}`}>
+              <p className="userInfo">
+                <span className="username">{c.username}</span> {c.text}
+              </p>
             </div>
-            <div>
-                <input type="text" placeholder="Add a comment..."/>
-            </div>
+          ))}
         </div>
-    )
+        <div className="commentInputContainer">
+          <input type="text" placeholder="Add a comment..." className="commentInput" />
+          <i className="fas fa-ellipsis-h" />
+        </div>
+      </div>;
 }
 
 export default CommentSection;
