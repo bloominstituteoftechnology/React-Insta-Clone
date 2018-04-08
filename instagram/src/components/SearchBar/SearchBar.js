30fb2df80052af6c5c54e7e0e037e7d89d6a7484
Justin Hong <jhong2009@gmail.com>
2018-04-08 16:20:24 -0700

    set up SearchBar component

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+class SearchBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-
-
-
-
-
+	render() {
+		return <h1>Search Bar goes Here</h1>
+	}
+}
 
 export default SearchBar
