ff727b25abb89452ebbeeb4d0ab44636d20cf9cf
Justin Hong <jhong2009@gmail.com>
2018-04-08 18:19:34 -0700

    added flex box and  general layout styles to SearchBar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -9,20 +9,23 @@ class SearchBar extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<h1>Search Bar goes Here</h1>
-			{/* brand icon goes here */}
-				<div><a href="https://www.instagram.com/?hl=en"><i class="fab fa-instagram fa-3x"></i></a></div>
-		{/* brand title goes here */}
-				<h1>Instagram</h1>
-	{/* search bar goes here */}
+			<div className="SearchBar">
+				{/* brand icon goes here */}
+				<div className="brand">
+					<a href="https://www.instagram.com/?hl=en"><i class="fab fa-instagram fa-3x"></i></a>
+					{/* brand title goes here */}
+					<p>instagram</p>
+				</div>
+				{/* search bar goes here */}
 				<form>
 					<input type="text" placeholder="Search here..." />
 				</form>
-{/* accent icons go here */}
-				<div><a href="https://www.instagram.com/?hl=en"><i class="far fa-compass fa-2x"></i></a></div>
-				<div><a href="https://www.instagram.com/?hl=en"><i class="far fa-heart fa-2x"></i></a></div>
-				<div><a href="https://www.instagram.com/?hl=en"><i class="far fa-user fa-2x"></i></a></div>
+				{/* accent icons go here */}
+				<div>
+					<a className="accentLogo" href="https://www.instagram.com/?hl=en"><i class="far fa-compass fa-2x"></i></a>
+					<a className="accentLogo" href="https://www.instagram.com/?hl=en"><i class="far fa-heart fa-2x"></i></a>
+					<a className="accentLogo" href="https://www.instagram.com/?hl=en"><i class="far fa-user fa-2x"></i></a>
+				</div>
 			</div>
 		);
 	}
