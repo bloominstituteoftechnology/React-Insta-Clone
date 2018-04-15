b01821cf301e567baa6d369d3ce30141f20cf3fb
Anthony <anthony@Anthonys-MacBook-Pro.local>
2018-04-15 07:40:45 -0400

    Added the Comment component to the comment section and fixed the layout

--- instagram/src/Components/PostContainer/PostContainer.js
+++ instagram/src/Components/PostContainer/PostContainer.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './PostContainer.css';
+import CommentSection from '../CommentSection/CommentSection.js';
 
 const PostContainer = () => {
-return <p>Hi!</p>
-};
-
-<p>Hi!</p>
+return (
+    <div className='PostContainer'>
+        <div className='User'>
+            <div>User Icon</div>
+            <div>User Name</div>
+        </div>
+        <div className='PostImage'>IMG</div>
+        <div className='interact'>
+            <div>Heart Button</div>
+            <div>Comment Bubble</div>
+        </div>
+        <div className='likeCount'> 400 Likes </div>
+        <div className='comments'>
+            <CommentSection />
+        </div>
+        <div className='time'>2 Hours Ago</div>
+    </div> 
+)};
 
 export default PostContainer;
