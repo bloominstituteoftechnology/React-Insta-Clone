ef8fc4d588a04ad57bcdc4c9790a00224206c309
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-02 15:12:16 -0700

    logo, username,and image mapped and displayed in PostContainer.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import CommentSection from './components/CommentSection';
+// import CommentSection from './components/CommentSection/CommentSection';
+import PostContainer from './components/PostContainer/PostContainer';
 import SearchBar from './components/SearchBar/SearchBar';
-// import PostContainer from './components/PostContainer';
-
-
+import { dummyData } from './dummy-data';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ users: dummyData })
+  }
+
   render() {
     return (
       <div className="App">
         <SearchBar />
+        <PostContainer users = {this.state.users}/>
       </div>
     );
   }
