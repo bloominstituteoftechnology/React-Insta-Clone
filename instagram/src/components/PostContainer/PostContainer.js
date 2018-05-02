d2321a2e28c3b77438fc24e5da85d3ca337f52c3
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-02 17:19:39 -0500

    added user thumbnail and styled post header

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -7,7 +7,7 @@ import CommentSection from '../CommentSection/CommentSection';
 const PostContainer = props => {
     return (
         <div className="post-container">
-        <div>{props.post.username}</div>
+        <div className="post-header"><img className="thumbnail" src={props.post.thumbnailUrl} alt="thumbnail"></img>{props.post.username}</div>
         <img className="post-image" src={props.post.imageUrl} alt={props.post.username}></img>
         <CommentSection comments={props.post.comments} />
         </div>
