06b5df64829b1206727bc68de90e687c09956f10
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-02 14:47:02 -0500

    created constructor, state, and componentDidMount in App.js. dummydata is now on state

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({posts: dummyData})
+  }
   render() {
     return (
       <div className="App">
