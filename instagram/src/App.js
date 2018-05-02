2bc9abffab36130e03020cb54601f02098890771
emortalix <emortalix@welcometoforever.io>
2018-05-02 17:45:11 -0400

    Connected App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from "./dummy-data";
+import PostContainer from "./components/PostContainer/PostContainer";
+import SearchBar from "./components/SearchBar/SearchBar";
+
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      tweets: []
+    }
+  }
+
+  componentDidMount() {
+    let allTweets = dummyData.slice();
+    this.setState({
+      tweets: allTweets
+    });
+  }
+
   render() {
+    console.log(dummyData);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchBar />
+        <PostContainer tweets={this.state.tweets}/>
+
+
       </div>
     );
   }
