be303f43cdd87c3d6ac6fb8c5d319c007a7f0697
Brandon Benefield <bsquared18@gmail.com>
2018-05-02 16:51:51 -0400

    feat: create new `Button` reusable comp import `Button` into `SearchBarContainer` for each button (x3)

--- instagram/src/components/SearchBar/SearchBarContainer.js
+++ instagram/src/components/SearchBar/SearchBarContainer.js
@@ -3,12 +3,19 @@ import React from 'react';
 // components
 import { SearchBarLogo } from './SearchBarLogo';
 import { Input } from '../misc/Input';
+import { Button } from '../misc/Button';
 
 export const SearchBarContainer = () => {
   return (
     <div className="searchbar-container">
       <SearchBarLogo />
       <Input />
+      <Button
+        buttonContent='C' />
+      <Button
+        buttonContent='H' />
+      <Button
+        buttonContent='P' />
     </div>
   );
 };
