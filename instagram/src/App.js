c9c08af8dd2cf644deff041378f23163d117672c
Brandon Benefield <bsquared18@gmail.com>
2018-05-02 16:27:23 -0400

    feat: create new component `SearchBarLogo.js` this comp will contain the logo for our insta-clone `SearchBarLogo` is imported to `SearchBarContainer` which is imported to `App`

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 // data
 import dummyData from './dummy-data';
 
+// components
+import { SearchBarContainer } from './components/SearchBar/SearchBarContainer';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <SearchBarContainer />
       </div>
     );
   }
--- instagram/src/components/SearchBar/SearchBarContainer.js
+++ instagram/src/components/SearchBar/SearchBarContainer.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+// components
+import { SearchBarLogo } from './SearchBarLogo';
+
 export const SearchBarContainer = () => {
   return (
     <div className="searchbar-container">
-    
+      <SearchBarLogo />
     </div>
   );
 };
