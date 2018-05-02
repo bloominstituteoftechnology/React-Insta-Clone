f5b907f50642d7a65bad52b9793d8dbd61759943
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-02 15:20:57 -0500

    added instagram header and input for search. Icons and styles in progress

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import dummyData from './dummy-data'
+import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
 
 class App extends Component {
   constructor() {
@@ -18,12 +18,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Instagram</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         Instagram Feed
         </p>
+        <SearchBar />
       </div>
     );
   }
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './SearchBar.css';
+
+
+const SearchBar = () => {
+    return (
+        <div className="insta-header">
+            <img/>
+            <span className="header-title">
+            Instagram
+            </span>
+            <input type="text" placeholder="Search" className="header-input"></input>
+            <img/>
+            <img/>
+        </div>
+    )
+}
+
+
+export default SearchBar;
