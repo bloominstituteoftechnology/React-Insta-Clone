625add0abdad5c26222bfdc9d71241e649cd3a6d
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2018-05-02 15:54:45 -0500

    Files additions
    moved dummy-data.js to SRC directory and created four new files in SRC

--- instagram/src/dummy-data.js
+++ instagram/src/dummy-data.js
@@ -7,7 +7,7 @@ const dummyData = [
         timestamp: "July 17th 2017, 12:42:40 pm",
         comments: [
             {
-            username: "philzcoffee",
+                username: "philzcoffee",
                 text: "We've got more than just delicious coffees to offer at our shops!"
             },
             {
