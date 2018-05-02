c0b5feabc98a9dba35426f460186f93862854bd1
mr-burton <taylormade800@gmail.com>
2018-05-02 13:46:49 -0700

    added importsto js files and setting up boiler plates

---
+++ instagram/src/SearchBar/searchBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    constructor() {
+        super();
+        this.state {
+
+        };
+    }
+
+
+    searchBarEventHandler = event => {
+        this.setState({ [event.target]: event.target })
+    };
+
+    searchBarEventHandler2 = event => {
+
+    };
+
+    render() {
+
+    }
+
+
+
+
+}
+
+export default SearchBar;
