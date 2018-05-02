1a29d42f00d339525aa373e65fcb8a6f34cac28b
Bradiowave <divebombwalker@gmail.com>
2018-05-02 14:52:44 -0700

    img element in SearchBar.js not working

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import dummyData from './dummy-data';
+import { SearchBar } from './components/SearchBar/SearchBar';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({data: dummyData})
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <SearchBar />
       </div>
     );
   }
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SearchBar = () => {
+    return (
+        <div>
+           <img src='../../assets/ig_search_bar' alt='ig_search_bar image' />
+        </div>
+    )
+};
