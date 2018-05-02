82edc708eada50cfcdb4bdc264dccdb8aae89432
Bradiowave <divebombwalker@gmail.com>
2018-05-02 15:37:17 -0700

    finished basic SearchBar.js

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import instagramLogo from '../../assets/instagram_logo.png';
+import instagramCursive from '../../assets/instagram_cursive.png';
 
 export const SearchBar = () => {
     return (
         <div>
-           <img src='../../assets/ig_search_bar' alt='ig_search_bar image' />
+           <img src={instagramLogo} width='4%' height= '4%' alt='instagram_logo' />
+           <img src={instagramCursive} width='15%' height= '15%' alt='instagram_cursive' />
+           <input type='text' placeholder = '🔎 search' />
         </div>
     )
 };
