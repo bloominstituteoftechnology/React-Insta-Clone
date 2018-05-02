fee5ec9883561f779caafdab06d03380c94177c5
Brandon Benefield <bsquared18@gmail.com>
2018-05-02 18:03:59 -0400

    feat: add new features to `Input` comp - `SearchBarContainer` now passes in `props` object - `Input` now has values for multiple attributes: `handleInputValueChange: func`, `inputClassName: string`, `inputName: string`, `inputPlaceHolder: string`, `inputValue: string`

--- instagram/src/components/SearchBar/SearchBarContainer.js
+++ instagram/src/components/SearchBar/SearchBarContainer.js
@@ -5,13 +5,17 @@ import { SearchBarLogo } from './SearchBarLogo';
 import { Input } from '../misc/Input';
 import { Button } from '../misc/Button';
 
-export const SearchBarContainer = () => {
+export const SearchBarContainer = props => {
   return (
     <div className="searchbar-container">
       <SearchBarLogo />
 
       <Input
+        handleInputValueChange={ props.handleInputValueChange }
+        inputClassName={ props.inputClassName }
+        inputName={ props.inputName }
         inputPlaceHolder='Search'
+        inputValue={ props.inputValue }
       />
 
       <Button
