ce979960c9f2a485aaae7787a82dfb659c2bdcd1
Brandon Benefield <bsquared18@gmail.com>
2018-05-02 18:05:26 -0400

    feat: `App` comp now contains state - `App` is currently only holding state for the `Input` comp inside of `SearchBarContainer` comp - `App` now handles the `onChange` event for `Input` comp inside `SearchBarContainer` comp

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -9,10 +9,27 @@ import { SearchBarContainer } from './components/SearchBar/SearchBarContainer';
 import { PostContainer } from './components/PostContainer/PostContainer';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    };
+  }
+
+  // handles every input value
+  handleInputValueChange = e => {
+    this.setState({
+      inputValue: e.target.value
+    });
+  };
+  
   render() {
     return (
       <div className="App">
-        <SearchBarContainer />
+        <SearchBarContainer
+          handleInputValueChange={ this.handleInputValueChange }
+          inputValue={ this.state.inputValue }
+        />
 
         <PostContainer />
       </div>
