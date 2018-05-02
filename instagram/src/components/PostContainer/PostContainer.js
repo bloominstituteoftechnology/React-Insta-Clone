eea783f82ac28895f8812a6fe2625ea07dc81ad6
Matthias Bakken <matthias.bakken@gmail.com>
2018-05-03 03:13:23 +0700

    Added necessary folders and files and setup export import statements

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './PostContainer.css';
+
+
+
+
+
+
+export default PostContainer;
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './SearchBar.css';
+
+
+
+
+
+
+export default SearchBar;
