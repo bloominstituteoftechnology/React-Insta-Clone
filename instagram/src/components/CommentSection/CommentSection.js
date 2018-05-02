abbda7f4385217fabe8b4c956e33d689fb184802
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-02 17:47:20 -0500

    added heart and comment icon under each post and cleaned up code

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -5,16 +5,20 @@ import './CommentSection.css'
 const CommentSection = props => {
     return (
        <div> 
-       {props.comments.map((comment) => 
-            {return (
-                <div className="comment-username">
-                    {comment.username}
-                    <span className="comment-text" >{comment.text}</span>
-                </div>
-           )})}
-           <div className="comment-container">
-            <input className="comment-input" type="text" placeholder="Add a comment..."></input><span><button className="commentBtn">...</button></span>
-           </div>
+            <div className="comment-icons">
+                <i class="far fa-heart"></i>
+                <i class="far fa-comment"></i>
+            </div>
+            {props.comments.map((comment) => 
+                    {return (
+                        <div className="comment-username">
+                            {comment.username}
+                            <span className="comment-text" >{comment.text}</span>
+                        </div>
+                    )})}
+            <div className="comment-container">
+                <input className="comment-input" type="text" placeholder="Add a comment..."></input><span><button className="commentBtn">...</button></span>
+            </div>
        </div>
     ) 
 }
