dd514ffe76bd572ceac1d7b1439eb23aa6f8dec5
Brandon Benefield <bsquared18@gmail.com>
2018-05-02 17:19:58 -0400

    insert new comps

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,9 +1,45 @@
 import React from 'react';
 
-exort const PostContainer = () => {
+// components
+import { Image } from '../misc/Image';
+import { UserName } from '../misc/UserName';
+import { Button } from '../misc/Button';
+
+export const PostContainer = props => {
   return(
     <div className="postcontainer">
+      <Image
+        className='postcontainer__profile-image'
+        // imgSrc={ props.imgSrc }
+      />
 
+      <UserName
+        className='postcontainer__username'
+        userName='philzcoffee'
+      />
+
+      <Image
+        className='postcontainer__post-image'
+        // imgSrc={ props.imgSrc }
+      />
+
+      <Button
+        buttonClassName={ props.heartPost }
+        handleButtonOnClick={ props.handleButtonOnClick }
+      />
+
+      <Button
+        buttonClassName={ props.commentOnPost }
+        handleButtonOnClick={ props.handleButtonOnClick }
+      />
+
+      <div className='postcontainer__likes-container'>
+        <span className='postcontainer__likes-container__likes'>
+          { props.postHeartCount } likes
+        </span>
+      </div>
+
+      
     </div>
   );
-}
+};
