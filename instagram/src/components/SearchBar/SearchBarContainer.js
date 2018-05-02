a29abcebc99ceaf520fdf504420ed3e0254fac84
Brandon Benefield <bsquared18@gmail.com>
2018-05-02 16:35:37 -0400

    fix: correct import statement for `Input` comp

--- instagram/src/components/SearchBar/SearchBarContainer.js
+++ instagram/src/components/SearchBar/SearchBarContainer.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 // components
 import { SearchBarLogo } from './SearchBarLogo';
-import { SearchBar } from './SearchBar';
+import { Input } from '../misc/Input';
 
 export const SearchBarContainer = () => {
   return (
     <div className="searchbar-container">
       <SearchBarLogo />
-      <SearchBar />
+      <Input />
     </div>
   );
 };
