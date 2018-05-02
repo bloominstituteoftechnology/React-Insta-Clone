7ada6af793463e60df8640663c5d496a249c521d
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2018-05-02 17:04:31 -0500

    App.js fleshed out some

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { dummyData } from  './dummy-data';
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      gramData: []
+    };
+  }
+
+  componentDidMount(){
+    this.setState({gramData: dummyData})
+  }
+
   render() {
     return (
       <div className="App">
