25d22e86796bf8d4c994535a5c603fa0e1e201a1
Brad Traversy <mercado7946@gmail.com>
2018-05-03 15:32:50 -0700

    creating a mockup before breaking down into smaller components

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { NavBar } from 'reactstrap';
+import dummyData from './dummy-data.js';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 
 class App extends Component {
   render() {
-    console.log(dummyData);
     return (
-      <div className="App">
-        <header className="App-header">
-          <Navbar><ul><li>asdfasdf</li></ul></Navbar>  
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <nav class="navbar navbar-light bg-light justify-content-between">
+          <a class="navbar-brand">Logo | Instagram</a>
+          <form class="form-inline">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search" />
+          </form>
+          <div>
+            <span>icon </span> <span>icon </span>  <span>icon </span>
+          </div>  
+
+        </nav>  
+        <Card>
+          <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+              <img src="/assets/brand/bootstrap-solid.svg" width="50" height="50" class="d-inline-block align-top" alt="logo" />
+                USERNAME
+            </a>
+          </nav>
+          <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+          <CardBody>
+            <CardText className="text-left"><span className="strong">UserName</span> Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          </CardBody>
+        </Card>
       </div>
     );
   }
