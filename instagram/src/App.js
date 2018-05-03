4111d937d6b1be0b655df749283e4ae51f222dbf
masontmorrow <masontmorrow@gmail.com>
2018-05-03 02:47:47 +0200

    Updated SearchBar Component + Styling

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import CommentSection from './components/CommentSection/CommentSection';
-import PostContainer from './components/PostContainer/PostContainer';
-import SearchBar from './components/SearchBar/SearchBar';
+import CommentSection from './components/CommentSection/CommentSection.js';
+import PostContainer from './components/PostContainer/PostContainer.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchBar />
       </div>
     );
   }
