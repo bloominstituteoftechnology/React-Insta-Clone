65447ac898fe51ec4065a35d72fae1b5a4823f21
CMVNK <Kopecky12112@gmail.com>
2018-05-03 18:45:19 -0500

    tweaked margin

--- instagram/src/Components/CommentSection/CommentSection.js
+++ instagram/src/Components/CommentSection/CommentSection.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Row } from 'reactstrap';
 import './CommentSection.css';
 import '../PostContainer/PostContainer.js'
 
 const CommentSection = props => {
     return (
-        <Row>
-            <span>{props.username}</span>
-            <span>{props.comment}</span>
-            
-        </Row>
+        <p>hello world</p>
     )};
     
 
