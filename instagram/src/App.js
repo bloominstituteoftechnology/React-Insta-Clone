f023c4747fdef065d5df4c7c67f0c5489ac0d9fa
maxwashington <mnwashington18@gmail.com>
2018-05-03 14:49:13 -0500

    -Created css fils for the components -Styled SearchBar -Added ReactStrap -Found new Logo -Finished SearchBar

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import dummyData from './dummy-data';
 import SearchBar from "./components/SearchBar/SearchBar.js";
 import './App.css';
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,25 +1,25 @@
 
 import React from "react";
-//import logo from "./logo.svg";
-//import compass from "./compass.svg";
-//import heart from "./heart.svg";
-//import person from "./user-circle.svg";
+import logo from '../../logo.svg';
+import compass from "../../compass.svg";
+import heart from "../../heart.svg";
+import person from "../../user-circle.svg";
 
 const SearchBar = () => {
     return (
         <div className="flex search">
             <div className="flex div-25">
-                <img className="imageLogo" src='./logo.svg'/>
+                <img className="search__Images search__Images-logo" src={logo}/>
                 <hr/>
                 <h4 className="" >Instagram</h4>
             </div>
             <div className="div-50">
-                <input className="searchBox" type="text" placeholder="Search"/>
+                <input className="search__input" type="text" placeholder="search"/>
             </div>
             <div className="div-25">
-                <img className="searchImgs" src='./compass.svg'/>
-                <img className="searchImgs" src='./heart.svg'/>
-                <img className="searchImgs" src= './user-circle.svg'/>
+                <img className="search__Images search__Images-group" src={compass}/>
+                <img className="search__Images search__Images-group" src={heart}/>
+                <img className="search__Images search__Images-group" src={person}/>
             </div>
         </div>
     );
