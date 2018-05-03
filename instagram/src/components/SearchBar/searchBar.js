141c24d531054c45448191b620ac18654af042d7
mr-burton <taylormade800@gmail.com>
2018-05-03 15:12:57 -0700

    adding constructor to SearchBar

--- instagram/src/components/SearchBar/searchBar.js
+++ instagram/src/components/SearchBar/searchBar.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
     render() {
         return (
             <div>
