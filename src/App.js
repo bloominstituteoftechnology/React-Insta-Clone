16fd382fc965c7c8786c34ac269efa09adf69a38
elitecamper <jggarza5@gmail.com>
2018-05-03 13:56:25 -0500

    completed searchbar component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import {dummyData} from './dummyData';
 import PostContainer from './components/PostContainer/PostContainer';
+import SearchBar from './components/SearchBar/SearchBar';
+
 
 class App extends Component {
     constructor(props) {
@@ -10,18 +12,29 @@ class App extends Component {
       this.state = {
         dummyData: []
       };
+      this.search = this.search.bind(this);
     };
 
     componentDidMount() {
       this.setState({ dummyData: dummyData });
     }
 
+
+    search(user) {
+          const searchPosts = this.state.dummyData.filter(post => post.username.includes(user));
+          this.setState({
+            dummyData: searchPosts,
+          });
+        }
+
+        
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <SearchBar posts={this.state.dummyData} search={this.search} init={this.initialState} />
         </header>
         <p className="App-intro">Instagram Clone!</p>
         <div className="Posts">
