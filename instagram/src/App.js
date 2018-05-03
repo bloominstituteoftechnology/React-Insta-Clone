5596e83253c369917964f1ce6cb7515f3d42a7b8
Bee C <b.choeung@gmail.com>
2018-05-03 16:35:38 -0400

    coding along to Ryan's solution video

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import dummyData from './dummy-data';
-import PostContainer from './PostContainer';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './Components/PostContainer/PostContainer';
+
+
+
+
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      username: '',
-      thumbnailUrl: '',
-      imageUrl: '',
-      likes: '',
-      timestamp: '',
-      comments: ''
-    };
-  }
   render() {
     return (
       <div className="App">
@@ -23,10 +16,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React Insta-Clone</h1>
         </header>
-          <input className='SearchBar' />
-          <PostContainer className='PostContainer' />
-          
-        
+        <SearchBar />
+        <div className="post-wrapper">
+          <PostContainer />
+        </div>
       </div>
     );
   }
