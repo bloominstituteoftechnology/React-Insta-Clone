08b27f298f989e41413e7d25a81297a8c116645d
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-05-03 13:05:46 -0700

    built out searchBar, not styled

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import dummyData from './dummy-data';
 import PostContainer from './components/PostContainer/PostContainer.js';
-// import CommentSection from '.components/CommentSection.js';
-import SearchBar from './components/Searchbar/SearchBar.js';
+// import CommentSection from './components/CommentSection/CommentSection.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
 import './App.css';
 
 class App extends Component {
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const SearchBar = props =>{
-    return <input />;
+    return (
+        <div>
+            <i className='fa fa-instagram'>|Instagram</i>
+            <input placeholder="search" />
+            <i className='fa fa-compass'></i>
+            <i className='fa fa-heart'></i>
+            <i className='fa fa-user'></i>
+        </div>
+    );
 }
 
 export default SearchBar;
