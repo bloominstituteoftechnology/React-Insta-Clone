41e9a416bf40795147fd35e991f802d85fd4b192
kammagik <kammagik@yahoo.com>
2018-05-02 21:59:45 -0700

    added info to CommentSectionfile.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from, {Component} 'react';
+import './App.js';
+import {dummyData} from './dummy-data.js'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: []
+    }
+  };
+
+  componentDidMount() {
+    this.state{(comments: dummy-data)};
+  }
+
+  render() {
+    return (
+      <divClassName='App'>
+    )
+  }
+}
