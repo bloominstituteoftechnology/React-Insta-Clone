0898853c8cd13d10d254bc6d9b44944d0ede8d43
Zach Campbell <beazlebubbles@gmail.com>
2018-05-03 16:57:40 -0600

    Worked on postcontainer Shows images and poster now

--- instagram/src/Components/PostContainer/PostContainer.js
+++ instagram/src/Components/PostContainer/PostContainer.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './PostContainer.css';
 import CommentSection from '../CommentSection/CommentSection';
+import { Card, CardImg, CardText, CardBody, CardLink,
+  CardTitle, CardSubtitle } from 'reactstrap';
 
 const PostContainer = (props) => {
   console.log(props.post.username);
   return (
     <div>
-      <div>
-        <img src={props.post.thumbnailUrl} alt="user avatar" />
-        <p>{props.post.username}</p>
-      </div>
-      <img src={props.post.imageUrl} alt="posted image" />
-      <CommentSection />
+      <Card>
+        <CardBody>
+            <CardTitle><img className="posterAvatar rounded-circle" src={props.post.thumbnailUrl} alt="user avatar" /> {props.post.username} </CardTitle>
+
+        </CardBody>
+        <CardImg width="100%" src={props.post.imageUrl} alt="posted image" />
+        <CardBody>
+          <p className="post-icons">☭ ♡</p>
+          <CommentSection />
+        </CardBody>
+      </Card>
+
     </div>
   );
 }
