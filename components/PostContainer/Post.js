f17a65562c38dea06950056a61e8419fa6bf2e66
Marco A Guzman <marcoguzman16@gmail.com>
2018-05-03 15:32:48 -0700

    done with post.js

---
+++ components/PostContainer/Post.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+const Post = (props) => {
+    return (
+        <div>
+            <Card>
+                <Row>
+                <CardImg style={{borderRadius: 90}} src="https://goo.gl/NcmRQw" alt="Zeratul" />
+                <CardBody>
+                    <CardTitle>Zeratul Instagram</CardTitle>
+                    <CardSubtitle>Dark Templar</CardSubtitle>
+                    <CardText>Zeratul, perhaps the greatest Dark templar of all time, seeks revenge against the Zerg and the vile "Kerrigan"</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+};
+
+export default Post;
