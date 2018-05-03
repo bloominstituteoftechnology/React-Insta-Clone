ea2d7260684b3be1fa742c61ee4692e5368a54c3
jaklundun <govi.escudero@gmail.com>
2018-05-02 21:38:16 -0700

    Successfully compiled app.js by creating constructor setting up state and creating componentDidMount method.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data.js';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      instaFeed: []
+    }
+  }
+
+  componentDidMount(){
+    this.setState({ instaFeed: dummyData}); // here we are setting empty people array to people array in people.js. 
+  }
+
   render() {
     return (
       <div className="App">
