df1d0c5e8890f2ad431f170e5cbdefd5ef569738
Brad Traversy <mercado7946@gmail.com>
2018-05-04 00:23:39 -0700

    put SearchBar and PostContainer into separate files

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,46 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import FaInstagram from 'react-icons/lib/fa/instagram';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
 import dummyData from './dummy-data.js';
-import {
-  Card, CardImg, CardText, CardTitle, CardBody} from 'reactstrap';
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App container">
-        <nav class="navbar navbar-light justify-content-between">
-          <a class="navbar-brand"><FaInstagram /> | <img src="./assets/instaword.png" alt="instagram" width="120" height="40"/></a>
-          <form class="form-inline">
-            <input class="form-control" type="search" placeholder="Search" aria-label="Search" />
-          </form>
-          <div>
-            <img src="./assets/discover.png" alt="discover" height="30" width="30"/>
-            <img src="./assets/like.png" alt="like" height="30" width="30"/>
-            <img src="./assets/profile.png" alt="profile" height="30" width="30"/>
-          </div>
-        </nav>
-        <Card>
-          <nav class="navbar navbar-light">
-            <a class="navbar-brand" href="#">
-              <img src="/assets/brand/bootstrap-solid.svg" width="50" height="50" class="d-inline-block align-top" alt="logo" />
-              USERNAME
-            </a>
-          </nav>
-          <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-          <CardBody>
-            <div>
-              <img className="float-left" src="./assets/like.png" alt="like" height="30" width="30" />
-              <img className="float-left" src="./assets/comment.png" alt="comment" height="28" width="25" />
-            </div>
-            
-            <div className="strong">
-              373 likes
-            </div>
-            <CardText className="text-left"><span>UserName</span> </CardText>
-          </CardBody>
-        </Card>
+        <SearchBar />
+        <PostContainer />
       </div>
     );
   }
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,3 +1,32 @@
 import React, { Component } from 'react';
 import './SearchBar.css';
+import FaInstagram from 'react-icons/lib/fa/instagram';
 
+
+class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchTerm: 'Search',
+      submitTerm: '',
+      update: '',
+    }
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-light justify-content-between">
+        <a className="navbar-brand"><FaInstagram /> | <img src="./assets/instaword.png" alt="instagram" width="120" height="40" /></a>
+        <form className="form-inline">
+          <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+        </form>
+        <div>
+          <img src="./assets/discover.png" alt="discover" height="30" width="30" />
+          <img src="./assets/like.png" alt="like" height="30" width="30" />
+          <img src="./assets/profile.png" alt="profile" height="30" width="30" />
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default SearchBar;
