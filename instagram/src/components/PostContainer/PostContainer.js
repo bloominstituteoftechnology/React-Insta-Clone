4ff631467eaf3b2062774e9c180b40a5f21d4b93
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-03 20:04:51 -0400

    Setup component folder structure and start up files and linked them to each other

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './PostContainer.css';
+import CommentSection from '../CommentSection/CommentSection';
+
+const PostContainer = props => {
+  return (
+    <div>Post Container</div>
+  );
+}
+ 
+export default PostContainer;
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SearchBar.css';
+
+const SearchBar = props => {
+  return (
+    <div>Search Bar</div>
+  );
+}
+ 
+export default SearchBar;
