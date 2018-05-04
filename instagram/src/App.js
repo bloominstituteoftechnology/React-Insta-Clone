27d06aa2bbddab8e28617305b248ce7fce70ab4c
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-03 20:11:09 -0400

    Import dummy data and set to App state after initial render

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import dummyData from './dummy-data';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      posts: []
+    }
+  }
+  componentDidMount(){
+    this.setState({posts : dummyData});
+  }
   render() {
     return (
       <div className="App">
