105b8e9474d80f4e8f0d3b30e58bed1186668a0f
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-03 20:09:07 -0400

    Built and styled SearchBar component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <SearchBar />
       </div>
     );
   }
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './SearchBar.css';
+import instagramLogoText from './assets/instagramLogoText.png';
 
 const SearchBar = props => {
   return (
-    <div>Search Bar</div>
+    <nav className="search-bar">
+      <div className="container">
+        <div className="row search-bar-wrapper">
+          <div className="col-3 logo">
+            <i className="fab fa-instagram"></i>
+            <div className="text-logo">
+              <img src={instagramLogoText} alt="Instagram Logo" />
+            </div>
+          </div>
+          <div className="col-6 search">
+            <input
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+          <div className="col-3 icons">
+            <i className="far fa-dot-circle"></i>
+            <i className="far fa-heart"></i>
+            <i className="far fa-user"></i>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
-}
- 
+};
+
 export default SearchBar;
