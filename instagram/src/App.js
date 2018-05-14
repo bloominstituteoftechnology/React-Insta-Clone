6d3095dcac15ba52b03ceca594802dc89b65299f
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-05-14 14:58:35 -0400

    Fixed Navbar to be more response

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar/SearchBar'; 
-import PostContainer from './components/PostContainer/PostContainer'; 
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SearchBar /> 
+        <SearchBar />
         <div className="post-div">
-          <PostContainer /> 
-        </div> 
-      </div> 
+          <PostContainer />
+        </div>
+      </div>
     );
   }
 }
