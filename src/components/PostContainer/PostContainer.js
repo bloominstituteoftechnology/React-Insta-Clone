cf7e97ea687527b850b3c0867b9827e10941aca6
Austin Merando <ajmerando1@yahoo.com>
2018-06-18 16:59:56 -0700

    few more formatting tweaks

--- src/components/PostContainer/PostContainer.js
+++ src/components/PostContainer/PostContainer.js
@@ -6,27 +6,27 @@ const PostContainer = props => {
     return (
         <div className="post-container">
             <div className="top-container">
-            <h1><img className="thumb-pic"src={props.postThumb} alt="thumb"></img>&nbsp;&nbsp;
+                <h1><img className="thumb-pic" src={props.postThumb} alt="thumb"></img>&nbsp;&nbsp;
             {props.postStarter}</h1>
             </div>
 
             <img className="top-picture" src={props.postImage} alt="post-img"></img>
-                <div className="comment-container">
-                    <div>
-                        {props.postComments.map(post => (
-                            <CommentSection key={Math.random()} postComments={post.text} postUser={post.username} />
-                        ))}
-                    </div>
-                    <div>
-                        <input className="comment-bar"type="text"
-                            placeholder="Add Comment..."
-                        />
-                    </div>
+            <div className="comment-container">
+                <div>
+                    {props.postComments.map(post => (
+                        <CommentSection key={Math.random()} postComments={post.text} postUser={post.username} />
+                    ))}
                 </div>
-                        </div>
-                );
-            };
-            
-      
-            
+                <div>
+                    <input className="comment-bar" type="text"
+                        placeholder="Add a comment..."
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+
 export default PostContainer;
