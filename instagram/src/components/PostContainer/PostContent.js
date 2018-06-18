d1e19707ef089682f09dd17192fb4a392f34e64e
Simon <Simon_Freelance@hotmail.com>
2018-06-18 16:30:17 -0400

    Starting on Post Content

---
+++ instagram/src/components/PostContainer/PostContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Post.css';
+
+const PostContent = (props) => {
+    return (
+    <div className='post-content-container'>
+
+    </div>
+    );
+}
+
+export default PostContent;
