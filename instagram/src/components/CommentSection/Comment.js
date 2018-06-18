65bc8bdadf43eeb8603856a86ee0c497f0995b9f
Nicky Chen <nicky.chen0@gmail.com>
2018-06-18 15:54:09 -0700

    added proptypes

--- instagram/src/components/CommentSection/Comment.js
+++ instagram/src/components/CommentSection/Comment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 const Comment = props => {
@@ -6,6 +7,13 @@ const Comment = props => {
         <div><strong>{props.c.username}</strong> {props.c.text}</div>
     )     
 }
+
+Comment.propTypes = {
+    c: PropTypes.shape({
+        username: PropTypes.string,
+        text: PropTypes.string
+    })
+};
 
 
 export default Comment;
