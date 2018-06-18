8a32e95aa6c639fa0a894fb1fd5f85ad6ef28210
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-18 17:34:22 -0500

    began composition

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import dummyData from './dummy-data';
+import PostContainer from './components/PostContainer/PostContainer';
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: dummyData,
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="search-container">
+        <SearchBar />
+        </div>
+        <PostContainer data={this.state.data} />
       </div>
     );
   }
