2513b9a0e968efbe4744deadb697762a946a97a4
Simon <Simon_Freelance@hotmail.com>
2018-06-18 16:39:55 -0400

    Added footer for post content

--- instagram/src/components/PostContainer/PostContent.js
+++ instagram/src/components/PostContainer/PostContent.js
@@ -3,11 +3,12 @@ import './Post.css';
 
 const PostContent = (props) => {
     return (
-    <div className='post-content-container'>
+        <div className='post-content-container'>
             <img src={props.picture} className='picture-content' />
             <div className='content-footer'>
-
-    </div>
+                
+            </div>
+        </div>
     );
 }
 
