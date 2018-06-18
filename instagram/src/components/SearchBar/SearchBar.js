d8a7b58851f30ed648c1f0eb691464910dcced9d
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-06-18 15:51:19 -0700

    adds styling for SearchBar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import './SearchBar.css';
 
-const SearchBar = () => <div>SearchBar</div>;
+const SearchBar = () => (
+  <div className="container">
+    <div className="row title">
+      <i className="col-sm-1 fab fa-instagram" />
+      <div className="col-sm-1 instaFont">Instagram</div>
+      <div className="col-sm-8">
+        <button className="searchButton">Search</button>
+      </div>
+      <div className="">
+        <i className="col-sm-1 far fa-compass" />
+        <i className="col-sm-1 far fa-heart" />
+        <i className="col-sm-1 far fa-user" />
+      </div>
+    </div>
+  </div>
+);
 
 export default SearchBar;
