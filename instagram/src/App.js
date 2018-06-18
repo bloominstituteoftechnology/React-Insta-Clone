b21d538ff23be0c8a48546bf0281c3b715dcd596
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-06-18 14:52:02 -0500

    Ahmed Belhadj - React Insta Clone
    SearchBar markup.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SearchBar from "../src/components/SearchBar/SearchBar";
+import dummyData from "./dummy-data";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <SearchBar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,71 @@
+import React from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Form,
+  FormGroup,
+  Input
+} from "reactstrap";
+export default class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">
+            <i className="fab fa-instagram fa-2x" /> | Instagram
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <Form inline>
+                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                  <Input
+                    type="search"
+                    name="search"
+                    id="search"
+                    placeholder={
+                      <i className="fas fa-search" /> + "\uf002 Search"
+                    }
+                  />
+                </FormGroup>
+              </Form>
+              <NavItem>
+                <NavLink href="#">
+                  <i className="far fa-compass fa-2x" />
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">
+                  <i className="far fa-heart fa-2x" />
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">
+                  <i class="far fa-user fa-2x" />
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
