c274667cbbb754d4ab797be8f27d7d35f4d24617
Erin Meredith <ekmeredith@att.net>
2018-06-18 19:45:06 -0400

    added search bar

--- instagram/src/components/SearchBar/Search.js
+++ instagram/src/components/SearchBar/Search.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './Search.css';
 
 const SearchBar = props => {
     return (
-        <div> Search bar </div>
+        <div className='searchbar'>
+            <div className='logos'>
+                <img className='logo' alt='logo' src='https://cdn.dribbble.com/users/59145/screenshots/2722476/instagram_new_logo.jpg'/>
+                <img className='instagram' alt='instagram' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png' />
+            </div>
+            <div>
+                <input type='text'
+                className='search'
+                placeholder='Search'
+                onChange={props.toDoHandler}
+                value={props.dataInput}/>
+            </div>
+            <div className='icons'>
+                 <img className='heart-icon' alt='heart icon' src='https://png.icons8.com/metro/1600/like.png'/>
+                 <img className='friend-icon' alt='friend icon' src='https://d30y9cdsu7xlg0.cloudfront.net/png/682465-200.png'/>
+            </div>
+        </div>
     )
 }
 
