e823eca5a52ee5243c4db0e1ca37b892b81389b5
TangledTessellations <mcevoya@oregonstate.edu>
2018-06-18 15:58:50 -0700

    Continued working on search bar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './SearchBar.css';
 
 const SearchBar = props => {
     return (
-        <div>
-            
+        <div className="search-bar">
+        <i class="fas fa-camera-retro"></i> 
+        <img className="insta-logo" src="https://www.edigitalagency.com.au/wp-content/uploads/instagram-logo-text-black-png.png" alt="instagram logo" /> 
+        <input></input> 
+        <i class="far fa-compass"></i>
+        <i class="far fa-heart"></i>
+        <i class="fas fa-user-alt"></i>
+
+
+
         </div>
     );
 }
