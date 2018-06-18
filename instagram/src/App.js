fb6fb0c9c286829e1cc82c59b05233507511f479
JacobLayton <jlayjlay3@gmail.com>
2018-06-18 15:59:02 -0700

    Search Bar components in place, needs styling

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
 
 class App extends Component {
   constructor() {
@@ -14,13 +15,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchBar />
       </div>
     );
   }
--- instagram/src/components/SearchBar/SearchBar.jsx
+++ instagram/src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,18 @@
 import React from 'react';
+import './SearchBar.css';
 
+const SearchBar = () => (
+    <div className="search-bar">
+        <div className="logo">Instagram</div>
+        <div className="search-input">
+            <input type="text" placeholder="search"></input>
+        </div>
+        <div className="navigation-logos">
+            <div className="compass">1</div>
+            <div className="likes">2</div>
+            <div className="profile">3</div>
+        </div>
+    </div>
+)
 
 export default SearchBar
