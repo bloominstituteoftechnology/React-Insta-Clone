38a3464114507b58ca2d9ce2a288cd9618d82183
Allen Hai <allenhai03@gmail.com>
2018-06-18 17:40:47 -0500

    display image in PostContainer

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -5,6 +5,7 @@ import './PostContainer.css'
 const PostContainer = ({data}) => (
     <div className="post-container">
         <PostHeader data={data} />
+        <img src={data.imageUrl} className="post-image" alt=""></img>
     </div>
 )
 
