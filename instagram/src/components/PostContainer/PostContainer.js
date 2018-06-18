c0f9c94dae8cb0a82df1e79cfbee7c8f64fe3f80
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-06-18 18:54:01 -0400

    added css to container, made some adjusments, added like and comment images

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import activity from '../SearchBar/activity.png';
+import like from './like.png';
+import comment from './comment.png';
 import CommentSection from '../CommentSection/CommentSection';
 import './PostContainer.css';
 
 const PostContainer = props => {
     return (
-        <div>
-            Posts
+        <div className="container">
             {props.data.map((post, index) => (
-                <div key={post.username + index}>
-                    <div>
-                        <img src={post.thumbnailUrl}/>
+                <div className="post" key={post.username + index}>
+                    <div className="post-header">
+                        <img className="header-image" src={post.thumbnailUrl}/>
                         {post.username}
                     </div>
-                    <div>
+                    <div className="post-image">
                         <img src={post.imageUrl}/>
                     </div>
-                    <div>
-                        <button><img src={activity}/></button>
+                    <div className="post-options">
+                        <button><img className="options-post" src={like}/></button>
+                        <button><img className="options-post" src={comment}/></button>
                     </div>
-                    <div className="likes">
-                        {post.likes}
+                    <div className="post-likes">
+                        {`${post.likes} likes`}
                     </div>                    
-                    <CommentSection comments={post.comments} />
+                    <CommentSection className="post-comments" comments={post.comments} />
                     <div className="timestamp">
                         {post.timestamp}
-                    </div>                    
+                    </div>
+                    <input className="newComment" placeholder="Add a comment..." />
                 </div>
             ))}
         </div>
