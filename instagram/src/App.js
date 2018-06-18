31bb8df205320961c2e7b41a4024997a6319a350
jeffyjkang <jeffyjkang@gmail.com>
2018-06-18 13:10:46 -0700

    jeff Kang update insta

---
+++ instagram/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+import dummyData from './dummy-data'
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: dummyData
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <header className="App-header">
+         
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+      </div>
+    );
+  }
+}
+
+export default App;
