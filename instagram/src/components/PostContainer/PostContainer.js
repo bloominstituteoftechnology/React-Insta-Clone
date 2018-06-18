5d2b22ba2c8971642b82da83ee8ebf540b581928
Kamry Bowman <kamry.bowman@gmail.com>
2018-06-18 16:10:17 -0600

    Incorporate all post data

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -6,7 +6,25 @@ import PropTypes from 'prop-types';
 function PostContainer (props) {
     return (
         <div className="post-container">
-            <h1>Post-container</h1>
+            <div className="post-header">
+                <div className="user-thumbnail">
+                    <img src={props.post.thumbnailUrl} alt={props.post.username + " thumbnail"}></img>
+                </div>
+                <div className="user-name">
+                    <h3>{props.post.username}</h3>
+                </div>
+            </div>
+            <div className="main-img">
+                <img src={props.post.imageUrl} alt="main post image"></img>
+            </div>
+            <div className="icon-strip">
+                <div className="heart-icon">HEART</div>
+                <div className="speech-icon">SPEECH</div>
+                <div className="bookmark-icon">BOOKMARK</div>
+            </div>
+            <div className="likes-strip">
+                <h3>{"" + props.post.likes + ((props.post.likes > 1) ? " likes" : " like")}</h3>
+            </div>
             <CommentSection comments={props.post.comments} />
             <input type="text" className="new-comment"/>
         </div>
