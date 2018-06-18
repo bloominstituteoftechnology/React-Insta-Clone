2da49dccc4ce4b8d04658f2cc7f518bb7b5ecc20
TangledTessellations <mcevoya@oregonstate.edu>
2018-06-18 15:21:31 -0700

    built CommentSection

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -3,7 +3,11 @@ import React from 'react';
 const CommentSection = props => {
     return (
         <div>
-
+            {props.commentInfo.map(comment => 
+                <div>
+                    {comment.username} {comment.text}<br />
+                </div>
+            )}
         </div>
     );
 }
