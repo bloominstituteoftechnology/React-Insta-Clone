5f601f4406dcc1b844115cea503ecd0856bf2425
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-06-18 13:53:00 -0700

    passes props to PostContainer

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import dummyData from './dummy-data';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData,
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <SearchBar />
-        <PostContainer />
+        {this.state.dummyData.map(d => {
+          return <PostContainer key={Math.random()} data={d} />;
+        })}
       </div>
     );
   }
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './PostContainer.css';
+import PropTypes from 'prop-types';
 import CommentSection from '../CommentSection/CommentSection';
 
-const PostContainer = () => (
+const PostContainer = props => (
   <div>
-    PostContainer
+    {props.data.username}
     <CommentSection />
   </div>
 );
+
+PostContainer.propTypes = {
+  data: PropTypes.shape({
+    username: PropTypes.string,
+    thumbnailUrl: PropTypes.string,
+    imageUrl: PropTypes.string,
+    likes: PropTypes.number,
+    timestamp: PropTypes.string,
+    comments: PropTypes.array,
+  }),
+};
 
 export default PostContainer;
