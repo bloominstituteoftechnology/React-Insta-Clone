9c10361cd3d9f5fa00c65d870f6d2575e16b11c0
Simon <Simon_Freelance@hotmail.com>
2018-06-18 18:25:56 -0400

    Finished rough draft styling of comment adder

--- instagram/src/components/CommentSection/CommentAdder.js
+++ instagram/src/components/CommentSection/CommentAdder.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Comment.css';
 
 const CommentAdder = (props) => {
-    return <input type = 'text' />;
+    return <input type = 'text' className = 'comment-adder' placeholder = 'Add a comment...'/>;
 }
 
 export default CommentAdder;
