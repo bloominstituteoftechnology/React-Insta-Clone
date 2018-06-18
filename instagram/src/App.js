8eb6bd6c50ee83857d470dec0ffa280c95484278
cmok4290 <cmok4290@gmail.com>
2018-06-18 15:47:11 -0400

    moved dummy data to src

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+
+        
       </div>
     );
   }
