cd8042280abde1b5f9a9babfaf186dc981dd0d1b
Nicky Chen <nicky.chen0@gmail.com>
2018-06-18 15:05:24 -0700

    added search bar

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import dummyData from './dummy-data';
 import PostContainer from './components/PostContainer/PostContainer';
+import SearchBar from './components/SearchBar/SearchBar';
 
 class App extends Component {
   constructor() {
@@ -19,6 +20,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to Instagram Clone</h1>
         </header>
+        <SearchBar />
         <PostContainer datas={this.state.dummyData} />
       </div>
     );
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return <input
+    placeholder="Search here"    
+    />
+}
+
+export default SearchBar;
