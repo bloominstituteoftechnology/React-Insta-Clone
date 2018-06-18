5d53eaa430f28a6312f19f12b0dc7333ed0107ba
Simon <Simon_Freelance@hotmail.com>
2018-06-18 19:59:46 -0400

    Added propTypes

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -3,6 +3,8 @@ import './Post.css';
 import PostHeader from './PostHeader';
 import PostContent from './PostContent';
 import CommentsContainer from '../CommentSection/CommentsContainer';
+import PropTypes from 'prop-types';
+
 const PostContainer = (props) => {
     return (
         <div className='post-container'>
@@ -14,10 +16,19 @@ const PostContainer = (props) => {
                 likes={props.data.likes}
             />
 
-           <CommentsContainer comments={props.data.comments}/>
+            <CommentsContainer comments={props.data.comments} />
         </div>
     );
 }
 
+PostContainer.propTypes = {
+    data: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        thumbnailUrl: PropTypes.string.isRequired,
+        imageUrl: PropTypes.string.isRequired,
+        likes: PropTypes.object.isRequired,
 
+        comments: PropTypes.array,
+    })
+};
 export default PostContainer;
