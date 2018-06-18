1eab4451fd69142cea62ba781f688406935ea357
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-06-18 14:00:01 -0700

    passes comment propst to CommentSection

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './CommentSection.css';
 
-const CommentSection = () => <div>CommentSection</div>;
+const CommentSection = props =>
+  props.comment.map(c => (
+    <div>
+      <div>{c.username}</div>
+      <div>{c.text}</div>
+    </div>
+  ));
 
+CommentSection.propTypes = {
+  commnet: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    comment: PropTypes.string,
+  }),
+};
 export default CommentSection;
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -6,18 +6,18 @@ import CommentSection from '../CommentSection/CommentSection';
 const PostContainer = props => (
   <div>
     {props.data.username}
-    <CommentSection />
+    <CommentSection comment={props.data.comments} />
   </div>
 );
 
 PostContainer.propTypes = {
   data: PropTypes.shape({
-    username: PropTypes.string,
-    thumbnailUrl: PropTypes.string,
-    imageUrl: PropTypes.string,
-    likes: PropTypes.number,
-    timestamp: PropTypes.string,
-    comments: PropTypes.array,
+    username: PropTypes.string.isRequired,
+    thumbnailUrl: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+    timestamp: PropTypes.string.isRequired,
+    comments: PropTypes.array.isRequired,
   }),
 };
 
