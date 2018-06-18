2c2d893899c79290ea2991fb32afcedd8a9d4b85
Simon <Simon_Freelance@hotmail.com>
2018-06-18 17:37:29 -0400

    Imported props types

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -2,19 +2,21 @@ import React from 'react';
 import './Post.css';
 import PostHeader from './PostHeader';
 import PostContent from './PostContent';
+import PropTypes from 'prop-types';
 const PostContainer = (props) => {
     return (
-    <div className='post-container'>
-        <PostHeader 
-        thumbnail = {props.data.thumbnailUrl} 
-        username = {props.data.username}/>
-        <PostContent 
-        picture = {props.data.imageUrl}
-        comments = {props.data.comments}
-        likes = {props.data.likes}
-         />
-    </div>
+        <div className='post-container'>
+            <PostHeader
+                thumbnail={props.data.thumbnailUrl}
+                username={props.data.username} />
+            <PostContent
+                picture={props.data.imageUrl}
+                comments={props.data.comments}
+                likes={props.data.likes}
+            />
+        </div>
     );
 }
+
 
 export default PostContainer;
