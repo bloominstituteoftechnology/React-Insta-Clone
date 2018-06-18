5b94c1f743eb0162fb357e1179956819626a5f7a
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-06-18 19:08:50 -0400

    removed top Instagram container

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -19,12 +19,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">
-            Instagram
-          </h1>
-        </header>
+      <div className="App">  
         <SearchBar />
         <PostContainer calcTime={this.calcTime} data={this.state.dummyData} today={this.state.today}/>
       </div>
