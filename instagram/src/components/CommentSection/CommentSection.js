389734469f0699e36bf6a6a9977d099ec315eb20
-tristan- <tristan.linehan@gmail.com>
2018-06-19 09:15:00 -0700

    added username to comments

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -6,21 +6,17 @@ const CommentSection = (props) => {
     return(
         <div className="comment-section">
             Placeholder: "CommentSection"
-            
+
             {props.propComments.map((comment, index) => {
                 return(
                     <div className="comment" key={index}>
                     
-                        <p>{comment.text}</p>
+                        <p>{comment.username + '- '}{comment.text}</p>
 
                     </div>
                 )
             })}
-            <div className="comment">
-
-            </div>
         </div>
-
     )
 }
 export default CommentSection ;
