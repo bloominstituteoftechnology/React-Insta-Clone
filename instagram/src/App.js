6f0b34f2b6d01c14a12cb74986a25559fd532458
Kamry Bowman <kamry.bowman@gmail.com>
2018-06-19 16:50:20 -0600

    Search in place but broken

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -10,19 +10,57 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      searchInput: '',
       posts: null
     }
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleSearchInputChange = this.handleSearchInputChange.bind(this);
   }
 
   componentDidMount() {
     this.setState({posts: dummyData});
   }
 
-  render() {
+  handleSearch (event) {
+    event.preventDefault();
+    if (this.state.searchInput !== '') {
+      let searchStr = this.state.searchInput;
+      let searchRx = new RegExp(searchStr, "i");
+      let posts = this.state.posts.slice(0,);
+      posts = posts.map((post) => {
+        let visibility = false;
+        if (searchRx.test(post.username)) {
+          visibility = true;
+        }
+        let returnObj = {visible: visibility};
+        return Object.assign(post, returnObj);
+      });
+      this.setState({
+        posts: posts,
+        searchInput: ''
+      })
+    }
+  }
 
-    let posts = null;
+  handleSearchInputChange(event) {
+    let text = event.target.value;
+    this.setState({
+      searchInput: text
+    }, this.handleSearch());
+  }
+
+  filterPostsforVisibility(arr) {
+    let filteredArr = arr.filter((post) => {
+      return post.visible;
+    });
+    return filteredArr;
+  }
+
+  render() {
+    let postElems = null;
     if (this.state.posts) {
-      posts = this.state.posts.map((post) => {
+      let posts = this.filterPostsforVisibility(this.state.posts);
+      posts = posts.map((post) => {
         return (
           <PostContainer key={post.thumbnailUrl} post={post} />
         );
@@ -32,9 +70,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <SearchBar />
+          <SearchBar searchInput={this.state.searchInput} handleSearchInputChange={this.handleSearchInputChange} handleSearch={this.handleSearch} />
         </header>
-        {posts}
+        {postElems}
       </div>
     );
   }
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,30 +1,45 @@
-import React from 'react';
-import './SearchBar.css';
-import logo from './img/instagram-header.png';
+import React from "react";
+import "./SearchBar.css";
+import logo from "./img/instagram-header.png";
 
-function SearchBar (props) {
-    return (
-        <div className="search-bar">
-        <div className="left-header-content">
+function SearchBar(props) {
+  return (
+    <div className="search-bar">
+      <div className="left-header-content">
         <a href="#">
-            <div className="logo">
-        <div className="logo-icon"><i className="fab fa-instagram"></i></div>
-
-          <img src={logo} className="logo-text"></img>
+          <div className="logo">
+            <div className="logo-icon">
+              <i className="fab fa-instagram" />
             </div>
+
+            <img src={logo} className="logo-text" />
+          </div>
+        </a>
+      </div>
+      <div className="search-input">
+        <form onSubmit={props.handleSearch}>
+          <input id="search-input" type="text" placeholder="Search" value={props.searchInput} onChange={props.handleSearchInputChange}/>
+        </form>
+      </div>
+      <div className="right-header-content">
+        <div className="navigation">
+          <a href="#">
+            <i className="far fa-compass" />
           </a>
         </div>
-        <div className="search-input">
-          <input id="search-input" type="text" placeholder="Search" />
-          </div>
-          <div className="right-header-content">
-          <div className="navigation"><a href="#"><i className="far fa-compass"></i></a></div>
-          <div className="like"><a href="#"><i className="far fa-heart"></i></a></div>
-          <div className="profile"><a href="#"><i className="far fa-user"></i></a></div>
-
-          </div>
+        <div className="like">
+          <a href="#">
+            <i className="far fa-heart" />
+          </a>
         </div>
-    );
+        <div className="profile">
+          <a href="#">
+            <i className="far fa-user" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SearchBar;
