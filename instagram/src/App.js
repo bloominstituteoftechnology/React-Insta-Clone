c6f57d72c5d2ef0f3031001cbd43aa4ec8a96fc7
Alec <afroenygam@gmail.com>
2018-06-19 17:00:57 -0400

    Code refactored to coincide with project guidelines

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
+import dummyData from './dummy-data';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      postData: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({postData: dummyData})
+  }
+
   render() {
     return (
       <div className="App">
         <SearchBar />
-        <div className="post-wrapper">
-          <PostContainer />
+        <div className="posts-wrapper">
+          {this.state.postData.map(post => <PostContainer post={post} />)}
         </div>
       </div>
     );
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,21 +1,25 @@
-import React, {Component} from 'react';
-import Post from '../Post/Post';
-import dummyData from '../../dummy-data';
+import React from 'react';
+import CommentSection from '../CommentSection/CommentSection';
+import { Card, CardImg, CardText, CardBody, CardLink,
+  CardTitle, CardSubtitle } from 'reactstrap';
 
-
-class PostContainer extends Component {
-  state = {
-    postData: []
-  };
-
-  componentDidMount() {
-    this.setState({postData: dummyData});
-  }
-
-  render() {
-    const { postData } = this.state;
-    return <div>{postData.map(post => <Post post={post} />)}</div>;
-  }
-}
+const PostContainer = props => {
+  return (
+      <Card className="test">
+        <CardBody className="card-header">
+        <img className="thumb-image" src={props.post.thumbnailUrl} alt="" />
+        <CardTitle className="username">{props.post.username}</CardTitle>
+        </CardBody>
+        <CardImg className="post-image" src={props.post.imageUrl} alt="" />
+        <CardBody>
+        <h5>{props.post.likes} likes</h5>
+        <CommentSection comments={props.post.comments} />
+        <h6>{props.post.timestamp}</h6>
+        <hr/>
+        <input style={{width: 500 +'px'}} type="text" name="comment" placeholder="Add a comment..." value={props.value} onChange={props.addCommentHandler}/>
+        </CardBody>
+      </Card>
+  );
+};
 
 export default PostContainer;
