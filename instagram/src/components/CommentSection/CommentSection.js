fbe5b5cc20be0ef1086f135ec50911d33421c4e2
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-19 15:23:21 -0500

    refactored code

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,15 +1,30 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import Comment from '../Comment/Comment';
+import PropTypes from 'prop-types';
+import Comment from './Comment';
+import CommentInput from './CommentInput';
 
-const CommentSection = props => {
-    return (
-        <div>
-            <p>{props.comment.username}</p>
-            <p>{props.comment.text}</p>
-        </div>
-    )
+class CommentSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: props.comments,
+        };
+    }
 
+    render() {
+        return(
+            <div>
+                {this.state.comments.map((comment, i) => <Comment key={i} comment={comment} />)}
+                <CommentInput />
+            </div>
+        )
+    };
 }
+
+CommentSection.propTypes = {
+    comments: PropTypes.arrayOf(
+        PropTypes.shape({text: PropTypes.string, username: PropTypes.string})
+    )
+};
 
 export default CommentSection;
