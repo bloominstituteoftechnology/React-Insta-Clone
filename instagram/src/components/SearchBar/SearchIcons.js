7335799b24c5941fd3cd8e6d67e69b2deab90cb4
Simon <Simon_Freelance@hotmail.com>
2018-06-18 23:37:24 -0400

    Changed class to ClassName

--- instagram/src/components/SearchBar/SearchIcons.js
+++ instagram/src/components/SearchBar/SearchIcons.js
@@ -5,9 +5,9 @@ import './Search.css';
 const SearchIcons = (props) => {
     return (
         <div className='search-icons-container'>
-            <i class="far fa-compass"></i>
-            <i class="far fa-heart"></i>
-            <i class="far fa-user"></i>
+            <i className ="far fa-compass"></i>
+            <i className ="far fa-heart"></i>
+            <i className ="far fa-user"></i>
         </div>
     )
 }
