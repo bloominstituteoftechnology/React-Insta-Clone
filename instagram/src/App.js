4b5a8861f6a7c46a12ba1e747955415ade672abd
Kamry Bowman <kamry.bowman@gmail.com>
2018-06-19 15:56:39 -0600

    Refactor to componentDidMount loading of dummyData

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -6,17 +6,35 @@ import './App.css';
 import dummyData from './dummy-data';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: null
+    }
+  }
+
+  componentDidMount() {
+    this.setState({posts: dummyData});
+  }
+
   render() {
+
+    let posts = null;
+    if (this.state.posts) {
+      posts = this.state.posts.map((post) => {
+        return (
+          <PostContainer key={post.thumbnailUrl} post={post} />
+        );
+      })
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <SearchBar />
         </header>
-        {dummyData.map((post) => {
-          return (
-            <PostContainer key={post.thumbnailUrl} post={post} />
-          );
-        })}
+        {posts}
       </div>
     );
   }
