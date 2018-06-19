30795e7890297a143214440b8e68374b03bfdf0b
Luis <luis@Luiss-MacBook-Air.local>
2018-06-18 19:03:08 -0700

    re-factor App to maintain a state & changed route to map over dummyData

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -5,11 +5,20 @@ import PostContainer from './components/PostContainer/PostContainer'
 import './index.css'
 import dummyData from './components/dummy-data'
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      post: dummyData
+    }
+  }
   render () {
     return (
       <div className='container'>
         <TheSearchBar />
-        <PostContainer post={dummyData} />
+        {/* <PostContainer /> */}
+        {this.state.post.map((post, index) => (
+          <PostContainer key={post.username + index} post={post} />
+        ))}
       </div>
     )
   }
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -7,28 +7,27 @@ import PropTypes from 'prop-types'
 const PostContainer = (props) => {
   return (
     <div className='Post-Container'>
-      {props.post.map((post, index) => (
-        <Card key={`${post.username}.${index}`}>
-          <CardBody>
-            <img
-              src={post.thumbnailUrl}
-              alt='profile-pic'
-              className='img-fluid rounded-circle'
-            />
-            <span>{post.username}</span>
-          </CardBody>
-          <img width='60%' src={post.imageUrl} alt='Card cap' />
-          <h6>{post.likes} likes</h6>
-          <CardBody>
-            <CommentSection comments={post.comments} />
-          </CardBody>
-          <h6>{post.timestamp}</h6>
-          <input placeholder='Add Comment' type='text' />
-        </Card>
-      ))}
+      <Card key={props.key}>
+        <CardBody>
+          <img
+            src={props.post.thumbnailUrl}
+            alt='profile-pic'
+            className='img-fluid rounded-circle'
+          />
+          <span>{props.post.username}</span>
+        </CardBody>
+        <img width='60%' src={props.post.imageUrl} alt='Card cap' />
+        <h6>{props.post.likes} likes</h6>
+        <CardBody>
+          <CommentSection comments={props.post.comments} />
+        </CardBody>
+        <h6>{props.post.timestamp}</h6>
+        <input placeholder='Add Comment' type='text' />
+      </Card>
     </div>
   )
 }
+
 PostContainer.propTypes = {
   post: PropTypes.array,
   likes: PropTypes.string
