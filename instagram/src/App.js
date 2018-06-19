d461dda42d92d79a807b599860a366343d41f9e7
snowcoding <vimalshah77@gmail.com>
2018-06-18 20:48:56 -0700

    Basic Search bar in place

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import dummyData from './dummy-data.js'
 import PostContainer from './components/PostContainer/PostContainer';
+import SearchBar from './components/SearchBar/SearchBar';
 
 class App extends Component {
   constructor(){
@@ -12,8 +13,11 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="app-container">
-       {this.state.data.map( postObj =>  <PostContainer key={postObj.timestamp} postObj={postObj}/>)}
+      <div>
+        <SearchBar />
+        <div className="app-container">
+          {this.state.data.map( postObj =>  <PostContainer key={postObj.timestamp} postObj={postObj}/>)}
+       </div>
       </div>
     );
   }
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,35 @@
+import React,{Component} from 'react';
+import './SearchBar.css'
+import gram_icon from './gram-icon.jpg'
+import gram_logo from './instagram.png'
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() { 
+    return ( 
+      <div className="container-fluid search-bar">
+        <div className="row">
+          <div className="col-md-3 d-flex">
+            <div className="gram-icon">
+              <img src={gram_icon} alt="" className="img-fluid gram-icon"/>
+            </div>
+            <div className="gram-logo">
+              <img src={gram_logo} alt="" className="img-fluid gram-logo"/>
+            </div>  
+          </div>
+          <div className="col-md-6 search-field">
+            <input type="text" value="" placeholder="Search" />
+          </div>
+          <div className="col-md-3 search-icon-group">
+            <i className="far fa-compass"></i>
+            <i className="far fa-heart"></i>
+            <i className="far fa-user"></i>
+          </div>
+        </div>
+      </div>
+     )
+  }
+}
+export default SearchBar;
