4079cf6fb6d11bd3ad7a422cdec35e0aab4e2bfe
Ryan Hamblin <ryanhca@gmail.com>
2018-06-19 09:19:23 -0600

    adds comments to state in commentSectionContainer

--- instagram/src/components/CommentSection/CommentSectionContainer.js
+++ instagram/src/components/CommentSection/CommentSectionContainer.js
@@ -3,14 +3,22 @@ import PropTypes from 'prop-types';
 import Comment from './Comment';
 import CommentInput from './CommentInput';
 
-const CommentSection = props => {
-  return (
-    <div>
-      {props.comments.map((c, i) => <Comment key={i} comment={c} />)}
-      <CommentInput />
-    </div>
-  );
-};
+class CommentSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: props.comments
+    };
+  }
+  render() {
+    return (
+      <div>
+        {this.state.comments.map((c, i) => <Comment key={i} comment={c} />)}
+        <CommentInput />
+      </div>
+    );
+  }
+}
 
 CommentSection.propTypes = {
   comments: PropTypes.arrayOf(
