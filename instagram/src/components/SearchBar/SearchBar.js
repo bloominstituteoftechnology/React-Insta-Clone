b2c0952dd85fa3676227f05b61bd7fab4bf44dfd
Daniel Chamorro <dan@dcitllc.com>
2018-06-19 02:04:10 -0400

    Still trying Search Bar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -4,11 +4,9 @@ import "./SearchBar.css";
 const SearchBar = () => {
   return (
     <div className="search-bar">
-      <div className="row">
-        <div className="col fab fa-instagram" />
-        <div className="col">Instagram</div>
-        <input type="text" />
-      </div>
+      <div className="fa fa-instagram" />
+      <div>instagram</div>
+      <input type="text" />
     </div>
   );
 };
