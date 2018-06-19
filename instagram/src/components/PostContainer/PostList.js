3aa3c28eed0755b4b7f78b2114c25d1a83f41763
Simon <Simon_Freelance@hotmail.com>
2018-06-19 19:13:49 -0400

    Key now uses ids

--- instagram/src/components/PostContainer/PostList.js
+++ instagram/src/components/PostContainer/PostList.js
@@ -3,11 +3,13 @@ import './Post.css';
 import PostContainer from './PostContainer';
 
 const PostList = (props) => {
+    
     return (
         <div className='post-list-container'>
-            {props.instaData.map((dataItem, i) => {
+            {
+                props.instaData.map((dataItem, i) => {
                 return <PostContainer dataItem={dataItem} 
-                    key={i}
+                    key={dataItem.id}
                     index={i}
                     changeCommentHandler={props.changeCommentHandler}
                     addCommentHandler={props.addCommentHandler}
