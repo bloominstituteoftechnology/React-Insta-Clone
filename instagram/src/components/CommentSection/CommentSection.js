3fd4fe44968e1a0c0d2369a2b3d73d305fea9fa8
Jameson <jamesonbrown@neolivity.com>
2018-06-19 16:09:24 -0600

    Add a comment method added.

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -2,30 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './CommentSection.css';
 
-const CommentSection = props => {
-    return (
-        <div className="comment-section">
-            {props.dummyData.map(comment => (
-                <div className="custom-comment" key={Math.random()}>
-                    <p>{comment.username}</p>
-                    <p>{comment.text}</p>
-                </div>
-            ))}
-            <div>
-                <input type="text" placeholder="Add a comment..."/>
+class CommentSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: props.dummyData,
+            input: '',
+            placeholder: 'Add a comment...'
+        };
+    }
+
+    changeInput = event => this.setState({input: event.target.value});
+
+    addNewComment = (event, i) => {
+        event.preventDefault();
+        const comments = this.state.comments.slice();
+        comments.push({text: this.state.input, username: 'username'});
+        this.setState({comments, input: ''});
+    }
+
+    render() {
+        return (
+            <div className="comment-section">
+                {this.state.comments.map(comment => (
+                    <div className="custom-comment" key={Math.random()}>
+                        <p>{comment.username}</p>
+                        <p>{comment.text}</p>
+                    </div>
+                ))}
+                <form onSubmit={this.addNewComment}>
+                    <input type="text" onChange={this.changeInput} placeholder={this.state.placeholder} value={this.state.input} />
+                </form>
             </div>
-        </div>
-    )
+        )    
+    }
+    
 }
 
-// CommentSection.propTypes = {
-//     dummyData: PropTypes.array
-//     // username: PropTypes.string,
-//     // text: PropTypes.string
-// };
 CommentSection.propTypes = {
-    comments: PropTypes.arrayOf(PropTypes.string)
+    comments: PropTypes.arrayOf(
+        PropTypes.shape({ username: PropTypes.string, text: PropTypes.string })
+    )
 };
-
  
 export default CommentSection;
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
-import CommentInteraction from '../CommentInteraction/CommentInteraction';
 import './PostContainer.css';
+import PropTypes from 'prop-types';
 
 const PostContainer = props => {
     return (
@@ -14,12 +14,27 @@ const PostContainer = props => {
                 <img src={props.dummyData.imageUrl} alt="post content"/>
             </div>
             <div className="post-comments">
-                <CommentInteraction />
+                <div className="comment-interaction">
+                    <i className="fa">&#xf08a;</i>
+                    <i className="fa">&#xf0e5;</i>
+                </div>
                 <p align="left">{props.dummyData.likes} likes</p>
                 <CommentSection dummyData={props.dummyData.comments} />
             </div>            
         </div>
     )
 }
+
+PostContainer.propTypes = {
+    comments: PropTypes.arrayOf(
+        PropTypes.shape({
+            thumnailUrl: PropTypes.string,
+            username: PropTypes.string,
+            imageURL: PropTypes.string,
+            likes: PropTypes.number
+        })
+    )
+};
+
  
 export default PostContainer;
