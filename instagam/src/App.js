6979fc72583a0f8eb10579e5ac26b5c9c345ed66
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-06-20 13:05:49 -0700

    imported Authenticate comp into App.js

--- instagam/src/App.js
+++ instagam/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import PostContainer from './components/PostContainer';
-import SearchBar from './components/SearchBar';
-import dummyData from './dummy-data';
 import PostsPage from './components/PostsPage';
+import Authenticate from './Authentication/Authenticate';
 
 function App() {
   return (
@@ -11,45 +9,4 @@ function App() {
 }
 
 
-export default App;
-
-
-
-
-
-// addComment =(url) => {
-  //   // e.preventDefault();
-  //   // console.log('e: ', e)
-  //   console.log('url: ', url)
-  //   // map over state - commentsData
-  //   // if object.imgURL === url { push newObj into data.comments}
-  //   // setState
-  //   const newCommentsCopy=this.state.commentsData.slice();
-  //   let slicedComment = newCommentsCopy.comments;
-  //   console.log('slicedComment: ', typeof(slicedComment))
-  
-  //   const newComment = this.state.newPost;
-  //   let arr; 
-  //   let newObj={
-  //       username: 'grej',
-  //       text: newComment
-  //   }
-  //   // if(newCommentsCopy.imageUrl===url) {
-  //   //   slicedComment.push(newObj);
-  //   //   this.setState({ commentsData: newCommentsCopy, newPost: '' })
-  //   // } null;
-   
-  //   newCommentsCopy.map(item => {
-  //     if(url === item.imageUrl){
-  //      arr = item.comments;
-  //     arr.push(newObj);
-  //     console.log('arr: ', arr)
-  
-  //       return arr;
-        
-  //   }  null;
-  // })  
-  // let newData= newCommentsCopy;
-  // this.setState({ commentsData: newData, newPost: '' })
-  
-  // }
+export default Authenticate(App);
