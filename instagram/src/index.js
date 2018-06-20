b4d7afc3c45571e38827f5d346f34dc8792a1d09
Simon <Simon_Freelance@hotmail.com>
2018-06-20 17:54:48 -0400

    Fixed what was injected into the DOM

--- instagram/src/index.js
+++ instagram/src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Authenticate from './components/Authentication/Authenticate';
-const WrappedApp = Authenticate(App);
 
-ReactDOM.render(<WrappedApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
