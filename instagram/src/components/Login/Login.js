dd1dcc80f6d3fff3eea4b2fc0d00863e718644a0
cmok4290 <cmok4290@gmail.com>
2018-06-20 16:05:45 -0400

    added login component

---
+++ instagram/src/components/Login/Login.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Login = username => {
+    return (
+        class extends React.Component {
+            constructor() {
+                super();
+                this.state = {
+                    username: ""
+                };
+            }
+
+            componentDidMount() {
+
+            }
+
+            componentWillUnmount() {
+                
+            }
+
+            render() {
+                return (
+                    null
+                );
+            }
+        }
+    );
+};
+
+export default Login;
