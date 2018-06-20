fdff5d85df1e20371f1e626e5e81fa3001c56bb4
snowcoding <vimalshah77@gmail.com>
2018-06-20 14:51:45 -0700

    LOGIN PAGE
    -IN Authenticate.js
    -added logged in flag to state
    -check local storage for item
    -render wrapper of login page conditionally

--- instagram/src/HOCs/Authenticate.js
+++ instagram/src/HOCs/Authenticate.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Login from '../components/Login/Login'
 
 const Authenticate = WrapperComponent =>
   class extends React.Component {
-    render() { 
+    constructor(){
+      super()
+      this.state = {
+        isLoggedIn : false
+      }
+    }
+    componentDidMount(){
+      //
+      if (localStorage.getItem('vim@gmail.com')) {
+        this.setState({isLoggedIn : true})
+      }
+    }
+    render() {
+      if (this.state.isLoggedIn)
       return <WrapperComponent />
+      else 
+      return <Login />
     }
   }
 
