3ffb162336218c1df8d6773e6254d01f620376cc
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-06-20 17:23:09 -0500

    Ahmed Belhadj - React Insta Clone
    Login component functionality.

--- instagram/src/components/Authentication/Authenticate.js
+++ instagram/src/components/Authentication/Authenticate.js
@@ -1,9 +1,23 @@
 import React from "react";
+import Login from "../Login/Login";
 
 const Authenticate = App =>
   class extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        loggedIn: null
+      };
+    }
+    componentDidMount() {
+      this.setState();
+    }
     render() {
-      return <App />;
+      if (this.state.loggedIn === true) {
+        return <App />;
+      } else {
+        return <Login />;
+      }
     }
   };
 
--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -2,18 +2,41 @@ import React from "react";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 export default class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: ""
+    };
+  }
+  loginFunction = event => {
+    event.preventDefault();
+    let username = this.state.username;
+    username = this.state.currentUser;
+    this.setState({
+      currentUser: "",
+      username: username
+    });
+    localStorage.setItem("username", username);
+    document.location.reload();
+  };
+  handleNewLogin = event => {
+    this.setState({ currentUser: event.target.value });
+  };
+
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.loginFunction}>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           {/* <Label for="exampleEmail" className="mr-sm-2">
             Email
           </Label> */}
           <Input
-            type="email"
             name="email"
-            id="exampleEmail"
-            placeholder="Email"
+            id="Email"
+            placeholder="Username"
+            type="text"
+            onChange={this.handleNewLogin}
+            value={this.state.currentUser}
           />
         </FormGroup>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
