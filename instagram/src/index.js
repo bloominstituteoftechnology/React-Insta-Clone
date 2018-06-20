aac47bbf22a5b3ae11d9ee324d110a42a3f80928
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-06-20 18:43:51 -0400

    Second commit
    Bult out LoginPage
    Stuck on linking it to PostsPage

--- instagram/src/index.js
+++ instagram/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-// import '../node_modules/font-awesome/css/font-awesome-min.css/';
+import Login from './components/Login/Login'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Login />, document.getElementById('root'));
