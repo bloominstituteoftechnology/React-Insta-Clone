15715da1ae4a2eda46788885f8151e9729788c79
snowcoding <vimalshah77@gmail.com>
2018-06-20 14:55:32 -0700

    LOGIN PAGE
    - IN Login.js
    - Added State for email and password
    - Added LoginHandler
    - Added onEmailChange
    - Added onPasswordChange
    - Added onLoginHandler
    - Added some d-flex and styling

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import { Container, Row, Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import './Login.css'
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.state ={
+      data : [{email:''},{password:''}]
+    }
   }
+
+  onEmailChange =(e) =>{
+    let localState = this.state.data.slice();
+    localState[0].email = e.target.value;
+    this.setState({data:localState})
+  }
+
+  onPasswordChange =(e) =>{
+    let localState = this.state.data.slice();
+    localState[1].password = e.target.value;
+    this.setState({data:localState})
+  }
+
+  onLoginHandler = (e) => {
+    localStorage.setItem(this.state.data[0].email,this.state.data[1].password)
+    window.location.reload();
+  }
+
   render() { 
     return (
-      <Container>
-        <Row className="d-flex justify-content-center">
+      <Container className="login-container">
+        <Row className="d-flex justify-content-center login-row">
           <Form>
             <FormGroup>
               <Label for="exampleEmail">Email</Label>
-              <Input type="email" name="email" id="exampleEmail" placeholder="Please Enter email" />
+              <Input className="email" type="email" name="email" id="exampleEmail" placeholder="Please Enter email" onChange={this.onEmailChange}/>
             </FormGroup>
             <FormGroup>
               <Label for="examplePassword">Password</Label>
-              <Input type="password" name="password" id="examplePassword" placeholder="Please enter password" />
+              <Input className="password" type="password" name="password" id="examplePassword" placeholder="Please enter password" onChange={this.onPasswordChange}/>
             </FormGroup>
-            <Button>Submit</Button>
+            <Button onClick={this.onLoginHandler}>Login</Button>
           </Form>
         </Row>
       </Container>
