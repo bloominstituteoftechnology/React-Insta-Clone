4e90a901a11b814342a3fecb2bc12c6b2ac14348
Tyson Gushiken <tysonism@gmail.com>
2018-06-20 16:44:06 -0700

    Move app logic out of index

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PostsPage from './components/PostContainer/PostsPage';
+import Authenticate from './components/Authentication/Authenticate';
 import './App.css';
 
-const App = props => {
+const App = () => {
   return (
-    <PostsPage loggedInUsername={props.loggedInUsername} />
+    <Authenticate app={PostsPage} />
   );
 }
 export default App;
--- instagram/src/index.js
+++ instagram/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Authenticate from './components/Authentication/Authenticate';
 import App from './App';
 
-ReactDOM.render(<Authenticate app={App} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
