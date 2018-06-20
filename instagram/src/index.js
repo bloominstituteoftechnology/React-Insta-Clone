1537cd0dcea78b74c06197679f2a944df452e7b6
Simon <Simon_Freelance@hotmail.com>
2018-06-20 16:28:23 -0400

    Wrapped App for authentication purposes

--- instagram/src/index.js
+++ instagram/src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Authenticate from './components/Authentication/Authenticate';
+const WrappedApp = Authenticate(App);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<WrappedApp />, document.getElementById('root'));
