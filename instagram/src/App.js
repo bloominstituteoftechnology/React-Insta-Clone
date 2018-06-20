919cdcf1d91bfdd3cc09fefeddd27113c601354c
Kamry Bowman <kamry.bowman@gmail.com>
2018-06-19 18:49:05 -0600

    Implement rest of search

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,58 +1,47 @@
-import React, { Component } from 'react';
-import SearchBar from './components/SearchBar/SearchBar';
-import PostContainer from './components/PostContainer/PostContainer'
+import React, { Component } from "react";
+import SearchBar from "./components/SearchBar/SearchBar";
+import PostContainer from "./components/PostContainer/PostContainer";
 
-import './App.css';
-import dummyData from './dummy-data';
+import "./App.css";
+import dummyData from "./dummy-data";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      searchInput: '',
+      searchInput: "",
       posts: null
-    }
-    this.handleSearch = this.handleSearch.bind(this);
+    };
     this.handleSearchInputChange = this.handleSearchInputChange.bind(this);
   }
 
   componentDidMount() {
-    let posts = dummyData.map((post) => {
-      return Object.assign({visible: true}, post);
+    let posts = dummyData.map(post => {
+      return Object.assign({ visible: true }, post);
     });
-    this.setState({posts: posts});
-  }
-
-  handleSearch () {
-    if (this.state.searchInput !== '') {
-      let searchStr = this.state.searchInput;
-      let searchRx = new RegExp(searchStr, "i");
-      let posts = this.state.posts.slice(0,);
-      posts = posts.map((post) => {
-        let visibility = false;
-        if (searchRx.test(post.username)) {
-          visibility = true;
-        }
-        let returnObj = {visible: visibility};
-        return Object.assign(post, returnObj);
-      });
-      this.setState({
-        posts: posts,
-        searchInput: ''
-      })
-    }
+    this.setState({ posts: posts });
   }
 
   handleSearchInputChange(event) {
     let text = event.target.value;
+    let searchRx = new RegExp(text, "i");
+    let posts = this.state.posts.slice();
+    posts = posts.map(post => {
+      let visibility = false;
+      if (searchRx.test(post.username)) {
+        visibility = true;
+      }
+      let returnObj = { visible: visibility };
+      return Object.assign(post, returnObj);
+    });
     this.setState({
+      posts: posts,
       searchInput: text
-    }, this.handleSearch());
+    });
   }
 
   filterPostsforVisibility(arr) {
-    let filteredArr = arr.filter((post) => {
+    let filteredArr = arr.filter(post => {
       return post.visible;
     });
     return filteredArr;
@@ -60,19 +49,22 @@ class App extends Component {
 
   render() {
     let postElems = null;
-    if (this.state.posts) {
-      let posts = this.filterPostsforVisibility(this.state.posts);
-      postElems = posts.map((post) => {
-        return (
-          <PostContainer key={post.thumbnailUrl} post={post} />
-        );
-      })
+    let posts = this.state.posts;
+    if (posts) {
+      posts = this.filterPostsforVisibility(posts);
+      postElems = posts.map(post => {
+        return <PostContainer key={post.thumbnailUrl} post={post} />;
+      });
     }
 
     return (
       <div className="App">
         <header className="App-header">
-          <SearchBar searchInput={this.state.searchInput} handleSearchInputChange={this.handleSearchInputChange} handleSearch={this.handleSearch} />
+          <SearchBar
+            searchInput={this.state.searchInput}
+            handleSearchInputChange={this.handleSearchInputChange}
+            handleSearch={this.handleSearch}
+          />
         </header>
         {postElems}
       </div>
