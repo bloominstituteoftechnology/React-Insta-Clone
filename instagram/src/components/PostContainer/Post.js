3f903c20b09953031a68a3fa842ca39c95fa9b44
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-20 17:05:08 -0500

    began toggle like function, work in progress

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -8,11 +8,26 @@ class Post extends React.Component {
         super(props);
         this.state = {
             likes: props.post.likes,
+            liked: false,
+            heartClass: 'far fa-heart'
         }
     }
 
-    incrementLikes = () => {
-        this.setState({likes: this.props.post.likes++});
+    likeToggle = () => {
+        if(!this.state.liked) {
+            this.incrementLike();
+        }
+        else {
+            this.decrementLike();
+        }
+    }
+
+    incrementLike = () => {
+        this.setState({likes: this.props.post.likes++, liked: true, heartClass: 'fas fa-heart'});
+    }
+
+    decrementLike = () => {
+        this.setState({likes: this.props.post.likes--, liked: false, heartClass: 'far fa-heart'});
     }
 
     render() {
@@ -25,7 +40,7 @@ class Post extends React.Component {
                     <img alt="post thumbnail" className="post-image" src={this.props.post.imageUrl} />
                 </div>
                 <div className="likes">
-                    <i onClick={this.incrementLikes} className="far fa-heart"></i>
+                    <i onClick={this.likeToggle} className={this.state.heartClass}></i>
                     <i className="far fa-comment"></i>
                     <p className="likes">{`${this.state.likes} likes`}</p>
                 </div>
