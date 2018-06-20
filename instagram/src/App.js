0b70214b9c2fe5cbf31458d2c80b081a8277538f
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-06-20 15:02:33 -0500

    Ahmed Belhadj - React Insta Clone
    Built High Order Component: Authenticate.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./App.css";
 import PostsPage from "./components/PostContainer/PostsPage";
 
+import Authenticate from "./components/Authentication/Authenticate";
+
 const App = props => {
   return <PostsPage />;
 };
 
-export default App;
+export default Authenticate(App);
