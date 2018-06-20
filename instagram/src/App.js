fd7ab775d3dc976244c4b174ab1e60ebf4e417c7
Allen Hai <allenhai03@gmail.com>
2018-06-20 15:22:59 -0500

    refactor App.js code into PostsPage component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
-import SearchBar from './components/SearchBar/SearchBar'
-import PostContainer from './components/PostContainer/PostContainer'
+import PostsPage from './components/PostContainer/PostsPage'
 import './App.css';
 import data from './dummy-data'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      data: data
-    }
-  }
   render() {
     return (
-      <div className="app-container">
-        <SearchBar />
-        <div className="posts">
-          {this.state.data.map((post, index) => (
-            <PostContainer key={index} data={post} />
-          ))}
-        </div>
-      </div>
+      <PostsPage />
     );
   }
 }
