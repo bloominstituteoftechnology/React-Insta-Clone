eef43cb8667c7652212187da7f0e9ad8b78bd83d
Simon <Simon_Freelance@hotmail.com>
2018-06-20 16:58:34 -0400

    Put Authenticate on the right page.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 
 import PostPage from './components/PostContainer/PostsPage';
+import Authenticate from './components/Authentication/Authenticate';
+const WrappedApp = Authenticate(PostPage);
 function App() {
     return (
-      <PostPage />
+      <WrappedApp />
     );
 }
 
