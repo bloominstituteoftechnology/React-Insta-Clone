01e5d89c7e5c40f2a33b29c293ffdccbcfe14957
Simon <Simon_Freelance@hotmail.com>
2018-06-20 18:53:04 -0400

    Rough draft of Login Page completed

---
+++ instagram/src/components/Login/Login.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+    changeText = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    render() {
+        return (
+            <form className='login__form' onSubmit={e => {
+                e.preventDefault();
+            }}>
+                <input type="text"
+                    className='login__username'
+                    name='username'
+                    placeholder='username'
+                    onChange={e => {
+                        this.changeText(e);
+                    }} />
+
+                <input type="text"
+                    className='login__password'
+                    name='password'
+                    placeholder='password'
+                    onChange={e => {
+                        this.changeText(e);
+                    }} />
+                <button className='login__submit' onClick={e => {
+
+                }}></button>
+            </form>
+        );
+    }
+
+}
+
+export default Login;
