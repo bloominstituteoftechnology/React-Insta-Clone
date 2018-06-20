6d41d146d7a493b5645ac40995aae9c75619a96a
cmok4290 <cmok4290@gmail.com>
2018-06-20 15:29:44 -0400

    added postpage and authenticate components

---
+++ instagram/src/components/Authentication/Authenticate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Authenticate = App => {
+    return (
+        class extends React.Component {
+            render() {
+                return <App />;
+            }
+        }
+    );
+};
+
+export default Authenticate;
---
+++ instagram/src/components/PostContainer/PostPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PostPage = post => {
+    return (
+        class extends React.Component {
+            constructor() {
+                super();
+                this.state = {};
+            }
+        }
+    );
+};
+
+export default PostPage;
