244293bcdf3175312c8b1a1376b627000fb3ae1b
Jameson <jamesonbrown@neolivity.com>
2018-06-19 18:34:14 -0600

    Added some code on SearchBar.js

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
-const SearchBar = props => {
-    return (
-        <div>
-            <input type="text" className="fa" placeholder="&#xf002; Search" />
-        </div>
-    )
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            dummyData: props.dummyData,
+            input: '',
+            placeholder: '&#xf002; Search'
+        }
+    }
+
+    changeSearch = event => this.setState({input: event.target.value});
+
+    // newSearch = (event, i) => {
+    //     event.preventDefault();
+    //     let search = this.state.dummyData.username.slic();
+    //     search = search.filter(item => )      
+    // }
+
+    render() {
+        return (
+            <form onSubmit={this.newSearch}>
+                <input type="text" onChange={this.changeSearch} className="fa" placeholder={this.state.placeholder} value={this.state.input} />
+            </form>
+        )  
+    }
+    
 }
  
 export default SearchBar;
