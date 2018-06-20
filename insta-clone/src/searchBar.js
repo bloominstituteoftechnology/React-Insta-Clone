3ee921e9dbbb7848d959412b104f73f720d79589
Jasper <joshtietz11@gmail.com>
2018-06-20 18:20:05 -0500

    added logout feature

--- insta-clone/src/searchBar.js
+++ insta-clone/src/searchBar.js
@@ -2,12 +2,18 @@ import React from 'react'
 import left from './imgs/left.png'
 import right from './imgs/right.png'
 
+const logOut = e => {
+  localStorage.removeItem('user');
+  window.location.reload();
+}
+
 const SearchBar = props => {
   return (
     <div className="header">
       <span><img src={left} alt="logo" /></span>
       <form onChange={props.searchHandler}><input type="text" placeholder="search" /></form>
       <span><img src={right} alt="navigation options" /></span>
+      <span className="logout" onClick={logOut}>Logout</span>
     </div>
   )
 }
