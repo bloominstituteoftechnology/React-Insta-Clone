1470d2f6751a889d157695d179f69a93d1ab8696
TangledTessellations <mcevoya@oregonstate.edu>
2018-06-20 13:46:39 -0700

    Add basic Login class structure

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,7 +1,30 @@
 import React, {Component} from 'react';
 
 class Login extends Component {
-    
+    constructor(){
+        super();
+        this.state = {
+            userName: this.getUserName(),
+            password: ''
+        }
+    }
+
+    getUserName = () => {
+        return localStorage.getItem("userName") === null ?  "" :  localStorage.getItem("userName");
+    }
+
+    render(){
+        return (
+            <div className="login">
+               UserName: <input className="user-name"
+                                type="text"
+                                value={this.state.userName} />
+                Password: <input type="password"/>
+                <button>Login</button>
+            </div>
+        );
+    }
+
 }
 
 export default Login;
