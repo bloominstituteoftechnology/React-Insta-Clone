18bfd60d8f784237a3249cc432719c874b6d2a0f
Alec <afroenygam@gmail.com>
2018-06-20 17:25:24 -0400

    Basic login page set up

---
+++ instagram/src/components/Login/Login.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../instagram.css'
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleChange = event => {
+    this.setState({[event.target.name: event.target.value]})
+  };
+
+  handleLogin = event => {
+    const user = this.state.username;
+    localStorage.setItem('user', user);
+    window.location.reload();
+  };
+
+  render() {
+    return (
+      <form>
+        Username: <input type="text" placeholder="username" name="username"
+        value={this.state.username} onChange={this.handleChange}/><br>
+        Password: <input type="text" name="password"
+         value={this.state.password} placeholder="password"
+         onChange={this.handleChange}/><br>
+        <input type="submit" value="Log In" onSubmit={this.handleLogin}/>
+      </form
+    )
+  }
+}
+
+export default Login;
