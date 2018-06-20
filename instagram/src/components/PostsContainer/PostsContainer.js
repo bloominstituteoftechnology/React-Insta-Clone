4621544d2079672841d0ab34de7e35e0e9f21151
Ciara Burkett <ciaraburkett@gmail.com>
2018-06-20 10:03:40 -0400

    Implemented Posts Container

---
+++ instagram/src/components/PostsContainer/PostsContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Post from './Post';
+import './Posts.css';
+
+const PostsContainer = props => {
+  return (
+    <div className="posts-container">
+      {props.posts.map(post => {
+        <Post key={post.timestamp} post={post} />
+      })}
+    </div>
+  );
+}
+
+export default PostsContainer;
