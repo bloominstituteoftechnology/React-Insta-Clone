0ddbd0f5e4ccff0aad27b5ac90ad565161cc473a
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-06-20 14:18:13 -0700

    adds login page

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import PostsPage from './components/PostContainer/PostsPage';
 
 const App = () => <PostsPage />;
--- instagram/src/index.js
+++ instagram/src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './components/Login/Login';
+import Authenticate from './components/Authentication/Authenticate';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const WrappedApp = Authenticate(Login);
+
+ReactDOM.render(<WrappedApp />, document.getElementById('root'));
