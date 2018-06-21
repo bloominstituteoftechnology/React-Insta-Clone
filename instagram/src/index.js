cab81ba89050e7aed616ecf64ec731735f80d1cb
Simon <Simon_Freelance@hotmail.com>
2018-06-20 22:20:07 -0400

    Removed an unused import

--- instagram/src/index.js
+++ instagram/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Authenticate from './components/Authentication/Authenticate';
 
 ReactDOM.render(<App />, document.getElementById('root'));
