0fd2370a4d5fa93d9469d94348b78b5bee46f66d
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-06-21 13:49:02 -0400

    added comment functionality, buggy but works

--- instagram/src/components/CommentSection/AddComment.js
+++ instagram/src/components/CommentSection/AddComment.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './CommentSection.css';
 
-const AddComment = () => {
+function AddComment (props) {
     return (
-        <div className='add-container'>
-            <form>
-                <input type='text' placeholder = 'Add a comment...' />
-            </form> 
+        <div className="comment-text">
+            <h3>{props.comment.username}</h3>
+            <p>{props.comment.text}</p>
         </div>
     );
-};
+}
 
-addNewComment = (event, comment, index) => {
-    event.preventDefault();
-    const newData = this.state.dummyData.slice();
-    newData[index].comments.push({username: this.state.placeHolder, text: comment});
-    this.setState({dummyData: newData});
-  }
+AddComment.propTypes = {
+    comment: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired
+    })
+};
 
 export default AddComment;
