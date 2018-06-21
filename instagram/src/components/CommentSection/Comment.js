0c7f676c0296d989f6ea157aef0a85c6fd40e608
Simon <Simon_Freelance@hotmail.com>
2018-06-21 18:24:01 -0400

    Refactored Comments section with styled components

--- instagram/src/components/CommentSection/Comment.js
+++ instagram/src/components/CommentSection/Comment.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import './Comment.css';
+import styled from 'styled-components';
 
+const CommentWrapper = styled.div`
+    display: flex;
+    margin-left: 3%;
+    line-height: 2rem;
+`;
+
+const Username = styled.h3`
+    font-weight: bold;
+    margin-right: 2%;
+`;
+const UserComment = styled.p`
+    font-size: 1.5rem;
+`;
 const Comment = (props) => {
     return (
-        <div className = 'comment-container'>
-            <h3 className='username'>{props.comment.username}</h3>
-            <p className='comment'>{props.comment.text}</p>
-        </div>
+        <CommentWrapper>
+            <Username >{props.comment.username}</Username>
+            <UserComment>{props.comment.text}</UserComment>
+        </CommentWrapper>
     )
 }
 
--- instagram/src/components/CommentSection/CommentsContainer.js
+++ instagram/src/components/CommentSection/CommentsContainer.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import './Comment.css';
-import Comment from './Comment';
-import CommentAdder from './CommentAdder';
-import uuidv1 from 'uuid';
+import React from "react";
+import Comment from "./Comment";
+import CommentAdder from "./CommentAdder";
+import uuidv1 from "uuid";
+import styled from "styled-components";
+const CommentsWrapper = styled.div`
+  margin-bottom: 12px;
+`;
+const CommentsContainer = props => {
+  return (
+    <CommentsWrapper>
+      {props.comments.map((comment, id) => {
+        return <Comment key={uuidv1()} comment={comment} />;
+      })}
 
-const CommentsContainer = (props) => {
-    return (
-        <div className='commentsContainer'>
-
-            {props.comments.map((comment, id) => {
-                return <Comment key={uuidv1()} comment={comment} />
-            })
-            }
-
-            <CommentAdder
-                changeCommentHandler={props.changeCommentHandler}
-                addCommentHandler={props.addCommentHandler}
-                index={props.index} />
-        </div>
-    );
-}
+      <CommentAdder
+        changeCommentHandler={props.changeCommentHandler}
+        addCommentHandler={props.addCommentHandler}
+        index={props.index}
+      />
+    </CommentsWrapper>
+  );
+};
 
 export default CommentsContainer;
