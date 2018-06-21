7951525e5328598008b9f2210135dfbae6c740d7
Ryan Hamblin <ryanhca@gmail.com>
2018-06-20 22:55:00 -0600

    implements readme for day four

--- instagram/src/components/SearchBar/SearchBarContainer.js
+++ instagram/src/components/SearchBar/SearchBarContainer.js
@@ -1,28 +1,84 @@
 import React from 'react';
+import CameraLogo from '../../assets/igcameralogo.png';
 import IGLogo from '../../assets/iglogo.png';
-import './SearchBar.css';
+import styled from 'styled-components';
 
+const Header = styled.header`
+  border-bottom: 1px solid #3333;
+  height: 80px;
+  line-height: 70px;
+  display: flex;
+  justify-content: space-between;
+  background: #ffffff;
+`;
+
+const LogoHeader = styled.div`
+  width: 300px;
+  line-height: 75px;
+`;
+
+const LogoImage = styled.img`
+  height: 50%;
+  padding: 0px 15px;
+`;
+
+const Divider = styled.span`
+  width: 50px;
+`;
+
+const InputWrapper = styled.div`
+  width: 300px;
+  margin: auto;
+  display: flex;
+`;
+
+const SearchInput = styled.input`
+  height: 30px;
+  width: 300px;
+  text-align: center;
+`;
+
+const IconsWrapper = styled.div`
+  display: flex;
+  width: 300px;
+`;
+const SearchIcon = styled.i`
+  position: absolute;
+  display: inline;
+  top: 33px;
+  padding-left: 10px;
+`;
+const SocialDiv = styled.div`
+  width: 50px;
+`;
 const SearchBar = props => {
   return (
-    <div className="search-bar-wrapper">
-      <div className="image-wrapper">
-        <img alt="instagram logo" src={IGLogo} className="logo-image" />
-      </div>
-      <div>
-        <input type="text" placeholder="Search" onKeyDown={props.searchPosts} />
-      </div>
-      <div className="social-wrapper">
-        <div className="social">
+    <Header>
+      <LogoHeader>
+        <LogoImage alt="instagram camera" src={CameraLogo} />
+        <Divider>|</Divider>
+        <LogoImage alt="instagram logo" src={IGLogo} />
+      </LogoHeader>
+      <InputWrapper>
+        <SearchIcon className="fas fa-search" />
+        <SearchInput
+          type="text"
+          placeholder="Search"
+          onKeyDown={props.searchPosts}
+        />
+      </InputWrapper>
+      <IconsWrapper>
+        <SocialDiv>
           <i className="far fa-compass" />
-        </div>
-        <div className="social">
+        </SocialDiv>
+        <SocialDiv>
           <i className="far fa-heart" />
-        </div>
-        <div className="social">
+        </SocialDiv>
+        <SocialDiv>
           <i className="far fa-user-circle" />
-        </div>
-      </div>
-    </div>
+        </SocialDiv>
+      </IconsWrapper>
+    </Header>
   );
 };
 
