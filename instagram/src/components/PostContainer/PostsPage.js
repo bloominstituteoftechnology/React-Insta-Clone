cc8d7b01877d3122c7ad2650930c597a6c5bdd9c
snowcoding <vimalshah77@gmail.com>
2018-06-21 16:08:11 -0700

    REACT WARNING
    - IN PostsPage.js
    
    - Removed Unused components

--- instagram/src/components/PostContainer/PostsPage.js
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PostContainer from './PostContainer';
 import SearchBar from '../SearchBar/SearchBar';
 import styled from 'styled-components'
