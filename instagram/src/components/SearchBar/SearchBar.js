a0260f54b4c1a3de462699735c4a7df165f57b55
snowcoding <vimalshah77@gmail.com>
2018-06-21 16:10:15 -0700

    SEARCH BAR STYLING
    - IN SearchBar.js
    
    - Added Tooltip (reactstrap)
    - Added Logout Handler
    - Added Logout Icon (fa)

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -2,13 +2,26 @@ import React,{Component} from 'react';
 import './SearchBar.css'
 import gram_icon from './gram-icon.jpg'
 import gram_logo from './instagram.png'
+import {Tooltip} from 'reactstrap'
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      tooltipOpen: false
+    };
+
     this.state = {
       inputValue : ''
     }
+  }
+
+  toggle() {
+    this.setState({
+      tooltipOpen: !this.state.tooltipOpen
+    });
   }
 
   onInputValueChange = (e) => {
@@ -16,6 +29,12 @@ class SearchBar extends Component {
       inputValue : e.target.value
     })
     this.props.searchHandler(e);
+  }
+
+  logoutHandler = (e) =>{
+    if (localStorage.getItem('instaclone')) 
+    localStorage.removeItem('instaclone')
+    window.location.reload();
   }
   
   render() { 
@@ -34,9 +53,13 @@ class SearchBar extends Component {
             <input type="text" placeholder="Search by username" value={this.state.inputValue} onChange={this.onInputValueChange}/>
           </div>
           <div className="col-md-3 search-icon-group">
-            <i className="far fa-compass"></i>
-            <i className="far fa-heart"></i>
-            <i className="far fa-user"></i>
+            <i className="far fa-compass disabled"></i>
+            <i className="far fa-heart disabled"></i>
+            <i className="far fa-user disabled"></i>
+            <i className="fas fa-sign-out-alt" id="logoutIcon" onClick={this.logoutHandler}></i>
+            <Tooltip placement="bottom" isOpen={this.state.tooltipOpen} target="logoutIcon" toggle={this.toggle}>
+              Logout
+            </Tooltip>
           </div>
         </div>
       </div>
