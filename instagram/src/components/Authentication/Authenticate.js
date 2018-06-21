e20ed7871ab3c139d4fc243905ee1d3556ffa9d1
Vu Cao <cdhvu@yahoo.com>
2018-06-20 22:05:12 -0700

    Created login handler for LoginForm

--- instagram/src/components/Authentication/Authenticate.js
+++ instagram/src/components/Authentication/Authenticate.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import LoginPage from '../Login/Login';
 
 const Authenticate = App => class extends React.Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             loggedIn: false
         }
     }
     render() {
-        return <App />;
+        return (
+            <div>
+                <LoginPage />
+                <App />
+            </div>
+
+        )
     };
 };
 
--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -9,17 +9,46 @@ class LoginPage extends React.Component{
             password: '',
         }
     }
+
+    usernameHandler = e => {
+        this.setState({username: e.target.value})
+    }
+
+    passwordHandler = e => {
+        this.setState({password: e.target.value})
+    }
+
+    loginSubmit = (e) => {
+        e.preventDefault();
+        if (!this.state.username || !this.state.password) {
+            alert('Please enter valid username or password');
+        } else {
+            if(localStorage.getItem(this.state.username) && this.state.password !== localStorage.getItem(this.state.username) ) {
+                alert('Wrong password')
+            }
+            else {
+                localStorage.setItem(this.state.username, this.state.password)
+                this.setState({username: '', password: ''})
+            }
+        }
+    }
     
     render() {
         return (
-            <Form className="login">
+            <Form onSubmit={this.loginSubmit} className="login">
             <FormGroup>
               <Label for="username">Username</Label>
-              <Input type="text" name="username" id="username" placeholder="..enter username" />
+              <Input 
+                type="text" 
+                name="username" 
+                id="username" 
+                placeholder="..enter username" 
+                onChange={this.usernameHandler} 
+                value={this.state.username}/>
             </FormGroup>
             <FormGroup>
               <Label for="password">Password</Label>
-              <Input type="password" name="password" id="password" placeholder="..enter password" />
+              <Input type="password" name="password" id="password" placeholder="..enter password" onChange={this.passwordHandler} value={this.state.password}/>
             </FormGroup>
             <Button>Submit</Button>
           </Form>
