d5fadce02659745e72fd9f957435d89c5d05c092
Nicky Chen <nicky.chen0@gmail.com>
2018-06-21 13:07:18 -0700

    added styled-components to searchbar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './SearchBar.css';
+import styled from "styled-components";
+
+const Bar = styled.input`
+    width: 250px;
+    height: 30px;
+`;
+
 
 const SearchBar = (props) => {
     return (
         <form>
-             <input
+             <Bar
                 type="text"
                 onKeyDown={props.toggleSelected}
                 className="searchbar"
