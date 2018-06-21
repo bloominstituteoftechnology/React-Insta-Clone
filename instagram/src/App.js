79845031918b9be0addba7c66f744d3f4165d003
DirupT <iamcooled@gmail.com>
2018-06-21 13:55:29 -0500

    converted app to use styled component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
 import PostsPage from './components/PostContainer/PostsPage';
 import Authenticate from './components/Authentication/Authenticate';
+import { AppWrapper } from './components/ReusableComponents/App';
 
 class App extends Component {
     constructor() {
@@ -11,9 +11,11 @@ class App extends Component {
 
     render() {
         return (
-            <div className='App'>
+            <AppWrapper>
+
                 <PostsPage />
-            </div>
+                
+            </AppWrapper>
         );
     }
 }
---
+++ instagram/src/components/ReusableComponents/App.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const AppWrapper = styled.div`
+    margin: 0 auto;
+`
