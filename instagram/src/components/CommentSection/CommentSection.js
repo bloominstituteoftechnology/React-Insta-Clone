0dcf5f20ebaceaba55f5a92732861d0a285d53f3
Ciara Burkett <ciaraburkett@gmail.com>
2018-06-21 08:49:01 -0400

    Finished? Refactoring Comment Section

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -6,31 +6,69 @@ import CommentInput from './CommentInput';
 class CommentSection extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      comments: props.comments
+      comments: props.comments,
+      comment: ''
     };
   }
+
+  componentDidMount() {
+    const id = this.props.postId;
+    if (localStorage.getItem(id)) {
+      this.setState({
+        comments: JSON.parse(localStorage.getItem(this.props.postId))
+      });
+    } else {
+      this.setComments();
+    }
+  }
+
+  componentWillUnmount() {
+    this.setComments();
+  }
+
+  setComments = () => {
+    localStorage.setItem(
+      this.props.postId,
+      JSON.stringify(this.state.comments)
+    );
+  };
+
+  commentHandler = event => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleCommentSubmit = event => {
+    event.preventDefault();
+
+    const newComment = { text: this.state.comment, username: 'ciara' };
+    const comments = this.state.comments.slice();
+
+    comments.push(newComment);
+
+    this.setState({ comments, comment: '' });
+    setTimeout(() => {
+      this.setComments();
+    }, 500);
+  };
 
   render() {
     return (
       <div>
-        {this.state.comments.map((comment, index) => {
-          <Comment key={index} comment={comment} />
-        })}
-
-        <CommentInput />
+        {this.state.comments.map((comment, index) => <Comment key={index} comment={comment} />)}
+        <CommentInput
+          comment={this.state.comment}
+          submitComment={this.handleCommentSubmit}
+          changeComment={this.commentHandler}
+        />
       </div>
     );
   }
 }
 
 CommentSection.propTypes = {
-  comments: PropTypes.arrayOf (
-    PropTypes.shape({
-      text: PropTypes.string,
-      username: PropTypes.string
-    })
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({ text: PropTypes.string, username: PropTypes.string })
   )
 };
 
