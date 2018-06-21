d8e777732ac0eebcbdfe74a792d3c159d6a686c6
snowcoding <vimalshah77@gmail.com>
2018-06-21 13:45:29 -0700

    STYLED COMPS LOGIN
    - In Login.js
    
    - Converted all CSS to Styled Comps

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import { Container, Row, Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-import './Login.css'
+import styled from 'styled-components';
 import logo from '../SearchBar/instagram.png'
 import appleStore from './apple-store.png'
 import googleStore from './google-store.png'
 import msStore from './ms-store.png'
+
+const LoginContainer = styled(Container)`
+  padding-top:15px;
+`
+
+const LoginLogo = styled.img`
+  padding-left: 8px;
+`
+
+const LoginRow = styled(Row)`
+  display:flex;
+  justify-content:center;
+`
+
+const FormContainer = styled.div`
+  padding: 5%;
+  padding-top: 2.5%;
+  border: 1px solid lightgray;
+  background-color: white;
+`
+
+const LogoContainer = styled.div`
+  padding-top:15px;
+`
+
+const StyledInput = styled(Input)`
+  &::-webkit-input-placeholder{
+    font-size:12px;
+  }
+`
+const StyledButton = styled(Button)`
+  width:100%;
+`
+
+const GetAppDiv = styled.div`
+  padding: 20px 0;
+`
 
 class Login extends React.Component {
   constructor(props) {
@@ -33,34 +70,34 @@ class Login extends React.Component {
 
   render() { 
     return (
-      <Container className="container-fluid login-container">
-        <Row className="d-flex justify-content-center login-row">
-          <div className="form-container">
-            <div className="logo-container">
-              <img className="gram-logo login" src={logo} alt="logo"/>
+      <LoginContainer fluid>
+        <LoginRow>
+          <FormContainer>
+            <div>
+              <LoginLogo className="gram-logo" src={logo} alt="logo"/>
             </div>
             <Form>
               <FormGroup>
-                <Input className="text" type="text" name="text" id="exampleText" placeholder="Please enter your username" onChange={this.onUsernameChange}/>
+                <StyledInput type="text" name="text" id="exampleText" placeholder="Please enter your username" onChange={this.onUsernameChange}/>
               </FormGroup>
               <FormGroup>
-                <Input className="password" type="password" name="password" id="examplePassword" placeholder="Please enter your password" onChange={this.onPasswordChange}/>
+                <StyledInput type="password" name="password" id="examplePassword" placeholder="Please enter your password" onChange={this.onPasswordChange}/>
               </FormGroup>
-              <Button onClick={this.onLoginHandler} color="primary">Login</Button>
+              <StyledButton color="primary" onClick={this.onLoginHandler}>Login</StyledButton>
             </Form>
-          </div>
-        </Row>
-        <Row className="d-flex justify-content-center">
-          <div className="get-app">Get the App</div>
-        </Row>
-        <Row className="d-flex justify-content-center">
+          </FormContainer>
+        </LoginRow>
+        <LoginRow>
+          <GetAppDiv>Get the App</GetAppDiv>
+        </LoginRow>
+        <LoginRow>
           <div className="store">    
             <a href="https://itunes.apple.com/us/app/instagram/id389801252" target="_blank"><img src={appleStore} alt="apple-store"/></a>
             <a href="https://play.google.com/store/apps/details?id=com.instagram.android" target="_blank"><img src={googleStore} alt="google-store"/></a>
             <a href="https://www.microsoft.com/en-us/p/instagram/9nblggh5l9xt" target="_blank"><img src={msStore} alt="microsoft-store"/></a>
           </div>
-        </Row>
-      </Container>
+        </LoginRow>
+      </LoginContainer>
      )
   }
 }
