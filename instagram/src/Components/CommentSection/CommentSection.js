ccb1edcd45000925d0e0fb633b423522af96c47b
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-06-22 00:33:51 +0100

    refactored commentSection

--- instagram/src/Components/CommentSection/CommentSection.js
+++ instagram/src/Components/CommentSection/CommentSection.js
@@ -1,34 +1,38 @@
 import React, { Component } from 'react';
-import Comment from './Comment';
+import PropTypes from 'prop-types';
+import Comment from './Comment'
 import CommentInput from './CommentInput';
+
 
 class CommentSection extends Component {
     constructor(props){
         super(props)
-        this.state ={
+        this.state = {
             comments : props.comments,
-            comment: ""
+            comment : ''
         }
     }
 
-    componentDidMount() {
-
+    changeComment = event => {
+        this.setState({comment : event.target.value })
     }
-
-
-    changeComment = event => this.setState({comment : event.target.value})
-
     render() {
-
         return (
             <div>
-              {this.state.comments.map((comment,index) => <Comment  comment ={comment} key ={index}/>)}
+              {this.state.comments.map((comment , index) => <Comment  key={index} comment ={comment}/>)}
               <CommentInput
-              changeComment ={this.commentChange}
-               />
+                comment = {this.state.comment}
+                changeComment = {this.changeComment}
+                handleCommentSubmit ={this.submitComment}
+              />
+
             </div>
         );
     }
 }
+
+CommentSection.propTypes = {
+
+};
 
 export default CommentSection;
