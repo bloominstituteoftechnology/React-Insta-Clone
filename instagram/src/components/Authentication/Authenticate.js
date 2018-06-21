eaf0a86fba115ffbc320ac9e59602ac6b2ae2da6
Kevin <brackkevin@gmail.com>
2018-06-21 14:55:42 -0500

    TESTING COMMIT TO NEW BRANCH
    - No real changes just testing

--- instagram/src/components/Authentication/Authenticate.js
+++ instagram/src/components/Authentication/Authenticate.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import LoginPage from '../Login/Login';
+import React from "react";
+import LoginPage from "../Login/Login";
 
 const Authenticate = App =>
   class extends React.Component {
@@ -10,12 +10,13 @@ const Authenticate = App =>
       };
     }
     componentDidMount() {
-      if (!localStorage.getItem('user')) {
+      if (!localStorage.getItem("user")) {
         this.setState({ loggedIn: false });
       } else {
         this.setState({ loggedIn: true });
       }
     }
+
     render() {
       if (this.state.loggedIn) return <App />;
       return <LoginPage />;
