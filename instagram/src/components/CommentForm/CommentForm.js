a91215c3ca135b130d20629646f6a4e974fd2030
cmok4290 <cmok4290@gmail.com>
2018-06-21 16:52:37 -0400

    converted comment form with styled-components

--- instagram/src/components/CommentForm/CommentForm.js
+++ instagram/src/components/CommentForm/CommentForm.js
@@ -1,13 +1,42 @@
 import React from 'react';
-
-import './CommentForm.css';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+// import './CommentForm.css';
+
+// styled-components
+const CommentFormContainer = styled.div`
+    max-width: 100%;
+    margin: 30px auto 20px;
+    border-radius: 5px;
+
+    form {
+        display: flex;
+        justify-content: space-between;
+        margin: 0 20px;
+
+        input {
+            font-size: 18px;
+            border: 0;
+        }
+
+        button {
+            background: white;
+            border: 0;
+            font-size: 18px;
+
+            &:hover {
+                cursor: pointer;
+            }
+        }
+    }
+`;
 
 // Return CommentForm
 // Props: comment={this.state.comment} handleChange={this.handleCommentChange} handleSubmit={this.handleCommentSubmit}
 const CommentForm = props => {
     return (
-        <div className="comment-form-container">
+        <CommentFormContainer>
             <form onSubmit={props.handleSubmit}>
                 <input
                     type="text"
@@ -17,7 +46,7 @@ const CommentForm = props => {
                 />
                 <button type="submit"><span>...</span></button>
             </form>
-        </div>
+        </CommentFormContainer>
     );
 };
 
