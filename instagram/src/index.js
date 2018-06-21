42b8fc9abf97a75805fbf43fdc7e3268cc1a5ac2
snowcoding <vimalshah77@gmail.com>
2018-06-21 13:52:37 -0700

    AUTHENTICATION
    - IN index.js
    
    - Removed Authentication and placed it back to original output of CRA

--- instagram/src/index.js
+++ instagram/src/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Authenticate from './HOCs/Authenticate'
 
-import Login from './components/Login/Login'
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const AuthenticatedApp = Authenticate(App)
-
-//ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<AuthenticatedApp />, document.getElementById('root'));
-// ReactDOM.render(<Login />, document.getElementById('root'));
