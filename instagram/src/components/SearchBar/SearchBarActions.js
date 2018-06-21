334235a3788f4de797d989d46f105e69ca805304
Tyson Gushiken <tysonism@gmail.com>
2018-06-21 14:22:22 -0700

    Reduce search bar action links into styled-components

--- instagram/src/components/SearchBar/SearchBarActions.js
+++ instagram/src/components/SearchBar/SearchBarActions.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import SearchBarActionsExploreButton from './SearchBarActionsExploreButton';
-import SearchBarActionsActivityButton from './SearchBarActionsActivityButton';
-import SearchBarActionsProfileButton from './SearchBarActionsProfileButton';
+import styled, { css } from 'styled-components';
+import sprites from'../instagram-sprites.png';
+
+const SearchBarActionsAnchor = styled.a`
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+  margin: 0 0 0 24px;
+  color: rgba(0,0,0,0);
+  background: url(${sprites});
+  background-size: 495px 483px;
+
+  ${props => props.explore && css`
+    background-position: -176px -399px;
+  `}
+
+  ${props => props.activity && css`
+    background-position: -150px -425px;
+  `}
+
+  ${props => props.profile && css`
+    background-position: -376px -399px;
+  `}
+`;
 
 const SearchBarActions = (props) => {
   return(
     <div className="search-bar-actions">
-      <SearchBarActionsExploreButton />
-      <SearchBarActionsActivityButton />
-      <SearchBarActionsProfileButton />
+      <SearchBarActionsAnchor explore href="explore">Explore</SearchBarActionsAnchor>
+      <SearchBarActionsAnchor activity href="activity">Activity</SearchBarActionsAnchor>
+      <SearchBarActionsAnchor profile href="profile">Profile</SearchBarActionsAnchor>
     </div>
   );
 };
