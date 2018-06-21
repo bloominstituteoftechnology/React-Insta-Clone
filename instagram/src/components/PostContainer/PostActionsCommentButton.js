c7699bc29db178802377893f7a8af657ad05c72d
Tyson Gushiken <tysonism@gmail.com>
2018-06-21 14:55:29 -0700

    Remove unnecessary PostActionsButton components

--- instagram/src/components/PostContainer/PostActionsCommentButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const PostActionsCommentButton = (props) => {
-  return(
-    <a className="post-actions__comment-button" href="comment">Comment</a>
-  );
-};
-
-export default PostActionsCommentButton;
--- instagram/src/components/PostContainer/PostActionsLikeButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const PostActionsLikeButton = (props) => {
-  return(
-    <a className="post-actions__like-button" href="like" onClick={e => props.addLike(e, props.postIndex)}>Like</a>
-  );
-};
-
-export default PostActionsLikeButton;
