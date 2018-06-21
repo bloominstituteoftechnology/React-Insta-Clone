59c684c1ade7feb9fd751dd242c2d8d5d4efe0ed
Alec <afroenygam@gmail.com>
2018-06-21 00:07:41 -0400

    Like functionality added (bug in styling though)

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,38 +1,59 @@
 import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
+import '../instagram.css';
 import { Card, CardImg, CardBody,
-  CardTitle, Button } from 'reactstrap';
+  CardTitle } from 'reactstrap';
 
   /*Need to come back and add prop types*/
 
-const PostContainer = props => {
+class PostContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: this.props.post.likes,
+      liked: false
+    }
+  }
+
+  toggleLike = event => {
+    let likes = this.state.likes;
+    if (this.state.liked) {
+    this.setState({likes: likes -=1, liked: false})
+  } else {
+    this.setState({likes: likes +=1, liked: true})
+  }
+  }
+
+  render() {
+    console.log(this.state)
   return (
       <Card className="test">
         <CardBody className="card-header">
-        <img className="thumb-image" src={props.post.thumbnailUrl} alt="" />
-        <CardTitle className="username">{props.post.username}</CardTitle>
+        <img className="thumb-image" src={this.props.post.thumbnailUrl} alt="" />
+        <CardTitle className="username">{this.props.post.username}</CardTitle>
         </CardBody>
-        <CardImg className="post-image" src={props.post.imageUrl} alt="" />
+        <CardImg className="post-image" src={this.props.post.imageUrl} alt="" />
         <CardBody>
         <div className="post-buttons">
         <div className="left-buttons">
-        <a className="like-button mx-2" onClick={props.toggleLike}><i className="far fa-heart"></i></a>
+        <a className="like-button mx-2" onClick={this.toggleLike}><i className={"fa-heart " + (this.state.liked ? "fas":"far")}></i></a>
         {/*<i className="fas fa-heart like-selected"></i>*/}
         <i className="far fa-comment" ></i>
         </div>
-        <i className="far fa-bookmark" onClick={props.toggleLike}></i>
+        <i className="far fa-bookmark" onClick={this.props.toggleLike}></i>
         {/*<i class="fas fa-bookmark"></i>*/}
         </div>
-        <h5 onClick={props.toggleLike}>{props.post.likes} likes</h5>
-        <CommentSection comments={props.post.comments} />
-        <h6>{props.post.timestamp}</h6>
+        <h5>{this.state.likes} likes</h5>
+        <CommentSection comments={this.props.post.comments} />
+        <h6>{this.props.post.timestamp}</h6>
         <hr/>
-        <form onSubmit={props.addCommentHandler} value={props.index}>
-        <input style={{width: 500 +'px'}} type="text" name="comment" placeholder="Add a comment..." value={props.value} onChange={props.commentChangeHandler} />
+        <form onSubmit={this.props.addCommentHandler} value={this.props.index}>
+        <input style={{width: 500 +'px'}} type="text" name="comment" placeholder="Add a comment..." value={this.props.value} onChange={this.props.commentChangeHandler} />
         </form>
         </CardBody>
       </Card>
   );
+}
 };
 
 export default PostContainer;
