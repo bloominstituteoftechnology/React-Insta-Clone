22562582267cfa9d087e5d3ef3f14c4d4388704c
Vu Cao <cdhvu@yahoo.com>
2018-06-21 15:30:52 -0700

    Styled PostContainer

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -4,29 +4,48 @@ import {
     Card, CardImg, CardBody
 } from 'reactstrap';
 import CommentSection from '../CommentSection/Comment';
+import styled from 'styled-components';
+
+const StyledCard = styled(Card)`
+    width: 604px;
+    margin: 30px auto;
+`
+
+const StyledImg = styled.img`
+    width: 30px;
+    height: 30px;
+    // margin-left: 16px;
+    // margin-right: 11px;
+    border-radius: 50%;
+    margin: 10px 11px 10px 16px;
+`
+
+const Like = styled.p`
+    font-size: 14px;
+    font-weight: bold;
+    margin-right: 10px;
+`
 
 const PostContainer = props => {
     // console.log(props.userData.comments)
     return (
         <div className='post-container container-fluid'>
-            <Card className='card'>
-                <div className="card-header">
-                    <img className="thumbnail" src={props.userData.thumbnailUrl} alt="thumbnail" />
+            <StyledCard className='card'>
+                <div>
+                    <StyledImg src={props.userData.thumbnailUrl} alt="thumbnail" />
                     <span className="bold-text">{props.userData.username}</span>
                 </div>
                 <CardImg top width="100%" src={props.userData.imageUrl} alt="Card image cap" />
                 <CardBody>
-                    <div>
-                        <i className="far fa-heart fa-lg mr-3"></i>
-                        <i className="far fa-comment fa-lg fa-flip-horizontal"></i>
-                    </div>
-                    <p className='bold-text mt-2 mb-2'>{props.userData.likes + ' likes'}</p>
+                    <i className="far fa-heart fa-lg mr-3"></i>
+                    <i className="far fa-comment fa-lg fa-flip-horizontal"></i>
+                    <Like className='mt-2 mb-2'>{props.userData.likes + ' likes'}</Like>
                     <CommentSection
-                        comments={props.userData.comments} 
+                        comments={props.userData.comments}
                         postId={props.userData.imageUrl}
-                        username={props.username}/>
+                        username={props.username} />
                 </CardBody>
-            </Card>
+            </StyledCard>
         </div>
     )
 }
--- instagram/src/components/SearchBar/Search.js
+++ instagram/src/components/SearchBar/Search.js
@@ -16,23 +16,28 @@ const StyledInput = styled.input`
     font-size: 14px;
 `
 
+const StyledRow = styled(Row)`
+    height: 100%;
+    background: white;
+`
+
 const SearchBar = props => {
     return (
         <Container>
-            <Row className="SearchBar">
+            <StyledRow>
                 <Col xs="5" className="d-flex align-items-center">
                     <StyledLink href=".SearchBar"><i className="fab fa-instagram fa-2x col-3"></i></StyledLink>
                     <StyledLink href=".SearchBar"><StyledImg src={Logo} alt="Instagram Logo" /></StyledLink>
                 </Col>
                 <Col xs="5" className="d-flex align-items-center">
-                    <StyledInput type="text" placeholder="&#xf002; Search" className="fa-search" onChange={(e)=> props.onChange(e)} value={props.value}/>
+                    <StyledInput type="text" placeholder="&#xf002; Search" onChange={(e)=> props.onChange(e)} value={props.value}/>
                 </Col>
                 <Col xs="2" className="d-flex justify-content-between align-items-center">
                     <StyledLink href=".SearchBar"><i className="far fa-compass fa-lg"></i></StyledLink>
                     <StyledLink href=".SearchBar"><i className="far fa-heart fa-lg"></i></StyledLink>
                     <StyledLink href=".SearchBar"><i className="far fa-user fa-lg"></i></StyledLink>
                 </Col>
-            </Row>
+            </StyledRow>
         </Container>
     )
 }
