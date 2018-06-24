9c2668e03879f6467f5b1947970599faae711b4a
Skid <Simon_Freelance@hotmail.com>
2018-06-24 17:32:16 -0400

    Added packages that were missing and replaced bootstrap with bulma

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,53 +1,64 @@
-import React from 'react';
-import './Login.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'reactstrap';
+import React from "react";
+import "./Login.css";
+import { Button } from "react-bulma-components/full";
 class Login extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            username: '',
-            password: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
 
-    changeText = e => {
-        this.setState({ [e.target.name]: e.target.value });
-    }
+  changeText = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    startLogin = () => {
-        localStorage.setItem('username', this.state.username);
-        localStorage.setItem('password', this.state.password);
-        window.location.reload(false);
-    }
-    render() {
-        return (
-            <div className='login-container'>
-                <form className='login__form' onSubmit={e => {
-                    e.preventDefault();
-                }}>
-                    {/* Username */}
-                    <input type="text"
-                        className='login__helpers login__username'
-                        name='username'
-                        placeholder='username'
-                        onChange={e => {
-                            this.changeText(e);
-                        }} />
-                    {/* Password */}
-                    <input type="text"
-                        className='login__helpers login__password'
-                        name='password'
-                        placeholder='password'
-                        onChange={e => {
-                            this.changeText(e);
-                        }} />
-                    <Button className='login__helpers login__submit' color = 'primary' onClick={this.startLogin}>Login</Button>
-                </form>
-            </div>
-        );
-    }
-
+  startLogin = () => {
+    localStorage.setItem("username", this.state.username);
+    localStorage.setItem("password", this.state.password);
+    window.location.reload(false);
+  };
+  render() {
+    return (
+      <div className="login-container">
+        <form
+          className="login__form"
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          {/* Username */}
+          <input
+            type="text"
+            className="login__helpers login__username"
+            name="username"
+            placeholder="username"
+            onChange={e => {
+              this.changeText(e);
+            }}
+          />
+          {/* Password */}
+          <input
+            type="text"
+            className="login__helpers login__password"
+            name="password"
+            placeholder="password"
+            onChange={e => {
+              this.changeText(e);
+            }}
+          />
+          <Button
+            className="login__helpers login__submit"
+            color="primary"
+            onClick={this.startLogin}
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
