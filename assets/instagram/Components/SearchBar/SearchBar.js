5c3686ff9772960dc6e2304540be036185a9e906
Your Name <you@example.com>
2018-06-25 15:21:01 -0700

    i tried applying the class name as search bar for now

---
+++ assets/instagram/Components/SearchBar/SearchBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./searchbar.css";
+
+fucntion App() {
+  return (
+    <div className="SearchBar">
+      <h1>Instagram</h1>
+      <h2>Instagram Search Bar</h2>
+    </div>
+  );
+}
+
+<head>
+
+
+
+
