056dc32952f9cbfcf44e9793ca011b2a18df1af4
Katie Gorbell <katiegorbell@gmail.com>
2018-06-25 15:39:12 -0500

    building a header structure, cant get images to load.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import SearchBar from './Components/SearchBar/SearchBar'
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Instagram</h1>
-        </header>
+      <div className="search-bar-container">
+        <div className='header-logo'>
+          <img src='imgs/instagram-camera.png' className='logo-camera' alt='insta-camera'>
+          <img src='imgs/instagram-logo.svg.png' className='logo-name' alt='header-logo'>
+        </div>
+        <SearchBar />
+        
       </div>
     );
   }
---
+++ instagram/src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+        <form className='search-bar-input'>
+        
+        </form>
+    );
+}
+ 
+export default SearchBar;
