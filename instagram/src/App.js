51723156aad7a9c77d1037b85434fc59e12744ec
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-06-25 14:00:17 -0600

    Added directories and files

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: Data
+    };
+  }
   render() {
     return (
       <div className="App">
