c33937470cf5e0760a89562554da740daa4c5d45
Adrian Adames <adrianadames@gmail.com>
2018-06-25 17:37:31 -0400

    continuing to set up file structure

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import dummyData from '../../dummy-data';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">IG APP CLONE</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        
       </div>
     );
   }
