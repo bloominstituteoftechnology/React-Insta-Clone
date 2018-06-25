19b94794229cbb7e253525b6857eaf00d1b5ed75
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-06-25 13:02:23 -0600

    deleted logo and related files

---
+++ instagram/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Instagram</h1>
+        </header>
+        <p className="App-intro">
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
