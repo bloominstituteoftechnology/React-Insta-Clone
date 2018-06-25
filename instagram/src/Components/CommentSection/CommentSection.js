beb6d1205c8f8f7b3af8a147989fd1e1bbd796d3
Katie Gorbell <katiegorbell@gmail.com>
2018-06-25 17:30:37 -0500

    built structure for post container

---
+++ instagram/src/Components/CommentSection/CommentSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommentSection = () => {
+    return (
+        <div className='comments-section'>
+            <span> ~userID goes here ~  ~ Text goes here ~</span>
+        </div>
+    );
+}
+ 
+export default CommentSection;
--- instagram/src/Components/PostContainer/PostContainer.js
+++ instagram/src/Components/PostContainer/PostContainer.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import './PostContainer.css'
+import CommentSection from '../CommentSection/CommentSection'
 
 const PostContainer = () => {
     return (
         <div className='post-container'>
+            <div className='post-header'>
+                <img className='user-thumbnail' src='' alt='User thumbnail image'/>
+                <span> ~ userID goes here ~ </span>
+            </div>
+            <div className='post-image'>
+                ~ Post Image goes here ~
+            </div>
+            <div className='post-content'>
+                <div className='icon-bar'>
+                    <i className='far fa-heart'></i>
+                    <i className='far fa-comment'></i>
+                </div>
+                <div className='likes-count'>
+                    ~ Likes count goes here ~
+                </div>
+                <CommentSection />
+            </div>
         </div>
     );
 }
