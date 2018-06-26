645fe928d07b95adec8fa7d2d2d514f94bd20b11
Daniel Chamorro <dan@dcitllc.com>
2018-06-26 12:29:29 -0400

    dChamorro added comment input

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -5,19 +5,39 @@ class CommentSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      comments: this.props.comments
+      comments: this.props.comments,
+      comment: ""
     };
   }
+
+  commentHandler = event => {
+    this.setState({ comment: event.target.value });
+  };
+
+  addComment = () => {
+    const comments = this.state.comments.slice();
+    comments.push(this.state.comment);
+    this.setState({ comments: comments, comment: "" });
+  };
 
   render() {
     const { likes } = this.props;
     const { comments } = this.state;
     return (
       <div>
+        <i className="fa fa-heart" />
+        <i className="fa fa-comment" />
         <span>{likes} likes</span>
         {comments.map((comment, i) => (
           <Comment username={comment.username} text={comment.text} key={i} />
         ))}
+        <input
+          type="text"
+          placeholder="Add a Comment"
+          onChange={this.commentHandler}
+          value={this.state.comment}
+        />
+        <button onClick={this.addComment}>...</button>
       </div>
     );
   }
