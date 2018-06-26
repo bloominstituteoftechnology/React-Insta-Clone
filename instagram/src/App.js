3b16b25cf028130c048ad32a97c437cf8bbaefea
cubsfan46350 <cubsfan.46350@msn.com>
2018-06-26 16:40:59 -0500

    imported dummy data

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import dummyData from './dummy-data';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: dummyData
+    };
+  }
   render() {
     return (
      <div className="App">App</div> 
