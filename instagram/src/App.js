a4fa74b2fb19889fda328edd046ec838297950ec
Sania Parekh <sparekh@Sanias-MacBook.home>
2018-06-26 18:13:40 -0500

    redid components files, were not working correctly for some reason

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      posts: dummyData
+    };
+  }
+  
+  
+  
+  
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
