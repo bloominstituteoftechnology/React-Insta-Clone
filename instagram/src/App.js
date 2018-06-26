6cde43286e1705118af06496a0f12b78cb658e2a
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-06-25 19:39:10 -0700

    tried to pass comments as an instance of props from post container

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import dummyData from './dummy-data';
 import './App.css';
+import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: dummyData
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.data.map((post) => <PostContainer key={post.username} post={post} />)}
       </div>
     );
   }
 }
 
 export default App;
+//mapping state before we pass it
---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PostContainer = (props) => {
+  return (
+    <div>
+      <div>{props.post.username}</div>
+      {/* <div>{props.post.thumbnailUrl}</div>
+      <div>{props.post.imageUrl}</div>
+      <div>{props.post.likes}</div>
+      <div>{props.post.timestamp}</div> */}
+      {/* <div>{props.post.comments}</div> */}
+      {props.post.map((comment) => <PostContainer key={props.post.username} comment={comment} />)}
+    </div>
+  );
+};
+
+export default PostContainer;
+
+PostContainer.propTypes = {
+  post: PropTypes.shape({
+    username: PropTypes.string,
+    thumbnailUrl: PropTypes.sting,
+    imageUrl: PropTypes.string,
+    likes: PropTypes.number,
+    timestamp: PropTypes.string,
+    comments: PropTypes.array
+  })
+};
