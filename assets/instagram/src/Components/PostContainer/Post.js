b5678d9a95f36214ea73ace7d950f4318ef13bc2
Your Name <you@example.com>
2018-06-27 14:29:39 -0700

    fixed " to '

--- assets/instagram/src/Components/PostContainer/Post.js
+++ assets/instagram/src/Components/PostContainer/Post.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
-import PostHeader from "./PostHeader";
+import PostHeader from "../PostHeader";
 
 import "./Posts.css";
 
