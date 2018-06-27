0abde985e175306fd3357e5fa7dbbf272f396b28
Daniel Chamorro <dan@dcitllc.com>
2018-06-27 10:46:51 -0400

    dChamorro - Completed w/o Styled Componenets Moving on to Sprint will circle back

--- instagram/src/components/CommentSection/Comment.js
+++ instagram/src/components/CommentSection/Comment.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./CommentSection.css";
+import PropTypes from "prop-types";
 
 const Comment = ({ username, text }) => {
   // Deconstructing
@@ -10,6 +11,13 @@ const Comment = ({ username, text }) => {
       <span className="comments-text">{text}</span>
     </div>
   );
+};
+
+Comment.propTypes = {
+  comment: PropTypes.shape({
+    text: PropTypes.string,
+    username: PropTypes.string
+  })
 };
 
 export default Comment;
