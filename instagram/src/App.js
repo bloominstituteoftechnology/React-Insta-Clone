2c400e004f84fc9fca3863548b04339f75579c38
unknown <may.alex43@gmail.com>
2018-06-27 18:44:46 -0400

    working on adding comment functionality

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -8,13 +8,22 @@ import PostContainer from './components/PostsContainer/PostsContainer';
 
 class App extends Component {
   constructor(){
+    // will be called first
     super();
     this.state={
       posts: dummyData
     };
   }//constructor
 
+  componetDidMount(){
+    //will be called third
+    // window.localStorage.setItem("comments", JSON.stringify(dummyData));
+    this.setState({ posts:dummyData }); 
+  }
+
   render() {
+    // will be called second
+    // will be called after setState
     return (
       <div className="App">
         <SearchBar />
