2ee3bf00edc2e21575cd5ec9541e86b4a53ac6da
JacobLayton <jlayjlay3@gmail.com>
2018-06-27 15:59:00 -0700

    not sure what changes are in this commit, from last night

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -8,19 +8,37 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      posts: []
+      posts: [],
+      filtered: [],
     };
   }
 
   componentDidMount() {
     this.setState({posts: dummyData});
   }
+
+  filterPosts = e => {
+    const filteredPosts = this.state.posts.filter(fp => {
+      if (fp.username.includes(e.target.value)) {
+        return fp;
+      }
+    });
+    this.setState({ filtered: filteredPosts});
+  };
+
+  displayedPosts() {
+    if (this.state.filtered.length > 0) {
+      return this.state.filtered;
+    } else return this.state.posts;
+  }
+  
   
   render() {
+    console.log(this.state.filtered);
     return (
       <div className="App">
-        <SearchBar />
-        <PostContainer posts={this.state.posts} />
+        <SearchBar filterPosts={this.filterPosts}/>
+        <PostContainer posts={ this.displayedPosts() }/>
       </div>
     );
   }
--- instagram/src/components/SearchBar/SearchBar.jsx
+++ instagram/src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './SearchBar.css';
 
-const SearchBar = () => (
+const SearchBar = props => (
     <div className="search-bar">
         <div className="logo">
             <i className="fab fa-instagram"></i>
             <img src="https://3rockmarketing.com/wp-content/uploads/2016/09/instagram-logo.png" alt=""/>
         </div>
         <div className="search-input">
-            <input type="text" placeholder="search"></input>
+            <form>
+                <input 
+                    type="text" 
+                    placeholder="search"
+                    onChange={props.filterPosts}
+                />
+            </form>
         </div>
         <div className="navigation-logos">
             <i className="far fa-compass"></i>
