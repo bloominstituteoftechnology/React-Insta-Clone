52230b5315756a26c7680d798ce86f686c25dc6b
JacobLayton <jlayjlay3@gmail.com>
2018-06-28 11:00:03 -0700

    Not sure sure how to save to local storage

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import PostsPage from './components/PostsContainer/PostsPage';
+import Authenticate from './components/Authenticate/Authenticate';
 
 class App extends Component {
   constructor() {
@@ -18,4 +19,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Authenticate(App);
