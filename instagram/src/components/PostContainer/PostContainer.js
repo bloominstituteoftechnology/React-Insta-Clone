94210a30fc93b773956814d6e03f767286322ca3
Adrian Adames <adrianadames@gmail.com>
2018-06-28 22:35:38 -0400

    Added space (i.e. margin) between the individual posts.

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,28 +1,41 @@
 import React from 'react';
-import PostHeader from './PostHeader'
-import PostPicture from './PostPicture'
-import CommentSection from '../CommentSection/CommentSection'
-import './postContainer.css'
+import PostHeader from './PostHeader';
+import PostPicture from './PostPicture';
+import CommentSection from '../CommentSection/CommentSection';
+import './postContainer.css';
+import styled from 'styled-components';
+
+
+const IndividualPostContainerStyles = styled.div`
+    margin-bottom:55px;
+`
+const PostContainerStyles = styled.div`
+    background-color: #fafafa;
+`
 
 const PostContainer = props => {
     return (
+        <PostContainerStyles>
         <div>
             {props.postData.map(post => (
-                <div>
-                    <PostHeader 
-                        username = {post.username}
-                        thumbnailURL = {post.thumbnailUrl}
-                    />
-                    <PostPicture 
-                        imageUrl = {post.imageUrl} 
-                    />
-                    <CommentSection 
-                        comments = {post.comments}
-                        numberOfLikes= {post.likes}
-                    />
-                </div>
+                <IndividualPostContainerStyles>
+                    <div>
+                        <PostHeader 
+                            username = {post.username}
+                            thumbnailURL = {post.thumbnailUrl}
+                        />
+                        <PostPicture 
+                            imageUrl = {post.imageUrl} 
+                        />
+                        <CommentSection 
+                            comments = {post.comments}
+                            numberOfLikes= {post.likes}
+                        />
+                    </div>
+                </IndividualPostContainerStyles>
             ))}
         </div>
+        </PostContainerStyles>
     );
 }
 
