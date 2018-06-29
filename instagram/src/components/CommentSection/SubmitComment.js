08e466acff7d37fb728bc8e272c49db3b3f7b4a5
Adrian Adames <adrianadames@gmail.com>
2018-06-28 22:22:08 -0400

    Finished styling <SubmitComment /> component.

--- instagram/src/components/CommentSection/SubmitComment.js
+++ instagram/src/components/CommentSection/SubmitComment.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SubmitCommentContainerStyles = styled.div`
+    display:flex;
+    justify-content:space-between;
+    border-top:0.5px solid lightgray;
+    margin-left:9px;
+    margin-right:9px;
+    padding-top:12px;
+    padding-bottom:10px; 
+`
+const SubmitCommentInputStyles = styled.input`
+    border: 0px solid;
+    margin-left:13px;
+    font-size:15px;
+    font-weight:bold;
+    color:lightgray;
+`
+const InterpunctStyles = styled.div`
+    font-weight:bold;
+    font-size:20px;
+`
 
 const SubmitComment = props => {
     return (
+        <SubmitCommentContainerStyles>
         <div>
             <form onSubmit = {props.addNewComment} >
-                <input 
+                <SubmitCommentInputStyles 
                     type = "text" 
                     placeholder = "Add a comment..."
                     onChange = {props.onChangeSignal}
@@ -12,6 +35,14 @@ const SubmitComment = props => {
                 />
             </form>
         </div>
+        <div>
+            <InterpunctStyles>
+                <div>
+                    &#183;&#183;&#183;
+                </div>
+            </InterpunctStyles>
+        </div>
+        </SubmitCommentContainerStyles>
     );
 }
 
