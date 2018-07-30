f532623ee9b835ebe7bc0794dcfb2aa6821389ef
Trevor <trevorfehrman@gmail.com>
2018-07-30 15:46:02 -0700

    Styling sort of done

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from "./Components/Header";
+import Card from "./Components/Card";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <section className="container App__cards-container">
+        <Card />
+        </section>
       </div>
     );
   }
