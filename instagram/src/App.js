e9edcc99d3d6f8c1340633fd371736fb06ccb1a8
Ashwin <aswinjeganath@gmail.com>
2018-07-31 03:00:57 +0530

    Add and style header

---
+++ instagram/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Components/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ instagram/src/Components/Header.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+  state = {
+    show: true
+  };
+
+  inputRef = React.createRef();
+
+  handleClick = () => {
+    this.setState({ show: false }, () => this.inputRef.current.focus());
+  };
+
+  handleFocusOut = () => this.setState({ show: true });
+
+  render() {
+    return (
+      <header className="Header">
+        <div className="container">
+          <div className="Header__nav">
+            <a href="#" className="Header__nav-brand">
+              <i className="fab fa-instagram Header__brand" />
+              Instagram
+            </a>
+            <div className="Header__search-bar">
+              <input
+                ref={this.inputRef}
+                onBlur={this.handleFocusOut}
+                type="text"
+                className="Header__search-input"
+                placeholder="Search"
+              />
+              {this.state.show ? (
+                <div
+                  onClick={this.handleClick}
+                  className="Header__input-overlay"
+                >
+                  <i className="fas fa-search" />
+                  Search
+                </div>
+              ) : null}
+            </div>
+            <div className="Header__three-buttons">
+              <i className="far fa-compass" />
+              <i className="far fa-heart" />
+              <i className="far fa-user" />
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
