e3eb62fd3529fa76676d88eed171ff5083bfc33a
francistse23 <wtse.ucsd@gmail.com>
2018-07-30 15:58:07 -0700

    Minor CSS styling adjustments

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -24,9 +24,6 @@ class App extends Component {
     super();
     this.state = {data:dummyData};
   }
-  addComment(e){
-
-  }
   render() {
     return (
       <div className="container">
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -4,7 +4,7 @@ import './SearchBar.css';
 const Search = props => {
     return (
         <div className='search-container'>
-            <div><i class="fa fa-instagram search-icons" aria-hidden="true"> | Instagram</i></div>
+            <div className='title'><i class="fa fa-instagram search-icons" aria-hidden="true"> |</i> Instagram</div>
             <div className='search'><span class="fa fa-search"></span><input placeholder='Search'></input></div>
             <div className='search-icons'>
                 <i className="fa fa-compass search-icons" aria-hidden="true"></i>
