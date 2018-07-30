50236665f10750ef1004b235e8a23fad380f6f4f
LaikaFusion <newrad4@gmail.com>
2018-07-30 17:08:00 -0400

    Completed top bar

--- instakg/src/SearchBar/SearchBar.js
+++ instakg/src/SearchBar/SearchBar.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import logo from './logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompass, faHeart, faUser } from '@fortawesome/free-solid-svg-icons';
 import './SearchBar.css';
+
 
  const SearchBar = (props) =>{
    return(
-    <div> Test </div>     
+    <div className="borderBox">
+        <div className="searchBarContainer"> 
+        <div className="logo"><img src={logo}  alt="logo" /> </div>
+        <span className="splitBar"> | </span> 
+        <span className="logoText"> InstaKG  </span> 
+        <input id='search' placeholder="   &#128269; Search" type="text"/>
+        <FontAwesomeIcon className="icon" icon={faCompass} />
+        <FontAwesomeIcon className="icon" icon={faHeart} />
+        <FontAwesomeIcon className="icon" icon={faUser} />
+
+        </div>  
+        </div>   
   )
 }
 
