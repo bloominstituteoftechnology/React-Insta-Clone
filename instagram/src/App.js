16d373fb35d1cc56ef21d1e70b788c0439871aad
codejoncode <jonathanjholloway@gmail.com>
2018-07-30 15:56:00 -0500

    on the page is a search bar a post container and a comment section data properties still have to be added but have been passed down via props

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import dummyData from './dummy-data'; 
 import SearchBar from './components/SearchBar/SearchBar.js'; 
 import PostContainer from './components/PostContainer/PostContainer.js'; 
+import CommentSection from './components/CommentSection/CommentSection.js'; 
 
 
 class App extends Component {
@@ -15,10 +16,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-        <SearchBar  />
-        <PostContainer />
-        </header>
         
+        
+        </header>
+        {dummyData.map((i,post) => <div className = "post"><SearchBar  key={i}/> <PostContainer key = {i} userName = {post.username} thumbNail={post.thumbnailUrl} image={post.imageUrl}
+        likes = {post.likes} time ={post.timestamp}/> <CommentSection comments = {post.comments}/> </div>)}
       </div>
     );
   }
--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,3 +1,24 @@
 import React from 'react';
 import './CommentSection.css'; 
 import PropTypes from 'prop-types'; 
+
+
+class CommentSection extends React.Component {
+    constructor(props){
+        super(); 
+        this.state = {}; 
+    }
+
+    render(){
+
+
+        return(
+            <div>
+            <h3>comment</h3>
+            <p></p>
+            </div>
+        );
+    }
+}
+
+export default CommentSection; 
