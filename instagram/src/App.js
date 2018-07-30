cf67084469c99b8a3b57bd3b11898b5abd28a9bd
codejoncode <jonathanjholloway@gmail.com>
2018-07-30 14:51:33 -0500

    starting to get the header put together

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data'; 
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <div>
+        <i class="fab fa-instagram">   &#124;</i> <span>Instagram</span> <input placeholder="search"/> <i class="far fa-heart"></i> <i class="far fa-user"></i>
+        </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
