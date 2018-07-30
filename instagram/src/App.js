9097ec113d0b0b034b4f0fe7f5d528b459bb7726
sliw <slioweng@gmail.com>
2018-07-31 05:14:15 +0800

    added proptypes

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
 import dummyData from './dummy-data';
+import Moment from 'react-moment';
 
 const commentArray = dummyData.map(postObject => {
   return postObject['comments'];
@@ -32,14 +34,15 @@ class PostContainer extends Component {
       <div>
         {this.state.data.map((postObject, index) => {
           const {username, thumbnailUrl, imageUrl, likes, timestamp} = postObject;
+          console.log(timestamp)
           return (
             <div className="post-container">
               <User username={username} thumbnailUrl={thumbnailUrl} />
               <img src={ imageUrl } alt={ username } />
               <ActionButton />
-              <p>{ likes }</p>
+              <p className="likes">{ likes } likes</p>
               <CommentSection comment={this.state.comment} index={index} />
-              <p>{ timestamp }</p>
+              <Moment parse="MMM D YYYY" className="timestamp" fromNow AGO>{timestamp}</Moment>
               <CommentInput />
             </div>
           )
@@ -51,6 +54,7 @@ class PostContainer extends Component {
 
 const User = props => {
   const {username, thumbnailUrl} = props;
+
   return (
     <div className="user">
       <img src={ thumbnailUrl } alt={ username } />
@@ -59,29 +63,42 @@ const User = props => {
   )
 }
 
+User.propTypes = {
+  username: PropTypes.string.isRequired,
+  thumbnailUrl: PropTypes.string.isRequired
+}
+
 const ActionButton = () => {
   return (
     <div className="action-buttons">
-      <span>heart icon</span>
-      <span>comment icon</span>
+      <span className="icon"><i className="far fa-heart"></i></span>
+      <span className="icon"><i className="far fa-comment"></i></span>
     </div>
   )
 }
 
-
 const CommentSection = props => {
+  const {comment, index} = props;
+
   return (
     <div className="comment-section">
-       { props.comment[props.index].map(commentObject => {
+       { comment[index].map(commentObject => {
+         const {username, text} = commentObject;
+
          return (
           <div className="comment">
-              <p>{ commentObject['username'] }</p>
-              <p>{ commentObject['text'] }</p>
+              <p>{ username }</p>
+              <p>{ text }</p>
           </div>
         )
        }) }
     </div>
   );
+}
+
+CommentSection.propTypes = {
+  comment: PropTypes.array.isRequired,
+  index: PropTypes.number.isRequired
 }
 
 const CommentInput = () => {
