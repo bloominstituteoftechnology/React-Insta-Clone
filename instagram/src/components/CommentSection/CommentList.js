ddb20b64f11e25286cadfde8cda490a75bc839f5
Erik Kimsey <erik@erikkimsey.com>
2018-07-30 18:45:32 -0400

    Built out comments and interpolated data

--- instagram/src/components/CommentSection/CommentList.js
+++ instagram/src/components/CommentSection/CommentList.js
@@ -3,12 +3,20 @@ import PropTypes from 'prop-types';
 import Comment from './Comment.js';
 
 const CommentList = (props) => {
-
+    
     return (
         <div className="comment-list-container">
-            <Comment />
+        {
+            props.comments.map((comment)=>{
+                    return <Comment key={comment.username} comment={comment}/>
+            })
+        }
         </div>
     )
+}
+
+CommentList.propTypes = {
+    comment: PropTypes.array, 
 }
 
 export default CommentList;
