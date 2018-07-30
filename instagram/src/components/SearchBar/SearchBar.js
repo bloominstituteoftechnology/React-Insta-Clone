03b08f4e76974e2c30bfa70651a12407c832b596
szincone <sawyerzincone@gmail.com>
2018-07-30 16:13:13 -0600

    added instagrame logo text to searchbar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './SearchBar.css';
-
+import logoText from './instagramLogoText.png';
 
 const SearchBar = (props) => {
   return (  
     <div className="searchBarMainDiv">
       {/* start logo div */}
       <div className="searchBarLogoDiv">
-        <img src=""/>|<i class="fa fa-instagram"></i>
+        <i class="fa fa-instagram"></i>|<img src={logoText} alt="instagram text" />
       </div>
 
       {/* start input div */}
