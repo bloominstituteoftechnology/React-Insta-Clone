326deb0a958ef77605d64e4c786f9c5499f5f559
Sean <sean47900@gmail.com>
2018-07-30 14:37:59 -0500

    created component files, made imports

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
+import SearchBar from './SearchBar'
+import Comments from './Comments'
+import PostContainer from './PostContainer'
+
 
 class App extends Component {
   render() {
