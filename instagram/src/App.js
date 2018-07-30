4e84ff789c385faf4825f70c04fc87abdfdbea2b
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-30 16:06:31 -0500

    got data set in state

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: dummyData
+    }
+  }
   render() {
     return (
       <div className="App">
