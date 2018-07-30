3a9bf00f1e0d8faa63b413149f0c67ee020cd233
Charlie Winslow <cwinslow22@yahoo.com>
2018-07-30 15:23:08 -0500

    moved the dummy-data.js into src folder and imported into App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data.js';
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        posts: dummyData
+    }
+  }
   render() {
     return (
       <div className="App">
