89b17387cbb7608584e63b00f380f74d1d796c4b
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-07-30 14:28:38 -0700

    Started setting up the JS functionality.

---
+++ instagram/components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SearchBar.css';
+
+const SearchBar = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default SearchBar;
--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: {dummyData}
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Instagram</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
