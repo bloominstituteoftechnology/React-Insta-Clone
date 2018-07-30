05c0c8a140f488c882a6b09ac8c63c997b4012c8
szincone <sawyerzincone@gmail.com>
2018-07-30 15:10:15 -0600

    installed font awesome, finished search bar input and logo divs

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SearchBar = (props) => {
+  return (  
+    <div className="searchBarMainDiv">
+      {/* start logo div */}
+      <div className="searchBarLogoDiv">
+        <img src=""/>|<img src=""/>
+      </div>
+
+      {/* start input div */}
+
+
+
+
+    </div>
+  );
+}
+ 
+export default SearchBar;
