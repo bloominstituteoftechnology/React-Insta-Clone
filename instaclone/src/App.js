75667dbf1c70a48e7774daec690955c1da20b2f6
Jon Simmons <jonsimo@aol.com>
2018-07-30 18:03:02 -0400

    directories created and imported dummydata

--- instaclone/src/App.js
+++ instaclone/src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data.js'
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data:dummyData
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -10,8 +18,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="app-list">
+          {this.state.data.map()}
         </p>
       </div>
     );
