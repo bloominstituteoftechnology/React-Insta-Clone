7e76ec4b4b1703962271009f2e42dd1e9ac1ee09
szincone <sawyerzincone@gmail.com>
2018-07-30 15:46:55 -0600

    finally imported fa...

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './SearchBar.css';
+
 
 const SearchBar = (props) => {
   return (  
     <div className="searchBarMainDiv">
       {/* start logo div */}
       <div className="searchBarLogoDiv">
-        <img src=""/>|<img src=""/>
+        <img src=""/>|<i class="fa fa-instagram"></i>
       </div>
 
       {/* start input div */}
+      <div className="searchBarInputDiv">
+        <input placeholder="Search" />
+      </div>
 
+      {/* start links div */}
+      <div className="searchBarLinksDiv">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
 
 
 
