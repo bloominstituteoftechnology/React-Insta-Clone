5bb79ef31116a48df288dcc7feebe2d80845f88d
AI2EJAY <JHammett1314@gmail.com>
2018-07-30 14:51:07 -0500

    Made folder structure look like the one in assets

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Instagram</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          We post pictures and then comment on the pictures and stuffs
         </p>
       </div>
     );
