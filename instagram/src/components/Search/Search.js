8730e59332c0143c95293491acd2d7ba1a11646b
Erik Kimsey <erik@erikkimsey.com>
2018-07-30 16:07:49 -0400

    Moved components I've already created to this repo

---
+++ instagram/src/components/Search/Search.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Search = (props) => {
+
+    return (
+        <div className="search-container">
+            <div className="logo">
+                logo
+            </div>
+            <div className="input-container">
+                <input />
+            </div>
+            <div className="options-container">
+                <div className="options-item"></div>
+                <div className="options-item"></div>
+                <div className="options-item"></div>
+            </div>
+        </div>
+    )
+}
