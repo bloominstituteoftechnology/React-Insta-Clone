8ec90effedbabe89d6b75e9e2655de5b7b942d9d
dasgma <dasgrigoma@gmail.com>
2018-07-30 12:54:57 -0700

    Fresh react App setup.

---
+++ instagram/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Fresh start
+      </div>
+    );
+  }
+}
+
+export default App;
