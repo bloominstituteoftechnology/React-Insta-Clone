ff9d6e29382a9062fd39f054f0597d926c447432
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-07-31 15:17:13 -0500

    Class Crashes Project
    The problem happened when I implemented the class from the solution into my code, not on my Comment Section file there are errors in it. specifically line 11 cannot read comments

--- insta-app/src/App.js
+++ insta-app/src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './components/Components.css';
-import dummyData from './dummy-data';
-import SearchBar from './components/SearchBar/SearchBar'; 
-import PostContainer from './components/PostContainer/PostContainer'; 
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./components/Components.css";
+import dummyData from "./dummy-data";
+import SearchBar from "./components/SearchBar/SearchBar";
+import PostContainer from "./components/PostContainer/PostContainer";
 
 class App extends Component {
-  constructor(){
-    super(); 
-    this.state={
+  constructor() {
+    super();
+    this.state = {
       data: dummyData
-    }
+    };
   }
   render() {
     return (
       <div className="app-container">
-      <div>
-      <SearchBar /> 
-      </div>
-      <PostContainer posts={this.state.data}/>
+        <div>
+          <SearchBar />
+        </div>
+        <PostContainer posts={this.state.data} />
       </div>
     );
   }
--- insta-app/src/components/SearchBar/SearchBar.js
+++ insta-app/src/components/SearchBar/SearchBar.js
@@ -1,28 +1,27 @@
-import React from 'react'; 
-import PropTypes from 'prop-types'; 
-
+import React from "react";
+import PropTypes from "prop-types";
 
 const SearchBar = props => {
-    return(
-        <div className="top-bar">
-            <div className="top-bar-left">
-            <i className="fas fa-camera"></i>
-            </div>
-                <form>
-                    <input
-                    className="search-bar" 
-                    type="text"
-                    name="search-bar"
-                    placeholder="          Search"
-                    />
-                </form>
-                <div className="top-bar-right">
-                    <i className="far fa-compass"></i>
-                    <i className="far fa-heart"></i>
-                    <i className="fas fa-user"></i>
-                </div>
-        </div>
-    )
-}
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <i className="fas fa-camera" />
+      </div>
+      <form>
+        <input
+          className="search-bar"
+          type="text"
+          name="search-bar"
+          placeholder="          Search"
+        />
+      </form>
+      <div className="top-bar-right">
+        <i className="far fa-compass" />
+        <i className="far fa-heart" />
+        <i className="fas fa-user" />
+      </div>
+    </div>
+  );
+};
 
-export default SearchBar; 
+export default SearchBar;
