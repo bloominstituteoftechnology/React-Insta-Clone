bcc980b3b9b6ed152ffbacc21d47c425438219a3
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-07-30 23:43:14 -0700

    Major changes - Likes Section
    Finished setting up the 'likes' section.

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -4,17 +4,20 @@ import CommentSection from '../CommentSection/CommentSection';
 
 const PostContainer = props => {
     return (
-        <div className="post-container">
+        <div className="post-container"> 
+            {/* TOP SECTION OF POST */}
             <div className="post-top">
                <img src={props.post.thumbnailUrl} alt="post thumbnail" className="user-thumbnail"/> 
                <h4 className="post-username">{props.post.username}</h4>
             </div>
 
+            {/* IMAGE SECTION */}
             <div className="image-container">
                 <img src={props.post.imageUrl} alt="post display" className="post-image"/> 
             </div>
 
-            <div className="likes-container">{props.post.likes} likes</div>
+            {/* LIKES SECTION */}
+            <div className="like-container">{props.post.likes} likes</div>
 
             <div className="comment-content">
                 {props.post.comments.map(comment => < CommentSection comment={comment} /> )}
