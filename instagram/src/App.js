94d7b73f98abe0442ec26e97dc760333da22b2d8
Carey <careycbaldwin@gmail.com>
2018-07-31 18:47:20 -0400

    pulled remaining (except for comments) data from dummy-data.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -57,7 +57,20 @@ const Post = props => {
   return (
     <div className = "post-container">
       <div className = "post">
-        <h3> {props.post.username} </h3>
+        
+        <div className = "post-header">
+          <img src = {props.post.thumbnailUrl} />
+          <h3> {props.post.username} </h3>
+        </div>
+        
+        <div className = "post-image">
+          <img src={props.post.imageUrl} />
+        </div>
+        
+        <div className = "post-footer">
+          <p> {props.post.likes} </p>
+          <p> {props.post.timestamp} </p>
+        </div>
       </div>
     </div>
   )
