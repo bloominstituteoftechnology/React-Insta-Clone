b47fd2a1646e53ceb0e5fba7b57d745fd39da447
cubsfan46350 <cubsfan.46350@msn.com>
2018-07-31 07:35:31 -0500

    Added map function for Posts

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import dummyData from './dummy-data';
+import Posts from './components/PostContainer/posts';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      appData: dummyData
+    }
+    dataFunction() = props => {
+      return (
+        props.appData.map(dataItem => <Posts />)
+      )
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ instagram/src/components/PostContainer/posts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Posts = props => {
+    return (
+
+    )
+}
+
+export default Posts;
