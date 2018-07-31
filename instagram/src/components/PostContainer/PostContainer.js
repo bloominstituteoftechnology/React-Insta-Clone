9c316c05f86836df9272c020205f2b0cdcac0cee
keegn <keegan.t.burkett@gmail.com>
2018-07-30 22:59:30 -0500

    added type check for post container file and helper commments

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -3,13 +3,22 @@ import "./PostContainer.css";
 import Post from "./Post";
 import PropTypes from "prop-types";
 
-// {props.posts.map(p => <Post key={p.imageUrl} post={p} />)}
+// we are passing down data from the parent state to this child component
+// this child component takes a list of elements and renders a new Post component for each object in dummyData
+// PostContainer is actually going to be mounting another Component Post for each item in our array
+
 const PostContainer = props => {
   return (
-    <div className="posts-container-wrapper">
+    <div>
       {props.posts.map(p => <Post key={p.imageUrl} post={p} />)}
     </div>
   );
 };
+
+PostContainer.propTypes = {
+    posts: PropTypes.arrayOf(PropTypes.object)
+};
+  
+  
 
 export default PostContainer;
