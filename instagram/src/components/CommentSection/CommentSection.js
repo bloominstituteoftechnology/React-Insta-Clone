da86bd21c003ef06ca03ddeb27ec004988bf9fd4
Lauren <llwor94@gmail.com>
2018-07-31 13:49:42 -0700

    delete button shows and hides on mouse enter/mouse leave

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,5 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types'
+import deleteImg from "../../assets/delete.png";
 import "./CommentSection.css";
 
 class CommentSection extends React.Component {
@@ -10,19 +11,23 @@ class CommentSection extends React.Component {
   showDelete = () => {
     this.setState({ deleteShowing: true });
   };
+
+  hideDelete = () => {
+    this.setState({ deleteShowing: false })
+  }
   render() {
     return (
-      <div className="comment" onMouseEnter={this.showDelete}>
+      <div className="comment" onMouseEnter={this.showDelete} onMouseLeave={this.hideDelete}>
         <h1>{this.props.comment.username}</h1>
         <p>{this.props.comment.text}</p>
-        <button onClick={this.props.onClick}>Delete</button>
+        {this.state.deleteShowing && <img className="deleteImg" src={deleteImg} onClick={this.props.onClick}/>}
       </div>
     );
   }
 }
 
-{/* CommentSection.propTypes = {
+CommentSection.propTypes = {
   comments: PropTypes.arrayOf(PropTypes.string),
-}; */}
+};
 
 export default CommentSection;
