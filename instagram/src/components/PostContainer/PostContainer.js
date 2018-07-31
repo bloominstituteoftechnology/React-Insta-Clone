59be62ddded0d235441b2e67ee01833d16eafe20
Lauren <llwor94@gmail.com>
2018-07-31 16:33:40 -0700

    fixed some issues with like and comment buttons, added functionality to focus on addComment input on button click

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -2,51 +2,64 @@ import React from "react";
 import CommentSection from "../CommentSection/CommentSection";
 import PropTypes from "prop-types";
 import unliked from "../../assets/like.png";
-import liked from "../../assets/liked.png"
+import liked from "../../assets/liked.png";
 import comment from "../../assets/comment.png";
 import moment from "moment";
 import "./PostContainer.css";
 
 class PostContainer extends React.Component {
-  state = {
-    comment: '',
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+    this.state = {
+      comment: "",  
+    };
   }
+  handleChange = e => {
+    this.setState({ comment: e.target.value });
+  };
 
-  handleChange = (e) => {
-    this.setState({ comment: e.target.value})
-  }
-
-  handleAddComment = (e) => {
+  handleAddComment = e => {
     if (e.keyCode === 13) {
-      this.props.handleAddComment(this.state.comment, this.props.post.id)
-      this.setState({ comment: '' })
+      this.props.handleAddComment(this.state.comment, this.props.post.id);
+      this.setState({ comment: "" });
     }
-  }
+  };
 
+  focusTextInput = () => {
+    this.textInput.current.focus();
+  }
   render() {
     return (
       <div className="post">
         <div className="post__header">
           <div className="post__header__image">
-            <img src={this.props.post.thumbnailUrl} alt="thumbnail"/>
+            <img src={this.props.post.thumbnailUrl} alt="thumbnail" />
           </div>
           <h1> {this.props.post.username} </h1>
         </div>
-        <img className="post__main-img" src={this.props.post.imageUrl} alt="main"/>
+        <img
+          className="post__main-img"
+          src={this.props.post.imageUrl}
+          alt="main"
+        />
         <div className="post__text">
           <div className="post__buttons">
             <img
               src={this.props.post.liked ? liked : unliked}
               onClick={() => this.props.handleLikeToggle(this.props.post.id)}
-              alt="liked" />
-            <img src={comment} alt="comment" />
+              alt="liked"
+            />
+            <img src={comment} alt="comment" onClick={this.focusTextInput} />
           </div>
           <h1 className="post__likes"> {this.props.post.likes} likes </h1>
-          {this.props.post.comments.map((comment,index) => (
-            <CommentSection 
+          {this.props.post.comments.map((comment, index) => (
+            <CommentSection
               key={index}
-              comment={comment} 
-              onClick={() => this.props.handleDeleteComment(comment.id, this.props.post.id)}
+              comment={comment}
+              onClick={() =>
+                this.props.handleDeleteComment(comment.id, this.props.post.id)
+              }
             />
           ))}
           <p className="post__date">
@@ -57,6 +70,7 @@ class PostContainer extends React.Component {
         </div>
         <div className="add-comment">
           <input
+            ref={this.textInput}
             type="text"
             value={this.state.comment}
             placeholder="Add Comment..."
