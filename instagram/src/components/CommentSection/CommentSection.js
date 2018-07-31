8f1486cd4472d87c725b170ec7faae3edc0dd4ce
Josue Peralta <jperal77@gmail.com>
2018-07-31 16:06:14 -0700

    Comments now get added

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -12,13 +12,26 @@ class CommentSection extends React.Component {
     };
   }
 
+  addNewComment = e => {
+    e.preventDefault();
+    const newComment = { text: this.state.comment, username: 'jperalta' };
+    const comments = this.state.comments.slice();
+    comments.push(newComment);
+    console.log(comments);
+    this.setState({comment: '', comments: comments});
+  }
+
+  editComment = e => {
+    this.setState({ comment: e.target.value });
+  }
+
   render () {
     return (
       <div>
         <CommentSectionHeader likes={this.props.likes} />
         {this.state.comments.map((comment, index) => <Comment key={index} comment={comment}/>)}
-        <form>
-          <input placeholder="Add a comment..." />
+        <form onSubmit={this.addNewComment}>
+          <input value={this.state.comment} onChange={this.editComment}placeholder="Add a comment..." />
         </form>
       </div>
     );
