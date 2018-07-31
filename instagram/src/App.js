731a7004660e16a70f2e063a71e81c1e7f3668ae
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-07-31 15:23:05 -0600

    Search component in progress, some bugs occurring

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(){
     super(); 
     this.state = {
-      data: []
+      data: [], 
+      filteredData : []
     }
   }
 
@@ -18,24 +19,58 @@ class App extends Component {
     })
   }
 
+  filterSearchHandler = (event) => {
+    const users = this.state.data.filter(post => {
+      if(post.username.includes(event.target.value)){
+        return post; 
+      }
+    }); 
+    this.setState({
+      filteredData: users
+    })
+  }
+
+ 
 
   render() {
 
+    let allUsers = null;
+    let filteredUsers = null;
+
+    if(this.state.filteredData.length > 0){
+      allUsers = null; 
+      filteredUsers = this.state.filteredData.map((user, index) =>  {
+        return (<PostContainer 
+          key = {index}
+          usernameImg = {user.thumbnailUrl}
+          username = {user.username}
+          img = {user.imageUrl}
+          likes = {user.likes}
+          comments = {user.comments}
+          timeStamp = {user.timestamp}
+          clickLike = {this.increaseLikeHandler}
+        />)
+      })
+    }else{
+      allUsers = this.state.data.map((user, index) =>  {
+        return (<PostContainer 
+          key = {index}
+          usernameImg = {user.thumbnailUrl}
+          username = {user.username}
+          img = {user.imageUrl}
+          likes = {user.likes}
+          comments = {user.comments}
+          timeStamp = {user.timestamp}
+          clickLike = {this.increaseLikeHandler}
+        />)
+      })
+    }
+
     return (
       <div className="App">
-        <SearchBar />
-        {this.state.data.map((user, index) =>  {
-          return <PostContainer 
-            key = {index}
-            usernameImg = {user.thumbnailUrl}
-            username = {user.username}
-            img = {user.imageUrl}
-            likes = {user.likes}
-            comments = {user.comments}
-            timeStamp = {user.timestamp}
-            clickLike = {this.increaseLikeHandler}
-          />
-        })}
+        <SearchBar keyDown = {this.filterSearchHandler} />
+        {allUsers}
+        {filteredUsers}
       </div>
     );
   }
