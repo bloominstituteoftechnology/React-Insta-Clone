a20d7c71dead86c55ff32e98ac37e807b6f74e4c
jordan-massingill <jordan_massingill@hotmail.com>
2018-07-31 18:24:22 -0500

    MVP changes
    Completed like function

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,15 +3,18 @@ import dummyData from './dummy-data';
 import PostContainer from './components/PostContainer/PostContainer.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
 import './App.css';
-import PropTypes from 'prop-types';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      posts: dummyData,
+      posts: [],
       searchInfo: ''
     }
+  }
+
+  componentDidMount() {
+    this.setState({posts: dummyData})
   }
 
   searchItemHandler =(event)=> {
@@ -20,15 +23,14 @@ class App extends Component {
 
   searchSubmitHandler=(event)=> {
     event.preventDefault();
-    let myPosts= this.state.posts.filter(item => item.username === this.state.searchInfo);
-    console.log(myPosts);
+    let myPosts= this.state.posts.filter(item => item.username.toLowerCase() === this.state.searchInfo.toLowerCase());
     this.setState({posts: myPosts, searchInfo: ''});
   }
 
   render() {
     return (
       <div className="App">
-        <SearchBar searchItem={this.searchItemHandler} searchValue={this.searchInfo} searchSubmit={this.searchSubmitHandler}/>
+        <SearchBar searchItem={this.searchItemHandler} searchValue={this.state.searchInfo} searchSubmit={this.searchSubmitHandler}/>
         {this.state.posts.map(item => <PostContainer post={item} comments={item.comments} key={item.timestamp} /> )}
       </div>
     );
--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -2,15 +2,32 @@ import React from 'react';
 import './PostContainer.css';
 import PropTypes from 'prop-types';
 
-const Post = (props) => {
-  return (
-      <div>
-        <p className="post-header"> <img className="profile-pic" src={props.post.thumbnailUrl} alt='Profile' /> {props.post.username}</p>
-        <img src={props.post.imageUrl} alt='Posted' />
-        <p>{props.post.likes} likes</p>
-        <p>Posted {props.post.timestamp}</p>
-      </div>
-    );
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profilePic: props.post.thumbnailUrl,
+      username: props.post.username,
+      postImg: props.post.imageUrl,
+      likes: props.post.likes,
+      timestamp: props.post.timestamp
+    }
+  }
+
+  likeHandler=(event)=> {
+    this.setState(prevState => ({likes: prevState.likes+1}))
+  }
+
+  render() {
+    return (
+        <div>
+          <p className="post-header"> <img className="profile-pic" src={this.state.profilePic} alt='Profile' /> {this.state.username}</p>
+          <img src={this.state.postImg} alt='Posted' />
+          <div className="post-icons-container"><img onClick={this.likeHandler} className="post-icon" src="https://d30y9cdsu7xlg0.cloudfront.net/png/682470-200.png"/> <img className="post-icon" src="https://image.freepik.com/free-icon/chat-bubble-ios-7-interface-symbol_318-38832.jpg" /> </div>
+          <p>{this.state.likes} likes</p>
+          <p>Posted {this.state.timestamp}</p>
+        </div>
+      );}
 }
 
 Post.propTypes = {
