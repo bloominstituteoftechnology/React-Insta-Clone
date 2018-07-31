700f4fd1a87076e40f568b4f94787d58500ee726
sca4cs <shannon.atkinson.ca@gmail.com>
2018-07-31 15:25:39 -0400

    added more styles to header

--- react-insta-clone/src/components/SearchComponents/SearchBar.js
+++ react-insta-clone/src/components/SearchComponents/SearchBar.js
@@ -1,18 +1,18 @@
 import React from "react";
 import './SearchBar.css';
 
-const SearchBar = props => {
+const SearchBar = () => {
     return (
         <div className = "search-container">
-        <div>
+        <div className = "instagram">
         <i className="fab fa-instagram"></i>
+        <img alt="instagram logo" src="https://raw.githubusercontent.com/LambdaSchool/React-Insta-Clone/4079cf6fb6d11bd3ad7a422cdec35e0aab4e2bfe/instagram/src/assets/iglogo.png" className="logo" />
         </div>
         <div>
-           <input
-            type="text"
-            name="search"
-            placeholder="&#128269; Search"
-            /> 
+        <input  type="text"
+                name="search"
+                className="fa fa-input"
+                placeholder="&#xf002; Search"/>
         </div>
         <div>
         <i className="far fa-compass"></i>
