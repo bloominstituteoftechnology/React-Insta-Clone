e83bddb1a7d2227f9fca74ed1d5eeb19cfb733ee
LaikaFusion <newrad4@gmail.com>
2018-08-01 15:39:58 -0400

    realized my components werent in the components folder

---
+++ instakg/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './CommentSection.css';
+import PropTypes from 'prop-types';
+
+ class CommentSection extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+     comments:[]
+    }
+  }
+  componentDidMount(){
+     this.setState({ comments: this.props.data },()=>{});
+  }
+  componentDidUpdate(prevProps){
+    if (this.props.data !== prevProps.data) {
+    this.setState({ comments: this.props.data },()=>{});
+    }
+ }
+  addComment(){
+    console.log('success')
+  }
+  render(){
+   return(
+    <div className ='commentBox'>
+        { this.state.comments.map((e,i) =>{
+          return (<div key={i} className='comment'>
+         <span className='userName'> {e.username}</span>
+          <span className='userComment'>{e.text}</span>
+          <span onClick={this.props.methods} className='deleteButton'> ␡ </span>
+          </div>)
+        })}
+    </div> 
+  )
+}
+ }
+CommentSection.propTypes = {
+   data: PropTypes.array.isRequired
+}
+
+export default CommentSection;
---
+++ instakg/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './PostContainer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faComment } from '@fortawesome/free-regular-svg-icons';
+import {  faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+import CommentSection from '../CommentSection/CommentSection';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+ const PostContainer = (props) =>{
+   return(
+    <div className="post">
+      <div className="row header">
+        <div className='userIcon'> <img alt='usericon' className="userIcon" src={props.data.thumbnailUrl}></img> </div>     
+        <div className='userName'> {props.data.username}</div>
+      </div>
+    <div className='postImageContainer'><img alt='postimage' src={props.data.imageUrl}></img> </div> 
+    <div className='row iconRow'>
+    
+      <FontAwesomeIcon id={props.id}  onClick={props.methods[1]} className="icon" icon={faHeart} />
+      <FontAwesomeIcon className="icon" icon={faComment} />
+    </div>
+    <div className="likes">{props.data.likes} likes</div>
+   < CommentSection methods={props.methods[2].bind(this)} data={props.data.comments} />
+    <div className='timeStamp'> {moment(props.data.timestamp, "MMMM Do YYYY, h:mm:ss a").fromNow()}</div>
+    <div className='commentBar'/>
+    <div className="row AddComment">
+    <input type='text' id={props.id} className='commentSubmitField' placeholder='Add a comment...' onKeyUp={props.methods[0]}></input>
+    <FontAwesomeIcon className="icon moreIcon" icon={faEllipsisH} />
+    </div>
+    </div>
+  )
+}
+PostContainer.propTypes = {
+  data: PropTypes.shape({
+      username:PropTypes.string.isRequired,
+      thumbnailUrl:PropTypes.string.isRequired,
+      imageUrl:PropTypes.string.isRequired,
+      likes:PropTypes.number.isRequired,
+      timestamp:PropTypes.string.isRequired,
+      comments:PropTypes.array.isRequired,
+  })
+}
+
+export default PostContainer;
