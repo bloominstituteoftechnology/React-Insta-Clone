5998e7637b749f81118b13ca39c5244ae3ce8f44
Arthur Bates <aabatesj@gmail.com>
2018-08-01 10:35:27 -0400

    Search and Like
    Search and Like functionality added, still working on adding comments

--- Instagram/src/App.js
+++ Instagram/src/App.js
@@ -8,15 +8,36 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      posts: dummyData
+      posts: dummyData,
+      filteredPosts: []
     };
   }
 
+  componentDidMount() {
+    this.setState({ posts: dummyData });
+  }
+  searchPostHandler = e => {
+    const posts = this.state.posts.filter(p => {
+      if (p.username.includes(e.target.value)) {
+        return p;
+      }
+    });
+    this.setState({ filteredPosts: posts });
+  };
   render() {
     return (
       <div className="App">
-        <SearchBar />
-        <PostsContainer posts={this.state.posts} />
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          searchPosts={this.searchPostHandler}
+        />
+        <PostsContainer
+          posts={
+            this.state.filteredPosts.length > 0
+              ? this.state.filteredPosts
+              : this.state.posts
+          }
+        />
       </div>
     );
   }
--- Instagram/src/components/CommentSection/CommentInput.js
+++ Instagram/src/components/CommentSection/CommentInput.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const CommentInput = props => {
     return (
-        <form>
-            <input type="text" placeholder="Add comment... " />
+        <form onSubmit={props.submitComment}>
+            <input
+                type="text"
+                value={props.comment}
+                placeholder="Add comment... "
+                onChange={props.changeComment}
+            />
         </form>
     );
 };
