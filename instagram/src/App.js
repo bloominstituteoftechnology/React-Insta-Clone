6de07edd7a8761039e0c814f78e97461ab391e40
keegn <keegan.t.burkett@gmail.com>
2018-07-31 20:30:15 -0500

    added search

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,35 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./App.css";
-import dummyData from './dummy-data.js';
-import SearchBar from './components/SearchBar/SearchBar';
-import PostContainer from './components/PostContainer/PostContainer';
+import dummyData from "./dummy-data.js";
+import SearchBar from "./components/SearchBar/SearchBar";
+import PostContainer from "./components/PostContainer/PostContainer";
 
 // Searchbar and PostContainer are children of App Component
 // App Component holds the state of the dummyData
-// if this.setState is called then 
+// if this.setState is called then
 // for search add  this to state object here.....filteredPosts: []
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      posts: dummyData,
+      posts: [],
       filteredPosts: []
     };
   }
- 
+
   componentDidMount() {
     this.setState({
-     posts: dummyData 
-      });
-      console.log("data mounted", this.state)
+      posts: dummyData
+    });
+    console.log("data mounted", this.state);
   }
-  
+  searchPostsHandler = e => {
+    const posts = this.state.posts.filter(p => {
+      if (p.username.includes(e.target.value)) {
+        return p;
+      }
+    });
+    this.setState({ filteredPosts: posts });
+  };
   render() {
     return (
       <div>
-      <SearchBar />
-      <PostContainer posts={this.state.posts} />
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          searchPosts={this.searchPostsHandler}
+        />
+        <PostContainer
+          posts={
+            this.state.filteredPosts.length > 0
+              ? this.state.filteredPosts
+              : this.state.posts
+          }
+        />
       </div>
     );
   }
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -6,7 +6,7 @@ import textLogo from "../../assets/insta-text-logo.svg";
 
 // input field should be refactored eventually into its own component
 
-const SearchBar = () => {
+const SearchBar = props => {
   return (
     <div className="header">
       <div className="container">
@@ -20,7 +20,7 @@ const SearchBar = () => {
             />
           </div>
           <div className="input-group">
-            <input type="text" placeholder="Search" />
+            <input type="text" placeholder="Search" onKeyDown={props.searchPosts} />
           </div>
           <div className="social-group">
              <span className="header-icon"><i className="far fa-compass icon"></i></span>
