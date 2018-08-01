cd6ddf0f0896ae9bc8950d40162a916c54aab427
Kairi <kairijb@gmail.com>
2018-08-01 17:41:58 -0500

    Login page mostly done

--- instagram/src/index.js
+++ instagram/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Authenticate from './Authentication/Authentication'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(Authenticate(<App />), document.getElementById('root'));
 registerServiceWorker();
