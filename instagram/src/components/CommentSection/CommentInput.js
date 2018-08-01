4dc19d189450f7103b862239d17f1025966ad911
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-08-01 11:31:43 -0500

    added input handlers and got functionality working

--- instagram/src/components/CommentSection/CommentInput.js
+++ instagram/src/components/CommentSection/CommentInput.js
@@ -4,9 +4,9 @@ import { Form, FormGroup, Input } from 'reactstrap';
 
 const CommentInput = props => {
     return (
-        <Form>
+        <Form onSubmit={props.submitComment}>
             <FormGroup >
-                <Input type="text" name="search" id="exampleSearch" placeholder="Add comment..." />
+                <Input value={props.comment} onChange={props.changeComment} type="text" name="search" id="exampleSearch" placeholder="Add comment..." />
             </FormGroup>
         </Form>
     )
--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -10,14 +10,31 @@ class CommentSection extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            comments: props.comments
+            comments: props.comments,
+            comment: ''
         }
     }
+
+    commentHandler = event => {
+        this.setState({ comment: event.target.value });
+    };
+
+    handleCommentSubmit = event => {
+        event.preventDefault();
+        const newComment = { text: this.state.comment, username: "kyle" };
+        const comments = this.state.comments.slice();
+        comments.push(newComment);
+        this.setState({ comments, comment: "" });
+    };
+
     render() {
         return (
             <Container>
                 {this.state.comments.map((comment, index) => <Comment key={index} comment={comment} />)}
-                <CommentInput />
+                <CommentInput
+                    comment={this.state.comment}
+                    submitComment={this.handleCommentSubmit}
+                    changeComment={this.commentHandler} />
 
             </Container>
         )
