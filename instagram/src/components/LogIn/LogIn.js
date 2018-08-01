a0e1a3cf5fe2a7243bc399e0cc7c901b6079ac2a
Lauren <llwor94@gmail.com>
2018-08-01 16:37:25 -0700

    button disabled until username and password filled out

--- instagram/src/components/LogIn/LogIn.js
+++ instagram/src/components/LogIn/LogIn.js
@@ -5,40 +5,62 @@ import logo from "../../assets/instagramlogin.png";
 class LogIn extends React.Component {
   constructor(props) {
     super(props);
-    this.state= {
-      username: ''
-    }
+    this.state = {
+      username: "",
+      password: "",
+    };
   }
 
   handleChange = e => {
-    this.setState({ username: e.target.value })
-  }
+    console.log(e.target.type)
+    if (e.target.type === 'text') {
+      this.setState({ username: e.target.value });
+    } if (e.target.type === 'password') {
+      this.setState({ password: e.target.value})
+    }
+  };
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
-    localStorage.setItem('username', this.state.username)
+    localStorage.setItem("username", this.state.username);
     this.props.handleLogIn(this.state.username);
-  }
+  };
 
   render() {
+    let fieldsValid = this.state.username && this.state.password;
+
     return (
-      <div className="login">
-        <div className="login__box">
-          <form 
-            className="login__box--inside"
-            onSubmit={this.handleSubmit}>
-            <img src={logo} alt="logo" className="login--logo" />
-            <div className="input-box">
-              <input 
-              className="input input--username" 
-              placeholder="Username"
-              value={this.state.username}
-              onChange={this.handleChange}
-               />
-              <input className="input input--password" placeholder="Password" />
+      <div className="wrapper">
+        <div className="content">
+          <div className="login">
+            <form className="login__box" onSubmit={this.handleSubmit}>
+              <img src={logo} alt="logo" className="login--logo" />
+              <div className="input-box">
+                <input
+                  type="text"
+                  className="input input--username"
+                  placeholder="Username"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                />
+                <input
+                  className="input input--password"
+                  type="password"
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <button disabled={!fieldsValid} className="login--btn" type="submit">
+                Log in
+              </button>
+            </form>
+          </div>
+          <div className="signUp">
+            <div className="signUp-box">
+              <p> Don't have an account? </p> <a ref="#">Sign Up</a>
             </div>
-            <button className="login--btn" type="submit">Log in</button>
-          </form>
+          </div>
         </div>
       </div>
     );
