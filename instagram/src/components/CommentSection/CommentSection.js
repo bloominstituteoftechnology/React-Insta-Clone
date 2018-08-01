3c8a8e63da4b342587eab16ab1d24e3b8f651e76
William <kwontaewoongah@gmail.com>
2018-07-31 18:46:51 -0700

    used moment.js library to dynamically render a timestamp

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Comment from './Comment.js';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import './CommentSection.css';
 import AddCommentBar from './addcommentbar.js';
 
@@ -24,11 +25,24 @@ class CommentSection extends React.Component {
     handleInputChange=(e)=>{
         this.setState({newComment:e.target.value});
     }
+    dynamicDate=(date)=>{
+        let dynamicDate=date;
+        dynamicDate=date.split(' ');
+        let newDate='';
+        for (let i=0; i<dynamicDate[1].length; i++) {
+            if (parseInt(dynamicDate[1][i],10)>0){
+                newDate+=dynamicDate[1][i];
+            }
+        }
+        dynamicDate[1]=newDate;
+        dynamicDate=new Date(dynamicDate.join(' '));
+        return moment(dynamicDate).fromNow();
+    }
     render() {
         return (
             <div>
                 {this.state.comments.map((e,i)=><Comment data={e} key={i}/>)}
-                <p className='time-stamp'>{this.state.timestamp}</p>
+                <p className='time-stamp'>{this.dynamicDate(this.state.timestamp)}</p>
                 <div className='rule'></div>
                 <AddCommentBar value={this.state.newComment} inputHelper={this.handleInputChange} handleInputSubmit={this.addNewComment} length={this.state.comments.length}/>
             </div>
