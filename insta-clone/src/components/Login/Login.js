9b25d2b530d24f81fc89d84edc583ad92e3bb647
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-01 12:16:49 -0700

    add styling to login page

--- insta-clone/src/components/Login/Login.js
+++ insta-clone/src/components/Login/Login.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Button, Form, Input, Alert } from 'reactstrap';
+import './Login.css';
 
 const Login = (props) => {
     return(
-        <div className="login">
-            <input type="text" name="username" placeholder="Username" onChange={props.storeUser}></input>
-            <input type="password" name="password" placeholder="Password"></input>
-            <button onClick={props.login} >Login</button>
+        <div className="container login">
+            <img src="https://3rockmarketing.com/wp-content/uploads/2016/09/instagram-logo.png" alt="instagram" className="instagram-text" />
+            <Alert color="info">Please login! </Alert>
+            <Form className="login" onSubmit={props.login}>
+                <Input type="username" name="username" placeholder="Username" onChange={props.storeUser}></Input>
+                <Input type="password" name="password" placeholder="Password" required></Input>
+                <Button>Login</Button>
+            </Form>
         </div>
     )
 }
