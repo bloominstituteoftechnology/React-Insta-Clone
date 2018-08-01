9f5c5807cdd334b38ff9a86038113e77cc49e7cb
kamalnrf <kamalnrf@hotmail.com>
2018-08-02 02:35:20 +0530

    Added Conditional Rendering

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -14,8 +14,6 @@ class App extends Component {
       data: [],
       search: '',
       cloneData: [],
-      userName: '',
-      password: ''
     };
   }
 
@@ -39,35 +37,14 @@ class App extends Component {
     this.setState({data: newData})
   }
 
-  loginUser (event){
-    event.preventDefault()
-    console.log('Storing')
-    const userName = this.state.userName 
-    const password = this.state.password
-    localStorage.setItem('userName', userName)
-    window.location.reload()
-    console.log(localStorage.getItem('userName'))
-  }
-
-  onUserInputChnage (event){
-    this.setState({userName: event.target.value})
-  }
-
-  onPasswordInputChange (event){
-    this.setState({password: event.target.value})
-  }
-
   render() {
     return (
-      <Login onChangeFunction = {{
-        userName: this.onUserInputChnage.bind(this),
-        password: this.onPasswordInputChange.bind(this),
-        loginUser: this.loginUser.bind(this)
-       }} />
+      <PostPage data={this.state.data} inputChange={this.onInputChange.bind(this)} onClick={this.search.bind(this)}/>
     )
   }
 }
 
-// <PostPage data={this.state.data} inputChange={this.onInputChange.bind(this)} onClick={this.search.bind(this)}/>
 
-export default App;
+
+
+export default Authenticate(App);
--- instagram/src/Authentication/Authenticate.js
+++ instagram/src/Authentication/Authenticate.js
@@ -1,16 +1,53 @@
 import React from 'react' 
+import Login from '../components/Login/Login';
 
-const Authenticate = App => 
+const Authenticate = (App) => 
     class extends React.Component {
         constructor (){
             super()
             this.state = {
-
+                logedIn: false,
+                userName: '',
+                password: ''
             }
         }
 
+        componentDidMount (){
+            const userName = localStorage.getItem('userName')
+            if (this.state.logedIn === false && userName){
+                this.setState({logedIn: true})
+            }
+        }
+
+        loginUser (event){
+            event.preventDefault()
+            console.log('Storing')
+            const userName = this.state.userName 
+            const password = this.state.password
+            localStorage.setItem('userName', userName)
+            window.location.reload()
+            console.log(localStorage.getItem('userName'))
+        }
+
+        onUserInputChnage (event){
+            this.setState({userName: event.target.value})
+        }
+        
+        onPasswordInputChange (event){
+            this.setState({password: event.target.value})
+        }
+
         render(){
-            return <App />
+            if (this.state.logedIn)
+                return (<App />)
+            else 
+                return (
+                <Login onChangeFunction = {{
+                    userName: this.onUserInputChnage.bind(this),
+                    password: this.onPasswordInputChange.bind(this),
+                    loginUser: this.loginUser.bind(this)
+                   }} />)
+
         }
     }
 export default Authenticate
