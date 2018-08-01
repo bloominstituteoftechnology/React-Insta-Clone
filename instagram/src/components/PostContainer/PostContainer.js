06f401757cae92c4ce206a455608b0dab96ba1b7
Josue Peralta <jperal77@gmail.com>
2018-07-31 22:24:50 -0700

    Posts can now be "liked"

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -3,24 +3,41 @@ import PostHeader from './PostHeader';
 import CommentSection from '../CommentSection/CommentSection';
 import './Post.css'
 
-const PostContainer = props => {
-  console.log(props.user)
-  return (
-    <div className='post'>
-      <PostHeader user={props.user}/>
-      <div className='post-img-wrapper'>
-        <img className='post-img' src={props.user.imageUrl} alt=''/>
-      </div>
-      <div className='like-section'>
-        <div className='like-section-icons'>
-          <i className='far fa-heart'></i>
-          <i className='far fa-comment'></i>
+class PostContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: props.user,
+      userIMG: props.user.imageUrl,
+      likes: props.user.likes,
+      comments: props.user.comments
+    };
+  }
+
+  incrementLikeCounter = e => {
+    let likes = this.state.likes + 1;
+
+      this.setState({likes: likes})
+  }
+
+  render () {
+    return (
+      <div className='post'>
+        <PostHeader user={this.state.user}/>
+        <div className='post-img-wrapper'>
+          <img className='post-img' src={this.state.userIMG} alt=''/>
         </div>
-        <p>{props.user.likes} likes</p>
+        <div className='like-section'>
+          <div className='like-section-icons'>
+            <i className='far fa-heart' onClick={this.incrementLikeCounter}></i>
+            <i className='far fa-comment'></i>
+          </div>
+          <p>{this.state.likes} likes</p>
+        </div>
+        <CommentSection comments={this.state.comments} />
       </div>
-      <CommentSection comments={props.user.comments} />
-    </div>
-  );
-};
+    );
+  };
+  };
 
 export default PostContainer;
