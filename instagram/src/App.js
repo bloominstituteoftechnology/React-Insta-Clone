3314bb689d12cf5a34bcf29af05ddf10ed6ba0d6
Charlie Winslow <cwinslow22@yahoo.com>
2018-08-01 15:42:08 -0500

    added Authenticate.js and imported into App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,49 +3,51 @@ import './App.css';
 import dummyData from './dummy-data.js';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
+import Authenticate from "./components/Authentication/Authenticate";
 
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      posts: [],
-      filteredPosts: []
-    }
-  }
-
-  componentDidMount() {
-    this.setState({
-      posts: dummyData
-    });
-  }
-
-  filterPosts = event => {
-    const posts = this.state.posts.filter(post => {
-      if (post.username == event.target.value) {
-        return post;
+const App = Authenticate(
+  class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        posts: [],
+        filteredPosts: []
       }
-    });
-    this.setState({ filteredPosts: posts });
-  }
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <div>
-          <SearchBar filterPostsHandler={this.filterPosts} /> 
-        </div>
-        <div>
-          {this.state.filteredPosts.length > 0 ? this.state.filteredPosts.map(post => (
-          <PostContainer key={post.timestamp} post={post} addLikeHandler={this.addLike} />
-          ) ) : this.state.posts.map(post => (
+    componentDidMount() {
+      this.setState({
+        posts: dummyData
+      });
+    }
+
+    filterPosts = event => {
+      const posts = this.state.posts.filter(post => {
+        if (post.username.includes(event.target.value)) {
+          return post;
+        }
+      });
+      this.setState({ filteredPosts: posts });
+    }
+
+    render() {
+      return (
+        <div className="App">
+          <div>
+            <SearchBar filterPostsHandler={this.filterPosts} /> 
+          </div>
+          <div>
+            {this.state.filteredPosts.length > 0 ? this.state.filteredPosts.map(post => (
             <PostContainer key={post.timestamp} post={post} addLikeHandler={this.addLike} />
-            ) )  } 
+            ) ) : this.state.posts.map(post => (
+              <PostContainer key={post.timestamp} post={post} addLikeHandler={this.addLike} />
+              ) )  } 
+          </div>
         </div>
-      </div>
-    );
-  }
-}
+      );
+    }
+  })
 
 
 
