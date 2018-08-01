d5a8f4a275908d76d2146dfe8020796f26654763
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-01 13:35:17 -0700

    set up styled-components for tomorrow, still using reactstrap for styling for now

--- insta-clone/src/components/Login/Login.js
+++ insta-clone/src/components/Login/Login.js
@@ -8,7 +8,7 @@ const Login = (props) => {
             <img src="https://3rockmarketing.com/wp-content/uploads/2016/09/instagram-logo.png" alt="instagram" className="instagram-text" />
             <Alert color="info">Please login! </Alert>
             <Form className="login" onSubmit={props.login}>
-                <Input type="username" name="username" placeholder="Username" onChange={props.storeUser}></Input>
+                <Input type="username" name="username" placeholder="Username" onChange={props.storeUser} required></Input>
                 <Input type="password" name="password" placeholder="Password" required></Input>
                 <Button>Login</Button>
             </Form>
