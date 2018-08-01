168230bd20a89e14e70483074001c56de8d39e88
szincone <sawyerzincone@gmail.com>
2018-08-01 14:42:06 -0600

    Authenticate imported and working on APp.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import PostsPage from './components/PostContainer/PostsPage';
+import Authenticate from './components/Authentication/Authenticate';
 
 class App extends Component {
   constructor () {
@@ -19,4 +20,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Authenticate(App);
---
+++ instagram/src/components/Authentication/Authenticate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Authenticate = App => 
+  class extends React.Component {
+    render() {
+      return <App />;
+    }
+  }
+
+
+export default Authenticate;
