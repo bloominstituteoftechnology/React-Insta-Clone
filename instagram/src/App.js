169561104c14fc5c9db62fbd972acf85c982bb65
codejoncode <jonathanjholloway@gmail.com>
2018-08-01 14:51:39 -0500

    created PostsPage and put everything on the page  got everything on the screen after many changes

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data'; 
-import SearchBar from './components/SearchBar/SearchBar.js'; 
-import PostContainer from './components/PostContainer/PostContainer.js'; 
-import CommentSection from './components/CommentSection/CommentSection.js'; 
+// import SearchBar from './components/SearchBar/SearchBar.js'; 
+// import PostContainer from './components/PostContainer/PostContainer.js'; 
+// import CommentSection from './components/CommentSection/CommentSection.js'; 
+import PostsPage from './components/PostContainer/PostsPage';
+
 
 
 class App extends Component {
@@ -17,41 +19,43 @@ class App extends Component {
     this.setState({dummyData:dummyData, prevDummyData: dummyData, cmdCalled: true});
   }
 
-  handleSearchChange = event => {
+  // handleSearchChange = event => {
     
-    const dummyData = this.state.prevDummyData; 
-    const prevDummyData = this.state.prevDummyData; 
-    //fd === filteredData 
+  //   const dummyData = this.state.prevDummyData; 
+  //   const prevDummyData = this.state.prevDummyData; 
+  //   //fd === filteredData 
 
-    const fd  = dummyData.filter(post => post.username.includes(event.target.value))
+  //   const fd  = dummyData.filter(post => post.username.includes(event.target.value))
     
-    this.setState({dummyData: fd, searchText:event.target.value}) 
+  //   this.setState({dummyData: fd, searchText:event.target.value}) 
 
-    if(event.target.value === 0){
-      this.setState({dummyData: prevDummyData});
-    }
+  //   if(event.target.value === 0){
+  //     this.setState({dummyData: prevDummyData});
+  //   }
     
 
-  }
+  // }
 
   render() {
-    let dummyData; 
-    this.state.cmdCalled ? dummyData = this.state.dummyData : dummyData = [];
+    let dummyData = this.state.dummyData.slice(); 
+    // this.state.cmdCalled ? dummyData = this.props.dummyData : dummyData = [];
     
     return (
-      <div className="App">
-        <SearchBar  value ={this.state.searchText} onChange = {this.handleSearchChange}/>
-        {dummyData.map((post, i) => <div key = {i} className = "post">
-        <hr className ="hrLine"/> <div className = "containerPost">
-        <PostContainer key = {Date.now()} userName = {post.username} thumbNail={post.thumbnailUrl} image={post.imageUrl}
-        likes = {post.likes}/> 
-        <CommentSection key ={i} comments = {post.comments} time = {post.timestamp}/> 
-        </div></div>) }
+      
+      //   <SearchBar  value ={this.state.searchText} onChange = {this.handleSearchChange}/>
+      //   {dummyData.map((post, i) => <div key = {i} className = "post">
+      //   <hr className ="hrLine"/> <div className = "containerPost">
+      //   <PostContainer key = {Date.now()} userName = {post.username} thumbNail={post.thumbnailUrl} image={post.imageUrl}
+      //   likes = {post.likes}/> 
+      //   <CommentSection key ={i} comments = {post.comments} time = {post.timestamp}/> 
+      //   </div></div>) }
+      // <div className = "App"></div>
+        <PostsPage dummyData={dummyData}/>
+        
+        
+      
+      
 
-        
-        
-        
-      </div>
     );
     
   }
---
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './PostContainer.css'; 
+
+import SearchBar from '../SearchBar/SearchBar.js'; 
+import PostContainer from './PostContainer.js'; 
+import CommentSection from '../CommentSection/CommentSection';
+
+
+
+class PostsPage extends React.Component{
+    constructor(){
+        super();
+        this.state = {dummyData: [], searchText: "", prevDummyData: []}; 
+    }
+
+    
+    
+    handleSearchChange = event => {
+    
+        const dummyData = this.state.prevDummyData.slice(); 
+        const prevDummyData = this.state.prevDummyData.slice(); 
+        //fd === filteredData 
+
+        const fd  = dummyData.filter(post => post.username.includes(event.target.value))
+        
+        this.setState({dummyData: fd, searchText:event.target.value}) 
+
+        if(event.target.value === 0){
+            this.setState({dummyData: prevDummyData});
+        }
+    
+
+    }
+
+    render() {
+        let dummyData = this.props.dummyData; 
+        
+
+        return(
+        <div className="App">
+            <SearchBar  value ={this.state.searchText} onChange = {this.handleSearchChange}/>
+            {dummyData.map((post, i) => <div key = {i} className = "post">
+            <hr className ="hrLine"/> <div className = "containerPost">
+            <PostContainer key = {Date.now()} userName = {post.username} thumbNail={post.thumbnailUrl} image={post.imageUrl}
+            likes = {post.likes}/> 
+            
+            <CommentSection key ={i} comments = {post.comments} time = {post.timestamp}/> 
+
+            </div></div>) }
+        
+        </div>
+        );
+    }
+}
+
+export default PostsPage; 
