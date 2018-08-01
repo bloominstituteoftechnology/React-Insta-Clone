e165af3776239b3108a4fa6ef91bb3858f3e0903
szincone <sawyerzincone@gmail.com>
2018-08-01 00:22:32 -0600

    MVP finished

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -28,8 +28,7 @@ class CommentSection extends React.Component {
     let arrayOfComments = this.state.comments.slice();
     arrayOfComments.push({
       username: "Frank",
-      text: this.state.text,
-      
+      text: this.state.text
     });
     this.setState({
       comments: arrayOfComments
@@ -41,31 +40,37 @@ class CommentSection extends React.Component {
     this.setState({ text: event.target.value });
   };
 
-  
   handleLike = () => {
     let likesTotal = this.state.likes + (this.state.liked ? -1 : 1);
     this.setState({
       likes: likesTotal,
       liked: !this.state.liked
-    })
-  }
+    });
+  };
   render() {
     return (
       <div className="commentDiv">
         <div className="userNameDiv">
-          <img src={this.state.comment.thumbnailUrl} alt="thumbnail of username"/>
+          <img
+            src={this.state.comment.thumbnailUrl}
+            alt="thumbnail of username"
+          />
           <h2>{this.state.comment.username}</h2>
         </div>
 
-        <img src={this.state.comment.imageUrl} className="commentImg" alt="subject in post"/>
+        <img
+          src={this.state.comment.imageUrl}
+          className="commentImg"
+          alt="subject in post"
+        />
 
         <div className="faPostDiv">
-          <i className="fa fa-heart-o" onClick={this.handleLike}/>
+          <i className="fa fa-heart-o" onClick={this.handleLike} />
           <i className="fa fa-comment-o fa-flip-horizontal" />
         </div>
         <h4>{this.state.likes} likes</h4>
         <div className="listOfCommentsDiv">
-          {this.state.comments.map(comment => <Comments comment={comment}/>)}
+          {this.state.comments.map(comment => <Comments comment={comment} />)}
         </div>
         <h4 className="timeStamp">{this.state.comment.timestamp} DAYS AGO</h4>
         <CommentInput
@@ -73,14 +78,15 @@ class CommentSection extends React.Component {
           comments={this.state.comment.comments}
           handleInputChange={this.handleInputChange}
         />
-        {/* <div className="commentInputForm">
-        <form onSubmit={this.handleSubmit}>
-          <input className="commentInput" placeholder="Add a comment..." />
-        </form>
-      </div> */}
       </div>
     );
   }
 }
+
+// CommentSection.propTypes = {
+//   comment: PropTypes.arrayOf(
+//     PropTypes.shape({ comments: PropTypes.string, username: PropTypes.string })
+//   )
+// };
 
 export default CommentSection;
