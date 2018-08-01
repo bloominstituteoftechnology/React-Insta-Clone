daa3e1335da9ca316f58d559b535fe4a475e2c3b
dasgma <dasgrigoma@gmail.com>
2018-08-01 11:58:39 -0700

    Finished Authenticate.js

--- instagram/src/components/Authenticate/Authenticate.js
+++ instagram/src/components/Authenticate/Authenticate.js
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
+import Login from '../Login/Login';
 
 const Authenticate = (App) => 
     class extends Component {
-        
+        constructor(props){
+            super(props);
+            this.state = {
+                login: false
+            }
+        }
+
+        componentDidMount() {
+            if(!localStorage.getItem("user")) {
+                this.setState({login: false});
+            } else {
+                this.setState({login: true});
+            }
+        }
+
+
         render() { 
-            return ( <App /> );
+            if (this.state.login) {
+                return ( <App /> );
+            } else {
+                return ( <Login /> );
+            }
+            
         }
     }
      
