9f744b44f42ed3ab69edf2ba79954ed3aa49f54e
tulf <timzulf@gmail.com>
2018-08-02 16:53:55 -0400

    Built out styled component, and I really don't like it at all.

--- instagram/src/components/CommentSection/CommentInput.js
+++ instagram/src/components/CommentSection/CommentInput.js
@@ -1,16 +1,22 @@
 import React from "react";
 import './CommentSection.css';
+import styled from 'styled-components';
+
+const CommentInputArea = styled.input`
+outline: none;
+border: hidden;
+margin: 10px 0 0 -20px;
+`;
 
 
 const CommentInput = props => {
   return (
     <form onSubmit={props.submitComment} >
-      <input
+      <CommentInputArea
         onChange={props.changeComment}
         value={props.comment}
         type="text"
         placeholder="Add comment... "
-        className= "formholder"
       />
     </form>
   );
--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -43,7 +43,7 @@ handleCommentSubmit = e => {
     alert("You must provide a comment");
     return;
   }
-  const newComment = { text: this.state.comment, username: "testbot" };
+  const newComment = { text: this.state.comment, username: this.state.username };
   const comments = this.state.comments.slice();
   comments.push(newComment);
   this.setState({ comments, comment: "" });
