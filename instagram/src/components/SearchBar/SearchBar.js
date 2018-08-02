58cc7b06fd1bd392afa1e0880f42dd5949b3b14f
keegn <keegan.t.burkett@gmail.com>
2018-08-02 17:42:51 -0500

    almost done refacting searchbar, regretting the amout of styling I used

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -2,16 +2,72 @@ import React from "react";
 import "./SearchBar.css";
 import cam from "../../assets/instagram-brands.svg";
 import textLogo from "../../assets/insta-text-logo.svg";
-import styles from "styled-components"
+import styled from "styled-components"
 
-const Header = styles.header`
-border-bottom: 1px solid #3333;
-height: 80px;
-background: #ffffff;
-position: fixed;
-width: 100vw;
-top: 0;
+const Header = styled.header`
+  border-bottom: 1px solid #3333;
+  height: 80px;
+  background: #ffffff;
+  position: fixed;
+  width: 100vw;
+  top: 0;
 `;
+
+const Container = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+const SearchBarWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+    > input {
+    border: solid 1px #dbdbdb;
+    border-radius: 3px;
+    color: #262626;
+    outline: 0;
+    padding: 7px 0px 7px 0px;
+    z-index: 2;
+    text-align: center;
+    width: 100%;
+    font-size: 14px;
+    font-weight:300;
+    }
+`;
+
+const SearchInput = styled.input`
+    border: solid 1px #dbdbdb;
+    border-radius: 3px;
+    color: #262626;
+    outline: 0;
+    padding: 7px 0px 7px 0px;
+    z-index: 2;
+    text-align: center;
+    width: 100%;
+    font-size: 14px;
+    font-weight:300;
+    @media (max-width: 480px) {
+    display: none;
+    }
+`;
+
+const ImageGroup = styled.div`
+    height: 40px;
+`;
+
+
+const InputGroup = styled.div`
+    flex: 0 1 auto;
+    min-width: 125px;
+    max-width: 215px;
+    @media (max-width: 480px) {
+    display: none;
+    }
+`;
+
+
 
 
 // input field should be refactored eventually into its own component
@@ -19,26 +75,26 @@ top: 0;
 const SearchBar = props => {
   return (
     <Header>
-      <div className="container">
-        <div className="search-bar">
-          <div className="image-group">
+      <Container>
+        <SearchBarWrapper>
+          <ImageGroup>
             <img alt="instagram camera" src={cam} className="logo-image" />
             <img
               alt="instagram logo"
               src={textLogo}
               className="logo-image text-logo"
             />
-          </div>
-          <div className="input-group">
-            <input type="text" placeholder="Search" onKeyDown={props.searchPosts} />
-          </div>
+          </ImageGroup>
+          <InputGroup>
+            <SearchInput type="text" placeholder="Search" onKeyDown={props.searchPosts} />
+          </InputGroup>
           <div className="social-group">
              <span className="header-icon"><i className="far fa-compass icon"></i></span>
              <span className="header-icon"><i className="far fa-heart"></i></span>
              <span className="header-icon"><i className="far fa-user-circle"></i></span>
           </div>
-        </div>
-      </div>
+        </SearchBarWrapper>
+      </Container>
     </Header>
   );
 };
