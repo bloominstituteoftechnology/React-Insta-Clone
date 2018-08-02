b6df4868c99af397d474c4c548d72a30cfdc4cb6
Erik Kimsey <erik@erikkimsey.com>
2018-08-02 17:54:38 -0400

    Added styled components to Search.js and Post.js

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -2,27 +2,99 @@ import React from 'react';
 import PropTypes from 'prop-types'; 
 import CommentList from '../CommentSection/CommentList.js';
 import './Post.css';
+import style from 'styled-components';
 import moment from 'moment';
+
+const PostContainer = style.div`
+    width: 100%;
+    display: flex;
+    flex-flow: column;
+    margin-top: 5px;
+    border: 1px solid black;
+`;
+
+const PostHeader = style.header`
+     width: 100%;
+    display: flex;
+    flex-flow: row;
+    justify-content: flex-start;
+    align-items: center;
+`;
+
+const PostImg = style.img`
+      width: 100%;
+    height: 616px;
+    background: gray;
+    margin-top: 5px;
+`;
+
+const PostUserAvatar = style.img`
+      width: 50px;
+    height: 50px;
+    background: gray;
+`;
+
+const PostUserHandle = style.div`
+     height: 50px;
+    width: 100px;
+    line-height: 3.2;
+`
+
+const LikesContainer = style.div`
+    width: 100%;
+    height: 50px;
+    display: flex;
+    flex-flow: row;
+    justify-content: flex-start;
+`;
+
+const LikesHeart = style.div`
+    height: 48px;
+    width: 48px;
+    border: 1px solid gray;
+`;
+
+const PostBubble = style.div`
+     height: 48px;
+    width: 48px;
+    border: 1px solid gray;
+`;
+
+const LikesCount = style.div`
+     text-align: start;
+    width: 100%;
+    height: 20px;
+`;
+
+const Timestamp = style.div`
+    width: 100%;
+    height: 50px;
+    font-size:100;
+    display:block;
+`;
+
+
+
 
 const Post = (props) => {
     
 let timestamp = moment(props.post.timestamp, 'MMMM Do YYYY, h:mm:ss a').fromNow();
     
     return (
-        <div className="post-container">
-            <div className="post-header">
-                <img className="post-user-avatar" src={props.post.thumbnailUrl}/>
-                <div className="post-user">{props.post.username}</div>
-            </div>
-            <img className="post-img" src={props.post.imageUrl}/>
-            <div className="likes-container">
-                <div className="post-heart" onClick={() => { props.handleLike(props.post.timestamp) }}></div>
-                <div className="post-bubble"></div>
-            </div>
-            <div className="likes-count">{props.post.likes} likes</div>
+        <PostContainer>
+            <PostHeader>
+                <PostUserAvatar src={props.post.thumbnailUrl}></PostUserAvatar>
+                <PostUserHandle>{props.post.username}</PostUserHandle>
+            </PostHeader>
+            <PostImg src={props.post.imageUrl}></PostImg>
+            <LikesContainer>
+                <LikesHeart onClick={() => { props.handleLike(props.post.timestamp) }}></LikesHeart>
+                <PostBubble></PostBubble>
+            </LikesContainer>
+            <LikesCount>{props.post.likes} likes!</LikesCount>
             <CommentList comments={props.post.comments}/>
-            <div className="timestamp">{timestamp}</div>
-        </div>
+            <Timestamp>{timestamp}</Timestamp>
+        </PostContainer>
     )
 }
 
--- instagram/src/components/SearchBar/Search.js
+++ instagram/src/components/SearchBar/Search.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Search.css';
+import style from 'styled-components';
+
+const Header= style.header`
+    width: 100%;
+    height: 50px;
+    display: flex;
+    flex-flow: row;
+    justify-content: space-between;
+`;
+
+const LogoHeader = style.div`
+    width: 201px;
+    height: 48px;
+    display: flex;
+    flex-flow: row;
+    justify-content: center;
+    align-items: center;
+`;
+
+const LogoImage = style.img`
+    
+`;
 
 const Search = (props) => {
     
     return (
-        <div className="search-container">
-            <div className="logo">
+        <Header>
+            <LogoHeader>
                 logo
-            </div>
+            </LogoHeader>
             <div className="input-container">
             <form onSubmit={props.handleSearchSubmit}>
                 <input placeholder="Search names..." onChange={props.handleSearchString}/>
@@ -20,7 +42,7 @@ const Search = (props) => {
                 <div className="options-item"></div>
                 <div className="options-item"></div>
             </div>
-        </div>
+        </Header>
     )
 }
 
