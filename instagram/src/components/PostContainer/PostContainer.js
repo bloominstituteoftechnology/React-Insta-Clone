c538fc8ea421b0d822d4c091759a910a85c57f1b
codejoncode <jonathanjholloway@gmail.com>
2018-08-02 17:00:00 -0500

    post section completed no css needed

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,7 +1,8 @@
 import React from 'react'; 
 import './PostContainer.css';
 import PropTypes from 'prop-types';
-
+import {Posting, Likes, PostIcons, Icon, PostHeader,
+    ThumbNail, PostUserName,PostImage, FlipIcon} from './PostStyled.js'
 
 class PostContainer extends React.Component {
     constructor(props){
@@ -16,24 +17,34 @@ class PostContainer extends React.Component {
     handleLikeClick = () => {
         this.setState({likes: this.state.likes + 1}); 
     }
-
+    handleHover = event => {
+        event.target.style.cursor = 'pointer';
+    }
 
     render() {
 
         let likes;
         this.state.cmdCalled ? likes = this.state.likes : likes = 0; 
         return(
-            <div className = "posting">
-                <div className = "postHeader">
-                <img className ="thumbNail" src={this.props.thumbNail} alt="" />
-                <div className ="postUserName">{this.props.userName}</div>
-                </div>
-                <img className ="postImage" src={this.props.image} alt="" />
-               <div className = "postIcons">
-               <i className="far fa-heart" onClick = {this.handleLikeClick}></i> <i className="far fa-comment"></i>
-               </div>
-               <h3 className = "likes" >{likes} likes </h3>
-            </div>
+            
+            <Posting>
+                
+                <PostHeader>
+                {/* <img className ="thumbNail" src={this.props.thumbNail} alt="" /> */}
+                <ThumbNail src = {this.props.thumbNail} alt=""></ThumbNail>
+                {/* <div className ="postUserName">{this.props.userName}</div> */}
+                <PostUserName>{this.props.userName}</PostUserName>
+                </PostHeader>
+                
+                {/* <img className ="postImage" src={this.props.image} alt="" /> */}
+                <PostImage src={this.props.image} alt=""/>
+               <PostIcons>
+                    {/* <i className="far fa-heart" onClick = {this.handleLikeClick}></i> <i className="far fa-comment"></i> */}
+                    <Icon className ="far fa-heart" onMouseEnter ={this.handleHover} onClick = {this.handleLikeClick}></Icon><FlipIcon className= "far fa-comment"></FlipIcon>
+               </PostIcons>
+               {/* <h3 className = "likes" >{likes} likes </h3> */}
+               <Likes>{likes} likes</Likes>
+            </Posting>
 
         );
     }
---
+++ instagram/src/components/PostContainer/PostStyled.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components'; 
+
+const Posting = styled.div`
+    max-width: 100%; 
+`;
+
+const Likes = styled.h3 `
+    display:flex;
+    align-items: flex-start;
+    margin-top:12px; 
+    margin-bottom:15px;
+    margin-left: 2.04498%;
+    font-weight: bold;
+    font-size: 1.5rem;
+`;
+
+const PostIcons = styled.div`
+    display:flex;
+    align-items: flex-start;
+    margin-left: 2.04498%;
+    margin-top: 10px;
+    font-size: 2.5rem;
+    width: 50px; 
+`;
+
+const Icon = styled.i`
+    margin-right:40%;
+`;
+
+const PostHeader = styled.div `
+    display:flex;
+    align-items: flex-start;
+    width: 100px;
+    margin-top: 15px;
+    font-weight: bold;
+    font-size: 1.5rem;
+    margin-left: 2.04498%; 
+    text-align: center;
+    max-width: 100%;
+`;
+
+const ThumbNail = styled.img`
+    width: 30.06748%;
+    height: 2.04498%;
+    border-radius: 50%; 
+    margin-right: 2.04498%;
+    max-width: 100%;
+`;
+
+const PostUserName = styled.div`
+    margin-top: 5px; 
+`;
+
+const PostImage = styled.img`
+    max-width: 100%;
+    width: 600px; 
+    display:flex;
+    align-items:flex-start;
+    margin-top: 15px;
+`;
+
+const FlipIcon = styled.i`
+    transform: rotateY(180deg); 
+`;
+
+export{Posting, Likes, PostIcons, Icon, PostHeader,
+ThumbNail, PostUserName,PostImage, FlipIcon }
