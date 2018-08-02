8f06508127b0ce51b973fb46e06ff56da65a932b
szincone <sawyerzincone@gmail.com>
2018-08-02 16:39:48 -0600

    refractored postcontainer using styled comps

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -2,14 +2,20 @@ import React from "react";
 // import PropTypes from 'prop-types';
 import "./PostContainer.css";
 import CommentSection from "../CommentSection/CommentSection";
+import styled from "styled-components";
+
+const PostContainerDiv = styled.div`
+  margin: 6rem auto 2rem auto;
+  max-width: 600px;
+`;
 
 const PostContainer = props => {
   return (
-    <div className="postContainerDiv">
+    <PostContainerDiv className="postContainerDiv">
       {props.comments.map(comment => (
         <CommentSection comment={comment} key={comment.imageUrl} />
       ))}
-    </div>
+    </PostContainerDiv>
   );
 };
 
