694e9cb115a53db1d7c07c4cbcc7db6c06e5e40d
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-02 12:43:10 -0700

    refactor SearchBar.js to use styled components

--- insta-clone/src/components/SearchBar/SearchBar.js
+++ insta-clone/src/components/SearchBar/SearchBar.js
@@ -1,20 +1,60 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './SearchBar.css';
+import styled from 'styled-components';
+
+const SearchBarComponent = styled.div`
+    display: flex;
+    height: 50px;
+    margin: 0 auto;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 640px;
+    width: 100%;
+`
+
+const InstagramIcon = styled.div`
+    .icon {
+        font-size: 32px;
+    }
+    img {
+        width: 100px;
+    }
+`
+
+const SearchInput = styled.input`
+    border: 1px solid grey;
+    border-radius: 8px;
+    height: 60%;
+    text-align: center;
+    margin: 0;
+    &:focus {
+        outline: none;
+    }
+`
+
+const IconHolder = styled.div`
+    .icon {
+        font-size: 24px;
+        margin-right: 20px;
+        &:last-child {
+            margin-right: 0;
+        }
+    }
+`
 
 const SearchBar = (props) => {
     return(
-        <div className="search-bar">
-            <div className="instagramIcon">
+        <SearchBarComponent>
+            <InstagramIcon>
                 <FontAwesomeIcon icon={['fab', 'instagram']} className="icon" /> | <img src="https://3rockmarketing.com/wp-content/uploads/2016/09/instagram-logo.png" alt="instagram" className="instagram-text" />
-            </div>
-            <input type="text" placeholder="Search" onChange={props.onSearch} ></input>
-            <div className="icon-holder">
+            </InstagramIcon>
+            <SearchInput type="text" placeholder="Search" onChange={props.onSearch} ></SearchInput>
+            <IconHolder>
                 <FontAwesomeIcon icon={['far', 'compass']} className="icon" />
                 <FontAwesomeIcon icon={['far', 'heart']} className="icon" />
                 <FontAwesomeIcon icon={['far', 'user']} className="icon" />
-            </div>
-        </div>
+            </IconHolder>
+        </SearchBarComponent>
     );
 }
 
