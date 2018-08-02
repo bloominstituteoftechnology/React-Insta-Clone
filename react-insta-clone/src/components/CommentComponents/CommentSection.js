3c32453ae201c76c5bc10cb421b92cf7f808e6c1
sca4cs <shannon.atkinson.ca@gmail.com>
2018-08-02 18:57:40 -0400

    added timestamp with moment js

--- react-insta-clone/src/components/CommentComponents/CommentSection.js
+++ react-insta-clone/src/components/CommentComponents/CommentSection.js
@@ -1,11 +1,20 @@
 import React from "react";
+import moment from "moment";
 import Comment from "./Comment";
 import CommentInput from "./CommentInput";
+import styled from 'styled-components';
+
+const Timestamp =  styled.p`
+  color: gray;
+  font-size: 10px;
+  font-family: Sans-Serif;
+`;
 
 class CommentSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      timestamp: props.timestamp,
       comments: props.comments,
       comment: ""
     };
@@ -19,16 +28,17 @@ class CommentSection extends React.Component {
     e.preventDefault();
     const comments = this.state.comments.slice();
     comments.push({ text: this.state.comment, username: "anon" });
+    console.log(this.state.timestamp);
     this.setState({ comments, comment: "" });
     //what you define here is what is replaced in state
   }
 
-  //let moment = moment().startOf('hour').fromNow();
-
+ 
   render() {
     return (
       <div>
         {this.state.comments.map((c, i) => <Comment key={i} newComment={c} />)}
+        <Timestamp>{moment(this.state.timestamp, 'MMMM Do YYYY, h:mm:ss a').fromNow().toUpperCase()}</Timestamp>
         <CommentInput 
           add={this.addNewComment}
           comment={this.state.comment}
