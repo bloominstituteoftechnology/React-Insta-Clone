a8b139863c718b86268e0a3c8751eec21f159fa8
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-08-02 10:54:13 -0500

    Working on Sign up
    I started building a sign up section on the login which will have the same functionality as the login.

--- insta-app/src/components/Login/Login.js
+++ insta-app/src/components/Login/Login.js
@@ -1,47 +1,93 @@
 import React, { Component } from "react";
-
+import "./Login.css";
 class Login extends Component {
-    constructor(props){
-        super(props); 
-        this.state ={
-            username: "", 
-            password: "", 
-        };
-    }
-    handleLoginInput = e =>{
-        this.setState({[e.target.name]: e.target.value });
+  constructor(props) {
+    super(props);
+    this.state = {
+      signup:"",
+      signuppass:"", 
+      name: "",
+      email:"", 
+      username: "",
+      password: ""
     };
-    handleSubmitInput = e => {
-        const user = this.state.username; 
-        localStorage.setItem("user", user); 
-        window.location.reload();
-    }
+  }
+  handleLoginInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmitInput = e => {
+    const user = this.state.username;
+    localStorage.setItem("user", user);
+    window.location.reload();
+  };
 
-render(){
-    return(
-        <form className="login-box">
+  render() {
+    return (
+      <div className="signup-login-page">
+        <div>
+          <form className="login-box">
             <input
-            className="input-login"
-            type="text"
-            placeholder="Username"
-            name="username"
-            onChange={this.handleLoginInput}
-            value={this.state.username}
+              className="input-login"
+              type="text"
+              placeholder="                       Username"
+              name="username"
+              onChange={this.handleLoginInput}
+              value={this.state.username}
             />
-             <input
-            className="input-login"
-            type="text"
-            placeholder="Password"
-            name="password"
-            onChange={this.handleLoginInput}
-            value={this.state.password}
+            <input
+              className="input-login"
+              type="text"
+              placeholder="                       Password"
+              name="password"
+              onChange={this.handleLoginInput}
+              value={this.state.password}
             />
             <button onClick={this.handleSubmitInput} className="login-btn">
-            Log in
+              Log in
             </button>
-        </form> 
-    ); 
-}
+          </form>
+        </div>
+        {/* Only refrence what is above cause that works with the logging into the app */}
+        <div>
+          <form className="signup-box">
+          <input
+              type="text"
+              name="name"
+              className="sign-user"
+              placeholder="                        Full Name"
+              onChange={this.handleLoginInput}
+              value={this.state.name}
+            />
+            <input
+              type="text"
+              name="email"
+              className="sign-user"
+              placeholder="                        E-mail"
+              onChange={this.handleLoginInput}
+              value={this.state.email}
+            />
+             <input
+              type="text"
+              name="signup"
+              className="sign-user"
+              placeholder="                        Username"
+              onChange={this.handleLoginInput}
+              value={this.state.signup}
+            />
+            <input
+              type="text"
+              name="signuppass"
+              className="sign-user"
+              placeholder="                        Password"
+              onChange={this.handleLoginInput}
+              value={this.state.signuppass}
+            />
+            <button onClick={this.handleSubmitInput} className="signup-btn">Sign Up</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Login; 
+export default Login;
