f6868096d2311ec741203f928bec48a5f6fff0fd
szincone <sawyerzincone@gmail.com>
2018-08-02 16:36:35 -0600

    login refractored for styled comps

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,6 +1,53 @@
 import React from "react";
 import "./Login.css";
 import logoText from "../SearchBar/instagramLogoText.png";
+import styled from "styled-components";
+
+const LoginDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  max-width: 600px;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+const LoginImgDiv = styled.div`
+  width: 100%;
+`;
+
+const LoginDivImg = styled.img`
+  width: auto;
+  height: 50px;
+  padding: 5rem 5rem 0 5rem;
+`;
+
+const LoginForm = styled.form``;
+
+const LoginInput = styled.input`
+  padding: 0.3rem 3rem 0.3rem 0.8rem;
+  border-radius: 4px;
+  font-size: 1rem;
+  margin: 0 1rem 1rem 1rem;
+  border: 1px solid black;
+  color: black;
+`;
+
+const LoginButton = styled.button`
+  padding: 0.3rem 3rem;
+  border-radius: 4px;
+  border: none;
+  font-size: 1rem;
+  background: black;
+  color: white;
+  &:hover {
+    cursor: pointer;
+    background: white;
+    border: 1px solid black;
+    color: black;
+  }
+`;
 
 class Login extends React.Component {
   constructor(props) {
@@ -14,22 +61,25 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div className="loginDiv">
-        <div className="loginImgDiv">
-          <img src={logoText} alt="instagram text" />
-        </div>
-        <form className="loginForm">
-          <input
+      <LoginDiv className="loginDiv">
+        <LoginImgDiv className="loginImgDiv">
+          <LoginDivImg src={logoText} alt="instagram text" />
+        </LoginImgDiv>
+        <LoginForm className="loginForm">
+          <LoginInput
             className="usernameInput"
             placeholder="Username"
             onChange={this.state.loginChangeHandler}
           />
-          <input className="passwordInput" placeholder="Password" />
-          <button className="loginButton" onClick={this.state.loginHandler}>
+          <LoginInput className="passwordInput" placeholder="Password" />
+          <LoginButton
+            className="loginButton"
+            onClick={this.state.loginHandler}
+          >
             Submit
-          </button>
-        </form>
-      </div>
+          </LoginButton>
+        </LoginForm>
+      </LoginDiv>
     );
   }
 }
