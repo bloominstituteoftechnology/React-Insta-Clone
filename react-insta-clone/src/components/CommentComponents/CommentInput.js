a628ce73cccdbf8799e624fef5370283a311f3f8
sca4cs <shannon.atkinson.ca@gmail.com>
2018-08-02 18:13:45 -0400

    added styles to comment section

--- react-insta-clone/src/components/CommentComponents/CommentInput.js
+++ react-insta-clone/src/components/CommentComponents/CommentInput.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const CommentInputSection =  styled.section`
+  width: 100%;
+  border-top: 1px solid lightgray;
+  padding-top: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  
+`;
+
+const CommentInputStyles =  styled.input`
+  font-size: 16px;
+  line-height: 2.5;
+  border: none;
+`;
+
+const Ellipsis = styled.i`
+  color: gray;
+`;
 
 const CommentInput = props => {
   return (
+    <CommentInputSection>
     <form onSubmit={props.add} >
-      <input 
+      <CommentInputStyles 
         onChange={props.change}
         value={props.comment}
         type="text"
-        placeholder="Add comment... "
+        placeholder="Add comment..."
        />
     </form>
+    <Ellipsis className="fas fa-ellipsis-h"></Ellipsis>
+    </CommentInputSection>
   );
 };
 
