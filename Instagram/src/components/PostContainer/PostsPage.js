d6bf760ba6152deb4287d91311ccefe5653253dc
Arthur Bates <aabatesj@gmail.com>
2018-08-02 00:11:19 -0400

    Authenticate.js
    finished PostPage and Authenticate

--- Instagram/src/components/PostContainer/PostsPage.js
+++ Instagram/src/components/PostContainer/PostsPage.js
@@ -1,22 +1,45 @@
 import React from 'react';
-import PostsContainer from './components/PostContainer/Postscontainer';
-import SearchBar from './components/SearchBar/SearchBar';
+import dummyData from '../../dummy-data';
+import PostsContainer from './Postscontainer';
+import SearchBar from '../SearchBar/SearchBarContainer';
 
-const PostsPage = props => {
-    return (
-        <div className="App">
-            <SearchBar
-                searchTerm={this.state.searchTerm}
-                searchPosts={this.searchPostHandler}
-            />
-            <PostsContainer
-                posts={
-                    this.state.filteredPosts.length > 0
-                        ? this.state.filteredPosts
-                        : this.state.posts
-                }
-            />
-        </div>
-    );
+class PostsPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            posts: [],
+            filteredPosts: []
+        };
+    }
+    componentDidMount() {
+        this.setSate({ posts: dummyData });
+    }
+
+    searchPostHandler = e => {
+        const posts = this.stateposts.filter(p => {
+            if (p.username.includes(e.target.value)) {
+                return p;
+            }
+        });
+        this.setState({ filterPosts: posts });
+    };
+    render() {
+        return (
+            <div className="App">
+                <SearchBar
+                    searchTerm={this.state.searchTerm}
+                    searchPosts={this.searchPostHandler}
+                />
+                <PostsContainer
+                    posts={
+                        this.state.filteredPosts.length > 0
+                            ? this.state.filteredPosts
+                            : this.state.posts
+                    }
+                />
+            </div>
+        );
+    }
 }
 
+export default PostPage;
