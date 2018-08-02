1159c29681d3aa1c2d223f79cd0905e39918edf0
Charlie Winslow <cwinslow22@yahoo.com>
2018-08-02 16:03:45 -0500

    refactored the searchbar div wrapper into a styled component called Header

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './SearchBar.css';
+import { Header } from '../Styling.js';
 
 const SearchBar = props => {
     return (
-        <div className="search-bar-wrap" >
+        <Header >
             <span className="search-bar-left" >
                 <i className="fab fa-instagram fa-2x "></i>
             </span>
             <img src="https://raw.githubusercontent.com/LambdaSchool/React-Insta-Clone/4079cf6fb6d11bd3ad7a422cdec35e0aab4e2bfe/instagram/src/assets/iglogo.png" alt="" className="iglogo" />
             <input type="text" className="search-input" placeholder="&#8981; Search" onKeyDown={props.filterPostsHandler} />
             <span className="search-icons" onClick={props.logoutHandler} >
-                <i class="fas fa-skull fa-lg"></i>
+                <i class="fas fa-door-closed fa-lg"></i>
             </span>
             <span className="search-icons" >
                 <i className="far fa-compass fa-lg" />
@@ -21,7 +22,7 @@ const SearchBar = props => {
             <span className="search-icons">
                 <i className="far fa-user-circle fa-lg" />
             </span>
-        </div>
+        </Header>
 
     );
 };
---
+++ instagram/src/components/Styling.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+    border: 1px solid lightgray;
+    display: flex; 
+    justify-content: space-between; 
+    align-items: center; 
+    margin: auto; 
+    height: 50px; 
+`
