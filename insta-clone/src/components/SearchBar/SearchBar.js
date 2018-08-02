d20dbbbebd64977288e7e959584b44a9831e391e
Bryce Evans <bryceevans@205-185-99-110.directcom.com>
2018-08-02 16:24:59 -0600

    styled-components implemented for search bar.

--- insta-clone/src/components/SearchBar/SearchBar.js
+++ insta-clone/src/components/SearchBar/SearchBar.js
@@ -1,25 +1,59 @@
 import React from 'react';
 import './SearchBar.css';
 import logo from '../../assets/instagramlogin.png';
+import styled from 'styled-components';
 
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 150px;
+    border-bottom: 1px solid #D3D3D3;
+`;
+
+const DoubleLogosWrapper = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const ImgWrapper = styled.img`
+    padding-left: 20px;
+    height: 50px;
+`;
+
+const SearchBarWrapper = styled.div`
+    position: relative;
+`;
+
+const SearchBarSearch = styled.input`
+    width: 400px;
+    height: 50px;
+    text-align: center;
+    font-size: 25px;
+    color: grey;
+`;
+
+const SocialWrapper = styled.div`
+
+`;
 
 const SearchBar = props => {
     return (
-        <div className="search-bar-top">
-            <div className="logos-wrapper">
+        <Header>
+            <DoubleLogosWrapper>
                 <i className="fab fa-instagram"></i>
-                <img src = {logo} alt="logo" className="insta-words" />
-            </div>
-            <div className="search-bar-wrapper">
+                <ImgWrapper src = {logo} alt="logo" />
+            </DoubleLogosWrapper>
+            <SearchBarWrapper>
                 <span className="fa fa-search"></span>
-                <input type="text" placeholder="Search" className="search-bar" onKeyDown={props.searchPosts}/>
-            </div>
-            <div className="social-wrapper">
+                <SearchBarSearch type="text" placeholder="Search" className="search-bar" onKeyDown={props.searchPosts}/>
+            </SearchBarWrapper>
+            <SocialWrapper>
                 <i className="far fa-compass"></i>
                 <i className="far fa-heart"></i>
                 <i className="far fa-user"></i>
-            </div>
-        </div>
+            </SocialWrapper>
+        </Header>
     );
 };
 
