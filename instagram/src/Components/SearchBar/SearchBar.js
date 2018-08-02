77bb3a57aeca2b9a4399a01780b1748fdca12b65
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-08-02 13:37:24 -0600

    SearchBar Component css redone using styled-components/ reactstrap

--- instagram/src/Components/SearchBar/SearchBar.js
+++ instagram/src/Components/SearchBar/SearchBar.js
@@ -1,22 +1,44 @@
 import React from 'react'; 
 import "./SearchBar.css"; 
+import styled from 'styled-components'; 
+import {Form, Input} from 'reactstrap'; 
+
+//custom styled-components
+
+const Header = styled.header`
+    display: flex; 
+    justify-content: space-between; 
+    margin-top: 30px; 
+    padding-bottom: 10px;
+    border-bottom: 1px solid lightgrey; 
+`;
+
+const LogoHeader = styled.div`
+    display: flex; 
+`; 
+
+const NavigationIcons = styled.div`
+    display: flex; 
+    align-items: center; 
+`;
+
 
 const SearchBar = props => {
     return (
-        <div className = "searchBar">
-            <div className = "logo">
+        <Header>
+            <LogoHeader>
                 <i className="fab fa-instagram fa-2x"></i>
                 <img className = "insta-logo" src = {require("./insta-logo.png")} alt = "Instagram logo"/>
-            </div>
-            <form onSubmit = {props.submit} className = "search">
-                <input onChange = {props.change} className = "search-input fa fa-input" type = "search" placeholder = "&#xf002; Search"/>
-            </form>
-            <div className = "nav-icons">
+            </LogoHeader>
+            <Form onSubmit = {props.submit} className = "search">
+                <Input onChange = {props.change} className = "search-input fa fa-input" type = "search" placeholder = "&#xf002; Search"/>
+            </Form>
+            <NavigationIcons className = "nav-icons">
                 <i className="far fa-compass fa-2x"></i>
                 <i className="far fa-heart fa-2x"></i>
                 <i className="far fa-user fa-2x"></i>
-            </div>
-        </div>
+            </NavigationIcons>
+        </Header>
     )
 }
 
--- instagram/src/index.js
+++ instagram/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';  
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
