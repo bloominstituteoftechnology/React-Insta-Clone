394095653d9a75f273b55599dfc7554dd82ce205
kamalnrf <kamalnrf@hotmail.com>
2018-08-03 02:13:56 +0530

    Added Styled components to PostContainer

--- instagram/src/components/PostComponent/card.js
+++ instagram/src/components/PostComponent/card.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import './PostContainer.css';
 import CommentSection from './CommentComponent/CommentSection';
 import moment from 'moment';
+import Styled from 'styled-components' 
+
+const Post = Styled.div`
+    border: 1px solid grey;
+    padding: 20px 0;
+    max-width: 642px;
+    margin: 0 auto;
+`
+
+const UserName = Styled.p`
+    font-weight: bold;
+`
+
+const Bottom = Styled.div`
+    margin: 10px 20px;
+`
 
 class Card extends Component {
     constructor(props){
@@ -49,15 +65,15 @@ class Card extends Component {
 
     render() {
         return (
-            <div className="post" key={this.props.post.timestamp}>
+            <Post key={this.props.post.timestamp}>
                 <div className="header row">
                     <div className="col-sm-12 d-flex">
                         <img src={this.props.post.thumbnailUrl} alt="thumbnail of user" />
-                        <p className="username">{this.props.post.username}</p>
+                        <UserName>{this.props.post.username}</UserName>
                     </div>
                 </div>
                 <img src={this.props.post.imageUrl} alt="user's beautiful post" />
-                <div className="bottom">
+                <Bottom>
                     <div className="icons">
                         <i className="fas fa-heart" onClick={this.addLike}></i>
                         <i className="fas fa-comment"></i>
@@ -68,8 +84,8 @@ class Card extends Component {
                     <form onSubmit={this.addComment} className="add-comment">
                         <input type="text" placeholder="Add a comment" onChange={this.commentHolder} ></input>
                     </form>
-                </div>
-            </div>
+                </Bottom>
+            </Post>
         );
     }
 }
--- instagram/src/components/PostComponent/cardBanner.js
+++ instagram/src/components/PostComponent/cardBanner.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Post.css'
+import Styled from 'styled-components' 
+
+const UserName = Styled.p`
+    font-weight: bold;
+`
 
 function CardBanner (props){
     return (
         <div className="cardBanner">
             <img src='https://tk-assets.lambdaschool.com/ecd33d34-c124-4b75-92d2-e5c52c171ed8_11201517_887808411287357_1307163552_a.jpg' alt="userICon"/>
-            <div className="userName">philzcoffee</div>
+            <UserName>philzcoffee</UserName>
         </div>
     )
 }
