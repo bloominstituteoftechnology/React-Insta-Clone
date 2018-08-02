7661c0a01d60702050f4420dcf5b0ddee999e7d8
Charlie Winslow <cwinslow22@yahoo.com>
2018-08-02 16:30:36 -0500

    refactored all of SearchBar using styled-components
    no longer using any CSS in searchbar component

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import './SearchBar.css';
-import { Header } from '../Styling.js';
+import { SearchHeader, IGsymbol, IGlogo, SearchInput, SearchIcons } from '../Styling.js';
 
 const SearchBar = props => {
     return (
-        <Header >
-            <span className="search-bar-left" >
+        <SearchHeader >
+            <IGsymbol >
                 <i className="fab fa-instagram fa-2x "></i>
-            </span>
-            <img src="https://raw.githubusercontent.com/LambdaSchool/React-Insta-Clone/4079cf6fb6d11bd3ad7a422cdec35e0aab4e2bfe/instagram/src/assets/iglogo.png" alt="" className="iglogo" />
-            <input type="text" className="search-input" placeholder="&#8981; Search" onKeyDown={props.filterPostsHandler} />
-            <span className="search-icons" onClick={props.logoutHandler} >
+            </IGsymbol>
+            <IGlogo src="https://raw.githubusercontent.com/LambdaSchool/React-Insta-Clone/4079cf6fb6d11bd3ad7a422cdec35e0aab4e2bfe/instagram/src/assets/iglogo.png" alt="" />
+            <SearchInput type="text" placeholder="&#8981; Search" onKeyDown={props.filterPostsHandler} />
+            <SearchIcons onClick={props.logoutHandler} >
                 <i class="fas fa-door-closed fa-lg"></i>
-            </span>
-            <span className="search-icons" >
+            </SearchIcons>
+            <SearchIcons >
                 <i className="far fa-compass fa-lg" />
-            </span>
-            <span className="search-icons">
+            </SearchIcons>
+            <SearchIcons >
                 <i className="far fa-heart fa-lg" />
-            </span>
-            <span className="search-icons">
+            </SearchIcons>
+            <SearchIcons >
                 <i className="far fa-user-circle fa-lg" />
-            </span>
-        </Header>
+            </SearchIcons>
+        </SearchHeader>
 
     );
 };
--- instagram/src/components/Styling.js
+++ instagram/src/components/Styling.js
@@ -1,10 +1,31 @@
 import styled from "styled-components";
 
-export const Header = styled.header`
+export const SearchHeader = styled.header`
     border: 1px solid lightgray;
     display: flex; 
     justify-content: space-between; 
     align-items: center; 
     margin: auto; 
     height: 50px; 
-`
+`;
+
+export const IGsymbol = styled.span`
+    width: 188px;
+    border-right: 1px solid lightgray;
+`;
+
+export const IGlogo = styled.img`
+    width: 75px;
+    margin-left: 10px;
+`;
+
+export const SearchInput = styled.input`
+    text-align: center;
+    margin-left: 15%;
+    margin-right: 15%;
+`;
+
+export const SearchIcons = styled.span`
+    margin: 0 5px;
+    font-weight: 100;
+`;
