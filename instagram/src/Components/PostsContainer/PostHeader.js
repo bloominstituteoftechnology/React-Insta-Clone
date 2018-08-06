2375043010632879476ed31fd51336f12e1e40ee
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-08-06 16:52:14 -0500

    edit to PostHeader

--- instagram/src/Components/PostsContainer/PostHeader.js
+++ instagram/src/Components/PostsContainer/PostHeader.js
@@ -4,11 +4,12 @@ import './Posts.css';
 const PostHeader = props => {
     return (
         <div className="post-header">
-           <div className="post-header-thumbnail">
+            <div className="post-header-thumbnail">
              <img alt="post-header-thumb" className="post-header-img" src={props.thumbnailURL} />
-           </div>
-           <div className="post-header-user"></div>
-           {props.username}
+            </div>
+            <div className="post-header-user">
+               {props.username}
+            </div>
         </div>
     );
 };
