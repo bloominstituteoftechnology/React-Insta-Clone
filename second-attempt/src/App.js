649c951753d15291fcc5a010efb22b1619de206c
Arthur Bates <aabatesj@gmail.com>
2018-08-06 18:46:38 -0400

    Bates second-attempt
    Was working in the wrong branch most of the day.
    
    Data to display, no attention to styling yet.
    
    admittedly some help from my old old to get some things running

---
+++ second-attempt/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import dummyData from './dummy-data';
+import PostsContainer from './components/PostContainer/PostContainer';
+import SearchBar from './components/SearchBar/SearchBar';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: dummyData
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+        <PostsContainer posts={this.state.posts} />
+      </div>
+    );
+  }
+}
+
+export default App;
