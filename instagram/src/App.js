b9005807c0d73915ad27e77a95afdb40acbf267a
Cwright0428 <cwright0428@yahoo.com>
2018-08-07 11:59:13 -0500

    some refactors while following along with todays lecture

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data';
+import PostsContainer from './components/PostContainer/PostContainer';
+import SearchBar from './components/SearchBar/SearchBar';
+
 
 class App extends Component {
   constructor(){
@@ -13,10 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
