38ab4005b38231d568daa45fc8d0b536946c9c2d
Fred <sohnf90@yahoo.com>
2018-08-07 13:41:58 -0700

    Added componentDidMount to App

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -8,9 +8,14 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      posts: dummyData,
+      posts: []
     }
   }
+
+  componentDidMount() {
+    this.setState({posts: dummyData})
+  }
+
   render() {
     return (
       <div>
