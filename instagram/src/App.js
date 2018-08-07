e30fa6628879ffce126f865d21cf6d1dc4450555
bri79319 <bri79319@gmail.com>
2018-08-06 22:44:39 -0400

    changes to code

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data';
 import PostContainer from './components/PostContainer';
+import SearchBar from './components/SearchBar';
 
 class App extends Component {
   constructor() {
@@ -15,9 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Instagram</h1>
-        </header>
+        <SearchBar/>
         <PostContainer posts={this.state.dummyData} />
       </div>
     );
