6098b3c2fe5a39155ba74be43ac88bcc4806b0a2
JoCatherine Griggs <jocafrin@gmail.com>
2018-08-07 12:59:03 -0400

    added search bar

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import dummyData from './dummy-data';
 import PostContainer from './components/PostContainer/PostContainer'
+import SearchBar from './components/SearchBar/SearchBar'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      posts: dummyData
+      posts: dummyData,
+      filteredPosts: []
     };
   }
   render() {
     return (
       <div className="App">
+        <SearchBar />
         <PostContainer posts={this.state.posts} />
       </div>
     );
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,1 +1,28 @@
 import React from "react";
+import './SearchBar.css';
+
+const SearchBar = () => {
+    return (
+        <div className="searchbar-wrapper">
+            <div className="img-wrapper">
+
+            </div>
+            <div>
+                <input type="text" placeholder="Search" />
+            </div>
+            <div className="social-wrapper">
+                <div className="social">
+                    <i className="fa fa-compass" />
+                </div>
+                <div className="social">
+                <i className="fa fa-heart" />
+                </div>
+                <div className="social">
+                <i className="fa fa-user-circle"></i>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SearchBar;
