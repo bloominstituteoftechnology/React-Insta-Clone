a0e26297f22e34ffc9b08395f9848c5e129a31f9
vanceleon <vanceleon44@gmail.com>
2018-08-07 18:03:15 -0400

    search bar additions and styling

--- instagram/src/SearchBar/SearchBar.js
+++ instagram/src/SearchBar/SearchBar.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
+import './SearchBar.css';
 
-const SearchBar = (props) => {
-    return (
-        <div>testing component</div>
-    )
-    
-}
+const SearchBar = props => {
+  return (
+    <div className="search-bar-container">
+      <div className="social-left">
+        <i class="fa fa-instagram" aria-hidden="true" /> 
+        <span>|</span>
+        <img src="../intagram/public/pictures/Instagram_logo.png" alt="instagram-logo"/>
+      </div>
+      <div className="social-right">
+        <i class="fa fa-compass" aria-hidden="true" />
+        <i class="fa fa-heart" aria-hidden="true" />
+        <i class="fa fa-user" aria-hidden="true" />
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;
