155c744b379f2e2fc7c1c531e38411fd5512a36d
vanceleon <vanceleon44@gmail.com>
2018-08-08 17:54:40 -0400

    moved items over to PostsPage

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,45 +2,26 @@ import React, { Component } from "react";
 import "./App.css";
 // import PropTypes from "prop-types";
 import dummyData from "./dummy-data";
-import SearchBar from './SearchBar/SearchBar';
-import PostContainer from './PostContainer/PostContainer';
-import Authenticate from './Authentication/Authentication'
+import PostsPage from "./PostContainer/PostsPage";
+// import Authenticate from './Authentication/Authentication';
 
 
 console.log(dummyData);
 
+// const Authenticate = Authenticate(App);
+
+
+
+
+
+
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-      comments: props.comments,
-      
-
-    };
-  }
-
-
-  componentDidMount(){
-    console.log("CDM was invoked");
-    // this.setState();
-
-    this.setState({posts: dummyData});
-  }
-
-  commentHandler () {
-    //have state be for the comments 
-  }
-
-
-  // searchPo
-
+  
   render() {
     return (
       <div className="App">
-        <SearchBar />
-        <PostContainer posts={this.state.posts} />
-        <Authenticate />
+        <PostsPage />
       </div>
     );
   }
--- instagram/src/Authentication/Authentication.js
+++ instagram/src/Authentication/Authentication.js
@@ -1,9 +1,31 @@
 import React from "react";
+import dummyData from "../dummy-data";
 
-const Authenticate = App => {
-    return (
-        <div>testing</div>
-    )
-}
+
+
+
+// const Authenticate = App => {
+    class Authenticate extends React.Component{
+        constructor(props){
+            super(props);
+            this.state = {
+                username: props.username,
+            }
+        }
+        
+        authUser = () => {
+            
+
+            
+        }
+
+
+        render(){
+            return <App /> 
+        }
+    
+    }
+
+
 
 export default Authenticate;
