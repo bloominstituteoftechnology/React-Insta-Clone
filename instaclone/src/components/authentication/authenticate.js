480c28f4cc45ff3309ffdfe8042955d08ea45894
Jon Simmons <jonsimo@aol.com>
2018-08-09 09:17:24 -0400

    Rewrote Authenticate

--- instaclone/src/components/authentication/authenticate.js
+++ instaclone/src/components/authentication/authenticate.js
@@ -1,37 +1,54 @@
 import React from 'react';
 import Login from '../login/login';
 
+
 let Authenticate = App =>
-    class extends React.Component {
+    class extends Component {
         constructor(props) {
             super(props);
-        this.state = {
-            username: '',
-            password: '',
-        }
-        }
-
-    handleInputChange = (event) => {
-        this.setState({[event.target.name]:event.target.value})
-    }    
-
-login = (event) => {
-    event.preventDefault();
-    window.localStorage.setItem('username', this.state.username);
-    window.location.reload(true);
-}
-
-    render() {
-        if (window.localStorage.username) {
-            return <App />
-        }else {
-            return <Login handleInputChange = {this.handleInputChange} login = {
-                this.login
+            this.state={
+                loggedin: false
             }
-            />
+        }
+    loginTrue(){
+        if(this.state.loggedin){
+            return <Page user={ JSON.parse(localStorage.getItem('userName'))}/>
+        }
+        else{
+            return <Login  methods={[this.login.bind(this), this.userNameChange.bind(this)]}  />
         }
     }
-
-}
-
+    userNameChange(event){
+        this.setState({ userName: event.target.value },()=>{});
+     }
+     login(event){
+        if(this.state.userName !== undefined){
+            this.writeToLS(this.state.userName)
+        }
+     }
+     writeToLS = (passin) =>{
+       localStorage.setItem('userName', JSON.stringify(passin));
+     }
+    componentDidMount(){ 
+        if (localStorage.getItem('userName')!= null){
+            let output = localStorage.getItem('userName');
+            if(output !== ''){
+                this.setState({loggedin:true})
+            }
+           
+        }
+        else{
+            localStorage.setItem('userName', ''); 
+        }   
+      
+    }
+    render() {
+        return (
+            <div>
+                {this.loginTrue()}
+                
+            </div>
+        );
+    }
+    }
 export default Authenticate;
