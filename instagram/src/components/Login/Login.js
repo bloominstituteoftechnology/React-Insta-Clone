0a26d8705665aea4cf8cc95e4d9c1794b3597814
Noah-Amar <ne.amar@outlook.com>
2018-08-09 16:02:12 -0500

    login form

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-const Login = (props) => {
-    return (
-        <div>
-        THis is login
-        </div>
-      );
-}
- 
-export default Login;
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username:"",
+            password:""
+         }
+    }
+    render() { 
+        return ( 
+            <div>
+                <Form inline>
+        <FormGroup>
+          <Label for="exampleUsername" hidden>Email</Label>
+          <Input type="username" name="username" id="exampleUsername" placeholder="Username" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword" hidden>Password</Label>
+          <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+            </div>
+         );
+    }
+};
