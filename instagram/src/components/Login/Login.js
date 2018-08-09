a8ec91d650514d8cea137d03d32b2cbda1b4a913
Noah-Amar <ne.amar@outlook.com>
2018-08-09 17:07:30 -0500

    added Login functionality

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,29 +1,64 @@
-import React, {Component} from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 export default class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username:"",
-            password:""
-         }
-    }
-    render() { 
-        return ( 
-            <div>
-                <Form inline>
-        <FormGroup>
-          <Label for="exampleUsername" hidden>Email</Label>
-          <Input type="username" name="username" id="exampleUsername" placeholder="Username" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="examplePassword" hidden>Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="Password" />
-        </FormGroup>
-        <Button>Submit</Button>
-      </Form>
-            </div>
-         );
-    }
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleChange = event => {
+    console.log(this.state.username);
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    localStorage.setItem("auth", `${this.state.username} is logged in`);
+    this.setState({
+      username: "",
+      password: ""
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Form inline onSubmit={this.handleSubmit}>
+          <FormGroup>
+            <Label for="exampleUsername" hidden>
+              Email
+            </Label>
+            <Input
+              type="text"
+              name="username"
+              value={this.state.username}
+              id="exampleUsername"
+              placeholder="Username"
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword" hidden>
+              Password
+            </Label>
+            <Input
+              type="password"
+              name="password"
+              value={this.state.password}
+              id="examplePassword"
+              placeholder="Password"
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <Button>Submit</Button>
+        </Form>
+      </div>
+    );
+  }
+}
--- instagram/src/components/SearchBar/SearchBar.jsx
+++ instagram/src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const SearchBar = () => {
     return ( 
         <div>
-            SearchBar
+            <Form inline>
+        <FormGroup>
+          <Label for="exampleSearch" hidden></Label>
+          <Input type="search" name="search" id="exampleSearch" placeholder="Search" />
+        </FormGroup>
+        </Form>
         </div>
      );
 }
