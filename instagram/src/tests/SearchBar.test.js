0d4b23d0cfed50d49fb1d1bb919ccf68113eddbd
JustinSHong <jhong2009@gmail.com>
2018-08-18 16:58:50 -0700

    SearchBar.test.js: checked to see handleSearch() updates state

--- instagram/src/tests/SearchBar.test.js
+++ instagram/src/tests/SearchBar.test.js
@@ -11,13 +11,33 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe("<SearchBar />", () => {
 	const search = jest.fn();
+	const tree = renderer.create(<SearchBar search={search} />);
 	it("should render without crashing", () => {
-		const tree = renderer.create(<SearchBar search={search} />);
 		expect(tree).toMatchSnapshot();
 	});
 
 	it("should render an input tag", () => {
 		const component = shallow(<SearchBar search={search} />);
 		expect(component.find("input").length).toBe(1);
+	});
+
+	it("should have an input that changes", () => {
+		const component = shallow(<SearchBar search={search} />);
+		const input = component.find("input");
+		const profile = "google";
+		expect(input.value).toBe(undefined);
+		input.value = profile;
+		expect(input.value).toEqual("google");
+	});
+
+	it("should call handleSearch to change state", () => {
+		const handleSearch = jest.fn();
+		const component = shallow(<SearchBar search={search} />);
+		const input = component.find("input");
+		const event = { target: { name: `${input.props().name}`, value: "abc" } };
+
+		expect(component.state("user").length).toBe(0);
+		component.instance().handleSearch(event);
+		expect(component.state("user")).toEqual("abc");
 	});
 });
