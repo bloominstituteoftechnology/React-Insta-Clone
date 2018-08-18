1eec02d438951af1f948eaa732eb50f58f2aef58
JustinSHong <jhong2009@gmail.com>
2018-08-17 22:34:15 -0700

    removed unneeded imports and console.logs

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import dummyData from "./dummy-data";
 // import SearchBar from './SearchBar/SearchBar.js';
@@ -15,28 +14,25 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    // console.log('rendered');
     this.setState({ dummyList: dummyData });
   };
 
   search = (username, event) => {
     // returns posts of usernames that user searched for
-    // alert('made it');
-    const searchResult = this.state.dummyList.filter((post) => {
+    const searchResult = this.state.dummyList.filter(post => {
       return post.username === username;
     });
     if (searchResult.length === 0) {
       // if no results were found OR input field is empty
       // "refresh the page" to the previous state
-      this.setState({dummyList: dummyData});
+      this.setState({ dummyList: dummyData });
     } else {
       // renders App to reflect search result
       this.setState({ dummyList: searchResult });
     }
-  }
+  };
 
   render() {
-    // console.log(this.state.dummyList);
     return (
       <div className="App">
         <SearchBar search={this.search} />
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,31 +1,37 @@
 import React from "react";
 import CommentSection from "../CommentSection/CommentSection";
 import "./PostContainer.css";
-import { Jumbotron } from 'reactstrap';
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle } from "reactstrap";
 
 const PostContainer = props => {
-	// alert('made it!');
-	// console.log('dummy datalist: ', props.dummy);
-  return (
-  	<div className="postWrapper">
-  		{props.dummy.map((post, index) => 		
+	return (
+		<div className="postWrapper">
+			{props.dummy.map((post, index) => (
 				<Card className="card" key={index}>
-						<div className="cardHeader">
-							<img src={post.thumbnailUrl} alt="thumbnail logo"/>
-							<CardTitle key={index}>{post.username}</CardTitle>
-						</div>
-					<CardImg className="cardImg" src={post.imageUrl} placeholder={post.username}/>
+					<div className="cardHeader">
+						<img src={post.thumbnailUrl} alt="thumbnail logo" />
+						<CardTitle key={index}>{post.username}</CardTitle>
+					</div>
+					<CardImg
+						className="cardImg"
+						src={post.imageUrl}
+						placeholder={post.username}
+					/>
 					<CardBody className="cardBody">
-						<div> <i className="far fa-heart"></i><i className="far fa-comment"></i></div>
-						<div><strong>{post.likes} likes</strong></div>
+						<div>
+							{" "}
+							<i className="far fa-heart" />
+							<i className="far fa-comment" />
+						</div>
+						<div>
+							<strong>{post.likes} likes</strong>
+						</div>
 						<CommentSection key={index} commentList={post.comments} />
 					</CardBody>
 				</Card>
-  		)}
-  	</div>
-  );
-}
+			))}
+		</div>
+	);
+};
 
 export default PostContainer;
