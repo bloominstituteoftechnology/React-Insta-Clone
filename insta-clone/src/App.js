340234c24f5c5e9a3d26d9d77079a0f8bc9face8
m-j-hacker <mjhacker@gmail.com>
2018-08-31 16:20:43 -0500

    created component PostList

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data.js';
 import SearchBar from './components/SearchBar/SearchBar';
@@ -8,7 +7,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      postData: []
+      postData: dummyData
     };
   }
 
---
+++ insta-clone/src/components/PostContainer/PostList.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+//this component will receive data from the state and feed it into
+//Post container
