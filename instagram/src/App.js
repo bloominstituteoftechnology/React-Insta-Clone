2ad69132e7f1d74c4f7befadf6e0665d1a7a0f2c
lyang9 <lyang9@buffalo.edu>
2018-08-31 19:53:37 -0400

    Created components directory and three sub-directories

---
+++ instagram/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import animals from "./animalsData";
+
+class App extends Component {
+ 
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title"></h1>
+        </header>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
