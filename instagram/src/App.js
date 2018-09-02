86eb4ec2acf2b2e764c6c3a23f1d523f63cc80d1
marshall lanners <marshalllanners@marshalls-MacBook-Air.local>
2018-09-01 23:26:31 -0700

    finied all mvp for monday

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import dummyData from './dummy-data';
 import PostList from './components/PostContainer/PostList';
+import SearchBar from './components/SearchBar/SearchBar';
+
 import './index.css';
 
 console.log(dummyData);
@@ -9,6 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
+      	<SearchBar />
         <PostList dataList={dummyData} />
       </div>
     );
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import insta from './insta.png';
+import square from './square.png';
+import person from './person.jpg';
+import compas from './compas.png';
+import h from './h.png';
+
+import './search.css';
+
+const SearchBar = (props) => {
+	return (
+		<div>
+			<div className="flex-logos-main">
+				<div className="flex-logos">
+					<img src={square} alt={"logo-icon"} className="square"/>
+					<hr className="logo-hr"/>
+					<img src={insta} alt={"instagram"} className="main-logo"/>
+				</div>
+				<input type="text" value="Search"/>
+				<div className="flex-logos2">
+					<img src={compas} className="logo-images"/>
+					<img src={h} className="logo-images"/>
+					<img src={person} className="logo-images"/>
+				</div>
+			</div>
+			<hr className="hr-search-1"/>
+			<hr className="hr-search-2"/>
+		</div>
+
+	)
+}
+
+export default SearchBar;
