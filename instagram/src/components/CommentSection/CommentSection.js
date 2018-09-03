519eed5807f553465901d8af1942d05fd7be573a
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-09-03 15:56:05 -0400

    All data is displaying, need to add styling and proptypes

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -2,8 +2,15 @@ import React from 'react';
 import Comment from './Comment';
 
 function CommentSection(props) {
-  console.log(props.comments);
-  return null;
+  // console.log(props.comments);
+  return (
+    <div className="comment-section">
+      {props.comments.map((comment) => (
+        <Comment comment={comment} key={comment.text} />
+      ))}
+      <input value="" placeholder="Add a comment..." />
+    </div>
+  );
 }
 
 export default CommentSection;
