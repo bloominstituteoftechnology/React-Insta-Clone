de13d642076f945c0893fde77336199091089964
mparedes003 <michelle.g.paredes@gmail.com>
2018-09-03 15:53:06 -0700

    Got logo.js to display on the screen.

---
+++ instagram/src/components/SearchBar/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './instagram-logo.png'
+
+const Logo = () => {
+  return ( 
+    <div>
+       <img src={logo} className="App-logo" alt="instagram-logo" />
+    </div>
+   );
+}
+ 
+export default Logo;
---
+++ instagram/src/components/SearchBar/searchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SearchBar.css';
+import Logo from './Logo';
+
+
+const SearchBar = () => {
+  return ( 
+      <div>
+
+       <Logo />
+
+      </div> 
+    );
+}
+ 
+export default SearchBar;
