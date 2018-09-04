61e828e28838f4753c6f0e5d442920ed81a2f35b
marshall lanners <marshalllanners@marshalls-MacBook-Air.local>
2018-09-04 09:46:08 -0700

    small change

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import Authenticate from './components/Authentication/Authenticate';
-import Login from './components/Login/Login'
+import PostsPage from './components/PostContainer/PostsPage';
 
 import './index.css';
 
 class App extends Component {
   render() {
-    return (<div className="container"><Login /></div>)
+    return (<div className="container"><PostsPage /></div>)
   }
 }
 
 export default Authenticate(App);
-
-// <PostsPage />
 
 
 
