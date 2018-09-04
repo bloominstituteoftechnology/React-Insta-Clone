03d06ddef27d97f34ade7e609bd8a2dec0e54412
alejandrok93 <alejandrok93@gmail.com>
2018-09-04 16:11:07 -0500

    added search bar styling

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -4,8 +4,10 @@ import "./SearchBar.css";
 const SearchBar = props => {
   return (
     <div className="search-bar-header">
-      <div className="logo" />
-      <input type="text" className="search-bar" />
+      <div className="logo">
+        {/* <img src="./instagram-log.png" alt="instagram logo" /> */}
+      </div>
+      <input type="text" placeholder="Search" className="search-bar" />
       <div className="account-stuff" />
     </div>
   );
