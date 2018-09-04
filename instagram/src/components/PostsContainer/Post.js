56c1863cbea49cb57e83a1d950ff0bcee45a6f88
lyang9 <lyang9@buffalo.edu>
2018-09-04 18:26:19 -0400

    Completed Post.js

---
+++ instagram/src/components/PostsContainer/Post.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Posts.css';
+import PostHeader from './PostHeader';
+import CommentSection from '../CommentSection/CommentSectionContainer';
+
+const Post = props => {
+    return (
+        <div className='post-border'>
+            <PostHeader 
+                username={props.post.username}
+                thumbnailUrl={props.post.thumbnailUrl}
+            />
+            <div className='post-image-wrapper'>
+                <img
+                    alt='post thumbnail'
+                    className='post-image'
+                    src={props.post.imageUrl}
+                />
+            </div>
+            <CommentSection comments={props.post.comments} />
+        </div>
+    );
+};
+
+export default Post;
