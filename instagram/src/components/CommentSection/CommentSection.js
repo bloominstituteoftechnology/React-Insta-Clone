27758e4356fc15226a8dece8bc61a99f7ac7adb7
lyang9 <lyang9@buffalo.edu>
2018-09-04 18:25:40 -0400

    Completed CommentSection.js

---
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Comment from './Comment';
+import CommentInput from './CommentInput';
+
+class CommentSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Comments: props.comments
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.comments.map((c, i) => <Comment key={i} comment={c} />)}
+                <CommentInput />
+            </div>
+        );
+    }
+}
+
+CommentSection.PropTypes = {
+    comments: PropTypes.arrayOf(
+        PropTypes.shape({ text: PropTypes.string, username: PropTypes.string })
+    )
+};
+
+export default CommentSection;
