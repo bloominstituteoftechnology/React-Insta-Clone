29d246f6b9242d1070011565a7485765d07fdaea
mparedes003 <michelle.g.paredes@gmail.com>
2018-09-03 17:03:42 -0700

    Got fontawesome icons in ThreeIcons.js to display on screen . Initially Styled SearchBar.js

---
+++ instagram/src/components/SearchBar/ThreeIcons.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const TreeIcons = () => {
+  return ( 
+    <div className="three-icons">
+      <section>
+       <i className="far fa-compass"></i>
+      </section>
+
+      <section>
+      <i class="far fa-heart"></i>
+      </section>
+
+      <section>
+      <i class="far fa-user"></i>
+      </section>
+
+    </div>
+   );
+}
+ 
+export default TreeIcons;
--- instagram/src/components/SearchBar/searchBar.js
+++ instagram/src/components/SearchBar/searchBar.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './SearchBar.css';
 import Logo from './Logo';
 import SearchInput from './SearchInput';
+import ThreeIcons from './ThreeIcons';
 
 
 const SearchBar = () => {
   return ( 
-      <div>
+      <div className="search-bar">
 
        <Logo />
        <SearchInput />
+       <ThreeIcons />
 
       </div> 
     );
