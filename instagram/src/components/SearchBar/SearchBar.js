8654e58c5950531250357f076fa76f4249464ab2
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-04 12:14:47 -0400

    Added header images

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import instaLogo from '../../assets/insta-logo.jpg';
+import './SearchBar.css';
 
-function SearchBar(props) {
+
+const SearchBar = () => {
     return (
-        <div>
-            SearchBar
+        <div className="header-container">
+            <div className="instagram-logos">
+                <img alt="instagram logo" src= {instaLogo} className="insta-logo" />
+                <img alt="instagram logo text" src="" className="text-logo" />
+            </div>
+            <div className="search-box">
+                <input type="text" placeholder="Search" />
+            </div>
+            <div className="icon-container">
+                <img src= "" className="nav-icon" />
+                <img src= "" className="heart-icon" />
+                <img src= "" className="user-icon" />
+            </div>
         </div>
     );
-}
+};
 
 export default SearchBar;
