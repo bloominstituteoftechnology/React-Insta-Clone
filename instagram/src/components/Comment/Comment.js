bd74a6d19ca4f96a319a48f381ecd141237edf1c
Max <maxkajiwara@gmail.com>
2018-09-04 12:31:27 -0700

    Convert CommentSection to class component instead (whoops)

--- instagram/src/components/Comment/Comment.js
+++ instagram/src/components/Comment/Comment.js
@@ -2,18 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Comment.css';
 
-class Comment extends React.Component {
-	render() {
-		return (
-			<div className="Comment">
-				<a href="/" className="username">
-					{this.props.comment.username}
-				</a>
-				<span className="comment-text">{this.props.comment.text}</span>
-			</div>
-		);
-	}
-}
+const Comment = props => {
+	return (
+		<div className="Comment">
+			<a href="/" className="username">
+				{props.comment.username}
+			</a>
+			<span className="comment-text">{props.comment.text}</span>
+		</div>
+	);
+};
 
 Comment.propTypes = {
 	comment: PropTypes.shape({
--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -2,22 +2,31 @@ import React from 'react';
 import Comment from '../Comment/Comment';
 import './CommentSection.css';
 
-const CommentSection = props => {
-	return (
-		<div className="CommentSection">
-			{props.comments.map(comment => (
-				<Comment
-					key={
-						comment.username +
-						': ' +
-						Date.now() +
-						Math.round(Math.random() * 1000)
-					}
-					comment={comment}
-				/>
-			))}
-		</div>
-	);
-};
+class CommentSection extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			comments: props.comments
+		};
+	}
+
+	render() {
+		return (
+			<div className="CommentSection">
+				{this.state.comments.map(comment => (
+					<Comment
+						key={
+							comment.username +
+							': ' +
+							Date.now() +
+							Math.round(Math.random() * 1000)
+						}
+						comment={comment}
+					/>
+				))}
+			</div>
+		);
+	}
+}
 
 export default CommentSection;
