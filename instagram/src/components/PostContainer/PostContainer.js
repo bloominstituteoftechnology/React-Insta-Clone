8b167a515a1cbb39b6bf87a0c766cb50a315ed21
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-03 22:08:13 -0400

    Fixed file path for comment section import

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import CommentSection from '/Users/caitlingiguere/web/instagram/src/components/CommentSection/CommentSection.js'
+import CommentSection from '../CommentSection/CommentSection.js'
 
 function PostContainer(props) {
     return (
