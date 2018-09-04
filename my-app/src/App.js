f046d551b3d4c4bf4995d28dd515830eb0f63ba3
Lucas Beemer <you@example.com>
2018-09-04 16:12:49 -0700

    my app clone

---
+++ my-app/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import dummyData from './dummy-data';
+import PostsContainer from './components/PostsContainer/PostsContainer';
+import SearchBar from './components/SearchBar/SearchBar';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      filteredPosts: []
+    };
+  }
+  componentDidMount() {
+    this.setState({posts: dummyData});
+  }
+searchPostsHandler = event => {
+  const posts = this.state.posts. filter(p => {
+    if (p.username.includes(event.target.value)) {
+      return p;
+    }
+  });
+  this.setState({filteredPosts: posts});
+}
+
+  render() {
+    return (
+      <div className="App">
+        <SearchBar
+        searchTerm={this.state.searchTerm}
+        searchPosts={this.searchPostsHandler} />
+        <PostsContainer posts={
+          this.state.filteredPosts.length > 0 
+          ? this.state.filteredPosts
+          : this.state.posts 
+          } 
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
