a1396d8737d3cd299cc62cdd6659b2eee6f5ad16
Joseph Chretien-Golden <joechre@gmail.com>
2018-09-04 20:14:41 -0500

    solution plus

---
+++ src/components/CommentSection/CommentInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommentInput = props => {
+  return (
+    <form>
+      <input type="text" placeholder="Add comment... " />
+    </form>
+  );
+};
+
+export default CommentInput;
