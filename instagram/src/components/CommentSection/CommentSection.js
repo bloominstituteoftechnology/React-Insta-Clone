8e31ac6b2ad26e6cbe76ae39fb0749b2d517c375
lyang9 <lyang9@buffalo.edu>
2018-09-05 02:27:35 -0400

    Completed addNewComment and handleChange

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -2,20 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Comment from './Comment';
 import CommentInput from './CommentInput';
+import Heart from '../../assets/heart.svg';
 
 class CommentSection extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            comments: props.comments
+            comments: props.comments,
+            newComment: ''
         };
+    }
+
+    addNewComment(event) {
+        event.preventDefault();
+        this.setState({
+            comments: [
+                ...this.state.comments,
+                {text: this.state.newComment, username: 'SomeUser'}
+            ],
+            newComment: ''
+        })
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
     }
 
     render() {
         return (
             <div className='comment-section'>
                 {this.state.comments.map((c, i) => <Comment key={i} comment={c} />)}
-                <CommentInput />
+                <CommentInput 
+                    addNewComment={this.addNewComment} 
+                    handleChange={this.addNewComment}
+                />
             </div>
         );
     }
