f4c47b0c643baf01c31d589c1853378670363cef
lyang9 <lyang9@buffalo.edu>
2018-09-04 23:11:52 -0400

    Added class on form and input

--- instagram/src/components/CommentSection/CommentInput.js
+++ instagram/src/components/CommentSection/CommentInput.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const CommentInput = props => {
     return (
-        <form>
-            <input type='text' placeholder='Add comment...' />
+        <form className='comment-input'>  
+            <input type='text' class='comment-control' placeholder='Add comment...' />
         </form>
     );
 };
