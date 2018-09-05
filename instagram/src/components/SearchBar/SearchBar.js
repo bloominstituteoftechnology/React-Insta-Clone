f294d0ce5ef42050f0ecab84d03a07ed158ec0e7
lyang9 <lyang9@buffalo.edu>
2018-09-04 20:56:38 -0400

    Added instagram logo using font-awesome

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -6,25 +6,25 @@ const SearchBar = () => {
     return (
         <div className='search-bar-wrapper'>
             <div className='image-wrapper'>
-                <i class="fab fa-instagram"></i>
+                <i class='fab fa-instagram fa-2x'></i>
                 <img alt='instagram logo' src={IGLogo} className='logo-image' />
             </div>
 
-            <div>
-                <input type='text' placeholder='Search' />
+            <div className='search-bar'>
+                <input type='text' class='form-control' placeholder='Search' />
             </div>
 
             <div className='social-wrapper'>
                 <div className='social'>
-                    <i class="far fa-compass"></i>
+                    <i class='far fa-compass'></i>
                 </div>
 
                 <div className='social'>
-                    <i class="far fa-heart"></i>
+                    <i class='far fa-heart'></i>
                 </div>
 
                 <div className='social'>
-                    <i class="far fa-user"></i>
+                    <i class='far fa-user'></i>
                 </div>
             </div>
         </div>
