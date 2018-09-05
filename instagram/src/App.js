07c7a3554ae7c3db96ead37c21ffc564284ad54d
Keiran <keirankozlowski@gmail.com>
2018-09-05 16:25:53 -0400

    Added Authentication class and imported into App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -4,6 +4,7 @@ import dummyData from './dummy-data';
 import SearchBarContainer from './components/SearchBar/SearchBarContainer';
 import PostContainer from './components/PostContainer/PostContainer';
 import PostsPage from './components/PostContainer/PostsPage';
+import Authenticate from './components/Authentication/Authenticate';
 
 class App extends Component {
   constructor() {
