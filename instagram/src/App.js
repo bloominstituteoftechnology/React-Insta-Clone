280d8cf837b3f14271036e4128a6ef19d7125582
Csillag61 <csillagok@gmail.com>
2018-09-05 17:58:53 -0400

    Day 3 Debugged

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import PostsPage from "./components/PostContainer/PostsPage";
 import Authenticate from "./components/Authentication/Authenticate";
-import ReactDOM from "react-dom";
 
 class App extends Component {
   constructor(){
