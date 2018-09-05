03594f70e745e2718013f2cd693b50d7aa1f1144
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-09-05 16:21:43 -0400

    Moved content from App.js to PostsPage.js. Everything seems to function normally

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,68 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import dummyData from './dummy-data';
-import SearchBar from './components/SearchBar/SearchBar';
-import PostContainer from './components/PostContainer/PostContainer';
-import IGLogo from './assets/iglogo.png';
+import PostsPage from './components/PostContainer/PostsPage';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      posts: [],
-      searchInput: '',
-      allPosts: dummyData,
-    };
   }
-
-  componentDidMount() {
-    this.setState({ posts: dummyData });
-  }
-
-  handleSearchInput = (event) => {
-    this.setState({ searchInput: event.target.value });
-  };
-
-  search = (event) => {
-    event.preventDefault();
-    console.log('test search');
-    let newPosts = [...this.state.allPosts];
-    if (this.state.searchInput === '') {
-    } else
-      newPosts = newPosts.filter((post) => {
-        if (post.username === this.state.searchInput) {
-          return post;
-        }
-      });
-    this.setState({ posts: newPosts });
-  };
 
   render() {
-    console.log('App render');
-    console.log(this.increaseLikes);
-    console.log('----------------');
     return (
-      // console.log(this.state.dummyData),
-      <div className="App">
-        <div className="title-bar">
-          <div className="logo">
-            <i className="fab fa-instagram" />{' '}
-            <img src={IGLogo} className="logo-image" />
-          </div>
-          <SearchBar
-            search={this.search}
-            handleSearchInput={this.handleSearchInput}
-          />
-          <div className="other-icons">
-            <i className="far fa-compass" />
-            <i className="far fa-heart" />
-            <i className="far fa-user" />
-          </div>
-        </div>
-        {this.state.posts.map((post, index) => (
-          <PostContainer post={post} key={index} />
-        ))}
+      <div className="app">
+        <PostsPage />;
       </div>
     );
   }
