98b9a1e34ac24fa37bef4fd761ceae2594c06b26
lyang9 <lyang9@buffalo.edu>
2018-09-05 02:26:57 -0400

    Completed addNewComment and handleChange

--- instagram/src/components/CommentSection/CommentInput.js
+++ instagram/src/components/CommentSection/CommentInput.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CommentInput = props => {
     return (
-        <form className='comment-input'>  
+        <form className='comment-input' onSubmit={props.addNewComment} onChange={props.handleChange}>  
             <input type='text' class='comment-control' placeholder='Add comment...' />
         </form>
     );
