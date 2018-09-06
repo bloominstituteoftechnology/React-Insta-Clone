874cac5549e12e2df39df56841541e8fb93b12b7
decagondev <decagondevelopment@gmail.com>
2018-09-06 20:09:45 +0100

    (DAY 4 MVP : STYLES) : refactored the css for the input into a SearchBarComponent styled comoponent

--- instagram/src/components/Auth/Auth.jsx
+++ instagram/src/components/Auth/Auth.jsx
@@ -19,7 +19,7 @@ class extends React.Component {
       });
     }
   }
-
+  
   render() {
     if (this.state.authenticated) {
       return (
--- instagram/src/components/SearchBar/SearchBar.jsx
+++ instagram/src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,30 @@
 import React from "react";
-import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import { InputGroup } from "reactstrap";
+import styled from "styled-components";
 import "./SearchBar.css";
+const SearchBarComponent = styled.input`
+  height: 50px;
+  width: 220px;
+  border: 1px solid #ccc;
+  border-radius: 7px;
+  padding: 2px 15px;
+  margin: 0 10px;
 
+  ::placeholder {
+    text-align: center;
+    font-size: 14px;
+    color: #ccc;
+  }
+`;
 const SearchBar = props => {
   return (
     <div className="search-bar">
-       <InputGroup>
-      <input
-        className="search"
-        placeholder="Search"
-        type="text"
-        onChange={props.searchHandler}
-      />
+      <InputGroup>
+        <SearchBarComponent
+          placeholder="Search"
+          type="text"
+          onChange={props.searchHandler}
+        />
       </InputGroup>
     </div>
   );
