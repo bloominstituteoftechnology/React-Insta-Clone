119cc21ac050f77e3f9df64daf0f3bdd0b3e516f
Michael Agard <mdagard@gmail.com>
2018-09-06 18:31:54 -0400

    added styled components

--- vapagram/src/components/CommentSection/Comment.js
+++ vapagram/src/components/CommentSection/Comment.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Comment.css';
+import styled from 'styled-components';
+
+const CommentText = styled.div`
+  text-align: left;
+  padding: 2px;
+  margin-left: 4px;
+`;
+
+const CommentClass = styled.span`
+  font-weight: 300;
+  font-size: 14px;
+`;
+
+const UserClass = styled.span`
+  font-weight: 500;
+  font-size: 12px;
+`;
 
 const Comment = props => {
   return (
-    <div className="comment-text">
-      <span className="comment">{props.comment.text}</span>{' '}
-      <span className="user">-{props.comment.username}</span>
-    </div>
+    <CommentText>
+      <CommentClass>{props.comment.text}</CommentClass>{' '}
+      <UserClass>-{props.comment.username}</UserClass>
+    </CommentText>
   );
 };
 
