3f600c05e9b7bca8a121ece2a36d0800137fa212
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-09-06 17:43:38 -0400

    added post searching and ComponentDidMount to App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -8,15 +8,29 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      posts: dummyData
+      posts: [],
+      filteredPosts: []
     };
   }
+
+  componentDidMount() {
+    this.setState({ posts: dummyData });
+  }
+
+  searchPostsHandler = e => {
+    const posts = this.state.posts.filter(p => {
+      if (p.username.includes(e.target.value)) {
+        return p;
+      }
+    });
+    this.setState({ filteredPosts: posts });
+  };
 
   render() {
   	return (
   	    <div className = 'App'>
-  	      <SearchBar/>
-  	      <PostContainer posts = {this.state.posts} />
+  	      <SearchBar searchTerm = {this.state.searchTerm} searchPosts = {this.searchPostsHandler} />
+  	      <PostContainer posts = {this.state.filteredPosts.length > 0 ? this.state.filteredPosts : this.state.posts} />
         </div>
     );
   }
