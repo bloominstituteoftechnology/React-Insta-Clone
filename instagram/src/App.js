fe6ed76c36f3c9b434a62f5df07355f1e69114d5
lyang9 <lyang9@buffalo.edu>
2018-09-06 11:11:36 -0400

    Rendered PostsPage component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,43 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import dummyData from './dummy-data';
-import PostsContainer from './components/PostsContainer/PostsContainer';
-import SearchBar from './components/SearchBar/SearchBar';
+import PostsPage from './components/PostsContainer/PostsPage';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      posts: [],
-      search: ''
-    };
+    this.state = {};
   }
 
-  search = event => {
-    event.preventDefault();
-    let newPosts = [...this.state.posts];
-    newPosts = newPosts.filter(post => {
-      if (post.username === this.state.search) {
-        return post;
-      }
-    });
-    this.setState({ posts: newPosts });
-  };
-  
-  handleChange = event => {
-    event.preventDefault();
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  componentDidMount() {
-    this.setState({posts: dummyData});
-  }
- 
   render() {
     return (
       <div className="App">
-        <SearchBar search={this.search} handleChange={this.handleChange} />
-        <PostsContainer posts={this.state.posts} />
+        <PostsPage />
       </div>
     );
   }
