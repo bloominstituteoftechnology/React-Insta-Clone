bdddd40d993d32a84f033d679929e23985b5b5d9
Michael Agard <mdagard@gmail.com>
2018-09-06 18:38:45 -0400

    Added styled components

--- vapagram/src/components/PostsContainer/Post.js
+++ vapagram/src/components/PostsContainer/Post.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import CommentSection from '../CommentSection/CommentSectionContainer';
 import PostHeader from './PostHeader';
+import styled from 'styled-components';
 
 import './Posts.css';
-
+const PostBorder = styled.div`
+  border: 1px solid #d3d3d3;
+  margin: 5px 0;
+  box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
+`;
+const PostImageWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+`;
 const Post = props => {
   return (
-    <div className="post-border">
+    <PostBorder>
       <PostHeader
         username={props.post.username}
         thumbnailUrl={props.post.thumbnailUrl}
       />
-      <div className="post-image-wrapper">
+      <PostImageWrapper>
         <img
           alt="post thumbnail"
           className="post-image"
           src={props.post.imageUrl}
         />
-      </div>
+      </PostImageWrapper>
       <CommentSection comments={props.post.comments} />
-    </div>
+    </PostBorder>
   );
 };
 
