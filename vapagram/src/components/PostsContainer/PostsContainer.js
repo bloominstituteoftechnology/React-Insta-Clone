040aa38076c2a956b66fc614980d5336bd99b163
Michael Agard <mdagard@gmail.com>
2018-09-06 18:41:45 -0400

    Added styled component

--- vapagram/src/components/PostsContainer/PostsContainer.js
+++ vapagram/src/components/PostsContainer/PostsContainer.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Post from './Post';
 import './Posts.css';
+import styled from 'styled-components';
 
+const PostsContainerWrapper = styled.div`
+  height: 500px;
+  width: 50%;
+  margin: 5px auto;
+`;
 const PostsContainer = props => {
   return (
     <div className="posts-container-wrapper">
