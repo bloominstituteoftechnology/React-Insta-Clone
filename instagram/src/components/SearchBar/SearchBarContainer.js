99e547c26f6a8714aa6a1df4dfc6858a9f1b6a6e
Kevin Nguyen <kevinn858@gmail.com>
2018-09-06 14:47:42 -0700

    refactored SearchBar into styled components

--- instagram/src/components/SearchBar/SearchBarContainer.js
+++ instagram/src/components/SearchBar/SearchBarContainer.js
@@ -1,28 +1,54 @@
 import React from 'react';
 import IGLogo from '../../assets/iglogo.png';
-import './SearchBar.css';
+//import './SearchBar.css';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  border-bottom: 1px solid #3333;
+  height: 80px;
+  line-height: 70px;
+  display: flex;
+  justify-content: space-between;
+  background: #ffffff;
+`;
+
+const ImageWrapper = styled.div`
+  height: 50px;
+`;
+
+const LogoImage = styled.img`
+  height: 100%;
+`;
+
+const SocialWrapper = styled.div`
+  display: flex;
+`;
+
+const Social = styled.div`
+  width: 50px;
+`;
 
 const SearchBar = props => {
   return (
-    <div className="search-bar-wrapper">
-      <div className="image-wrapper">
-        <img alt="instagram logo" src={IGLogo} className="logo-image" />
-      </div>
+    <Header>
+      <ImageWrapper>
+        <LogoImage alt="instagram logo" src={IGLogo} className="logo-image" />
+      </ImageWrapper>
       <div>
         <input type="text" placeholder="Search" onKeyDown={props.searchPosts} />
       </div>
-      <div className="social-wrapper">
-        <div className="social">
+      <SocialWrapper className="social-wrapper">
+        <Social className="social">
           <i className="far fa-compass" />
-        </div>
-        <div className="social">
+        </Social>
+        <Social className="social">
           <i className="far fa-heart" />
-        </div>
-        <div className="social">
+        </Social>
+        <Social className="social">
           <i className="far fa-user-circle" />
-        </div>
-      </div>
-    </div>
+        </Social>
+      </SocialWrapper>
+    </Header>
   );
 };
 
