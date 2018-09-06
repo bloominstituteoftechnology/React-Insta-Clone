98a8e88a41a35a0b6dc167a7b2e4160af1314b9e
Casey Baker <caseye.baker@gmail.com>
2018-09-06 15:35:13 -0700

    switched to styled components on Search Bar and Posts Container

--- instagram/src/components/PostsContainer/PostsContainer.js
+++ instagram/src/components/PostsContainer/PostsContainer.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Post from './Post';
-import './Posts.css';
+//import './Posts.css';
+import styled, {css} from 'styled-components';
+
+const PostsContainerWrapper = styled.div`
+height: 500px;
+width: 30%;
+margin: 5px auto;
+`;
 
 const PostsContainer = props => {
   return (
-    <div className="posts-container-wrapper">
+    <PostsContainerWrapper>
       {props.posts.map(p => <Post key={p.imageUrl} post={p} />)}
-    </div>
+    </PostsContainerWrapper>
   );
 };
 
--- instagram/src/components/SearchBar/SearchBarContainer.js
+++ instagram/src/components/SearchBar/SearchBarContainer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import IGLogo from '../../assets/iglogo.png';
-import './SearchBar.css';
+//import './SearchBar.css';
 import styled, {css} from 'styled-components';
 
-const SearchBarWrapper = styled.header`
+const Header = styled.header`
 border-bottom: 1px solid #3333;
   height: 80px;
   line-height: 70px;
@@ -12,35 +12,44 @@ border-bottom: 1px solid #3333;
   background: #ffffff;
 `;
 
-const ImageWrapper = styled.div`
+const LogoHeader = styled.div`
   height: 50px;
+`;
+
+const LogoImage = styled.img`
+  height: 100%;
 `;
 
 const SocialWrapper = styled.div`
   display: flex;
 `;
 
+const SocialIcons = styled.div`
+  width: 50px;
+`;
+
 const SearchBar = props => {
   return (
-    <SearchBarWrapper>
-      <ImageWrapper>
-        <img alt="instagram logo" src={IGLogo} className="logo-image" />
-      </ImageWrapper>
+    <Header>
+      <LogoHeader>
+        <LogoImage src={IGLogo} />
+        {/* <img alt="instagram logo" src={IGLogo} className="logo-image" /> */}
+      </LogoHeader>
       <div>
         <input type="text" placeholder="Search" onKeyDown={props.searchPosts} />
       </div>
       <SocialWrapper>
-        <div className="social">
+        <SocialIcons>
           <i className="far fa-compass" />
-        </div>
-        <div className="social">
+        </SocialIcons>
+        <SocialIcons>
           <i className="far fa-heart" />
-        </div>
-        <div className="social">
+        </SocialIcons>
+        <SocialIcons>
           <i className="far fa-user-circle" />
-        </div>
+        </SocialIcons>
       </SocialWrapper>
-    </SearchBarWrapper>
+    </Header>
   );
 };
 
