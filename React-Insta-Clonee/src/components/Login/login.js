ba9c270e3ecd42ad0a4b34a4e0ae11a117450898
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-09-06 16:10:25 -0400

    Day IV styled searchbar after removing css and using styled components

--- React-Insta-Clonee/src/components/Login/login.js
+++ React-Insta-Clonee/src/components/Login/login.js
@@ -5,24 +5,26 @@ class Login extends React.Component{
     constructor(props) {
         super(props);
         this.state={
-            username: ''
+            username: '',
+            password: ''
             
         }
     }
 handleUserNameChange=(e)=> {
-    this.setState({username:e.target.value});
+    this.setState({[e.target.name]:e.target.value},()=>console.log(this.state.username));
 }
 handlePassWordChange=(e)=>{
-    this.setState({password:e.target.value});
+    this.setState({[e.target.name]:e.target.value},()=>console.log(this.state.password));
 }
 logIn=()=>{
     localStorage.setItem('username',this.state.username);
+    localStorage.setItem('password',this.state.password);
 }
 render () {
     return (
         <form className='login' onSubmit={this.login}>
-            <input type='text' placeholder='enter a username' value={this.state.username} onChange={(e)=>this.handleUserNameChange(e)}/>
-            <input type='password' placeholder='enter a password' value={this.state.password} onChange={(e)=>this.handlePassWordChange(e)}/>
+            <input type='text' name ='username' placeholder='enter a username' value={this.state.username} onChange={(e)=>this.handleUserNameChange(e)}/>
+            <input type='password' name = 'password' placeholder='enter a password' value={this.state.password} onChange={(e)=>this.handlePassWordChange(e)}/>
             <button className='btn'>Login</button>
         </form>
     )
--- React-Insta-Clonee/src/components/SearchBar/SearchBar.js
+++ React-Insta-Clonee/src/components/SearchBar/SearchBar.js
@@ -1,23 +1,50 @@
 import React from 'react';
 import './SearchBar.css';
+import styled from 'styled-components';
+
+const InstaLogo=styled.img`
+width: 200px
+height: 200px`;
+ const InstagramTradeMark=styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 20%;
+`;
+const SearchBarActual=styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    align-items: center;
+    height: 200px;
+    width: 100%;
+`
+const SearchBarInput=styled.div`
+    width: 20%;
+`
+const SearchBarIcons=styled.div`
+    width: 20%;
+    display: flex;
+    justify-content: space-evenly;
+`
 
  const SearchBar=props=>{
     return (
-        <div className='searchbar'>
-            <div className='instagramTrademark'>
-            <i className="fab fa-instagram fa-3x"></i>
-            <div className='vr'></div>
-            <img className='instalogo' src='http://assets.stickpng.com/thumbs/5a4e432a2da5ad73df7efe7a.png' alt='instagram logo'/>
-        </div>    
-        <div className='searchBarInput'>
+        <SearchBarActual>
+        <InstagramTradeMark>
+        <i className="fab fa-instagram fa-3x"></i>
+        <InstaLogo src='http://assets.stickpng.com/thumbs/5a4e432a2da5ad73df7efe7a.png' alt='instagram logo'/>  
+        </InstagramTradeMark>
+        <SearchBarInput>
         <input type='text' placeholder='&#x1F50D; Search' value={props.searchValue} onChange={(e)=>props.handleInputChange(e)}/>
-        </div>
-        <div className='searchBarIcons'>
+        </SearchBarInput>
+        <SearchBarIcons>
             <i className="far fa-compass"></i>
             <i className="far fa-heart"></i>
             <i className="far fa-user"></i>
-        </div>
-        </div>
+        </SearchBarIcons>
+        </SearchBarActual>
     )
 }
 export default SearchBar; 
