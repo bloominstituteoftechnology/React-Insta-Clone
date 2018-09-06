37b4d9b3930db2a70abf6ea06d1704e468918d72
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-09-06 17:28:12 -0400

    Comment Section Styling used style - component to style the comment section after removing the css added a timestamp

--- React-Insta-Clonee/src/components/CommentSection/Comment.js
+++ React-Insta-Clonee/src/components/CommentSection/Comment.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import './CommentSection.css';
+
 import PropTypes from 'prop-types';
-const Comment=prop=><p className='user-comment'><span>{prop.data.username}</span> {prop.data.text}</p>
+import styled from 'styled-components';
+
+const UserComment=styled.p`
+    text-align: left;
+    margin-left: 3%;
+    font-weight: 'Regular';
+`
+const Username=styled.span`
+    font-weight: bold;
+`
+const Userpost=styled.span`
+    font-weight: normal !important;
+`
+const DeleteButton=styled.i`
+    opacity:0;
+    &:hover{
+        opacity:1;
+    }
+`
+ const Comment=prop=><UserComment>
+<Username>{prop.data.username}</Username> <Userpost>{prop.data.text}</Userpost> {prop.data.username===prop.username?
+<DeleteButton onClick={()=>prop.commentDelete(prop.itemNumber)} className="fas fa-eraser"></DeleteButton>:null}
+</UserComment>
 
 Comment.propTypes={
     data: PropTypes.objectOf(PropTypes.string).isRequired
--- React-Insta-Clonee/src/components/CommentSection/CommentSection.js
+++ React-Insta-Clonee/src/components/CommentSection/CommentSection.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import Comment from './Comment.js';
 import PropTypes from 'prop-types';
-import './CommentSection.css';
 import AddCommentBar from './AddCommentBar.js';
+import moment from 'moment';
+import styled from 'styled-components';
 
+const Timestamp=styled.p`
+    text-align: left;
+    margin-left: 3%;
+    color: #9fabb7;
+    font-size: 12px;
+`
+const Rule=styled.div`
+    border: 1px solid #f5f5f5;
+    width: 95%;
+    margin: 0 auto;
+`
 class CommentSection extends React.Component {
     constructor(props) {
         super(props);
@@ -31,13 +43,22 @@ class CommentSection extends React.Component {
     handleInputChange=(e)=>{
         this.setState({newComment:e.target.value});
     }
+    dynamicDate=(date)=>{
+        let dynamicDate=date;
+        dynamicDate=date.split(' ');
+        let newDate='';
+        dynamicDate[1].split('').forEach(e=>{if (parseInt(e,10)>0){newDate+=e}});
+        dynamicDate[1]=newDate;
+        dynamicDate=new Date(dynamicDate.join(' '));
+        return moment(dynamicDate).fromNow();
+    }
 
     render() {
         return (
             <div>
                 {this.state.comments.map((e,i)=><Comment data={e} key={i}/>)}
-                <p className='time-stamp'>{this.state.timestamp}</p>
-                <div className='rule'></div>
+                <Timestamp>{this.dynamicDate(this.state.timestamp)}</Timestamp>
+                <Rule/>
                 <AddCommentBar value={this.state.newComment} inputHelper={this.handleInputChange} handleInputSubmit={this.addNewComment} length={this.state.comments.length}/>
             </div>
        
