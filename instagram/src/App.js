5518be4569de0eb9013b66adc24860b70ee9174b
Luisan <luisangelsc@gmail.com>
2018-09-05 18:20:10 -0600

    mvp Day III works

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,79 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PropTypes from 'prop-types';
-import dummyData from './dummy-data';
-import SearchBar from './components/SearchBar/SearchBar'
-import PostContainer from './components/PostContainer/PostContainer'
+import PostsPage from './components/PostContainer/PostsPage'
+import Authenticate from './components/Authentication/Authenticate'
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      entries: [],
-      filter: ""
-    }
-  }
-
-  componentDidMount(){
-    this.setState(
-      {entries: dummyData}
-    )
-    console.log(dummyData);
-  }
-
-  updateFilter = e => {
-    this.setState({
-      filter: e.target.value
-    })
-  }
-
-  filterEntries = e => {
-    e.preventDefault();
-    let new_entries = dummyData.filter(entry => JSON.stringify(entry).includes(this.state.filter))
-    this.setState({
-      entries: new_entries,
-      filter: ""
-    })
   }
 
   render() {
-    // dummyData.forEach(item =>
-    // console.log(item))
-    // dummyData.forEach(item =>
-    // console.log(typeof item))
-    // console.log(this.state.entries);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Animals: by Luisan</h1>
         </header>
-        <SearchBar
-          value={this.state.filter}
-          updateFilter={this.updateFilter}
-          filterEntries={this.filterEntries}
-        />
-          {this.state.entries.map(entry =>
-          <PostContainer entry={entry} key={entry.timestamp}/>
-        )}
-
+        <PostsPage />
       </div>
-
-
     );
   }
 }
 
-// App.propTypes = {
-//   entry: PropTypes.shape({
-//     comments: PropTypes.array.isRequired,
-//     imageUrl: PropTypes.number.isRequired,
-//     likes: PropTypes.number.isRequired,
-//     thumbnailUrl: PropTypes.string.isRequired,
-//     timestamp: PropTypes.string.isRequired,
-//     username: PropTypes.string.isRequired,
-//   }).isRequired
-// }
 
-export default App;
+
+
+export default Authenticate(App);
