9ae92750adb3b666b38e1adaaac62c76e5928edb
Michael Agard <mdagard@gmail.com>
2018-09-06 19:44:12 -0400

    fixed 100% width image wrapper

--- vapagram/src/components/PostsContainer/Post.js
+++ vapagram/src/components/PostsContainer/Post.js
@@ -8,7 +8,7 @@ const PostBorder = styled.div`
   margin: 5px 0;
   box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
 `;
-const PostImageWrapper = styled.div`
+const PostImageWrapper = styled.img`
   height: 100%;
   width: 100%;
 `;
@@ -20,13 +20,13 @@ const Post = props => {
         username={props.post.username}
         thumbnailUrl={props.post.thumbnailUrl}
       />
-      <PostImageWrapper>
-        <img
+      <div>
+        <PostImageWrapper
           alt="post thumbnail"
           className="post-image"
           src={props.post.imageUrl}
         />
-      </PostImageWrapper>
+      </div>
       <CommentSection comments={props.post.comments} />
     </PostBorder>
   );
