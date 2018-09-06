ac4d2eda22d94253a2f36939c0347e621c9ad742
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-09-06 19:26:07 -0400

    Styled SearchBar in Javascript Styled components

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -2,22 +2,65 @@ import React from 'react';
 import './SearchBar.css';
 import IGLogo from '../../assets/iglogo.png';
 import SearchForm from './SearchForm';
+import styled from 'styled-components';
 
 function SearchBar(props) {
   return (
-    <div className="search-bar">
-      <div className="logo">
-        <i className="fab fa-instagram" />{' '}
-        <img src={IGLogo} className="logo-image" alt="logo" />
-      </div>
+    <Header className="search-bar">
+      <LogoHeader>
+        <i className="fab fa-instagram" />
+        <LogoImage src={IGLogo} alt="logo" />
+      </LogoHeader>
       <SearchForm search={props.search} handleChange={props.handleChange} />
-      <div className="other-icons">
+      <SocialIcons>
         <i className="far fa-compass" />
         <i className="far fa-heart" />
         <i className="far fa-user" />
-      </div>
-    </div>
+      </SocialIcons>
+    </Header>
   );
 }
 
 export default SearchBar;
+
+const Header = styled.div`
+  display: flex;
+  width: 640px;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const LogoHeader = styled.div`
+  width: 25%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo-image {
+    width: 60%;
+    margin-top: 10px;
+  }
+
+  i {
+    font-size: 2rem;
+    border-right: 1px solid darkgray;
+    padding-right: 20px;
+  }
+`;
+
+const LogoImage = styled.img`
+  width: 60%;
+  margin-top: 10px;
+`;
+
+const SocialIcons = styled.div`
+  width: 25%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  i {
+    font-size: 1.5rem;
+    color: darkgray;
+  }
+`;
