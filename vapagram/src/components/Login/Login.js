b1bd2ca730bc07ac7558853e530037094c0d4ecd
Michael Agard <mdagard@gmail.com>
2018-09-06 11:04:13 -0400

    added comments

--- vapagram/src/components/Login/Login.js
+++ vapagram/src/components/Login/Login.js
@@ -3,18 +3,24 @@ class Login extends React.Component {
   constructor() {
     super();
     this.state = {
+      // Sets the state's username and password to an empty string.
       username: '',
       password: ''
     };
   }
-  setUsername = event => {
+  // Assigns an anonmyous function to the variable login.
+  login = event => {
+    // Stops the page refresh event from happening.
     event.preventDefault();
+    // Sets username into the web browser's storage.
+    // ? event.target.username.value
+    localStorage.setItem('Password', event.target.username.value);
   };
 
   render() {
     return (
       <div className="Login">
-        <form className="login">
+        <form className="loginForm">
           <input type="text" className="username" />
           <input type="text" className="password" />
         </form>
