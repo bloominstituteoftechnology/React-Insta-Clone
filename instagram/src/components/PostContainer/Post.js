346fb54f297f0d397845933fcdc6430ac140e3c0
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-09-06 17:37:45 -0400

    added LikeSection.js and updated Post.js

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
 import PostHeader from './PostHeader';
+import LikeSection from './LikeSection';
 import './PostContainer.css';
 
-const Post = props => {
-  return (
-    <div className = 'post-border'>
-      <PostHeader username = {props.post.username} thumbnailUrl = {props.post.thumbnailUrl} />
+const Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: props.post.likes;
+    };
+  }
 
-      <div className = 'post-img-wrapper'>
-        <img alt = 'Thumbnail' className = 'post-img' src = {props.post.imageUrl} />
-      </div>
+  incrementLike = () => {
+    this.setState(state => ({ likes: (state.likes += 1) }));
+  };
 
-      <CommentSection comments = {props.post.comments} />
-    </div> // post border
-  );
+  render() {
+    return (
+      <div className = 'post-border'>
+        <PostHeader username = {this.props.post.username} thumbnailUrl = {this.props.post.thumbnailUrl} />
+
+        <div className = 'post-img-wrapper'>
+          <img alt = 'Thumbnail' className = 'post-img' src = {this.props.post.imageUrl} />
+        </div>
+
+        <LikeSection incrementLike = {this.incrementLike} likes = {this.state.likes} />
+
+        <CommentSection postId = {this.props.post.imageUrl} comments = {this.props.post.comments} />
+      </div> // post border
+    );
+  }
+}
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    username: PropTypes.string,
+    thumbnailUrl: PropTypes.string,
+    imageUrl: PropTypes.string
+  }),
 };
 
 export default Post;
