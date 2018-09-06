292c0e9b9e37fff9dab30925ba2cd3d40cce1cdc
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-09-06 10:07:23 -0600

    Push up react and node files.

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -8,16 +8,19 @@ const Login = props => {
         <section className="login-container">
             <div className="login-form">
                 <h1>Login</h1>
+                
                 <form className="login-here" onSubmit={props.login}>
                     <div className="username">
                         <label htmlFor="username">Phone number, username, or email</label>
                         <input type="text" name="username" className="username" onChange={(e) => {props.updateUsername(e); props.disableLoginButton(e)}}/>
                     </div>
+
                     <div className="password">
                         <label htmlFor="password">Password</label>
                         <input type="password" name="password" className="password" onChange={props.updatePassword}/>
                     </div>
-                        <button className="login" disabled>Log in</button>
+
+                    <button className="login" disabled>Log in</button>
                 </form>
                 <p className="forgot-password">Forgot Password?</p>
             </div>
