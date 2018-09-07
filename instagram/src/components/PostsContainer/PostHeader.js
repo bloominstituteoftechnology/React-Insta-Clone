80364b10df11287f570f660e0a759ca89a3b9fc1
lyang9 <lyang9@buffalo.edu>
2018-09-06 22:29:27 -0400

    Imported styled-components and added styles

--- instagram/src/components/PostsContainer/PostHeader.js
+++ instagram/src/components/PostsContainer/PostHeader.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import './Posts.css';
+import styled from 'styled-components'; 
+
+const UserHeader = styled.div `
+    width: 600px;
+    height: 70px;
+    display: flex;
+    flex-direction: row;
+  `;
+
+const PostThumbWrapper = styled.div `
+    margin: 10px 20px;
+  `;
+
+const PostThumb = styled.img `
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+  `;
+
+const Username = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 600;
+  `;
 
 const PostHeader = props => {
   return (
-    <div className="post-header">
-      <div className="post-thumb-wrapper">
-        <img
-          alt="post header"
-          className="post-thumb"
+    <UserHeader>
+      <PostThumbWrapper>
+        <PostThumb
           src={props.thumbnailUrl}
+          alt="post header"
         />
-      </div>
-      <div className='username'>{props.username}</div>
-    </div>
+      </PostThumbWrapper>
+      <Username>{props.username}</Username>
+    </UserHeader>
   );
 };
 
