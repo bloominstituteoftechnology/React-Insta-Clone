0c2e6f59d37de5e42e28d5479793e7c41b771531
BrianARuff <thelonewolf198927@gmail.com>
2018-09-06 23:54:10 -0400

    styled-component PostPage

--- instagram/src/components/PostPage/PostPage.js
+++ instagram/src/components/PostPage/PostPage.js
@@ -3,7 +3,7 @@ import dummy_data from '../../dummy-data';
 import uuidv4 from 'uuid/v4';
 import SearchBar from '../SearchBar/SearchBar';
 import PostContainer from  '../PostContainer/PostContainer';
-import './PostPage.css';
+import styled from 'styled-components';
 
 class PostPage extends React.Component {
   constructor() {
@@ -34,19 +34,36 @@ class PostPage extends React.Component {
   render() {
     const {dummyData} = this.state;
     return (
-      <div className="App">
-        <button className="logout-button" onClick={this.logout}>Logout</button>
+      <AppContainer>
+      <LogoutButton onClick={this.logout}>Logout</LogoutButton>
         <SearchBar filterSearch={this.filterSearch} />
         {
-          dummyData.map(post => {
-            return (
-              <PostContainer key={uuidv4()} postData={post} />
-            )
-          })
-        }
-      </div>
+          dummyData.map(post => <PostContainer key={uuidv4()} postData={post} />)
+        }        
+      </AppContainer>
     );
   }
 }
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const LogoutButton = styled.button`
+  width: 100%;
+  border: 1px solid transparent;
+  font-size: 1.2rem;
+  background: lightcoral;
+  color: #fff;
+  font-weight: bold;
+  margin-top: 20px;
+  padding: 10px 20px;
+  border-radius: 3px;
+  cursor: pointer;
+  transition: 300ms ease-in-out;
+  &:hover {
+    box-shadow: 0 4px 7px rgba(0,0,0,0.25), 0 3px 3px rgba(0,0,0,0.22);
+  }
+`;
 
 export default PostPage;
