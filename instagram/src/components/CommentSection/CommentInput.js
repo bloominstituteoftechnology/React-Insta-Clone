bb6204df753fa2f021ad805c8a8293a6b877b34b
lyang9 <lyang9@buffalo.edu>
2018-09-06 23:30:59 -0400

    Imported styled-components and added styles

--- instagram/src/components/CommentSection/CommentInput.js
+++ instagram/src/components/CommentSection/CommentInput.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import styled from 'styled-components'; 
+
+const CommentForm = styled.form `
+        border-top: 1px solid lightgray;
+        width: 570px;
+        margin-left: 10px;
+        margin-top: 20px;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+    `;
+
+const CommentControl = styled.input `
+        margin-top: 20px;
+        width: 200px;
+        height: 20px;
+    `;
 
 const CommentInput = props => {
     return (
-        <form className='comment-input' onSubmit={props.addNewComment} onChange={props.handleChange}>  
-            <input type='text' class='comment-control' placeholder='Add comment...' />
-        </form>
+        <CommentForm onSubmit={props.addNewComment} onChange={props.handleChange}>  
+            <CommentControl type='text' placeholder='Add comment...' />
+        </CommentForm>
     );
 };
 
