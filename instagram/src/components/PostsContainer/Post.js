9932fbfb1fca490631242821060fcb0e7320dcec
lyang9 <lyang9@buffalo.edu>
2018-09-06 22:28:49 -0400

    Imported styled-components and added styles

--- instagram/src/components/PostsContainer/Post.js
+++ instagram/src/components/PostsContainer/Post.js
@@ -2,24 +2,39 @@ import React from 'react';
 import './Posts.css';
 import CommentSection from '../CommentSection/CommentSection';
 import PostHeader from './PostHeader';
+import styled from 'styled-components';
 
+const PostBorder = styled.div `
+    border: 1px solid lightgray;
+    width: 600px;
+    height: 880px;
+    margin: 40px auto;
+  `;
+
+const PostImageWrapper = styled.div `
+    width: 600px;
+  `;
+
+const PostImage = styled.img `
+    width: 600px;
+    height: 600px;
+  `;
 
 const Post = props => {
   return (
-    <div className="post-border">
+    <PostBorder>
       <PostHeader
         username={props.post.username}
         thumbnailUrl={props.post.thumbnailUrl}
       />
-      <div className="post-image-wrapper">
-        <img
-          alt="post thumbnail"
-          className="post-image"
+      <PostImageWrapper>
+        <PostImage
           src={props.post.imageUrl}
+          alt="post thumbnail"
         />
-      </div>
+      </PostImageWrapper>
       <CommentSection comments={props.post.comments} />
-    </div>
+    </PostBorder>
   );
 };
 
