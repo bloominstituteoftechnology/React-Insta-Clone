59ffabc9bc57588a802d84c0c34943a6a723984c
Matthew Wright <matthewbwright@gmail.com>
2018-09-10 15:46:23 -0700

    begins work on mapping and proptypes

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,23 +2,42 @@ import React, { Component } from 'react';
 import SearchBar from './Components/SearchBar/SearchBar';
 import PostContainer from './Components/PostContainer/PostContainer';
 import CommentSection from './Components/CommentSection/CommentSection';
+import PropTypes from 'prop-types';
+import dummyData from './dummy-data';
+// import heart from './images/instaheart.png';
 
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: dummyData
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <SearchBar />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <SearchBar />
+          </header>
+        </div>
+        <div>
+          <PostContainer p={this.state.dummyData} />
+        </div>
+        ))}
       </div>
     );
   }
 }
+
+PostContainer.propTypes = {
+  p: PropTypes.oneOf({
+    username: PropTypes.string,
+    imageUrl: PropTypes.string
+  })
+};
 
 export default App;
--- instagram/src/Components/PostContainer/PostContainer.js
+++ instagram/src/Components/PostContainer/PostContainer.js
@@ -1,5 +1,33 @@
-// import React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// import './PostContainer.css';
+import './PostContainer.css';
 
-// export default PostContainer;
+const PostContainer = props => {
+  return (
+    <div>
+      {props.p.map(p => (
+        <div key={p.username}>
+          <img src={p.imageUrl} />
+        </div>
+      ))}
+      ;
+    </div>
+  );
+};
+
+/* <header>{p.thumbnailUrl} {p.username}</header>
+            
+              <div>
+                <div>
+                <img src={heart} className="heart" alt="heart" />
+                </div>
+                {p.likes} {p.comments} {p.timestamp}
+              </div>
+              </div>
+             
+              <div />
+  )
+} */
+
+export default PostContainer;
