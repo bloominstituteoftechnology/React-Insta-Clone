20b60dfb23ebc8dc9de257bcae966782cbf0ade5
Marious Davis <mlancelot88@gmail.com>
2018-09-10 18:23:03 -0500

    adding files to post components

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super ();
+    this.state = {
+      dummyData: [],
+    };
+  }
+
   render() {
     return (
       <div className="App">
