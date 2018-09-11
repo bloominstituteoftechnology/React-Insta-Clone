15558941021ff779ea0f5dd39f2b9f16a94ea107
Marious Davis <mlancelot88@gmail.com>
2018-09-11 09:21:44 -0500

    built out CommentSection components

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import dummyData from './dummy-data';
 
 class App extends Component {
   constructor() {
     super ();
     this.state = {
-      dummyData: [],
+      searchInput: '',
+      data: dummyData,
     };
   }
 
   render() {
     return (
       <div className="App">
+        <SearchBar />
+        <PostContainer posts = {this.state.data} />
       </div>
     );
   }
