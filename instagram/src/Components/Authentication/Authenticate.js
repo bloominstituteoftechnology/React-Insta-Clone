02dff8fb532ff7b67a1f50877cdc9aab6407934a
Matthew Wright <matthewbwright@gmail.com>
2018-09-12 13:32:15 -0700

    first attempt at HOC

--- instagram/src/Components/Authentication/Authenticate.js
+++ instagram/src/Components/Authentication/Authenticate.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+const authenticate = PassedComponent =>
+  class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {};
+    }
+    render() {
+      return <PassedComponent />;
+    }
+  };
+
+export default authenticate;
