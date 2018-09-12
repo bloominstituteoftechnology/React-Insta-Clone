63989b49035fdc30e7394a7787073095eee2948d
MichelleOk <umbralcompendium@gmail.com>
2018-09-12 17:17:45 -0600

    trying to get comments to work

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,16 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 import dummyData from './dummy-data';
 import PostContainer from './components/PostContainer/PostContainer';
-
+import CommentInput from './components/CommentSection/CommentInput.js';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       data: dummyData, //we can just pull the array of obj in like so
+      inputComments: [],
+      typedComment: ""
     };
   }
-  
+
+  // componentDidMount() {
+  //   this.setState({ data: commentInput});
+  // }
+
+  addComment = event => {
+    event.preventDefault();
+    if(this.state.typedComment) {
+      this.setState({
+        inputComments: [...this.state.inputComments, this.state.typedComment],
+        typedComment: ""
+      });
+    }
+  };
+  handleInput = event => {
+    this.setState({
+      typedComment: event.target.value
+    });
+  };
+
   render() {
     console.log(this.state);
     return (
@@ -22,6 +43,10 @@ class App extends Component {
         <PostContainer data={this.state.data} /> {/*here we are naming the component(Postcontainer) 
         and making the first key/value pair, data/{this.state.data} which will be wrapped in the 
         object we call props*/}
+        <CommentInput addComment={this.addComment}
+                      inputComment={this.state.inputComment}
+                      handleInput={this.handleInput}
+                      />
       </div>
     );
   }
