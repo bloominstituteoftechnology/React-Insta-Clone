8df4c91ea4395bb20afde42f3a9127e30664a40a
A2i <abdulfatah.a.ahmad@gmx.com>
2018-09-12 01:09:15 -0400

    Styled CommentSection componenets

---
+++ instagram/src/components/CommentSection/Comment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Comment = props => {
+    return(
+        <div className='Comment'>
+            <span>{props.comment.username}</span>
+            <span>{props.comment.username}</span>
+        </div>
+    );
+}
+
+export default Comment;
--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
+import Comment from './Comment'
+import './CommentSection.css'
 
 class CommentSection extends Component {
     constructor(props){
         super(props);
         this.state={
-            comments: props.comments,
+            comments: [],
             commentInput: ''
         }
-        console.log(this.state.comments)
+        
     }
+
+    // componentDidMount(){
+    //     this.setState({comments: props.comments});
+    // }
 
 
 
     render() { 
        
         return (  
-
-            <div>hello</div>
+                <div className='commnet-section-instances'>
+                {this.state.comments.map(comment => <Comment key={comment.username} comment={comment}/>)}
+                
+                </div>
 
         );
     }
