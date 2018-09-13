c4d64553f95e2db2c90a36b0ccdb05549e90d446
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-09-12 19:10:35 -0500

    functionality for login intact

--- instagram/src/Components/Authentication/Authenticate.js
+++ instagram/src/Components/Authentication/Authenticate.js
@@ -1,26 +1,37 @@
-import React, {Component} from "react";
+import React from "react";
 import Login from '../Login/Login';
 
-const Authenticate = App => {
-  class extends Component {
-    constructor () {
+const Authenticate = App => 
+  class extends React.Component {
+    constructor (props) {
       super();
       this.state = {
         loggedIn: false
       };
+      console.log(this.state)
     }
     
-    componentDidMount () {
-      if(localStorage.getItem('Username' !== null)) {
-        this.setState({loggedIn: true})
+    componentDidMount() {
+      if (!localStorage.getItem('user')) {
+        this.setState({ loggedIn: false });
+      } else {
+        this.setState({ loggedIn: true });
       }
     }
 
+    // componentDidMount () {
+    //   if(localStorage.getItem('user' !== !null)) {
+    //     this.setState({loggedIn: true});
+    //   } my code
+    // }
+
     render () {
-      let output = this.state.loggedIn ? <App /> : <Login />;
-      return output;
+      if (this.state.loggedIn) return <App />;
+      return <Login />;
+      // let output = this.state.loggedIn ? <App /> : <Login />;
+      // return output;
     }
   }
-};
+
 
 export default Authenticate;
--- instagram/src/Components/Login/Login.js
+++ instagram/src/Components/Login/Login.js
@@ -3,18 +3,40 @@ import React, {Component} from 'react';
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
     }
 
+    handleLoginSubmit = e => {
+    //     const user = this.state.username;
+    //     localStorage.setItem('user', user);
+        window.location.reload();
+    
+      };
+
     login = e => {
-        localStorage.setItem('Username', e.target.username.value);
-    };
+        const user = this.state.username;
+        localStorage.setItem('user', user);  
+        this.setState({[e.target.name]: e.target.value});
+    }
+
 
     render() {
     return (
-        <form onSubmit={this.login}>
-           <input type='text' placeholder='Username'></input>
-           <input type='text' placeholder='Password'></input>
-           <button>Login</button>
+        <form>
+           <input 
+           onChange={this.login}
+           value={this.state.username} 
+           type='text' name='username' 
+           placeholder='Username'></input>
+
+           <input 
+           type="text"  
+           placeholder='Password'></input>
+
+           <button onClick={this.handleLoginSubmit}>Login</button>
        </form>
         );
     };
