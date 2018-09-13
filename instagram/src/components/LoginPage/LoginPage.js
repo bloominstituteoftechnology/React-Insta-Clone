2fb653ba8ac08e4fe9b82ce242a650da46be8963
verydecent <wnjhwng@gmail.com>
2018-09-13 15:40:19 -0700

    Refactored LoginPage css to styled components

--- instagram/src/components/LoginPage/LoginPage.js
+++ instagram/src/components/LoginPage/LoginPage.js
@@ -1,5 +1,47 @@
 import React from 'react';
 import './LoginPage.css';
+import styled from 'styled-components';
+
+
+
+const LoginPagediv = styled.div`
+    background: offwhite;
+    width: 100%;
+    max-width: 300px;
+    height: 260px;
+    margin: 0 auto;
+    margin-top: 200px;
+    border: 1.5px solid lightgray;
+    border-radius: 5px;
+    text-align: center;
+`
+
+const LoginHeader = styled.h1`
+    font-size: 18px;
+`
+
+const LoginForm = styled.form `
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const LoginInput = styled.input `
+    margin-top: 12px;
+    width: 160px;
+    height: 30px;
+    text-align: center;
+    font-size: 15px;
+`
+
+const LoginButton = styled.button `
+    margin-top: 20px;
+    width: 70px;
+    height: 27px;
+`
+
+
 
 class LoginPage extends React.Component {
     constructor() {
@@ -21,13 +63,15 @@ class LoginPage extends React.Component {
 
     render() {
         return (
-            <div className="LoginPageDiv">
-                <form>
-                    <input onChange={this.handleChange} placeholder="username.." name="usernameInput"/>
-                    <input onChange={this.handleChange} placeholder="password.." name="passwordInput"/>
-                    <button onClick={this.logIn}></button>
-                </form>
-            </div>
+            <LoginPagediv>
+                <LoginForm>
+                    <LoginHeader>Instagram</LoginHeader>
+                    <LoginInput onChange={this.handleChange} placeholder="username.." name="usernameInput"/>
+                    <LoginInput onChange={this.handleChange} placeholder="password.." name="passwordInput"/>
+                    <LoginButton onClick={this.logIn}>Log in</LoginButton>
+                    <LoginButton onClick={this.logIn}>Sign up</LoginButton>
+                </LoginForm>
+            </LoginPagediv>
         )
     }
 }
