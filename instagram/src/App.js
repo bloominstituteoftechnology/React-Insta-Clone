7c9a3971d662f28a8776986e91487fb0e8156037
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-10-03 14:35:30 -0500

    created the basic skeleton for the PostContainer area

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 //import dummyData from './dummy-data';
 import SearchBar from './Components/SearchBar/SearchBar.js'; 
+import PostContainer from './Components/PostContainer/PostContainer.js'; 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <SearchBar /> 
-      
+      <PostContainer /> 
       </div>
     );
   }
---
+++ instagram/src/Components/PostContainer/PostContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PostContainer = props => {
+  return (
+      <div className="post-container">
+          Container Area
+      </div> 
+  )
+}
+
+
+export default PostContainer; 
