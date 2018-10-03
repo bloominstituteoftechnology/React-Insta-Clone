c0a5bc2a5ac06c8ae109936de60e8b9aaf8b1904
Jennifer King <jenbellpgh@gmail.com>
2018-10-03 00:17:56 -0400

    Base layout for the react insta clone. Started create react-app. Added directories and files, tied each together with imports/exports.

---
+++ src/components/PostContainer/PostContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PostContainer.css';
+
+// Body of the container
+
+const PostContainer = (props) => {
+    return (
+        <div>PostContainer.js</div>
+    );
+};
+
+export default PostContainer;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SearchBar.css';
+
+// Header containing the search bar.
+
+const SearchBar = (props) => {
+    return (
+        <div>SearchBar.js</div>
+    );
+}
+
+export default SearchBar;
