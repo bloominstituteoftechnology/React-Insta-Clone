0da7d3c8f0ec2d0121d9e7b9e54cb96118c3a32e
Matthew Meitl <darksamus6@gmail.com>
2018-10-03 18:59:42 -0500

    added font-awesome cdn

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+
+class PostContainer extends React.Component {
+    constructor(props){
+        super(props);
+
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <i class="fas fa-search">Hi</i>
+            </div>
+        )
+    }
+}
+
+export default PostContainer;
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -30,34 +30,11 @@ class SearchBar extends React.Component {
     return (
       <div>
         <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarBrand href="/">Instagram</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
+           
+
           </Collapse>
         </Navbar>
       </div>
