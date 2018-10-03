b4eaf21f8a9eb61b7fe19e443087b3e468c61a25
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-10-03 14:23:28 -0500

    created a basic skeleton of the searchbar

---
+++ instagram/src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const SearchBar = props =>{
+  return (
+    <div className="searchBar">
+        <div> left portion of search bar </div>
+        <input></input> 
+        <div> right portion</div> 
+    </div>
+  )
+}
+
+
+export default SearchBar; 
