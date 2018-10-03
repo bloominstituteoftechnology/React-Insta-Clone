d4cb52ee9abec951034a71c56679581f54960fb0
jbrock1981 <jbrock1981@gmail.com>
2018-10-02 19:22:44 -0500

    started over, project set up again

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import dummyData from './dummy-data';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      //todo
-      dummyData: []
-    }
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
--- instagram/src/components/CommentSection/Comment.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Comment = props => {
-    return (
-        <div>Comment</div>
-    );
-}
-
-export default Comment;
