d4ca0b7c39215a8f9f06560eb0aa0a98ee6b4f9c
Eric Fleming <efleming111@yahoo.com>
2018-10-03 11:53:49 -0700

    Started styling SeachBar component

--- exchange-o-gram/src/components/SearchBar/SearchBar.js
+++ exchange-o-gram/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './SearchBar.css';
 
 class SearchBar extends React.Component{
     render(){
         return (
-            <div>
-                <span><i className="fas fa-camera-retro"></i>Exchange-O-Gram</span>
-                <input placeholder="Search"></input>
+            <div className="search-bar">
+                <div><i className="fas fa-camera-retro fa-2x"></i><span className="app-name">Exchange-O-Gram</span></div>
+                <input className="search-input" placeholder="Search"></input>
                 <span><i className="far fa-heart"></i><i className="far fa-user"></i></span>
             </div>
         )
