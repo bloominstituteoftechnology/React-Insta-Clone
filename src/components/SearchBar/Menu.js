cdf7ef922c2175a92b6610308bd27cbe41f80389
Jennifer King <jenbellpgh@gmail.com>
2018-10-03 01:45:44 -0400

    Added styling for the menu icons on the right side of the menu bar.

--- src/components/SearchBar/Menu.js
+++ src/components/SearchBar/Menu.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import compass from '../../assets/compass.jpg';
+import heart from '../../assets/heart.jpg';
+import profile from '../../assets/profile.jpg';
 
 const Menu = () => {
     return (
-        <div className='menu'>Menu Icons</div>
+        <div className='menu'>
+            <img className='compass' src={compass} alt=''/>
+            <img className='heart' src={heart} alt=''/>
+            <img className='profile' src={profile} alt=''/>
+        </div>
     );
 };
 
