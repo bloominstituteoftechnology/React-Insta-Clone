81ec1af611d89e597527e4f1b342e34d7a3b8234
Joseph Thompson <joe.thompson838@gmail.com>
2018-10-02 21:17:10 -0400

    updated the heart icon to add likes

--- react-insta-clone/src/components/PostContainer/PostContent/PostIcons/PostIcons.js
+++ react-insta-clone/src/components/PostContainer/PostContent/PostIcons/PostIcons.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const PostIcons = (props) => {
-  return (
-    <section className='icon-container'>
-      <section className='post-icons'>
-        <FontAwesomeIcon icon='heart' />
-        <FontAwesomeIcon icon='comment-alt' />
+class PostIcons extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      likes : 0,
+      counter: 1
+    }
+  }
+  componentDidMount() {
+    this.setState({likes: this.props.likes});
+  }
+
+  updateLikes = (e) => {
+    this.setState({
+      likes: this.state.likes + this.state.counter
+    })
+  }
+  render() {
+    return (
+      <section className='icon-container'>
+        <section className='post-icons'>
+          <FontAwesomeIcon icon='heart' onClick={this.updateLikes}/>
+          <FontAwesomeIcon icon='comment-alt' />
+        </section>
+        <section className='post-likes'>
+        {this.state.likes} <span>likes</span>
+        </section>
       </section>
-      <section className='post-likes'>
-      {props.likes} <span>likes</span>
-      </section>
-    </section>
-  );
+    );
+  }
 }
 
 export default PostIcons;
