f7050337c7c21f61b288181f6acc76ae5a599ea1
BrendonKootz <BrendonKootz@gmail.com>
2018-10-03 17:18:47 -0500

    Started building search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import './dummy-data.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-		<h1>InstaClone</h1>
+		<div className="SearchBarContent">
+			<img src="" alt="Logo"></img>
+			<h1>InstaClone</h1>
+
+			<div className="search">
+				<input/>
+			</div>
+
+			<div className="Buttons">
+				<img src="" alt="button1"></img>
+				<img src="img/heart.png" alt="button2"></img>
+				<img src="" alt="button3"></img>
+			</div>
+		</div>
       </div>
     );
   }
