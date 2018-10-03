92ce5335596db3e187d1f2334546fa47a4c5bb22
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-10-02 19:09:15 -0600

    Search Bar completed.

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import logo from '../../assets/instagramlogin.png';
+import styled from 'styled-components';
+import './searchbar.css';
+
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 150px;
+    border-bottom: 1px solid #D3D3D3;
+`;
+
+const LeftItems = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+// this isn't working with styled-componenets but is with css!
+// const InstaBadge = styled.i`
+//     margin: 40px 0 40px 40px;
+//     padding-right: 40px;
+//     font-size: 70px;
+//     border-right: 1px solid grey;
+// `;
+
+const InstaImage = styled.img`
+    padding-left: 20px;
+    height: 50px;
+`;
+
+const MiddleItems = styled.div`
+    padding-left: 20px;
+    height: 50px;
+`;
+
+const SearchInput = styled.input`
+    width: 400px;
+    height: 50px;
+    text-align: center;
+    font-size: 25px;
+    color: grey;
+`;
+
+const SearchBar = () => {
+    return ( 
+        <Header>
+            <LeftItems>
+                <i class="fab fa-instagram"></i>
+                <InstaImage src={logo} alt="logo" />
+            </LeftItems>
+            <MiddleItems>
+                <SearchInput type="text" placeholder="&#128269; Search" />
+            </MiddleItems> 
+            <div className="right-items">
+                <i class="far fa-compass"></i>
+                <i class="far fa-heart"></i>
+                <i class="far fa-user"></i>
+            </div>
+        </Header>
+     );
+}
+ 
+export default SearchBar;
