614ec94de3e7d30935cf7a4c332108580788cb96
alasalle <angela.lasalle91@gmail.com>
2018-10-03 16:36:06 -0400

    set up state and passed down props to components basic structure of components done still need styling of components still need Add comment box still need typechecking

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react';
-import SearchBar from '../src/components/SearchBar/SearchBar'
-
+import React, { Component } from "react";
+import SearchBar from "../src/components/SearchBar/SearchBar";
+import PostContainer from "../src/components/PostContainer/PostContainer";
+import dummyData from "./dummy-data"
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: dummyData,
+    }
+  }
   render() {
     return (
-      <SearchBar />
+      <>
+        <SearchBar />
+        {this.state.dummyData.map(post => {
+          return (
+            <PostContainer key={post.username} post={post} />
+          )})
+        }
+      </>
     );
   }
 }
---
+++ instagram/src/components/Comment/Comment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Comment extends React.Component {
+    render() {
+        return (
+            <div>{this.props.comment.username}:{this.props.comment.text}</div>
+        )
+    }
+}
+
+export default Comment;
