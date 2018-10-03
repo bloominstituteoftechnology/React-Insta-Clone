ba20c7e2d9c5331edff07358f8b1949f32b2ff65
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-10-03 18:45:00 -0400

    Initial commit.

---
+++ instagram/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SearchBar from './components/SearchBar/SearchBar.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    render()    {
+        return(
+            <div>
+            Test
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
