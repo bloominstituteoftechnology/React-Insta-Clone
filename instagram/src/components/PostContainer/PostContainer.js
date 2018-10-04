8759c38c5494aca86d97ee7beeb3ff7c2000e36e
Willie Yturralde <willieino@hotmail.com>
2018-10-03 20:24:11 -0700

    modified css

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -4,21 +4,39 @@ import "../../App.css"
 
 const PostContainer = props => {
 
-return (
+  return (
 
-<div> <CommentSection /></div>
+    <div className="postContainer">
+      <header className="header">
+        <div className="thumbNail">thumbNail</div>
+        <div className="userName">userName</div>
+      </header>
+      <div className="image">pic placeholder</div>
+      <div className="iconContainer2">
+        <div className="postIcon1" onClick={props.postIcon1}></div>
+        <div className="postIcon2" onClick={props.postIcon2}></div>
+      </div>
+      <div className="commentContainer">comments go here</div>
+      <div className="timeStamp">2 hours ago</div>
+      <footer className="addCommentContainer">
+        <input className="addComment" type="text" value={props.addComment} />
+        <div className="dots" onClick={props.dots}>...</div>
+      </footer>
 
-)
-  
- /*  return props.list.map((item, id) => (
-    <li
-      onClick={() => props.toggleComplete(item.id)}
-      key={id}
-     
-    >
-      {item.task}
-    </li>
-  )); */
+
+
+
+
+
+
+      <CommentSection /></div>
+
+  )
+
+  /*  return props.list.map((item, id) => (
+     <li
+       onClick={() => props.toggleComplete(item.id)}
+       )); */
 };
 
 export default PostContainer;
