d49122e273ddbff6c80f7956b8a108081ecb5af5
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-10-03 23:13:36 -0500

    App component started

--- instagram/src/index.js
+++ instagram/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+;
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
