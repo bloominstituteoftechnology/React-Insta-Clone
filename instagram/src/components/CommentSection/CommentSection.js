76a24f0e2f54c55b2428b8aaf33b03db013efd0d
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-10-04 00:09:31 -0400

    corrected props passed to CommentSection

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CommentSection = props => {
   return (
     <div className='comment-section'>
-      {props.post.comments.map(comment => {
+      {props.comments.map(comment => {
         return <Comment key={comment.username} comment={comment.text} />
       })}
     </div>
--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CommentSection from '../components/CommentSection/CommentSection.js';
+import CommentSection from '../CommentSection/CommentSection.js';
 
 const Post = props => {
   return (
