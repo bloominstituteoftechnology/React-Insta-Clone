cc05e3cfc46390e479c8e755100feb36ddc928e8
Griffin <griffinshuth@gmail.com>
2018-10-03 20:58:00 -0600

    need help with images

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,8 @@
+import React from './react';
+import dummyData from './dummy-data';
+
+const PostContainer = (props) =>{
+    return({
+        
+    })
+}
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SearchBar = (props) =>{
+    return(
+        <div>
+            <img src ='./components/SearchBar/instagram_new_logo.jpg' alt="logo"></img>
+            <img src ='./components/SearchBar/instagram.png' alt ="Instagram"></img>
+            <input></input>
+            icons
+        </div>
+    
+        
+    )
+}
+
+export default SearchBar
