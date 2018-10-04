e66c91e2b230d79e2f820b4e1a8f7edd4741af94
Denise Escobar <descobar1490@gmail.com>
2018-10-04 15:00:34 -0500

    finished formating and adding data to my PostContainer component
    -Next I will add a new component that will be used to map out and format the comments

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -2,9 +2,28 @@ import React from 'react';
 
 const PostContainer = (props) =>{
     return(
-        <div>
-            {props.post.username}
-            <img src={props.post.imageUrl}/>
+        <div className='wholePost'>
+            <div className='titleBar'>
+                <img src={props.post.thumbnailUrl} />
+                <span>{props.post.username}</span>   
+            </div>
+            <div className='image'>
+                <img src={props.post.imageUrl}/>    
+            </div>
+            <div className='Comments'>
+                <div>
+                    <i className="far fa-heart"></i>
+                    <i className="far fa-comment"></i>
+                </div>
+                <p>{props.post.likes}</p>
+                Map in comments here
+                <p>{props.post.timestamp}</p>
+            </div>
+            <div className='addComment'>
+                <p>Add a comment...</p>
+                <i className="fas fa-ellipsis-h"></i>
+            </div>
+            
         </div>
     )
 }
