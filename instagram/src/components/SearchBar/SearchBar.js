8b275d2a86413f07fa51264ad965ba89b930ec8a
Alex Ostrander <alexanderostrander23@gmail.com>
2018-10-03 23:54:09 -0400

    Currently creating the search bar. Styling the bar and finding all the icons/fonts for it

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,8 +1,17 @@
 import React from 'react';
-
+import './Search.css'
+import logo from './logo.png'
+import searchSolid from './search-solid.svg'
 const SearchBar = props => {
 	return (
-		<p>Search Bar</p>
+		<div className="searchBar">
+			<div className="left-items">
+				<i class="fab fa-instagram fa-2x"></i>
+				<div className="vertical"></div>
+				<img src={logo} alt="instagram logo" className="logo" />
+			</div>
+			<input className="inputBar" type="text" placeholder="search"></input>
+		</div>
 	)
 };
 
