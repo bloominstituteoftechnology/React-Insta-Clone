aad448ee02852d6eca582d8982cef3e92c99cf3d
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-10-03 21:53:32 -0400

    added constructor to class app set dummyData as state for posts

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import dummyData from './dummy-data';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: dummyData
+    };
+  }
   render() {
     return (
       <div className="App">
