21b6d14b981ec66af4f2045035be61a6c7814b72
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-10-03 23:53:57 -0500

    font awesome 5 added

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import SearchBar from './Components/SearchBar/SearchBar';
-
+import 'font-awesome/css/font-awesome.min.css';
 
 class App extends React.Component {
   render() {
--- instagram/src/Components/SearchBar/SearchBar.js
+++ instagram/src/Components/SearchBar/SearchBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 class SearchBar extends React.Component {
     constructor(){
         super();
