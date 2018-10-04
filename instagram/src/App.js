f9b89adda749c5d10f094184dad9a6d486244a3a
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-10-03 21:50:10 -0400

    added the search component and the made a couple css changes

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Search from './components/SearchBar/Search'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Search />
       </div>
     );
   }
