c67e177645573602a596c2e0c9b9c3e91a0518d9
Denise Escobar <descobar1490@gmail.com>
2018-10-03 22:08:52 -0500

    Finished a basic version of the search bar
    -I added all the elements including a search input box, logos, and misc buttons
    -I styled the elements and positioned the generally where they belong

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './SearchBar.css';
 
 const SearchBar = () =>{
     return(
-        <div>
-            This is a search bar
+        <div className='searchBar'>
+            <div className='logo'>
+                <i class="fab fa-instagram"></i>
+                <span>Instagram</span>
+            </div>
+            <div className='search'>
+                <input value='search'/>
+            </div>
+            <div className='buttons'>
+                <i class="fas fa-compass"></i>
+                <i class="far fa-heart"></i>
+                <i class="far fa-user"></i>
+            </div>
         </div>
     )
 }
