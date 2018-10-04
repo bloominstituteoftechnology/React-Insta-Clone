c27b613ce422e89344b77ef981bfbe57a2c90431
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-10-03 20:42:59 -0400

    Project boilerplate set up: component files, imports, css files created (but not imported yet), content rendering to screen.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
+import PostContainer from './components/PostContainer/PostContainer'
 
-class App extends Component {
+
+class App extends React.Component {
+
+  constructor(){
+    super()
+
+    this.state = {
+
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+      <PostContainer />
+      </>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
     );
   }
 }
+
 
 export default App;
