2acc7b780560cea3d48c8c8f9c7ebfe17c28c56b
Andres Rivera <andres.rivera.toro@gmail.com>
2018-10-04 19:59:06 -0400

    started on typechecking

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -3,6 +3,7 @@ import PostHeader from './PostHeader';
 import PostContent from './PostContent';
 import CommentSection from '../CommentSection/CommentSection';
 import './PostContainer.css';
+import PropTypes from 'prop-types';
 
 const PostContainer = props => {
   return (
@@ -12,6 +13,22 @@ const PostContainer = props => {
       <CommentSection comments={props.post.comments} likes={props.post.likes} />
     </div>
   );
+}
+
+PostContainer.propTypes = {
+  post: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    thumbnailUrl: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+    timestamp: PropTypes.string.isRequired,
+    comments: PropTypes.arrayOf(
+      PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+  }).isRequired,
 }
 
 export default PostContainer;
