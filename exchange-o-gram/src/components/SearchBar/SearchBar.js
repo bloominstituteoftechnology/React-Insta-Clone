a9f198b1002f0d59607e7145dce10772f2ddd01e
Rick Fleming <elfeming111@yahoo.com>
2018-10-03 20:26:40 -0700

    Add compass icon to SearchBar

--- exchange-o-gram/src/components/SearchBar/SearchBar.js
+++ exchange-o-gram/src/components/SearchBar/SearchBar.js
@@ -8,7 +8,7 @@ class SearchBar extends React.Component{
                 <div className="icon-cam"><i className="fas fa-camera-retro fa-3x"></i></div>
                 <div className="name"><span className="app-name">Exchange-O-Gram</span></div>
                 <div className="search-con"><span className="input-icon"><i className="fas fa-search fa-lg"></i></span><input className="search-input" placeholder="Search"></input></div>
-                <div className="icons"><i className="far fa-heart fa-2x"></i><i className="far fa-user fa-2x"></i></div>
+                <div className="icons"><i className="far fa-compass fa-2x"></i><i className="far fa-heart fa-2x"></i><i className="far fa-user fa-2x"></i></div>
             </div>
         )
     }
