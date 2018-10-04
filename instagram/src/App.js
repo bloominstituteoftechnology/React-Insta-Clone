9017ec7fc25d72d526ea840a50fdb7d738676134
Alex Ostrander <alexanderostrander23@gmail.com>
2018-10-03 21:54:31 -0400

    Created files for each component section. Imported Dummy Data to the root app.

---
+++ instagram/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import dummyData from './dummy-data';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
