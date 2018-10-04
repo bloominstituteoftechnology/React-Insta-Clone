e9cc00138889808e43aedbc37bddcfcc45c9d40b
Glenn-David Daniel <d2rd@outlook.com>
2018-10-03 22:04:53 -0700

    Components and input form working

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -15,7 +15,33 @@ class App extends Component {
     };
     this.setState({instaClonePosts: data});
   }
+/* DATA MODEL
+{
+    username: "philzcoffee",
+    thumbnailUrl:
+      "https://tk-assets.lambdaschool.com/ecd33d34-c124-4b75-92d2-e5c52c171ed8_11201517_887808411287357_1307163552_a.jpg",
 
+    imageUrl:
+      "https://tk-assets.lambdaschool.com/69cf901b-f96d-466e-a745-ff2a01effac9_philz-image.jpg",
+    likes: 400,
+    timestamp: "July 17th 2017, 12:42:40 pm",
+    comments: [
+      {
+        username: "philzcoffee",
+        text:
+          "We've got more than just delicious coffees to offer at our shops!"
+      },
+      {
+        username: "biancasaurus",
+        text: "Looks delicious!"
+      },
+      {
+        username: "martinseludo",
+        text: "Can't wait to try it!"
+      }
+    ]
+  },
+*/
   render() {
     return (
       <div className="App">
@@ -32,9 +58,9 @@ class App extends Component {
           >
             About Me
           </a>
+          <SearchBar />
         </header>
         <div>
-          <SearchBar />
           <PostContainer />
 
         </div>
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -2,8 +2,34 @@ import React from "react";
 import "./SearchBar.css";
 
 const SearchBar = () => {
-  return <div>
+  return (
+    <div>
     SearchBar
+    <form >
+      <input 
+      className="searchInput"
+      name="search"
+      type="text"
+      placeholder="Search"
+      // onChange={this.handleInputChange}
+      />
+    </form>
+    
+    {/* <form className="column-Layout"> // change to `row-layout`
+        <input
+        className="Input"
+        value={this.instaClonePosts.text}
+        name="search"
+        type="text"
+        placeholder="Search"
+        onChange={this.handleInputChange}
+        />
+        
+        <button className="btn-Search" onClick={() => this.handleAddNote()} type="button">
+        Find this!
+        </button>
+      </form> */}
   </div>
+  )
 }
 export default SearchBar;
