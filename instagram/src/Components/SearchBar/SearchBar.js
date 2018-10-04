c64858931cc4db4bca3f12cec672065788664d20
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-10-03 21:06:40 -0500

    Search bar class structure done

---
+++ instagram/src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,25 @@
+import react from "react";
+
+class SearchBar extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            searchValue = '',
+        }
+    }
+
+    render(){
+        return (
+            <from className="header-container">
+                {/*logo image*/}
+                {/*Instagram text logo*/}
+                {/*searchbar*/}
+                <div className="icons-container">
+                    {/*3 icons*/}
+                </div>
+            </from>
+        )
+    }
+}
+
+export default SearchBar;
