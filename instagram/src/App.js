195cc3ba150da288d05de03bd6fd5c1357f5d13f
stevekutz <stkutz@gmail.com>
2018-10-03 23:50:48 -0500

    discoverd instructions are wrong, trying to fix directory structure

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import dummyData from './components/dummy-data'
+import SearchBar from '../src/components/Searchbar/SearchBar';
+
+
 import './App.css';
 
+
+
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      dummyData: dummyData,
+      comment: ''
+
+    }
+  }
+
+
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App-container'>
+        <SearchBar/>
+
+
       </div>
+
     );
+
   }
 }
 
