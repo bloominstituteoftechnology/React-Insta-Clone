4201ea0224dead1aec5c6aff2de2a82035489aa9
Wesley Meeks <wesmeeks88@gmail.com>
2018-10-03 20:58:25 -0600

    working on styling the search bar

--- instagram/src/components/SearchBar/SearchBar.jsx
+++ instagram/src/components/SearchBar/SearchBar.jsx
@@ -4,11 +4,11 @@ const SearchBar = () => {
   return (
     <div className="row search-bar d-flex justify-content-start align-items-center">
       <i className="fab fa-instagram col-1 insta-icon" />
-      <img
+      {/* <img
         className="instagram-logo"
-        src="https://cdn.iphoneincanada.ca/wp-content/uploads/2017/06/Instagram_logo.svg_.png"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
         alt=""
-      />
+      /> */}
       <form className="col-8">
         <input placeholder="search" type="text" />
       </form>
