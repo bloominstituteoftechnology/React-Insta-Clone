1ab2cb0af3a2e8a28475f46425367c2ab726455d
Robert Billington <chillingtonbillington@gmail.com>
2018-10-04 11:20:18 -0500

    Robert Billington, FSWPT2, PM @ JohnSpraul, Insta-React-Clone, intitial commit

---
+++ instagram/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import dummyData from './Dummy'
+
+class App extends Component {
+  state ={
+    dummyData : dummyData,
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>
+         
+        </h1>
+      </div>
+    );
+  }
+}
+console.log(dummyData);
+export default App;
