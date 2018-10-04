55ef12d5e1bce684cdb97a0523e456dac078d9cf
Levi Appenfelder <levi.appenfelder@gmail.com>
2018-10-03 20:10:06 -0600

    Search Bar Functionality Complete

---
+++ instagram/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import SearchBar from './SearchBar'
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      comments:[],
+      search: '',
+    } 
+  }
+
+  inputHandler = event => {
+    const value = event.target.value;
+
+    this.setState (
+      {search:value}
+    );
+    }
+
+
+  render() {
+    return (
+      <div className="App">
+      <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
