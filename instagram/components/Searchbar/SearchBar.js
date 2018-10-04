4f4c2116a87436d13244f761451ce8f2cb1c6fab
stevekutz <stkutz@gmail.com>
2018-10-03 23:24:10 -0500

    building search bar and Apps.js

---
+++ instagram/components/Searchbar/SearchBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import 'SearchBar.css';
+
+class SearchBar extends React.Component {
+
+  render () {
+    return (
+      <div className = 'searchBar-container'>
+        <div className = 'imageLOGO-container'>
+          <img className = 'cameraIMG'  alt = 'instagramIMAGE'  />
+          <p className = 'InstagramLOGO'> Instagram</p>
+        </div>
+
+        <div className = 'searchInput-container'>
+          <input name = 'search' className = 'searchInput' />
+        </div>
+
+        <div className = 'littleIcons'>
+          <img className = 'compassIMG icons'/>
+          <img className = 'hearrtIMG icons'/>
+          <img className = 'personIMG icons'/>
+        </div>
+
+      </div> /* searchBar-container   */
+
+
+
+
+
+      )
+
+
+
+  }
+
+
+
+}
+
+export default SearchBar
