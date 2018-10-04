bc49a6341095435e52d0e459ac1fa15f77c0d38a
Wesley Meeks <wesmeeks88@gmail.com>
2018-10-03 20:15:15 -0600

    Moved dummy-data to my source file, got it to generate using components in post container and comment section -still need to style.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import dummyData from "./dummy-data";
+import PostContainer from "./components/PostContainer/postcontainer";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { data: dummyData };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* {console.log(this.state.data)} */}
+        {this.state.data.map(item => (
+          <PostContainer data={item} />
+        ))}
       </div>
     );
   }
