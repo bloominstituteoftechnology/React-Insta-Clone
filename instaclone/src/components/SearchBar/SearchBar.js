bd95ee395872c17a1379709da8483a431b1d9af0
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2018-10-03 23:33:13 -0400

    header complete

--- instaclone/src/components/SearchBar/SearchBar.js
+++ instaclone/src/components/SearchBar/SearchBar.js
@@ -3,11 +3,16 @@ import './SearchBar.css';
 
 class SearchBar extends Component {
 	render() {
-		return (
-			<div className= 'container'>
-				<img src={require("./images/instagram.png")} alt="instagram"></img>
-			</div>
-		);
+		return <div className="container">
+				<img src={require('./images/instagram.png')} alt="instagram" />
+				<h2>Instagram</h2>
+				<input className="search" placeholder="Search" />
+				<div className="icons" >
+				<i class="far fa-compass" />
+        <i class="far fa-heart" />
+        <i class="far fa-user"></i>
+        </div>
+			</div>;
 	}
 }
 
