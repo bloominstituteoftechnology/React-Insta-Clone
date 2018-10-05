49a53a98139fbaed01bf7977c2eeb4bd52d95cc8
Cesar Aponte <caponte4@win.coat.com>
2018-10-05 12:40:42 -0400

    little styling done

--- src/components/Posts/PostComments/PostCommentForm.js
+++ src/components/Posts/PostComments/PostCommentForm.js
@@ -4,9 +4,15 @@ import '../post.css';
 
 const PostCommentForm = props => {
     return(
+        
         <div className={"commentInputForm"}>
         <input className={"addACommentInput"} type="text" placeholder="Add a omment..."></input>
+
+        <div className={"addCommentActionMenu"}>
+        &bull;&bull;&bull; 
         </div>
+        </div>
+        
     )
 }
 
