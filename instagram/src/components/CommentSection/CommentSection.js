f061b12fb842dc7e795f41e145ca2184d474e6b0
Colin Dismuke <colin.dismuke@gmail.com>
2018-10-05 14:57:45 -0500

    Refactored CommentSection to be a pure component.

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,30 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Comment from './Comment';
+import PropTypes from "prop-types";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './comment.css';
 
-const CommentSection = (props) => {
-    return (
-        <section className="comment-container">
-            <section className="comment-icons">
-                <FontAwesomeIcon icon='heart' />
-                <FontAwesomeIcon icon='comment-alt' />
+class CommentSection extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            comments: [],
+            commentInput: '',
+
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            comments: this.props.comments
+        })
+    }
+
+    render () {
+        return (
+            <section className="comment-container">
+                <section className="comment-icons">
+                    <FontAwesomeIcon icon='heart' />
+                    <FontAwesomeIcon icon='comment-alt' />
+                </section>
+                <p><strong>{this.props.comments.likes} likes</strong></p>
+                <section className="comments">
+                    {this.props.comments.map(comment => (
+                        <div>
+                            <Comment user={comment.username}
+                                    text={comment.text}/>
+                        </div>
+                    ))}
+                </section>
+                <section className="add-comment">
+                    <input placeholder=" Add a comment..."/>
+                    <FontAwesomeIcon icon='ellipsis-h'/>
+                </section>
             </section>
-            <p><strong>{props.likes} likes</strong></p>
-            <section className="comments">
-                {props.comments.map(comment => (
-                    <div>
-                        <Comment user={comment.username}
-                                 text={comment.text}/>
-                    </div>
-                ))}
-            </section>
-            <section className="add-comment">
-                <input placeholder=" Add a comment..."/>
-                <FontAwesomeIcon icon='ellipsis-h'/>
-            </section>
-        </section>
-    )
+        )
+    }
+}
+
+CommentSection.propTypes = {
+    comments: PropTypes.array,
 }
 
 export default CommentSection;
