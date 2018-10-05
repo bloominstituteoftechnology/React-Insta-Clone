6992fee6a3e44fc04300e618f78397381deccdde
Matthew Meitl <darksamus6@gmail.com>
2018-10-04 20:02:42 -0500

    beautified a couple files

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
-import './comments.css';
+import React from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import "./comments.css";
 
-
-
-
-const CommentSection = (props) =>{
-
-
-
-
-    
-        return (
-            <div className={'comments'}>
-            {props.comments.map(comment=>(
-            <div className={'comment'}><div className={'commentusername'}>{comment.username}:</div>   {comment.text}</div>
-            ))
-            }
-
-            </div>
-        )
-    }
-
+const CommentSection = props => {
+  return (
+    <div className={"comments"}>
+      {props.comments.map(comment => (
+        <div className={"comment"}>
+          <div className={"commentusername"}>{comment.username}:</div>{" "}
+          {comment.text}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default CommentSection;
