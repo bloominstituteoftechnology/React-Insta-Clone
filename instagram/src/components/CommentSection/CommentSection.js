cbf394b3fe057a357e1b47b90afd7aa37d0021cd
rytwalker <rytwalker@gmail.com>
2018-10-05 05:29:40 -0400

    Added prop types

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import Comment from './Comment';
+import PropTypes from 'prop-types';
 import './CommentSection.css';
 import CommentInput from './CommentInput';
 
 const CommentSection = props => {
   return (
     <div className="CommentSection">
-      {props.comments.comments.map(comment => {
-        return <Comment comment={comment} />;
+      {props.comments.comments.map((comment, i) => {
+        return <Comment comment={comment} key={i} />;
       })}
       <div className="TimeStamp">2 Hours Ago</div>
       <CommentInput />
     </div>
   );
+};
+
+CommentSection.propTypes = {
+  comments: PropTypes.object
 };
 
 export default CommentSection;
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './PostContainer.css';
+import PropTypes from 'prop-types';
 import CommentSection from '../CommentSection/CommentSection';
 
 const PostContainer = props => {
@@ -20,6 +21,17 @@ const PostContainer = props => {
       <CommentSection comments={props.data} />
     </div>
   );
+};
+
+PostContainer.propTypes = {
+  post: PropTypes.shape({
+    username: PropTypes.string,
+    thumbnailUrl: PropTypes.string,
+    imageUrl: PropTypes.string,
+    likes: PropTypes.number,
+    timestamp: PropTypes.string,
+    comments: PropTypes.arrayOf(PropTypes.object)
+  })
 };
 
 export default PostContainer;
