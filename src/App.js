11aa258f7fddecf85d6f6395dca60447dcc99834
Cesar Aponte <caponte4@win.coat.com>
2018-10-05 17:12:11 -0400

    Working on getting the right comments in the right sections.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import dummyData   from "./dummy-data";
 import SearchbarContainer from "./components/SearchBar/SearchbarContainer";
 import Post from "./components/Posts/Post/Post";
+import arraylooper from "./arrayLooper";
 
 
 class App extends Component {
@@ -12,11 +13,19 @@ class App extends Component {
       data:[dummyData]
     };
 }
+
+arrayloop(i){
+
+}
+
   render() {
     return (
+
       <div className="App">
         <SearchbarContainer />
+
         <Post data={this.state.data[0]} comments={this.state.data[0][0].comments}/>
+        
       </div>
     );
   }
---
+++ src/arrayLooper.js
@@ -0,0 +1,13 @@
+import dummyData from "./dummy-data";
+
+
+var loopInit = 0;
+
+
+const arraylooper = () => {
+    for(var i = 0; i < dummyData[0][0]; i++){
+       return loopInit++
+    }
+}
+
+export default arraylooper
