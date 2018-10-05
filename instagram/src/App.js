030da907e67f8ae2213dd3a3c0bb414a901a9ac0
SavannahGreuel <smallidgesavannah@gmail.com>
2018-10-05 17:13:45 -0600

    project set up

---
+++ instagram/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import SearchBar from './components/SearchBar/search-bar'
+import './App.css';
+
+class App extends Component {
+ 
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ instagram/src/components/SearchBar/search-bar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import insta_logo from './assets/insta_logo';
+
+const SearchBar =
+    <div className = 'searchBarContainer'>
+        <div>
+            <img className ='instaLogo' src={insta_logo} />
+        </div>
+    </div>
+    
+
+
+export default SearchBar;
