a6254f2617e109199c477fde367cc0ecae9c2f4f
rytwalker <rytwalker@gmail.com>
2018-10-05 03:41:04 -0400

    Built out navigation

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './SearchBar.css';
 
 const SearchBar = () => {
   return (
-    <div>
-      SearchBar
-      <p>Hello</p>
+    <div className="SearchBar">
+      <div className="container">
+        <h1 className="SearchBar-Logo">
+          <span className="Logo">
+            <ion-icon name="logo-instagram" />
+          </span>
+          <span className="Brand">Instagram</span>
+        </h1>
+        <input type="text" placeholder="Search" className="SearchBar-Input" />
+        <div className="SearchBar-Icons">
+          <ion-icon name="compass" />
+          <ion-icon name="heart-empty" />
+          <ion-icon name="person" />
+        </div>
+      </div>
     </div>
   );
 };
