4b4c00931dee5b3b6413abbae1cc77b980a88466
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-10-06 12:10:06 -0500

    added the componentDidMount part of the assigment

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -6,11 +6,28 @@ import PostContainer from './Components/PostContainer/PostContainer.js';
 
 
 class App extends Component {
+  constructor(){
+    super(); 
+    this.state = {
+    data: []
+    }
+  }
+
+  componentDidMount(){
+    this.setState({
+      data: dummyData
+    });
+  }
+
   render() {
+    if(!this.state.data.length){
+      return <h3>"Loading..."</h3> 
+    }
+
     return (
       <div className="App">
         <SearchBar/> 
-        <PostContainer dummyData={dummyData} /> 
+        <PostContainer dummyData={this.state.data} /> 
       </div>
     );
   }
