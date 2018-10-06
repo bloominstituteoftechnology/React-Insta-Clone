af2c80ecd0e671c85b749c73b9cafaebef0b3e41
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-10-06 13:46:31 -0500

    got the comment section to finally work

--- instagram/src/Components/CommentSection/CommentSection.js
+++ instagram/src/Components/CommentSection/CommentSection.js
@@ -1,29 +1,80 @@
 import React from 'react'; 
 import PropTypes from 'prop-types'
 
-const CommentSection = props => {
+class CommentSection extends React.Component {
+  constructor(props){
+  super(props); 
+  this.state = {
+      comments: props.comments,
+      likes: props.likes,
+      timestamp: props.timestamp,
+      input: ""
+   }
+  }
+
+  commentHandler = (event) => {
+    const {value} = event.target;
+    this.setState({
+      input: value,
+    });
+  };
+
+   addNewComment = (event) => {
+    if (this.state.input) {
+      this.setState({
+        comments: [
+          ...this.state.comments,
+          {
+            text: this.state.input,
+            username: "Anonymous"
+          }
+        ]
+      });
+     event.currentTarget.value = null;
+    }
+  };
+
+   submitHandler = event => {
+     if (event.keyCode === 13 && event.shiftKey === false) {
+       if (this.state.input.length >= 1)
+         event.preventDefault();
+         this.addNewComment(event);
+     }
+   };
+   
+
+render(){
   return (
     <div className="commentContainer">
       <div className="likeTab">
         <i class="far fa-heart"></i>
         <i class="far fa-comment"></i>
       </div>
-      <p className="userLikes">{props.likes} likes</p>
+      <p className="userLikes">{this.state.likes} likes</p>
       <div>
-        {props.comments.map( (comment, index) =>{
+        {this.state.comments.map( (comment, index) =>{
           return <p className="userComments" key={index}>
                    <span>{comment.username}</span>
                  {comment.text}</p>  
         })}
       </div> 
-      <p className="timeStamp">{props.timestamp}</p>
+      <p className="timeStamp">{this.state.timestamp}</p>
       <hr/> 
       <div className="pageFooter">
-        <p>Add a Comment...</p>
-        <p id="footerTab">...</p>  
+
+      <form onSubmit={this.addNewComment}>
+        <textarea
+          value={this.state.input} 
+          onChange={this.commentHandler} 
+          onKeyDown={this.submitHandler} 
+          placeholder="Add a comment..."
+        />
+      </form>
+      <p id="footerTab">...</p>  
       </div> 
     </div> 
-  )
+   )
+  }
 }
 
 CommentSection.propTypes = {
