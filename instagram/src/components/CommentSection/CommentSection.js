c58ef6bb0571aa012d84ad3e8229701b766871c2
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2018-10-06 08:50:55 -0700

    Refactored comment section so that comments are stored as state object

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -4,11 +4,18 @@ import Comment from './Comment.js';
 import PropTypes from 'prop-types';
 
 class CommentSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: this.props.data.comments
+        }
+    }
+
     render() {
         return (
             <div>
                 {/* list of comments */}
-                {this.props.data.comments.map(comment => {
+                {this.state.comments.map(comment => {
                     return (
                         <Comment key={Math.random()} comment={comment}/>
                 )
