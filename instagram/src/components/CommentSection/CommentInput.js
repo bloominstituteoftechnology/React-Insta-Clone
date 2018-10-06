a2a28727d324200d68c0ec89a5023f5261fe16d6
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-10-06 10:43:50 -0600

    Dealt with styling.

--- instagram/src/components/CommentSection/CommentInput.js
+++ instagram/src/components/CommentSection/CommentInput.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const CommentInputForm = styled.input`
+const InputForm = styled.form`
     border: 1px solid white;
     border-top: 1px solid lightgrey;
-    width: 94%;
+    width: 97%;
     height: 40px;
     text-align: left;
     font-size: 15px;
     color: grey;
-    margin: 10px 0;
+    margin: 5px 10px;
+`;
+
+const InputDisplay = styled.div`
+
+`;
+
+const CommentInputForm = styled.input`
+    width: 96%;
+    height: 40px;
+    text-align: left;
+    font-size: 15px;
+    color: grey;
+    padding-right: 25px;
+    border: white;
+`;
+
+const Ellipsis = styled.i`
+    color: black;
 `;
 
 const CommentInput = () => {
     return ( 
         <div>
-            <form>
+            <InputForm>
+            <InputDisplay>
                 <CommentInputForm type="text" placeholder="Add a comment..." />
-            </form>
+                <Ellipsis className="fas fa-ellipsis-h"></Ellipsis>
+            </InputDisplay>
+            </InputForm>
         </div>
     );
 }
