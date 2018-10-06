60346f86eee59d76b50b1869a9a76de148f2f9f4
BrendonKootz <BrendonKootz@gmail.com>
2018-10-06 13:27:34 -0500

    Trying some new things

--- src/components/CommentSection/CommentSection.js
+++ src/components/CommentSection/CommentSection.js
@@ -4,13 +4,16 @@ import React, {Component} from 'react';
 const CommentSection = (props) => {
 	return(
 		<div>
-			<div className="Comment-Section">
-				{props.content.map(comment => {
-					return <p><strong>{comment.username}</strong>{comment.text}</p>
-				})}
-			</div>
+
 		</div>
 	)
 }
 
 export default CommentSection;
+
+/*
+			<div className="Comment-Section">
+				{props.content.map(comment => {
+					return <p><strong>{comment.username}</strong>{comment.text}</p>
+				})}
+*/
--- src/components/PostContainer/PostContainer.js
+++ src/components/PostContainer/PostContainer.js
@@ -3,26 +3,34 @@ import CommentSection from '../CommentSection/CommentSection.js';
 import dummyData from '../dummy-data.js';
 
 const PostContainer = (props) => {
-	return(
-		<div className="Container">
-			<div className="Wrapper">
-				<img src={dummyData.thumbnailUrl}/>
-				{dummyData.username}
+	PostTitle = () => {
+		return(
+			<div className = "Post-Title">
+				<img src={this.dummyData.thumbnailUrl} alt = "User-Thumbnail" className = "Post-Title-Img" />
+				<div className="Post-Username">{this.dummyData.username}</div>
 			</div>
+		)
+	}
 
-			<img className="Post-Img" src={dummyData.imageUrl}/>
+	PostImg = () => {
+		return(
+			<img src={this.props.dummyData.imgageUrl} alt="Post-Image" className="Post-Img"/>
+		)
+	}
 
-			<div className="Post-Buttons">
-				<i className="Heart"></i>
-				<i className="Comment"></i>
+	render() {
+		return(
+			<div className = "Container">
+				<div className = "Wrapper">
+					<img src={this.props.dummyData.thumbnailUrl} alt="Profile-Image" className = "Profile-img"/>
+					<div className = "Profile-Username">{this.props.dummyData.username}</div>
+				</div>
+				<img src={this.props.dummyData.imageUrl} alt="Post Image" className = "Posted-Image"/>
+				{this.PostTitle()}
+				{this.PostImage()}
 			</div>
-
-			<div className="Likes">
-				{dummyData.likes} likes
-			</div>
-			<CommentSection conent={dummyData.comments}/>
-		</div>
-	)
+		)
+	}
 }
 
 export default PostContainer;
