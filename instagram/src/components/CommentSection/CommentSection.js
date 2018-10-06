6271896498a189cd33855cc3021a97c5cd421ab0
Louis <7.louislee@gmail.com>
2018-10-07 01:08:41 +0800

    added function to add comment but refresh everytime

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,20 +1,57 @@
 import React, { Component } from "react";
 import Comment from "./Comment";
-import moment from 'moment'
-
-
+import moment from "moment";
 
 class CommentSection extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
+
+  handleInput = e => {
+    this.setState({
+      input: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const newObj = {
+      username: "7.liii",
+      text: this.state.input
+    };
+    if (this.state.input !== "") {
+      this.props.comments.push(newObj);
+    }
+  };
+
   render() {
-    const timestamp = moment(this.props.timestamp, 'MMMM Do YYYY, h:mm:ss a', true).startOf('hour').fromNow();
+    const timestamp = moment(
+      this.props.timestamp,
+      "MMMM Do YYYY, h:mm:ss a",
+      true
+    )
+      .startOf("hour")
+      .fromNow();
     return (
       <div className="comment-section">
         <h3 className="likes">{this.props.likes} likes</h3>
         {this.props.comments.map(x => (
           <Comment username={x.username} text={x.text} />
         ))}
-        <p className='timestamp'>{timestamp}</p>
-        <input type="text" placeholder="Add a comment..." className="add-comment"/>
+        <p className="timestamp">{timestamp}</p>
+        <form>
+          <input
+            type="text"
+            value={this.state.input}
+            placeholder="Add a comment..."
+            className="add-comment"
+            onChange={this.handleInput}
+            onSubmit={this.handleSubmit}
+          />
+        </form>
       </div>
     );
   }
