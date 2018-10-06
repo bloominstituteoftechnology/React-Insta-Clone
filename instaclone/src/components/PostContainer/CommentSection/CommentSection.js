a986c266d31f4a6210fd5205a16d91dc7f6bf28f
Michael Trew <mtrew2015@gmail.com>
2018-10-06 01:24:42 -0400

    fix onsubmit still get error

--- instaclone/src/components/PostContainer/CommentSection/CommentSection.js
+++ instaclone/src/components/PostContainer/CommentSection/CommentSection.js
@@ -1,69 +1,70 @@
-import React, { Component } from 'react';
-import './CommentSection.css';
-import ActualComment from './Comment';
+import React, { Component } from "react";
+import "./CommentSection.css";
+import ActualComment from "./Comment";
 
 class CommentSection extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			comments: this.props.comment,
-			newComment: ''
-		};
-	}
-	commentInput = e => {
-		this.setState({ newComment: e.target.value });
-	};
-	preventDefault = event => {
-		event.preventDefault();
-	};
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: this.props.comment,
+      newComment: ""
+    };
+  }
+  commentInput = e => {
+    this.setState({ newComment: e.target.value });
+  };
+  preventDefault = event => {
+    event.preventDefault();
+  };
 
-	clearForm = () => {
-		this.setState({
-			newComment: ''
-		});
-	};
-	addNewComment = event => {
-		event.preventDefault();
-		const empty = '';
-		const userName = 'Michael';
-		const nComment = { text: this.state.newComment, username: userName };
-		const commentList = this.state.comments.slice();
-		commentList.push(nComment);
-		this.setState({
-			comments: commentList
-		});
-		this.clearForm();
-	};
-	render() {
-		return (
-			<div>
-				{this.state.comments.map(item => {
-					return (
-						<div>
-							<ActualComment
-								key={Math.random()}
-								commentData={item}
-								username={'Michael'}
-							/>
-						</div>
-					);
-				})}
-				<p className="postTime">Posted: {this.props.time}</p>
+  clearForm = () => {
+    this.setState({
+      newComment: ""
+    });
+  };
+  addNewComment = event => {
+    event.preventDefault();
+    const empty = "";
+    const userName = "Michael";
+    const nComment = { text: this.state.newComment, username: userName };
+    const commentList = this.state.comments.slice();
+    commentList.push(nComment);
+    this.setState({
+      comments: commentList
+    });
+    this.clearForm();
+  };
+  render() {
+    return (
+      <div>
+        {this.state.comments.map(item => {
+          return (
+            <div>
+              <ActualComment
+                key={Math.random()}
+                commentData={item}
+                username={"Michael"}
+              />
+            </div>
+          );
+        })}
+        <p className="postTime">Posted: {this.props.time}</p>
 
-				<div className="addCommentContainer">
-					<input
-						onChange={this.commentInput}
-						onKeyPress={this.addNewComment}
-						type="text"
-						placeholder="Add Comment..."
-						className="addComment"
-					/>
+        <div className="addCommentContainer">
+          <form onSubmit={this.addNewComment}>
+            <input
+              onChange={this.commentInput}
+              type="text"
+              placeholder="Add Comment..."
+              className="addComment"
+            />
+          </form>
 
-					<i className="fas fa-ellipsis-h" />
-				</div>
-			</div>
-		);
-	}
+          <i className="fas fa-ellipsis-h" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CommentSection;
