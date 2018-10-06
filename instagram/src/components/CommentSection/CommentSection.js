394cb8bd1210b3b3ffb1305901dc4857fbc5c5a6
Kyle Teeter <kyleteeter12@gmail.com>
2018-10-06 12:33:07 -0500

    Comments can now be added to the posts.

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -12,6 +12,9 @@ class CommentSection extends Component {
     constructor(props) {
         super(props);
         this.state = {
+          comments: props.data.comments,
+          images: props.image,
+          likes: props.likes,
           input: '',
         }
       }
@@ -24,6 +27,29 @@ class CommentSection extends Component {
         });
       };
 
+      submitHandler = (event) => {
+        if (this.state.input) {
+            this.setState({
+                comments: [
+                    ...this.state.comments,
+                    {
+                        username: "unknown",
+                        text: this.state.input
+                    }
+                ]
+            });
+            event.currentTarget.value = null;
+        }
+      };
+
+      handleSubmit = event => {
+        if (event.keyCode === 13 && event.shiftKey === false) {
+          if (this.state.text.length >= 1)
+            event.preventDefault();
+          this.submitHandler(event);
+        }
+      };
+
     render() {
     return (
         
@@ -32,13 +58,14 @@ class CommentSection extends Component {
                 <i class="far fa-heart fa-2x"></i><i class="far fa-comment fa-2x"></i>
                 </div>
                 <p><strong>{this.props.data.likes} likes</strong></p>
-                {this.props.data.comments.map((comment =>
+                {this.state.comments.map((comment =>
                     <div>
                         <p><strong>{comment.username}</strong><span> {comment.text}</span></p>
                     </div>
                     ))}
                 <p>{this.props.data.timestamp}</p>
-                <input value={this.state.input} onChange={this.commentHandler} defaultValue="Add a comment..."></input>
+                <input value={this.state.input} onChange={this.commentHandler} onKeyDown={this.enterHandler} defaultValue="Add a comment..."></input>
+                <button onClick={this.submitHandler}>Submit</button>
             </div>
             
     )
