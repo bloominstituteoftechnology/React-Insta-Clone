b05ed5aa14ba81a8ca42df7b0b9d21a06ec7332a
Julie Jonak <juliejonak10@gmail.com>
2018-10-06 15:49:02 -0500

    Adding new comment functionality works

--- instagram/src/CommentSection/CommentSection.js
+++ instagram/src/CommentSection/CommentSection.js
@@ -6,15 +6,40 @@ class CommentSection extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            comments: this.props.obj.comments
+            comments: this.props.obj.comments,
+            counter: this.props.obj.likes,
+            input: ''
         }
+    }
+
+    handleInput = (event) => {
+        const {value} = event.target;
+        this.setState({
+            input: value
+        });
+    }
+
+    addNewComment = (event) => {
+        event.preventDefault();
+        const newComment = {
+            username: "T-rex_Ninja",
+            text: this.state.input
+        }
+        this.setState({
+            comments: [...this.state.comments, newComment],
+            input: ''
+        })
+    }
+
+    addNewLike(event){
+        event.preventDefault();
     }
 
     render() {
         return (
             <div className="commentSection">
                 <div className="comment-icons">
-                    <a href="#"><i className="far fa-heart"></i></a>
+                    <a href="#" onClick={this.addNewLike}><i className="far fa-heart"></i></a>
                     <a href="#"><i className="far fa-comment"></i></a>
                 </div>
 
@@ -22,18 +47,20 @@ class CommentSection extends React.Component {
                     ( <p className="like">1 like</p> ) :
                     ( <p className="likes">{this.props.obj.likes} likes</p> )
                 }
+
                 <div className="commentsContainer">
-                    {this.props.obj.comments.map(item => {
+                    {this.state.comments.map(item => {
                         return (
                             <Comment key={item.text} username={item.username} text={item.text} />
                         )
                     })}
                 </div>
+
                 <p className="timestamp">{this.props.obj.timestamp}</p>
 
-                <form className="addComment">
-                <input placeholder="Add a comment..."></input>
-                <p>...</p>
+                <form className="addComment" onSubmit={this.addNewComment}>
+                    <input onChange={this.handleInput} placeholder="Add a comment..."></input>
+                    <p>...</p>
                 </form>
 
             </div>
