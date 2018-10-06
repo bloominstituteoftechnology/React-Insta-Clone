123dc106ba158a897330589f1d7433d82642923f
Glenn-David Daniel <d2rd@outlook.com>
2018-10-06 14:42:07 -0700

    WIP added new-comment input form, classNames to PostContainer js/css

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -22,8 +22,26 @@ const PostContainer = props => {
         <img src={props.imageUrl} alt="Post image" width={200} height={200} />
         {/* or use class name in css */}
       </div>
+      <div className="Icon-row">
+
+      </div>
       <div className="Comments">
-        <CommentSection commentProps={props.comments} />
+        <CommentSection commentProps={props.comments}  />
+      </div>
+      <div className="Timestamp">
+       {props.timestamp}
+      </div>
+      <div >
+   <form className="AddComment">
+      <input 
+      className="commentInput"
+      name="addComment"
+      type="text"
+      placeholder="Add a comment..."
+      // onChange={this.handleInputChange}
+      />
+      <input type="submit" value="Add" />
+    </form>
       </div>
     </div>
   );
