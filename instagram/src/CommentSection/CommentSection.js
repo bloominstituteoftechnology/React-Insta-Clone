0b7e130df2c5745293120d65785161d372909c2f
Julie Jonak <juliejonak10@gmail.com>
2018-10-06 15:12:40 -0500

    Turned CommentSection into a Class Component with its own state

--- instagram/src/CommentSection/CommentSection.js
+++ instagram/src/CommentSection/CommentSection.js
@@ -2,7 +2,15 @@ import React from 'react';
 import Comment from './Comment';
 import './CommentSection.css';
 
-const CommentSection = (props) => {
+class CommentSection extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            comments: this.props.obj.comments
+        }
+    }
+
+    render() {
         return (
             <div className="commentSection">
                 <div className="comment-icons">
@@ -10,18 +18,18 @@ const CommentSection = (props) => {
                     <a href="#"><i className="far fa-comment"></i></a>
                 </div>
 
-                { props.obj.likes === 1 ? 
+                { this.props.obj.likes === 1 ? 
                     ( <p className="like">1 like</p> ) :
-                    ( <p className="likes">{props.obj.likes} likes</p> )
+                    ( <p className="likes">{this.props.obj.likes} likes</p> )
                 }
                 <div className="commentsContainer">
-                    {props.obj.comments.map(item => {
+                    {this.props.obj.comments.map(item => {
                         return (
                             <Comment key={item.text} username={item.username} text={item.text} />
                         )
                     })}
                 </div>
-                <p className="timestamp">{props.obj.timestamp}</p>
+                <p className="timestamp">{this.props.obj.timestamp}</p>
 
                 <form className="addComment">
                 <input placeholder="Add a comment..."></input>
@@ -30,6 +38,7 @@ const CommentSection = (props) => {
 
             </div>
         )
+    }
 }
 
 export default CommentSection;
