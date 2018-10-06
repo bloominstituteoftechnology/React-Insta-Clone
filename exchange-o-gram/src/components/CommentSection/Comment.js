0f7409cb2b8c208de402f363b7e54241cd72fef6
Rick Fleming <elfeming111@yahoo.com>
2018-10-06 11:45:07 -0700

    Adding delete comment functionality

--- exchange-o-gram/src/components/CommentSection/Comment.js
+++ exchange-o-gram/src/components/CommentSection/Comment.js
@@ -3,10 +3,37 @@ import PropTypes from 'prop-types';
 import './CommentSection.css';
 
 class Comment extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            displayDeleteComment: false,
+            displayDeleteCommentstyle: {}
+        }
+    }
+    
+    askForDelete = (event)=>{
+        event.preventDefault();
+        const style = {
+            position: 'absolute',
+            top: event.clientY,
+            left: event.clientX
+        }
+        this.setState({
+            displayDeleteComment: true,
+            displayDeleteCommentstyle: style
+        })
+    }
+
+    deleteComment = (event)=>{
+        event.preventDefault();
+        // Pass in from App to delete comment from data
+        console.log("delete comment");
+    }
     render(){
         return(
             <div className="comment">
-                <p><strong>{this.props.comment.username}</strong> {this.props.comment.text}</p>
+                <p onClick={this.askForDelete}><strong>{this.props.comment.username}</strong> {this.props.comment.text}</p>
+                {this.state.displayDeleteComment ? <div className="delete-comment" onClick={this.deleteComment} style={this.state.displayDeleteCommentstyle}>Click To Delete Comment</div> : <span></span>}
             </div>
         )
     }
