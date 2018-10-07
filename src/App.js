67bd2f3c2049675e9a2a6de70ea58d23da731aa3
kurtve <kurt@vanetten.us>
2018-10-07 01:55:24 -0400

    Just show that we can read dummy-data.js and throw the stuff on the screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
+import dummyData from './dummy-data.js';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({posts: dummyData});
+  }
+
   render() {
+    if (!this.state.posts.length) {
+      return <h2>Loading ...</h2>;
+    }
+ 
     return (
       <div className="App">
-        <p>My app will live here.</p>
+        <div className="container">
+          <h1>Insta-message</h1>
+
+            {this.state.posts.map(post => {
+              return (
+              <div key={post.timestamp} className="post">
+                <p>
+                  <img className="post-thumb" src={post.thumbnailUrl} alt="thumbnail" />
+                  {post.username}
+                </p>
+                <img className="post-img" src={post.imageUrl} alt="image" />
+                <div className='like-symbol'>
+                  <span className="far fa-heart"></span>
+                  <span className="far fa-comment fa-flip-horizontal"></span>
+                </div>
+                <div className='like-count'>
+                  {post.likes} likes
+                </div>
+              </div>
+              );
+            })}
+          
+        </div>
       </div>
-    );
+    )
+    ;
   }
 }
 
