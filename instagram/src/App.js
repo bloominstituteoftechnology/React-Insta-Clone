7ac40a7644301788c59745db1230c0f83d7a329c
Matthew Wright <matthewbwright@gmail.com>
2018-10-08 13:18:35 -0700

    adds images for the project

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PostsPage from '../components/PostContainer/PostsPage';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      search: ''
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <PostsPage />
       </div>
     );
   }
