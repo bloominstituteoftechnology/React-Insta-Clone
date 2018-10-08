55bc2c7fd06897303f8adbfa3a947122740a32d0
Drewsup123c <drewsup123c@gmail.com>
2018-10-08 15:38:45 -0600

    added logo to app.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from "./components/SearchBarContent/SearchBar"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <div className="top-header">
+          <logo />
+          <SearchBar />
+        </div>
       </div>
     );
   }
--- instagram/src/components/SearchBarContent/SearchBar.js
+++ instagram/src/components/SearchBarContent/SearchBar.js
@@ -3,7 +3,11 @@ import React from 'react';
 const SearchBar = props =>{
     return(
         <div className="search-bar">
-            
+            <header>
+                <img src ="http://localhost:3000/src/logo.svg" />
+            </header>
         </div>
     )
 }
+
+export default SearchBar;
