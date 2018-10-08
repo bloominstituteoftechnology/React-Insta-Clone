fb9ea5da9249d5d8884c73e20336b39d5e53f401
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-10-08 16:13:07 -0700

    Nav logo built and styled

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import SearchBar from "./components/SearchBar/SearchBar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <SearchBar />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavLogo from "./NavLogo";
+
+const SearchBar = () => {
+  return (
+    <div>
+      {/* Logo component */}
+      <NavLogo />
+      {/* Search component */}
+      {/* Icons component */}
+    </div>
+  );
+};
+
+export default SearchBar;
