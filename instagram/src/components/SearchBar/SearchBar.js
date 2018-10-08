f7c6b165225f00c78ede74dc94943f33838491cc
Jasmine Galang <jsmn.glng@gmail.com>
2018-10-08 15:04:13 -0700

    Finished styling SearchBar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import logo from './img/instagram.png';
+import './SearchBar.css';
 
 const SearchBar = props => {
   return (
-    <div className="Search-bar-container">
-      <h1>Instagram</h1>
-      <input type="text" placeholder="Search" />
+    <div className="Fixed-navbar">
+      <div className="Search-bar-container">
+        <header>
+          <i className="fab fa-instagram fa-2x icon"></i>
+          <div className="divider"></div>
+          <img src={logo} alt="Instagram logo" />
+        </header>
+
+        <input type="text" placeholder="&#61442; Search" />
+
+        <div className="Menu">
+          <i className="far fa-compass fa-lg icon"></i>
+          <i className="far fa-heart fa-lg icon"></i>
+          <i className="far fa-user fa-lg icon"></i>
+        </div>
+      </div>
     </div>
   );
 };
