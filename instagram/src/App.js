d7d853185b81f76e9607aabf3e35b9bdd1f63946
Wanda <doubleuevans@gmail.com>
2018-10-08 14:49:57 -0500

    project setup

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import dummyData from './dummy-data'
+
 class App extends Component {
   render() {
     return (
