0f10f19cbd821cca309042359e6f34eaa31a2bb3
Stublink <chrisrey95@gmail.com>
2018-10-09 16:06:54 -0400

    Add comments

--- instagram/src/Components/CommentSection/CommentSection.js
+++ instagram/src/Components/CommentSection/CommentSection.js
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './CommentSection.css';
 import PostActions from './PostActions';
 import CommentContainer from './CommentContainer';
 
-const CommentSection = ({likes, timestamp, comments}) => {
-    console.log(timestamp);
-    console.log(Date.parse(timestamp));
-    return (
-        <div className="comment-section">
+class CommentSection extends Component {
+    constructor(props) {
+        super(props);
+        const { likes, timestamp, comments} = props;
+        this.state = {
+            likes,
+            timestamp,
+            comments,
+            commentValue: '',
+            username: 'chris'
+        }
+    }
+
+    addNewComment = (event) => {
+        event.preventDefault();
+        if(this.state.commentValue.length < 3) return;
+        this.setState(prevState => {
+            const newComment = {
+                username: this.state.username,
+                text: this.state.commentValue,
+            }
+            return {
+                comments: [...prevState.comments, newComment],
+                commentValue: '',
+            }
+        });
+    }
+
+    changeHandler = (event) => {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    render() {
+        return (
+            <div className="comment-section">
             <PostActions />
-            <div className="likes">{likes} likes</div>
-            <CommentContainer comments={comments} />
+            <div className="likes">{this.state.likes} likes</div>
+            <CommentContainer comments={this.state.comments} />
             <time>21 minutes ago</time>
-            <div className="comment-input">
-                <textarea placeholder="Add a comment..."/>
-                <span className="sprite options-icon"></span>
-            </div>
+            <form className="comment-input" onSubmit={this.addNewComment}>
+                <textarea 
+                    placeholder="Add a comment..." 
+                    value={this.state.commentValue}
+                    onChange={this.changeHandler}
+                    name="commentValue"
+                />
+                <button className="post-text">Post</button>
+            </form>
         </div>
     );
+}
 }
 
 export default CommentSection;
