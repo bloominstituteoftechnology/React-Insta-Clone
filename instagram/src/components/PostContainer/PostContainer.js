8aa0e0dd7f1e0252397c4be7d54e4cd24a57506e
Unknown <zachary.arney@gmail.com>
2018-10-09 15:57:30 -0500

    Added proptypes to PostContainer

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -24,3 +24,21 @@ const PostContainer = (props) => {
     </div>
   );
 };
+
+PostContainer.propTypes = {
+  post: PropTypes.shape({
+    username: PropTypes.string,
+    thumbnailUrl: PropTypes.string,
+    imageUrl: PropTypes.string,
+    likes: PropTypes.number,
+    timestamp: PropTypes.string,
+    comments: PropTypes.arrayOf(
+      PropTypes.shape({
+        username: PropTypes.string,
+        text: PropTypes.string,
+      })
+    ),
+  }),
+};
+
+export default PostContainer;
