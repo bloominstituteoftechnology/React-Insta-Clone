a7afaaf20343082c9a40ae6dd98a593427e8eb0b
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-10-08 18:28:01 -0700

    adjustments to nav items spacing

--- instagram/src/components/SearchBar/SearchInput.js
+++ instagram/src/components/SearchBar/SearchInput.js
@@ -4,7 +4,7 @@ import "./SearchBar.css";
 const SearchInput = () => {
   return (
     <div>
-      <i className="fas fa-search" />
+      {/* <i className="fas fa-search" /> */}
       <input className="search" type="text" placeholder="Search" />
     </div>
   );
