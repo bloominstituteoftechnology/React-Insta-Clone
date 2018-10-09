1c31d9edb17173039b9d7e4bc15641e2adf1af3c
Lee Len Formento <leebatef@gmail.com>
2018-10-08 21:12:33 -0400

    Search

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
+import Posts from './components/PostContainer/Posts';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: dummyData,
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <SearchBar />
+        <Posts dummyData={this.state.dummyData}/>
       </div>
     );
   }
---
+++ instagram/src/components/SearchBar/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+let Search = props => {
+    return (
+           <div>
+                <i className="fas fa-search" />
+                <input type="text" placeholder="Search" />
+           </div>
+    )
+}
+
+export default Search;
