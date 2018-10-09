6af8243f8f4d20f74d6876f17d650a22553909fe
Jasmine Galang <jsmn.glng@gmail.com>
2018-10-09 13:40:46 -0700

    Added like post feature

--- instagram/src/components/PostsContainer/Post.js
+++ instagram/src/components/PostsContainer/Post.js
@@ -2,32 +2,52 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Posts.css';
 import CommentSection from '../CommentSection/CommentSectionContainer';
-// import CommentForm from '../CommentSection/CommentForm';
 
-const Post = props => {
-  const { username, thumbnailUrl, imageUrl, likes, comments, timestamp } = props.post;
-  return (
-    <div className="Post">
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: props.post
+    }
+  }
 
-      <header className="Post-header">
-        <img src={thumbnailUrl} alt="user thumbnail" />
-        <h2>{username}</h2>
-      </header>
+  incrementLike = (event) => {
+    event.preventDefault();
 
-      <section className="Post-body">
-        <img src={imageUrl} alt="post img" />
-        <div className="Post-action">
-          <i className="far fa-heart fa-2x icon"></i>
-          <i className="far fa-comment fa-2x icon"></i>
-        </div>
+    const post = JSON.parse(JSON.stringify(this.state.post))
+    post.likes = post.likes + 1;
 
-        <p className="likes">{likes} likes</p>
-      </section>
+    this.setState({ post });
+  }
 
-      <CommentSection comments={comments} timestamp={timestamp} />
-    </div>
-  );
-};
+  render() {
+    const { username, thumbnailUrl, imageUrl, likes, comments, timestamp } = this.state.post;
+    return (
+      <div className="Post">
+
+        <header className="Post-header">
+          <img src={thumbnailUrl} alt="user thumbnail" />
+          <h2>{username}</h2>
+        </header>
+
+        <section className="Post-body">
+          <img src={imageUrl} alt="post img" />
+          <div className="Post-action">
+            <div className="like-btn" onClick={this.incrementLike}>
+              <i className="far fa-heart fa-2x icon"></i>
+            </div>
+            {/* <i class="fas fa-heart fa-2x liked-btn icon show"></i> */}
+            <i className="far fa-comment fa-2x icon comment-btn"></i>
+          </div>
+
+          <p className="likes">{likes} likes</p>
+        </section>
+
+        <CommentSection comments={comments} timestamp={timestamp} />
+      </div>
+    );
+  }
+}
 
 Post.propTypes = {
   post: PropTypes.shape({
