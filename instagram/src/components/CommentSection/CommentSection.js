82b90be787255c0d6e118d5ebb580655668bbf4d
Axolotl <samuelmachat@gmail.com>
2018-10-09 17:17:45 -0400

    added proptypes and comment display to commentsection

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,5 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const CommentSection = () => <p>comment section</p>
+const CommentSection = ({ comments }) => (
+  <div>
+    {comments.map(({ username, text }, i) => (
+      <div key={i}>
+        <p>{username}</p>
+        <p>{text}</p>
+      </div>
+    ))}
+  </div>
+)
+
+CommentSection.propTypes = {
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      username: PropTypes.string,
+      text: PropTypes.string
+    })
+  )
+}
 
 export default CommentSection
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -27,33 +27,3 @@ PostContainer.propTypes = {
 }
 
 export default PostContainer
-
-/*
-
-  {
-    username: 'philzcoffee',
-    thumbnailUrl:
-      'https://tk-assets.lambdaschool.com/ecd33d34-c124-4b75-92d2-e5c52c171ed8_11201517_887808411287357_1307163552_a.jpg',
-
-    imageUrl:
-      'https://tk-assets.lambdaschool.com/69cf901b-f96d-466e-a745-ff2a01effac9_philz-image.jpg',
-    likes: 400,
-    timestamp: 'July 17th 2017, 12:42:40 pm',
-    comments: [
-      {
-        username: 'philzcoffee',
-        text:
-          "We've got more than just delicious coffees to offer at our shops!"
-      },
-      {
-        username: 'biancasaurus',
-        text: 'Looks delicious!'
-      },
-      {
-        username: 'martinseludo',
-        text: "Can't wait to try it!"
-      }
-    ]
-  },
-
-  */
