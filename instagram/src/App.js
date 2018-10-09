001da4789ec55c654843b0fd381317e675f30614
Unknown <zachary.arney@gmail.com>
2018-10-09 15:56:09 -0500

    Imported and added Searchbar and PostContainer components to App

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import dummyData from './dummy-data.js';
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+        <SearchBar />
+        {dummyData.map((post) => {
+          return <PostContainer post={post} key={post.timestamp} />;
+        })}
           </p>
           <a
             className="App-link"
