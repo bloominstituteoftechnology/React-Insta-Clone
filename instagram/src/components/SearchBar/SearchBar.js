de3e1902746c26a6b23357118e1b14aff107c84d
Elvis Ibarra <ielvisd@gmail.com>
2018-10-08 23:03:31 -0700

    still trying with the images

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -3,9 +3,10 @@ import "./SearchBar.css";
 
 const SearchBar = props => {
   console.log("Search bar props are:", { props });
+  console.log("Search bar props are:", { props });
   return (
     <div className="SearchBar">
-      <img className="IG-logo" src='../images/ig_search_bar' alt="IG logo"/>
+      <img className="IG-logo" src='./images/instagramlogo.svg' alt="IG logo Test"/>
       <h1>I am a Search Bar</h1>
     </div>
   );
