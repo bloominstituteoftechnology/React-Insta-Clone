c27e2ca745d635f565987e0749c05ad42a3f6c98
kurtve <kurt@vanetten.us>
2018-10-08 23:27:50 -0400

    Add type checking with PropTypes

--- src/components/CommentSection/CommentSection.js
+++ src/components/CommentSection/CommentSection.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './CommentSection.css';
 
 class CommentSection extends Component {
@@ -21,5 +22,17 @@ class CommentSection extends Component {
     );
   }
 }
+
+
+CommentSection.propTypes = {
+  ts: PropTypes.string.isRequired,
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      username: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
+
 
 export default CommentSection;
--- src/components/PostContainer/PostContainer.js
+++ src/components/PostContainer/PostContainer.js
@@ -31,35 +31,19 @@ class PostContainer extends Component {
 
 
 PostContainer.propTypes = {
-  post: PropTypes.string.isRequired
+  post: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    thumbnailUrl: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+    timestamp: PropTypes.string.isRequired,
+    comments: PropTypes.arrayOf(PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired
+      })
+    ).isRequired
+  }).isRequired
 };
 
-/*
-  {
-    username: "philzcoffee",
-    thumbnailUrl:
-      "https://tk-assets.lambdaschool.com/ecd33d34-c124-4b75-92d2-e5c52c171ed8_11201517_887808411287357_1307163552_a.jpg",
-
-    imageUrl:
-      "https://tk-assets.lambdaschool.com/69cf901b-f96d-466e-a745-ff2a01effac9_philz-image.jpg",
-    likes: 400,
-    timestamp: "July 17th 2017, 12:42:40 pm",
-    comments: [
-      {
-        username: "philzcoffee",
-        text:
-          "We've got more than just delicious coffees to offer at our shops!"
-      },
-      {
-        username: "biancasaurus",
-        text: "Looks delicious!"
-      },
-      {
-        username: "martinseludo",
-        text: "Can't wait to try it!"
-      }
-    ]
-  }
-*/
 
 export default PostContainer;
