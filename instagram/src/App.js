9f873caceff7c8d9ea033cf3fe9aa8542fb09eb2
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-09 15:05:49 -0500

    App js / Search Bar Container

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import dummyData from './dummy-data';
+import SearchBarContainer from './components/SearchBar/SearchBarContainer';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: dummyData
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <SearchBarContainer />
       </div>
     );
   }
