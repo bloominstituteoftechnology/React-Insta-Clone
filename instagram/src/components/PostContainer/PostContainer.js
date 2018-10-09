713b326fba68cb31c4a9830584d1a08ca51a8a04
Mindysueasaurus <mymindyyeager@gmail.com>
2018-10-09 16:13:38 -0500

    Filing out the post container map and post

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Post from './Post';
 
-const PostContainer = props => {
+const PostContainer = (props, index) => {
+  
   return (
     <div>
-      {props.posts.map(post => key={index} post={post})}
+      {props.posts.map(post => 
+        <Post {post.likes} />)}
+      
     </div>
   )
 }
