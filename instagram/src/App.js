b7b4e1bcbdc9b5a0c1fd9bb5d4fa131b486fbb30
Katie Cleary <katiecleary@airmaster.lan>
2018-10-09 16:08:33 -0400

    built component file structure, imported dummy data

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import data from './dummy-data.js';
+import dummyData from './dummy-data.js';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData:[]
+    };
+  }
+
+
   render() {
+    console.log(dummyData);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
