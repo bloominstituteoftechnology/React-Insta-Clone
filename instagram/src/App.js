c737bb0f7f787cf28a4eb2bb56a043cf7e099145
Mindysueasaurus <mymindyyeager@gmail.com>
2018-10-09 15:09:04 -0500

    files moved

---
+++ instagram/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import DummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <SearchBar />
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <div>
+      search bar
+    </div>
+  )
+}
+
+export default SearchBar;
