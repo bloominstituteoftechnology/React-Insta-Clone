d9c52ed6fdcb8344507deb363d89a90ee32ff425
Gannon <gkleblanc@gmail.com>
2018-10-09 18:29:48 -0400

    I don't think I saved everything correctly in my last push, here is teh comment section finished, the like section added, and the post section being worked on

--- instagram/src/components/PostContainer/Posts.js
+++ instagram/src/components/PostContainer/Posts.js
@@ -1,27 +1,55 @@
 import React from "react";
+import PropTypes from "prop-types";
 import PostHeader from "./PostHeader";
 import CommentSection from "../CommentSection/CommentSection";
+import LikeSection from "./LikeSection";
 
 import "./PostContainerStyle.css";
 
-const Post = props => {
-  return (
-    <div className="postBorder">
-      <PostHeader
-        username={props.post.username}
-        thumbnailUrl={props.post.thumbnailUrl}
-      />
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: props.post.likes
+    };
+  }
 
-      <div className="postImageContainer">
-        <img
-          className="postImage"
-          alt="TheThumbnail"
-          src={props.post.imageUrl}
+  uplike = () => {
+    let likes = this.state.likes + 1;
+    this.setState({ likes });
+  };
+
+  render() {
+    return (
+      <div className="postBorder">
+        <PostHeader
+          username={this.props.post.username}
+          thumbnailUrl={this.props.post.thumbnailUrl}
         />
+
+        <div className="postImageContainer">
+          <img
+            className="postImage"
+            alt="TheThumbnail"
+            src={this.props.post.imageUrl}
+          />
+        </div>
+        <CommentSection
+          comments={this.props.post.comments}
+          postId={this.props.post.imageUrl}
+        />
+        <LikeSection uplike={this.uplike} likes={this.state.likes} />
       </div>
-      <CommentSection comments={props.post.comments} />
-    </div>
-  );
+    );
+  }
+}
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    username: PropTypes.string,
+    thumbnailUrl: PropTypes.string,
+    imageUrl: PropTypes.string
+  })
 };
 
 export default Post;
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -9,7 +9,7 @@ const SearchBar = () => {
       </div>
 
       <div>
-        <input type="text" placeholder="Search" onKeyDown={props.searchPosts} />
+        <input type="text" placeholder="Search" />
       </div>
     </div>
   );
