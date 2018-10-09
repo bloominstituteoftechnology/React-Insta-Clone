d73e296b02687dd8d44bacca325be6473bec4aba
Chris Juelfs <CRJuelfs24@comcast.net>
2018-10-09 04:00:17 -0500

    stuck! i think my addNewComment wont bind properly..

--- react-insta-clone-app/src/App.js
+++ react-insta-clone-app/src/App.js
@@ -2,39 +2,141 @@ import React from "react";
 import "./App.css";
 import dummyData from "./dummy-data";
 import SearchBar from "./components/SearchBar/SearchBar";
-import PostConainer from "./components/PostContainer/PostContainer"
+import PostContainer from "./components/PostContainer/PostContainer"
 import CommentSection from "./components/CommentSection/CommentSection"
 import heart from "./img/heart-2.svg";
 import chat from "./img/chat-46.svg";
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      data: [],
+      dataComments: [],
+    }
+  }
+  componentDidMount() {
+    let newData = this.state.data;
+    newData.push(dummyData);
+    // console.log(this.state.data[0].map(function(object){
+    //   return (
+    //     object.comments.map(function(comment){
+    //       return (
+    //         comment
+    //       )
+    //     })
+    //   )
+    // }))
+    let newCommentData = this.state.dataComments;
+   newCommentData.push(this.state.data[0].map(function(object){
+      return (
+        object.comments.map(function(comment){
+          return (
+            comment
+          )
+        })
+      )
+    }));
+    this.setState({
+      data: newData,
+      dataComments: newCommentData,
+    });
+    // console.log(this.state.data)
+    console.log(this.state.data[0])
+  }
+
+  addNewComment = (event, index) => {
+    event.preventDefault();
+    if (event.key === 'Enter'){
+        let commentsArr = this.state.dataComments[0];
+        let commentsList = commentsArr.map(function(commentGrp){
+          return (
+            commentGrp.map(function(comment){
+              return (
+                comment
+              )
+            })
+          )
+        })
+        commentsList.push({
+          username: "CJuelfs15",
+          text: event.target.value
+        });
+        this.setState({
+            dataComments: commentsList,
+        });
+        event.target.value = '';
+        console.log('works inside')
+    } 
+    console.log('works')
+  }
+  handleLikesIncrement = (event, index) => {
+    event.preventDefault();
+    this.state.data[0].map(function(post){
+      
+    })
+  }
+
+  // Day 1 refactored:
+  // render() {
+  //   return (
+  //     <div className='App'>
+  //       <SearchBar className='search-bar-container'/>
+  //       <div className='all-posts'>
+  //         {dummyData.map(function(item){
+  //           return (
+  //             <div className='post-container'>
+  //               <PostContainer src2={item.imageUrl} src1={item.thumbnailUrl} alt={item.username} title={item.username} srcIcon1={heart} srcIcon2={chat} altIcon1='heart-icon' altIcon2='chat-icon' likesCounter={item.likes}/>
+  //               {/* <div className='post-title'>
+  //                 <img className='post-title-thumbnail' src={item.thumbnailUrl} alt={item.username} />
+  //                 <span className='post-title-username'>{item.username}</span>
+  //               </div> */}
+  //               {/* <img className='post-image' src={item.imageUrl} alt={item.username}/> */}
+  //               <div className='comment-section'>
+  //                 {item.comments.map(function(comment){
+  //                   return (
+  //                     // <div className='individual-comment'>
+  //                     //   <span className='comment-username'>{comment.username}</span>
+  //                     //   <span className='comment-text'>{comment.text}</span>
+  //                     // </div>
+  //                     <CommentSection commentUsername={comment.username} commentText={comment.text} />
+  //                   );
+  //                 })}
+  //                 <input placeholder='Add a comment...' className='comment-input'></input>
+  //               </div>
+  //             </div>
+  //           );
+  //         })}
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
+  //Day 2 (data thru state):
   render() {
     return (
       <div className='App'>
         <SearchBar className='search-bar-container'/>
         <div className='all-posts'>
-          {dummyData.map(function(item){
+          {this.state.data.map(function(item){
             return (
-              <div className='post-container'>
-                <PostConainer src2={item.imageUrl} src1={item.thumbnailUrl} alt={item.username} title={item.username} srcIcon1={heart} srcIcon2={chat} altIcon1='heart-icon' altIcon2='chat-icon' likesCounter={item.likes}/>
-                {/* <div className='post-title'>
-                  <img className='post-title-thumbnail' src={item.thumbnailUrl} alt={item.username} />
-                  <span className='post-title-username'>{item.username}</span>
-                </div> */}
-                {/* <img className='post-image' src={item.imageUrl} alt={item.username}/> */}
-                <div className='comment-section'>
-                  {item.comments.map(function(comment){
-                    return (
-                      // <div className='individual-comment'>
-                      //   <span className='comment-username'>{comment.username}</span>
-                      //   <span className='comment-text'>{comment.text}</span>
-                      // </div>
-                      <CommentSection commentUsername={comment.username} commentText={comment.text} />
-                    );
-                  })}
-                  <input placeholder='Add a comment...' className='comment-input'></input>
-                </div>
-              </div>
+              item.map(function(object){
+                return (
+                  <div key={object.timestamp} className='post-container'>
+                    <PostContainer src1={object.thumbnailUrl} src2={object.imageUrl} alt={object.username} title={object.username} srcIcon1={heart} srcIcon2={chat} altIcon1='heart-icon' altIcon2='chat-icon' likesCounter={object.likes} />
+                    <div className='comment-section'>
+                      {object.comments.map(function(comment){
+                        return (
+                          <CommentSection commentUsername={comment.username} commentText={comment.text} />
+                        );
+                      })}
+                      <form onSubmit = {(index) => this.addNewComment}>
+                        <input placeholder='Add a comment...' className='comment-input'></input>
+                      </form>
+                    </div>
+                  </div>
+                )
+              })
             );
           })}
         </div>
--- react-insta-clone-app/src/components/CommentSection/CommentSection.js
+++ react-insta-clone-app/src/components/CommentSection/CommentSection.js
@@ -2,11 +2,50 @@ import React from 'react';
 import './CommentSection.css';
 
 
-export default function CommentSection(props){
-    return (
-        <div className='individual-comment'>
-                <span className='comment-username'>{props.commentUsername}</span>
-                <span className='comment-text'>{props.commentText}</span>
-        </div>
-    );
+export default class CommentSection extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    // addNewComment = event => {
+    //     if (event.key === 'Enter'){
+    //         let commentsArr = this.props.dataComments[0];
+    //         commentsArr.push({username: "CJuelfs15", text: event.target.value});
+    //         this.setState({
+    //             dataComments: commentsArr,
+    //         });
+    //         event.target.value = '';
+    //     }
+    // }
+    addNewComment = (event, index) => {
+        event.preventDefault();
+        if (event.key === 'Enter'){
+            let commentsArr = this.props.dataComments[0];
+            let commentsList = commentsArr.map(function(commentGrp){
+              return (
+                commentGrp.map(function(comment){
+                  return (
+                    comment
+                  )
+                })
+              )
+            })
+            commentsList.push({
+              username: "CJuelfs15",
+              text: event.target.value
+            });
+            this.setState({
+                dataComments: commentsList,
+            });
+            event.target.value = '';
+        } 
+        console.log('works')
+      }
+    render(){
+        return (
+            <div className='individual-comment'>
+                <span className='comment-username'>{this.props.commentUsername}</span>
+                <span className='comment-text'>{this.props.commentText}</span>
+            </div>
+        );
+    }
 }
