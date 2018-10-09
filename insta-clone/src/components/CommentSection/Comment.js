99321e07c73a821621aa6d32278899ce53e88790
SeanOne <basement365@gmail.com>
2018-10-08 20:19:32 -0700

    props transfering correctly

--- insta-clone/src/components/CommentSection/Comment.js
+++ insta-clone/src/components/CommentSection/Comment.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const Comment = props => {
-    return <div>Comment</div>
+    return (
+        <p>{props.comment.username} {props.comment.text}</p>
+    )
+}
+
+Comment.propTypes = {
+    username: PropTypes.string,
+    text: PropTypes.string,
 }
 
 export default Comment;
