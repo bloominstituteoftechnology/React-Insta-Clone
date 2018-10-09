44f6bd993c936448319df058a49855db8422a378
Blake <koenneckerb@gmail.com>
2018-10-08 20:42:01 -0700

    Added icons.

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,25 +1,43 @@
 import React from "react";
+import "./SearchBar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCompass } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const SearchBar = () => (
   <div className="search-bar">
-    <img
-      src="https://www.edigitalagency.com.au/wp-content/uploads/instagram-glyph-logo-icon-png-black-white-300x300.png"
-      alt="camera logo"
-      className="camera-logo"
-    />
-    <img
-      src="https://www.edigitalagency.com.au/wp-content/uploads/instagram-logo-text-black-png-768x221.png"
-      alt="instagram text logo"
-      className="text-logo"
-    />
-    <input type="text" name="search bar" placeholder="Search" />
-    <FontAwesomeIcon icon={faCompass} />
-    <FontAwesomeIcon icon={faHeart} />
-    <FontAwesomeIcon icon={faUserCircle} />
+    <div className="logos">
+      <img
+        src="https://www.edigitalagency.com.au/wp-content/uploads/instagram-glyph-logo-icon-png-black-white-300x300.png"
+        alt="camera logo"
+        className="camera-logo"
+      />
+      <img
+        src="https://www.edigitalagency.com.au/wp-content/uploads/instagram-logo-text-black-png-768x221.png"
+        alt="instagram text logo"
+        className="text-logo"
+      />
+    </div>
+    <div className="search-text">
+      <FontAwesomeIcon icon={faSearch} />
+      <input type="text" name="search bar" placeholder="Search" />
+    </div>
+    <div className="icons">
+      <FontAwesomeIcon
+        icon={faCompass}
+        style={{ height: "25px", width: "25px" }}
+      />
+      <FontAwesomeIcon
+        icon={faHeart}
+        style={{ height: "25px", width: "25px" }}
+      />
+      <FontAwesomeIcon
+        icon={faUser}
+        style={{ height: "25px", width: "25px" }}
+      />
+    </div>
   </div>
 );
 
