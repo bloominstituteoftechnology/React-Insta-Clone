d88818dcad3f22af91d39d67a62e220eb6b0978a
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-09 17:25:31 -0400

    built post component

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -1,2 +1,20 @@
 import React from 'react';
+import "../App.css"
+
+const Post = props => {
+    return(
+        <div className="post">
+
+            <div className="thumbnail-profile">
+                <img src={props.thumbnail}/>
+                <p>{props.username}</p>
+            </div>
+
+            <div className="image">
+                <img src={props.image} />
+            </div>
+            
+        </div>
+    );
+}
 
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const PostContainer = () => {
     return(
-        <div>
+        <div className="post-container">
+            {}
         </div>
     );
 }
