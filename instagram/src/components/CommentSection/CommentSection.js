8d2e6a04c4c24eae88b19749571dd8e619fc9edd
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-10-09 13:49:11 -0700

    change commentsection to a class

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,51 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import NewComment from './NewComment.js'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './CommentSection.css';
 
-addNewComment = (event, index) => {
-    this.setState({ 
-        dummyData: [
-            ...this.props.dummyData,
-            {
-                comment: this.props.newComment
-            }
-        ],
-        newComment: ''
-    });
-};
+class CommentSection extends Component {
+    constructor(props) {
+        super(props);
+        this.state = props.item;
+    }
 
-const CommentSection = props => {
-    return (
-        <div className="commentContainer">
-            <div className="commentIcons">
-                <FontAwesomeIcon className="commentIcon" icon={['far', 'heart']} size="2x" />
-                <FontAwesomeIcon className="commentIcon" icon={['far', 'comment']} size="2x" />
+    addNewComment = (event, index) => {
+        event.preventDefault();
+        this.setState({ 
+            item: [
+                ...this.state.item,
+                {
+                    comment: this.state.newComment
+                }
+            ],
+            newComment: ''
+        });
+    };
+
+    render() {
+        return (
+            <div className="commentContainer">
+                <div className="commentIcons">
+                    <FontAwesomeIcon className="commentIcon" icon={['far', 'heart']} size="2x" />
+                    <FontAwesomeIcon className="commentIcon" icon={['far', 'comment']} size="2x" />
+                </div>
+                <div className="commentSection">
+                    <h4>{this.state.likes} likes</h4>
+                        
+                    <div className="messages">
+                        <h4>{this.state.comments[0].username}</h4>
+                        <p>{this.state.comments[0].text}</p>
+                    </div>
+    
+                    <div className="messages">
+                        <h4>{this.state.comments[1].username}</h4>
+                        <p>{this.state.comments[1].text}</p>
+                    </div>
+    
+                    <div className="messages">
+                        <h4>{this.state.comments[2].username}</h4>
+                        <p>{this.state.comments[2].text}</p>
+                    </div>
+    
+                    <h4>{this.state.timestamp}</h4>
+                </div>
+                <NewComment newComment={this.state.newComment} addNewComment={this.addNewComment}/>
             </div>
-            <div className="commentSection">
-                <h4>{props.item.likes} likes</h4>
-                    
-                <div className="messages">
-                    <h4>{props.item.comments[0].username}</h4>
-                    <p>{props.item.comments[0].text}</p>
-                </div>
-
-                <div className="messages">
-                    <h4>{props.item.comments[1].username}</h4>
-                    <p>{props.item.comments[1].text}</p>
-                </div>
-
-                <div className="messages">
-                    <h4>{props.item.comments[2].username}</h4>
-                    <p>{props.item.comments[2].text}</p>
-                </div>
-
-                <h4>{props.item.timestamp}</h4>
-            </div>
-            <NewComment newComment={props.newComment} addNewComment={this.addNewComment}/>
-        </div>
-    );
+        );
+    }
 }
 
 CommentSection.propTypes = {
--- instagram/src/components/CommentSection/NewComment.js
+++ instagram/src/components/CommentSection/NewComment.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import './NewComment.css';
 
-// const NewComment = props => {
-//     return (
-//         <form className="newComment" onSubmit={props.newComment}>
-//             <input id="newCmt" type="text" placeholder="Add a comment..."/>
-//             <label className="commentLabel" htmlFor="newCmt">...</label>
-//         </form>
-//     );
-// }
-
-function NewComment(props) {
+const NewComment = props => {
     return (
-        <div className="App">
-            <form className="newComment" onSubmit={props.newComment}>
+        <form className="newComment" onSubmit={props.newComment}>
             <input id="newCmt" type="text" placeholder="Add a comment..."/>
             <label className="commentLabel" htmlFor="newCmt">...</label>
         </form>
-      </div>
     );
 }
+
+// function NewComment(props) {
+//     return (
+//         <div className="App">
+//             <form className="newComment" onSubmit={props.newComment}>
+//                 <input id="newCmt" type="text" placeholder="Add a comment..."/>
+//                 <label className="commentLabel" htmlFor="newCmt">...</label>
+//             </form>
+//       </div>
+//     );
+// }
 
 export default NewComment;
