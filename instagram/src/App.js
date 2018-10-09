ea14a99e79b0e7aec9cf43f98c5e76df22078c34
Yanrong Xu <xuyanrong@icloud.com>
2018-10-09 17:34:15 -0400

    particle done with MVP

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
-import SearchBar from "./components/SearchBar/SearchBar";
-import PostContainer from "./components/PostContainer/PostContainer";
-import dummyData from "./dummy-data.js";
 import './App.css';
+import dummyData from './dummy-data';
+import PostsContainer from './components/PostsContainer/PostsContainer';
+import SearchBar from './components/SearchBar/SearchBarContainer';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {dummyData};
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      filteredPosts: []
+    };
   }
-
+  componentDidMount() {
+    this.setState({ posts: dummyData });
+  }
+  searchPostsHandler = e => {
+    const posts = this.state.posts.filter(p => {
+      if (p.username.includes(e.target.value)) {
+        return p;
+      }
+    });
+    this.setState({ filteredPosts: posts });
+  };
   render() {
     return (
       <div className="App">
-        <SearchBar />
-        <PostContainer posts={this.state.dummyData} />
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          searchPosts={this.searchPostsHandler}
+        />
+        <PostsContainer
+          posts={
+            this.state.filteredPosts.length > 0
+              ? this.state.filteredPosts
+              : this.state.posts
+          }
+        />
       </div>
     );
   }
