089083c79f57e9c938f913b0634c0f7e4783d29d
Adam <ajmboosh@gmail.com>
2018-10-09 15:21:22 -0500

    day2 goal adding comments complete: using changeHandler function for AddComment.js' input and addNewComment function, users can add comment to list of individual posts

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Comment from "./Comment";
 import PropTypes from "prop-types";
-
+import AddComment from "./AddComment";
 import "./CommentSection.css";
 
 class CommentSection extends React.Component {
@@ -9,9 +9,37 @@ class CommentSection extends React.Component {
     super(props);
     this.state = {
       comments: props.user.comments,
-      timestamp: props.user.timestamp
+      timestamp: props.user.timestamp,
+      newComment: ""
     };
   }
+
+  addNewComment = (event, index) => {
+    //function needs to add the comment that is on the event object to the post at the index number
+    event.preventDefault();
+    if (this.state.newComment !== "") {
+      let newCommentList = [
+        ...this.state.comments,
+        {
+          id: index,
+          username: "BillyEveryTeen",
+          text: this.state.newComment
+        }
+      ];
+
+      this.setState({
+        comments: newCommentList,
+        newComment: ""
+      });
+    }
+  };
+
+  changeHandler = (key, value) => {
+    this.setState({
+      [key]: value
+    });
+  };
+
   render() {
     return (
       <div className="comment-section">
@@ -19,7 +47,12 @@ class CommentSection extends React.Component {
           return <Comment key={comment.id} comment={comment} />;
         })}
         <p className="timestamp">{this.state.timestamp}</p>
-        <input className="add-comment" placeholder="Add a comment..." />
+        <AddComment
+          addNewComment={this.addNewComment}
+          changeHandler={this.changeHandler}
+          newComment={this.state.newComment}
+          comments={this.state.comments}
+        />
       </div>
     );
   }
