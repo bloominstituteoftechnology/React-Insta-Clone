9155e6fbf27a65db14b7943d520fa0ea313ff73d
hraya <hraya@lssu.edu>
2018-10-08 21:53:54 -0500

    Completed Styling

--- insta-clone/src/components/CommentSection/CommentSection.js
+++ insta-clone/src/components/CommentSection/CommentSection.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import moment from 'moment';
+import './CommentSection.css';
 import Comment from './Comment'
 
 const CommentSection = (props) => {
     return(
-        <>
+        <div className="comment-section">
         <i class="far fa-heart icon"></i>
         <i class="far fa-comment icon comment"></i>
-        <p className="likes"><strong>{props.likes}</strong></p>
+        <p className="likes"><strong>{props.likes} likes</strong></p>
         
         {props.comArray.map(item =>{
             return(
                 <Comment key={item.text} comUserName={item.username} comText={item.text} />
             )
         })}
-        <p>{moment().startOf('hour').fromNow() }</p>
-        <input className="comment-input" placeholder="Add Comment..."></input>
-        <i class="fas fa-ellipsis-h icon"></i>
-        </>
+        <p className="timestamp">{moment().startOf('hour').fromNow() }</p>
+        <input className="comment-input" placeholder=" &nbsp; Add Comment..."></input>
+        <p className="ellipise"> <i class="fas fa-ellipsis-h icon"></i></p>
+        </div>
     )
 }
 
--- insta-clone/src/components/PostContainer/PostContainer.js
+++ insta-clone/src/components/PostContainer/PostContainer.js
@@ -9,7 +9,7 @@ import CommentSection from '../CommentSection/CommentSection';
 const PostContainer = (props) =>{
     return(
         <div className="post-container">
-            <Header thumbnail={props.obj.thumbnailUrl} username={props.obj.username}/>
+            <Header  thumbnail={props.obj.thumbnailUrl} username={props.obj.username}/>
             <Body postPic={props.obj.imageUrl} />
             <CommentSection comArray={props.obj.comments} likes={props.obj.likes}/>
         </div>
