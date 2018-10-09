a44fd9af5ae0faaf8deae723a44f7255da2df788
Axolotl <samuelmachat@gmail.com>
2018-10-09 18:41:25 -0400

    implemented addlike function to postcontainer

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,24 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import CommentSection from '../CommentSection/CommentSection'
 
 import './PostContainer.css'
 
-const PostContainer = ({
-  post: { username, thumbnailUrl, imageUrl, likes, timestamp, comments }
-}) => (
-  <div className="post-container">
-    <div className="post-header">
-      <img className="user-thumbnail" src={thumbnailUrl} alt="user thumbnail" />
-      <p className="username">{username}</p>
-    </div>
+class PostContainer extends Component {
+  constructor({
+    post: { username, thumbnailUrl, imageUrl, likes, timestamp, comments }
+  }) {
+    super()
+    this.state = {
+      username,
+      thumbnailUrl,
+      imageUrl,
+      likes,
+      timestamp,
+      comments
+    }
+  }
 
-    <img className="post-image" src={imageUrl} alt="post" />
-    <p className="likes-text">{likes} likes</p>
-    <CommentSection comments={comments} />
-    <p className="timestamp">{timestamp}</p>
-  </div>
-)
+  addLike = () => {
+    this.setState({ likes: this.state.likes + 1 })
+  }
+
+  render() {
+    const { username, thumbnailUrl, imageUrl, likes, timestamp, comments } = this.state
+    const { addLike } = this
+
+    return (
+      <div className="post-container">
+        <div className="post-header">
+          <img
+            className="user-thumbnail"
+            src={thumbnailUrl}
+            alt="user thumbnail"
+          />
+          <p className="username">{username}</p>
+        </div>
+
+        <img className="post-image" src={imageUrl} alt="post" />
+        <p onClick={addLike} className="likes-text">{likes} likes</p>
+        <CommentSection comments={comments} />
+        <p className="timestamp">{timestamp}</p>
+      </div>
+    )
+  }
+}
 
 PostContainer.propTypes = {
   post: PropTypes.shape({
