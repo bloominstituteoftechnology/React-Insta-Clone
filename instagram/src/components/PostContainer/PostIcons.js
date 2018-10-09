fc11999c541e9f7c4a23db6774d545ddc245e6c2
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-09 14:02:03 -0600

    added counter to heart like button

--- instagram/src/components/PostContainer/PostIcons.js
+++ instagram/src/components/PostContainer/PostIcons.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
- const PostIcons = (props) => {
+
+
+
+ class PostIcons extends React.Component  {
+     constructor(props){
+         super(props);
+
+         this.state = {
+             likes: 0,
+             counter: 1
+         }
+     }
+     componentDidMount(){
+         this.setState({likes: this.props.likes});
+     }
+
+     updateLikes = (event) => {
+         this.setState({
+             likes: this.state.likes + this.state.counter
+         })
+     }
+
+     render(){
   return (
     <section className='icon-container'>
-      <section className='post-icons'>
-        <FontAwesomeIcon icon='heart' />
-        <FontAwesomeIcon icon='comment-alt' />
-      </section>
-      <section className='post-likes'>
-      {props.likes} <span>likes</span>
-      </section>
+        <section className='post-icons'>
+          <FontAwesomeIcon icon='heart' onClick={this.updateLikes}/>
+          <FontAwesomeIcon icon='comment-alt' />
+        </section>
+        <section className='post-likes'>
+        {this.state.likes} <span>likes</span>
+        </section>
     </section>
   );
 }
+ }
  export default PostIcons;
