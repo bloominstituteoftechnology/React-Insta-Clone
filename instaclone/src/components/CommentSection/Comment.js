4041452b63ab2e8c31c770836f18cef5a3107df1
Peter Pham <phamhere@gmail.com>
2018-10-09 14:53:21 -0600

    added localStorage to comments

--- instaclone/src/components/CommentSection/Comment.js
+++ instaclone/src/components/CommentSection/Comment.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './CommentSection.css';
 
-const Comment = props => <p className="comment"><strong>{props.comment.username}</strong> {props.comment.text}</p>;
+const Comment = props => <p className="comment"><strong>{props.comment.username}</strong> {props.comment.text} <i className="fas fa-minus-circle" onClick={props.deleteComment}></i></p>;
 
 Comment.propTypes = {
   comment: PropTypes.shape({
--- instaclone/src/components/CommentSection/CommentSection.js
+++ instaclone/src/components/CommentSection/CommentSection.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import moment from 'moment';
 import './CommentSection.css';
 import Comment from './Comment';
+
+let initialComments;
+localStorage.getItem('comments') ? initialComments = JSON.parse(localStorage.getItem('comments')) : initialComments = [];
 
 class CommentSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      comments: this.props.comments,
+      comments: (initialComments.length > 0 ? initialComments : this.props.comments),
       comment: ''
     }
   }
   addNewComment = e => {
+    let newComments = [...this.state.comments, {username: 'dummy', text: this.state.comment}]
     if (e.key === 'Enter') {
       this.setState({ 
-        comments: [...this.state.comments, {username: 'dummy', text: this.state.comment, timestamp: new Date(Date.now())}],
+        comments: newComments,
         comment: ''
        })
     }
+    localStorage.setItem('comments', JSON.stringify(newComments));
+  }
+  deleteComment = e => {
+    let filteredComments = this.state.comments.filter(comment => e.target.parentNode.textContent.indexOf(comment.text) === -1);
+    this.setState({ comments: filteredComments });
+    localStorage.setItem('comments', JSON.stringify(filteredComments));
   }
   onCommentChange = e => {
     this.setState({ [e.target.name]: e.target.value })
@@ -25,9 +34,14 @@ class CommentSection extends React.Component {
   render() {
     return (
       <React.Fragment>
-        {this.state.comments.map((comment,i) => <Comment comment={comment} key={i} />)}
-        <p className="moment">{moment(`${this.state.comments[this.state.comments.length - 1].timestamp}`).fromNow()}</p>
-        <input type="text" name="comment" placeholder="Add a comment" onChange={this.onCommentChange} value={this.state.comment} onKeyPress={this.addNewComment}/>
+        {this.state.comments.map((comment,i) => <Comment comment={comment} key={i} deleteComment={this.deleteComment}/>)}
+        <input type="text" 
+          id= "commentInput"
+          name="comment"
+          placeholder="Add a comment..."
+          onChange={this.onCommentChange}
+          value={this.state.comment}
+          onKeyPress={this.addNewComment}/>
       </React.Fragment>
     )
   }
