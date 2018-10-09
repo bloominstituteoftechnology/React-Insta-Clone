b1372bc586dd0f922eb70e52de1aee97fe688d35
Justin <jdlow2@gmail.com>
2018-10-09 15:30:29 -0400

    added PropTypes to CommentSection and Comment components

--- instagram/src/components/CommentSection/Comment.js
+++ instagram/src/components/CommentSection/Comment.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 const Comment = props => {
   return (
@@ -9,4 +10,9 @@ const Comment = props => {
   )
 };
 
+Comment.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.shape({
+  username: PropTypes.string,
+  text: PropTypes.string,
+}))}
 export default Comment;
--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Comment from "./Comment";
+import PropTypes from 'prop-types';
 
 const CommentSection = props => {
   return (
     <div>
       {props.comments.map((com, idx) => (
-        <Comment comment={com} />
+        <Comment key={idx} comment={com} />
       ))}
+      <input type="text" name="addComment" placeholder="Add a comment..."/>
     </div>
   )
 }
+
+CommentSection.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.shape({
+  username: PropTypes.string,
+  text: PropTypes.string,
+}))}
 
 export default CommentSection;
