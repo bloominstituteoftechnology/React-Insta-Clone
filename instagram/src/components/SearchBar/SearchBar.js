fd0a11092a80102ce04b6470b3faceafa4fb53df
Unknown <zachary.arney@gmail.com>
2018-10-09 15:51:11 -0500

    Populated SearchBar with everything except sprites

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBar = (props) => {
+  return (
+    <div>
+      <img src="../../instagramsprites1" alt="" />
+      <form>
+        <input type="text" placeholder="Search" />
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
