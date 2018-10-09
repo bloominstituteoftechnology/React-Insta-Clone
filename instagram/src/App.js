97fc554053bc9e0df32072c2843cc64434b1e3c9
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-09 13:09:49 -0600

    inital commit

---
+++ instagram/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
