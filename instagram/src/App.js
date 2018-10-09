c7969cd0beb409edbba8724c8952cdc084e68903
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-09 13:16:53 -0600

    Comment and Post .js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHeart, faCommentAlt, faEllipsisH, faCompass, faUser, faCameraRetro } from '@fortawesome/free-solid-svg-icons';
+import dummyData from '../../dummy-data';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state= {
+      dummyData: dummyData
+    }
+  }
   render() {
     return (
       <div className="App">
-        
+      {this.state.dummyData.map(username => {
+        return(
+          <PostContainer data = {username} key = {username.timestamp}></PostContainer>
+        )
+      })}
+        <a href = "https://fontawesome.com/license">Icons by FontAwesome</a>
       </div>
     );
   }
