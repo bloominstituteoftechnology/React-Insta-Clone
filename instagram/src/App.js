b74c66bb2959046036682477bf3dd20524929f32
Yanrong Xu <xuyanrong@icloud.com>
2018-10-09 02:41:54 -0400

    done with mvp

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import SearchBar from "./components/SearchBar/SearchBar";
 import PostContainer from "./components/PostContainer/PostContainer";
 import dummyData from "./dummy-data.js";
-import '../node_modules/font-awesome/css/font-awesome.min.css';
 import './App.css';
 
 class App extends Component {
