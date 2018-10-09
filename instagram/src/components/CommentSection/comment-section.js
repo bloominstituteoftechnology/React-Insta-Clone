df1dbdd844deb6def23da4cb67be47b86c8a0066
SavannahGreuel <smallidgesavannah@gmail.com>
2018-10-09 16:49:02 -0600

    working on adding comments

--- instagram/src/components/CommentSection/comment-section.js
+++ instagram/src/components/CommentSection/comment-section.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import dots from '../../images/dots.svg'
 import '../CommentSection/CommentSection.css'
- const CommentSection = (props) => {
+
+class CommentSection extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            comments: this.state.comments,
+            newComment: '',
+            timeStamp: props.timeStamp,
+        }
+    }
+
+    // addNewComment = event => {
+    //     event.preventDefault();
+    //     if (this.state.newComment.length > 0) {
+    //         this.setState ({
+    //             comments: [...this.state.comments, {
+    //                 username: 'username',
+    //                 text: this.state.newComment,
+    //             }],
+    //             newComment: '',
+    //         })
+    //     }
+    // }
+
+    // handleChange = event => {
+    //     this.setState ({
+    //         [event.target.name]: event.target.value,
+    //     })
+    // }
+    render() {
     return (
         <div className ='addComment'>
             <form action = 'submit'>
@@ -10,5 +39,6 @@ import '../CommentSection/CommentSection.css'
             <img src={dots} alt = 'options for commenting' />
          </div>
     )
+}
 }
  export default CommentSection;
