d7abfdc08b35b47f687a4ae882f29a0f9b30db41
SavannahGreuel <smallidgesavannah@gmail.com>
2018-10-09 16:39:37 -0600

    added componentDidMount

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import SearchBar from './components/SearchBar/search-bar'
 import PostContainer from './components/PostContainer/post-container'
 import './App.css';
+import dummyData from './dummyData'
 //import CommentSection from './components/CommentSection/comment-section';
 
  class App extends Component {
+   constructor (props) {
+     super(props);
+     this.state = {
+       dummyData: []
+     };
+   }
   
+   componentDidMount () {
+     this.setState ({dummyData: dummyData});
+   }
+
   render() {
     return (
       
