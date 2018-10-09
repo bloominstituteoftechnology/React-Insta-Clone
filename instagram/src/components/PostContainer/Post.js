67647f5be2473a21b1ac42aa839cbbda9b647585
Blake <koenneckerb@gmail.com>
2018-10-09 12:43:34 -0700

    Clicking the heart icon now adds a like to posts. Refactored PostContainer.js into two separate files so the 'likes' property can be stored in state and be mutable.

---
+++ instagram/src/components/PostContainer/Post.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import CommentSection from "../CommentSection/CommentSection";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faComment } from "@fortawesome/free-solid-svg-icons";
+class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: props.post.likes
+    };
+  }
+  addLike = () => this.setState({ likes: this.state.likes + 1 });
+  render() {
+    return (
+      <div className="post">
+        <div className="profile">
+          <img
+            src={this.props.post.thumbnailUrl}
+            alt={this.props.post.username}
+            className="profile-picture"
+          />
+          <span className="username">{this.props.post.username}</span>
+        </div>
+        <img
+          src={this.props.post.imageUrl}
+          alt={this.props.post.comments[0].text}
+        />
+        <div className="icons">
+          <FontAwesomeIcon
+            icon={faHeart}
+            style={{ height: "25px", width: "25px", cursor: "pointer" }}
+            onClick={this.addLike}
+          />
+          <FontAwesomeIcon
+            icon={faComment}
+            style={{ height: "25px", width: "25px" }}
+          />
+        </div>
+        <p className="likes">{this.state.likes} likes</p>
+        <CommentSection comments={this.props.post.comments} />
+        <p className="timestamp">{this.props.post.timestamp}</p>
+      </div>
+    );
+  }
+}
+export default Post;
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,28 +1,5 @@
 import React from "react";
-import CommentSection from "../CommentSection/CommentSection";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { faComment } from "@fortawesome/free-solid-svg-icons";
+import Post from "./Post";
 const PostContainer = props =>
-  props.data.map((post, index) => (
-    <div key={index} className="post">
-      <div className="profile">
-        <img
-          src={post.thumbnailUrl}
-          alt={post.username}
-          className="profile-picture"
-        />
-        <span className="username">{post.username}</span>
-      </div>
-      <img src={post.imageUrl} alt={post.comments[0].text} />
-      <div className="icons">
-        <FontAwesomeIcon icon={faHeart} style={{ height: "25px", width: "25px" }}/>
-        <FontAwesomeIcon icon={faComment} style={{ height: "25px", width: "25px" }}/>
-      </div>
-      <p className="likes">{post.likes} likes</p>
-      <CommentSection comments={post.comments} />
-      <p className="timestamp">{post.timestamp}</p>
-    </div>
-  ));
-
+  props.data.map((post, index) => <Post post={post} key={index} />);
 export default PostContainer;
