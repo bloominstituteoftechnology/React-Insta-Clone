7e000d4044167298f493df7935c5a669b52d0f5a
Drewsup123c <drewsup123c@gmail.com>
2018-10-08 22:38:23 -0600

    created PostContainer.js and added some jsx

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBar from "./components/SearchBarContent/SearchBar"
+import SearchBar from "./components/SearchBarContent/SearchBar";
+import PostContainer from "./components/PostContent/PostContainer";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
         <div className="top-header">
           <SearchBar />
         </div>
+        <PostContainer />
       </div>
     );
   }
