57251031f91de66b3fc476262234cbc6a9a26dcb
Riley-Brown <riley@riley.gg>
2018-10-09 17:16:18 -0400

    Updated data to be dynamic

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,42 +1,45 @@
 import React from "react";
 import "./Post.css";
-const PostContainer = () => {
+const PostContainer = props => {
   return (
     <div className="post-wrapper">
-      <div className="post-container">
-        <div className="post-header">
-          <img
-            src="http://www.mycustomer.com/sites/all/themes/pp/img/default-user.png"
-            alt="test icon"
-          />
-          <h2 className="post-username">Test Username</h2>
-        </div>
-        <div className="post-body">
-          <img
-            src="https://images.pexels.com/photos/164175/pexels-photo-164175.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-            alt="Photo"
-          />
-        </div>
-        <div className="post-comment-container">
-          <div className="comment-icons">
-            <i className="far fa-heart" />
-            <i className="far fa-comment" />
-            <i className="far fa-share-square" />
+      {props.posts.map(post => (
+        <div className="post-container" key={post.timestamp}>
+          <div className="post-header">
+            <img src={post.thumbnailUrl} alt="test icon" />
+            <h2 className="post-username">{post.username}</h2>
           </div>
-          <div className="comments">
-            <div className="likes">74 likes</div>
-            <div className="post-title">
-              <span>Test Account</span>
-              <span>
-                Lorem ipsum, dolor sit adipisicing elit. Nostrum, unde.
-              </span>
+          <div className="post-body">
+            <img src={post.imageUrl} alt="Photo" />
+          </div>
+          <div className="post-comment-container">
+            <div className="comment-icons">
+              <i className="far fa-heart" />
+              <i className="far fa-comment" />
+              <i className="far fa-share-square" />
             </div>
-            <div className="post-comments">
-              <span>Load more comments</span>
+            <div className="comments">
+              <div className="likes">
+                {post.likes} <span>likes</span>
+              </div>
+              {/* <div className="post-title">
+                <span>{post.username}</span>
+                <span>
+                  Lorem ipsum, dolor sit adipisicing elit. Nostrum, unde.
+                </span>
+              </div> */}
+              <div className="post-comments">
+                {post.comments.map(comment => (
+                  <div>
+                    <span className="comment-username">{comment.username}</span>
+                    <span>{comment.text}</span>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
