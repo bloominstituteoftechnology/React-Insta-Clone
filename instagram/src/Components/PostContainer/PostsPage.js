48178f21b093682146b2485269556e997821c23a
ShardeDeLane <rhcprocker@yahoo.com>
2018-10-10 02:49:33 -0400

    log out on icon

--- instagram/src/Components/PostContainer/PostsPage.js
+++ instagram/src/Components/PostContainer/PostsPage.js
@@ -26,6 +26,13 @@ class PostsPage extends Component {
 		this.setState({ filteredPosts: newPosts })
 	}
 
+	signOut = () => {
+		// localStorage.removeItem('username')
+		// localStorage.removeItem('password')
+		localStorage.clear()
+		window.location.reload()
+	}
+
 	render() {
 		let posts
 		if (this.state.filteredPosts.length > 0) {
@@ -36,7 +43,7 @@ class PostsPage extends Component {
 		return (
 			<div className="App">
 				<h1 style={{ fontSize: '50px', fontWeight: '400' }}>Instagram</h1>
-				<SearchBar search={this.search} />
+				<SearchBar signout={this.signOut} search={this.search} />
 				<PostContainer posts={posts} />
 			</div>
 		)
