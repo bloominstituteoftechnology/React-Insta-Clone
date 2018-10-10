bff188c7a0c12a0420a8d18e461f2478e2e90de8
Axolotl <samuelmachat@gmail.com>
2018-10-10 16:27:58 -0400

    refactored by abstracting postspage out of app.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,59 +1,12 @@
 import React, { Component } from 'react'
-import SearchBar from './components/SearchBar/SearchBar'
-import PostContainer from './components/PostContainer/PostContainer'
+import PostsPage from './components/PostContainer/PostsPage'
 
 import './reset.css'
 import './App.css'
 
-import dummyData from './dummy-data.js'
-
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      posts: [],
-      searchParam: ''
-    }
-  }
-
-  componentDidMount() {
-    this.setState({ posts: dummyData })
-  }
-
-  search = event => {
-    event.preventDefault()
-    console.log(this.state.searchParam)
-  }
-
-  handleSearchInputChange = event => {
-    this.setState({ searchParam: event.target.value })
-  }
-
   render() {
-    let { posts, searchParam } = this.state
-    const { search, handleSearchInputChange } = this
-
-    console.log('posts')
-    console.log(posts)
-    // console.log(posts.filter(({ username }) => username.match(searchParam)))
-
-    posts = posts.filter(({ username }) => username.match(searchParam))
-
-    console.log(posts)
-
-    return (
-      <div className="App">
-        <SearchBar
-          searchParam={searchParam}
-          search={search}
-          handleSearchInputChange={handleSearchInputChange}
-        />
-        <div className="posts-container">
-          {posts.length > 0 &&
-            posts.map((post, i) => <PostContainer key={i} post={post} />)}
-        </div>
-      </div>
-    )
+    return <PostsPage />
   }
 }
 
