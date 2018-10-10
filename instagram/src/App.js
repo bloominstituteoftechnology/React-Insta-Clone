b2ac1c6b180b2b1abf4803e0ebbe36233a68bf8f
Courtney Buratto <you@example.com>
2018-10-09 21:44:04 -0700

    MVP (imported dummy-data to App.js)

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import dummyData from './dummy-data';
+
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      dummyData: dummyData
     }
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
-        </header>
+       
       </div>
     );
   }
