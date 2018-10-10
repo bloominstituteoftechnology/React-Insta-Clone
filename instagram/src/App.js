fa4610f091be4aacf204c482d23dc3cec03573db
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-10-09 19:55:01 -0600

    Posts page rendering and working correctly

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,56 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-import PostContainer from './components/PostContainer/PostContainer';
-import SearchBar from './components/SearchBar/SearchBar';
 
 import dummyData from './dummy-data';
 
+import PostsPage from './components/PostContainer/PostsPage';
+
 class App extends Component {
-
-  state = {
-    posts: [],
-    user: 'edub',
-  }
-
-  addNewComment = comment => {
-    let posts = [...this.state.posts];
-    posts[comment.key].comments.push({username: this.state.user, text: comment.text });
-    this.setState({ posts: posts});
-  }
-
-  addLike = key => {
-    let posts = [...this.state.posts];
-    posts[key].likes++;
-    this.setState({ posts: posts });
-  }
-
-  search = text => {
-    let posts = this.state.posts.filter(post => post.username === text);
-    this.setState({ posts: posts });
-  }
-  
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ posts: dummyData });
-    }, 500);
-  }
 
   render() {
     return (
       <div className="App">
-        <SearchBar search={this.search} />
-        <div className="posts">
-          {this.state.posts.map((post, index) => (
-            <PostContainer 
-              postKey={index} 
-              post={post} 
-              addNewComment={this.addNewComment} 
-              addLike={this.addLike}
-             />
-          ))}
-        </div>
+        <PostsPage data={dummyData} />
       </div>
     );
   }
---
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './PostContainer.css';
+
+import PostContainer from './PostContainer';
+import SearchBar from '../SearchBar/SearchBar';
+
+class PostsPage extends React.Component {
+    state = {
+        posts: [],
+        user: 'edub',
+      }
+    
+      addNewComment = comment => {
+        let posts = [...this.state.posts];
+        posts[comment.key].comments.push({username: this.state.user, text: comment.text });
+        this.setState({ posts: posts});
+      }
+    
+      addLike = key => {
+        let posts = [...this.state.posts];
+        posts[key].likes++;
+        this.setState({ posts: posts });
+      }
+    
+      search = text => {
+        let posts = this.state.posts.filter(post => post.username === text);
+        this.setState({ posts: posts });
+      }
+      
+      componentDidMount() {
+        setTimeout(() => {
+          this.setState({ posts: this.props.data });
+        }, 500);
+      }
+    
+      render() {
+        return (
+          <div className="posts-page">
+            <SearchBar search={this.search} />
+            <div className="posts">
+              {this.state.posts.map((post, index) => (
+                <PostContainer 
+                  postKey={index} 
+                  post={post} 
+                  addNewComment={this.addNewComment} 
+                  addLike={this.addLike}
+                 />
+              ))}
+            </div>
+          </div>
+        );
+      }
+}
+
+export default PostsPage;
