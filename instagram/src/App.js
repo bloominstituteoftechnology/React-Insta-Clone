af800fdb21994b359edb9ba998d013fd569033b3
ShardeDeLane <rhcprocker@yahoo.com>
2018-10-10 00:49:16 -0400

    app file functional component instead of class

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,37 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.css'
-import PostContainer from './Components/PostContainer/PostContainer'
-import dummyData from './dummy-data'
-import CommentSection from './Components/CommentSection/CommentSection'
-import SearchBar from './Components/SearchBar/SearchBar'
+import PostsPage from './Components/PostContainer/PostsPage'
 
-class App extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			posts: [],
-			username: ''
-		}
-	}
-
-	componentDidMount() {
-		localStorage.setItem('username', JSON.stringify(this.state.username))
-		const instagramUser = localStorage.getItem('username')
-		this.setState({
-			posts: dummyData,
-			username: instagramUser
-		})
-	}
-
-	render() {
-		return (
-			<div className="App">
-				<h1 style={{ 'font-size': '50px', 'font-weight': '400' }}>Instagram</h1>
-				<SearchBar />
-				<PostContainer posts={this.state.posts} />
-			</div>
-		)
-	}
-}
+const App = () => <PostsPage />
 
 export default App
