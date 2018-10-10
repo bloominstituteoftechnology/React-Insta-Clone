d4b38c470800096f55a9d6684007b7b281474a25
BryanGf <briangf@uw.edu>
2018-10-10 13:23:24 -0700

    Refractor + HOC

--- instagram-new/src/App.js
+++ instagram-new/src/App.js
@@ -1,61 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import dummyData from './dummy-data';
-import Post from './components/PostContainer/Post';
-import Search from './components/SearchBar/Search';
-
+import PostsPage from './components/PostContainer/PostsPage';
+import Authenticate from './components/Authentication/Authenticate'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dummyData: [],
-      filteredData: [],
-      fitlerFor: '',
-    };
-  };
-
-  componentDidMount = () => {
-    this.setState({dummyData: dummyData,
-                  filteredData: dummyData});
-
-  }
-
-  inputChange = (event, index) => {
-    this.setState({[event.target.name]: event.target.value})
-  }
-
-  filtered = (event) => {
-    event.preventDefault();
-    
-    let filterData= this.state.dummyData.filter(posts => {
-      let upperCase = posts.username.toUpperCase();
-      if(upperCase.includes(this.state.filterFor.toUpperCase())) {
-        return posts;
-      }
-    })
-    this.setState({filteredData: filterData});
-  }
-
-  counter = (event, index) => {
-    event.preventDefault();
-    const hold = this.state.filteredData;
-    hold[index].likes++;
-    this.setState({filteredData:hold})
-  }
-
   render() {
     return (
       
       <div className="App">
-        <Search inputChange = {this.inputChange} filtered = {this.filtered}/>
-        <div className = "content-container">
-          <Post dummyData ={this.state.filteredData} counter = {this.counter}/>
-        </div>
+        <Authentication />
       </div>
     );
   }
 }
 
+const Authentication = Authenticate(PostsPage);
 export default App;
