5136bce685cba4392d72b0f68fb9dc29dabea8f3
Brad Mortensen <b.mort86@gmail.com>
2018-10-10 16:37:50 -0600

    working on functionality

--- instagram/src/components/Authentication/authenticate.js
+++ instagram/src/components/Authentication/authenticate.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import LoginPage from '/Users/BradMortensen/Documents/Git/React/React-Insta-Clone/instagram/src/components/Login/loginpage';
+import SearchBar from '/Users/BradMortensen/Documents/Git/React/React-Insta-Clone/instagram/src/components/SearchBar/searchbar'
 
 // import App from '/Users/BradMortensen/Documents/Git/React/React-Insta-Clone/instagram/src/App'
 
 const Authenticate = App =>
     class extends React.Component {
-        render() {
-            return <App / > ;
+        constructor(props) {
+            super(props);
+            this.state = {
+                loggedIn: false
+            };
+        } 
+        logIn(event) {
+            event.preventDefault();
+            console.log(this.state.loggedIn)
+        }   
+        render() { 
+            if (this.state.loggedIn){ return <App />};
+            return (    
+                <div className='login-container'>
+                    <SearchBar />
+                    <LoginPage content={this.state} logIn={this.logIn} />
+                </div>
+            );
         }
     };
 
--- instagram/src/components/Login/loginpage.js
+++ instagram/src/components/Login/loginpage.js
@@ -1,2 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react'
 
+import './loginpage.css'
+
+class LoginPage extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      content:props.content,
+      username:'',
+      password:''
+    }
+  }
+  handleInputChange = event => this.setState({ [event.target.name]: event.target.value });
+
+  render(){  
+    return (
+      <div className='login-stuff'>
+        {/* <img src=/>       */}
+        <h2>Username:</h2>
+        <input 
+          className='login-username' 
+          placeholder='Enter Your Username'          
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleInputChange}></input>
+        <h2>Password:</h2>
+        <input 
+          className='login-password' 
+          placeholder='Enter Your Password'
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleInputChange}></input>
+        <button className='login-button'>Log In</button>
+      </div>
+    );
+  }
+}
+
+
+export default LoginPage;
