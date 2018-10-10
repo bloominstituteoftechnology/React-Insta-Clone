ab5696ab7963ed8daf3df834b2e91a32daae009e
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-10 15:26:43 -0500

    PostPage js / App js re-factor

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,43 +1,17 @@
 import React, { Component } from 'react';
-
 import './App.css';
-import dummyData from './dummy-data';
-import SearchBarContainer from './components/SearchBar/SearchBarContainer';
-import PostsContainer from './components/PostContainer/PostsContainer';
+import PostsPage from './components/PostContainer/PostsPage';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      posts: [],
-      filterPost: []
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ posts: dummyData });
-  }
-
-  searchPostHandler = event => {
-    const posts = this.state.posts.filter(post => {
-      if (post.username.includes(event.target.value)) {
-        return post;
-      }
-    })
-    this.setState({filterPost: posts});
+    this.state = {};
   }
 
   render() {
     return (
       <div className="App">
-        <SearchBarContainer 
-          searchPost={this.searchPostHandler}
-        />
-        <PostsContainer 
-          posts={this.state.filterPost.length > 0 
-            ? this.state.filterPost
-            : this.state.posts} 
-        />
+        <PostsPage />
       </div>
     );
   }
