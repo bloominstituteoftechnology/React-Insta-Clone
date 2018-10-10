df2f5899d7465dcd76c1f19735762cf904a5508d
Rahul Desai <rd825@georgetown.edu>
2018-10-10 19:11:22 -0400

    Trying to fix comment.js

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 
-import Coment from './Comment';
+import Coment from './Coment';
 import CommentForm from './CommentForm';
 
 import './commentsection.css';
