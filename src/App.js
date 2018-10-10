bd75c2d4e4432ee56c49ca3204a6572e3e5ba8ba
SabrinaGear <sabrinagear.ft@gmail.com>
2018-10-10 14:43:07 -0500

    hit save aaaahhh

--- src/App.js
+++ src/App.js
@@ -45,37 +45,3 @@ class App extends Component {
 }
 
 export default App;
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       dummyData: [],
-//       search: ""
-//     };
-//   }
-//   componentDidMount() {
-//     this.setState({ dummyData: dummyData });
-//   }
-
-//   handleSearch = event => {
-//     this.setState({ [event.target.name]: event.target.value });
-//   };
-
-//   render() {
-//     let data = this.state.dummyData.filter(
-//       data => data.nickname.indexOf(this.state.search) !== -1
-//     );
-
-//     return (
-//       <div className="App">
-//         <Header input={this.state.search} handleSearch={this.handleSearch} />
-//         {data.map((data, i) => (
-//           <Post key={i} data={data} />
-//         ))}
-//         <Comments data={data} />
-//       </div>
-//     );
-//   }
-// }
-// export default App;
