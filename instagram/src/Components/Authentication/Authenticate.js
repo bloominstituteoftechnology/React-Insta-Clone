2c1cbac4842157ad365af5fa3cf680e656e52cb6
ShardeDeLane <rhcprocker@yahoo.com>
2018-10-10 02:38:30 -0400

    login, currently unstyled

--- instagram/src/Components/Authentication/Authenticate.js
+++ instagram/src/Components/Authentication/Authenticate.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
+import Login from '../Login/Login'
 
 const Authenticate = App =>
 	class extends Component {
+		state = {
+			loggedIn: false,
+			username: ''
+		}
+
+		componentDidMount() {
+			if (
+				localStorage.getItem('username') &&
+				localStorage.getItem('password')
+			) {
+				this.setState({
+					loggedIn: true,
+					username: localStorage.getItem('username')
+				})
+			} else {
+				this.setState({
+					loggedIn: false
+				})
+			}
+		}
 		render() {
-			return <App />
+			if (this.state.loggedIn === true) {
+				return <App />
+			} else if (this.state.loggedIn === false) {
+				return <Login />
+			}
 		}
 	}
 
