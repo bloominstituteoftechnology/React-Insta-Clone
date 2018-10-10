dc8fba17268ae0c19bcf2705f465a5845aed5904
Brandon Vilaychith <blvilaychith@outlook.com>
2018-10-10 14:13:04 -0500

    Moved app js contents to PostsPage component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import dummyData from './dummy-data';
-import PostContainer from './components/PostContainer/PostContainer';
-import SearchBar from './components/SearchBar/SearchBar';
+import PostsPage from '../src/components/PostContainer/PostsPage';
 import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
@@ -17,50 +15,8 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 library.add(faComment, faHeart, faEllipsisH, faCompass, faUser, faSearch, fab);
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      postData: [],
-      search: ''
-    };
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        postData: dummyData
-      });
-    }, 800);
-  }
-
-  // onChangeHandler = event => {
-  //   this.setState({ [event.target.name]: event.target.value });
-  // };
-
-  search = event => {
-    this.setState({
-      search: event.target.value.substr(0, 20)
-    });
-  };
-
   render() {
-    let filterSearch = this.state.postData.filter(post => {
-      return post.username.indexOf(this.state.search) !== -1;
-    });
-    return (
-      <div className="appContainer">
-        {!this.state.postData.length ? (
-          <h1 className="loadingScreen">Loading...</h1>
-        ) : (
-          <div>
-            <SearchBar stateSearch={this.state.search} search={this.search} />
-            {filterSearch.map(postInfo => (
-              <PostContainer postInfo={postInfo} key={postInfo.timestamp} />
-            ))}
-          </div>
-        )}
-      </div>
-    );
+    return <PostsPage />;
   }
 }
 
---
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import dummyData from '../../dummy-data';
+import PostContainer from '../PostContainer/PostContainer';
+import SearchBar from '../SearchBar/SearchBar';
+
+class PostsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      postData: [],
+      search: ''
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        postData: dummyData
+      });
+    }, 800);
+  }
+
+  search = event => {
+    this.setState({
+      search: event.target.value.substr(0, 20)
+    });
+  };
+
+  render() {
+    let filterSearch = this.state.postData.filter(post => {
+      return post.username.indexOf(this.state.search) !== -1;
+    });
+    return (
+      <div className="appContainer">
+        {!this.state.postData.length ? (
+          <h1 className="loadingScreen">Loading...</h1>
+        ) : (
+          <div>
+            <SearchBar stateSearch={this.state.search} search={this.search} />
+            {filterSearch.map(postInfo => (
+              <PostContainer postInfo={postInfo} key={postInfo.timestamp} />
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default PostsPage;
