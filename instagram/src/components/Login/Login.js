7eef24658c016699690edab2e6583127acc34108
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-10-10 15:18:55 -0700

    finished mvp added styles to login form

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import './Login.css';
 
 const Login = props => {
 
     return (
-        <form onSubmit={props.loginEvent}>
-            <input type="text" placeholder="Username" onChange={props.handleUsername} />
-            <input type="password" placeholder="Password" onChange={props.handlePassword} />
-            <button>Login</button>
+        <div>
+            <h1>Login</h1>
+                <form className="loginFrom" onSubmit={props.loginEvent}>
+                <input type="text" placeholder="Username" onChange={props.handleUsername} />
+                <input type="password" placeholder="Password" onChange={props.handlePassword} />
+                <button>Login</button>
         </form>
+        </div>
     );
 }
 
