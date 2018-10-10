b78cb48769db261b281a4aa526758440e27b4136
Ryan Boris <neodynia@icloud.com>
2018-10-10 18:59:13 -0400

    Build out <Login />

---
+++ instagram/src/components/Login/Login.js
@@ -0,0 +1,35 @@
+
+import React from 'react';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: ""
+        }
+    }
+
+    usernameChangeHandler = (event) => {
+        this.setState({
+             username: event.target.value
+        })
+    }
+    
+    submitLogin = () => {
+        window.localStorage.setItem('username', this.state.username);
+    }
+
+    render() {
+        return (
+            <div className="login">
+                <form className="login-form" onSubmit={this.submitLogin}>
+                    <input type="text" placeholder="Username" value={this.state.username} onChange={this.usernameChangeHandler} />
+                    <input type="password" placeholder="Password" />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
