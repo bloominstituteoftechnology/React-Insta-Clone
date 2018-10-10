85d7eab9957c250b0242d95c962b582d6828105e
Drewsup123c <drewsup123c@gmail.com>
2018-10-10 14:44:58 -0600

    got PostsPage to render out all the posts with all the functionality

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,30 +3,32 @@ import './App.css';
 import dummyData from './dummy-data';
 import PostsContainer from './components/PostsContainer/PostsContainer';
 import SearchBar from './components/SearchBar/SearchBarContainer';
+import PostsPage from './components/PostsContainer/PostsPage'
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      posts: [],
-      filteredPosts: []
-    };
+    // this.state = {
+    //   posts: [],
+    //   filteredPosts: [],
+    //   isLoggedIn : false,
+    // };
   }
-  componentDidMount() {
-    this.setState({ posts: dummyData });
-  }
-  searchPostsHandler = e => {
-    const posts = this.state.posts.filter(p => {
-      if (p.username.includes(e.target.value)) {
-        return p;
-      }
-    });
-    this.setState({ filteredPosts: posts });
-  };
+  // componentDidMount() {
+  //   this.setState({ posts: dummyData });
+  // }
+  // searchPostsHandler = e => {
+  //   const posts = this.state.posts.filter(p => {
+  //     if (p.username.includes(e.target.value)) {
+  //       return p;
+  //     }
+  //   });
+  //   this.setState({ filteredPosts: posts });
+  // };
   render() {
     return (
       <div className="App">
-        <SearchBar
+        {/* <SearchBar
           searchTerm={this.state.searchTerm}
           searchPosts={this.searchPostsHandler}
         />
@@ -36,7 +38,8 @@ class App extends Component {
               ? this.state.filteredPosts
               : this.state.posts
           }
-        />
+        /> */}
+        <PostsPage />
       </div>
     );
   }
