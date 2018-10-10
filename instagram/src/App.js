54f61afff1874706a82dc8ec52606d98785b3da6
Adam <ajmboosh@gmail.com>
2018-10-10 15:22:08 -0500

    day3 task done: moved bulk of what used to be App into new PostsPage file, rendered this in new parent App. refactored imports in PostsPage to make sure everything functions like before

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import dummyData from "./dummy-data";
 import PostContainer from "./components/PostContainer/PostContainer";
 import SearchBar from "./components/SearchBar/SearchBar";
+import PostsPage from "./components/PostContainer/PostsPage";
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,102 +14,12 @@ class App extends Component {
       newSearch: "",
       filtered: false
     };
-    // this.saveState = this.saveState.bind(this);
-    // this.hydrateState = this.hydrateState.bind(this);
   }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ postData: dummyData });
-    }, 500);
-    // window.addEventListener("beforeunload", this.saveState);
-  }
-
-  // componentWillUnmount() {
-  //   window.removeEventListener("beforeunload", this.saveState);
-  // }
-
-  // saveState() {
-  //   console.log(this.state);
-  //   for (let key in this.state) {
-  //     console.log(this.state[key]);
-  //     // localStorage.setItem(key, JSON.stringify(this.state[key]));
-  //   }
-  // }
-
-  // hydrateState() {
-  //   for (let key in this.state) {
-  //     if (localStorage.hasOwnProperty(key)) {
-  //       let value = localStorage.getItem(key);
-  //       try {
-  //         value = JSON.parse(value);
-  //         this.setState({ [key]: value });
-  //       } catch (error) {
-  //         this.setState({ [key]: value });
-  //       }
-  //     }
-  //   }
-  // }
-
-  startSearch = event => {
-    event.preventDefault();
-    let newData = dummyData;
-    let filteredArr = newData.filter(
-      post => post.username === this.state.newSearch
-    );
-    if (filteredArr.length) {
-      this.setState({
-        postData: filteredArr,
-        newSearch: "",
-        filtered: true
-      });
-    } else if (this.state.filtered === true) {
-      this.setState({
-        postData: dummyData,
-        newSearch: "",
-        filtered: false
-      });
-    } else {
-      alert("Sorry, but we couldn't find any matches with that username");
-      this.setState({
-        newSearch: ""
-      });
-    }
-  };
-
-  changeHandler = (key, value) => {
-    this.setState({
-      [key]: value
-    });
-  };
 
   render() {
     return (
       <div className="App">
-        {!this.state.postData.length ? (
-          <h2>Loading, please wait...</h2>
-        ) : (
-          <div>
-            <SearchBar
-              changeHandler={this.changeHandler}
-              startSearch={this.startSearch}
-              value={this.state.newSearch}
-              filtered={this.state.filtered ? "filtered" : "not-filtered"}
-            />
-            <div className="post-container-list">
-              {this.state.postData.map(user => {
-                return (
-                  <PostContainer
-                    key={user.timestamp}
-                    userData={user}
-                    // saveState={this.saveState}
-                    // hydrateState={this.hydrateState}
-                  />
-                );
-              })}
-            </div>
-          </div>
-        )}
+        <PostsPage />
       </div>
     );
   }
