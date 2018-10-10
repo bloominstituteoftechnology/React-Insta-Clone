7c941f7f548acb1b414618f25993c727f15d1667
Axolotl <samuelmachat@gmail.com>
2018-10-10 17:50:40 -0400

    added login functionality

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,4 +1,3 @@
-import React, { Component } from 'react'
 import PostsPage from './components/PostContainer/PostsPage'
 import Authenticate from './components/Authentication/Authenticate'
 import Login from './components/Login/Login'
@@ -6,23 +5,4 @@ import Login from './components/Login/Login'
 import './reset.css'
 import './App.css'
 
-class App extends Component {
-  login = username => {
-    localStorage.setItem('username', username)
-    console.log(username)
-    this.forceUpdate()
-  }
-
-  render() {
-    const { login } = this
-
-    return (
-      <div>
-        <Login login={login} />
-        <PostsPage />
-      </div>
-    )
-  }
-}
-
-export default Authenticate(App)
+export default Authenticate(PostsPage)(Login)
