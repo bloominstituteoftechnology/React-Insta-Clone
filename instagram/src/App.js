26cbfd3d34d155547b09817c0ca5e79a1dc94c0a
Jasmine Galang <jsmn.glng@gmail.com>
2018-10-10 12:44:30 -0700

    Built login component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
 import PostsPage from './components/PostsContainer/PostsPage';
 import Authenticate from './components/Authentication/Authenticate';
+import Login from './components/Login/Login';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      username: '',
+      password: '',
+      loggedIn: false
+    };
+  }
+
+  usernameHandler = event => {
+    this.setState({ username: event.target.value });
+  };
+
+  passwordHandler = event => {
+    this.setState({ password: event.target.value });
+  };
+
+  loginSubmit = event => {
+    event.preventDefault();
+
+    const loginCredentials = { username: 'admin', password: 'admin123' };
+    const tryUsername = this.state.username;
+    const tryPassword = this.state.password;
+
+    if (loginCredentials.username === tryUsername) {
+      if (loginCredentials.password === tryPassword) {
+        localStorage.setItem('username', tryUsername);
+        window.location.reload();
+        this.setState({ loggedIn: true });
+      }
+    } else {
+      this.setState({ loggedIn: false });
+    }
+
   }
 
   render() {
     return (
       <div className="App">
+        <Login
+          username={this.state.username}
+          usernameHandler={this.usernameHandler}
+
+          password={this.state.password}
+          passwordHandler={this.passwordHandler}
+
+          loginSubmit={this.loginSubmit}
+        />
         <PostsPage />
       </div>
     );
