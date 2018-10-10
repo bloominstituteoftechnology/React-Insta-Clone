5149954208aa3232b4bde28e39910e760b078c51
Andre Myrick <amyrick.820@gmail.com>
2018-10-10 16:05:34 -0400

    Added Authenticate.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,69 +1,15 @@
 import React, { Component } from 'react';
-import SearchBar from "./components/SearchBar/SearchBar";
-import PostContainer from "./components/PostContainer/PostContainer";
-import dummyData from "./dummy-data.js";
-import '../node_modules/font-awesome/css/font-awesome.min.css';
-import './App.css';
-let data = dummyData;
+import PostPage from "./components/PostContainer/PostPage";
+import Authenticate from "./components/Authentication/Authenticate"
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dummyData: [],
-      search: "",
-    };
-  }
   
-  componentDidMount() {
-    this.setState({dummyData: data});
-  }
-
-  searchInputHandler = event => {
-    this.setState({ [event.target.name]: event.target.value });
-}
-
-  searchPosts = event => {
-    event.preventDefault();
-    let searchInput = this.state.search;
-
-    if (!searchInput) {
-        this.setState({
-          dummyData: data, 
-          search: ""
-        });
-      }
-    else {
-        let posts = dummyData;
-        let searchedPosts = posts.filter(post => {
-          return post.username.includes(searchInput);
-    })
-      this.setState({dummyData: searchedPosts});
-        }
-}
-      // else {
-      //   this.setState({
-      //     dummyData: dummyData,
-      //     dummyData: filtered,
-      //   })
-      // }
-    
-
-
   render() {
     return (
-      <div className="App">
-        <SearchBar 
-        search={this.state.search}
-        searchInputHandler={this.searchInputHandler}
-        searchPosts={this.searchPosts}
-        />
-        <PostContainer 
-        posts={this.state.dummyData}
-        />
-      </div>
+      <PostPage />
     );
   }
 }
 
+Authenticate(App);
 export default App;
---
+++ instagram/src/components/PostContainer/PostPage.js
@@ -0,0 +1,70 @@
+import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import PostContainer from "./PostContainer";
+import dummyData from "../../dummy-data.js";
+import "../../../node_modules/font-awesome/css/font-awesome.min.css";
+import '../../App.css';
+let data = dummyData;
+
+class PostPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          dummyData: [],
+          search: "",
+        };
+      }
+      
+      componentDidMount() {
+        this.setState({dummyData: data});
+      }
+    
+      searchInputHandler = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+    
+      searchPosts = event => {
+        event.preventDefault();
+        let searchInput = this.state.search;
+    
+        if (!searchInput) {
+            this.setState({
+              dummyData: data, 
+              search: ""
+            });
+          }
+        else {
+            let posts = dummyData;
+            let searchedPosts = posts.filter(post => {
+              return post.username.includes(searchInput);
+        })
+          this.setState({dummyData: searchedPosts});
+            }
+    }
+          // else {
+          //   this.setState({
+          //     dummyData: dummyData,
+          //     dummyData: filtered,
+          //   })
+          // }
+        
+    
+    
+      render() {
+        return (
+          <div className="App">
+            <SearchBar 
+            search={this.state.search}
+            searchInputHandler={this.searchInputHandler}
+            searchPosts={this.searchPosts}
+            />
+            <PostContainer 
+            posts={this.state.dummyData}
+            />
+          </div>
+        );
+      }
+    }
+
+
+export default PostPage;
