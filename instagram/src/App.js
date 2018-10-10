a7affbd94c9598f5c845bf8cbcd76513045065fd
Yanrong Xu <xuyanrong@icloud.com>
2018-10-10 17:36:12 -0400

    Moving everything form App to PostsPage

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,74 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
-import dummyData from "./dummy-data";
-import PostContainer from "./components/PostContainer/PostContainer";
-import SearchBar from "./components/SearchBar/SearchBar";
+import PostsPage from "./components/PostContainer/PostsPage";
+import LoginPage from "./components/Login/LoginPage";
+import Authenticate from "./components/Authentication/Authenticate";
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      postData: [],
-      newSearch: "",
-      filtered: false
-    };
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ postData: dummyData });
-    }, 2000);
-  }
-
-  startSearch = event => {
-    event.preventDefault();
-    if (this.state.newSearch !== "") {
-      let newData = dummyData;
-      let filteredArr = newData.filter(
-        post => post.username === this.state.newSearch
-      );
-      this.setState({
-        postData: filteredArr,
-        newSearch: "",
-        filtered: true
-      });
-    } else {
-      this.setState({
-        postData: dummyData,
-        newSearch: "",
-        filtered: false
-      });
-    }
-  };
-
-  changeHandler = (key, value) => {
-    this.setState({
-      [key]: value
-    });
-  };
-
   render() {
     return (
       <div className="App">
-        {!this.state.postData.length ? (
-          <h2>Loading, please wait...</h2>
-        ) : (
-          <div>
-            <SearchBar
-              changeHandler={this.changeHandler}
-              startSearch={this.startSearch}
-              value={this.state.newSearch}
-              filtered={this.state.filtered ? "filtered" : "not-filtered"}
-            />
-            <div className="post-container-list">
-              {this.state.postData.map(user => {
-                return <PostContainer key={user.timestamp} userData={user} />;
-              })}
-            </div>
-          </div>
-        )}
+        <PostsPage />
       </div>
     );
   }
 }
 
-export default App;
+export default Authenticate(App, LoginPage);
