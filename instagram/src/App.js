4627e8ecfc11ad377604404fb144392ce2c3ccee
Jasmine Galang <jsmn.glng@gmail.com>
2018-10-10 11:44:55 -0700

    Refactored App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,40 +1,17 @@
 import React, { Component } from 'react';
-import SearchBar from './components/SearchBar/SearchBarContainer';
-import PostsContainer from './components/PostsContainer/PostsContainer';
-import dummyData from './dummy-data';
+import PostsPage from './components/PostsContainer/PostsPage';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      posts: [],
-      filteredPosts: []
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      posts: dummyData
-    })
-  }
-
-  searchPosts = (event) => {
-    event.preventDefault();
-
-    let filteredPosts = this.state.posts;
-    filteredPosts = filteredPosts.filter(post => {
-      return post.username.toLowerCase() === event.target.value
-    });
-
-    this.setState({ filteredPosts });
+    this.state = {};
   }
 
   render() {
     return (
       <div className="App">
-        <SearchBar searchPosts={this.searchPosts} />
-        <PostsContainer posts={this.state.filteredPosts.length > 0 ? this.state.filteredPosts : this.state.posts} />
+        <PostsPage />
       </div>
     );
   }
