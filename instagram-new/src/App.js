5299eb0dd7e07cb56ec42ac7a7e456a7560254c7
BryanGf <briangf@uw.edu>
2018-10-10 14:01:42 -0700

    login (NON-HOC) + localStorage

--- instagram-new/src/App.js
+++ instagram-new/src/App.js
@@ -2,17 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import PostsPage from './components/PostContainer/PostsPage';
 import Authenticate from './components/Authentication/Authenticate'
+import Login from './components/Login/Login';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+
+  login = event => {
+    localStorage.setItem('username', this.state.username)
+  }
+
+  inputChange = event => {
+    this.setState({[event.target.name] : event.target.value})
+  }
+
   render() {
     return (
       
       <div className="App">
-        <Authentication />
+        <Authentication login ={this.login} inputChange = {this.inputChange}/>
       </div>
     );
   }
 }
 
-const Authentication = Authenticate(PostsPage);
+const Authentication = Authenticate(PostsPage)(Login);
 export default App;
