9094dd5653c5e16978d5eaf1e09bbf0c9a97198b
SabrinaGear <sabrinagear.ft@gmail.com>
2018-10-10 15:06:01 -0500

    refactored

--- src/App.js
+++ src/App.js
@@ -1,46 +1,16 @@
 import React, { Component } from "react";
-import "./App.css";
+
 import Header from "./instagram/SearchBar/Header";
 import PostContainer from "./instagram/PostContainer/PostContainer";
 import Post from "./instagram/PostContainer/Post";
 import Comments from "./instagram/CommentSection/Comments";
-import dummyData from "./dummy-data";
+import dummyData from "./instagram/PostContainer/dummy-data";
+import PostsPage from "./instagram/PostContainer/PostsPage";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      posts: [],
-      filteredPosts: []
-    };
-  }
-  componentDidMount() {
-    this.setState({ posts: dummyData });
-  }
-  searchPostsHandler = e => {
-    const posts = this.state.posts.filter(p => {
-      if (p.nickname.includes(e.target.value)) {
-        return p;
-      }
-    });
-    this.setState({ filteredPosts: posts });
-  };
+  state = {};
   render() {
-    return (
-      <div className="App">
-        <Header
-          searchTerm={this.state.searchTerm}
-          searchPosts={this.searchPostsHandler}
-        />
-        <PostContainer
-          posts={
-            this.state.filteredPosts.length > 0
-              ? this.state.filteredPosts
-              : this.state.posts
-          }
-        />
-      </div>
-    );
+    return <PostsPage />;
   }
 }
 
---
+++ src/instagram/PostContainer/PostsPage.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import "./App.css";
+import Header from "../SearchBar/Header";
+import PostContainer from "./PostContainer";
+import Post from "./Post";
+import Comments from "../CommentSection/Comments";
+import dummyData from "./dummy-data";
+
+class PostsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      filteredPosts: []
+    };
+  }
+  componentDidMount() {
+    this.setState({ posts: dummyData });
+  }
+  searchPostsHandler = e => {
+    const posts = this.state.posts.filter(p => {
+      if (p.nickname.includes(e.target.value)) {
+        return p;
+      }
+    });
+    this.setState({ filteredPosts: posts });
+  };
+  render() {
+    return (
+      <div className="PostsPage">
+        <Header
+          searchTerm={this.state.searchTerm}
+          searchPosts={this.searchPostsHandler}
+        />
+        <PostContainer
+          posts={
+            this.state.filteredPosts.length > 0
+              ? this.state.filteredPosts
+              : this.state.posts
+          }
+        />
+      </div>
+    );
+  }
+}
+
+export default PostsPage;
