62b380ce18d8266d7f7320202c2bcbdb74028deb
Courtney Buratto <you@example.com>
2018-10-10 15:30:15 -0700

    FIX (reorganized App.js, included SearchBar, now to work on func. components)

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,23 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
-import dummyData from './dummy-data';
-import PostContainer from './components/PostContainer';
-
-
-
-
+import React, { Component } from "react";
+import "./App.css";
+import dummyData from "./dummy-data";
+import SearchBar from "./components/SearchBar";
+import PostContainer from "./components/PostContainer";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       dummyData: dummyData
-    }
+    };
   }
   render() {
     return (
-      <div className = "App">
-        <PostContainer dummyData = {this.state.dummyData} />
+      <div className="App">
+        <SearchBar />
+        {this.state.dummyData.map(post => {
+          return <div className="post">
+              <PostContainer dummyData={this.state.dummyData} />
+            </div>
+        })}
       </div>
     );
   }
