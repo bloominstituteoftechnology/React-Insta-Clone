1535b758368f055daddec75bf524d0630a6546ea
ShardeDeLane <rhcprocker@yahoo.com>
2018-10-10 01:43:24 -0400

    made authentication folder

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './App.css'
 import PostsPage from './Components/PostContainer/PostsPage'
+import Authenticate from './Components/Authentication/Authenticate'
 
 const App = () => <PostsPage />
 
-export default App
+export default Authenticate(App)
