fc843bd8ff018f54167512ff298f52a8c89a414a
Blake <koenneckerb@gmail.com>
2018-10-10 13:56:51 -0700

    Created Login directory and files.

---
+++ instagram/src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Login;
