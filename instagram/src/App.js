d637a1c03cbfcc1e25c671fa0f07f6d51a0d6b02
Brian G <brian.gregorius@gmail.com>
2018-10-10 15:29:22 -0400

    setup PostsPage and refactored app.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -6,59 +6,20 @@ import SearchBar from './components/SearchBar/seachbar';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import PostsPage from './components/PostContainer/PostsPage';
 
 class App extends Component {
   constructor(){
-    super()
-    this.state={
-      userData: [],
-      searchedPost:[],
-      searchResult:'',
-      comment:""
-    };
-  console.log(this.state);
-  
-  }
-
-  componentDidMount() {
-        setTimeout(() => {
-      this.setState({ userData: dummyData });
-    }, 500);
-  }
-
-  handleInput= event =>{
-    this.setState({ searchResult: event.target.value});
-  }
-  
-  searchResults= event =>{
-this.handleInput(event);
-this.setState(prevState =>{
-  const searchedPost = prevState.userData.filter(result =>{
-    return result.username.includes(prevState.searchResult);
-  });
-  return{searchedPost: searchedPost};
-});
-  }
+    super();
+    this.state={}
+  };
 
 
  
   render() {
     return (
       <div className="App">
-
-      <SearchBar 
-      changeHandle={this.searchResults}
-      searchPost={this.state.searchResult}
-      />
-
-      <PostContainer 
-      userData={
-        this.state.searchedPost.length >0
-        ? this.state.searchedPost
-      :this.state.userData
-      }/>    
-        
+      <PostsPage />
         
       </div>
     );
