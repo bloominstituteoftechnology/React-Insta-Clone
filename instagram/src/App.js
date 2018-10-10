b775a1e2c7095d6410caaf6188cdf616e1048d62
Adam <ajmboosh@gmail.com>
2018-10-10 15:33:07 -0500

    day3 task complete: built Authenticate HOC and passed App through it in App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from "react";
 
 import "./App.css";
-import dummyData from "./dummy-data";
-import PostContainer from "./components/PostContainer/PostContainer";
-import SearchBar from "./components/SearchBar/SearchBar";
 import PostsPage from "./components/PostContainer/PostsPage";
+import Authenticate from "./Authentication/Authenticate";
+
+Authenticate(App);
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      postData: [],
-      newSearch: "",
-      filtered: false
-    };
   }
 
   render() {
---
+++ instagram/src/Authentication/Authenticate.js
@@ -0,0 +1,10 @@
+import React from "react";
+//HOC magic
+const Authenticate = Component =>
+  class extends React.Component {
+    render() {
+      return <Component />;
+    }
+  };
+
+export default Authenticate;
