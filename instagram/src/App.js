ffa53085d9a7ad9e4c90f698fa750ee0e99ee56a
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-10-10 16:12:19 -0400

    moves App content to PostsPage and renders PostsPage from App

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,82 +1,19 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import './App.css';
-import dummyData from './data/dummy-data';
-import PostContainer from './components/PostContainer/PostContainer';
-import SearchBar from './components/SearchBar/SearchBar';
+//import dummyData from './data/dummy-data';
+//import PostContainer from './components/PostContainer/PostContainer';
+//import SearchBar from './components/SearchBar/SearchBar';
+
+import PostsPage from './components/PostContainer/PostsPage'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      posts: []
-    }
-  }
-
-  //once component is mounted, set state of App
-  componentDidMount() {
-    this.setState({
-      posts: dummyData.map((post, index) => {
-        post.id = index + 1;
-        return post;
-      })
-    });
-  }
-
-  submitSearch = (event, value) => {
-    event.preventDefault();
-    if (!value) {
-      this.setState({
-        posts: dummyData
-      });
-    } else {
-      const regex = RegExp(value, 'i');
-      const filteredDummyData = dummyData.filter((post) => regex.test(post.username));
-      if (filteredDummyData.length) {
-        this.setState({
-          posts: filteredDummyData
-        });
-      }
-      else {
-        this.setState({
-          posts: []
-        });
-      }
-    }
-  }
 
   render() {
     return (
-      <div className="App">
-        <SearchBar submitSearch={this.submitSearch} />
-        {(this.state.posts || []).map((post, index) => <PostContainer post={post} key={post.id} />)}
-        {/* {this.state.posts.map((post, index) => {
-        if(post.username.contains(searchValue)){
-          return <PostContainer post={post} key={index} />
-        }
-      }) */}
-
-      </div>
+      <PostsPage />
     );
   }
-}
-
-//validate data
-App.propTypes = {
-  posts: PropTypes.arrayOf(
-    PropTypes.shape({
-      username: PropTypes.string,
-      thumbnailUrl: PropTypes.string,
-      imageUrl: PropTypes.string,
-      likes: PropTypes.number,
-      timestamp: PropTypes.string,
-      comments: PropTypes.arrayOf(
-        PropTypes.shape({
-          username: PropTypes.string,
-          text: PropTypes.string
-        }))
-    })
-  )
 }
 
 export default App;
---
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './PostContainer.css';
+import dummyData from '../../data/dummy-data';
+import PostContainer from './PostContainer';
+import SearchBar from '../SearchBar/SearchBar';
+
+class PostsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    }
+  }
+
+  //once component is mounted, set state of App
+  componentDidMount() {
+    this.setState({
+      posts: dummyData.map((post, index) => {
+        post.id = index + 1;
+        return post;
+      })
+    });
+  }
+
+  submitSearch = (event, value) => {
+    event.preventDefault();
+    if (!value) {
+      this.setState({
+        posts: dummyData
+      });
+    } else {
+      const regex = RegExp(value, 'i');
+      const filteredDummyData = dummyData.filter((post) => regex.test(post.username));
+      if (filteredDummyData.length) {
+        this.setState({
+          posts: filteredDummyData
+        });
+      }
+      else {
+        this.setState({
+          posts: []
+        });
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SearchBar submitSearch={this.submitSearch} />
+        {(this.state.posts || []).map((post, index) => <PostContainer post={post} key={post.id} />)}
+        {/* {this.state.posts.map((post, index) => {
+        if(post.username.contains(searchValue)){
+          return <PostContainer post={post} key={index} />
+        }
+      }) */}
+
+      </div>
+    );
+  }
+}
+
+//validate data
+PostsPage.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      username: PropTypes.string,
+      thumbnailUrl: PropTypes.string,
+      imageUrl: PropTypes.string,
+      likes: PropTypes.number,
+      timestamp: PropTypes.string,
+      comments: PropTypes.arrayOf(
+        PropTypes.shape({
+          username: PropTypes.string,
+          text: PropTypes.string
+        }))
+    })
+  )
+}
+
+export default PostsPage;
