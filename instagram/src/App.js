5e04498713702fd287486bf3cb77bc18f2ccbfd1
Gannon <gkleblanc@gmail.com>
2018-10-10 11:46:09 -0400

    testing something

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,47 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
-import dummyData from "../src/dummy-data";
-import PostsContainer from "./components/PostContainer/PostContainer";
-import SearchBar from "./components/SearchBar/SearchBar";
+import PostsPage from "./components/PostContainer/PostsPage";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      posts: [],
-      filteredPosts: []
-    };
+    this.state = {};
   }
-
-  componentDidMount() {
-    this.setState({ posts: dummyData });
-  }
-  searchPostsHandler = event => {
-    const posts = this.state.posts.filter(post => {
-      if (post.username.includes(event.target.value)) {
-        return post;
-      }
-    });
-    this.setState({ filteredPosts: posts });
-  };
 
   render() {
     return (
       <div className="App">
-        <SearchBar
-          searchTerm={this.state.searchTerm}
-          searchPosts={this.searchPostsHandler}
-        />
-        <PostsContainer
-          posts={
-            this.state.filteredPosts.length > 0
-              ? this.state.filteredPosts
-              : this.state.posts
-          }
-        />
+        <PostsPage />
       </div>
     );
   }
 }
-
 export default App;
---
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import dummyData from "../../dummy-data";
+import PostsContainer from "./PostsContainer";
+import SearchBar from "../SearchBar/SearchBar";
+
+class PostsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      filteredPosts: []
+    };
+  }
+  componentDidMount() {
+    this.setState({ posts: dummyData });
+  }
+
+  searchPostsHandler = event => {
+    // eslint-disable-next-line
+    const posts = this.state.posts.filter(post => {
+      if (post.username.includes(event.target.value)) {
+        return post;
+      }
+    });
+    this.setState({ filteredPosts: posts });
+  };
+  render() {
+    return (
+      <div className="App">
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          searchPosts={this.searchPostsHandler}
+        />
+        <PostsContainer
+          posts={
+            this.state.filteredPosts.length > 0
+              ? this.state.filteredPosts
+              : this.state.posts
+          }
+        />
+      </div>
+    );
+  }
+}
+
+export default PostsPage;
