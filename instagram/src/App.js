6819d8903e81a48d4e9fb4f1677810f7dbd3a1df
Axolotl <samuelmachat@gmail.com>
2018-10-10 19:06:41 -0400

    removed injectGlobal

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,18 +1,7 @@
 import PostsPage from './components/PostContainer/PostsPage'
 import Authenticate from './components/Authentication/Authenticate'
 import Login from './components/Login/Login'
-import { injectGlobal } from 'styled-components'
 
 import './reset.css'
-
-injectGlobal`
-  html {
-    font-size: 62.5%;
-  }
-
-  * {
-    box-sizing: border-box;
-  }
-`
 
 export default Authenticate(PostsPage)(Login)
