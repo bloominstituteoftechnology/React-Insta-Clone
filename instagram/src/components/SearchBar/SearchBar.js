3071f63181af034c62fdd67c4581c48e2f456c9c
Jesse <theiseja@gmail.com>
2018-10-10 07:20:45 -0500

    Created components directory, will sort out issue today

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+        <div className='search-bar-wrap'>
+        <header class='head'>
+            <img src ='./src/assets/iglogo.png' alt='instagram logo' />
+        </header>
+        
+        </div>
+    )
+}
+
+export default SearchBar;
