044942c5f4b784ea2e6b63835679b75963ab7556
Drewsup123c <drewsup123c@gmail.com>
2018-10-09 20:44:57 -0600

    random changes

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import './CommentSectionStyles.css';
 
-const addNewComment =(event,index)=>{
-    if(event.key === "Enter"){
-        event.stopPropagation();
-        console.log("Enter was clicked")
-    }
-}
+// const addNewComment =(event,index)=>{
+//     if(event.key === "Enter"){
+//         event.stopPropagation();
+//         console.log("Enter was clicked")
+//     }
+// }
 
-
-const CommentSection = props =>{
-    return(
-        <div>
-        {props.comments.map((comment,index) => {
-            return(
-                <p key={index}><span className="comment-username">{comment.username}</span> {comment.text}</p>
-            )
-        })}
-        <input onKeyPress={addNewComment} className="comment-form" type="text" placeholder="Add a comment" />
-        </div>
-    )
-}
+//Make a class component
+// const CommentSection = props =>{
+//     return(
+//         <div>
+//         {props.comments.map((comment,index) => {
+//             return(
+//                 <p key={index}><span className="comment-username">{comment.username}</span> {comment.text}</p>
+//             )
+//         })}
+//         <input onKeyPress={addNewComment} className="comment-form" type="text" placeholder="Add a comment" />
+//         </div>
+//     )
+// }
 
 export default CommentSection;
