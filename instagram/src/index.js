ae0443d75374d8348749824cdcaa788523152875
Elvis Ibarra <ielvisd@gmail.com>
2018-10-10 13:16:05 -0700

    Day 2 fresh start

--- instagram/src/index.js
+++ instagram/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
