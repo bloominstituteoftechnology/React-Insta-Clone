d9e378a676cd6e2d5a07a56f44c0c7392c867851
Blake <koenneckerb@gmail.com>
2018-10-10 13:53:46 -0700

    Added Authenticate.js and imported to App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import PostsPage from "./components/PostContainer/PostsPage";
+import Authenticate from "./components/Authentication/Authenticate";
 class App extends Component {
   render() {
     return <PostsPage />;
   }
 }
 
-export default App;
+export default Authenticate(App);
