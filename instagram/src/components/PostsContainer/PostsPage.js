5c1260c5cdc5fd72696ffa96d6b9edd7363e02bc
Elvis Ibarra <ielvisd@gmail.com>
2018-10-10 15:43:52 -0700

    Added Postspage

---
+++ instagram/src/components/PostsContainer/PostsPage.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+import dummyData from './dummy-data';
+import PostsContainer from './components/PostsContainer/PostsContainer';
+import SearchBar from './components/SearchBar/SearchBarContainer';
+
+// Creating the overall app. This sets the state and creates the searchPostsHandler which matches and returns the filtered posts. IF there are no matches it returns all the posts. This only searches the username titles. 
+class PostsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      filteredPosts: []
+    };
+  }
+  componentDidMount() {
+    this.setState({ posts: dummyData });
+  }
+  searchPostsHandler = e => {
+    const posts = this.state.posts.filter(p => {
+      if (p.username.includes(e.target.value)) {
+        return p;
+      }
+    });
+    this.setState({ filteredPosts: posts });
+  };
+  render() {
+    return (
+      <div className="PostsPage">
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          searchPosts={this.searchPostsHandler}
+        />
+        <PostsContainer
+          posts={
+            this.state.filteredPosts.length > 0
+              ? this.state.filteredPosts
+              : this.state.posts
+          }
+        />
+      </div>
+    );
+  }
+}
+
+          // If there are filtered posts then display those but otherwise set the state to all the posts.
+export default PostsPage;
