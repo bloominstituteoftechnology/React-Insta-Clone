8ce7db5c10112a4acb20ba9ce8d60372267ce5ca
Sungyun Won <seanwon8@gmail.com>
2018-10-10 14:41:40 -0700

    testing

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SearchBar.css';
+
+const SearchBar = () => {
+    return (
+      <div className="search-bar container">
+        <div>
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+    );
+  };
+  
+  export default SearchBar;
+  
