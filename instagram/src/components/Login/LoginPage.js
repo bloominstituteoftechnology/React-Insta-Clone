c3860dcfd55864d7d6df8b6a9d9e1837711b79fe
Yanrong Xu <xuyanrong@icloud.com>
2018-10-10 17:39:32 -0400

    built out LoginPage

---
+++ instagram/src/components/Login/LoginPage.js
@@ -0,0 +1,48 @@
+import React from "react";
+ class LoginPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+   login() {
+    //save username and password to state
+    localStorage.setItem("username", this.state.username);
+    //change state
+    this.setState({
+      username: "",
+      password: ""
+    });
+  }
+   changeHandler = (key, value) => {
+    this.setState({
+      [key]: value
+    });
+  };
+   render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            className="login-username"
+            placeholder="Username"
+            onChange={this.changeHandler("username", event.target.value)}
+            value={this.state.username}
+          />
+          <input
+            type="text"
+            className="login-password"
+            placeholder="Password"
+            onChange={this.changeHandler("password", event.target.value)}
+            value={this.state.password}
+          />
+          <button className="login-button">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+ export default LoginPage;
