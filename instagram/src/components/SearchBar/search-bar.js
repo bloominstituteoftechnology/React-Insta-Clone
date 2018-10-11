7fc88aea7dd7c1065e046c216acf308ea836f49b
SavannahGreuel <smallidgesavannah@gmail.com>
2018-10-11 15:30:29 -0600

    working on styling

--- instagram/src/components/SearchBar/search-bar.js
+++ instagram/src/components/SearchBar/search-bar.js
@@ -5,26 +5,54 @@ import compass from '../../images/compass.png';
 import heart from '../../images/Instagram-Heart.png';
 import person from '../../images/Instagram-Person.png';
 import line from'../../images/line.png';
+import styled from 'styled-components';
+
+const Header = styled.header`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin: 20px;
+`;
+
+const logoheader = styled.logoheader`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 30px;
+    padding: 20px;
+`;
+const Img = styled.img`
+    margin: 0px;
+
+    ${props => (props.type === 'instaLogo' ? `height: 40px;` : null)}
 
 
+`;
+
+const Logoimage = styled.Logoimage`
+
+
+`
  const SearchBar = (props) => {
     return (
-    <div className = 'searchBarContainer'>
-        <div className  = 'leftContent'>
-            <img className ='instaLogo' src={insta_logo} alt = 'logo' />
+    <header className = 'searchBarContainer'>
+        <logoheader className  = 'leftContent'>
+            <img type='instaLogo' src={insta_logo} alt = 'logo' />
             <img className = 'line' src ={line} alt = 'line' />
             <img className = 'textLogo' src={instagramTextLogo} alt = 'text logo'/>
-        </div>
-         <div className = 'searchBarFunction'>
+        </logoheader>
+         <form className = 'searchBarFunction'>
            
             <input type="text" placeholder="Search" onKeyDown={props.searchPosts} />
             
-        </div>
-         <div className ='rightContent'>
+        </form>
+         <logoimage className ='rightContent'>
             <img className ='compass' src={compass} alt ='compass' />
             <img className = 'heart' src = {heart} alt ='heart'/>
             <img className ='person' src ={person} alt = 'person'/>
-        </div>
-    </div>
+        </logoimage>
+    </header>
     )}
  export default SearchBar;
