cb5a450162bacbad0513a4962a5484dd0a5a26cf
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-11 01:48:59 -0400

    #

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -40,25 +40,9 @@ class CommentSection extends React.Component {
         this.setState({ comment: event.target.value });
     };
 
-<<<<<<< HEAD
     submitCommentHandle = e => {
         e.preventDefault();
         const newComment = { text: this.state.comment, username: 'Pedro Montesinos' };
-=======
-    // submitCommentHandle = event => {
-    //     event.preventDefault();
-    //     const newComment = {text: this.state.comment, username: 'Pedro Montesinos'};
-    //     const comments = this.state.comments.slice();
-    //     comments.push(newComment);
-    //     this.setState({ comments, comment: '' });
-    //     setTimeout(() => {
-    //         this.addComments();
-    //     }, 300);
-    // };
-    submitCommentHandle = e => {
-        e.preventDefault();
-        const newComment = { text: this.state.comment, username: 'ryanhamblin' };
->>>>>>> f0034b3e0cbf6540a366b8fd97885d8bf551c9da
         const comments = this.state.comments.slice();
         comments.push(newComment);
         this.setState({ comments, comment: '' });
@@ -70,11 +54,7 @@ class CommentSection extends React.Component {
     render() {
         return (
             <div>
-<<<<<<< HEAD
                 {this.state.comments.map((comment, index) => <Comment key={index} comment={comment} />)}
-=======
-                {this.props.comments.map((comment, index) => <Comment key={index} comment={comment} />)}
->>>>>>> f0034b3e0cbf6540a366b8fd97885d8bf551c9da
                 <CommentInput
                     comment={this.state.comment}
                     submitComment={this.submitCommentHandle}
