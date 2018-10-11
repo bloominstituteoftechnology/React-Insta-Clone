4ca04d81ff2e4f35d492c0699e03c32448c8f872
Rahul Desai <rd825@georgetown.edu>
2018-10-11 13:22:33 -0400

    Completed styling for Post Container via styled components

--- instagram/src/components/PostContainer/PostHeader.js
+++ instagram/src/components/PostContainer/PostHeader.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const PostHead = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+`;
+
+const Thumbnail = styled.img`
+    display: inline-block;
+    height: 30px;
+    width: auto;
+    border-radius: 50px;
+    margin: 10px 0 0 15px;
+`;
+
+const Username = styled.h2`
+    display: inline-block;
+    font-size: 15px;
+    margin-top: 19px;
+    margin-left: 9px;
+`;
 
 let PostHeader = props => {
     return (
-        <div className='postHeader'>
-            <img className='thumbnail' alt={props.post.username} src={props.post.thumbnailUrl} />
-            <h2 className="username">{props.post.username}</h2>
-        </div>
+        <PostHead>
+            <Thumbnail alt={props.post.username} src={props.post.thumbnailUrl} />
+            <Username>{props.post.username}</Username>
+        </PostHead>
     )
 }
 
--- instagram/src/components/PostContainer/Posts.js
+++ instagram/src/components/PostContainer/Posts.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import PostContainer from './PostContainer';
 import './postcontainer.css';
+import styled from 'styled-components';
+
+let AllPosts = styled.div`
+    margin: 0 auto;
+    max-width: 600px;
+    margin-bottom: 50px;
+    padding-top: 60px;
+`;
 
 let Posts = (props) => {
     if (props.searchString === '') {
         return (
-            <div className="posts">
+            <AllPosts>
                 {props.postData.map((post, i) => <PostContainer key={i} post={post} username={props.username}/>)}
-            </div>
+            </AllPosts>
         )
     }
+
     else {
         let searchArr = props.postData.filter(post => post.username.includes(props.searchString));
         return (
-            <div className="posts">
+            <AllPosts>
                 {searchArr.map((post) => <PostContainer post={post} username={props.username}/>)}
-            </div>
+            </AllPosts>
         )
-
     }
 }
 
