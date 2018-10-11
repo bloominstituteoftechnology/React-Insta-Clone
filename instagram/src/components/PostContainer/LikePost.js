6268ecb038639a65a76b86c10da9e76a51833e39
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-11 01:44:08 -0400

    managed to display new comments and saved them in local storage

--- instagram/src/components/PostContainer/LikePost.js
+++ instagram/src/components/PostContainer/LikePost.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const LikePost = () => {
+const LikePost = (props) => {
     return [
-        <div className="like-section" key="likes-icons">
+        <div className="like-section" key="likes-icons" onClick={props.increment}>
             <div className="icons-container">
                 <i className="far fa-heart fa-2x"></i>
                 <i className="far fa-comment fa-2x"></i>
             </div>
         </div>,
         <div className="like-section" key="likes-container">
-            <div className="likes">93,430,972</div>
+            <div className="likes">{props.likes}</div>
         </div>
     ]
 }
