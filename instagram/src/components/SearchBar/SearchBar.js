997bd3b7b4d99756f628bd09e8a136fd97beb1bc
Axolotl <samuelmachat@gmail.com>
2018-10-11 18:08:04 -0400

    styled search bar (still working on input field)

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,19 +1,41 @@
 import React from 'react'
 import logo from '../../img/glyph-logo_May2016.png'
-import { Container, Logo, Text } from './SearchBarStyles'
+import {
+  Container,
+  Group,
+  Logo,
+  Text,
+  Input,
+  Placeholder,
+  Form
+} from './SearchBarStyles'
+
+// import sprites
+import { Person, Heart, Search } from '../Sprites/Sprites'
 
 const SearchBar = ({ searchParam, search, handleSearchInputChange }) => (
   <Container>
-    <Logo src={logo} alt="instagram" />
-    <Text>Instagram</Text>
-    <form onSubmit={search}>
-      <input
-        type="text"
-        placeholder="Search"
-        value={searchParam}
-        onChange={handleSearchInputChange}
-      />
-    </form>
+    <Group>
+      <Logo src={logo} alt="instagram" />
+      <Text>Instagram</Text>
+    </Group>
+
+    <Group>
+      <Form onSubmit={search}>
+        <Input
+          type="text"
+          value={searchParam}
+          onChange={handleSearchInputChange}
+        />
+        <Placeholder>
+          <Search />Search
+        </Placeholder>
+      </Form>
+    </Group>
+    <Group>
+      <Heart />
+      <Person />
+    </Group>
   </Container>
 )
 
