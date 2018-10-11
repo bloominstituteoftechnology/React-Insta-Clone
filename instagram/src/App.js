150f8c457b6122f0ee18d45748f8f9ac637d4a6f
David247365 <ds051128@gmail.com>
2018-10-10 21:30:40 -0500

    Finished styling on Login page

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PostsPage from "./components/PostsContainer/PostsPage";
 import Authenticate from "./components/Authentication/Authenticate";
 
--- instagram/src/components/PostsContainer/LikeSection.js
+++ instagram/src/components/PostsContainer/LikeSection.js
@@ -6,7 +6,7 @@ const LikeSection = props => {
   return [
     <div className="like-section" key="likes-icons-container">
       <div onClick={props.incrementLike}>
-        <i className="far fa-heart" />
+        <i className="fas fa-heart" />
       </div>
       <div>
         <i className="far fa-comment" />
