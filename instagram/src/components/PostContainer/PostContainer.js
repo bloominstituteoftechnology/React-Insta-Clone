c4c6b1f38c8e71542ee7b066fdac21478a94b2c4
Denise Escobar <descobar1490@gmail.com>
2018-10-11 15:02:32 -0500

    Made postContainer into a class component

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CommentSection from '../CommentSection/CommentSection';
 import './PostContainer.css';
 
+class PostContainer extends Component{
+    constructor(props){
+        super(props);
+        this.state={
 
+        }
+    }
 
-const PostContainer = (props) =>{
-    return(
-        <div className='wholePost'>
-            <div className='titleBar'>
-                <img src={props.post.thumbnailUrl} alt='' />
-                <span>{props.post.username}</span>   
-            </div>
-            <div className='image'>
-                <img src={props.post.imageUrl} alt=''/>    
-            </div>
-            <div className='comments'>
-                <div>
-                    <i className="far fa-heart"></i>
-                    <i className="far fa-comment"></i>
+    render(){
+        return(
+            <div className='wholePost'>
+                <div className='titleBar'>
+                    <img src={this.props.post.thumbnailUrl} alt='' />
+                    <span>{this.props.post.username}</span>   
                 </div>
-                <p>{props.post.likes} likes</p>
-                <CommentSection comments={props.comments} timestamp={props.post.timestamp} />
+                <div className='image'>
+                    <img src={this.props.post.imageUrl} alt=''/>    
+                </div>
+                <div className='comments'>
+                    <div>
+                        <i className="far fa-heart"></i>
+                        <i className="far fa-comment"></i>
+                    </div>
+                    <p>{this.props.post.likes} likes</p>
+                    <CommentSection comments={this.props.comments} timestamp={this.props.post.timestamp} />
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 
