83c92bcc9bae342d66e21de45d5664373eb3deec
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-10-11 14:26:25 -0400

    completed the authenticate file

--- instagram/src/Authentication/Authenticate.js
+++ instagram/src/Authentication/Authenticate.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import LoginPage from '../components/Login/LoginPage'
 
 const Authenticate = App =>  
   class extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        isLoggedIn : false
+      };
+    }
+
+    componentDidMount() {
+      if(!localStorage.getItem('username')) {
+        this.setState({isLoggedIn : false})
+      } else {
+        this.setState({isLoggedIn :  true})
+      }
+    }
+
     render() {
-      return <App />
+      if (this.state.isLoggedIn) return <App />;
+      return <LoginPage />
     }
   };
 
