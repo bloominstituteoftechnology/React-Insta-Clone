2efb719a05094288f6e34ef0c1bc9c0d9b73137b
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-11 16:16:18 -0500

    Post container / post header styled-components

--- instagram/src/components/PostContainer/PostHeader.js
+++ instagram/src/components/PostContainer/PostHeader.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import './Post.css';
+import styled, { css } from 'styled-components';
+
+const PostsHeader = styled.div`
+    display: flex;
+    `;
+const ThumbnailWrapper = styled.div`
+  height: 50px;
+  width: 50px;
+  margin: 5px 10px;
+  `;
+
+const UserName = styled.div`
+    margin: 20px 10px;
+    ${props => props.bold && css`
+        font-weight: bold;
+        `}
+    `;
 
 const PostHeader = (props) => {
     return(
-        <div className="post-header">
-            <div className="thumbnail-wrapper">
+        <PostsHeader>
+            <ThumbnailWrapper>
                 <img  alt="post thumbnail" className="post-thumbnail" src={props.thumbnailUrl}/>
-            </div>
-            <div className="header-user">{props.username}</div>
-        </div>
+            </ThumbnailWrapper>
+            <UserName bold>{props.username}</UserName>
+        </PostsHeader>
     );
     
 }
--- instagram/src/components/PostContainer/PostsContainer.js
+++ instagram/src/components/PostContainer/PostsContainer.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Post from './Post';
-import './Post.css';
+
+import styled from 'styled-components';
+
+const PostContainer = styled.div`
+    height: 500px;
+    width: 50%;
+    margin: 0 auto;
+    `;
 
 const PostsContainer = props => {
     return (
-        <div className="post-container">
+        <PostContainer>
             {props.posts.map(p => <Post key={p.imageUrl} post={p} />)}
-        </div>
+        </PostContainer>
     );
 }
 
