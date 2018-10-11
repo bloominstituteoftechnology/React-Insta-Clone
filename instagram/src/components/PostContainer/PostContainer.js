ecbf918efadcb65caca6a6056bc219d78b615443
Gannon <gkleblanc@gmail.com>
2018-10-11 09:27:43 -0400

    fixed an error with my JSON files, made style edits to Post Contatiner

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Post from "./Posts";
 import "./PostContainerStyle.css";
+import styled from "styled-components";
+
+const PostContainer = styled.div`
+  height: 40%;
+  width: 50%;
+  min-width: 300px;
+  margin: 5px auto;
+`;
 
 const PostsContainer = props => {
   return (
-    <div className="PostsContainerContainer">
+    <PostContainer>
       {props.posts.map(post => (
         <Post key={post.imageUrl} post={post} />
       ))}
-    </div>
+    </PostContainer>
   );
 };
 
