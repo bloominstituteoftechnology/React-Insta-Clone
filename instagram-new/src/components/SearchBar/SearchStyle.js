aa9a582b4ea1a99404e90b9843c862e3420eb8de
BryanGf <briangf@uw.edu>
2018-10-11 13:08:28 -0700

    Forgot to add

---
+++ instagram-new/src/components/SearchBar/SearchStyle.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const StyleSearchInput = styled.input`
+    height: 2.5rem;
+    width: 21.2rem;
+    text-align: center;
+    border: 1px solid gray;
+    font-size: 1.5rem;
+    border-radius: .3rem;
+    background: #f7f6f6;
+`;
+
+export const StyleSearhBar= styled.div`
+    display: flex;
+    padding: 1.9rem 1.5rem;
+    align-items: center;
+    margin-bottom: 2rem;
+    box-shadow: 0px 1px 1px 0px #ccc;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    background: white;
+`;
+
+export const StyleImgLogo = styled.img`
+    width: 3rem;
+    height: 3rem;
+    padding-right: 2%;
+    border-right: 1px solid black;
+`;
+
+export const StyleWordLogo = styled.img`
+    width: 11.3rem;
+    height: 4rem;
+    padding-left: 2%;
+    padding-right: 19%;
+    
+`;
