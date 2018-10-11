5d2864cd06b928b8dc4d4d10567b2fcf30560cb4
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-11 03:30:02 -0400

    created event handlers for the input fields and button also set a username on localStorage

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,15 +1,40 @@
 import React from 'react';
 
 class Login extends React.Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        };
     }
+
+    inputChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+    loginHandle = () => {
+        const user = this.state.username;
+        localStorage.setItem('user', user);
+        window.location.reload();
+    };
     render() {
         return(
             <form className="login-form">
-                <input type="text" />
-                <input type="password" />
-                <button>Login</button>
+            <h1>Log in!</h1>
+                <input type="text" 
+                    name="username" 
+                    placeholder="username"
+                    value={this.state.username} 
+                    onChange={this.inputChange}
+                />
+
+                <input type="password" 
+                    name="password" 
+                    placeholder="password"
+                    value={this.state.password} 
+                    onChange={this.inputChange}
+                />
+                <button onClick={this.loginHandle}>Login</button>
             </form>
         );
     }
