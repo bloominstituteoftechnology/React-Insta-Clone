5add1663989238522495b80d1d354e1de26cfacc
stevekutz <stkutz@gmail.com>
2018-10-10 21:50:42 -0500

    update check again

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,31 +1,57 @@
 import React, { Component } from 'react';
 import dummyData from './components/dummy-data'
 import SearchBar from './components/Searchbar/SearchBar';
-
-
+import PostContainer from "./components/PostContainer/PostContainer";
 import './App.css';
 
 
 
-class App extends Component {
+export default class App extends Component {
   constructor () {
     super();
     this.state = {
-      dummyData: dummyData,
-      comment: ''
-
-    }
+      dummyData: [],
+      search: ''
+    };
   }
 
+  componentDidMount () {
+    setTimeout( () => {
+      this.setState({dummyData})
+    }, 1000)
+  }
 
+  handleSearch = event => {
+    this.setState({searchInfo: event.target.value});
+  };
 
+  searchDemPosts = event => {
+    event.preventDefault();
+    if(this.state.searchInfo) {
+      this.setState({
+        dummyData: this.state.dummyData.filter(post => {
+          if(post.username.includes(this.state.searchInfo)) return post;
+        })
 
+      })} else this.componentDidMount();
+
+  };
 
   render() {
+
+    if(!this.state.dummyData) {
+      return <div className = 'loadingMSG'> loading...</div>
+    }
+
     return (
       <div className='App-container'>
-        <SearchBar/>
+        <SearchBar
+          handleSearch = {this.handleSearch}
+          searchDemPosts = {this.searchDemPosts}
+          searchInfo = {this.state.searchInfo}
+        />
 
+        <PostContainer data = {this.state.dummyData}/>
 
       </div>
 
@@ -34,4 +60,3 @@ class App extends Component {
   }
 }
 
-export default App;
