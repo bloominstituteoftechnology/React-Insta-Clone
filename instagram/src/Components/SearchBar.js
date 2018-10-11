5f7b06ce534bd26605da0504cc6952189cff0357
Michael Checo <checomichael2@gmail.com>
2018-10-11 15:43:05 -0400

    converted search bar to a styled component

--- instagram/src/Components/SearchBar.js
+++ instagram/src/Components/SearchBar.js
@@ -1,25 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Instagramtext, Searchdiv, RightIcons } from "./Styles"
 
 const SearchBar = props => {
+
     return (
-        <div className="search">
+        <Searchdiv>
             <div className="search-left">
         <i className="fab fa-instagram fa-2x"></i>
         <p className="line">|</p>
-            <p className="insta-border"> Instagram</p>
+           <Instagramtext>Instagram</Instagramtext>
             </div>
         <input placeholder="Search" type="text"
          className="search-input"
           value={props.filterTarget}
           onChange={props.changeHandler}
            name="filterTarget"/>
-        <div className="right-icons">
+        <RightIcons>
         <i className="far fa-compass fa-2x"></i>
         <i className="far fa-heart fa-2x"></i>
         <i className="far fa-user fa-2x"></i>
-        </div>
-        </div>
+        </RightIcons>
+        </Searchdiv>
     )
 }
 
---
+++ instagram/src/Components/Styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+let Instagramtext = styled.p`
+    font-size: 24px;
+    font-family: cursive;
+    font-weight: bold;
+    `
+    let Searchdiv = styled.div`
+    border-bottom: 1px solid lightgray;
+    display: flex;
+    height: 75px;
+    justify-content: space-between;
+    `
+    let RightIcons = styled.div`
+    width: 33%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    `
+    export {
+        Instagramtext,
+        Searchdiv,
+        RightIcons,
+    }
