29d8f12081af0f8b9eb1ec30ec0cf121290d41d3
erscriber <erscriber@gmail.com>
2018-10-11 00:31:34 -0400

    SearchBar code beginning

---
+++ instagram/src/components/SearchBarContainer/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SearchBar.css';
+
+const SearchBar = () => {
+	return (
+		<div>
+			<input type="text" placeholder="Search"/>
+		</div>
+	)
+};
+
+export default SearchBar;
