d2e6bdb6a4be82b3ce8bf7fd270b787481f60cd0
Michael Checo <checomichael2@gmail.com>
2018-10-11 01:44:41 -0400

    updated likes to use styled=components library

--- instagram/src/Components/PostContainer.js
+++ instagram/src/Components/PostContainer.js
@@ -3,12 +3,18 @@ import PropTypes from "prop-types";
 import CommentSection from "./CommentSection";
 import Post from "./Post";
 import Icons from "./Icons";
-
+import styled from "styled-components";
 
 const PostContainer = props => {
     if (!props.data.length) {
         return <h1>Loading ...</h1>
      }
+     const StyledP = styled.p`
+     width: 80%;
+     margin: 10px 90px;
+    display: flex;
+    font-weight: bold;
+     `
     return (
         <div className="post">
             <div>
@@ -17,7 +23,8 @@ const PostContainer = props => {
                         <div key={data.timestamp}>
                         <Post thumbnail={data.thumbnailUrl} image={data.imageUrl} username={data.username}/>
                         <Icons increment={props.increment}/>
-                        <p className="likes"> {props.likes} likes</p>
+                        <StyledP>{props.likes} likes</StyledP>
+                        {/* <p className="likes"> {props.likes} likes</p> */}
                         <div className="comments-container">
                         <CommentSection comments={data.comments} />
                         </div>
