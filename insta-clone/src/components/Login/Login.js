97ca123d264ac22d0b17956a5ebed84857f81280
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-10-11 17:56:52 -0400

    Basic login styling with JS

--- insta-clone/src/components/Login/Login.js
+++ insta-clone/src/components/Login/Login.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 
+const WrapperDiv = styled.div`
+   font-family: sans-serif;
+   text-align: center;
+`
 class Login extends Component {
     constructor(props) {
         super(props)
@@ -21,14 +26,15 @@ class Login extends Component {
     }
     render(){
     return (
-    <div>
+    <WrapperDiv>
      <h2>Login</h2>
      <form>
-         <input type="text" name="username" value="Username"></input>
-         <input type="text" name="password" value="Password"></input>
+         <input placeholder= "Username" type="text" name="username" value = {this.state.username} onChange= {this.handleInputChange}></input>
+         <input placeholder= "Password" type="text" name="password" value = {this.state.password} onChange= {this.handleInputChange}></input>
+         <button onClick={this.handleLoginSubmit}>Log In</button>
 
      </form>
-    </div>
+    </WrapperDiv>
     )
     }
 }
