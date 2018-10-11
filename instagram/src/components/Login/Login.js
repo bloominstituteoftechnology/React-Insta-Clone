60e2216acacaaa864a4163ed7977ac7cdd08c6bf
Kyle Teeter <kyleteeter12@gmail.com>
2018-10-10 22:22:09 -0500

    Reconfigured form into reactstrap

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 class Login extends Component {
     render() {
     return (
-        <form>
-            <input type="text" name="username" placeholder="Username" required />
-            <input type="password" name="password" placeholder="Password" required />
-            <input type="submit" value="Login" />
-        </form>
+        <Form inline>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Label for="exampleEmail" className="mr-sm-2">Username</Label>
+          <Input type="text" name="username" id="Username" placeholder="username" required />
+        </FormGroup>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Label for="examplePassword" className="mr-sm-2">Password</Label>
+          <Input type="password" name="password" id="Password" placeholder="..." required />
+        </FormGroup>
+        <Button type="submit" value="Login" >Submit</Button>
+      </Form>
     )
     }
 }
