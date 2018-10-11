dbc07714edc2b169d1318f47c91d800276f456eb
Angelo Deleon <rudy209@comcast.net>
2018-10-11 14:54:56 -0700

    Main App.js setup now adding components day1-4

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import PropTypes from 'prop-types';
+
 import PostPage from './components/PostPage/PostPage'
 import Authenticate from './components/Authentication/Authenticate';
 import Login from './components/Login/Login';
 // STARTING OVER
+
+
 class App extends Component {
   constructor() {
+    console.log('contructor is running');
+    this.state = {
+      username: ''
+    };
   }
+
+  componentDidMount() {
+    const user = localStorage.getItem('user');
+    this.setState({ username: user })
+  }
+
+  render() {
+    console.log('render', this.state.posts);
+
+    return (
+      <div className="App">
+
+      <ShowCorrectComponent username={this.state.username} />
+                
+      </div>
+    );
+  }
+}
+  const ShowCorrectComponent = Authenticate(PostPage)(Login);
+
 export default App;
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import './searchbar.css';
-
+import './SearchBar.css';
 
 const SearchBar = props => {
-   
+
+    return(
+    
+        <div className='searchBar'>
+            <div className='instaLogo'>
+                <img src='InstagramIcon.png' alt="instaLogo"></img>
+            </div>
+         
+            <input type="text" name="search" value={props.search} 
+                onChange={props.onInputChange} placeholder="Search"/>
+           
+            <div className='icons'>
+                <i className="far fa-compass"></i>
+                <i className="far fa-heart" ></i>
+    
+                <i className="far fa-user"></i>
+             </div>
+        </div>
+
+    );
 }
+
+
 
 export default SearchBar;
