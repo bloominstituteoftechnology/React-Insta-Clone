145938463100078879885e4df30f594464c15ae3
Jess <jessdev83@gmail.com>
2018-10-10 22:06:49 -0400

    Login Page Styling

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Login.css";
+import { Button } from "reactstrap";
 
 class Login extends React.Component {
   constructor() {
@@ -33,13 +35,22 @@ class Login extends React.Component {
   };
   render() {
     return (
-      <>
-        <p>Username</p>
-        <input type="text" defaultValue="" onChange={this.userChange} />
-        <p>Password</p>
-        <input type="text" defaultValue="" onChange={this.passwordChange} />
-        <div onClick={this.submitHandler}>Submit</div>
-      </>
+      <div className="login-page">
+        <h1>InstaClone Login</h1>
+        <div className="login-input">
+          <p>Username</p>
+          <input type="text" defaultValue="" onChange={this.userChange} />
+        </div>
+        <div className="login-input">
+          <p>Password</p>
+          <input type="text" defaultValue="" onChange={this.passwordChange} />
+        </div>
+        <div className="login-button">
+          <Button color="info" outline onClick={this.submitHandler}>
+            Submit
+          </Button>
+        </div>
+      </div>
     );
   }
 }
