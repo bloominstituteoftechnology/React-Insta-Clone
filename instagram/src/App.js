f59d10baf962f6f6047e5b9fa2835e54aefe775c
unknown <quan.nguyen199712@gmail.com>
2018-10-11 16:19:48 -0400

    added functionality to the log in page

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -4,38 +4,53 @@ import "./App.css";
 import PostContainer from "./PostContainer/PostContainer";
 import SearchBarContainer from "./SearchBar/SearchBarContainer";
 import dummyData from "./dummy-data";
-import PostPage from './PostContainer/PostPage';
-import authenticate from './Authentication/Authenticate';
-import Login from './Login/Login';
-class App extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      loggedIn: false
-    };
-  }
-  loginEvent=()=>{
-    this.setState({loggedIn:true})
-  }
+import PostPage from "./PostContainer/PostPage";
+import Authenticate from "./Authentication/Authenticate";
+import Login from "./Login/Login";
+// class App extends Component {
+//   constructor(props) {
+//     super();
+//     this.state = {
+//       loggedIn: false
+//     };
+//   }
+//   loginEvent=()=>{
+//     this.setState({loggedIn:true})
+//   }
 
+//   render() {
+//     if(this.state.loggedIn===false)
+//       return(
+//         <div className="App">
+//           <ShowConditionally loggedIn={this.state.loggedIn} />
+//           <button onClick={this.loginEvent}>Log In</button>
+//       </div>
+//       )
+//     else
+//       return (
+//           <div className="App">
+//             <ShowConditionally loggedIn={this.state.loggedIn} />
+
+//         </div>
+//     );
+//   }
+// }
+// const ShowConditionally = authenticate(PostPage)(Login);
+
+// export default App;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
   render() {
-    if(this.state.loggedIn===false)
-      return(
-        <div className="App">
-          <ShowConditionally loggedIn={this.state.loggedIn} /> 
-          <button onClick={this.loginEvent}>Log In</button>
+    return (
+      <div className="App">
+        <PostPage />
       </div>
-      )
-    else 
-      return (
-          <div className="App">
-            <ShowConditionally loggedIn={this.state.loggedIn} /> 
-            
-        </div>
     );
   }
 }
-const ShowConditionally = authenticate(PostPage)(Login);
 
-export default App;
+export default Authenticate(App);
--- instagram/src/Login/Login.js
+++ instagram/src/Login/Login.js
@@ -1,13 +1,72 @@
-import React, {Component} from 'react';
-const Login= prop =>{
-    return(
-        <div>
-            <form>
-                <input className="login-user" type='text' placeholder='Username'></input>
-                <input className="login-pwd" type='text' placeholder='Password'></input>
-            </form>
-        </div>
-    )
-        
+// import React, {Component} from 'react';
+// const Login= prop =>{
+//     return(
+//         <div>
+//             <form>
+//                 <input className="login-user" type='text' placeholder='Username'></input>
+//                 <input className="login-pwd" type='text' placeholder='Password'></input>
+//             </form>
+//         </div>
+//     )
+
+// }
+// export default Login;
+import React, { Component } from "react";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleLoginSubmit = e => {
+    const user = this.state.username;
+    localStorage.setItem("user", user);
+    window.location.reload();
+  };
+
+  render() {
+    return (
+      <div>
+        <form className="login-form">
+          <h3>Welcome to React Insta Clone</h3>
+          <div>Please Login</div>
+          
+            <input
+              type="text"
+              placeholder="User Name"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleInputChange}
+            />
+
+
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+            />
+            <br />
+            <button
+              color="success"
+              size="large"
+              onClick={this.handleLoginSubmit}
+            >
+              Log In
+            </button>
+        </form>
+      </div>
+    );
+  }
 }
+
 export default Login;
