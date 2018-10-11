bac41b0d7029499297882656a73a08e79207e40c
Carlos Lantigua <clantigua954@gmail.com>
2018-10-11 18:31:13 -0400

    fixed loader issue

--- instagram/src/components/LoadTime/LoadTime.js
+++ instagram/src/components/LoadTime/LoadTime.js
@@ -1,28 +1,27 @@
-import React from "react";
-import "./LoadTime.css";
+import React from 'react';
+import './LoadTime.css';
 
-const LoadTime = props => {
-  if (!props.length) {
-    return (
-      <div className="sk-fading-circle">
-        <div className="sk-circle1 sk-circle" />
-        <div className="sk-circle2 sk-circle" />
-        <div className="sk-circle3 sk-circle" />
-        <div className="sk-circle4 sk-circle" />
-        <div className="sk-circle5 sk-circle" />
-        <div className="sk-circle6 sk-circle" />
-        <div className="sk-circle7 sk-circle" />
-        <div className="sk-circle8 sk-circle" />
-        <div className="sk-circle9 sk-circle" />
-        <div className="sk-circle10 sk-circle" />
-        <div className="sk-circle11 sk-circle" />
-        <div className="sk-circle12 sk-circle" />
-      </div>
-    );
-  }
-  if (props.length) {
-    return null;
-  }
+const LoadTime = (props) => {
+	// console.log(props);
+	if (!props.posts.length) {
+		return (
+			<div className="sk-fading-circle">
+				<div className="sk-circle1 sk-circle" />
+				<div className="sk-circle2 sk-circle" />
+				<div className="sk-circle3 sk-circle" />
+				<div className="sk-circle4 sk-circle" />
+				<div className="sk-circle5 sk-circle" />
+				<div className="sk-circle6 sk-circle" />
+				<div className="sk-circle7 sk-circle" />
+				<div className="sk-circle8 sk-circle" />
+				<div className="sk-circle9 sk-circle" />
+				<div className="sk-circle10 sk-circle" />
+				<div className="sk-circle11 sk-circle" />
+				<div className="sk-circle12 sk-circle" />
+			</div>
+		);
+	}
+	return null;
 };
 
 export default LoadTime;
--- instagram/src/components/PostsContainer/PostsPage.js
+++ instagram/src/components/PostsContainer/PostsPage.js
@@ -1,48 +1,39 @@
-import React, { Component } from "react";
-import dummyData from "../../dummy-data";
-import PostsContainer from "./PostsContainer";
-import SearchBar from "../SearchBar/SearchBarContainer";
-import LoadTime from "../LoadTime/LoadTime";
+import React, { Component } from 'react';
+import dummyData from '../../dummy-data';
+import PostsContainer from './PostsContainer';
+import SearchBar from '../SearchBar/SearchBarContainer';
+import LoadTime from '../LoadTime/LoadTime';
 
 class PostsPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      dummyData: [],
-      posts: [],
-      filteredPosts: []
-    };
-  }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ posts: dummyData });
-    }, 800);
-  }
-  searchPostsHandler = e => {
-    const filteredPosts = this.state.posts.filter(p => {
-      return p.username.includes(e.target.value);
-    });
-    return this.setState({ filteredPosts });
-  };
-  render() {
-    return (
-      <div className="App">
-        <SearchBar
-          searchTerm={this.state.searchTerm}
-          searchPosts={this.searchPostsHandler}
-        />
-        <PostsContainer
-          thumbnail={this.state.dummyData.thumbnail}
-          posts={
-            this.state.filteredPosts.length > 0
-              ? this.state.filteredPosts
-              : this.state.posts
-          }
-        />
-        <LoadTime dummyData={this.state.dummyData} />
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			posts: [],
+			filteredPosts: []
+		};
+	}
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({ posts: dummyData });
+		}, 800);
+	}
+	searchPostsHandler = (e) => {
+		const filteredPosts = this.state.posts.filter((post) => {
+			return post.username.includes(e.target.value);
+		});
+		return this.setState({ filteredPosts });
+	};
+	render() {
+		return (
+			<div className="App">
+				<SearchBar searchTerm={this.state.searchTerm} searchPosts={this.searchPostsHandler} />
+				<PostsContainer
+					posts={this.state.filteredPosts.length > 0 ? this.state.filteredPosts : this.state.posts}
+				/>
+				<LoadTime posts={this.state.posts} />
+			</div>
+		);
+	}
 }
 
 export default PostsPage;
