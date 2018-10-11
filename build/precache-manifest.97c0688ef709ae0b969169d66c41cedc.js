da316b94139e60161346609758636b7ead91a30a
cembreyfarquhar <cembreyfarquhar@gmail.com>
2018-10-10 20:19:59 -0500

    changed file structure

---
+++ build/precache-manifest.97c0688ef709ae0b969169d66c41cedc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c6c537d0d4685dfe61cc",
+    "url": "/static/js/main.c6c537d0.chunk.js"
+  },
+  {
+    "revision": "fb16c921ebbc2c6ae74c",
+    "url": "/static/js/1.fb16c921.chunk.js"
+  },
+  {
+    "revision": "c6c537d0d4685dfe61cc",
+    "url": "/static/css/main.f642b053.chunk.css"
+  },
+  {
+    "revision": "d69704d787941e503ade6565586004ea",
+    "url": "/index.html"
+  }
+];
