2b30f11a633eee4ee4b5982692e93bf5068191cc
Gannon <gkleblanc@gmail.com>
2018-10-10 21:07:44 -0400

    added the styling tot he posts.js and starting on post header next

--- instagram/src/components/PostContainer/PostHeader.js
+++ instagram/src/components/PostContainer/PostHeader.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./PostContainerStyle.css";
+import styled, { css } from "styled-components";
 
 const PostHeader = props => {
   return (
--- instagram/src/components/PostContainer/Posts.js
+++ instagram/src/components/PostContainer/Posts.js
@@ -3,8 +3,19 @@ import PropTypes from "prop-types";
 import PostHeader from "./PostHeader";
 import CommentSection from "../CommentSection/CommentSection";
 import LikeSection from "./LikeSection";
+import styled from "styled-components";
 
 import "./PostContainerStyle.css";
+
+const PostBorder = styled.div`
+  border: 1px solid black;
+  margin: 18px 0;
+  box-shadow: 0px 5px 15px;
+`;
+
+const PostImage = styled.img`
+  width: 100%;
+`;
 
 class Post extends React.Component {
   constructor(props) {
@@ -14,42 +25,30 @@ class Post extends React.Component {
     };
   }
 
-  uplike = () => {
+  incrementLike = () => {
     let likes = this.state.likes + 1;
     this.setState({ likes });
   };
 
   render() {
     return (
-      <div className="postBorder">
+      <PostBorder>
         <PostHeader
           username={this.props.post.username}
           thumbnailUrl={this.props.post.thumbnailUrl}
         />
-
-        <div className="postImageContainer">
-          <img
-            className="postImage"
-            alt="TheThumbnail"
-            src={this.props.post.imageUrl}
-          />
-        </div>
-        <CommentSection
-          comments={this.props.post.comments}
-          postId={this.props.post.imageUrl}
+        <PostImage alt="post thumbnail" src={this.props.post.imageUrl} />
+        <LikeSection
+          incrementLike={this.incrementLike}
+          likes={this.state.likes}
         />
-        <LikeSection uplike={this.uplike} likes={this.state.likes} />
-      </div>
+        <CommentSection
+          postId={this.props.post.imageUrl}
+          comments={this.props.post.comments}
+        />
+      </PostBorder>
     );
   }
 }
-
-Post.propTypes = {
-  post: PropTypes.shape({
-    username: PropTypes.string,
-    thumbnailUrl: PropTypes.string,
-    imageUrl: PropTypes.string
-  })
-};
 
 export default Post;
