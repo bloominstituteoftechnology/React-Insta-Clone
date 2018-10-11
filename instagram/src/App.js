ebb7aa0c25787d46160cea17beb55a5daf876144
Unknown <zachary.arney@gmail.com>
2018-10-10 23:04:59 -0500

    Moved SearchBar over to PostsPage

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,29 +2,13 @@ import React, { Component } from 'react';
 // import dummyData from './dummy-data.js';
 
 import './App.css';
-import SearchBar from './components/SearchBar/SearchBar';
 import PostsPage from './components/PostContainer/PostsPage';
 import Authenticate from './components/Authentication/Authenticate';
 
 class App extends Component {
-  state = {
-    searchQuery: '',
-  };
-
-  changeHandler = (event) => {
-    this.setState({ searchQuery: event.target.value });
-  };
-
-  search = () => {};
-
   render() {
     return (
       <div className="App">
-        <SearchBar
-          search={this.search}
-          changeHandler={this.changeHandler}
-          text={this.state.searchQuery}
-        />
         <PostsPage />
       </div>
     );
