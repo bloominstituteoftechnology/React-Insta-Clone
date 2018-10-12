41616f62655d8c1bd3eb7efdb281580e8ab6de36
TheGameFreak720 <luis.alvarez@commithub.com>
2018-10-11 23:37:59 -0400

    Login looks nicer

--- insta/src/components/Login/Login.js
+++ insta/src/components/Login/Login.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const LoginContainer = styled.section `
+    width: 50%;
+    margin: 0 auto;
+`;
+
+const Input = styled.input `
+    width: 100%;
+    margin: 20px 0;
+`;
+
+const Button = styled.button `
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    width: 100%;
+`;
 
 class Login extends Component {
     render() {
         return (
-            <>
+            <LoginContainer>
+                <h1>Login to have access</h1>
                 <form onSubmit={this.props.login}>
-                    <input type='text' placeholder='Username' name='username' />
-                    <input type='text' placeholder='Password' name='password'/>
-                    <button>Login</button>
+                    <Input type='text' placeholder='Username' name='username' />
+                    <Input type='text' placeholder='Password' name='password'/>
+                    <Button>Login</Button>
                 </form>
-            </>
+            </LoginContainer>
         )
     }
 }
