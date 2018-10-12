583ee7ac9e895ef8e8e3bfea4bb59a9fc387738d
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-10-11 22:23:09 -0400

    made a styled file in the search component

---
+++ instagram/src/components/SearchBar/styled.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const SearchContainer = styled.div `
+  display: flex;
+  height: 50px;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+`
