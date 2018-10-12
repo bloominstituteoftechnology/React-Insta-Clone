cf700de46aba7c80d3c9eb574ef30153c2be16d1
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-10-11 22:45:45 -0400

    swtiched the styling for the search component into styled components

--- instagram/src/components/SearchBar/Search.js
+++ instagram/src/components/SearchBar/Search.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import './Search.css';
+import {SearchContainer, Logo, Img, LogoImg, Input, Icons} from './styled'
 
 export default (props) => {
 	return (
-		<div className='searchContainer'>
-			<div className='logo'>
-				<img src='https://image.flaticon.com/icons/svg/1077/1077042.svg' alt='instagram logo'/>
+		<SearchContainer>
+			<Logo>
+				<LogoImg src='https://image.flaticon.com/icons/svg/1077/1077042.svg' alt='instagram logo'/>
 				<p>Instagram</p>
-			</div>
-				<input onChange={props.search} type='text' placeholder= 'Search'/>
-			<div className='icons'>
-				<img src='https://image.flaticon.com/icons/svg/77/77521.svg' alt='explore'/>
-				<img src='https://image.flaticon.com/icons/svg/149/149217.svg' alt='likes'/>
-				<img src='https://image.flaticon.com/icons/svg/126/126486.svg' alt='avatar'/>
-			</div>
-		</div>
+			</Logo>
+				<Input onChange={props.search} type='text' placeholder= 'Search'/>
+			<Icons>
+				<Img src='https://image.flaticon.com/icons/svg/77/77521.svg' alt='explore'/>
+				<Img src='https://image.flaticon.com/icons/svg/149/149217.svg' alt='likes'/>
+				<Img src='https://image.flaticon.com/icons/svg/126/126486.svg' alt='avatar'/>
+			</Icons>
+		</SearchContainer>
 	)
 }
