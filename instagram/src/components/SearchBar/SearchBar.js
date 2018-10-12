a61a01b69eb52d965cabf78f9b994c72bf9a9798
DenEscobar <descobar1490@gmail.com>
2018-10-11 21:32:45 -0500

    turned the search bar into a class component

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './SearchBar.css';
 
-const SearchBar = () =>{
-    return(
-        <div className='searchBar'>
-            <div className='logo'>
-                <i className="fab fa-instagram"></i>
-                <img src='http://www.stickpng.com/assets/images/5a4e432a2da5ad73df7efe7a.png' alt='Instagram logo text'/>
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+
+        }
+    }
+
+    render(){
+        return(
+            <div className='searchBar'>
+                <div className='logo'>
+                    <i className="fab fa-instagram"></i>
+                    <img src='http://www.stickpng.com/assets/images/5a4e432a2da5ad73df7efe7a.png' alt='Instagram logo text'/>
+                </div>
+                <div className='search'>
+                    <input placeholder='&#x1F50D; Search'/>
+                </div>
+                <div className='buttons'>
+                    <i className="fas fa-compass"></i>
+                    <i className="far fa-heart"></i>
+                    <i className="far fa-user"></i>
+                </div>
             </div>
-            <div className='search'>
-                <input placeholder='&#x1F50D; Search'/>
-            </div>
-            <div className='buttons'>
-                <i className="fas fa-compass"></i>
-                <i className="far fa-heart"></i>
-                <i className="far fa-user"></i>
-            </div>
-        </div>
-    )
+        )
+    }
 }
+
 
 export default SearchBar
