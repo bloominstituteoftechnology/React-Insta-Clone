b418d825d6a98fb3e5f679811005e24e976409b7
stevekutz <stkutz@gmail.com>
2018-10-11 22:35:46 -0500

    update check again

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,62 +1,27 @@
 import React, { Component } from 'react';
-import dummyData from './components/dummy-data'
-import SearchBar from './components/Searchbar/SearchBar';
-import PostContainer from "./components/PostContainer/PostContainer";
+import PostsPage from './components/PostContainer/PostsPage';
+import Authenticate from './components/Authentication/Authenticate';
 import './App.css';
 
 
 
-export default class App extends Component {
+class App extends Component {
   constructor () {
     super();
-    this.state = {
-      dummyData: [],
-      search: ''
-    };
+    this.state = {};
   }
-
-  componentDidMount () {
-    setTimeout( () => {
-      this.setState({dummyData})
-    }, 1000)
-  }
-
-  handleSearch = event => {
-    this.setState({searchInfo: event.target.value});
-  };
-
-  searchDemPosts = event => {
-    event.preventDefault();
-    if(this.state.searchInfo) {
-      this.setState({
-        dummyData: this.state.dummyData.filter(post => {
-          if(post.username.includes(this.state.searchInfo)) return post;
-        })
-
-      })} else this.componentDidMount();
-
-  };
 
   render() {
-
-    if(!this.state.dummyData) {
-      return <div className = 'loadingMSG'> loading...</div>
-    }
-
     return (
-      <div className='App-container'>
-        <SearchBar
-          handleSearch = {this.handleSearch}
-          searchDemPosts = {this.searchDemPosts}
-          searchInfo = {this.state.searchInfo}
-        />
-
-        <PostContainer data = {this.state.dummyData}/>
+      <div className = 'App-container'>
+        <PostsPage/>
 
       </div>
 
-    );
+    )
 
   }
 }
+
+export default Authenticate(App);
 
