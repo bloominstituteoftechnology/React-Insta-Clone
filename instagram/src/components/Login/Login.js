6cd838ba15461949e1018f13b77e3d8b215955ff
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-10-11 22:01:33 -0400

    Style all login page components as styled-components

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 100px auto 0 auto;
+    background: dodgerblue;
+    height: 200px;
+    width: 50%;
+    border-radius: 5px;
+`
+
+const Input = styled.input`
+    width: 200px;
+    height: 25px;
+    margin 5px;
+    border: none;
+    border-radius: 2px;
+`
+
+const Button = styled.button`
+    margin-top: 10px;
+    width: 75px;
+    height: 25px;
+    background: springgreen;
+    border: solid 1px black;
+    border-radius: 50px;
+`
 
 const LoginPage =   (props) =>  {
     return(
-        <form onSubmit={event   =>  props.login(event)}>
-            <input placeholder="username..." onChange={props.inputChangeHandler}></input>
-            <input placeholder="password..." type="password"></input>
-            <button type="submit">Login</button>
-        </form>
+        <Form onSubmit={event   =>  props.login(event)}>
+            <Input placeholder="username..." onChange={props.inputChangeHandler}></Input>
+            <Input placeholder="password..." type="password"></Input>
+            <Button type="submit">Login</Button>
+        </Form>
     )
 }
 
