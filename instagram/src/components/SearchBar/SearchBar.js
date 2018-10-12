c8f217dd18978d4b95b16440ed0d8bc466c6a482
DenEscobar <descobar1490@gmail.com>
2018-10-11 21:51:11 -0500

    Fixed the start of the search functionality
    -I turned the SearchBar Component back into a functional component
    -I turned the PostContainter component into a class compnent and moved the inputHandler there.
    -I passed the inputHandler to the searchbar as a prop

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,32 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './SearchBar.css';
 
-class SearchBar extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-
-        }
-    }
-
-    render(){
-        return(
-            <div className='searchBar'>
-                <div className='logo'>
-                    <i className="fab fa-instagram"></i>
-                    <img src='http://www.stickpng.com/assets/images/5a4e432a2da5ad73df7efe7a.png' alt='Instagram logo text'/>
-                </div>
-                <div className='search'>
-                    <input placeholder='&#x1F50D; Search'/>
-                </div>
-                <div className='buttons'>
-                    <i className="fas fa-compass"></i>
-                    <i className="far fa-heart"></i>
-                    <i className="far fa-user"></i>
-                </div>
+const SearchBar=(props)=>{
+    return(
+        <div className='searchBar'>
+            <div className='logo'>
+                <i className="fab fa-instagram"></i>
+                <img src='http://www.stickpng.com/assets/images/5a4e432a2da5ad73df7efe7a.png' alt='Instagram logo text'/>
             </div>
-        )
-    }
+            <div className='search'>
+                <input type='text' value={props.searchValue} placeholder='&#x1F50D; Search' onChange={props.searchInputHandler}/>
+            </div>
+            <div className='buttons'>
+                <i className="fas fa-compass"></i>
+                <i className="far fa-heart"></i>
+                <i className="far fa-user"></i>
+            </div>
+        </div>
+    )
+    
 }
 
 
