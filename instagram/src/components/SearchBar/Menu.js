64e8f2c681fdb9ef1cffa8c55558d3b5abd1d33a
Rob Salzberg <robsalzberg@gmail.com>
2018-10-11 23:38:45 -0400

    started to use styled components to style the seachbar

--- instagram/src/components/SearchBar/Menu.js
+++ instagram/src/components/SearchBar/Menu.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import './searchbar.css';
+import styled from 'styled-components';
+
+const MenuStyle = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	width: 110px;
+`;
+const ImageStyleOne = styled.img`
+	height:15px
+`;
 
 const Menu = () => {
 	return (
-		<div className="menu">
-			<img
-				className="compass"
-				src={require('../assets/compass.jpg')}
-				alt={'compass'}
-			/>
+		<MenuStyle>
+			<ImageStyleOne src={require('../assets/compass.jpg')} alt={'compass'} />
+
 			<img
 				className="heart"
 				src={require('../assets/heart.jpg')}
 				alt={'heart'}
 			/>
-			<img
-				className="profile"
-				src={require('../assets/profile.jpg')}
-				alt={'profile'}
-			/>
-		</div>
+
+			<ImageStyleOne src={require('../assets/profile.jpg')} alt={'profile'} />
+		</MenuStyle>
 	);
 };
 
--- instagram/src/components/SearchBar/searchBar.js
+++ instagram/src/components/SearchBar/searchBar.js
@@ -2,6 +2,21 @@ import React from 'react';
 import Logo from './Logo';
 import Menu from './Menu';
 import './searchbar.css';
+import styled from 'styled-components'
+
+const Header= styled.header`
+display: flex;
+	align-items: center;
+	justify-content: space-between;
+	border: 1px solid black;
+	height: 50px;
+	width: 800px;
+	margin: auto;
+	padding: 0px 15px;
+	background-color: white;
+
+
+`
 
 class SearchBar extends React.Component {
 	handleChange = e => {
@@ -9,8 +24,8 @@ class SearchBar extends React.Component {
 	};
 	render() {
 		return (
-			<>
-				<div className="searchBar">
+			<Header>
+				
 					<Logo />
 					<input
 						className="search"
@@ -19,8 +34,8 @@ class SearchBar extends React.Component {
 						onChange={this.handleChange}
 					/>
 					<Menu />
-				</div>
-			</>
+				
+			</Header>
 		);
 	}
 }
