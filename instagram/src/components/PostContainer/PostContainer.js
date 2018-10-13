f03f12badbdd4f09b0ee981400246e6af38e069d
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-10-13 09:16:41 -0600

    Moved the styled components to the search bar js file

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import PropTypes from "prop-types";import styled from 'styled-components';
 import './PostContainer.css';
 
 import CommentSection from '../CommentSection/CommentSection';
