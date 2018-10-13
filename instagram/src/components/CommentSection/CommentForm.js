f004680e6bc1ac51bbb9d5b5663876fd04361332
Robin U Familara <robin.familara@cgius.com>
2018-10-13 09:00:25 -0500

    Finished styling -Styled the Apps -Adjusted the JavaScripts -Styled the posts and comment section

---
+++ instagram/src/components/CommentSection/CommentForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CommentSection.css';
+
+const CommentForm = props => {
+  return (
+    <div className="Comment-input">
+      <input type="text" placeholder="Add a comment..." />
+      <i class="fas fa-ellipsis-h fa-sm"></i>
+    </div>
+  );
+};
+
+export default CommentForm;
