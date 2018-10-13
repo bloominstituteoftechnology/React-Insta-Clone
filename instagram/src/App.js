84073addfbee0238a816b3721b75ffd4253452d5
erscriber <erscriber@gmail.com>
2018-10-13 14:20:53 -0400

    CommentInput file completed

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import dummyData from './dummy-data';
 import SearchBar from './components/SearchBarContainer/SearchBar.js';
+import PostConatiner from './components/PostContainer/PostContainer.js'
 
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-
+			post: dummyData
 		};
 	}
 
@@ -16,7 +17,7 @@ class App extends Component {
     return (
       <div className="App">
       	<SearchBar />
-      	
+      	<PostContainer post={this.state.post} />
       </div>
     );
   }
---
+++ instagram/src/components/CommentSectionContainer/CommentInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommentInput = props => {
+	return	(
+		<div className="form">
+			<input type="text" placeholder="Add comment..." />
+		</div>
+	);
+};
+
+export default CommentInput;
