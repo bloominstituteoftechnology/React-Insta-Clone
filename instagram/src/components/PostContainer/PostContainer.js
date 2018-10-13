18de1a327afb9ee2297d622743e75aba02d18d2b
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-10-13 09:40:34 -0600

    Converted Postcontainer to styled components

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,8 +1,61 @@
 import React from 'react';
-import PropTypes from "prop-types";import styled from 'styled-components';
-import './PostContainer.css';
+import styled from 'styled-components';
+import PropTypes from "prop-types";
 
 import CommentSection from '../CommentSection/CommentSection';
+
+const StyledPost = styled.div`
+    margin-bottom: 50px;
+    width: 100%;
+    border: 1px solid #eeeeee;
+    border-radius: 5px;
+    color: #333333;
+`
+
+const StyledPostHeader = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 5px;
+`
+
+const StyledHeaderThumbnail = styled.img`
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    margin: 15px 10px 10px 15px;
+`
+
+const StyledHeaderUsername = styled.h2`
+    font-size: 1.5rem;
+    line-height: 2rem;
+    letter-spacing: 0.02rem;
+    font-weight: bold;
+`
+
+const StyledPostContent = styled.div`
+
+`
+
+const StyledContentImage = styled.img`
+    width: 100%;
+`
+
+const StyledSocialButtons = styled.p`
+    margin: 10px 15px;
+`
+
+const StyledSocialButton = styled.a`
+    font-size: 2.4rem;
+    margin-right: 15px;
+`
+
+const StyledLikesCount = styled.p`
+    margin-left: 15px;
+    font-size: 1.4rem;
+    letter-spacing: 0.02rem;
+    font-weight: bold;
+`
+
 
 class PostContainer extends React.Component {
 
@@ -13,25 +66,25 @@ class PostContainer extends React.Component {
 
     render() {
         return (
-            <div className="post">
-                <div className="post-header">
-                    <img className="header-thumbnail" src={this.props.post.thumbnailUrl} alt="Thumbnail" />
-                    <h2 className="header-username">{this.props.post.username}</h2>
-                </div>
-                <div className="post-content">
-                    <img className="content-image" src={this.props.post.imageUrl} alt="Post" />
-                    <p className="social-buttons">
-                        <a onClick={this.handleLike}><i className="far fa-heart"></i></a>
-                        <a><i className="far fa-comment fa-flip-horizontal"></i></a>
-                    </p>
-                    <p className="likes-count">{this.props.post.likes} likes</p>
-                </div>
+            <StyledPost>
+                <StyledPostHeader>
+                    <StyledHeaderThumbnail src={this.props.post.thumbnailUrl} alt="Thumbnail" />
+                    <StyledHeaderUsername>{this.props.post.username}</StyledHeaderUsername>
+                </StyledPostHeader>
+                <StyledPostContent>
+                    <StyledContentImage src={this.props.post.imageUrl} alt="Post" />
+                    <StyledSocialButtons>
+                        <StyledSocialButton onClick={this.handleLike}><i className="far fa-heart"></i></StyledSocialButton>
+                        <StyledSocialButton><i className="far fa-comment fa-flip-horizontal"></i></StyledSocialButton>
+                    </StyledSocialButtons>
+                    <StyledLikesCount>{this.props.post.likes} likes</StyledLikesCount>
+                </StyledPostContent>
                 <CommentSection 
                     postKey={this.props.postKey} 
                     timestamp={this.props.post.timestamp} 
                     addNewComment={this.props.addNewComment} 
                     comments={this.props.post.comments} />
-            </div>
+            </StyledPost>
         )
     }
 }
