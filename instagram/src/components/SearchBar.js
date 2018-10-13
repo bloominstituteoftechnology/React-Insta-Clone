7cbff60f0a2a834abfcdfac2ca0e0eae39bc44d3
FBT <flaming.bunny.tomb@gmail.com>
2018-10-13 12:07:39 -0700

    Search Bar images added. Basic structure of Search bar component complete. Search Bar CSS  still needed.

--- instagram/src/components/SearchBar.js
+++ instagram/src/components/SearchBar.js
@@ -1,7 +1,28 @@
 import React from 'react';
-const SearchBar = props => {
+import Logo from './SearchBarAssets/Instagram-Logo.png';
+import Brand from './SearchBarAssets/instagram-logo-text-black-png.png';
+import Compass from './SearchBarAssets/compass.png'
+import User from './SearchBarAssets/User.png'
+import Heart from './SearchBarAssets/heart.png'
+
+
+
+const SearchBar = () => {
     return(
-        'Test Search Bar'
+        <div ClassName = 'SearchBarContainer'>
+            <div className = 'branding'>
+                <img src={Logo}></img>
+                <img src={Brand}></img>
+            </div>
+            <div className = 'SearchBar'>
+                <input type="text" placeholder="Search"/>
+            </div>
+            <div className = 'ProfileSection'>
+                <img src={Compass}></img>
+                <img src={User}></img>
+                <img src={Heart}></img>
+            </div>
+		</div>
     )
 }
 
