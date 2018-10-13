307e8eda94d4f3c13925d8477ef436ddc1125527
erscriber <erscriber@gmail.com>
2018-10-13 14:01:43 -0400

    SearchBarConatiner complete

--- instagram/src/components/SearchBarContainer/SearchBar.js
+++ instagram/src/components/SearchBarContainer/SearchBar.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './SearchBar.css';
+import Logo from './Instagram-Logo.png';
+import Brand from './instagram-logo-text-black-png.png';
+import Compass from './compass.png'
+import User from './User.png'
+import Heart from './heart.png'
 
 const SearchBar = () => {
 	return (
-		<div>
-			<input type="text" placeholder="Search"/>
+		<div className="search-container">
+			<div className="logo-container">
+				<img src={Logo} className="logo" />
+				<img src={Brand} className="brand"/>
+			</div>
+			<div className="input-container">
+				<input type="text" placeholder="	      &#128269; Search"/>
+			</div>
+			<div className="social-container">
+				<div>
+					<img src={Compass} className="social"/>
+				</div>
+				<div>
+					<img src={Heart} className="social"/>
+				</div>
+				<div>
+					<img src={User} className="social"/>
+				</div>
+			</div>
+			
 		</div>
 	)
 };
