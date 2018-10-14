e49bcc11c746892461af4f91f988fc226ff28baf
Courtney Buratto <you@example.com>
2018-10-14 11:44:17 -0700

    MVP DAY 2 (used componentDidMount to set state)

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -8,9 +8,15 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      posts: dummyData
+      posts: []
     };
   }
+
+  componentDidMount() {
+    this.setState({posts: dummyData});
+  }
+
+  //But the key is that such configuration should be done at the highest level component of your app (the root component). That means 99% of your components should probably not use componentWillMount.
 
   render() {
     return (
