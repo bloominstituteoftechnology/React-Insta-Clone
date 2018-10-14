1f650be39925a91ea507baf3c707c995592f07a7
Andres Rivera <andres.rivera.toro@gmail.com>
2018-10-13 20:46:43 -0400

    completed styling for SearchBar components

--- instagram/src/components/SearchBar/SearchBarInput.js
+++ instagram/src/components/SearchBar/SearchBarInput.js
@@ -5,24 +5,21 @@ import PropTypes from 'prop-types';
 const SearchBarInputContainer = styled.div`
   align-items: center;
   display: flex;
-`;
 
-const SearchIcon = styled.span`
- font-size: 16px;
- `;
+  i {
+    font-size: 16px;
+  }
 
-const SearchInput = styled.input`
-  text-align: center;
+  input {
+    text-align: center;
+  }
 `;
-  
 
 const SearchBarInput = ({ onChange }) => {
   return (
     <SearchBarInputContainer>
-      <SearchIcon>
-        <i className="fas fa-search" />
-      </SearchIcon>
-      <SearchInput
+      <i className="fas fa-search" />
+      <input
         type="search"
         name="search"
         placeholder="Search"
