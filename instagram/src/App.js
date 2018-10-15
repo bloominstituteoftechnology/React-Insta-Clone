f7e87f8ef9a588826fe98fdf738b37b4fa4312be
mikaelacurrier <mikaela.currier@gmail.com>
2018-10-15 15:13:15 -0400

    added instagram package

---
+++ instagram/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import animals from './animals.js'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      animals: animals
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
