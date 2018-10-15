6f354af52509cf74f91adc51424c11cc1d6127f8
Victor G <victorgordian103@gmail.com>
2018-10-15 16:17:25 -0700

    Search Bar completed and functional

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     super();
     this.state = {
       comments:[],
-      search: '',
+      search: '🔍 Search',
 
     }
   }
@@ -24,7 +24,7 @@ class App extends Component {
   render() {
     return(
       <div className="App">
-      <SearchBar />
+      <SearchBar value={this.state.search} inputHandler={this.inputHandler}/>
 
       </div>
     );
---
+++ instagram/src/Components/PostContainer.js
@@ -0,0 +1,8 @@
+
+
+const SearchBar = (props) => {
+    <div>
+        <img src='' alt=''></img>
+        <h2>App</h2>
+    </div>
+}
