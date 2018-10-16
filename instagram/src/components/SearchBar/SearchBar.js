e5e1d8476d9dc8bfa994178608ca608ab22960be
shannonhealy <sahealy525@gmail.com>
2018-10-15 22:50:39 -0400

    refactored SearchBar to use styled components

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,22 +1,104 @@
 import React from "react";
 import "./SearchBar.css"
+import styled from "styled-components"
+
+const Bar = styled.div`
+    width: 100%;
+    height: 75px;
+    border: 1px solid rgb(230,230,230);
+    background-color: rgb(255,255,255);
+    display: flex;
+    justify-content: center;
+`;
+
+const BarWithinContainer = styled.div`
+    width: 70%;
+    height: 75px;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const LogoContainer = styled.div`
+    width: 18%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const CameraImage = styled.img.attrs({
+    src: "./img/camera_logo.png"
+})`
+    flex: initial;
+    padding-right: 15px;
+`;
+
+const DividingBar = styled.img.attrs({
+    src: "./img/divider.png"
+})`
+    flex: initial;
+`;
+
+const InstagramTextLogo = styled.img.attrs({
+    src: "./img/text_logo.png"
+})`
+    flex: initial;
+    padding-top: 8px;
+    padding-left: 15px;
+`;
+
+const SearchBarContainer = styled.div`
+    width: 23%;
+`;
+
+const MenuNavigation = styled.nav`
+    width: 14%;
+    height: 27px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const NavigationCompass = styled.img.attrs({
+    src: "./img/compass_icon.png"
+})`
+    width: 25px;
+    height: 25px;
+`;
+
+const NavigationHeart = styled.img.attrs({
+    src: "./img/heart_icon.png"
+})`
+    width: 25px;
+    height: 22px;
+`;
+
+const NavigationPerson = styled.img.attrs({
+    src: "./img/person_icon.png"
+})`
+    width: 25px;
+    height: 23px;
+`;
 
 const SearchBar = props => (
-    <div className="bar"> {/*Container which contains the entire search bar content*/}
-        <div className="bar-within-container">
-            <div className="logo-container"> {/*Contains all the elements that make up the logo*/}
-                <img className="camera-image" src="./img/camera_logo.png"></img> {/*Camera picture part of logo*/}
-                <img className="dividing-bar" src="./img/divider.png"></img> {/*Dividing bar between logo elements*/}
-                <img className="instagram-text-logo" src="./img/text_logo.png"></img> {/*Text logo of Instagram*/}
-            </div> {/*End logo container*/}
-            <div className="search-bar-container"><input onChange={props.handleSearchInput} type="text" className="search-bar montserrat" placeholder="Search"></input></div> {/*Search bar*/}
-            <nav className="menu-navigation"> {/*Contains navigation icon buttons*/}
-                <img className="navigation-compass" src="./img/compass_icon.png"></img> {/*Compass menu icon*/}
-                <img className="navigation-heart" src="./img/heart_icon.png"></img> {/*Heart menu icon*/}
-                <img className="navigation-person" src="./img/person_icon.png"></img> {/*Person menu icon*/}
-            </nav> {/*End menu navigation*/}
-        </div>
-    </div> 
+    <Bar> {/*Container which contains the entire search bar content*/}
+        <BarWithinContainer>
+            <LogoContainer> {/*Contains all the elements that make up the logo*/}
+                <CameraImage /> {/*Camera picture part of logo*/}
+                <DividingBar /> {/*Dividing bar between logo elements*/}
+                <InstagramTextLogo /> {/*Text logo of Instagram*/}
+            </LogoContainer> {/*End logo container*/}
+            <SearchBarContainer><input onChange={props.handleSearchInput} type="text" className="search-bar montserrat" placeholder="Search"></input></SearchBarContainer> {/*Search bar*/}
+            <MenuNavigation> {/*Contains navigation icon buttons*/}
+                <NavigationCompass /> {/*Compass menu icon*/}
+                <NavigationHeart /> {/*Heart menu icon*/}
+                <NavigationPerson /> {/*Person menu icon*/}
+            </MenuNavigation> {/*End menu navigation*/}
+        </BarWithinContainer>
+    </Bar> 
 );
 
 {/*SEARCH BAR HAS NO PROPS TO VALIDATE, NO PROP TYPE CHECK*/}
