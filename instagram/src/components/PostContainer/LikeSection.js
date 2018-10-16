71d8824490f9ff24ee83a0c3320b1f5d12078947
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-15 23:27:52 -0400

    [added] likes images

--- instagram/src/components/PostContainer/LikeSection.js
+++ instagram/src/components/PostContainer/LikeSection.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import heart from './img/heart.png';
+import bubble from './img/speech-bubble.png';
 
 
 const LikeSection = props => {
     return(
         <div className="likes-container">
+            <div className="img-like-box">
+            <img className="like-img" src={heart} alt=""/>
+            <img className="like-img" src={bubble} alt=""/>
+            </div>
+            
             <p className="likes">{props.likes} likes</p>
         </div>
     );
