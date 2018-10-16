b2405088e7a36d5e2b9e243f7f83c788958d04a4
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-16 00:32:09 -0400

    [completed] finished styling

--- instagram/src/components/PostContainer/TimeStamp.js
+++ instagram/src/components/PostContainer/TimeStamp.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// import Moment from 'react-moment';
 
 
 const TimeStamp = props => {
+
     return(
         <div className="timestamp-container">
-            <p className="timestamp">{props.timestamp}</p>
+            <p className="timestamp">
+            {props.timestamp} 
+            {/* <Moment fromNow>{props.timestamp}</Moment> */}
+            </p>
         </div>
     );
 }
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import logo from './imgs/logo.png';
 import logotxt from './imgs/logo-txt.png';
+import heart from './imgs/heart.png';
+import compass from './imgs/compass.png';
+import user from './imgs/user.png';
 
 const SearchBar = () => {
    return(
     <div className="search-bar-container">
         <div className="logo">
             <img className="logoBox"src={logo} alt=""/>
-            <p className="logoPipe">|</p>
+            <span className="logoPipe">|</span>
             <img className="logoTxt" src={logotxt} alt=""/>
         </div>
-        <div>
-            <input type="text"/>
+        <div className="search-container">
+            <input className="search-input" type="text" placeholder="Search"/>
         </div>
-        <div>
-            <p>text</p>
+        <div className="search-bar-icons">
+            <img className="search-icons" src={compass} alt=""/>
+            <img className="search-icons" src={heart} alt=""/>
+            <img className="search-icons" src={user }alt=""/>
         </div>
     </div>
    );
