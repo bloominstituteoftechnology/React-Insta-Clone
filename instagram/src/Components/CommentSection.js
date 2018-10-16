646dbc9bfc013354beab75f73194936478f9b8b3
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-10-16 16:38:30 -0400

    commentsection refactor

--- instagram/src/Components/CommentSection.js
+++ instagram/src/Components/CommentSection.js
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ThreeDots from './Img/three dot.png';
 
-const CommentSection = props => {
-  console.log(props);
-  return (
-    <div className='comment'>
-      {props.comments.map((comment, index) => {
-        return (
-          <div key={index}>
-            <h5>{comment.username}</h5>
-            <p>{comment.text}</p>
-          </div>
-        )
-      })}
-      <div>
-        <h5>Add a comment...</h5>
-        <img src={ThreeDots} alt='three-dots'/>
-      </div>
-    </div>
+class CommentSection extends Component {
+  constructor() {
+    super();
+    this.state = {
+      comments: [this.props.comments],
+      user: "",
+      text: ""
+    }
+  }
 
-  )
+
+  addNewComment = (event, index) => {
+    event.preventDefault();
+    const comments = this.state.comments.slice();
+    comments.push({username: 'VaderSteve', text: this.state.text});
+    this.setState({comments: comments, text: ''});
+  };
+  changeComment = event => {
+    event.preventDefault();
+    this.setState({text: event.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.comments.map((comment, index) => {
+          return <Comment comment={comment} key={index} />;
+        })}
+        <Input
+          add={this.addNewComment}
+          change={this.changeComment}
+          value={this.state.text}
+        />
+      </div>
+    )
+  }
 }
+
+
 
 CommentSection.propTypes = {
   comments: PropTypes.array.isRequired
