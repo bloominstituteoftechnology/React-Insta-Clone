4cc97b7f08bb47a98f3e4d42bcd477e163afd2ed
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-10-15 23:02:02 -0500

    built search bar, added ractstrap and feather

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import './SearchBar.css'
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import * as Icon from 'react-feather'
 
 const SearchBar = props => {
   return(
     <div className="searchBar">
-      <h1>
-        Search Bar goes here
-      </h1>
+      <div className="searchBarLeft">
+        <Icon.Instagram className="instaIcon" />
+        <img className="instaCursive"
+        src="https://fontmeme.com/images/instagram-new-logo.png" alt="Instagram cursive logo"/>
+      </div>
+      <Form>
+        <FormGroup>
+          <Input type="search" name="search" id="exampleSearch" placeholder="🔍 Search" />
+        </FormGroup>
+      </Form>
+      <div className="searchBarRight">
+        <Icon.Compass className="navIcons" /> 
+        <Icon.Heart className="navIcons" />
+        <Icon.User className="navIcons" />
+      </div>  
     </div>
   )
 }
