927bf1186073ed59a656e6617a651d3ac58673a4
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-10-16 16:58:57 -0500

    imported "add a comment" box to comment section

--- instagram/src/components/PostContainer/CommentSection.js
+++ instagram/src/components/PostContainer/CommentSection.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './CommentSection.css'
 import * as Icon from 'react-feather'
+import {  InputGroup, InputGroupAddon, Input } from 'reactstrap';
 
 const CommentSection = props => {
   return(
@@ -12,14 +13,20 @@ const CommentSection = props => {
         </div>  
         <p>{props.post.likes} likes</p>
       </div>
-      {props.post.comments.map( (comment, i) => {
-         return (
-            <div className="comment">
-               <h3>{comment.username}</h3>
-               <p>{comment.text}</p>
-            </div>
-            )
-        })}
+        <div>
+          {props.post.comments.map( (comment, i) => {
+             return (
+                <div className="comment">
+                   <h3>{comment.username}</h3>
+                   <p>{comment.text}</p>
+                </div>
+                )
+            })}
+        </div> 
+        <InputGroup>
+          <Input placeholder="Add a comment..." />
+          <InputGroupAddon addonType="append">...</InputGroupAddon>
+      </InputGroup>   
     </div>
   )
 }
