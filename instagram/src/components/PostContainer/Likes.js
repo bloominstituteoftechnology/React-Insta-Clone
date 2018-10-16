3e90af5416ea76acc41e83cd8028a41f4a0502e6
Courtney Buratto <you@example.com>
2018-10-16 12:10:43 -0700

    MVP (font-awesome up and running)

--- instagram/src/components/PostContainer/Likes.js
+++ instagram/src/components/PostContainer/Likes.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Likes = props => {
     return (
         <div className = "likes-wrapper">
-            <div>{props.likes}</div>
+            <div className="social-wrapper">
+                <div className="social">
+                    <i className="fa fa-heart" />
+                </div>
+                <div className="social">
+                    <i className="fa fa-comment" />
+                </div>
+                <div className="social">
+                    <i className="fa fa-paper-plane" />
+                </div>
+            </div>
+            <h2 className = "likes">{props.likes} likes</h2>
         </div>
     )
 
