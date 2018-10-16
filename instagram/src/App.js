fa2b15d0a6f343d97f87a67e5755f7dfd1852667
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-10-16 16:55:21 -0400

    refactored App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,22 +2,36 @@ import React, { Component } from 'react';
 import SearchBar from '../src/Components/SearchBar';
 import dummyData from './dummy-data';
 import PostContainer from './Components/PostContainer';
-import Post from './Components/Post';
+import Post from './Components/SearchBar';
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      dummyData: []
+      posts: [],
+      likes: 370
     };
   }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({posts: dummyData});
+    }, 450);
+  }
+  increment = prevState => {
+    this.setState(prevState => ({likes: prevState.likes + 1}));
+  }
+  handleInput = event => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
 
   render() {
     return (
       <div className='app-wrap'>
         <SearchBar />
-        <Post />
+        <PostContainer likes={this.state.likes} increment={this.increment} />
       </div>
     );
   }
