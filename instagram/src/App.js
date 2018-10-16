e18ec1485bedfa48590df533f2e0d9adb8a7bb4e
jaklundun <govi.escudero@gmail.com>
2018-10-16 13:35:41 -0700

    Built out scaffolding for SearchBar component.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar';
 
 class App extends Component {
 
@@ -16,6 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <SearchBar instaData = {this.state.instaData}/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ instagram/src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SearchBar.css';
+
+const SearchBar = props => {
+    return (
+        <div className= "searchBar_wrapper">
+            <div className= "instaLogo_container">-Insta Logos-</div>
+            <div className= "searchBar_container">-SEARCH BAR-</div>
+            <div className= "instaHeaderIcons_container">-Insta Icons * :-> $-</div>
+        </div>
+    )
+} 
+
+export default SearchBar;
