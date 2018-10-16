1c787ac620123dbad60dc44d44d5a09c878ecf3d
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-15 23:43:22 -0400

    [added] ig logos

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import logo from './imgs/logo.png';
+import logotxt from './imgs/logo-txt.png';
 
 const SearchBar = () => {
    return(
     <div className="search-bar-container">
         <div className="logo">
-            logo
+            <img className="logoBox"src={logo} alt=""/>
+            <p className="logoPipe">|</p>
+            <img className="logoTxt" src={logotxt} alt=""/>
         </div>
         <div>
             <input type="text"/>
