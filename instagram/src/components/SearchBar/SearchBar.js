c7fe8a48dc09b7bc7935624767f0ef2ca5f296d1
Wanda <doubleuevans@gmail.com>
2018-10-17 17:58:55 -0500

    styled search bar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -8,28 +8,28 @@ import person from '../Icons/person.png'
 const SearchBar = props => {
   return (
 
-  <div className='fullSearchBar'>
+  <div className='search'>
     <div className='leftside'>
       <img src={instacamera} alt="instacamera" className="instacamera" />
       <div className="line">|</div>
       <div className="instatext">Instagram</div>    
     </div>
 
-    <div className='searchcenter'>
-      <form>
-        <input 
-          type="text" 
-          name="searchbar"
-          placeholder="search"
-        />
-      </form>
-    </div>
+    <form >
+      <input 
+        type="text" 
+        name="searchbar"
+        placeholder="Search"
+        className="searchinput"
+      />
+    </form>
 
     <div className="rightside">
-      <img src={compass} alt="compass" />
-      <img src={heart} alt="heart" />
-      <img src={person} alt="person" />
+      <img src={compass} alt="compass" className="compass"/>
+      <img src={heart} alt="heart" className="heart"/>
+      <img src={person} alt="person" className="person"/>
     </div>
+    
   </div>
 
   )
