dea0b23de114052a4a3278fa2ee1d71f1e7f9fa6
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-10-17 12:07:09 -0500

    App state now set up by componentDidMount

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 import dummyData from './dummy-data';
 import SearchBar from './components/SearchBar/SearchBar'
 import PostContainer from './components/PostContainer/PostContainer'
@@ -12,9 +10,14 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      dummyData: dummyData
+      dummyData: []
     }
   }
+
+componentDidMount() {
+  this.setState({ dummyData: dummyData })
+}
+
   render() {
     return (
       <div className="App">
