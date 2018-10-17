c17d3a00962251587ee28548ca917f07a17d4664
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-17 19:09:54 -0400

    [completed] likes component with incrimental counter

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
     this.setState({data: dummyData});
   }
 
+
   render() {
     return (
       <div className="App">
--- instagram/src/components/Likes/likes.js
+++ instagram/src/components/Likes/likes.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import heart from './img/heart.png';
 import bubble from './img/speech-bubble.png';
 
 
-const LikeSection = props => {
+const Likes = props => {
     return(
         <div className="likes-container">
             <div className="img-like-box">
-            <img className="like-img" src={heart} alt=""/>
+            <img className="like-img" onClick={props.addLike}src={heart} alt=""/>
             <img className="like-img" src={bubble} alt=""/>
             </div>
-            
             <p className="likes">{props.likes} likes</p>
         </div>
     );
 }
 
-export default LikeSection;
 
-
-LikeSection.propTypes = {
-
-    likes: PropTypes.number
-}
+export default Likes;
