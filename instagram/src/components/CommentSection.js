2eb7742eaeb8f90226a2304e7a5ef94294f4a373
jaklundun <govi.escudero@gmail.com>
2018-10-17 16:04:19 -0700

    Styling updates to CommentSection - WIP.

--- instagram/src/components/CommentSection.js
+++ instagram/src/components/CommentSection.js
@@ -3,14 +3,12 @@ import './CommentSection.css';
 
  const CommentSection = (props) => {
     return (
-        <div className= "comments-section-wrapper">
+        <div className= "comments-section-wrapper-main">
             {props.comments.map(comment => {
                 return (
-                    <div>
-                        <h4>{comment.username}</h4>
-                        <span>
-                        <p>{comment.text}</p>
-                        </span>
+                    <div className= "comments-section-wrapper">
+                        <p><b>{comment.username}</b></p>
+                        <p className= "comment-text">{comment.text}</p>
                     </div>
                 )
             })}
