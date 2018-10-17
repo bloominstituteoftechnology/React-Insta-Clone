b161e0e692a2fad347722ee6eed553a374100508
Courtney Buratto <you@example.com>
2018-10-16 21:55:27 -0700

    MVP DAY 2 (started building out Login.js)

---
+++ instagram/src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = props => {
+    return (
+        <form>
+            <input placeholder = 'username' />
+            <input placeholder = 'password' />
+            <button>Login</button>
+        </form>
+        
+    )
+}
+
+export default Login
