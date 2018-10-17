0ffcbf9ef47dc48df0f88529799ce4c4c6be1a7c
jaklundun <govi.escudero@gmail.com>
2018-10-17 12:47:19 -0700

    For PostContainer was able to get user post imgs to appear on the DOM dynamically. Todo: Type Check Components.

--- instagram/src/components/PostContainer.js
+++ instagram/src/components/PostContainer.js
@@ -6,8 +6,8 @@ const PostContainer = (props) => {
         <div>
             {props.instaData.map(user => {
                 return (
-                    <div className= "postcontainer-wrapper">
-                        <div className= "postContainer-header" key= {user.username}>
+                    <div className= "postcontainer-wrapper" key= {user.username}>
+                        <div className= "postContainer-header">
                             <img className= "postContainer-header-img" src={user.thumbnailUrl} alt= "userProfileImg" width="50px"></img>
                             <h4 className="postContainer-header-user">{user.username}</h4>
                         </div>
@@ -15,13 +15,20 @@ const PostContainer = (props) => {
                 )
             })}
 
-            <div className= "post-content-wrapper">
-                <div className= "post-content-body">
-                    CONTENT BODY
-                </div>
-                <div className= "post-content-footer">
-                    CONTENT FOOTER
-                </div>
+            <div>
+                {props.instaData.map(post => {
+                    return (
+                        <div className= "post-content-wrapper" key={post.username}>
+                            <div className= "post-content-body">
+                                <img className= "post-content-body-img" src={post.imageUrl} alt= "userPostImg" width="700px"></img>
+                            </div>
+                            <div className= "post-content-footer">
+                                CONTENT FOOTER
+                            </div>
+                        </div>
+                    )
+                })}
+
             </div>
         </div>
     )
