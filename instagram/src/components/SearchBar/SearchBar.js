63ca1107617df36fd910b0444d19586e88554502
Paul DeYonghe <p.deyonghe@gmail.com>
2018-10-16 21:57:17 -0500

    refactored FontAwesome imports

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const SearchBar = (props) => {
         return(
                 <div>
-                <FontAwesomeIcon icon='ghost' />
+                <FontAwesomeIcon icon='fa-instagram' />
                 <input></input>
                 </div>
         )
