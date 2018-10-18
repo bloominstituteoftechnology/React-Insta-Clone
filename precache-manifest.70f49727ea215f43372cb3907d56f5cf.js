537d4b1b91bc343bde77eeb8a79b8d3868a8b247
Jesse <theiseja@gmail.com>
2018-10-18 13:28:55 -0500

    Updates

---
+++ precache-manifest.70f49727ea215f43372cb3907d56f5cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff854302b1aeaf008c572d7d015a0317",
+    "url": "/React-Insta-Clone/static/media/iglogo.ff854302.png"
+  },
+  {
+    "revision": "8bea3be130bc96ea5570",
+    "url": "/React-Insta-Clone/static/js/runtime~main.8bea3be1.js"
+  },
+  {
+    "revision": "a1709d2f14e5d4b9e9fc",
+    "url": "/React-Insta-Clone/static/js/main.a1709d2f.chunk.js"
+  },
+  {
+    "revision": "b6bc0c361aaf9a3de02c",
+    "url": "/React-Insta-Clone/static/js/1.b6bc0c36.chunk.js"
+  },
+  {
+    "revision": "a1709d2f14e5d4b9e9fc",
+    "url": "/React-Insta-Clone/static/css/main.028a4930.chunk.css"
+  },
+  {
+    "revision": "d56d9891b538ebcadb8d727e163e3ef9",
+    "url": "/React-Insta-Clone/index.html"
+  }
+];
