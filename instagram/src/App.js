a85235d0dd939d5e0ef730f5778fd696799a3c26
Courtney Buratto <you@example.com>
2018-10-17 19:45:39 -0700

    MVP DAY 3 (refactored App.js)

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,33 +1,32 @@
 import React, { Component } from "react";
 import "./App.css";
-import dummyData from "./dummy-data";
-import PostContainer from "./components/PostContainer/PostContainer";
-import SearchBar from "./components/SearchBar/SearchBar";
+import Authenticate from './Authentication/Authenticate;
+import PostsPage from "./components/PostContainer/PostsPage";
+ 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      posts: []
+      username: ''
     };
   }
 
   componentDidMount() {
-    this.setState({posts: dummyData});
+    const user = localStorage.getItem('user')
+    this.setState({username: user})
   }
-
-  //But the key is that such configuration should be done at the highest level component of your app (the root component). That means 99% of your components should probably not use componentWillMount.
+//the key is that such configuration should be done at the highest level component of your app (the root component). That means 99% of your components should probably not use componentWillMount.
 
   render() {
     return (
       <div className="App">
-        <SearchBar />
-        <PostContainer posts = {this.state.posts} />
+        <PostsPage />
       </div>
     );
   }
 }
 
-export default App;
+export default Authenticate(App);
 
 
