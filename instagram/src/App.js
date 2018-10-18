53ff47108d24b9d5121bf435a6caa1f0d33bde88
Jake Thomas <jmaxt12@gmail.com>
2018-10-18 11:00:40 -0600

    Day 2 complete.... on day 4

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -33,7 +33,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Instagram</h1>
         <SearchBar search={this.Search} />
         <PostContainer
           dummyData={
--- instagram/src/components/SearchBar/searchBar.js
+++ instagram/src/components/SearchBar/searchBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './searchBar.css';
+import instaLogo from '../../Images/insta_logo.png';
+import instaIcon from '../../Images/insta_icon.png';
 
 
 class SearchBar extends React.Component {
@@ -14,18 +16,18 @@ class SearchBar extends React.Component {
     }
 
     
-
-    //  changeHandler = event => {
-    //     console.log('event name', event.target.name);
-    //     console.log('event value', event.target.value);
-    //     this.setState({ search: event.target.value });
-    // };
     
     render () {
         return (
-            <form className = 'searchBar'>
-            <input onKeyDown={this.props.search} type="text" placeholder="Search" />
-            </form> 
+            <div>
+                <div>
+                    <img className = 'instaIcon' src = {instaIcon} />
+                    <img className = 'instaLogo' src = {instaLogo} />
+                </div>
+                <form className = 'searchBar'>
+                <input onKeyDown={this.props.search} type="text" placeholder="Search" />
+                </form>
+            </div>
 
         )
       
