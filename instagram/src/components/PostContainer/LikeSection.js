ea0784b1881d16a7ef676a5da4ff4081aca95faf
erscriber <erscriber@gmail.com>
2018-10-18 14:11:17 -0400

    Added LikeSection.js and updated Post.js

---
+++ instagram/src/components/PostContainer/LikeSection.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Heart from './heart.png';
+import Comment from './comment.png';
+
+const LikeSection = props => {
+	return [
+		<div className="like-container" key="icons" onClick={props.incrementLike}>
+			<div className="like-wrapper">
+				<img src={Heart} className="social"/>
+			</div>
+			<div className="like-wrapper">
+				<img src={Comment} className="social"/>
+			</div>
+		</div>,
+		<div className="like-container" key="likes">
+			<div className="like-wrapper">
+				{props.likes}
+			</div>
+		</div>
+	];
+};
+
+export default LikeSection;
--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -1,27 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import CommentSection from '../CommentSectionContainer/CommentSection.js';
+import LikeSection from './LikeSection.js';
 import PostHead from './PostHead.js';
-import './Post.css'
+import './Post.css';
 
-const Post = props => {
-	return (
-		<div className="post-bucket">
-			<PostHead
-				username={props.post.username}
-				thumbnailUrl={props.post.thumbnailUrl}
-			/>
-			<div className="img-container">
-				<img 
-					src={props.post.imageUrl}
-					alt="post image"
-					className="post-image"
+
+class Post extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			like: props.post.likes
+		};
+	}
+
+	incrementLike = () => {
+		let likes = this.state.likes + 1;
+		this.setState({ likes });
+	}
+
+	render() {
+		return (
+			<div className="post-bucket">
+				<PostHead
+					username={props.post.username}
+					thumbnailUrl={props.post.thumbnailUrl}
+				/>
+				<div className="img-container">
+					<img 
+						src={props.post.imageUrl}
+						alt="post image"
+						className="post-image"
+					/>
+				</div>
+				<LikeSection
+					incrementLike={this.incrementLike}
+					likes={this.state.likes}
+				/>
+				<CommentSection 
+					comments={props.post.comments}
 				/>
 			</div>
-			<CommentSection 
-				comments={props.post.comments}
-			/>
-		</div>
-	);
+		);	
+	}
 };
 
 export default Post;
