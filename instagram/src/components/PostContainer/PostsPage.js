2cea2e2583e9976969eefa653ab64d6b893ec56c
Courtney Buratto <you@example.com>
2018-10-18 20:03:49 -0700

    FIX (refactored postsHandler, no longer throwing error)

--- instagram/src/components/PostContainer/PostsPage.js
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -1,44 +1,55 @@
-import React from 'react';
-// import "./App.css";
-// import "../src/Authentication/Authenticate";
- 
+import React from "react";
 import dummyData from "./dummy-data";
-import PostContainer from "./components/PostContainer/PostContainer";
-import SearchBar from "./components/SearchBar/SearchBar";
+import PostContainer from "./PostContainer";
+import SearchBar from "../SearchBar";
 
- 
+//main state goes in this component
 
 class PostsPage extends React.Component {
-    constructor() {
-        super()
+  constructor() {
+    super();
 
-        this.state = {
-            posts: [],
-            filteredPosts: []
-        }
-    }
+    this.state = {
+      posts: [],
+      filteredPosts: []
+    };
+  }
 
-    componentDidMount() {
-        this.setState({posts: dummyData});
-    }
+  componentDidMount() {
+    this.setState({ posts: dummyData });
+  }
 
-    postsHandler = event => {
-        const posts = this.state.posts.filter(posts => {
-            if (posts.username.includes(event.target.value)) {
-                return posts
-            }
-        })
-        this.setState({filteredPosts: posts})
-    }
+  postsHandler = event => {
+      this.setState({
+          filteredPosts: this.state.posts.filter(post => {
+            return post.username.includes(event.target.value)
+          })
+      })
+    // const posts = this.state.posts.filter(posts => {
+    //   if (posts.username.includes(event.target.value)) {
+    //     return posts;
+    //   }
+    // });
+    // this.setState({ filteredPosts: posts });
+  };
 
-    render () {
-        return (
-            <div className = "App">
-                <SearchBar />
-                <PostContainer /> 
-            </div>
-        )
-    }
-    }
-j
+  render() {
+    return (
+      <div className="App">
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          searchPosts={this.postsHandler}
+        />
+        <PostContainer
+          posts={
+            this.state.filteredPosts.length > 0
+              ? this.state.filteredPosts
+              : this.state.posts
+          }
+        />
+      </div>
+    );
+  }
+}
+
 export default PostsPage;
