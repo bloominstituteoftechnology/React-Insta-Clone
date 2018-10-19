c5c9c20a751e4fe427368fac1366ac5312b141c5
erscriber <erscriber@gmail.com>
2018-10-18 21:48:00 -0400

    authenication directory built, Authenticate.js skeleton structure complete and imported to App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import PostsPage from './components/PostContainer/PostsPage.js';
+import Authenticate from './authentication/Authenticate.js';
 
 
 class App extends React.Component {
---
+++ instagram/src/authentication/Authenticate.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Authenticate = App =>
+	class extends React.Component {
+		constructor() {
+			super();
+			this.state = {
+				loggedIn: false
+			};
+		}
+
+		render() {
+			return <App />;
+		}
+	};
