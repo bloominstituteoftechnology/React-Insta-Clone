3574bd2d443bd2228f59f186c13b0c167c476762
Julie Jonak <juliejonak10@gmail.com>
2018-10-20 13:16:04 -0500

    Added styled components to Comment.js

--- instagram/src/CommentSection/Comment.js
+++ instagram/src/CommentSection/Comment.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './Comment.css';
+import styled from 'styled-components';
+
+const CommentStyling = styled.div`
+    display: flex;
+    font-size: 14px;
+    margin-left: 3%;
+    text-align: left;
+`;
+
+const Words = styled.p`
+    line-height: 22px;
+    margin: 2px 0;
+`;
 
 const Comment = (props) => {
     return (
-        <div className="comment">
-            <p><strong>{props.username}</strong>   {props.text}</p>
-        </div>
+        <CommentStyling>
+            <Words><strong>{props.username}</strong>   {props.text}</Words>
+        </CommentStyling>
     )
 }
 
