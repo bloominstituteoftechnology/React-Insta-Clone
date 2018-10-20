56502f5a96ddb6784613615b2f289f9af93cea8b
cocoitali <cmburatto@gmail.com>
2018-10-19 21:51:07 -0700

    MVP DAY 4(refactored SearchBar.js, deleted SearchBar.css, plan to add more styles)

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,12 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
+const Header = styled.header`
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: space-evenly
+`;
+const LeftWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const InstaLogo = styled.div`
+  font-family: Grand Hotel;
+  font-size: 3rem;
+`;
 const SearchBar = () => {
   return (
-    <div className="search-bar-wrapper">
-      <div className="left-wrapper">
+    <Header>
+      <LeftWrapper>
         <i className="fas fa-camera-retro fa-2x" />
-        <div className="insta-logo"> Instagram</div>
-      </div>
+        <InstaLogo> Instagram</InstaLogo>
+      </LeftWrapper>
       <form>
         <input type="text" placeholder="&#x1f50d; &nbsp;Search" />
       </form>
@@ -14,11 +29,10 @@ const SearchBar = () => {
       <i className="far fa-compass fa-2x " />
       <i className="far fa-heart fa-2x" />
       <i className="far fa-user-circle fa-2x" />
-    </div>
+    </Header>
   );
 };
 
 export default SearchBar;
 
 //logo image needs to be placed in image-wrapper <div>
- 
