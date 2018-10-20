a56872e6b1c4a9bfe11addf96dc8762b93338829
Julie Jonak <juliejonak10@gmail.com>
2018-10-20 13:20:45 -0500

    Added styled components to PostContainer.js

--- instagram/src/PostContainer/PostContainer.js
+++ instagram/src/PostContainer/PostContainer.js
@@ -1,22 +1,54 @@
 import React from 'react';
 import './PostContainer.css';
 import CommentSection from '../CommentSection/CommentSection';
+import styled from 'styled-components';
 
 
 const PostContainer = (props) => {
         return (
-            <div className="postContainer">
-                <div className='username-header'>
-                    <img src={props.obj.thumbnailUrl} alt='user thumbnail' />
-                    <p>{props.obj.username}</p>
-                </div>
-                <img className="postImage" src={props.obj.imageUrl} alt='' />
+            <PostContainerStyle>
+                <UsernameHeader>
+                    <UsernameImage src={props.obj.thumbnailUrl} alt='user thumbnail' />
+                    <UsernameText>{props.obj.username}</UsernameText>
+                </UsernameHeader>
+                <PostImage className="postImage" src={props.obj.imageUrl} alt='' />
                 <CommentSection
                     key={props.obj.timestamp}
                     obj={props.obj}
                 />
-            </div>
+            </PostContainerStyle>
         )
 }
+
+const PostContainerStyle = styled.div`
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+`;
+
+const UsernameHeader = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-bottom: 10px;
+`;
+
+const UsernameImage = styled.img`
+    border-radius: 100px;
+    width: 30px;
+    height: 30px;
+    margin-left: 15px;
+`;
+
+const UsernameText = styled.p`
+    font-weight: bold;
+    font-size: 14px;
+    margin-left: 10px;
+`;
+
+const PostImage = styled.img`
+    width: 100%;
+`;
 
 export default PostContainer;
