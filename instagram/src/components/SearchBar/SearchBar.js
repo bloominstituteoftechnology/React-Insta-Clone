573ccf33539a62e0b222c0219260fc20694550e1
jheinz1999 <jonathan.heinz@aol.com>
2018-11-09 13:55:24 -0800

    finished SearchBar (not responsive AT ALL)

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,13 +1,41 @@
 import React from 'react';
+
 import './SearchBar.scss';
+
+import logo from './logo.png';
 
 export default function SearchBar() {
 
   return (
 
-    <div className='searchbar'>
+    <div className='search-bar'>
 
-      
+      <div>
+
+        <p className="fab fa-instagram"></p>
+        <p className='divider'>|</p>
+        <img className='logo' src={logo}></img>
+
+      </div>
+
+      <div>
+
+        <div className='input'>
+
+          <input type='text' placeholder='Search' />
+          <p className='fas fa-search'></p>
+
+        </div>
+
+      </div>
+
+      <div>
+
+        <p className='right far fa-compass'></p>
+        <p className='right far fa-heart'></p>
+        <p className='right far fa-user'></p>
+
+      </div>
 
     </div>
 
