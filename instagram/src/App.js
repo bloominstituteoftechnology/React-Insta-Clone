0256bc8e57bdbe5e615712793f125f6c67f953b9
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-10 18:02:52 -0500

    "animals data added to src files,  constructor/state added to app.js."

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      animals: []
+    }
+  }
   render() {
     return (
       <div className="App">
