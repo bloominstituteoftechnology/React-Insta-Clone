5953ba02f035df2c9da65a2f21974290a231965f
Dennis Orbison <orbison.dennis@gmail.com>
2018-11-11 14:33:37 -0800

    basic component structure define

---
+++ instagram/src/Components/CommentSection/CommentSection.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CommentSection = (props) => {
+  return (
+    <h1>hello from CommentSection</h1>
+  );
+}
+
+export default CommentSection;
---
+++ instagram/src/Components/PostContainer/PostContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CommentSection from '../CommentSection/CommentSection.js';
+
+const PostContainer = (props) => {
+  return (
+    <div>
+      <h1>hello from PostContainer</h1>
+      <CommentSection />
+    </div>
+  );
+}
+
+export default PostContainer;
