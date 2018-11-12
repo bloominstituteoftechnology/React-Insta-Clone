f98e2c743b1ca073c86e30dd506b684d4c28f042
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-11-12 11:51:10 -0500

    Removed default stylling from App.css and  default header from App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 class App extends Component {
@@ -7,18 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Insta-Clone</h1>
         </header>
       </div>
     );
