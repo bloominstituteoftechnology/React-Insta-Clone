066c866f37bb10bd7670f0fa696cd7e8bc63a4b6
David Freitag <freitagdavid@outlook.com>
2018-11-12 08:04:01 -0500

    instagram/src/App: generating searchbar and post containers

---
+++ instagram/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            posts: dummyData
+        };
+    }
+    render() {
+        return (
+            <div className="App">
+                <SearchBar />
+                {this.state.posts.map(post => (
+                    <PostContainer post={post} />
+                ))}
+            </div>
+        );
+    }
+}
+
+export default App;
