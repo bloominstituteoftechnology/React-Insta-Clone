174d1eab5457156e593730aa76a8595a3a668e6b
David Freitag <freitagdavid@outlook.com>
2018-11-12 08:28:56 -0500

    instagram/src/components/CommentSection/CommentContainer: Generating unique id

--- instagram/src/components/CommentSection/CommentContainer.js
+++ instagram/src/components/CommentSection/CommentContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Comment from './Comment';
+import shortid from 'shortid';
 
 const CommentContainer = props => {
     return (
         <>
             {props.comments.map(comment => (
-                <Comment comment={comment} />
+                <Comment comment={comment} key={shortid.generate()} />
             ))}
         </>
     );
