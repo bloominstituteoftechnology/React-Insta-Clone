65bdbdf40a8461303f5ded2f44d889b2c8e05702
David Freitag <freitagdavid@outlook.com>
2018-11-12 08:05:46 -0500

    instagram/src/components/SearchBar/SearchBar: search bar and social icons implemented

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Row, Col, Input } from 'reactstrap';
+
+const SearchBar = props => {
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col xs="4">
+                        <h1>Logo placeholder</h1>
+                    </Col>
+                    <Col xs="4">
+                        <Input type="text" name="search" id="search" placeholder="Search" />
+                    </Col>
+                    <Col xs="4">
+                        <h1>Profile icons</h1>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+};
+
+export default SearchBar;
