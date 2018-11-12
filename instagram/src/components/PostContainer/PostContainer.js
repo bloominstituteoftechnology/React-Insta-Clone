565255458fd5ca3bd19294994bcd51c6e4c34965
David Freitag <freitagdavid@outlook.com>
2018-11-12 14:22:22 -0500

    instagram/src/components/PostContainer/PostContainer: added sass

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PostBody from './PostBody';
 import PostUserInfo from './PostUserInfo';
 import PropTypes from 'prop-types';
+import './PostContainer.sass';
 
 const PostContainer = props => {
     return (
