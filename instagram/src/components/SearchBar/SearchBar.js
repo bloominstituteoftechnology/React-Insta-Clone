827baf330a8c37872fc5b0c36f8d7b8e5b44cd12
David Freitag <freitagdavid@outlook.com>
2018-11-12 09:55:04 -0500

    instagram/src/components/SearchBar/SearchBar: Code formatting

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -4,21 +4,19 @@ import './SearchBar.sass';
 
 const SearchBar = props => {
     return (
-        <>
-            <Container>
-                <Row>
-                    <Col xs="4">
-                        <h1>Logo placeholder</h1>
-                    </Col>
-                    <Col xs="4">
-                        <Input type="text" name="search" id="search" placeholder="Search" />
-                    </Col>
-                    <Col xs="4">
-                        <h1>Profile icons</h1>
-                    </Col>
-                </Row>
-            </Container>
-        </>
+        <Container>
+            <Row>
+                <Col xs="4">
+                    <h1>Logo placeholder</h1>
+                </Col>
+                <Col xs="4">
+                    <Input type="text" name="search" id="search" placeholder="Search" />
+                </Col>
+                <Col xs="4">
+                    <h1>Profile icons</h1>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
