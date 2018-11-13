95210c22fff79e75329d870e3ef88a55d23feaf8
Trai Lynne <trailynne@gmail.com>
2018-11-13 15:05:41 -0700

    Ran command 'yarn add prop-types' Set up file for searchbar placeholder

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 import dummyData from './dummy-data';
+
+import SearchBar from './components/SearchBar/SearchBar';
 
 class App extends Component {
   constructor(){
@@ -20,22 +21,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Fragment>
+          <header>
+            <SearchBar/>
+          </header>
+        </Fragment>
     );
   }
 }
