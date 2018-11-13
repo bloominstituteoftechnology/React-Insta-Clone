5a3e1517ab09ecad736023353be2e47a1257a4da
Addison-Stavlo <AddieStavlo@gmail.com>
2018-11-13 16:43:08 -0500

    add likes complete, MVP complete

--- DAY_TWO_README.md
+++ DAY_TWO_README.md
@@ -34,9 +34,9 @@
   - [X] The 'Add a comment...' input should be wrapped in a `<form></form>` element. Using that form's `onSubmit` event handler, invoke the `addNewComment` function and pass it the required arguments it needs to add a new comment.
   - [X] Update your state with the new comment (This should trigger your component tree to "re-render" with your new comment on that post).
 - Liking posts
-  - This will be set up a lot like the 'Add a comment...' input. Pass a function down to where your heart icon is, and use `onClick` event handler to increment that post's likes.
+  - [X] This will be set up a lot like the 'Add a comment...' input. Pass a function down to where your heart icon is, and use `onClick` event handler to increment that post's likes.
 - Search
-  - Set up the search bar will like the comment input and the like button. In your function, filter out any post whose username doesn't match the search term passed in, then update the state with the resulting data.
+  - [X] Set up the search bar will like the comment input and the like button. In your function, filter out any post whose username doesn't match the search term passed in, then update the state with the resulting data.
 
 #### Stretch Problems (Day II)
 
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CommentSection from '../CommentSection/CommentSection';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
@@ -34,20 +34,53 @@ const Post = styled.div`
     }
     p {
         padding: 0 20px;
+        cursor: pointer;
     }
 `
 
-const PostContainer = (props) => {
-    return (
-        <Post className={`${props.thisPost.display ? 'displayed' : 'hidden'}`} >
-            <h2><img src={props.thisPost.thumbnailUrl}></img>{props.thisPost.username}<p>{props.thisPost.timestamp}</p></h2>
-            <img src={props.thisPost.imageUrl} />
+// const PostContainer = (props) => {
+//     return (
+//         <Post className={`${props.thisPost.display ? 'displayed' : 'hidden'}`} >
+//             <h2><img src={props.thisPost.thumbnailUrl}></img>{props.thisPost.username}<p>{props.thisPost.timestamp}</p></h2>
+//             <img src={props.thisPost.imageUrl} />
             
-            <p>{props.thisPost.likes} likes</p>
-            <CommentSection thisPost={props.thisPost} postIndex={props.index} />
-        </Post>
-    );
+//             <p>{props.thisPost.likes} likes</p>
+//             <CommentSection thisPost={props.thisPost} postIndex={props.index} />
+//         </Post>
+//     );
+// }
+
+class PostContainer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            likes: 0
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            likes: this.props.thisPost.likes
+        })
+    }
+
+    addLike = () => {
+        this.setState({likes: this.state.likes+1})
+    }
+
+    render(){
+        return (
+        <Post className={`${this.props.thisPost.display ? 'displayed' : 'hidden'}`} >
+        <h2><img src={this.props.thisPost.thumbnailUrl}></img>{this.props.thisPost.username}<p>{this.props.thisPost.timestamp}</p></h2>
+        <img src={this.props.thisPost.imageUrl} />
+        
+        <p onClick={this.addLike}>{this.state.likes} likes</p>
+        <CommentSection thisPost={this.props.thisPost} postIndex={this.props.index} />
+    </Post>
+        )
+    }
 }
+
 
 PostContainer.propTypes = {
     thisPost: PropTypes.shape({
