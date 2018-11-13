78f9a4d36e3a5cf073342278b4daa8a445910a91
Trai Lynne <trailynne@gmail.com>
2018-11-13 16:11:43 -0700

    Defined Posts PropTypes

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -7,4 +7,18 @@ const Post = props => {
     )
 }
 
+Post.propTypes = {
+    post: PropTypes.shape({
+        username: PropTypes.string, 
+        thumbnail: PropTypes.string, 
+        imageUrl: PropTypes.string,
+        likes: PropTypes.number,
+        timestamp: PropTypes.string,
+        comments: PropTypes.arrayOf(PropTypes.shape({
+            username: PropTypes.string,
+            text: PropTypes.string
+        })) 
+    })
+}
+
 export default Post;
