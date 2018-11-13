60272ac8da203c0765ff94c54b4c14fa5822fc88
darenlarson <larson.daren@gmail.com>
2018-11-13 10:07:39 -0600

    Search bar styled somewhat.

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './SearchBar.css';
+import Logo from './screenshotlogo.png';
+
+import Icons from './searchbaricons.png';
 
 const SearchBar = props => {
     return (
-        <header className="search-bar">
-            <div>
-                <div>icon</div>
-                <div>Instagram</div>
+        <header className="search-bar-wrapper">
+            <div className="logo-wrapper">
+                <img className="logo" src={Logo} alt="logo" />
             </div>
-            <div>Search Bar</div>
-            <div>3 icons on right side</div>
+
+            <input className="search-input" type="text" placeholder="Search" />
+
+            <div className="icons-wrapper">
+                <img className="icons" src={Icons} alt="logos" />
+            </div>
         </header>
     )
 }
