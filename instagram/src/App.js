43267a422d385c5741c81e4d3e9f57fa3a5bb8f6
Trai Lynne <trailynne@gmail.com>
2018-11-14 16:18:44 -0700

    Imported dummyData Used cDM to set state to dummyData

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,25 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Data
+import dummyData from './dummy-data';
+
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount(){
+    this.setState({
+      posts: dummyData
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <SearchBar />
+
+      <Posts />
     );
   }
 }
