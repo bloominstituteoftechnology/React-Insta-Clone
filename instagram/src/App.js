e8d8d4ba951b0db355c8e554d3bc2b5d777e7727
wisdomtooth <papaya2002@hotmail.com>
2018-11-14 17:58:31 -0600

    add likes feature

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,37 +3,49 @@ import "./App.css";
 import dummyData from "./dummy-data";
 import PostContainer from "./components/PostContainer/PostContainer";
 import SearchBar from "./components/SearchBar/SearchBar";
-import * as Icon from 'react-feather';
+import * as Icon from "react-feather";
+import PostsPage from "./components/PostContainer/PostsPage";
 class App extends Component {
   constructor() {
     super();
     this.state = {
       search: "",
       post: [],
+      completed: false
     };
   }
+  componentDidMount() {
+    this.setState({
+      post: dummyData
+    });
+  }
   handleLikes = timestamp => {
+    // console.log(timestamp);
     let posts = this.state.post.slice();
-    posts = posts.map(post =>
-      {
-        if (post.timestamp === timestamp){
+    posts = posts.map(post => {
+      if (post.timestamp === timestamp) {
+        let completed = this.state.completed;
+        if (completed === false) {
           post.likes = post.likes + 1;
+          completed = !completed;
+          this.setState({ posts, completed: completed });
+          // console.log(completed);
+        } else {
+          post.likes = post.likes - 1;
+          completed = !completed;
+          this.setState({ posts, completed: completed });
         }
       }
-      )
-    this.setState({posts});
-  }
-  componentDidMount(){
-    this.setState({
-      post: dummyData,
-    })
-  }
+    });
+  };
   render() {
+    console.log(this.state.completed);
     return (
-      <div className="App"> 
+      <div className="App">
+        <PostsPage />
         <header className="App-header">
           <div className="App-header__logo">
-            <Icon.Instagram className="logo"/>
+            <Icon.Instagram className="logo" />
           </div>
           <div>
             <img
@@ -44,13 +56,23 @@ class App extends Component {
           </div>
 
           <SearchBar className="search-bar" />
-          <a href="#" className="link"><Icon.Compass className="link-compass"/></a>
-          <a href="#" className="link"><Icon.Heart className="link-heart"/></a>
-          <a href="#" className="link"><Icon.User className="link-user"/></a>
-          
+          <a href="#" className="link">
+            <Icon.Compass className="link-compass" />
+          </a>
+          <a href="#" className="link">
+            <Icon.Heart className="link-heart" />
+          </a>
+          <a href="#" className="link">
+            <Icon.User className="link-user" />
+          </a>
         </header>
         {this.state.post.map(post => (
-          <PostContainer key={post.timestamp} post={post} handlelikesprop={this.handleLikes}/>
+          <PostContainer
+            key={post.timestamp}
+            post={post}
+            handlelikesprop={this.handleLikes}
+            completedProp={this.state.completed}
+          />
         ))}
       </div>
     );
---
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PostsPage = props =>{
+    return(
+        <h1>test</h1>
+    );
+}
+
+export default PostsPage;
