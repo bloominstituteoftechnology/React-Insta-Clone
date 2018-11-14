d03dc3374d9c6b010b0223bd9afac2f4587b68ac
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2018-11-13 18:13:17 -0600

    IT WORKS. day 1 and day 2 mvp complete

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -9,9 +9,9 @@ const PostContainer = props => {
 
     
         return(
-        <div className='post' id={props.id} >
-            <Post data={props.data} addLike={props.addNewLike}/>
-            <CommentSection comments={props.data.comments}/>
+        <div className='post'  >
+            <Post data={props.data} addLike={props.addNewLike} id={props.id}/>
+            <CommentSection comments={props.data.comments} />
         </div>
     );
     
