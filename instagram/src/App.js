b88c5fcf81a1518dacd8c887b002a766f9366226
Trai Lynne <trailynne@gmail.com>
2018-11-14 16:32:30 -0700

    Set Up Posts props Sent data into Posts Set up Posts.propTypes

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
       <>
         <SearchBar />
 
-        <Posts />
+        <Posts posts={this.state.posts} />
       </>
     );
   }
--- instagram/src/components/PostContainer/Posts.js
+++ instagram/src/components/PostContainer/Posts.js
@@ -9,4 +9,18 @@ const Posts = props => {
     )
 }
 
+Posts.propTypes = {
+    posts: PropTypes.arrayOf(PropTypes.shape({
+        comments: PropTypes.arrayOf(PropTypes.shape({
+            text: PropTypes.string,
+            username: PropTypes.string
+        })),
+        imageUrl: PropTypes.string,
+        likees: PropTypes.number,
+        thumbnailUrl: PropTypes.string,
+        timestamp: PropTypes.string,
+        username: PropTypes.string
+    }))
+}
+
 export default Posts;
