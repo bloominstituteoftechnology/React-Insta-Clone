40bec8fa26d5be7c62fd255c0a4991d639d530cb
kmarker1101 <kmarker1101@icloud.com>
2018-11-14 17:20:07 -0600

    WIP

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,28 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+import Authenticate from './Authentication/Authenticate';
+import dummyData from './dummy-data';
+import styled from 'styled-components';
+
+const AppStyled = styled.div`
+  .App {
+    max-width: 570px;
+    margin: 0 auto;
+  }
+`
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      posts : dummyData,
+      userinput: '',
+    }
+  }
+
+  componentDidMount() {
+    this.setState ({
+      posts : dummyData,
+    })
+  }
+
+  searchHandler = (event) => {
+    event.preventDefault();
+    if ( event.keyCode === 13 && event.target.value.length > 0 ) {
+          let newData = this.state.posts.filter( (posts) => {
+            return posts.username === event.target.value;
+          })
+          this.setState({ posts: newData });
+          event.target.value = "";
+        }
+      }
+    
+  
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppStyled>
+        <div className="App">
+          <SearchBar searchHandler = {this.searchHandler} />
+            {this.state.posts.map(post => {
+              return <PostContainer post = {post} key = {post.timestamp} addLike = {this.addLike} />
+            })}
+        </div>
+      </AppStyled>
     );
   }
 }
 
-export default App;
+export default Authenticate(App);
+
+	 
---
+++ instagram/src/components/PostContainer/CommentButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import InstaCommentButt from '../../img/comment.png';
+
+
+
+const CommentButton = () => {
+    return (
+        <div>
+            <img className = "commentButt" alt="''" src= {InstaCommentButt} />
+        </div>
+    )
+}
+
+export default CommentButton;
