6c4ffcee3c39d7cf5733960866fed7b342e0323f
darenlarson <larson.daren@gmail.com>
2018-11-14 14:55:24 -0600

    Refactored App.js to PostsPage.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,48 +1,19 @@
 import React, { Component } from 'react';
 import './index.css';
-import SearchBar from './components/SearchBar/SearchBar.js';
-import dummyData from './dummy-data.js';
-import PostsContainer from './components/PostsContainer/PostsContainer.js';
 import './App.css';
+import PostsPage from './components/PostsContainer/PostsPage';
 
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      postData: [],
-      filteredPosts: []
-    }
-  }
-
-  componentDidMount() {
-    this.setState({postData: dummyData})
-  }
-
-  searchPosts = event => {
-    const searchPosts = this.state.postData.filter(post => {
-      if (post.username.includes(event.target.value)) {
-        return post;
-      }
-    });
-    this.setState({ filteredPosts: searchPosts})
+    this.state = { }
   }
 
   render() {
-
-
     return (
       <div className="App">
-        <SearchBar 
-          searchPosts={this.searchPosts}
-        />
-
-        <PostsContainer 
-          posts={
-            this.state.filteredPosts.length > 0 ? this.state.filteredPosts : this.state.postData
-          }
-        />
-
+        <PostsPage />
       </div>
     );
   }
