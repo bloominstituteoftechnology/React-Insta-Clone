d813cb2941fa3615b5020e7ce10d36e210e9179d
wisdomtooth <papaya2002@hotmail.com>
2018-11-15 00:15:45 -0600

    add filter functionality

--- instagram/src/components/PostContainer/PostsPage.js
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const PostsPage = props =>{
-    return(
-        <h1>test</h1>
-    );
-}
+const PostsPage = props => {
+  return <h1>test</h1>;
+};
 
 export default PostsPage;
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,16 @@
-import React,{Fragment} from 'react';
+import React, { Fragment } from "react";
 
 const SearchBar = props => {
-    return(
-        <Fragment>
-            <input type="text" placeholder="search here.."/>
-        </Fragment>
-    );
-}
+  return (
+    <Fragment>
+      <input
+        type="text"
+        placeholder="search here.."
+        onChange={props.handleSearch}
+        value={props.searchText}
+      />
+    </Fragment>
+  );
+};
 
 export default SearchBar;
