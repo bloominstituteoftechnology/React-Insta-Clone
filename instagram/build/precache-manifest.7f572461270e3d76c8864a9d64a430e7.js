e9e7981f8de27348969e762ddcefbdb8fbba60cc
jheinz1999 <jonathan.heinz@aol.com>
2018-11-15 13:29:13 -0800

    set up continuous deployment on netlify

---
+++ instagram/build/precache-manifest.7f572461270e3d76c8864a9d64a430e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dec417928863d43f88a01ec70a89eaf",
+    "url": "/static/media/logo.0dec4179.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1c51bfea15537e7b868",
+    "url": "/static/js/main.f1c51bfe.chunk.js"
+  },
+  {
+    "revision": "b4c1cbe96f8873725ef0",
+    "url": "/static/js/1.b4c1cbe9.chunk.js"
+  },
+  {
+    "revision": "f1c51bfea15537e7b868",
+    "url": "/static/css/main.52583f51.chunk.css"
+  },
+  {
+    "revision": "c4de9ac8a99f811913e5186dd6541ab1",
+    "url": "/index.html"
+  }
+];
