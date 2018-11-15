33e4d16a27a458c97b9c599ea44fca61afdfc7d0
David Freitag <freitagdavid@outlook.com>
2018-11-15 18:28:45 -0500

    instagram/src/components/CommentSection/CommentContainer:  converted to styled component and broke social icons down

--- instagram/src/components/CommentSection/CommentContainer.js
+++ instagram/src/components/CommentSection/CommentContainer.js
@@ -1,58 +1,13 @@
 import React, { Component } from 'react';
 import Comment from './Comment';
 import shortid from 'shortid';
-// import './CommentContainer.sass';
 import styled from 'styled-components';
+import SocialIcons from './SocialIcons';
 
 const Div = styled.div`
     padding: 0 16px;
     display: flex;
     flex-direction: column;
-    .comment--social-icons {
-        margin-top: 4px;
-        display: grid;
-        width: 100%;
-        grid-template-columns: 40px 40px 40px auto;
-        div {
-            display: flex;
-            align-items: center;
-            cursor: pointer;
-            height: 40px;
-            width: 40px;
-            span {
-                background-image: url(https://www.instagram.com/static/bundles/base/sprite_glyphs.png/07c1c2c6e459.png);
-                display: block;
-                background-repeat: no-repeat;
-                height: 24px;
-                width: 24px;
-            }
-            &.icon-heart {
-                span {
-                    background-position: -130px -115px;
-                    &.liked {
-                        background-position: -104px -115px;
-                    }
-                }
-            }
-            &.icon-comment {
-                span {
-                    background-position: -217px 0px;
-                }
-            }
-            &.icon-share {
-                span {
-                    background-position: -182px -167px;
-                }
-            }
-            &.icon-bookmark {
-                justify-self: end;
-                margin-right: -18px;
-                span {
-                    background-position: -52px -167px;
-                }
-            }
-        }
-    }
 `;
 
 class CommentContainer extends Component {
@@ -88,25 +43,8 @@ class CommentContainer extends Component {
     };
     render() {
         return (
-            <Div className="comment-container">
-                <div className="comment--social-icons">
-                    <div
-                        className="icon-heart"
-                        onClick={() => {
-                            this.likeHandler();
-                        }}>
-                        {this.state.liked ? <span className="liked" /> : <span />}
-                    </div>
-                    <div className="icon-comment">
-                        <span />
-                    </div>
-                    <div className="icon-share">
-                        <span />
-                    </div>
-                    <div className="icon-bookmark">
-                        <span />
-                    </div>
-                </div>
+            <Div>
+                <SocialIcons liked={this.state.liked} likeHandler={this.likeHandler} />
                 <p>{this.state.likes}</p>
                 {this.state.comments.map((comment) => <Comment comment={comment} key={shortid.generate()} />)}
                 <form
