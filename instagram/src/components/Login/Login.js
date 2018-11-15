d4746d99a83b743cedad9ec18d35c2a5dc2b42ba
Yasirah Krueng <ykrueng@Lambda-School-Loaner-39s-MacBook-Pro.local>
2018-11-15 15:41:47 -0600

    refactor Login.css to use styled components

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,6 +1,58 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import './Login.css';
+
+const StyledLoginForm = styled.div`
+  width: 50rem;
+  margin: 7rem auto;
+`;
+
+const StyledForm = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledInput = styled.input`
+  all: unset;
+  padding: 2rem;
+  font-size: 1.4rem;
+  border: 1px solid gray;
+  border-radius: .5rem;
+  margin-bottom: 1rem;
+  
+  &:active,
+  &:focus{
+    background-color: lightyellow;
+  }
+`;
+
+const StyledButton = styled.button`
+  padding: 2rem;
+  font-size: 2.4rem;
+  font-weight: bold;
+  border: 2px solid white;
+  color: white;
+  background-color: lightgray;
+  border-radius: .5rem;
+
+  &:hover {
+    color: darkgray;
+    background-color: white;
+    border-color: darkgray;
+  }
+
+  &:active {
+    background-color: cyan;
+  }
+`;
+
+const Hint = styled.ul`
+  margin: 2rem;
+  list-style: square inside;
+  font-size: 1.2rem;
+  color: gray;
+`;
 
 class Login extends React.Component {
   constructor(props) {
@@ -37,21 +89,21 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div className="login--form">
-        <form>
-          <input name="username" onChange={e => this.handleChange(e)} type="text" placeholder="Username"/>
-          <input name="password" onKeyPress={e => this.handleKey(e)} onChange={e => this.handleChange(e)} type="password" placeholder="Password"/>
-          <button type="button" onClick={() => this.login()}>Log In</button>
-        </form>
-        <ul className="login--hint" style={{display: this.state.hint ? 'block' : 'none' }}>
+      <StyledLoginForm className="login--form">
+        <StyledForm>
+          <StyledInput name="username" onChange={e => this.handleChange(e)} type="text" placeholder="Username"/>
+          <StyledInput name="password" onKeyPress={e => this.handleKey(e)} onChange={e => this.handleChange(e)} type="password" placeholder="Password"/>
+          <StyledButton type="button" onClick={() => this.login()}>Log In</StyledButton>
+        </StyledForm>
+        <Hint className="login--hint" style={{display: this.state.hint ? 'block' : 'none' }}>
           <li>
             Username must have at least one character
           </li>
           <li>
             Password: lambda
           </li>
-        </ul>
-      </div>
+        </Hint>
+      </StyledLoginForm>
     );
   }
 }
