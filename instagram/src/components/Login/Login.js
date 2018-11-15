aae4944ea8288811406f6d32af32e2ec2abc3871
darenlarson <larson.daren@gmail.com>
2018-11-15 16:54:35 -0600

    Styled the login page using styled components.

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,4 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
+import instagramlogo from './instagramlogo.png';
+
+const LoginPage = styled.div`
+    background-color: #FAFAFA;
+    display: flex;
+    justify-content: center;
+`;
+
+const LoginWrapper = styled.div`
+    background-color: #FFFFFF;
+    border: 1px solid lightgrey;
+    padding: 25px 35px;
+    margin-top: 75px;
+    width: 350px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+const Logo = styled.img`
+    width: 70%;
+    margin: 10px;
+`;
+const Form = styled.form`
+
+`;
+const Input = styled.input`
+    width: 100%;
+    line-height: 30px;
+    margin: 3px 0;
+    background-color: #FAFAFA;
+    border: 1px solid lightgrey;
+    border-radius: 5px;
+`;
+const Button = styled.button`
+    width: 100%;
+    background-color: #3797F0;
+    color: white;
+    border-radius: 5px;
+    margin: 3px 0;
+    line-height: 25px;
+    font-size: 14px;
+`;
+
 
 class Login extends React.Component {
     constructor() {
@@ -20,29 +64,31 @@ class Login extends React.Component {
 
     render() {
         return (
-            <>
-                <div>Login Page Yo</div>
-                <form>
-                    <input 
-                        type="text"
-                        placeholder="Enter your user name"
-                        name="username"
-                        value={this.state.username}
-                        onChange={this.handleInput}
-                    />
+            <LoginPage>
+                <LoginWrapper>
+                    <Logo src={instagramlogo} alt="logo" />
+                    <Form>
+                        <Input 
+                            type="text"
+                            placeholder="  Phone number, username, or email"
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.handleInput}
+                        />
 
-                    <input
-                        type="password"
-                        placeholder="Enter your password"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleInput} 
-                    />
+                        <Input
+                            type="password"
+                            placeholder="  Password"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.handleInput} 
+                        />
 
-                    <button onClick={this.handleSubmitLogin}>Log In</button>
+                        <Button onClick={this.handleSubmitLogin}>Log in</Button>
 
-                </form>
-            </>
+                    </Form>
+                </LoginWrapper>
+            </LoginPage>
         )
     }
 }
