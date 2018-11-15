accd2c9cd34b78bcdb853100b1cf0074632088a2
ToMMaaY <thuynh629@gmail.com>
2018-11-15 15:55:11 -0800

    Converted the search bar CSS into styled components

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,20 +1,52 @@
 import React, { Component } from "react";
 import "./SearchBar.css";
+import styled, { css } from "styled-components";
+
+const SpaceBetween = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const FlexStart = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const NavIcon = styled.i`
+  width: 50px;
+  height: auto;
+`;
+
+const SearchField = styled.input`
+  .search-bar {
+    width: 100%;
+    height: 30px;
+    border: 1px solid lightgray;
+    border-radius: 5px;
+    background: rgb(243, 243, 243);
+    color: gray;
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: lighter;
+  }
+`;
 
 const SearchBar = props => {
   return (
-    <div className="search-section">
-      <div className="instagram-icons">
-        <i className="fa fa-instagram fa-3x" />
+    <SpaceBetween className="nav-bar">
+      <FlexStart>
+        <NavIcon className="fa fa-instagram fa-3x" />
         <img
           src="https://itsdcdn.com/resources/services/logowide/170/instagram.png"
           className="instagram-title"
           alt="Instagram Logo"
         />
-      </div>
+      </FlexStart>
 
       <form className="search-container">
-        <input
+        <SearchField
           type="text"
           placeholder="Search"
           className="search-bar"
@@ -23,12 +55,12 @@ const SearchBar = props => {
         />
       </form>
 
-      <div className="menu-icons">
-        <i className="fa fa-compass fa-3x" />
-        <i className="fa fa-heart-o fa-3x" />
-        <i className="fa fa-user-o fa-3x" />
-      </div>
-    </div>
+      <FlexStart>
+        <NavIcon className="fa fa-compass fa-3x" />
+        <NavIcon className="fa fa-heart-o fa-3x" />
+        <NavIcon className="fa fa-user-o fa-3x" />
+      </FlexStart>
+    </SpaceBetween>
   );
 };
 
