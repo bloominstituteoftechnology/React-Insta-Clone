ac8d4b01c2b69057fb5687dc1697ebcccdca9f47
David Freitag <freitagdavid@outlook.com>
2018-11-15 16:49:40 -0500

    instagram/src/components/PostContainer/PostContainer: refactored to styled

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -2,14 +2,34 @@ import React from 'react';
 import PostBody from './PostBody';
 import PostUserInfo from './PostUserInfo';
 import PropTypes from 'prop-types';
-import './PostContainer.sass';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    align-items: stretch;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 60px;
+    border-radius: 3px;
+    border: 1px solid #e6e6e6;
+    background-color: white;
+    margin-left: -1px;
+    margin-right: -1px;
+    .post--body {
+        display: flex;
+        align-items: stretch;
+        flex-direction: column;
+        img {
+            width: 100%;
+        }
+    }
+`;
 
 const PostContainer = (props) => {
     return (
-        <div className="post-container">
+        <Div>
             <PostUserInfo username={props.post.username} thumbnailUrl={props.post.thumbnailUrl} />
             <PostBody post={props.post} likes={props.likes} index={props.index} />
-        </div>
+        </Div>
     );
 };
 
