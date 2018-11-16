11fc3fc014bb628bf2d2275c8fecfdb9e79bb4d9
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-11-16 10:14:22 -0500

    Authenticate is working

--- instagram/src/components/Authentication/Authenticate.js
+++ instagram/src/components/Authentication/Authenticate.js
@@ -1,25 +1,26 @@
 import React from "react";
+import Login from "../../components/Login/Login";
 
 const Authenticate = App => LoginPage =>
-  class extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        loggedIn: false
-      };
-    }
+    class extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                loggedIn: false
+            };
+        }
 
-    componentDidMount() {
-      if (!localStorage.getItem("user")) {
-        this.setState({ loggedIn: false });
-      } else {
-        this.setState({ loggedIn: true });
-      }
-    }
-    render() {
-      if (this.state.loggedIn) return <App />;
-      return <LoginPage />;
-    }
-  };
+        componentDidMount() {
+            if (!localStorage.getItem("user")) {
+                this.setState({ loggedIn: false });
+            } else {
+                this.setState({ loggedIn: true });
+            }
+        }
+        render() {
+            if (this.state.loggedIn) return <App / > ;
+            return <LoginPage / > ;
+        }
+    };
 
 export default Authenticate;
