85931397516b82ef3d70e80e613ec91c7020faad
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-11-26 16:49:33 -0500

    index.js

--- instagram/src/index.js
+++ instagram/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'font-awesome/css/font-awesome.min.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
