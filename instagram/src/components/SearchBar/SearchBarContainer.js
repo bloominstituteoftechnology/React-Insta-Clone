23c654f15bbc0637a82407905d274d12a062def6
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-11-27 17:51:24 -0500

    instagram icons

--- instagram/src/components/SearchBar/SearchBarContainer.js
+++ instagram/src/components/SearchBar/SearchBarContainer.js
@@ -4,11 +4,23 @@ import './SearchBar.css'
 const SearchBar = () => {
     return (
         <div className="search-bar-wrap">
-        <div className="img-wrap"></div>
-        <div>
+          <div className="img-wrap">
+            <i className="fa fa-instagram" />
+            </div>
+          <div>
             <input type ="text" placeholder="Search" />
+            </div>
+          <div className="social-icons-wrapper">
+          <div className="social-icon">
+            <i className="fa fa-compass" />
           </div>
-        <div className="social-icons-wrapper"></div>
+          <div className="social-icon">
+            <i className="fa fa-heart" />
+          </div>
+          <div className="social-icon">
+            <i className="fa fa-user-circle" />
+          </div>
+            </div>
         </div>
     )
 }
