17277aa79be3bd18b182291edd2d51b30e37de2e
1@$@1github <asaofdiamonds@gmail.com>
2018-11-26 19:55:44 -0700

    working on the searchbar

--- instagram/src/components/SearchBar/searchBar.js
+++ instagram/src/components/SearchBar/searchBar.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+
 import './SearchBar.css'
 
 const SearchBar = props => {
     return (
         <div className='search-bar'>
+            <div className='search-input'>
+                <input type='text' placeholder='What are you looking for...'  />
+            </div>
+            <div className='social'>
+                <i className='fa fa-compass' />
+            </div>
+            <div className='social'>
+                <i className='fa fa-heart' />
+            </div>
+            <div className='social'>
+                <i className='fa fa-user-circle' />
+            </div>
+
         This is the search bar
         </div>
     );
