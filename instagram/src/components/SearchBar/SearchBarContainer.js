c01256ccbdd9a30f385ba75cf1d3ec71dc1e7677
Joseph Eastman <josephryan96@gmail.com>
2018-11-27 15:11:43 -0800

    js somewhat function, css wip

---
+++ instagram/src/components/SearchBar/SearchBarContainer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import IGLogo from "./../../img/iglogo.png";
+import "./Search.css";
+
+const SearchBarContainer = () => {
+  return (
+    <div className="search-bar">
+      <div className="search-img">
+      <i className='fab fa-instagram' />
+        <img alt="instagram logo" src={IGLogo} className="logo-image" />
+      </div>
+      <div>
+        <input type="text" placeholder="Search..." />
+      </div>
+      <div className="social-wrapper">
+        <div className="social">
+          <i className="fa fa-compass" />
+        </div>
+        <div className="social">
+          <i className="fa fa-heart" />
+        </div>
+        <div className="social">
+          <i className="fa fa-user-circle" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBarContainer;
