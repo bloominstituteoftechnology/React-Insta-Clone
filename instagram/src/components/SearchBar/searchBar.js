8f5f697b2aaeb537ee8bad3a6247ff3c8e54cfc5
1@$@1github <asaofdiamonds@gmail.com>
2018-11-28 16:06:51 -0700

    added logo

--- instagram/src/components/SearchBar/searchBar.js
+++ instagram/src/components/SearchBar/searchBar.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import LogoInstagram from "../../assets/logoInstagram.png";
 import './SearchBar.css'
 
 const SearchBar = props => {
     return (
         <div className='search-bar'>
+            <div className='logo-wrap'>
+                <img className='logo-instagram'
+                     src={LogoInstagram}
+                     alt='Instagram Logo'
+                     />
+            </div>
             <div className='search-input'>
                 <input type='text' placeholder='What are you looking for...' onKeyDown={props.searchPosts}  />
             </div>
