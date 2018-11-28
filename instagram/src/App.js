45a6f266a524763527de3c4c6313fc0840c047c5
chrisgins <christiansgina@gmail.com>
2018-11-28 11:50:35 -0700

    refactored app.css

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PostsPage from './components/PostContainer/PostsPage.js'
+import Authenticate from './components/Authentication/Authenticate.js'
+import {WrapperDiv} from './components/Styles'
 import './App.css';
 
 class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = ({
+      username: this.props.username,
+    })
+  }
+  
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return(
+      <WrapperDiv>
+        <PostsPage username={this.state.username} />
+      </WrapperDiv>
+    )
   }
 }
 
-export default App;
+export default Authenticate(App);
