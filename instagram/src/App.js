d0444d8bac8f10a44bdb084e58d42dbd2fe0bb88
jrizza88 <jetorres88@gmail.com>
2018-12-11 00:08:05 -0500

    imported dummy-data.js file into App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dummyData from  '../../dummyData'
+
 
 class App extends Component {
   render() {
