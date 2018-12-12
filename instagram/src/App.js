e6ec866c8c1fc52c2c3d558a6e7bbfc025dd56b0
Trip1701 <bj2336@email.vccs.edu>
2018-12-11 22:51:02 -0500

    React Project Set Up, Dependecies Installed

---
+++ instagram/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Proptypes from 'prop-types';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        test!!!!!!!!!!
+      </div>
+    );
+  }
+}
+
+export default App;
