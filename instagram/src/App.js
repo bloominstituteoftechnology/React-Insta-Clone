5474afb287509783d8544185e06d3de5f8038968
Jonathan Dorety <kyullog@gmail.com>
2018-12-12 00:04:48 -0500

    Add SearchBar

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <SearchBar />
+      </div>
+    );
   }
 }
 
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Input } from "reactstrap";
+
+const SearchBar = props => <Input placeHolder="Search" />;
+
+export default SearchBar;
