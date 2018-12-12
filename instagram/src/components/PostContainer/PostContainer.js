957e5b0701f1ac61be9bbec1d0468d64d5f433c4
Trip1701 <bj2336@email.vccs.edu>
2018-12-12 10:54:26 -0500

    PostContainer Type Checking Implemented

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Proptypes from 'prop-types';
+import PropTypes from 'prop-types';
 import CommentSection from '../CommentSection/CommentSection';
 
 import './PostContainer.css';
@@ -35,6 +35,20 @@ class PostContainer extends React.Component {
       </div>
     );
   }
+}
+
+PostContainer.propTypes = {
+  user: PropTypes.string,
+  thumbnail: PropTypes.string,
+  image: PropTypes.string,
+  time: PropTypes.string,
+  likes: PropTypes.number,
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      username: PropTypes.string,
+      text: PropTypes.string,
+    })
+  ),
 }
 
 export default PostContainer;
