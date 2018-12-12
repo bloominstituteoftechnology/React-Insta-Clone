0c8caf77a936663565ed7dd45b1710d55ff18ef5
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-12-12 18:13:01 -0500

    ADD POSTCONTAINER COMPONENT FILE, ADD TO APP.JS

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostComponent/PostComponent';
 
 class App extends Component {
+  
+  
+
   render() {
     return (
       <div className="App">
         <nav>
           <SearchBar />
         </nav>
+        <div className="body-wrapper">
+          <PostContainer />
+        </div>
       </div>
     );
   }
