338f3fff6b12ef5ab5344eb3fc83575a2753a53f
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-12-12 14:14:41 -0500

    ADD SEARCHBAR COMPONENT, ADD FONT-AWESOME
    - add font-awesome library
    - add logo to searchbar
    - add searchbar css
    - add search button
    - add social icons

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SearchBar />
+        <nav>
+          <SearchBar />
+        </nav>
       </div>
     );
   }
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import logo from './logo.png';
 
+import '../../../node_modules/font-awesome/css/font-awesome.min.css';
 
 const SearchBar = () => {
 
     return(
-        <div>SearchBar</div>
+        <div className="searchbar-wrapper">
+            <div className="navs img-logo">
+                <img src={logo} alt="insta-logo" />
+                <h3>Instagram</h3>
+            </div>
+            <div className="navs">
+                <button>
+                    <i className="fa fa-search"></i>
+                    Search
+                </button>
+            </div>
+            <div className="navs icons">
+                <div className="icons">
+                    <i className="fa fa-compass"></i>
+                </div>
+                <div className="icons">
+                    <i className="fa fa-heart-o"></i>
+                </div>
+                <div className="icons">
+                    <i className="fa fa-user-o"></i>
+                </div>
+            </div>
+        </div>
     )
 }
 
