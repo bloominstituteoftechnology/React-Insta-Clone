9af3583010267ec49de1bc6920332b73690d2552
mustafa inaya <moose.inaya@icloud.com>
2018-12-12 14:53:26 -0800

    DEBUG

--- instagram/src/Components/CommentSection.js
+++ instagram/src/Components/CommentSection.js
@@ -1,6 +1,22 @@
 import React from 'react';
-
-const Comment = (props) => {};
-const CommentSection = (props) => {
-	return <div />;
+import dummyData from '../dummy-data';
+const Comment = (props) => {
+	return (
+		<div>
+			{dummyData.comments.map((item) => (
+				<div key={item.username}>
+					{item.text} {item.username}
+				</div>
+			))}
+		</div>
+	);
 };
+const CommentSection = (props) => {
+	return (
+		<div>
+			<Comment />
+			<input value={props.text} onChange={props.commentInput} />
+		</div>
+	);
+};
+export default CommentSection;
