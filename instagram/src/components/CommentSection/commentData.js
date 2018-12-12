206282cf0cc524a4853720b83e9c7a460b2d48f6
jrizza88 <jetorres88@gmail.com>
2018-12-11 23:32:29 -0500

    created a commentData.js file

---
+++ instagram/src/components/CommentSection/commentData.js
@@ -0,0 +1,59 @@
+const commentData = [
+    {
+        comments: [
+            {
+              username: "philzcoffee",
+              text:
+                "We've got more than just delicious coffees to offer at our shops!"
+            },
+            {
+              username: "biancasaurus",
+              text: "Looks delicious!"
+            },
+            {
+              username: "martinseludo",
+              text: "Can't wait to try it!"
+            }
+          ]
+    },
+    {
+    comments: [
+        {
+          username: "twitch",
+          text: "Epic Street Fighter action here in Las Vegas at #EVO2017!"
+        },
+        {
+          username: "michaelmarzetta",
+          text: "Omg that match was crazy"
+        },
+        {
+          username: "themexican_leprechaun",
+          text: "What a setup"
+        },
+        {
+          username: "dennis_futbol",
+          text: "It that injustice"
+        },
+        {
+          username: "dennis_futbol",
+          text: "Is"
+        }
+      ]
+    },
+    {
+    comments: [
+        {
+          username: "playhearthstone",
+          text: "Love this shot!"
+        },
+        {
+          username: "awaywetravel",
+          text: "Yosemite is my most favorite place in the universe"
+        },
+        {
+          username: "awesomebt28",
+          text: "I like how Half Dome looks so old and useless"
+        }
+      ]
+    }
+]
