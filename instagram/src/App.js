6ca61b641a3e07bf6432250c62207606b03de5f7
Taylor Belk <taylorbelk@Taylors-MacBook-Air.local>
2018-12-12 07:50:10 -0600

    moved dummy data to src and added the imports for the main components to app.js as well as the calls

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import dummyData from './dummy-data';
 import './App.css';
+import SearchBar from './components/SearchBar/search';
+import Posts from './components/PostContainer/posts';
+import Comments from './components/CommentSection/comments';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <SearchBar />
+        <Posts />
+        <Comments />
       </div>
     );
   }
