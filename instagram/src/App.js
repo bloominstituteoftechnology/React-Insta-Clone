f3ad023d2d6fea919f35bf2b724782ff38218709
Jason Cook <the.root.user@gmail.com>
2018-12-12 00:00:39 -0500

    Import PropTypes, configure node-sass, import Bulma

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+import './App.scss';
 
 class App extends Component {
   render() {
