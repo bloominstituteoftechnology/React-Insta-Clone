c326405d5bb2feba6ec4e09250a7b356f89ebdfa
iradsanchez <ira.d.sanchez@gmail.com>
2018-12-11 22:08:39 -0700

    began building comment section

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import dummyData from "./dummy-data";
 import SearchBar from "./components/SearchBar/SearchBar";
+import PostContainer from "./components/PostContainer/PostContainer";
+
+// - The root App component of your application should import the dummy data from the `dummy-data.js` file with `import dummyData from './dummy-data';` and iterate over said data, passing each individual object as a prop to an instance of `PostContainer`.
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: dummyData
+    };
+  }
   render() {
+    console.log(this.state.users);
     return (
       <div className="App">
         <SearchBar className="searchbar-container" />
+        <PostContainer users={this.state.users} />
       </div>
     );
   }
---
+++ instagram/src/components/CommentSection/CommentSectionContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class CommentSection extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      //array of comments goes here
+    };
+  }
+
+  render() {
+    return <div>{this.state}</div>;
+  }
+}
