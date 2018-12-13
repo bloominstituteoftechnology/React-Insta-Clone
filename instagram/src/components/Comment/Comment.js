549388fd7b11e8f77c07e2ec40d3e85d973b6f15
edreeseg <edward.reeseg@gmail.com>
2018-12-13 11:43:37 -0500

    Comment styled-components, all components now converted to S-C

--- instagram/src/components/Comment/Comment.js
+++ instagram/src/components/Comment/Comment.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import deleteIcon from '../../assets/insta-delete.png';
-import './Comment.css';
+
+const Instance = styled.section`
+    display: flex;
+    align-items: center;
+    text-align: left;
+    margin-bottom: 10px;
+
+    p {
+        margin-right: 10px;
+    }
+    span {
+        font-weight: bold;
+        cursor: pointer;
+    }
+    img {
+        height: auto;
+        width: 20px;
+        cursor: pointer;
+    }
+`;
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
 
 const Comment = props => (
-    <div className="comment-instance">
+    <Instance>
         <p><span>{props.name}</span> {props.text}</p>
         <img src={deleteIcon} onClick={props.onDelete} alt="Delete button" title="Delete" />
-    </div>
+    </Instance>
 )
 
 Comment.propTypes = {
