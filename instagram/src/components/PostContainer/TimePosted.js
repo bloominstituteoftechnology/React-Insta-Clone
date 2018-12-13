8b2ca2eb45d88475fc9490ff94ed90493877faad
Jonathan Dorety <kyullog@gmail.com>
2018-12-12 22:50:51 -0500

    Add PropTypes check for TimePosted

--- instagram/src/components/PostContainer/TimePosted.js
+++ instagram/src/components/PostContainer/TimePosted.js
@@ -1,8 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./PostContainer.css";
 
 const TimePosted = props => {
   return <span className={"time-posted"}>{props.timestamp}</span>;
+};
+
+TimePosted.propTypes = {
+  timestamp: PropTypes.string
 };
 
 export default TimePosted;
