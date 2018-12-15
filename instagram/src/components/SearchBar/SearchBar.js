256e7fa6cb044da53c8f1eec5abdff6d0dce5df5
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2018-12-15 10:05:44 -0600

    Finished search bar, just needs additional styling.

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,29 @@
-import React from 'react';
- class SearchBar extends React.Component
-{
-  render()
-  {
-    return(
-        <div className = "searchbar">SearchBar</div>
-    )
-  }
-}
- export default SearchBar; 
+import React from "react";
+import iglogo from "./iglogo.png";
+import "./SearchBar.css";
+
+const SearchBar = () => {
+  return (
+    <div className="search-bar-wrapper">
+      <div className="image-wrapper">
+        <img alt="instagram logo" src={iglogo} className="logo-image" />
+      </div>
+      <div>
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className="social-wrapper">
+        <div className="social">
+          <i className="far fa-compass" />
+        </div>
+        <div className="social">
+          <i className="far fa-heart" />
+        </div>
+        <div className="social">
+          <i className="far fa-user-circle" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
