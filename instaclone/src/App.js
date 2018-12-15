e0107517d41f2f944de09f9768f6e1cfb7fe5e33
Noah <noah.stoeckel@gmail.com>
2018-12-15 13:21:32 -0500

    Adds LifeCycle Methods

--- instaclone/src/App.js
+++ instaclone/src/App.js
@@ -11,17 +11,27 @@ class App extends Component {
   constructor(){
     super();
     this.state ={
-      dummyData:dummyData
+      dummyData:[]
     }
   }
+
+componentDidMount(){
+  setTimeout(()=>{
+    this.setState({dummyData:dummyData})
+  }, 1000)
+  
+}
+
   render() {
     return (
       <div className="App">
         <SearchBar />
         <div>
-        {this.state.dummyData.map((post,i)=>{
-          return <PostContainer key={i}  post={post} />
-        })}
+          {this.state.dummyData.length === 0? <p>LOADING</p>:
+            this.state.dummyData.map((post,i)=>{
+              return <PostContainer key={i}  post={post} index={i} />
+            })
+          }          
         </div>
       </div>
     );
