cc450b63fc5d2bc13bf8f476cacabd276405a2b2
Jonathan Dorety <kyullog@gmail.com>
2018-12-15 00:02:47 -0500

    Refactor CommentSection to class, implement componentDidMount

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -3,30 +3,43 @@ import PropTypes from "prop-types";
 import "./CommentSection.css";
 import { FiHeart, FiMessageCircle, FiSend } from "react-icons/fi";
 
-const CommentSection = props => {
-  return (
-    <div className={"comments-container"}>
-      <div className={"icon-container"}>
-        <FiHeart className={"icon"} />
-        <FiMessageCircle className={"icon"} />
-        <FiSend className={"icon"} />
+class CommentSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      comments: this.props.data.comments
+    });
+  }
+
+  render() {
+    return (
+      <div className={"comments-container"}>
+        <div className={"icon-container"}>
+          <FiHeart className={"icon"} />
+          <FiMessageCircle className={"icon"} />
+          <FiSend className={"icon"} />
+        </div>
+        <span className={"likes"}>{this.props.data.likes} likes</span>
+        {this.props.data.comments.map((comments, index) => {
+          return (
+            <div className={"comments-field"} key={comments.text}>
+              <p className={"comment-content"}>
+                <span className={"comment-username"}>{comments.username}</span>
+                {comments.text}
+              </p>
+            </div>
+          );
+        })}
       </div>
-      <span className={"likes"}>{props.data.likes} likes</span>
-      {props.data.comments.map((comments, index) => {
-        return (
-          <div className={"comments-field"} key={comments.text}>
-            <p className={"comment-content"}>
-              <span className={"comment-username"}>
-                {comments.username}
-              </span>
-              {comments.text}
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+    );
+  }
+}
 
 CommentSection.propTypes = {
   data: PropTypes.shape({
