db44b6765a404dc9b11bd06dc025f387400dd263
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2018-12-15 19:08:58 -0600

    Added event handlers for search functionality

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,53 +1,53 @@
-import React, { Component } from 'react';
-import './App.css';
-import dummyData from './dummy-data';
-import PostContainer from './components/PostContainer/PostContainer';
-import SearchBar from './components/SearchBar/SearchBar';
-import propTypes from 'prop-types';
-
+import React, { Component } from "react";
+import "./App.css";
+import dummyData from "./dummy-data";
+import PostContainer from "./components/PostContainer/PostContainer";
+import SearchBar from "./components/SearchBar/SearchBar";
+import propTypes from "prop-types";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       post: [],
-      searchInput: '',
+      searchInput: ""
     };
   }
   componentDidMount() {
     this.setState({
-      post: dummyData,
-    })
+      post: dummyData
+    });
   }
 
-  searchHandler = event => {
-    this.setState({
-      searchInput: event.target.value,
-    });
-    console.log(this.state.searchInput)
+  searchHandler = text => {
+    if (text === "") {
+      this.setState({ post: dummyData });
+    } else {
+      const searchResults = this.state.post.filter(c => c.username === text);
+      this.setState({ post: searchResults });
+    }
   };
 
   search = event => {
     event.preventDefault();
-      if (!(this.state.searchInput === '')) {
-          this.setState({
-              post: [],
-              inputText: ''
-            });
-        }
-    };
+    if (!(this.state.searchInput === "")) {
+      this.setState({
+        post: [],
+        inputText: ""
+      });
+    }
+  };
 
   render() {
     return (
       <div className="App">
-        <SearchBar searchHandler ={this.searchHandler}/>
-        <PostContainer  posts={this.state.post} user={this.props.user}/>
+        <SearchBar searchHandler={this.searchHandler} />
+        <PostContainer posts={this.state.post} user={this.props.user} />
       </div>
     );
   }
 }
 
-App.propTypes = {card:propTypes.arrayOf(propTypes.object)};
-
+App.propTypes = { card: propTypes.arrayOf(propTypes.object) };
 
 export default App;
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -2,33 +2,47 @@ import React from "react";
 import iglogo from "./iglogo.png";
 import "./SearchBar.css";
 
-const SearchBar = () => {
-    return(
-    <div className="search-bar-wrapper">
-      <div className="image-wrapper">
-      <div className="social">
-        <i className ="fab fa-instagram" ></i>
-      </div>
-        <img alt="instagram logo" src={iglogo} className="logo-image" />
-      </div>
-      <div>
-        <input className="search" type="text" placeholder="Search" />
-      </div>
-      <div className="social-wrapper">
-        <div className="social">
-          <i className="far fa-compass" />
+class SearchBar extends React.Component {
+  state = { search: "" };
+
+  submitSearch = event => {
+    console.log("on submit");
+    event.preventDefault();
+    this.props.searchHandler(this.state.search);
+  };
+
+  render() {
+    return (
+      <div className="search-bar-wrapper">
+        <div className="image-wrapper">
+          <div className="social">
+            <i className="fab fa-instagram" />
+          </div>
+          <img alt="instagram logo" src={iglogo} className="logo-image" />
         </div>
-        <div className="social">
-          <i className="far fa-heart" />
-        </div>
-        <div className="social">
-          <i className="far fa-user-circle" />
+        <form onSubmit={this.submitSearch}>
+          <input
+            className="search"
+            type="text"
+            value={this.state.search}
+            placeholder="Search"
+            onChange={e => this.setState({ search: e.target.value })}
+          />
+        </form>
+        <div className="social-wrapper">
+          <div className="social">
+            <i className="far fa-compass" />
+          </div>
+          <div className="social">
+            <i className="far fa-heart" />
+          </div>
+          <div className="social">
+            <i className="far fa-user-circle" />
+          </div>
         </div>
       </div>
-    </div>
     );
   }
-
-  
+}
 
 export default SearchBar;
