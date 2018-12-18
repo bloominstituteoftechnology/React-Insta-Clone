0e4449903355ec24b6d972181171ef4fcf39ae7b
iradsanchez <ira.d.sanchez@gmail.com>
2018-12-18 15:15:33 -0700

    app.js day 2 edits

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from "react";
 import "./App.css";
 import dummyData from "./dummy-data";
-import PostsContainer from "./components/PostContainer/PostContainer";
-import SearchBar from "./components/SearchBar/SearchBar";
+import PostsContainer from "./components/PostsContainer/PostsContainer";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      posts: []
+      posts: [],
+      filteredPosts: []
     };
   }
-
   componentDidMount() {
     this.setState({ posts: dummyData });
   }
-
+  searchPostsHandler = e => {
+    const posts = this.state.posts.filter(p => {
+      if (p.username.includes(e.target.value)) {
+        return p;
+      }
+    });
+    this.setState({ filteredPosts: posts });
+  };
   render() {
     return (
       <div className="App">
-        <SearchBar />
-        <PostsContainer posts={this.state.posts} />
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          searchPosts={this.searchPostsHandler}
+        />
+        <PostsContainer
+          posts={
+            this.state.filteredPosts.length > 0
+              ? this.state.filteredPosts
+              : this.state.posts
+          }
+        />
       </div>
     );
   }
