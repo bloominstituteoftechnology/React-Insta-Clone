95889a67bf05c87884239fc1317520c758da3fe7
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2018-12-28 11:25:49 -0700

    added feature to search bar - functionality to search for a username and display only their post

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -8,17 +8,47 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      data: dummyData
+      data: dummyData,
+      users: [],
+      searched: ""
     };
   }
   // componentDidMount() {
   //   this.setState({data: dummyData});
   // }
+
+  handleNewSearch = (event) => {
+    this.setState({searched: event.target.value});
+  }
+
+  onSearch = (event) => {
+    event.preventDefault();
+    const searchedUser = this.state.searched;
+    const users = this.state.data.slice();
+    const searchedUserResult = users.filter(user => user.username === searchedUser);
+
+    console.log(users);
+    console.log(searchedUserResult);
+    console.log(this.state.searched);
+
+    // if (searchedUserResult.length > 0) {
+    //   this.setState({users: searchedUserResult}, () => console.log(this.state.users));
+    // } 
+
+    if (searchedUserResult.length > 0) {
+      this.setState({data: searchedUserResult},
+        () => console.log(this.state.data)
+      )
+    } 
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <SearchBar users={this.state.data.map(user => { return user.username })}/>
-        <PostContainer data={this.state.data} />
+        <SearchBar data={this.state.data} onSearch={this.onSearch} handleNewSearch={this.handleNewSearch} />
+        {/* <SearchBar users={this.state.data.map(user => { return user.username })}/> */}
+        <PostContainer data={this.state.data} users={this.state.users}/>
       </div>
     );
   }
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -4,36 +4,9 @@ import './SearchBar.css';
 class SearchBar extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      users: this.props.users,
-      searched: ""
-    }
-  }
-
-  handleNewSearch = (event) => {
-    this.setState({searched: event.target.value});
-  }
-
-  onSearch = (event) => {
-    event.preventDefault();
-    const searchedUser = this.state.searched;
-    const searchedUserResult = this.state.users.filter(user => user === searchedUser);
-    console.log(searchedUserResult)
-    if (searchedUserResult.length > 0) {
-      this.setState({users: searchedUserResult});
-    } else {
-      alert(`No user found`);
-    }
-    this.setState({
-      searched: ""
-    })
-    // console.log(searchedUserResult);
-    
   }
 
   render() {  
-    // console.log(this.props.users)
-    // console.log(this.state.users)
   
     return (
       <div className="header">
@@ -51,8 +24,8 @@ class SearchBar extends React.Component {
           </div>
   
           <div className="search-bar-input">
-            <form onSubmit={this.onSearch}>
-              <input type="text" placeholder="Search" onChange={this.handleNewSearch}/>
+            <form onSubmit={this.props.onSearch}>
+              <input type="text" placeholder="Search" onChange={this.props.handleNewSearch}/>
             </form>
           </div>
           
