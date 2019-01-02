f13f49613a97fb0ccba7f2c619bca1e927d3aef7
derekjones-io <42226382+derekjones-io@users.noreply.github.com>
2019-01-01 22:18:21 -0500

    Add file structure for components .js and .css files

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return <input className="searchBarInput" placeholder="&#x1F50D; Search" />;
+  }
+}
+
+export default SearchBar;
