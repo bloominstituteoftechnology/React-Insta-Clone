a6b7181b723eeb3ab8c44b3ec734cb15f81372d6
Alando Appling <alandoappling@gmail.com>
2019-01-03 23:28:19 -0800

    Added Styled Components Dependancy // Working On Login Page Styled Components

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,5 +1,60 @@
 import React from 'react';
 import './Login.css';
+import styled from 'styled-components';
+const PageContainer = styled.body`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    background:#8aacc8;
+    width:100%;
+    height:100vh;
+`;
+const LoginContainer = styled.form`
+    display:flex;
+    justify-content:center;
+    text-align:center;
+    background:#bbdefb;
+    flex-wrap:wrap;
+    align-content:center;
+    height:200px;
+    width:80%;
+    border-radius:10px;
+
+`;
+const UsernameInput = styled.input`
+    display:flex;
+    text-align:center;
+    height:40px;
+    width:51%;
+    border:none;
+    border-radius:5px;
+    margin-bottom:10px;
+`;
+
+const PasswordInput = styled.input`
+    display:flex;
+    text-align:center;
+    height:40px;
+    width:51%;
+    border:none;
+    border-radius:5px;
+`;
+
+const LoginButton = styled.input`
+    display:flex;
+    justify-content:center;
+    height:20px;
+    width:25%;
+    border:none;
+    border-radius:5px;
+    background:white;
+`;
+const LineBreak = styled.div`
+    display:flex;
+    width:100%;
+    height:2px;
+    margin-bottom:10px;
+`;
 
 class Login extends React.Component{
     constructor(props){
@@ -28,13 +83,14 @@ class Login extends React.Component{
 
     render(){
         return(
-            <div className="login-page">
-                <form>
-                    <input type="text" name="Username" value={this.state.loginInput} onChange={this.handleLoginInput} placeholder="Username"/>
-                    <input type="text" name="Password" placeholder="Password"/>
-                    <input type="submit" onClick={this.loggingIn} value="Login" />
-                </form>
-            </div>
+            <PageContainer>
+                <LoginContainer>
+                    <UsernameInput type="text" name="Username" value={this.state.loginInput} onChange={this.handleLoginInput} placeholder="Username"/>
+                    <PasswordInput type="text" name="Password" placeholder="Password"/>
+                    <LineBreak />
+                    <LoginButton type="submit" onClick={this.loggingIn} Value="Login"  />
+                </LoginContainer>
+            </PageContainer>
         );
     }
 }
