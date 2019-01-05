5527588630c5728fe29cf555a82be8acf98a6cf6
karlie0 <kamabrapa88@gmail.com>
2019-01-05 15:10:31 -0500

    added searchbar icons and working on stylying

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
-import searchbar from './ig_search_bar.png'
+import searchbar from './ig_search_bar.png';
+import ig_icon from './instagram-icon.png';
+import ig_letters from './instagram-letters.png';
+import explore_icon from './explore-icon.png';
+import heart from '../CommentSection/ig-heart-icon-unliked.png'
+import profile from './profile-icon.png'
+import './SearchBar.css';
 
 class SearchBar extends Component {
     render() {
         return(
-            <div>
-                <img src={searchbar} alt="searchbar" />
+            <div className="searchBar">
+                <div className="ig-icon-div">
+                    <img src={ig_icon} alt="IG icon" className="ig-icon"/>
+                    <img src={ig_letters} alt="IG letters" className="ig-letters"/>
+                </div>
+                {/* <img src={searchbar} alt="searchbar" /> */}
             </div>
         )
     }
