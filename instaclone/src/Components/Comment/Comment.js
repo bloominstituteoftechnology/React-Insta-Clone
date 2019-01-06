524bb657e86cd788d4e146ae32fd0ed0774c3719
Noah <noah.stoeckel@gmail.com>
2019-01-05 20:54:46 -0500

    Refactors Comments To Use Styled Components

--- instaclone/src/Components/Comment/Comment.js
+++ instaclone/src/Components/Comment/Comment.js
@@ -1,5 +1,21 @@
 import React, { Component } from 'react';
-import './Comment.css'
+//import './Comment.css'
+import styled from 'styled-components'
+import {Username} from '../../Styles/Reusables'
+
+const CommentWrapper = styled.div`
+    margin:0rem 0rem;
+    margin-bottom:.2rem; 
+    display: flex;
+`
+const CommentUsername = styled(Username)`
+    margin: 0rem;
+    margin-right: .3rem;
+`
+const CommentText =  styled.span`
+    margin: 0rem;
+    font-weight: normal;
+`
 
 class Comment extends Component {
     constructor(props) {
@@ -8,7 +24,9 @@ class Comment extends Component {
     }
     render() { 
         return (
-            <p className="comment"><span className="user">{this.props.username}</span>{this.props.text}</p>
+            <CommentWrapper>
+                <CommentUsername>{this.props.username}<CommentText>{this.props.text}</CommentText></CommentUsername>
+            </CommentWrapper>
           );
     }
 }
