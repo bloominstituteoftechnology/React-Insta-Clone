5c505e4d0e2011bc6688de1aaccd97e674db5696
Trip1701 <bj2336@email.vccs.edu>
2019-01-08 01:21:07 -0500

    Quick fix

--- instagram/src/Authentication/Authenticate.js
+++ instagram/src/Authentication/Authenticate.js
@@ -4,9 +4,7 @@ import PostsPage from '../components/PostContainer/PostsPage';
 const Authenticate = Component => {
   return class extends React.Component {
     render() {
-      return <Component page={<PostsPage/>}>
-
-             </Component>;
+      return <Component />
     }
   };
 }
