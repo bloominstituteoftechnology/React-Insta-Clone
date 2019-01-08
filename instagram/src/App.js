fc0cd821b915453f517f8ebcaf10ae27f11875da
ltann <leeroyleeroy18@gmail.com>
2019-01-07 22:08:22 -0800

    Created PostPage and refactor App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,53 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar/SearchBar';
-import dummyData from './dummy-data';
-import PostContainer from './components/PostContainer/PostContainer';
+import PostPage from './components/PostContainer/PostPage';
 
 class App extends Component {
   constructor() {
-    console.log("Constructor Invoked");
     super();
     this.state = {
-      dummyData: [],
-      filteredData: []
-    }
-  }
 
-  componentDidMount() {
-    console.log("CDM Invoked");
-    this.setState({dummyData: dummyData});
-  }
- 
-  handleSearch = event => {
-    let currentData = this.state.dummyData.slice()
-    let matchingData = currentData.filter(user => {
-      if (user.username === event) {
-        return true
-      } else {
-        return false
-      }
-    })
-
-    if (matchingData.length > 0) {
-      this.setState({dummyData : matchingData})
-    } else {
-      prompt("User not found")
     }
   }
 
   render() {
-    console.log("Render Invoked")
     return (
       <div className="App">       
-       <SearchBar search={this.handleSearch} />
-
-       {this.state.dummyData.map(data => (
-         <PostContainer 
-            key={data.timestamp} 
-            data={data} 
-          />
-       ))}   
+       <PostPage /> 
       </div>
     );
   }
---
+++ instagram/src/components/PostContainer/PostPage.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import dummyData from '../../dummy-data';
+import PostContainer from './PostContainer';
+
+class PostPage extends Component {
+  constructor() {
+    console.log("Constructor Invoked");
+    super();
+    this.state = {
+      dummyData: [],
+      filteredData: []
+    }
+  }
+
+  componentDidMount() {
+    console.log("CDM Invoked");
+    this.setState({dummyData: dummyData});
+  }
+ 
+  handleSearch = event => {
+    let currentData = this.state.dummyData.slice()
+    let matchingData = currentData.filter(user => {
+      if (user.username === event) {
+        return true
+      } else {
+        return false
+      }
+    })
+
+    if (matchingData.length > 0) {
+      this.setState({dummyData : matchingData})
+    } else {
+      prompt("User not found")
+    }
+  }
+
+  render() {
+    console.log("Render Invoked")
+    return (
+      <div className="App">       
+       <SearchBar search={this.handleSearch} />
+
+       {this.state.dummyData.map(data => (
+         <PostContainer 
+            key={data.timestamp} 
+            data={data} 
+          />
+       ))}   
+      </div>
+    );
+  }
+}
+
+export default PostPage;
