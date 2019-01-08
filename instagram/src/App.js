5d301eccf142f520639c006c98cb2a8a8994d3a0
ltann <leeroyleeroy18@gmail.com>
2019-01-07 22:39:41 -0800

    Created HOC Authenticate

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import PostPage from './components/PostContainer/PostPage';
+import Authenticate from './Authentication/Authenticate';
 
 class App extends Component {
   constructor() {
@@ -19,4 +20,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Authenticate(App);
