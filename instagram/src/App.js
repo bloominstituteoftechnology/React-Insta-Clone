b88b463ae6963820c157912a3112cd6366b07a4e
combatpacifist <35746304+combatpacifist@users.noreply.github.com>
2019-01-09 07:39:49 -0800

    login and authentication pages added

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import PostsPage from './components/PostContainer/PostsPage'
+import Authenticate from './components/Authentication/Authenticate'
 
 
 class App extends Component {
