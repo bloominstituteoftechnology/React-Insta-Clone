c592dab072871c1e8cc48d1be49bb33e8209a31d
combatpacifist <35746304+combatpacifist@users.noreply.github.com>
2019-01-09 07:09:11 -0800

    added pages and moved app content

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PostsPage from './components/PostContainer/PostsPage'
+
 
 class App extends Component {
+constructor(props){
+  super(props);
+  this.state = {};
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <PostsPage />
       </div>
     );
   }
 }
 
-export default App;
+export default App
