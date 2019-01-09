32fad02b8667cc49a861f3f1271c137e77a4cb9c
Paul DeYonghe <p.deyonghe@gmail.com>
2019-01-08 20:55:04 -0600

    first commit since break

--- instagram/src/components/CommentSection.js
+++ instagram/src/components/CommentSection.js
@@ -4,29 +4,29 @@ import PropTypes from 'prop-types';
 
 class CommentSection extends Component {
     constructor (props) {
-      super (props)
-      this.state = {
-        comment: props.comment
+        super (props)
+        this.state = {
+            comment: props.comment
         }
 }
 
 
     addNewComment = (event, indNumber) => {
-      return(this.setState({event: this.event,
+        return(this.setState({event: this.event,
                                 indNumber: this.indNumber  }))
     }
-  
+
     render = () => {
         console.log("Check: ", this.props )
-      return (
-        <div>
-          <p>
-            <span className="comment_username">{this.props.username}</span>
-            <span className="comUser">{this.state.comment.username} </span>
-            <span>{this.state.comment.text}</span> 
-          </p>
+            return (
+                <div>
+                    <p>
+                        <span className="comment_username">{this.props.username}</span>
+                        <span className="comUser">{this.state.comment.username} </span>
+                        <span>{this.state.comment.text}</span> 
+                    </p>
         </div>
-      )
+        )
     }
 }
 
