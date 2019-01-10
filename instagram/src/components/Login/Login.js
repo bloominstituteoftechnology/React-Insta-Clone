a35d02a5a0446a9dae351d24262eb8b3aa3ae128
ltann <leeroyleeroy18@gmail.com>
2019-01-09 17:44:06 -0800

    finished day 3 MVP

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -3,18 +3,41 @@ import React from 'react';
 class Login extends React.Component {
     constructor() {
         super()
-        this.setState={
+        this.state={
             username : "",
             password : ""
         }
     }
-    
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        const inputName = this.state.username
+        localStorage.setItem('user', inputName)
+        window.location.reload()
+    }
+
     render() {
         return(
             <div>
-                <form>
-                    <input type="text" placeholder="username" />
-                    <input type="password" placeholder="password" />
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text" 
+                        placeholder="username"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="password" 
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
                     <button>Login</button>
                 </form>
             </div>
