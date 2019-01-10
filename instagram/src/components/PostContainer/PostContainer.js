eb3b4e7eb63ce6d38a314bf015449107762e2ab5
Christene Bennett <thechristene@gmail.com>
2019-01-10 15:33:10 -0700

    started refactoring Post Container component to include styled components

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -2,25 +2,47 @@ import React from 'react';
 import './PostContainer.css';
 import CommentSection from '../CommentSection/CommentSection';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+const Post = styled.div`
+  border: 1px solid rgba(0,0,0,0.2);
+  max-width: 650px;
+  width: 100%;
+  margin: 0 auto;
+`
+const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+`
+const ProfilePic = styled.div`
+  border-radius: 50%;
+  overflow: hidden;
+  height: 30px;
+  width: 30px;
+  position: relative;
+  margin: 2%;
+`
+const UserName = styled.div`
+  font-weight: bold;
+`
 
 const PostContainer = props => {
     return (  
       <div className="post-container">{props.data.map((d,i) => (
-        <div key={i} className="post">
-          <div className="user-info">
-            <div className="profile-pic">
+        <Post key={i}>
+          <UserInfo>
+            <ProfilePic>
               <img src={d.thumbnailUrl} alt="" />
-            </div>
-            <div className="username">{d.username}</div>
-          </div>
+            </ProfilePic>
+            <UserName>{d.username}</UserName>
+          </UserInfo>
           <div className="user-post">
             <img src={d.imageUrl} alt="" />
           </div>
           <div className="user-comments">
             <CommentSection comments={d.comments} likes={d.likes} timestamp={d.timestamp}/>
           </div>
-        </div>
+        </Post>
   ))}
       </div>
   )
