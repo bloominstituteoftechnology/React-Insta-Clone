6cf0c8ee0b0c2fda955af5ac2ecfe804c05b9ea9
echopecho <pbsmithtbn@gmail.com>
2019-01-10 21:17:39 -0600

    Refactor the Header to styled-components
    I did a basic refactor of the styling using the styled-components library.  I has not been optimized yet. As I get more familiar with the library, I'll update portions of the code.

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import CommentSection from './../CommentSection/CommentSection';
 import './post.css';
+import styled from 'styled-components';
+
+const Post = styled.div `
+  border: 1px solid rgba(170, 170, 170, 0.637);
+  border-radius: 2px;
+  max-width: 640px;
+  margin-top: 40px;
+`;
+
+const PostHeader = styled.div `
+  display: flex;
+  align-items: center;
+`;
+
+const PostImage = styled.img `
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  padding: 0 10px;
+`;
+
+const LikesContainer = styled.div `
+  padding: 10px 10px 0;
+`;
+
+const LikesImage = styled.img `
+  padding-right: 10px;
+`;
+
+
 
 const PostContainer = props => {
   return (
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,23 +1,74 @@
 import React from 'react';
-import './search.css';
+// import './search.css';
+import styled from 'styled-components';
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 0;
+  width: 100%
+  border-bottom: 1px solid rgba(170, 170, 170, 0.637);
+`;
+
+const LogoHeader = styled.div`
+
+`;
+
+const LogoImage = styled.img`
+  padding: 0 15px 10px;
+`;
+
+const LogoInsta = styled(LogoImage)`
+  height: 46px;
+  width: auto;
+  border-bottom: 0;
+  border-left: 1px solid rgba(170, 170, 170, 0.637);
+`;
+
+const SearchForm = styled.form`
+  padding-top: 10px;
+`;
+
+const SearchInput = styled.input`
+  border-radius: 2px;
+  border: 1px solid rgba(170, 170, 170, 0.637);
+  text-align: center;
+  height: 24px;
+  width: 200px;
+  background-color: #ddd;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const NavHeader = styled.div `
+  padding-top: 10px;
+`;
+
+const NavImage = styled.img `
+  padding-left: 10px;
+  height: 24px;
+  width: 24px;
+`;
 
 
 const SearchBar = props => {
   return (
-    <div className="nav-bar">
-      <div className="logo">
-        <img src={require('./../images/instagram-logo.png')} alt="" />
-        <img onClick={props.clearSearch} src={require('./../images/instagram-font.png')} className="instagram-font" alt="" />
-      </div>
-      <form onSubmit={props.search}>
-        <input onChange={props.changeText} type="text" value={props.value} name="searchText" placeholder="Search"></input>
-      </form>
-      <div className="nav-items">
-        <img src={require('./../images/compass.png')} alt="" />
-        <img src={require('./../images/favourite-heart-lrg.png')} alt="" />
-        <img src={require('./../images/profile.png')} alt="" />
-      </div>
-    </div>
+    <Header className="nav-bar">
+      <LogoHeader>
+        <LogoImage src={require('./../images/instagram-logo.png')}></LogoImage>
+        <LogoInsta onClick={props.clearSearch} src={require('./../images/instagram-font.png')}></LogoInsta>
+      </LogoHeader>
+      <SearchForm onSubmit={props.search}>
+        <SearchInput onChange={props.changeText} type="text" value={props.value} name="searchText" placeholder="Search"></SearchInput>
+      </SearchForm>
+      <NavHeader>
+        <NavImage src={require('./../images/compass.png')}></NavImage>
+        <NavImage src={require('./../images/favourite-heart-lrg.png')}></NavImage>
+        <NavImage src={require('./../images/profile.png')}></NavImage>
+      </NavHeader>
+    </Header>
   )
 }
 
