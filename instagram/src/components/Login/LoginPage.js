8ed1988e38873f8ba35f8dcd0587bf133b5eb124
Jonathan Dorety <kyullog@gmail.com>
2019-01-10 22:55:12 -0500

    Basic styled component implementation for Login component

--- instagram/src/components/Login/LoginPage.js
+++ instagram/src/components/Login/LoginPage.js
@@ -1,25 +1,48 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Input } from "reactstrap";
+// import { Input } from "reactstrap";
+import styled from "styled-components";
 import "./Login.css";
+
+const InputField = styled.input`
+  max-width: 600px;
+  width: 100%;
+  font-size: 2.5rem;
+  margin: 10px auto;
+`;
+
+const SubmitButton = styled.button`
+  max-width: 250px;
+  width: 100%;
+  font-size: 2.5rem;
+`;
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 600px;
+  width: 100%;
+  margin: 0 auto;
+`;
 
 const LoginPage = props => {
   return (
     <div className="login-page">
-      <form className="login-form" name="login" onSubmit={props.onSubmit}>
-        <Input
+      <LoginForm className="login-form" name="login" onSubmit={props.onSubmit}>
+        <InputField
           placeholder="username"
           value={props.userName}
           onChange={props.nameInput}
         />
-        <Input
+        <InputField
           type="password"
           placeholder="password"
           value={props.password}
           onChange={props.passInput}
         />
-        <button type="submit">Submit</button>
-      </form>
+        <SubmitButton type="submit">Submit</SubmitButton>
+      </LoginForm>
     </div>
   );
 };
