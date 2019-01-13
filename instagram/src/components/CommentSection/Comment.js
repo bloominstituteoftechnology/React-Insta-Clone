6252d018986804475b03978de63b2ac527ad698b
Trip1701 <bj2336@email.vccs.edu>
2019-01-13 15:09:38 -0500

    like comment feature added

--- instagram/src/components/CommentSection/Comment.js
+++ instagram/src/components/CommentSection/Comment.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Comment = props => {
-  return(
-    <p><strong>{props.user}</strong> {props.text} <span onClick={props.deleteHandler}>&times;</span></p>
-  );
+import heart from '../../img/insta-heart.png';
+import liked from '../../img/insta-heart-liked.png';
+
+class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      liked: false,
+    }
+  }
+
+  likeComment = event => {
+    this.setState((prevState) => {
+          return {liked: !prevState.liked};
+      });
+  }
+
+  render() {
+    let likedIcon;
+
+    if(this.state.liked) {
+      likedIcon = liked;
+    } else {
+      likedIcon = heart;
+    }
+
+    return(
+      <p><strong>{this.props.user}</strong> {this.props.text} <span className="comment-like"><img src={likedIcon} onClick={this.likeComment} alt="insta-heart" /></span></p>
+    );
+  }
 }
 
 Comment.propTypes = {
