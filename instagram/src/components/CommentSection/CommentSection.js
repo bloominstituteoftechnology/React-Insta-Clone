4ede22cefb2aa0955019f3af9283b47226d75c91
Matt Basile <matthewbasile2@gmail.com>
2019-01-14 16:11:08 -0500

    Basic Layout and data rendering. Need to style and add props testing.

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -4,9 +4,15 @@ const CommentSection = props =>{
     console.log(props.comments);
     return (
       <div >
+          <div>
+              <button>Heart</button>
+              <button>Comment</button>
+          </div>
+          <p>{373} likes</p>
           {props.comments.map((comment)=>{
               return <p>{comment.username} {comment.text}</p>
           })}
+          <p className="time-stamp">2 HOURS AGO</p>
           <input placeholder="Add a comment..."/>
         
 
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -3,12 +3,17 @@ import CommentSection from '../CommentSection/CommentSection';
 import '../../App.css'
 
 const PostContainer = props =>{
-    console.log(props.post.comments);
+    console.log(props.post.imageUrl);
     return (
       <div className="post">
-        
-        <CommentSection comments={props.post.comments} /> 
-    
+        <div className="post-header">
+            <img src={props.post.thumbnailUrl}/>
+            <p>{props.post.username}</p>
+        </div>
+        <div className="post-img">
+            <img src={props.post.imageUrl} alt=""/>
+        </div>
+            <CommentSection comments={props.post.comments} /> 
       </div>
     );
 }
