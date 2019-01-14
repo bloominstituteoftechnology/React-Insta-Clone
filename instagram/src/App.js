5ca509579251ae44c34f82a0a07a1ab424a35894
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-14 13:40:56 -0700

    post container set up, rendering for each post

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
+
+import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.postData = dummyData;
+    this.posts = this.postData.map(post => {
+      return <PostContainer post={post} />
+    });
+  }
+
   render() {
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        hello
+        {this.posts}
       </div>
     );
   }
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,3 +1,16 @@
 import React from 'react';
 
 import './PostContainer.css';
+
+function PostContainer(props) {
+
+  return (
+    <div className="post-container">
+    here is a post:
+      {props.post.username}
+    </div>
+  );
+}
+
+
+export default PostContainer;
