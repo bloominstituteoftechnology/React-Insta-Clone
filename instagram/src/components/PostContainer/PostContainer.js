a80ff503f3e0030670c337056d4b7eccacedbe3f
amadison17 <angelmadison2015@gmail.com>
2019-01-14 00:24:30 -0500

    added props

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,13 @@
+
+
+function PostContainer(props) {
+    return(
+        <div>
+            {props.dummyData.map(data
+                <dummyData key = {data.username} data={username} />
+            )}
+        </div>
+    )
+}
+
+export default PostContainer;
