48e9b0925620ec2b25f5cfd877e7ac2d8d1ae320
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-01-14 21:55:25 -0500

    Fixed Logo

--- src/components/NavBar/Logo.js
+++ src/components/NavBar/Logo.js
@@ -3,11 +3,16 @@ import React from "react";
 const Logo = props => {
   return (
     <div className="logo">
-      <img alt="Instagram Clone | Hamza Elkhoudiri" src="logo.png" />
-
-      <h1 className="logo-text" aria-label="Instagram">
+      <img src="logo.instagram.png" alt="Hamza Elkhoudiri | Insta Clone" />
+      {/* <img alt="Instagram Clone | Hamza Elkhoudiri" src="logo.png" />
+      <img
+        className="text-logo"
+        alt="Instagram Clone | Hamza Elkhoudiri"
+        src="instagram-new-logo.png"
+      /> */}
+      {/* <h1 className="logo-text" aria-label="Instagram">
         Instagram
-      </h1>
+      </h1> */}
     </div>
   );
 };
