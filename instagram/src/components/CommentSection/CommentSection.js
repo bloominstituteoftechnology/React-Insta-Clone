1f6e6ced9db7c3e59d40592aa9bb5aab671ea185
yannafaith <yannafaith97@gmail.com>
2019-01-15 03:16:20 -0700

    added searchbar and comment section icons

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import '../CommentSection/CommentSection.css'
+import speechbub from './speechbub.png';
+import instaheart from './instaheart.png';
 
 function CommentSection(props) {
     return (
         <div className='comment-container'>
             <div className='comment-icons'>
-                <img alt='heart'/>
-                <img alt='bubble'/>
+                <div className='comment-heart'>
+                    <img src={instaheart} alt='heart'/>
+                </div>
+                <div className='comment-bub'>
+                    <img src={speechbub} alt='bubble'/>
+                </div>
             </div>
+
             <p> <span> {props.data.likes} likes </span> </p>
             <p> 
                 <span> {props.data.comments[0].username} </span>  {props.data.comments[0].text}
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import './SearchBar.css'
+import gramhead from './gramhead.png';
+import gramlogo from './gramlogo.png';
+import instaheart from '../CommentSection/instaheart.png';
+import instaloco from './instaloco.png';
+import instaperson from './instaperson.png';
 
 function SearchBar(props) {
     return (
         <div className='search-container'> 
-            <div>
-                <img alt='logo' src='./SearchBar/gramhead.png'/>
-                <img alt='title' src='./gramhead.png'/>
+            <div className='search-imgs'>
+                <div className='title'>
+                    <img src={gramhead}/>
+                </div>
+                <p>|</p>
+                <div className='logo'>
+                    <img src={gramlogo}/>
+                </div>
             </div>
 
             <form>
                 <input placeholder='Search'/>
             </form>
 
-            <div>
-                <img alt='heart' src=''/>
-                <img alt='location' src=''/>
-                <img alt='person' src=''/>
+            <div className='search-icons'>
+                <img alt='heart' src={instaheart}/>
+                <img alt='location' src={instaloco}/>
+                <img alt='person' src={instaperson}/>
             </div>
 
         </div>
