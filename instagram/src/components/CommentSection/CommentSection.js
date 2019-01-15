b163ee870c65d9badf96af99ae7023f100b9dd02
Zooheck <davidscottflack@gmail.com>
2019-01-15 17:56:51 -0600

    Add toggle like functionality

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Comment from './Comment'
-import PostContainer from '../PostContainer/PostContainer'
+
 class CommentSection extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       comments: [],
-      currentComment: null,
+      currentComment: '',
+      commentNumber: 0,
       likes: 0,
-      commentNumber: 0
+      isLiked: false,
     }
   }
   componentDidMount = () => {
     this.setState({
       comments: this.props.comments,
       currentComment: '',
-      commentNumber: this.props.comments.length
+      commentNumber: this.props.comments.length,
+      likes: this.props.likes,
+      isLiked: true
     })
   }
   handleChange = (event) => {
@@ -24,6 +27,27 @@ class CommentSection extends React.Component {
     this.setState({
       [name]: value,
     })
+  }
+  toggleLike = () => {
+    this.setState(previousState => { 
+      return {
+        isLiked: !previousState.isLiked,
+      }
+    })
+    {this.state.isLiked ? 
+    this.setState(previousState => {
+      return {
+        likes: previousState.likes +=1,
+      }
+    })
+    :
+    this.setState(previousState => {
+      return {
+        likes: previousState.likes -=1,
+      }
+    })
+    }
+    console.log(this.state.isLiked)
   }
   addComment = (event) => {
     event.preventDefault()
@@ -40,12 +64,12 @@ class CommentSection extends React.Component {
       <div>
         <div className="likes-comments">
           <div className="likes-comments-icons">
-          
-          
+            <i className="far fa-heart" onClick={this.toggleLike}></i>
+            <i className="far fa-comment"></i>
           </div>
           <div className="likes-comments-stats">
-            <p>{this.state.commentNumber} comments</p>
             <p>{this.state.likes} likes</p>
+            <p>{this.state.commentNumber} comments</p>
           </div>
         
         </div>
@@ -59,7 +83,7 @@ class CommentSection extends React.Component {
           value={this.state.currentComment}
           onChange={this.handleChange}
           onSubmit={this.addComment}
-          isRequired={true}/>
+          />
           <button onClick={this.addComment} disabled={this.state.currentComment === '' ? true : false}>Submit</button>
         </form>
 
