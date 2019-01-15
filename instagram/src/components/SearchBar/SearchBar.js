3a351cc64fdf234c1540456f8a5c9499707b2abd
Zooheck <davidscottflack@gmail.com>
2019-01-14 18:02:11 -0600

    Add searchbar input and action icons

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -8,6 +8,12 @@ const SearchBar = props => {
           <i className="fas fa-camera fa-3x"></i>
           <h1>Instagram</h1>      
         </div>
+        <input type="text" />
+        <div className="search-actions">
+          <i className="far fa-compass"></i>
+          <i className="far fa-heart"></i>
+          <i className="far fa-user"></i>
+        </div>
       </div>
 
 
