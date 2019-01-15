94fa5b66ca30791d54d46e0cd70adb8d5fadbfe0
Zooheck <davidscottflack@gmail.com>
2019-01-15 15:36:04 -0600

    Add comment functionality

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,15 +1,57 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Comment from './Comment'
-const CommentSection = props => {
-  return (
-    <div>
-      {props.comments.map(comment => {
-        return <Comment username={comment.username} text={comment.text}/>
-      })}
-    </div>
-    
-  )
+import { DEFAULT_ECDH_CURVE } from 'tls';
+class CommentSection extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      comments: [],
+      currentComment: null,
+    }
+  }
+  componentDidMount = () => {
+    this.setState({
+      comments: this.props.comments,
+      currentComment: ''
+    })
+  }
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value,
+    })
+  }
+  addComment = (event) => {
+    event.preventDefault()
+    const newCommentList = [...this.state.comments, {username: 'DEFAULT', text: this.state.currentComment}]
+    this.setState({
+      comments: newCommentList,
+      currentComment: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.comments.map(comment => {
+          return <Comment username={comment.username} text={comment.text}/>
+        })}
+        <form>
+          <input 
+          type="text"
+          name="currentComment"
+          value={this.state.currentComment}
+          onChange={this.handleChange}
+          onSubmit={this.addComment}/>
+          <button onClick={this.addComment}>Submit</button>
+        </form>
+
+      </div>
+      
+    )
+  }
+
 }
 
 CommentSection.propTypes = {
