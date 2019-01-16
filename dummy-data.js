f03eab6f2bdd9248c77243e49bfdc6fe305e68f0
joepound <14357797+joepound@users.noreply.github.com>
2019-01-15 18:06:07 -0700

    Update dummy-data.js

--- dummy-data.js
+++ dummy-data.js
@@ -1,78 +1,115 @@
 const dummyData = [
   {
+    _id: "15475942200556282393",
     username: "philzcoffee",
-    thumbnailUrl:
-      "https://tk-assets.lambdaschool.com/ecd33d34-c124-4b75-92d2-e5c52c171ed8_11201517_887808411287357_1307163552_a.jpg",
 
+    /* Boo hoo! My profile thumbnail URL was removed for demo purposes (defaultProps)! */
+    
     imageUrl:
       "https://tk-assets.lambdaschool.com/69cf901b-f96d-466e-a745-ff2a01effac9_philz-image.jpg",
-    likes: 400,
+    likes: [
+      "biancasaurus",
+      "martinseludo",
+      "twitch",
+      "michaelmarzetta",
+      "themexican_leprechaun",
+      "dennis_futbol",
+    ],
     timestamp: "July 17th 2017, 12:42:40 pm",
     comments: [
       {
+        _id: "154759444904864946",
         username: "philzcoffee",
         text:
           "We've got more than just delicious coffees to offer at our shops!"
       },
       {
+        _id: "15475944494151575420",
         username: "biancasaurus",
         text: "Looks delicious!"
       },
       {
+        _id: "15475944498317961871",
         username: "martinseludo",
         text: "Can't wait to try it!"
       }
     ]
   },
   {
+    _id: "15475942397929379511",
     username: "fortnite",
     thumbnailUrl:
       "https://tk-assets.lambdaschool.com/ce601fdf-7cb0-4098-83d3-1a1584a72513_30841289_342445456281079_112845064497004544_n.jpg",
     imageUrl:
       "https://tk-assets.lambdaschool.com/89d13918-b7a2-4b40-9658-f376ea3f6b59_37131538_213683546146400_1083714364399157248_n.jpg",
-    likes: 4307,
+    likes: [
+      "philzcoffee",
+      "biancasaurus",
+      "martinseludo",
+      "twitch",
+      "michaelmarzetta",
+      "themexican_leprechaun",
+      "dennis_futbol",
+      "playhearthstone",
+      "awaywetravel",
+      "awesomebt28"
+    ],
     timestamp: "July 15th 2017, 03:12:09 pm",
     comments: [
       {
+        _id: "15475943895587357794",
         username: "twitch",
         text: "Epic Street Fighter action here in Las Vegas at #EVO2017!"
       },
       {
+        _id: "15475943987635881808",
         username: "michaelmarzetta",
         text: "Omg that match was crazy"
       },
       {
+        _id: "15475944080539286962",
         username: "themexican_leprechaun",
         text: "What a setup"
       },
       {
+        _id: "1547594418175902893",
         username: "dennis_futbol",
         text: "It that injustice"
       },
       {
+        _id: "1547594434538343681",
         username: "dennis_futbol",
         text: "Is"
       }
     ]
   },
   {
+    _id: "15475942642193337551",
     username: "playhearthstone",
     thumbnailUrl:
       "https://tk-assets.lambdaschool.com/c432f179-8bd7-4758-959d-d88a21f96bca_37105899_432228420518610_5035444036064772096_n.jpg",
     imageUrl:
       "https://tk-assets.lambdaschool.com/43bf01f9-319c-469d-8cf5-0120fe1007f1_yosemite.jpg",
-    likes: 5306,
+    likes: [
+      "playhearthstone",
+      "awaywetravel",
+      "fortnite",
+      "awesomebt28"
+    ],
     timestamp: "July 14th 2017, 10:04:08 am",
     comments: [
       {
+        _id: "15475943316226872124",
         username: "playhearthstone",
         text: "Love this shot!"
       },
       {
+        _id: "15475943345972035596",
         username: "awaywetravel",
         text: "Yosemite is my most favorite place in the universe"
       },
       {
+        _id: "15475943510794241041",
         username: "awesomebt28",
         text: "I like how Half Dome looks so old and useless"
       }
