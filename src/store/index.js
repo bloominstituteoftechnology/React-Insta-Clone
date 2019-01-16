33847ff67ae6f3559a6c1137cf5d98a1c28d6f87
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-01-16 18:46:17 -0500

    Finished login page and functionality backend and frent end

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,8 @@ import thunk from "redux-thunk";
 import rootReducer from "../reducers";
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, compose(applyMiddleware(...middleware)));
 
 export default store;
+
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
