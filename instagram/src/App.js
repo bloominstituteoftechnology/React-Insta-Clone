e894beb6edcd26d147bd92cab2dcdee231dd7df9
yannafaith <yannafaith97@gmail.com>
2019-01-16 04:13:20 -0700

    added Authenticate HOC

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PostsPage from './components/PostContainer/PostsPage';
+import Authenticate from './components/Authentication/Authenticate';
 
 class App extends Component {
   constructor() {
@@ -12,6 +13,7 @@ class App extends Component {
   render () {
     return (
       <div>
+        <Authenticate App={<App/>}/>
         <PostsPage />
       </div>
     )
