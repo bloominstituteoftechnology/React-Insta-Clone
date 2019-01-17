a6f523f6c487859399836502122b6abdea91f067
byronholmes2018 <lshopeful2018@mgmail.com>
2019-01-17 17:41:37 -0600

    Created a Header styled-component that wraps entire search bar.

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -5,16 +5,19 @@ const SearchBarContainer = styled.div``;
 const SearchForm = styled.form``;
 const SearchInput = styled.input``;
 const SubmitSearch = styled.input``;
-
+const SearchHeader = styled.header``;
 
 const SearchBar= props =>{
   return(
-    <SearchBarContainer>
-      <StyledForm onSubmit = {(e)=>props.submitFunction(e)}>
-          <SearchInput onChange = {(e)=>props.handleChange(e)} type = "text" value = {props.fieldValue} />
-          <SubmitSearch type ="submit" value = "Submit"/>
-        </StyledForm>
-    </SearchBarContainer>
+    <SearchHeader>
+      <SearchBarContainer>
+        <StyledForm onSubmit = {(e)=>props.submitFunction(e)}>
+            <SearchInput onChange = {(e)=>props.handleChange(e)} type = "text" value = {props.fieldValue} />
+            <SubmitSearch type ="submit" value = "Submit"/>
+          </StyledForm>
+      </SearchBarContainer>
+    </SearchHeader
+
   );
 }
 
