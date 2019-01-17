25105acb4757df69d13645c5bc2ea5c87fee4f93
yannafaith <yannafaith97@gmail.com>
2019-01-17 15:02:12 -0700

    refactored css to use styled components - Post Container

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import './PostContainer.css'
 import CommentSection from '../CommentSection/CommentSection';
+import styled from 'styled-components';
 
+const Post = styled.div`{
+    border: solid gray 3px;
+    width: 640px;
+    margin-top: 20px;
+    margin-left: 25%;
+}`;
 
+const PostHead = styled.div`{
+    height: 50px;
+    width: 200px;
+    display: flex;
+    justify-content: space-evenly;
+    border: solid red 2px;
+}`;
+
+const PostImg = styled.div`{
+    border-radius: 30px;
+    width: 40px;
+    height: 40px;
+    align-self: center;
+}`
 
 const PostContainer = (props) => {
     return (
-        <div className='post-container'>
-            <div className='post-head'>
-                <img src={props.data.thumbnailUrl} />
+        <Post>
+            <PostHead>
+                <img id='post' src={props.data.thumbnailUrl} />
                 <p> <span>{props.data.username}</span></p>
-            </div>
-            <div className='post-img'>
+            </PostHead>
                 <img src={props.data.imageUrl}/> 
-            </div>
             <CommentSection data={props.data}/>
-        </div>
+        </Post>
     );
 }
 
