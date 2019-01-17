41d8b43baf987179b2c77efa143ea8b54f85f02f
byronholmes2018 <lshopeful2018@mgmail.com>
2019-01-17 03:32:20 -0600

    Cleaned up code. Made spacing more uniform, removed commented-out test functions, removed console log statements (which were for testing purposes only)

--- instagram/src/components/Authentication/Authenticate.js
+++ instagram/src/components/Authentication/Authenticate.js
@@ -1,24 +1,41 @@
 import React from 'react'
+
 import Login from '../Login/Login.js';
 
 const Authenticate = (App) =>
+
   class extends React.Component{
+
     constructor(){
+
       super();
+
       this.state = {
+
         loggedIn: false
-      }
-    }
-    componentDidMount(){
-      this.setState({loggedIn: localStorage.getItem("username") && localStorage.getItem("password")})
-    }
-    render(){
-      if(this.state.loggedIn){return <App />;}
-      else{
-        return <Login />
+
       }
 
     }
+
+    componentDidMount(){
+
+      this.setState({loggedIn: localStorage.getItem("username") && localStorage.getItem("password")})
+
+    }
+
+    render(){
+
+      if(this.state.loggedIn){return <App />;}
+
+      else{
+
+        return <Login />
+
+      }
+
+    }
+
   }
 
 export default Authenticate;
--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,96 +1,120 @@
 import React, { Component } from 'react';
+
 import './CommentSection.css';
 
-/*const CommentSection = props =>{
-    const commentList = props.comments.map(comment =>{
-      return <li>
+//newComment holds comment that is made to one of the instagram posts; count is the numbrer of likes
 
+//for a given post
 
-              <strong>{comment.username}</strong> <p>{comment.text}</p>
-
-
-
-             </li>
-    });
-
-
-  return(<ul>{commentList}</ul>);
-}*/
 let newComment;
+
 let count = 0;
 
 class CommentSection extends Component{
+
   constructor(props){
+
     super(props);
+
     this.state = {
+
       currentComment: "",
+
       comments: [...props.comments],
+
       count:count,
+
       thisUserClicked: false
+
     }
+
   }
 
   getNewComment = (event, index)=>{
+
     newComment = event.target.value;
+
     console.log(newComment)
+
     console.log(this.state.comments)
+
     this.setState(prevState => ({currentComment: newComment}))
 
-    /*this.setState(prevState=>({
-      comments: [...prevState.comments, event.target.value]
-    }))*/
+
   }
+
   addNewComment = (e)=>{
 
     e.preventDefault();
+
     console.log('new comment is ' + newComment)
+
     this.setState(prevState =>({
+
       comments: [{username: 'User', text: newComment},...prevState.comments]
+
     }), ()=>{this.setState(prevState =>({currentComment: ""}))})
-    //newComment = ""
+
   }
 
   addLike = (e) =>{
+
     if(!this.state.thisUserClicked){
+
     count+=1;
+
     this.setState(prevState => ({count: count, thisUserClicked: true}));}
+
     else{
+
       count -=1;
+
       this.setState(prevState =>({count: count, thisUserClicked: false}))
+
     }
 
   }
 
   componentWillReceiveProps(nextProps){
+
     this.setState(nextProps)
+
   }
 
   render(){
-    console.log(this.state.comments)
+
     return(
 
       <div className = "comment-section">
+
       <div className = "post-reactions"><i  onClick = {()=>{this.addLike()}} className="heart-size fa">&#xf08a;<span>{this.state.count}</span></i></div>
 
         <ul>
 
         {this.state.comments.map(comment =>{
-          return <li>
 
+          return <li>
 
                   <strong>{comment.username}</strong> <p>{comment.text}</p>
 
-
-
                  </li>})}
+
         </ul>
+
         <form onSubmit = {this.addNewComment}>
+
           <input onChange = {this.getNewComment} type = "text" name = "comment" value = {this.state.currentComment} />
+
           <input type = "submit" value = "Submit " />
+
         </form>
+
       </div>
+
     );
+
   };
+
 }
 
 export default CommentSection;
