1d5982835d88d465aec279e8c5c4967c52d4f9ce
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-01-17 12:56:33 -0500

    Fixed my login page bug WooHoo

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,7 @@ import thunk from "redux-thunk";
 import rootReducer from "../reducers";
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, compose(applyMiddleware(...middleware)));
 
 export default store;
 
