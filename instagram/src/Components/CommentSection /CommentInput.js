5ec0cf1bee7488badc474360d09eebdc83b91f89
mustafa inaya <moose.inaya@icloud.com>
2019-01-17 20:38:36 -0800

    SET,Setting The Input Comment Section

--- instagram/src/Components/CommentSection /CommentInput.js
+++ instagram/src/Components/CommentSection /CommentInput.js
@@ -1,10 +1,10 @@
-// import React from 'react';
+import React from 'react';
 
-// const CommentInput = (props) => {
-// 	return (
-// 		<form>
-// 			<input value={props.comment} onChange={props.changeComment} />
-// 		</form>
-// 	);
-// };
-// export default CommentInput;
+const CommentInput = (props) => {
+	return (
+		<form>
+			<input value={props.comment} onChange={props.changeComment} />
+		</form>
+	);
+};
+export default CommentInput;
