e45bfbc3865e6ef6a3d6a27b3511e5142d09cf86
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-01-19 18:37:11 -0500

    Finished up the stories side

--- src/components/Home/Stories.js
+++ src/components/Home/Stories.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { connect } from "react-redux";
 
 const Stories = props => {
+  console.log(props.posts);
   return (
     <>
       <div className="stories">
@@ -8,17 +10,55 @@ const Stories = props => {
           <small>Stories</small>
           <h1>Watch All</h1>
         </div>
-        <div className="stories-content" />
+        <div className="stories-content">
+          {props.posts.map(post => {
+            return (
+              <div key={post._id} className="story">
+                <img
+                  src={post.imageUrl}
+                  alt="Hamza Elkhoudiri"
+                  className="rightImg"
+                />
+                <span className="sub_item">
+                  <h4>{post.username}</h4>
+                  <p>{post.timestamp}</p>
+                </span>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div className="suggestions">
         <div className="suggestions-header">
           <small>Suggestions For You</small>
           <h1>See All</h1>
         </div>
-        <div className="suggestions-content" />
+        <div className="suggestions-content">
+          {props.posts.map(post => {
+            return (
+              <div key={post._id} className="story">
+                <img
+                  src={post.thumbnailUrl}
+                  alt="Hamza Elkhoudiri"
+                  className="rightImg"
+                />
+                <span className="sub_item">
+                  <h4>{post.username}</h4>
+                </span>
+                <span>
+                  <a href="#">Follow</a>
+                </span>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
 };
 
-export default Stories;
+const mapStateToProps = state => ({
+  posts: state.Insta.data
+});
+
+export default connect(mapStateToProps)(Stories);
