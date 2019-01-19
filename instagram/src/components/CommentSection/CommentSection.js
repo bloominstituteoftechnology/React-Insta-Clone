c629d54557fd5891c8303c7afc9ff2897d79ed8b
thomasutsey <tomutsey@yahoo.com>
2019-01-19 11:27:02 -0500

    Implement comment section functionality
    Add state object to receive new input
    
    add onsubmit event listener to attach new input to existing comments
    
    use lambda as placeholder for username until functionality is implemented further in project.

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PostContainer from './../PostContainer/PostContainer'
+import PostContainer from './../PostContainer/PostContainer';
 import PropTypes from 'prop-types';
 
 class CommentSection extends React.Component{
   constructor(props){
     super();
-<<<<<<< HEAD
+
  this.state = {
    comment:'',
    comments:props.comm
   }
   this.onFormSubmit = (e) =>  {
     e.preventDefault();
-    let newinfo = {... this.state.comments, username: this.state.comment.username, text: this.state.comment};
-    this.setState.comment = {username: this.newinfo.username, text: this.newninfo.text}
-    console.log (newinfo);
-  
-=======
- this.state = {comment:''}
- this.onFormSubmit = (e) =>  {
-  e.preventDefault();
-  // this.setState.value
->>>>>>> 2cfa852af673265e1d824cba5c2b7a0bf182fbe7
-  //some action to render comment to screen ???????????
-}
+    // let newinfo = {, username: this.state.comment.username, text: this.state.comment};
+    this.setState({
+      comments:[
+        ... this.state.comments,
+        { username: 'Lambda', text: this.state.comment}
+      ],
+      comment: ''
+    });
+    e.target.reset();
+    console.log (this.state.comment);
   }
+}
 render() {
   return (
    
     <div>
-      {this.state.comment.map( (e) => {
+      {this.state.comments.map( (e) => {
         return( <div key={`${e.username}${e.text}`}><div>{e.username}</div>
           <div>{e.text}</div></div>
           )
@@ -45,10 +44,11 @@ render() {
       </div>
     </div>
    
-  );
+  )
 }
+
   }
- 
+   
 CommentSection.propTypes = {
   comm: PropTypes.arrayOf(PropTypes.shape(
     {
@@ -57,4 +57,5 @@ CommentSection.propTypes = {
     }
   ))
   }
+
 export default CommentSection;
