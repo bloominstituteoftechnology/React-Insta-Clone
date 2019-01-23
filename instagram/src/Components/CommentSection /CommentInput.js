e0388b499a8c19d60864a3b9599759d4109277cf
mustafa inaya <moose.inaya@icloud.com>
2019-01-22 16:45:30 -0800

    DEBUG

--- instagram/src/Components/CommentSection /CommentInput.js
+++ instagram/src/Components/CommentSection /CommentInput.js
@@ -13,7 +13,7 @@ import React from 'react';
 
 const CommentInput = (props) => {
 	return (
-		<form onSubmit={props.submitComment}>
+		<form onSubmit={props.commentSubmit}>
 			<input type="text" value={props.comment} placeholder="Add here" onChange={props.changeComment} />
 		</form>
 	);
