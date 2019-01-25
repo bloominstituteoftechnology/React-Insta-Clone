d39349ec3a82968c2bb72d659e6049e3f20c3032
Paul DeYonghe <p.deyonghe@gmail.com>
2019-01-24 20:19:50 -0600

    still working on it

--- instagram/src/components/CommentsSection/CommentSection.js
+++ instagram/src/components/CommentsSection/CommentSection.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import '../../App.css';
 import Comment from './Comment.js'
+import CommentInput from './CommentInput'
 /*import PropTypes from 'prop-types';*/
 
 class CommentSection extends Component {
     constructor (props) {
         super (props)
         this.state = {
-            comment: props.comment
+            comments: props.comments,
+            comment: '',
         }
+}
+
+componentDidMount() {
+    const id = this.props.postId;
+    if (localStorage.getItem(id)) {
+        this.setState({
+            comments: JSON.parse(localStorage.getItem(this.props.id))
+        })
+    } else {
+        this.setComments();
+    }
+}
+
+componentWillUnmount() {
+    this.setComments();
+}
+
+setComments = () => {
+    localStorage.setItem(
+        this.props.postId,
+        JSON.stringify(this.state.comments)
+    )
+}
+
+commentHandler = ev => {
+    this.setState({ comment: ev.target.value })
 }
 
 
