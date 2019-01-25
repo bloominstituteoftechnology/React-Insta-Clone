4d5a74fa1fa7966745ccc82a81522923208045dd
thomasutsey <tomutsey@yahoo.com>
2019-01-24 23:21:04 -0500

    convert SearchBar to function
    Begin coding a way for app.js to send the input value as a prop to searchbar
    
    Use if statement to determing if state for filtered data is true or false in order to determine the data rendered.

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -2,54 +2,62 @@ import React from 'react';
 import './SearchBar.css';
 import App from './../../App'
 /*At the end of Day I there will be a single instance of the Search Bar being rendered at the top of the page */
-class SearchBar extends React.Component{
-    constructor(props){
-        super(props)
-    this.state = {
-        query: '',
-        Data: []
+
+
+const SearchBar = (props) => {
+  return {
+    
+  }
+
+}
+// class SearchBar extends React.Component{
+//     constructor(props){
+//         super(props)
+//     this.state = {
+//         query: '',
+//         Data: []
         
-      }
+//       }
      
-     this.handleInputChange = (e) => {
-         e.preventDefault();
-        this.setState({
-          query: this.search.value,
-          Data: (this.props.content.map((e)=>{
-            return (e.username)
-          }))
-          })
+//      this.handleInputChange = (e) => {
+//          e.preventDefault();
+//         this.setState({
+//           query: this.search.value,
+//           Data: (this.props.content.map((e)=>{
+//             return (e.username)
+//           }))
+//           })
         
-      }
-    this.handleOnSubmit = (e) => {
-        e.preventDefault();
-    }
+//       }
+//     this.handleOnSubmit = (e) => {
+//         e.preventDefault();
+//     }
   
     
     
     
-    }
+//     }
 
      
     
     
-      render() {
-          console.log(this.state.Data)
-        return (
-          <form onSubmit={this.handleOnSubmit}>
-            <input
-              placeholder="Search"
-              ref={input => this.search = input}
-              onChange={this.handleInputChange}
-            />
+//       render() {
+//           console.log(this.state.Data)
+//         return (
+//           <form onSubmit={this.handleOnSubmit}>
+//             <input
+//               placeholder="Search"
+//               ref={input => this.search = input}
+//               onChange={this.handleInputChange}
+//             />
            
-            <p>{(this.state.Data.filter((term)=>{return((term === this.search.value))
+//             <p>{(this.state.Data.filter((term)=>{return((term === this.search.value))
 
-            }
-            ))}</p>
-          </form>
-        )
-      }
-}
-export default SearchBar 
+//             }
+//             ))}</p>
+//           </form>
+//         )
+//       }
+// }
+// export default SearchBar 
 
