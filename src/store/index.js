ea35ff78beadc1f251bd97b88a807d4a03ce2a2b
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-01-27 18:04:31 -0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers";
+
 const middleware = [thunk];
 
 const store = createStore(rootReducer, compose(applyMiddleware(...middleware)));
