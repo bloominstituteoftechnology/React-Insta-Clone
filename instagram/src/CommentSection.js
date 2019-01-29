55b67746c8f793c048a56d89ec1a611f16efdef0
Melvin Merlos <melvinamerlos@gmail.com>
2019-01-28 21:14:39 -0500

    update 6: more components, bug fixes

--- instagram/src/CommentSection.js
+++ instagram/src/CommentSection.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Comment from "./Comment";
 import PropTypes from 'prop-types';
+import CommentInput from './CommentInput';
 
 const CommentSection = props => {
     return (
       <div className="commentSection">
-        <input className="input" type="text" placeholder="Write a comment"/>
+        <CommentInput />
           {props.dummyData.comments.map(commenters => {
             return (
               <Comment key={commenters.text.length} commentData={commenters}/>
