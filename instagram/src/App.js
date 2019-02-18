f062c4645e006ea87f575a0f45a047555dae3e44
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-02-18 18:05:18 -0500

    Looks like trash, but react works

---
+++ instagram/src/App.js
@@ -0,0 +1,27 @@
+
+import React, { Component } from 'react';
+import './App.css';
+import dummyData from './dummy-data';
+import PostsContainer from './components/PostContainer/PostsContainer';
+import SearchBar from './components/SearchBar/SearchBarContainer';
+
+// console.log(dummyData);
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: dummyData
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+        <PostsContainer posts={this.state.posts} />
+      </div>
+    );
+  }
+}
+
+export default App;
