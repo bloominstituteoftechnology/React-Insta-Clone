2b3c65b33d73f8368ea249e0b6d5f584e9d2c6db
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-18 16:46:56 -0500

    Basic component structure, exports/imports in place.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
-//import dummyData from "./dummy-data";
+import dummyData from "./dummy-data";
+import SearchBar from "./components/SearchBar/SearchBar";
+import PostContainer from "./components/PostContainer/PostContainer";
+
 import "./css/main.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { data: dummyData };
+  }
+
+  changeHandler = id => {
+    console.log(id);
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p> filler text</p>
+        <header className="search-bar">
+          <SearchBar changeHandler={this.changeHandler} />
         </header>
+        <div className="posts">
+          {this.state.data.map(post => (
+            <PostContainer
+              key={post.timestamp}
+              post={post}
+              className="post-container"
+            />
+          ))}
+        </div>
       </div>
     );
   }
---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostContainer = props => {
+  return <div>console.log("PostContainerLog")</div>;
+};
+
+export default PostContainer;
