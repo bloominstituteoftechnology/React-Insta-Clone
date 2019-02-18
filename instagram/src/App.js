7c52d1a5c615e81b2a8aa1a008bc58743aaca22d
tactnician <tactnician@gmail.com>
2019-02-18 18:36:57 -0500

    added PosContainer

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './DummyData/dummy-data';
+import dummyData from './DummyData/dummy-data';
+import PostContainer from './components/PostContainer/PostContatiner';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      data : dummyData,
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <PostContainer dataProps = {this.state.data} />
       </div>
     );
   }
