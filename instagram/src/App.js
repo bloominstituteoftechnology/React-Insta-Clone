b13fe58dafba0a3491b544ae481dccc5c0a2345a
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-18 16:10:06 -0500

    Deleted unused files. Set up LESS.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,24 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import dummyData from "./dummy-data";
+import "./css/main.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p> filler text</p>
         </header>
       </div>
     );
