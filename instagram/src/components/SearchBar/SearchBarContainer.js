deecc0f4f7b5572c3bef091498141d5a9f570dba
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-02-18 19:02:26 -0500

    AS far as I could get, still needs styling, restarted multiple times

--- instagram/src/components/SearchBar/SearchBarContainer.js
+++ instagram/src/components/SearchBar/SearchBarContainer.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import IGLogo from '../../../src/logo.svg';
+import Logo from '../../assets/logo.png';
+import IGLogo from '../../assets/iglogo.png';
+import Safari from '../../assets/safari.png';
+import Heart from '../../assets/heart.png';
+import Friend from '../../assets/friend.png';
 import './SearchBar.css';
 
 const SearchBar = () => {
   return (
     <div className="search-bar-wrapper">
-      {/* <div className="image-wrapper">
-        <img alt="instagram logo" src={IGLogo} className="logo-image" />
-      </div> */}
+      <div className="image-wrapper">
+        <img alt="instagram logo" src={Logo} className="logo-image" />
+        <img alt="instagram logo text" src={IGLogo} className="logo-image-text" />
+      </div>
       <div>
         <input type="text" placeholder="Search" />
       </div>
       <div className="social-wrapper">
         <div className="social">
-          <i className="fa fa-compass" />
+          <img alt="instagram logo" src={Safari}  className="fa fa-compass" />
         </div>
         <div className="social">
-          <i className="fa fa-heart" />
+          <img alt="instagram logo" src={Heart} className="fa fa-heart" />
         </div>
         <div className="social">
-          <i className="fa fa-user-circle" />
+          <img alt="instagram logo" src={Friend} className="fa fa-user-circle" />
         </div>
       </div>
     </div>
