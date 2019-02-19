8f86a5ff465cd3c71f7ed468143993b0ba294875
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-18 19:13:11 -0500

    More styling added.

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -5,6 +5,14 @@ import PropTypes from "prop-types";
 const PostContainer = ({ post }) => {
   return (
     <section className="post">
+      <div className="post-header">
+        <img
+          src={post.thumbnailUrl}
+          alt="post.thumbnailUrl"
+          className="user-thumbnail"
+        />
+        <h3>{post.username}</h3>
+      </div>
       <img src={post.imageUrl} alt={post.imageUrl} />
       <CommentSection comments={post.comments} className="comment-section" />
     </section>
