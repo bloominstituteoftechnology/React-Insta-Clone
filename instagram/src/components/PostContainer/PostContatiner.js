40eb6001f76883dd9c6c9d5e578bed9edd1a176d
tactnician <tactnician@gmail.com>
2019-02-19 15:54:50 -0500

    implemented PostContainer component

--- instagram/src/components/PostContainer/PostContatiner.js
+++ instagram/src/components/PostContainer/PostContatiner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PostContainer = props => {
-
+    return <div> {props.dataProps.forEach(point =>{ <CommentSection comment={point.comment} key={point.username}/>})} </div>
 }
 
 export default PostContainer;
