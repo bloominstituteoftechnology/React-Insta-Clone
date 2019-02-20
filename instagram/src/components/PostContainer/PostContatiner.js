ed822431805d040625a0d1a63e57ed367e5a5292
tactnician <tactnician@gmail.com>
2019-02-20 13:05:25 -0500

    implemeted PropType checking on PostContainer

--- instagram/src/components/PostContainer/PostContatiner.js
+++ instagram/src/components/PostContainer/PostContatiner.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
+import PropTypes from 'prop-types';
 
 const PostContainer = props => {
     return( 
     <div>    
-        {/* {props.comments.} */}
         {props.dataProps.map(point => 
-        <div key ={point.timestamp}> 
-            <img src={point.imageUrl} alt={point.timestamp}/>
-            <CommentSection 
-                comments={point.comments} />
-        </div>
+            <div key ={point.timestamp}> 
+                <img src={point.imageUrl} alt={point.timestamp}/>
+                <CommentSection 
+                    comments={point.comments} />
+            </div>
         )} 
     </div>)
+}
+
+PostContainer.propTypes = {
+    dataProps: PropTypes.arrayOf(
+        PropTypes.shape({
+            username: PropTypes.string,
+            imgUrl: PropTypes.string,
+            thumbnailUrl: PropTypes.string,
+            comments: PropTypes.array
+        })
+    )
 }
 
 export default PostContainer;
