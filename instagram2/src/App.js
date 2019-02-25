681e15ac4118b838b5f292d09507f28ff49c87cf
paintedlbird7 <rosaperezlarios@gmail.com>
2019-02-25 14:22:47 -0800

    Initial Commit

---
+++ instagram2/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import dummyData from './dummy-data';
+class App extends Component {
+  constructor() {
+    // responsible for setting up the component's state
+    super();
+    this.state = {
+      instagram: instagram
+      // instagram: dummyData
+  };
+}
+}
+
+export default App;
