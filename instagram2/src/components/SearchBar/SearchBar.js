d1a2cc52e51eb6b53d0ebe1044c99affbcbd00c4
paintedlbird7 <rosaperezlarios@gmail.com>
2019-02-27 19:44:00 -0800

    Added icon & logo

--- instagram2/src/components/SearchBar/SearchBar.js
+++ instagram2/src/components/SearchBar/SearchBar.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import '../SearchBar/SearchBar.css';
-import { Form, Input } from 'reactstrap';
-import * as Icon from 'react-feather';
+import React from "react";
+import "../SearchBar/SearchBar.css";
+import { Form, Input } from "reactstrap";
+import * as Icon from "react-feather";
 
 const SearchBar = props => {
-    return (
-<div className='searchBar'>
-<div className='searchLeft'>
+  return (
+    <div className="searchBar" >
+      <div className="searchBarLeft" >
+        <Icon.Instagram className='instaIcon' />
+        <img className='instaCursive'
+            src='https://fontmeme.com/images/instagram-new-logo.png'
+            alt='Instagram Cursive' />
+        </div>
+      <Form>
 
-</div>
-<Form>
+      </Form>
+    <div className="searchBarRight">
 
-</Form>
-</div>
+    </div>
+    </div>
 
-    )
+  )
 }
 
 export default SearchBar;
-
