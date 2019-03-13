4a11fb277f82d01b3c8a49b4e89b6ce88ef5e59b
alejandrok93 <alejandrok93@gmail.com>
2019-03-13 10:18:07 -0700

    updated login styling

--- instagram/src/components/LoginPage/Login.js
+++ instagram/src/components/LoginPage/Login.js
@@ -1,53 +1,55 @@
-import React from "react";
-import "./Login.css";
-import { Alert, Button } from "reactstrap";
-import SearchBar from "../SearchBar/SearchBar";
+import React from 'react';
+import './Login.css';
+import { Alert, Button } from 'reactstrap';
+import SearchBar from '../SearchBar/SearchBar';
 
 //Define and style components
 
 //const LogoImg = SearchBar.LogoImg;
 
 class LoginPage extends React.Component {
-  constructor() {
-    super();
-    this.state = { inputText: "" };
-  }
+	constructor() {
+		super();
+		this.state = { inputText: '' };
+	}
 
-  login(event) {
-    event.preventDefault();
-    let username = this.state.inputText;
-    localStorage.setItem("user", username);
-    window.location.reload();
-  }
+	login(event) {
+		event.preventDefault();
+		let username = this.state.inputText;
+		localStorage.setItem('user', username);
+		window.location.reload();
+	}
 
-  handleChange(event) {
-    console.log(event.target.value);
-    this.setState({ inputText: event.target.value });
-  }
+	handleChange(event) {
+		console.log(event.target.value);
+		this.setState({ inputText: event.target.value });
+	}
 
-  render() {
-    return (
-      <form
-        onSubmit={e => {
-          this.login(e);
-        }}
-        className="login-form"
-      >
-        {/* <LogoImg /> */}
-        <h2>Instagram Clone</h2>
-        <input
-          value={this.state.inputText}
-          type="text"
-          placeholder="username"
-          onChange={event => {
-            this.handleChange(event);
-          }}
-        />
-        <input name="password" type="password" placeholder="password" />
-        <Button color="primary">Login</Button>
-      </form>
-    );
-  }
+	render() {
+		return (
+			<form
+				onSubmit={e => {
+					this.login(e);
+				}}
+				className="login-form"
+			>
+				{/* <LogoImg /> */}
+				<h2>Instagram Clone</h2>
+				<div className="login-input">
+					<input
+						value={this.state.inputText}
+						type="text"
+						placeholder="username"
+						onChange={event => {
+							this.handleChange(event);
+						}}
+					/>
+					<input name="password" type="password" placeholder="password" />
+					<Button color="primary">Login</Button>
+				</div>
+			</form>
+		);
+	}
 }
 
 export default LoginPage;
