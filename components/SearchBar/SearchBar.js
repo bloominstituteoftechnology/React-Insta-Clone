5efe8c56f562d59a8c6a1322814fcd25bfb99245
JustinAndrade <j.andrade.john@gmail.com>
2019-03-13 20:31:24 -0400

    project base complete

---
+++ components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchBar = (props) => {
+    <div className ='search-bar'>
+        <form>
+            <input>
+                search
+            </input>
+        </form>
+    </div>
+}
+
+export default SearchBar
---
+++ instagram/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Doing work <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
