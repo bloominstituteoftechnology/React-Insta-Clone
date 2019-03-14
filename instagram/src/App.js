51a61e33a4e2f9e801a78c052d87acdc693cc9b6
JustinAndrade <j.andrade.john@gmail.com>
2019-03-13 22:46:01 -0400

    nav bar created

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
 
+import './app.css';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Doing work <code>src/App.js</code> and save to reload.
-          </p>
           <SearchBar />
         </header>
       </div>
---
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CommentSection = (props) => {
+
+}
+
+export default CommentSection;
