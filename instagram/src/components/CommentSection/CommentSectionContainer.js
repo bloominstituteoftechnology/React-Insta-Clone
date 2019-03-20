85c88272250a692178e5eec6f310737007334896
JustinAndrade <j.andrade.john@gmail.com>
2019-03-19 23:00:47 -0400

    basic features and styling  added

--- instagram/src/components/CommentSection/CommentSectionContainer.js
+++ instagram/src/components/CommentSection/CommentSectionContainer.js
@@ -8,19 +8,60 @@ class CommentSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      comments: []
+      comments: props.comments,
+      comment: ''
     };
   }
 
+  
   componentDidMount(){
-    this.setState({comments: this.props.comments})
+    const id = this.props.postId;
+  if (localStorage.getItem(id)) {
+    this.setState({
+      comments: JSON.parse(localStorage.getItem(this.props.postId))
+    });
+
+  } else {
+    this.setComments();
   }
+}
+
+  componentWillUnmount() {
+    this.setComments();
+  }
+
+  setComments = () => {
+    localStorage.setItem(
+      this.props.postId,
+      JSON.stringify(this.state.comments)
+    );
+  };
+
+  
+  commentHandler = event => {
+    this.setState({comment: event.target.value})
+  }
+
+  handleCommentSubmit = e => {
+    e.preventDefault();
+    const newComment = { text: this.state.comment, username: 'justinandrade' };
+    const comments = this.state.comments.slice();
+    comments.push(newComment);
+    this.setState({ comments, comment: ''});
+    setTimeout(() => {
+      this.setComments();
+    }, 500);
+  };
 
   render() {
     return (
       <div>
         {this.state.comments.map((c, i) => <Comment key={i} comment={c} />)}
-        <CommentInput />
+        <CommentInput  
+        comment={this.state.comment}
+        submitComment={this.handleCommentSubmit}
+        changeComment={this.commentHandler}
+        />
       </div>
     );
   }
--- instagram/src/components/PostsContainer/Post.js
+++ instagram/src/components/PostsContainer/Post.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import CommentSection from '../CommentSection/CommentSectionContainer';
 import PostHeader from './PostHeader';
+import heart from '../../assets/heart.svg';
+import message from '../../assets/message.svg';
+import share from '../../assets/share.svg'
 
 import './Posts.css';
 
@@ -18,6 +21,11 @@ const Post = props => {
           className="post-image"
           src={props.post.imageUrl}
         />
+      </div>
+      <div className='post-interface'>
+        <img className='logo' alt='heart' src={heart}/>
+        <img className='logo' alt='message icon' src={message}/>
+        <img className= 'logo'alt='share icon' src={share}/>
       </div>
       <CommentSection comments={props.post.comments} />
     </div>
