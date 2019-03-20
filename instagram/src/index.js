47ac8c17878b28a3bcdda5e37a5659679fec3cb6
Matthias Bakken <matthias.bakken@gmail.com>
2019-03-19 20:27:37 -0700

    added png assets. completed search bar

--- instagram/src/index.js
+++ instagram/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
