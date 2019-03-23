fcefc9b408a9aae6d8aaa268406e59c45974e01d
Memitaru <amandatwah@gmail.com>
2019-03-22 21:48:07 -0700

    Finished styling login page

--- instaclone/src/components/Login/Login.js
+++ instaclone/src/components/Login/Login.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import mainimage from '../../assets/mainimage.PNG'
 import styled from 'styled-components';
 
 const LoginContainer = styled.div`
     height: 100vh;
-    background-color: #E6E6E6;
+    background-color: #FAFAFA;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -11,17 +12,24 @@ const LoginContainer = styled.div`
 
 const Form = styled.div`
     width: 400px;
-    border: 1px solid darkgray;
+    border: 1px solid #E6E6E6;
     background-color: white;
     height: 450px;
 `
 
 const Welcome = styled.h2`
+    font-family: Pacifico;
+    padding-top: 30px;
+    text-align: center;
+    font-size: 30px;
+    padding-bottom: 0;
+    margin-bottom: 0;
     
 `
 
-const SearchInput = styled.input`
+const LoginInput = styled.input`
     height: 25px;
+    width: 80%;
     margin-bottom: 5px;
     background-color: #FAFAFA;
     border: 1px solid #E6E6E6;
@@ -29,6 +37,43 @@ const SearchInput = styled.input`
     text-align: center;
     &::-webkit-input-placeholder {
         color: #A5A7AA;
+`
+
+const Instaclone = styled(Welcome)`
+    font-size: 40px;
+    padding-top: 0;
+    margin-top: 0;
+`
+const PleaseLogin = styled.p`
+    text-align: center;
+    font-size: 20px;
+    margin-top: 45px;
+`
+
+const MainImage = styled.img`
+    padding-right: 30px;
+
+    @media (max-width:800px) {
+        display: none;
+    }
+`
+
+const LoginForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20px;
+`
+
+const LoginButton = styled.button`
+    background-color: #3897F0;
+    color: white;
+    width: 250px;
+    height: 30px;
+    border: none;
+    border-radius: 5px;
+    margin-top: 20px;
+    font-weight: bold;
 `
 
 class Login extends React.Component {
@@ -53,14 +98,16 @@ class Login extends React.Component {
     render(){
         return(
             <LoginContainer>
+                <MainImage src={mainimage} />
                 <Form>
-            <Welcome>Welcome to React Insta Clone</Welcome>
-            <p>Please sign in to continue.</p>
-            <form>
-                <input type="text" placeholder="User Name" name="username" value={this.state.username} onChange={this.inputHandler} />
-                <input type="text" placeholder="Password" name="password" value={this.state.password} onChange={this.inputHandler} />
-                <button onClick={this.login}>Login</button>
-            </form>
+            <Welcome>Welcome to</Welcome> 
+            <Instaclone>React Insta Clone</Instaclone>
+            <PleaseLogin>Please sign in to continue.</PleaseLogin>
+            <LoginForm>
+                <LoginInput type="text" placeholder="User Name" name="username" value={this.state.username} onChange={this.inputHandler} />
+                <LoginInput type="text" placeholder="Password" name="password" value={this.state.password} onChange={this.inputHandler} />
+                <LoginButton onClick={this.login}>Login</LoginButton>
+            </LoginForm>
             </Form>
             </LoginContainer>
         )
