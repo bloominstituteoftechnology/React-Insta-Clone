76e07b0248d6c380bb5702e6de87fe82f53ba9fd
omar okla <omaro@me.com>
2019-03-25 18:08:21 -0400

    styling updates

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./SearchBar.css";
 
 const SearchBar = () => {
-	return <div>Hello</div>;
+	return (
+		<div className='searchBar'>
+			<div className='logo'>
+				<i class='fab fa-instagram' /> <h4>Instagram</h4>
+			</div>
+
+			<div className='search'>
+				<form>
+					<input type='text' placeholder='Search' />
+				</form>
+			</div>
+
+			<div className='nav'>
+				<h1>holder</h1>
+			</div>
+		</div>
+	);
 };
 
 export default SearchBar;
