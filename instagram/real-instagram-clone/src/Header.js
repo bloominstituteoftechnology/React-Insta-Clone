58cd68a1b2f2e57053069dd45cdbd478ff9d3bd3
Lambda_School_Loaner_93 <lambda_school_loaner_93@Lambda-School-Loaner-93s-MacBook-Pro.local>
2019-03-26 12:24:57 -0400

    trying to deploy

--- instagram/real-instagram-clone/src/Header.js
+++ instagram/real-instagram-clone/src/Header.js
@@ -1,28 +1,29 @@
 import React from "react";
-import './App.css'
+import "./App.css";
 
-const Header = ()=>{
-  return(
-      <div>
-      <div className ="header" >
-      <div className = 'insta-header'>
-      <div>
-        <i className="fab fa-instagram"></i>
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <div className="insta-header">
+          <div>
+            <i className="fab fa-instagram" />
+          </div>
+          <div>
+            <h1 className="header-name">Instagram</h1>
+          </div>
         </div>
         <div>
-            <h1 className = "header-name">Instagram</h1>
+          <input type="text" placeholder="Search....." />
         </div>
-        </div>
-        <div>
-        <input type="text" placeholder="Search.."></input>
-        </div>
-        <div className = " header-nav">
-        <i class="far fa-compass"></i>
-        <i class="far fa-heart"></i>
-        <i class="far fa-user"></i>
+        <div className=" header-nav">
+          <i class="far fa-compass" />
+          <i class="far fa-heart" />
+          <i class="far fa-user" />
         </div>
       </div>
-      </div>
-  )
-}
-  export default Header
+    </div>
+    
+  );
+};
+export default Header;
--- instagram/real-instagram-clone/src/components/PostContainer.js
+++ instagram/real-instagram-clone/src/components/PostContainer.js
@@ -1,32 +1,35 @@
-import React from 'react'
-import CommentSection from './CommentSection'
+import React from "react";
+import CommentSection from "./CommentSection";
 
-const PostContainer = (props) =>{
-    // console.log(props)
-    return(
-        <div className = 'post-container'>
-            {props.post.map( post=>
-        
-          <div className ="shook"key ={post.timestamp}>
-            <div className = "profileName">
-          <div className = "userinfo"><img className = "thumbimg"src = {post.thumbnailUrl} alt = 'fuck 12'></img> <h3 className ="user-name">{post.username}</h3></div> </div>
-          <div ><img className ="postimg"src = {post.imageUrl} alt = "fuck 12"></img></div>
-          
-          <div className =" interactions">
-          <i class="far fa-heart"></i>
-          <i class="far fa-comment"></i>
+const PostContainer = props => {
+  // console.log(props)
+  return (
+    <div className="post-container">
+      {props.post.map(post => (
+        <div className="post" key={post.timestamp}>
+          <div className="profileName">
+            <div className="userinfo">
+              <img className="thumbimg" src={post.thumbnailUrl} alt="a post" />{" "}
+              <h3 className="user-name">{post.username}</h3>
+            </div>{" "}
           </div>
           <div>
-          <div ><p  className = "likes">{post.likes} Likes</p></div>
+            <img className="postimg" src={post.imageUrl} alt="a post" />
           </div>
-          <CommentSection comment = {post.comments}/>
+
+          <div className=" interactions">
+            <i class="far fa-heart" />
+            <i class="far fa-comment" />
           </div>
-                
-            )}
-          </div>  
-       
-    )
-    
-    
-        }
+          <div>
+            <div>
+              <p className="likes">{post.likes} Likes</p>
+            </div>
+          </div>
+          <CommentSection comment={post.comments} />
+        </div>
+      ))}
+    </div>
+  );
+};
 export default PostContainer;
