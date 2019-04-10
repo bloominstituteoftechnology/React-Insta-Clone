e2b0703a94fab76962785dab3e9ba3a7c9260b9f
Brock Falfas <brockfalfas@gmail.com>
2019-04-10 18:54:28 -0400

    update 1

--- instagram/graminsta/src/App.js
+++ instagram/graminsta/src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import dummyData from "./dummy-data.js"
 import Searchbar from "./Components/Searchbar.js"
 import PostContainer from "./Components/PostContainer.js"
 import CommentSection from "./Components/CommentSection.js"
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super() 
+      this.state = {
+        userData = []
+      }
+
+    }
+    componentDidMount() {
+      this.setState({userData:dummyData}) 
+    }
+    
+  
   render() {
     return (
       <div className="App">
---
+++ instagram/graminsta/src/Components/Post.js
@@ -0,0 +1,9 @@
+import React from "react"
+import PostContainer from "./PostContainer";
+
+function Post = props => {
+    return (
+        
+    )
+}
+export default PostContainer;
