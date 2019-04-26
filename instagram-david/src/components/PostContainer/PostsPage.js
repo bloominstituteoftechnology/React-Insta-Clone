532a94b4af0fb77735e43e890d5081fe9998a340
Zooheck <davidscottflack@gmail.com>
2019-04-25 19:40:26 -0500

    Prettier

--- instagram-david/src/components/PostContainer/PostsPage.js
+++ instagram-david/src/components/PostContainer/PostsPage.js
@@ -1,32 +1,36 @@
-import React from 'react'
-import styled from 'styled-components'
-import PropTypes from 'prop-types'
-import SearchBar from '../SearchBar/SearchBar'
-import PostContainer from './PostContainer'
-import '../../App.css'
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import SearchBar from "../SearchBar/SearchBar";
+import PostContainer from "./PostContainer";
+import "../../App.css";
 
 const MainContentContainer = styled.div`
-    padding-top: 60px;
-    background-color: #F9F9F9;
-`
+  padding-top: 60px;
+  background-color: #f9f9f9;
+`;
 const PostsPage = props => {
   return (
     <div className="app-container">
-        <SearchBar onChange={props.searchPosts} value={props.searchValue} logout={props.logout}/>
-        <MainContentContainer>
-            {props.displayedPosts.map(post => {
-            return <PostContainer post={post} userProfile={props.userProfile}/>
-        })}        
-        </MainContentContainer>
-  </div>
-  )
-}
+      <SearchBar
+        onChange={props.searchPosts}
+        value={props.searchValue}
+        logout={props.logout}
+      />
+      <MainContentContainer>
+        {props.displayedPosts.map(post => {
+          return <PostContainer post={post} userProfile={props.userProfile} />;
+        })}
+      </MainContentContainer>
+    </div>
+  );
+};
 
 PostsPage.propTypes = {
   searchPosts: PropTypes.func,
   searchValue: PropTypes.string,
   logout: PropTypes.func,
   userProfile: PropTypes.string
-}
+};
 
-export default PostsPage
+export default PostsPage;
