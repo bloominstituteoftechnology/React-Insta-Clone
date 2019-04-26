450d36c701604f7f2fcc0ac589331cbe482f809d
Zooheck <davidscottflack@gmail.com>
2019-04-25 20:19:33 -0500

    Move post container styles to new file

--- instagram-david/src/components/PostContainer/PostContainer.js
+++ instagram-david/src/components/PostContainer/PostContainer.js
@@ -1,42 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
 import CommentSection from "../CommentSection/CommentSection";
-const PostContainerDiv = styled.div`
-  width: 600px;
-  max-width: 95%;
-  margin: auto;
-  margin-bottom: 30px;
-  border: 1px solid #e2e2e2;
-  background-color: white;
-`;
-const PostHeading = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 15px;
-`;
-const PostHeadingImgContainer = styled.div`
-  width: 30px;
-  margin-right: 10px;
-`;
-const ProfilePictureImage = styled.img`
-  display: block;
-  width: 100%;
-  height: auto;
-  border-radius: 50%;
-`;
-const Username = styled.h2`
-  font-size: 1.5rem;
-`;
-const PostImageContainer = styled.div`
-  width: 100%;
-  max-width: 600px;
-`;
-const PostImage = styled.img`
-  width: 100%;
-  height: auto;
-`;
+import {
+  PostContainerDiv,
+  PostHeading,
+  PostHeadingImgContainer,
+  ProfilePictureImage,
+  Username,
+  PostImageContainer,
+  PostImage
+} from "./PostContainerStyles.js";
 const PostContainer = props => {
   const { username, thumbnailUrl, imageUrl, likes, comments } = props.post;
   return (
---
+++ instagram-david/src/components/PostContainer/PostContainerStyles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const PostContainerDiv = styled.div`
+  width: 600px;
+  max-width: 95%;
+  margin: auto;
+  margin-bottom: 30px;
+  border: 1px solid #e2e2e2;
+  background-color: white;
+`;
+export const PostHeading = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 15px;
+`;
+export const PostHeadingImgContainer = styled.div`
+  width: 30px;
+  margin-right: 10px;
+`;
+export const ProfilePictureImage = styled.img`
+  display: block;
+  width: 100%;
+  height: auto;
+  border-radius: 50%;
+`;
+export const Username = styled.h2`
+  font-size: 1.5rem;
+`;
+export const PostImageContainer = styled.div`
+  width: 100%;
+  max-width: 600px;
+`;
+export const PostImage = styled.img`
+  width: 100%;
+  height: auto;
+`;
