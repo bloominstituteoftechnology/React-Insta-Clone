c6b379d4f3e1775c1ad510b0d70533148e830754
Zooheck <davidscottflack@gmail.com>
2019-04-25 20:16:25 -0500

    Move searchbar styled components to separate file

--- instagram-david/src/components/SearchBar/SearchBar.js
+++ instagram-david/src/components/SearchBar/SearchBar.js
@@ -1,53 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import {
+  SearchContainer,
+  SearchItemContainer,
+  SearchHeading,
+  LogoIcon,
+  MainHeading,
+  SearchInput,
+  SearchActionContainer,
+  SearchActionIcon,
+  LogoutIcon
+} from "./SearchBarStyles.js";
 
-const SearchContainer = styled.div`
-  font-family: "Grand Hotel", cursive;
-  width: 100%;
-  padding: 20px 0;
-  border-bottom: 2px solid #e2e2e2;
-`;
-const SearchItemContainer = styled.div`
-  max-width: 900px;
-  margin: auto;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-const SearchHeading = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`;
-const LogoIcon = styled.div`
-  padding-right: 15px;
-  border-right: 1px solid black;
-`;
-const MainHeading = styled.h1`
-  margin-left: 20px;
-  font-size: 3rem;
-  color: #343434;
-  letter-spacing: 1px;
-`;
-const SearchInput = styled.input`
-  padding: 3px 18px;
-`;
-const SearchActionContainer = styled.div`
-  width: 133px;
-  display: flex;
-  justify-content: space-between;
-`;
-const SearchActionIcon = styled.div`
-  margin-left: 10px;
-`;
-const LogoutIcon = styled.div`
-  margin-left: 10px;
-  :hover {
-    cursor: pointer;
-  }
-`;
 const SearchBar = props => {
   return (
     <SearchContainer>
---
+++ instagram-david/src/components/SearchBar/SearchBarStyles.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const SearchContainer = styled.div`
+  font-family: "Grand Hotel", cursive;
+  width: 100%;
+  padding: 20px 0;
+  border-bottom: 2px solid #e2e2e2;
+`;
+export const SearchItemContainer = styled.div`
+  max-width: 900px;
+  margin: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const SearchHeading = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+export const LogoIcon = styled.div`
+  padding-right: 15px;
+  border-right: 1px solid black;
+`;
+export const MainHeading = styled.h1`
+  margin-left: 20px;
+  font-size: 3rem;
+  color: #343434;
+  letter-spacing: 1px;
+`;
+export const SearchInput = styled.input`
+  padding: 3px 18px;
+`;
+export const SearchActionContainer = styled.div`
+  width: 133px;
+  display: flex;
+  justify-content: space-between;
+`;
+export const SearchActionIcon = styled.div`
+  margin-left: 10px;
+`;
+export const LogoutIcon = styled.div`
+  margin-left: 10px;
+  :hover {
+    cursor: pointer;
+  }
+`;
