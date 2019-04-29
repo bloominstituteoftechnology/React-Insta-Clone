cdc5b1af412dd13e6ff3cdc06eb4183d27cd2ad5
Firealem Erko <firealemg@yahoo.com>
2019-04-29 18:03:25 -0400

    added some styling to search bar

--- instagram-app/src/Components/PostContainer/PostContainer.jsx
+++ instagram-app/src/Components/PostContainer/PostContainer.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import CommentSection from "../CommentSection/CommentSection";
-
+import "./Post.css";
 const PostContainer = props => {
   return (
     <div className="PostContainer">
-      <img src={props.Data.thumbnailUrl} alt="users" />
-      <div>{props.Data.username}</div>
-      <img src={props.Data.imageUrl} alt="posted pic" />
-      <div>{props.Data.timestamp}</div>
-      <div>{props.Data.likes}</div>
-      <CommentSection comments={props.Data.comments} />
-      <input placeholder="Add a comment..."></input>
+      <div className="postwrapper">
+        <img src={props.Data.thumbnailUrl} alt="users" />
+        <div>{props.Data.username}</div>
+        <img src={props.Data.imageUrl} alt="posted pic" />
+        <div>{props.Data.timestamp}</div>
+        <div>{props.Data.likes}</div>
+        <CommentSection comments={props.Data.comments} />
+        <input placeholder="Add a comment..." />
+      </div>
     </div>
   );
 };
--- instagram-app/src/Components/SearchBar/SearchBar.jsx
+++ instagram-app/src/Components/SearchBar/SearchBar.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './Search.css'
+import React from "react";
+import "./Search.css";
 
 export default function SearchBar() {
   return (
-    <div>
-      
+    <div className="SearchBar">
+
+      <div className="leftofsearch">
+        <i class="fab fa-instagram" />
+        <div className="logotext">Instagram</div>
+      </div>
+
+      <div className="search">
+        <i class="fas fa-search" aria-hidden="true" />
+        <input placeholder="Search" />
+      </div>
+
+      <div className="rightofsearch">
+        <i class="far fa-compass" />
+        <i class="far fa-heart" />
+        <i class="far fa-user" />
+      </div>
     </div>
-  )
+  );
 }
