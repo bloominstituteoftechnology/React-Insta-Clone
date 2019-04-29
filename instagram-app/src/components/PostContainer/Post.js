fc38d5644ea4d2be93014a1f9d2869836af94bb5
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-29 15:44:47 -0700

    Passed in more data via props to my post

--- instagram-app/src/components/PostContainer/Post.js
+++ instagram-app/src/components/PostContainer/Post.js
@@ -4,9 +4,20 @@ import CommentSection from '../CommentSection/CommentSection';
 const Post = props => {
     return(
         <div className="postWrapper">
-            <div ClassName="postContent">
+            
+            <div ClassName="postheader">
+                <img className="thumbnail" src={props.post.thumbnailUrl}/>
+
                 <p>{props.post.username}</p>
             </div>
+
+            <img src= {props.post.imageUrl} />
+            
+            <div className="engagement">
+            <p>{props.post.likes} likes </p>
+            </div>
+
+
 
             <div className="comments">
             <CommentSection comments={props.post.comments}/>
