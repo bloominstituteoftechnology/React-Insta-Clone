32002ac656e5272ea6d763711327d817d8312caf
Your Name <you@example.com>
2019-04-29 17:53:24 -0500

    search bar,searchbar.js

---
+++ instagram-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './App.css';
+import SearchBar from './components/SearchBar/searchbar';
+
+function App() {
+  return (
+    <div className="App">
+    <SearchBar/>
+   
+    
+    </div>
+  );
+}
+
+export default App;
+
+
+//<footer><div>Icons made by <a href="https://www.freepik.com/?__hstc=57440181.1c30f6235294cef2ff3ca408fc918a7f.1556573791465.1556573791465.1556573791465.1&__hssc=57440181.2.1556573791467&__hsfp=3025663657" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> 
+//</footer>   <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+//<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+//<div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
---
+++ instagram-app/src/components/SearchBar/searchbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import CameraLogo from './instagram-logo.png';
+import InstaLogo from './logo.png';
+import DM from './telegram.png';
+import Like from './like.png';
+import User from './user.png';
+//import './instagram-app/src/App.css';
+
+
+export default function SearchBar(props){
+    return(
+        <div className='searchbar'>
+        
+     <img alt ='cameralogo' src={CameraLogo} className='cameralogo'></img>
+     <img alt='instalogo' src={InstaLogo} className='instalogo'></img>
+        <input 
+        type='text'
+        placeholder='                       Search'
+       />
+
+    <img alt='dm' src={DM} className='icons'></img>
+       <img alt='like' src={Like} className='icons'></img>
+       <img alt='user' src={User} className='icons'></img>
+       
+        </div>
+    )
+}
