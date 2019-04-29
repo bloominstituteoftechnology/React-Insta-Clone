07d22e6e3d3ff0dc8168772de306beb6b98df5ae
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-29 13:01:58 -0700

    Mapping over each data item and passing to PostContainer as prop

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
 
-// Importing dummy data
 
-function App() {
+import dummyData from './dummy-data'; 
+
+import PostContainer from './components/PostContainer/PostContainer'; 
+
+class App extends React.Component {
+  constructor () {
+    super(); 
+    this.state= {
+      posts: dummyData
+    };
+  }
+
+
+render () {
   return (
     <div className="App">
+      {this.state.posts.map((dummyData) => (
+        <p>
+          <PostContainer post={dummyData}/> 
+        </p>
+      ))}
     </div>
   );
 }
+}
 
 export default App;
+
---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PostContainer = props => {
+    return (
+        <p>{props.post.username}</p>
+    )
+}
+
+export default PostContainer; 
+
