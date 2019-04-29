46d36f047e042a06e139d30f6ccd8581dcb7d61e
PetejStone <petejstoney@gmail.com>
2019-04-29 15:15:54 -0700

    added searchbar sylings

--- instagram-app/src/SearchBar/SearchBar.js
+++ instagram-app/src/SearchBar/SearchBar.js
@@ -3,17 +3,25 @@ import instagram from '../instagram.svg';
 import compass from '../compass.svg';
 import heart from '../heart.svg';
 import user from '../user.svg';
+import './SearchBar.css';
 
 const SearchBar = props => {
     return (
         <div className="search-bar">
-            <img src={instagram} className="instagram-logo" alt="logo" />
+            <div className="heading-logo">
+                <img src={instagram} className="instagram-logo" alt="logo" />
+                <h1>Instagram</h1>
+            </div>
+            
 
-            <input type="text" placeholder="&#128269; Search" />
+            <input className="search" type="text" placeholder="&#128269; Search" />
 
-            <img src={compass} className="instagram-logo" alt="logo" />
-            <img src={heart} className="instagram-logo" alt="logo" />
-            <img src={user} className="instagram-logo" alt="logo" />
+            <div className="user-logos">
+                <img src={compass} className="compass-logo logo" alt="logo" />
+                <img src={heart} className="heart-logo logo" alt="logo" />
+                <img src={user} className="user-logo logo" alt="logo" />
+            </div>
+            
         </div>
     );
 }
