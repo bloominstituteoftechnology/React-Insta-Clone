3334cabfbb672ba0a75b5508b6708009aebb8fcf
PetejStone <petejstoney@gmail.com>
2019-04-29 15:36:20 -0700

    added user banner stylings

--- instagram-app/src/PostContainer/PostContainer.js
+++ instagram-app/src/PostContainer/PostContainer.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Comment from '../CommentSection/Comment';
 import PropTypes from 'prop-types';
+import './PostContainer.css';
 
 const PostContainer = props => {
     return (
         <div>
-            {props.data.comments.map((comment,index) => 
-                <Comment username={comment.username} text={comment.text} key={index}/>
-                )}
-                <input type="text" placeholder="Add Comment" />
+            {console.log(props.data)}
+            <div className="user-banner">
+                <img className="user-img" src={props.data.thumbnailUrl} alt="user-img"/>
+                <h3>{props.data.username}</h3>
+            </div>
+            <img className="user-post" src={props.data.imageUrl} alt="user-post"/>
+            <p className="likes">{props.data.likes} Likes</p>
+            <div className="comment-section">
+                {props.data.comments.map((comment,index) => 
+                    <Comment username={comment.username} text={comment.text} key={index}/>
+                    )}
+                    <input type="text" placeholder="Add Comment" />
+            </div>
         </div>
     );
 }
