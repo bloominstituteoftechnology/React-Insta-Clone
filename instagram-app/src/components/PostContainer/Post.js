6029c0f87cd41bdbc1b06406fd61b3d46530d73f
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-29 15:58:04 -0700

    Added PropTypes testing to Post

--- instagram-app/src/components/PostContainer/Post.js
+++ instagram-app/src/components/PostContainer/Post.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import CommentSection from '../CommentSection/CommentSection'; 
+import PropTypes from 'prop-types';
 
 const Post = props => {
     return(
@@ -25,5 +26,28 @@ const Post = props => {
         </div>
     );
 }; 
+
+Post.propTypes = {
+    post: PropTypes.arrayOf(
+      PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        thumbnailUrl: PropTypes.string.isRequired,
+        imageUrl: PropTypes.string.isRequired,
+        likes: PropTypes.number,
+        timestamp: PropTypes.string, 
+        comments: PropTypes.arrayOf(
+            PropTypes.shape({
+              username: PropTypes.string.isRequired,
+              text: PropTypes.string.isRequired
+            })
+          ).isRequired
+      })
+    ).isRequired
+  };
+  
+  // Just in case we haven't gotten data back, going to do a default prop array
+  Post.defaultProps = {
+    post: []
+  };
 
 export default Post; 
