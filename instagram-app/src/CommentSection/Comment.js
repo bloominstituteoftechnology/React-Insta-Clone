07e8ed8e97fe969106bcd800fed597d2cd1f378c
PetejStone <petejstoney@gmail.com>
2019-04-29 13:23:14 -0700

    rendered all componets to the screen

--- instagram-app/src/CommentSection/Comment.js
+++ instagram-app/src/CommentSection/Comment.js
@@ -4,7 +4,7 @@ import PostContainer from '../PostContainer/PostContainer';
 const Comment = props => {
     return (
         <div>
-            <h3>{props.username}</h3>
+            <h4>{props.username}</h4>
             <p>{props.text}</p>
         </div>
     );
--- instagram-app/src/PostContainer/PostContainer.js
+++ instagram-app/src/PostContainer/PostContainer.js
@@ -4,7 +4,7 @@ import Comment from '../CommentSection/Comment';
 const PostContainer = props => {
     return (
         <div>
-            {console.log(props.data.comments.map(comment => <Comment username={comment.username} text={comment.text} />))}
+            {props.data.comments.map(comment => <Comment username={comment.username} text={comment.text} />)}
         </div>
     );
 }
