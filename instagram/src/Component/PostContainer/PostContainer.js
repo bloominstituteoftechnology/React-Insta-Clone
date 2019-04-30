0774ff4611f2ad8ca9e9202c253c1dabe84aa8dc
Manju KC <Manju KC>
2019-04-30 07:45:28 -0400

    figureout the propsTypes

--- instagram/src/Component/PostContainer/PostContainer.js
+++ instagram/src/Component/PostContainer/PostContainer.js
@@ -2,28 +2,29 @@
 import React from "react";
 import heart from '../../images/Instagram-Heart.png';
 import './PostContainer.css';
+import PropTypes from 'prop-types';
 
 const PostContainer = (props) => {
     return (
       <div className="mainpart"> 
       <div>
-      <img  className="secondimg"src={props.item.thumbnailUrl}></img>
+      <img  className="secondimg"src={props.item.thumbnailUrl} alt="props.item.username"/>
         <span className="username">{props.item.username} </span> 
           </div>
           
           {console.log(props)}
-          <img  className="firstimg" src={props.item.imageUrl}></img>
+          <img  className="firstimg" src={props.item.imageUrl} alt=""/>
         
     
           <div>
-          <img className = "likepic" src={heart}></img>
+          <img className="likepic" src={heart} alt=""/>
           <i class="far fa-comment"></i>
           </div>
           <h1>{props.item.likes}<span> likes</span></h1>    
           {
               props.item.comments.map((msg) => {
                   return (
-                      <div><span>{msg.username}</span>  {msg.text} </div>
+            <div><span className="commnents">{msg.username}</span>  {msg.text} </div>
                   )
               })
           }
@@ -31,6 +32,14 @@ const PostContainer = (props) => {
     )
 
 }
+
+PostContainer.propTypes = {
+    item: PropTypes.shape({
+      username: PropTypes.string,
+      thumbnailUrl: PropTypes.string,
+      imageUrl: PropTypes.string
+    })
+  };
 
 export default PostContainer
 
