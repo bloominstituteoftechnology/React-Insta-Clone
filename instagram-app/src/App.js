b641fdf24e8560d52e2154dbf8637bf38514d68a
Firealem Erko <firealemg@yahoo.com>
2019-05-01 10:46:57 -0400

    added search handler in appjs and passed down to searchbar container

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,32 +1,43 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 import dummyData from "./dummy-data";
 import "./App.css";
 import PostContainer from "./Components/PostContainer/PostContainer";
-import SearchBarContainer from './Components/SearchBar/SearchBarContainer'
-
-
+import SearchBarContainer from "./Components/SearchBar/SearchBarContainer";
 
 class App extends Component {
+  state = {
+    coolData: [],
+    filteredData: []
+  };
 
-  state={
-    coolData:[]
-    }
+  componentDidMount() {
+    this.setState({ coolData: dummyData });
+  }
 
-    componentDidMount() {
-      this.setState({ coolData: dummyData });
-    }
-  
+
+  searchPostsHandler = e => {
+    var updatedData = this.state.initialItems;
+    updatedData = updatedData.filter(function(item){
+      return item.toLowerCase().search(
+        e.target.value.toLowerCase()) !== -1;
+    });
+    this.setState({filteredData: updatedData});
+  };
+
+
+
   render() {
     return (
-    <div className="App">
-        <SearchBarContainer/>
-      {this.state.coolData.map(e => (
-        <PostContainer Data={e} key={e.timestamp} />
-      ))}
-    </div>
-  );
-
-}
+      <div className="App">
+        <SearchBarContainer searchfn={this.searchPostsHandler} />
+        {this.state.coolData.map(e => (
+          <PostContainer Data={e} key={e.timestamp} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+//
