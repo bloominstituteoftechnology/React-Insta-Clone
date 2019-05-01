1a0f6ecb22423ddfd3ba9e9a7c2220350e868dd8
Firealem Erko <firealemg@yahoo.com>
2019-05-01 16:06:49 -0400

    added postspage component, moved code from appjs to postspage

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,34 +1,20 @@
 import React, { Component } from "react";
-import dummyData from "./dummy-data";
+// import dummyData from "./dummy-data";
 import "./App.css";
-import PostContainer from "./Components/PostContainer/PostContainer";
-import SearchBarContainer from "./Components/SearchBar/SearchBarContainer";
+// import PostContainer from "./Components/PostContainer/PostContainer";
+// import SearchBarContainer from "./Components/SearchBar/SearchBarContainer";
+import PostsPage from './Components/PostContainer/PostsPage.js'
 
 class App extends Component {
-  state = {
-    coolData: [],
-    filteredData: []
-  };
+  state = {};
+    
 
-  componentDidMount() {
-    this.setState({ coolData: dummyData });
-  }
-
-  searchPostsHandler = e => {
-    var updatedData = this.state.coolData;
-    updatedData = updatedData.filter(function(item) {
-      return item.search(e.target.value.toLowerCase()) !== -1;
-    });
-    this.setState({filteredData: updatedData });
-  };
+ 
 
   render() {
     return (
       <div className="App">
-        <SearchBarContainer searchfn={this.searchPostsHandler} />
-        {this.state.coolData.map(e => (
-          <PostContainer Data={e} key={e.timestamp} />
-        ))}
+        <PostsPage/>
       </div>
     );
   }
