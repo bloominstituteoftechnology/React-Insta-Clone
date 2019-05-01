f4ca358b649b273c5bb5123dd7845b2cec279723
Firealem Erko <firealemg@yahoo.com>
2019-05-01 17:34:15 -0400

    added change handler and submit handler to form

--- instagram-app/src/Components/Login/Login.jsx
+++ instagram-app/src/Components/Login/Login.jsx
@@ -10,13 +10,20 @@ class Login extends Component {
         };
       }
 
+      loginHandle =(e)=>{
+        const user = this.state.username;
+        localStorage.setItem('user', user);
+        window.location.reload();
+      };
+      databinder=(e)=>{this.setState({ [e.target.name]: e.target.value });}
+
     render() {
         return (
             <div>
                 <form>
-                    <input type="text" placeholder="Enter User Name"></input>
-                    <input type="password" placeholder="password"></input>
-                    <button></button>
+                    <input onChange= {this.dataBinder} type="text" placeholder="Enter User Name"></input>
+                    <input onChange={this.dataBinder} type="password" placeholder="password"></input>
+                    <button onClick={this.loginHandle}>Log In</button > 
                 </form>
             </div>
         );
