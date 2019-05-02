178f13aee62fd4b43e1fd88f5d4d3bdb6e9faa87
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-05-02 13:45:14 -0700

    Added Login page styling

--- instagram-app/src/components/Login/Login.js
+++ instagram-app/src/components/Login/Login.js
@@ -2,9 +2,54 @@ import React from "react";
 import styled from 'styled-components';
 
 const LoginWrapper = styled.div`
-  border: 2px solid red; 
-`
+  font-family: 'Pacifico', cursive;
+  margin: 0 auto; 
+  margin-top: 10%; 
+  border-radius: 10%; 
+  width: 60%;  
+  padding-top: 10%; 
+  padding-bottom: 10%; 
+  background-color: black; 
+  color: white; 
+`;
 
+const LoginTitle = styled.h1`
+  font-size: 3rem; 
+`; 
+
+const LoginSubtitle = styled.h2`
+  font-size: 1.5rem; 
+  font-family: 'Oswald', sans-serif;
+`;
+
+const LoginButtonsWrapper = styled.form`
+  display: flex; 
+  flex-direction: column; 
+  align-items: center; 
+`;
+
+const LoginInput = styled.input`
+  width: 30%; 
+  margin: 2%; 
+  font-family: 'Oswald', sans-serif;
+  font-size: 1rem; 
+
+`;
+
+const LoginButton = styled.button`
+  width: 20%; 
+  font-family: 'Pacifico', cursive;
+  font-size: 1.5rem;
+  margin-top: 3%; 
+  border-radius: 10%; 
+  color: black; 
+  background-color: white; 
+
+  :hover {
+    background-color: black; 
+    color: white; 
+  }
+`;
 
 class Login extends React.Component {
   constructor() {
@@ -38,29 +83,29 @@ class Login extends React.Component {
   render() {
     return (
       <LoginWrapper>
-        <h1>Dollygram</h1>
-        <h4>Find out who you are and do it on purpose.</h4>
-        <form>
-          <input
+        <LoginTitle>Dollygram</LoginTitle>
+        <LoginSubtitle>Find out who you are and do it on purpose.</LoginSubtitle>
+        <LoginButtonsWrapper>
+          <LoginInput
             onChange={this.handleLogin}
             className="username"
             type="text"
             name="username"
-            placeholder="...username"
+            placeholder="...Type username"
             
           />
 
-          <input
+          <LoginInput
             onChange={this.handleLogin}
             className="password"
             type="text"
             name="password"
-            placeholder="...password"
+            placeholder="...Type password"
             
           />
 
-          <button className='login-btn' onClick={this.login}>Login</button>
-        </form>
+          <LoginButton onClick={this.login}>Login</LoginButton>
+        </LoginButtonsWrapper>
       </LoginWrapper>
     );
   }
