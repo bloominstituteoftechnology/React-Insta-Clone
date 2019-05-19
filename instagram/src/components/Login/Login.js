51853c5b6a7486c9dcb491653ecf638e038a17d0
Andrew <andrewryanogle@gmail.com>
2019-05-19 12:13:11 -0700

    deleted bootstrap line in login.js

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import { Button, Form, FormGroup, Input } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 class Login extends Component {
     constructor(props) {
