9aee7d977ade5c761a4c002162e0e4e6044a4c42
Zooheck <davidscottflack@gmail.com>
2019-05-22 22:02:56 -0500

    Make search bar mobile responsive

--- instagram-david/src/components/SearchBar/SearchBarStyles.js
+++ instagram-david/src/components/SearchBar/SearchBarStyles.js
@@ -5,6 +5,9 @@ export const SearchContainer = styled.div`
   width: 100%;
   padding: 20px 0;
   border-bottom: 2px solid #e2e2e2;
+  @media (max-width: 700px) {
+    padding: 20px 30px;
+  }
 `;
 export const SearchItemContainer = styled.div`
   max-width: 900px;
@@ -22,12 +25,18 @@ export const SearchHeading = styled.div`
 export const LogoIcon = styled.div`
   padding-right: 15px;
   border-right: 1px solid black;
+  @media (max-width: 700px) {
+    border-right: none;
+  }
 `;
 export const MainHeading = styled.h1`
   margin-left: 20px;
   font-size: 3rem;
   color: #343434;
   letter-spacing: 1px;
+  @media (max-width: 700px) {
+    display: none;
+  }
 `;
 export const SearchInput = styled.input`
   padding: 3px 18px;
@@ -39,6 +48,9 @@ export const SearchActionContainer = styled.div`
 `;
 export const SearchActionIcon = styled.div`
   margin-left: 10px;
+  @media (max-width: 700px) {
+    display: none;
+  }
 `;
 export const LogoutIcon = styled.div`
   margin-left: 10px;
