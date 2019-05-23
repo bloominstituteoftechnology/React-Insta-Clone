ccb0365229e628028fe0ae55906822512c9397bf
Zooheck <davidscottflack@gmail.com>
2019-05-22 20:23:22 -0500

    Move login styles to new file

--- instagram-david/src/components/Login/Login.js
+++ instagram-david/src/components/Login/Login.js
@@ -1,53 +1,13 @@
 import React from "react";
-import styled from "styled-components";
+import {
+  LoginPage,
+  LoginContainer,
+  Instagram,
+  LoginForm,
+  LoginInput,
+  LoginButton
+} from "./LoginStyles";
 
-const LoginPage = styled.div`
-  background-color: #fafafa;
-  height: 100vh;
-  padding-top: 50px;
-`;
-const LoginContainer = styled.div`
-  width: 400px;
-  margin: auto;
-  border: 1px solid #e6e6e6;
-  border-radius: 3px;
-  background-color: white;
-`;
-const Instagram = styled.h1`
-  font-family: "Grand Hotel", cursive;
-  text-align: center;
-  font-size: 4.5rem;
-  color: #343434;
-  letter-spacing: 1px;
-  margin-bottom: 10px;
-`;
-const LoginForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  padding: 15px;
-`;
-const LoginInput = styled.input`
-  padding: 10px;
-  width: 300px;
-  margin: auto;
-  margin-bottom: 10px;
-  border-radius: 4px;
-  border: 1px solid #e6e6e6;
-`;
-const LoginButton = styled.button`
-  width: 300px;
-  color: white;
-  background-color: #3897f0;
-  margin: auto;
-  border: none;
-  outline: none;
-  border-radius: 4px;
-  padding: 10px;
-  font-size: 1.5rem;
-  :active {
-    transform: scale(0.99);
-  }
-`;
 class Login extends React.Component {
   constructor() {
     super();
