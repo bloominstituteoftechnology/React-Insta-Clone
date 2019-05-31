6e5a6ab7d6db0809cca29631bdd0c125e337a9ce
Millan Figueroa <millan.fig@gmail.com>
2019-05-30 19:21:31 -0700

    Added props and map to app.js added images to postcontainer

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -2,9 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 const PostContainer = (props) => {
+
   return (
-    <div>PostContainer</div>
-  );
-}
+    
+    <div>
+       {props.post.map((post, index) => {
+
+         return (
+          <div key = {index}>
+           <div>{post.username}</div>
+           <div>{post.thumbnailUrl}</div>
+           <div><img src ={post.imageUrl} alt= "Something"></img> </div>
+           <div>{post.likes}</div>
+           <div>{post.timestamp}</div>
+           </div>
+         )
+        
+       }
+       )
+      }
+       </div>
+  )
+    }
+      
 
 export default PostContainer;
