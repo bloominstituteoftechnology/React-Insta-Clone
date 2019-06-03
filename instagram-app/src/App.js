61f97b5e9d452a3c353e748051787b60d8bb0f97
Rexben <rexben.rb@gmail.com>
2019-06-03 12:37:53 +0100

    import the dummy data and passed it as props to the PostContainer component

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import dummyData from "./dummy-data";
+import PostContainer from "./components/PostContainer/PostContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {dummyData.map(data => {
+        console.log(data);
+        return <PostContainer key={data.id} data={data} />;
+      })}
     </div>
   );
 }
---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const postContainer = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default postContainer;
