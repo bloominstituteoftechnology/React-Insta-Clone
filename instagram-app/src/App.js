9d29864f0aa0c1612a15b36ddccc9f7aebfd8487
LadyKerr <kadiekerr@gmail.com>
2019-06-03 15:00:20 -0400

    initial commit

---
+++ instagram-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from App!</h1>
+    </div>
+  );
+}
+
+export default App;
