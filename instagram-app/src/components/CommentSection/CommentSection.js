071149d535446d27f478dd4aecead300b82ac89a
Rexben <rexben.rb@gmail.com>
2019-06-03 12:42:16 +0100

    pass comment  as props to the CommentSection component

---
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const commentSection = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default commentSection;
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import CommentSection from '../CommentSection/CommentSection';
+
 const postContainer = (props) => {
+    const { data } = props;
     return (
         <div>
-            
+            {data.comments.map(comment => {
+                return <CommentSection comment={comment}/>
+            })}
         </div>
     )
 }
