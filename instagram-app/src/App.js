01f58838162fa730b8bd56de928f3b1aabd11b8b
LadyKerr <kadiekerr@gmail.com>
2019-06-03 15:32:48 -0400

    created App Component & moved data to src

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import dummyData from './dummy-data';
+import SearchBar from "./components/SearchBar/SearchBar";
+import PostContainer from "./components/PostContainer/PostContainer";
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from App!</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      postData: dummyData
+    };
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <h1>Hello from App!</h1>
+        <SearchBar />
+        <PostContainer 
+        postData = {this.state.postData}
+        />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
