fa07d6d597cd63806a4c716e19544fa98553b95f
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-06-04 04:22:11 -0500

    Add files via upload
    Components getting setup

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import dummyData from "./dummy-data";
+import './App.css';
+
+/*function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        
+        
+      </header>
+    </div>
+  );
+}*/
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: []
+    };
+  }};
+export default App;
