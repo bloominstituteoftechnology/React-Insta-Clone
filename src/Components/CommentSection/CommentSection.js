54bf2e131546ff46d75a4ee84f1a9b8a5c0d1e51
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-06-04 04:29:51 -0500

    Add files via upload
    Setting up existing comments

---
+++ src/Components/CommentSection/CommentSection.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from "react";
+import "./CommentSection.scss";
+import Comment from "../Comment/Comment";
+
+class CommentSection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: this.props.comments,
+      text: ''
+    };
+  }
+};
