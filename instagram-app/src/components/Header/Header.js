3b869f63c0d5a2fb83e0da3cd5adfa32989551f1
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-06-06 06:04:43 -0500

    Add files via upload

---
+++ instagram-app/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+    // src/components/Header/index.js
+    import React from "react";
+
+    class Header extends React.Component{
+        render(){
+            return (
+                 <div className="newContainer">
+                   <img src ="https://raw.githubusercontent.com/Bryce-Soghigian/React-Insta-Clone/master/Instagram.png" className = "Header-logo"/>
+                 </div>
+           );
+        }   
+    }
+    export default Header;
