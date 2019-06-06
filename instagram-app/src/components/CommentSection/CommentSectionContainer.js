af5407a75040e948f2a6260d257e32ac5129d033
JacobWellner83 <jcwellner83@gmail.com>
2019-06-05 20:45:26 -0500

    CommentSection constructor written and imports done.

---
+++ instagram-app/src/components/CommentSection/CommentSectionContainer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Comment from './Comment';
+import CommentForm from './CommentForm';
+
+class CommentSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comment: props.CommentSection
+        };
+    }
+    render() {
+        return(
+            <div>
+                {this.state.comments.map((b, a) => <Comment key={a} comment={b} />)}
+                <CommentForm />
+            </div>
+        );
+    }
+}
+
