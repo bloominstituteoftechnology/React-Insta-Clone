da01011fcfae4e8920fa1561df622ea6099aeb12
JacobWellner83 <jcwellner83@gmail.com>
2019-06-05 20:47:49 -0500

    CommentForm completed.

---
+++ instagram-app/src/components/CommentSection/CommentForm.js
@@ -0,0 +1,11 @@
+    import React from 'react';
+
+const CommentForm = props => {
+  return (
+    <form>
+      <input type="text" placeholder="Make a Comment!" />
+    </form>
+  );
+};
+
+export default CommentForm;
