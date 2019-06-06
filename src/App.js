4cb87daa65e90312079ce23ea711f33f1c81454c
PascalUlor <pascalulor@yahoo.com>
2019-06-06 15:13:14 +0100

    ft(deploy)

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import uuidv4 from "uuid/v4";
+import data from "./dummy-data";
+// import SearchBar from "./components/SearchBar/SearchBar";
+// import PostContainer from "./components/PostContainer/PostContainer";
+import withAuthenticate from './components/authentication/withAuthenticate';
+import PostPage from './components/PostContainer/PostPage';
+import Login from './components/Login/Login';
+import "./App.css";
+
+const preprocessData = data.map(post=> {
+  return {
+      ...post, postId: uuidv4()
+    }
+  })
+
+function App() {
+  const [posts, setData] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const allData = localStorage.getItem("posts");
+    let postData
+    if(allData) {
+      postData = JSON.parse(allData);
+    }else {
+      localStorage.setItem("posts", JSON.stringify(preprocessData));
+      postData = JSON.parse(localStorage.getItem("posts"));
+    }
+    setData(postData);
+  }, []);
+
+  const handleSearch = e => {
+    e.preventDefault();
+    const data = posts;
+    const query = [];
+    setSearch(e.target.value.toLowerCase());
+    data.map(user => {
+      if (user.username.toLowerCase().includes(search)) {
+        query.push(user);
+      }
+      return query;
+    });
+    setData(query);
+  };
+
+  const ComponentFromWithAuthenticate = withAuthenticate(PostPage, Login);
+
+  return (
+    <div className="App">
+      {/* <SearchBar search={search} handleSearch={handleSearch} />
+      {posts.map((userPost, index) => {
+        return <PostContainer 
+        key={index} 
+        props={userPost} 
+
+        />;
+      })} */}
+      <ComponentFromWithAuthenticate 
+        handleSearch={handleSearch}
+        search={search}
+        posts={posts}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PostContainer/PostContainer.js
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react";
+import pt from "prop-types";
+import uuidv4 from "uuid/v4";
+import moment from "moment";
+import faker from "faker";
+import { IoIosHeartEmpty } from "react-icons/io";
+import { FaRegComment } from "react-icons/fa";
+import CommentSection from "../CommentSection/CommentSection";
+import Form from "../Form/Form";
+import "./PostContainer.css";
+
+const PostContainer = ({ props }) => {
+  const {
+    postId,
+    comments,
+    thumbnailUrl,
+    imageUrl,
+    timestamp,
+    likes,
+    username
+  } = props;
+  const commentDate = timestamp.replace(/th/, "");
+  const [inputValue, setInputValue] = useState("");
+  const [inputComment, setInputComment] = useState(comments);
+  const [createdAt, setCreatedAt] = useState(
+    moment(new Date(commentDate), "MMM D LTS").fromNow()
+  );
+
+  const [addLikes, updateLikes] = useState(likes);
+console.log(createdAt);
+  useEffect(()=>{
+    const post = JSON.parse(localStorage.getItem("posts"));
+    const postUpdate = post.map((userPost) => {
+      if(postId === userPost.postId) {
+        return {
+          ...userPost, comments: inputComment, timestamp: `${moment(new Date(), "MMM D LTS")}`, likes: addLikes
+        }
+      }
+      return userPost;
+    });
+    localStorage.setItem("posts", JSON.stringify(postUpdate));
+  },[inputComment, postId, createdAt, addLikes])
+
+  const handleChange = e => {
+    setInputValue(e.target.value);
+  };
+  const postComment = e => {
+    e.preventDefault();
+    const newComment = {
+      postId: postId,
+      id: uuidv4(),
+      username: faker.name.findName(),
+      text: inputValue
+    };
+    setInputComment([...inputComment, newComment]);
+    setInputValue("");
+    setCreatedAt(moment(new Date(), "MMM D LTS").fromNow());
+  };
+  const handleLikes = () => {
+    let newLike = likes;
+    updateLikes(newLike + 1);
+  };
+
+  return (
+    <div className="post__container">
+      <div className="user__deets">
+        <img className="profile__pix" src={thumbnailUrl} alt="user-profile" />
+        <p>{username}</p>
+      </div>
+      <div className="user__post">
+        <img className="post__image" src={imageUrl} alt="user-post" />
+      </div>
+      <div className="reaction">
+        <div className="post__icons">
+          <span onClick={handleLikes}>
+            <IoIosHeartEmpty />
+          </span>
+
+          <span>
+            <FaRegComment />
+          </span>
+        </div>
+        {addLikes} likes
+      </div>
+      {inputComment.map(comment => {
+        return <CommentSection key={comment.id} props={comment} />;
+      })}
+      <div className="timestamp">{createdAt}</div>
+      <Form
+        inputValue={inputValue}
+        changeHandler={handleChange}
+        addComment={postComment}
+      />
+    </div>
+  );
+};
+
+export default PostContainer;
+
+PostContainer.propTypes = {
+  props: pt.shape({
+    comments: pt.arrayOf(
+      pt.shape({
+        id: pt.string.isRequired,
+        username: pt.string.isRequired,
+        text: pt.string.isRequired
+      })
+    ),
+    thumbnailUrl: pt.string.isRequired,
+    imageUrl: pt.string.isRequired,
+    timestamp: pt.string.isRequired,
+    likes: pt.number.isRequired,
+    username: pt.string.isRequired
+  }).isRequired
+};
