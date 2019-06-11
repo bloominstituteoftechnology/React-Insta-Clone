af55f3392f7704555b06f598e0f9b2aff66f3028
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-06-11 16:43:52 -0400

    post cont

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import SearchBar from './components/SearchBar';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
 
 function App() {
   return (
     <div className="App">
 
       <SearchBar />
+      <PostContainer />
       
     </div>
   );
---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CommentSection from '../CommentSection/CommentSection';
+
+
+class PostContainer extends React.Component {
+
+
+    render() {
+        return (
+           <CommentSection />
+        );
+    };
+}
+
+export default PostContainer;
