adb8c5fd7c1b0212b80fdd076d01495582abf113
JacobWellner83 <jcwellner83@gmail.com>
2019-06-11 21:00:43 -0500

    PostsPage created with imports

---
+++ instagram-app/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import dummyData from '../../dummy-data';
+import PostsContainer from './PostsContainer';
+import SearchBar from '../SearchBar/SearchBarContainer';
