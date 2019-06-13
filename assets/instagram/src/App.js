224dc80abb0bd7117501abf922b551e9cfb150ef
sweyome <sweyome1@gmail.com>
2019-06-12 21:37:29 -0400

    app.js update

--- assets/instagram/src/App.js
+++ assets/instagram/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import './App.css';
-import PostsPage from './components/PostContainer/PostsPage';
+import PostPage from './components/PostContainer/PostsPage';
 import Authenticate from './components/Authentication/Authenticate';
 
 class App extends Component {
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <PostsPage />
+        <PostPage />
       </div>
     );
   }
