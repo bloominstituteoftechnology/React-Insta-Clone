d7dd85dd0b9c551bdfda6fd075b8c5232647c66b
Crystal Martin <drcrystalm@gmail.com>
2019-06-18 23:23:48 -0500

    starting my project

---
+++ instagram-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./App.css"
+
+function App() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <p>Is this working?</p>
+            </header>
+        </div>
+    )
+}
+
+export default App
