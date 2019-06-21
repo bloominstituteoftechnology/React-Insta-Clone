7eba3acd1207a9b00f927a68a18afddba0d60a36
Juan Aleman <juan.j.aleman@gmail.com>
2019-06-21 17:41:21 -0500

    mvp

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import dummyData from "./dummy-data";
+import "./App.css";
+import PostContainer from "./components/PostContainer/postContainer";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {dummyData.map(data => {
+          return (
+            <div>
+              <PostContainer key={data.id} data={data} />
+            </div>
+          );
+        })}
       </header>
     </div>
   );
-}
+};
 
 export default App;
