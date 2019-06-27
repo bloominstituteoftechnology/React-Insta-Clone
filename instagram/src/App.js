dcc3e369fb8656c32e51a1d6406c04e20f8a123e
Kevin-S <kevsilvo@gmail.com>
2019-06-26 21:44:13 -0400

    dummy-data

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
+import { render } from "react-dom";
 import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+
+  constructor(){
+    super(dummyData);
+    this.state = {
+      data: dummyData
+    }
+  }
+
+    render(){
+
+      console.log(this.state.data);
+      return (
+        <div className="App">
+          <SearchBar />
+        </div>
+      );
+  }
 }
+
+
 
 export default App;
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SearchBar = props => {
+
+
+    return(
+      <div className="search-bar">
+             <img src=""/>
+      </div>
+    )
+}
+
+
+
+
+
+
+export default SearchBar;
