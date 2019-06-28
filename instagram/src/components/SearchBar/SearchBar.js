0b812a672ba9d97be713c50728e5a60ce634cb0f
Kevin-S <kevsilvo@gmail.com>
2019-06-27 22:58:05 -0400

    headV1

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import './search-bar.css';
+import logo from '../../img/logo.png';
+import explore from '../../img/explore.png';
+import heart from '../../img/heart.png';
+import profile from '../../img/profile.png';
+
 
 const SearchBar = props => {
 
 
     return(
       <div className="search-bar">
-             <img src=""/>
+             <img className="logo" src={logo}/>
+             <input className="bar" type="text" placeholder="Search"/>
+             <ul className="icons">
+                 <li><img className="explore" src={explore}/></li>
+                 <li><img className="heart" src={heart}/></li>
+                 <li><img className="profile" src={profile}/></li>
+             </ul>
+
       </div>
     )
 }
