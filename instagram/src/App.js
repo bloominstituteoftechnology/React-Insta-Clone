e7a00296bdd9c25d9d37e156bc97dff3858ccd4c
Phil Hart <philhart23@gmail.com>
2019-09-03 21:16:44 -0600

    Fixing up my app

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,47 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import dummyData from './dummy-data';
-import SearchBar from './components/SearchBar/searchBar';
-import PostContainer from './components/PostContainer/postContainer';
-
+import PostsPage from './components/PostsContainer/PostsPage';
+import Authenticate from './components/Authentication/Authenticate';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      dummyData: dummyData,
-      filteredPosts: [],
-      searchInput: "",
-    }
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ dummyData: dummyData });
-    }, 800);
-  }
-
-  Search = event => {
-    const posts = this.state.dummyData.filter(post => {
-      if (post.username.includes(event.target.value)) {
-        return post
-      }
-    })
-    this.setState({ filteredPosts: posts })
+    this.state = {};
   }
 
   render() {
     return (
       <div className="App">
-        <SearchBar search={this.Search} />
-        <PostContainer
-          dummyData={
-            this.state.filteredPosts.length > 0
-              ? this.state.filteredPosts
-              : this.state.dummyData} />
+        <PostsPage />
       </div>
     );
   }
 }
 
-export default App;
+export default Authenticate(App);
--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -2,14 +2,54 @@ import React, { Component } from 'react';
 import { Button, Form, FormGroup, Input } from 'reactstrap';
 import './Login.css';
 
-class  extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return (  );
-    }
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleLoginSubmit = e => {
+    const user = this.state.username;
+    localStorage.setItem('user', user);
+    window.location.reload();
+  };
+
+  render() {
+    return (
+        <Form className="login-form">
+        <h3 className="instaHeader">InstaClone</h3>
+        <FormGroup>
+          <Input
+            type="text"
+            placeholder="User Name"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleInputChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+          />
+          <br />
+          <Button color="info" size="large" onClick={this.handleLoginSubmit}>
+            Log In
+          </Button>
+        </FormGroup>
+      </Form>
+    );
+  }
 }
- 
-export default ;
+
+export default Login;
